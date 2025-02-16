19aebbd6b3dc769c857fb98a188073b499e06b0d
kamran saberifard <kamisaberi@gmail.com>
2025-02-17 01:55:09 +0330

    mnist dataset started to be intialized

--- torch-extension/include/datasets/mnist.h
+++ torch-extension/include/datasets/mnist.h
@@ -1,13 +1,53 @@
 #pragma once
 
+//#include <vector>
+#include <fstream>
+//#include <iostream>
+//#include <string>
+#include <filesystem>
+//#include <curl/curl.h>
 #include <torch/torch.h>
+#include <vector>
+#include <tuple>
+//#include <fstream>
+//#include <iostream>
+//#include <string>
+//#include <filesystem>
+#include "../utils/downloader.h"
+#include "../utils/archiver.h"
+#include "../utils/md5.h"
 #include "../exceptions/implementation.h"
+
+using namespace std;
+namespace fs = std::filesystem;
 
 
 namespace torch::ext::data::datasets {
-   class MNIST : torch::data::Dataset<MNIST> {
+    class MNIST : torch::data::Dataset<MNIST> {
 
     public :
-        MNIST();
+        MNIST(const std::string &root , bool train= true, bool download = false);
+
+        torch::data::Example<> get(size_t index) override;
+
+        torch::optional<size_t> size() const override;
+    private :
+        std::vector<torch::Tensor> data; // Store image data as tensors
+        std::vector<int64_t> labels;      // Store labels
+        std::string download_url_base = "https://ossci-datasets.s3.amazonaws.com/mnist/";
+        fs::path root;
+        fs::path dataset_path;
+        fs::path dataset_folder_name = "MNIST/raw";
+
+        vector<tuple<fs::path ,std::string >> resources = {
+                {fs::path("train-images-idx3-ubyte.gz"),"f68b3c2dcbeaaa9fbdd348bbdeb94873" },
+                {fs::path("train-labels-idx1-ubyte.gz"),"d53e105ee54ea40749a09fcbcd1e9432" },
+                {fs::path("t10k-images-idx3-ubyte.gz"),"9fb629c4189551a2d022fa330f9573f3" },
+                {fs::path("t10k-labels-idx1-ubyte.gz"),"ec29112dd5afa0611ce80d1b7f02629c" },
+        };
+
+        void load_data(bool train = true);
+
+
     };
 }
--- torch-extension/src/datasets/mnist.cpp
+++ torch-extension/src/datasets/mnist.cpp
@@ -2,8 +2,84 @@
 
 namespace torch::ext::data::datasets {
 
-    MNIST::MNIST() {
-        throw NotImplementedException();
+
+    //------------------ MNIST ------------------//
+    MNIST::MNIST(const std::string &root, bool train, bool download) {
+
+        bool res = true;
+        for (const auto &resource: this->resources) {
+            auto pth = std::get<0>(resource);
+            auto md = std::get<1>(resource);
+        }
+
+//        // Load data from the specified root directory
+//        this->root = fs::path(root);
+//        this->dataset_path = this->root / this->dataset_folder_name;
+
+//        if (download) {
+//            bool should_download = false;
+//            if (!fs::exists(this->root / this->archive_file_name)) {
+//                should_download = true;
+//            } else {
+//                std::string md5 = md5File((this->root / this->archive_file_name).string());
+//                if (md5 != archive_file_md5) {
+//                    should_download = true;
+//                }
+//            }
+//            if (should_download) {
+//                auto [result, path] = download_data(this->download_url, this->root.string());
+//                res = result;
+//            }
+//            if (res) {
+//                string pth = (this->root / this->archive_file_name).string();
+//                res = extract(pth, this->root);
+//            }
+//        }
+        if (res) {
+            load_data(train);
+        }
     }
+
+    torch::data::Example<> MNIST::get(size_t index) {
+        return {data[index].clone(), torch::tensor(labels[index])}; // Clone to ensure tensor validity
+    }
+
+    torch::optional<size_t> MNIST::size() const {
+        return data.size();
+    }
+
+    void MNIST::load_data(bool train) {
+
+
+        const int num_files = 5;
+        for (int i = 1; i <= num_files; ++i) {
+            std::string file_path = root / "/data_batch_" / std::to_string(i) / ".bin";
+            std::ifstream file(file_path, std::ios::binary);
+            if (!file.is_open()) {
+                std::cerr << "Failed to open file: " << file_path << std::endl;
+                continue;
+            }
+
+            for (int j = 0; j < 10000; ++j) {
+                uint8_t label;
+                file.read(reinterpret_cast<char *>(&label), sizeof(label));
+                labels.push_back(static_cast<int64_t>(label));
+
+                std::vector<uint8_t> image(3072); // 32x32x3 = 3072
+                file.read(reinterpret_cast<char *>(image.data()), image.size());
+
+                // Reshape the image to 3x32x32 and convert to a Torch tensor
+                auto tensor_image = torch::from_blob(image.data(), {3, 32, 32},
+                                                     torch::kByte).clone(); // Clone to ensure memory management
+                tensor_image = tensor_image.permute(
+                        {0, 2, 1}); // Permute to get the correct order (C, H, W)
+
+                data.push_back(tensor_image); // Store the tensor in the data vector
+            }
+
+            file.close();
+        }
+    }
+
 
 }
