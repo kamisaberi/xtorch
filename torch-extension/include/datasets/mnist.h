cf1a94c62ddb87269138aaac5e3e0d54295505f6
kamran saberifard <kamisaberi@gmail.com>
2025-02-17 02:53:22 +0330

    fashion-mnist merged to mnist files

--- torch-extension/include/datasets/mnist.h
+++ torch-extension/include/datasets/mnist.h
@@ -23,6 +23,10 @@ namespace fs = std::filesystem;
 
 
 namespace torch::ext::data::datasets {
+
+    std::vector<std::vector<uint8_t>> read_mnist_images(const std::string &file_path, int num_images);
+    std::vector<uint8_t> read_mnist_labels(const std::string &file_path, int num_labels);
+
     class MNIST : torch::data::Dataset<MNIST> {
 
     public :
@@ -50,4 +54,32 @@ namespace torch::ext::data::datasets {
 
 
     };
+
+
+
+    class FashionMNIST : public torch::data::datasets::Dataset<FashionMNIST> {
+    private:
+        torch::Tensor images_;
+        torch::Tensor labels_;
+        std::string download_url_base = "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/";
+        fs::path root;
+        fs::path dataset_path;
+        fs::path dataset_folder_name = "MNIST/raw";
+
+        vector<tuple<fs::path ,std::string >> resources = {
+                {fs::path("train-images-idx3-ubyte.gz"),"8d4fb7e6c68d591d4c3dfef9ec88bf0d" },
+                {fs::path("train-labels-idx1-ubyte.gz"),"25c81989df183df01b3e8a0aad5dffbe" },
+                {fs::path("t10k-images-idx3-ubyte.gz"),"bef4ecab320f06d8554ea6380940ec79" },
+                {fs::path("t10k-labels-idx1-ubyte.gz"),"bb300cfdad3c16e7a12a480ee83cd310" },
+        };
+
+    public:
+        // Constructor: Loads images and labels from files
+        FashionMNIST(const std::string &images_path, const std::string &labels_path, int num_samples);
+        torch::data::Example<> get(size_t index) override;
+        torch::optional<size_t> size() const override;
+    };
+
+
+
 }
