d588a272d604ff23e0767b3e5282ebd917773ff0
kamran saberifard <kamisaberi@gmail.com>
2025-02-18 00:01:51 +0330

    emnist merged to mnist files

--- torch-extension/include/datasets/mnist.h
+++ torch-extension/include/datasets/mnist.h
@@ -109,5 +109,25 @@ namespace torch::ext::data::datasets {
 
     };
 
+    class EMNIST : public torch::data::Dataset<EMNIST> {
+    private:
+        torch::Tensor images_;
+        torch::Tensor labels_;
+        std::string download_url_base = "https://biometrics.nist.gov/cs_links/EMNIST/";
+        fs::path root;
+        fs::path dataset_path;
+        fs::path dataset_folder_name = "EMNIST/raw";
+        fs::path archive_file_name = "gzip.zip";
+        std::string archive_file_md5 = "58c8d27c78d21e728a6bc7b3cc06412e";
+
+    public :
+        EMNIST(const std::string &images_path, const std::string &labels_path, int num_samples);
+
+        torch::data::Example<> get(size_t index) override;
+
+        torch::optional<size_t> size() const override;
+
+    };
+
 
 }
--- torch-extension/src/datasets/emnist.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../include/datasets/emnist.h"
-
-namespace torch::ext::data::datasets {
-
-    EMNIST::EMNIST() {
-        throw NotImplementedException();
-    }
-
-}
