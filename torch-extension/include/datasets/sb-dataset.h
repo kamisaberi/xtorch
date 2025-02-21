697fc10fa503b83a65df79c09716941312f822f1
kamran saberifard <kamisaberi@gmail.com>
2025-02-21 10:31:52 +0330

    sbd datasets list added

--- torch-extension/include/datasets/sb-dataset.h
+++ torch-extension/include/datasets/sb-dataset.h
@@ -1,13 +1,39 @@
 #pragma once
-
+//#include <vector>
+#include <fstream>
+//#include <iostream>
+//#include <string>
+#include <filesystem>
+//#include <curl/curl.h>
 #include <torch/torch.h>
+#include <vector>
+#include <tuple>
+#include <map>
+//#include <fstream>
+//#include <iostream>
+//#include <string>
+//#include <filesystem>
+#include "../utils/downloader.h"
+#include "../utils/archiver.h"
+#include "../utils/md5.h"
 #include "../exceptions/implementation.h"
+
+using namespace std;
+namespace fs = std::filesystem;
 
 
 namespace torch::ext::data::datasets {
-   class SBDataset : torch::data::Dataset<SBDataset> {
+    class SBDataset : torch::data::Dataset<SBDataset> {
+    private:
+        fs::path url = fs::path("https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz");
+        std::string md5 = "82b4d87ceb2ed10f6038a1cba92111cb";
+        fs::path filename = fs::path("benchmark.tgz");
+
+        fs::path voc_train_url = fs::path("https://www.cs.cornell.edu/~bharathh/train_noval.txt");
+        fs::path voc_split_filename = fs::path("train_noval.txt");
+        std::string voc_split_md5 = "79bff800c5f0b1ec6b21080a3c066722";
 
     public :
-       SBDataset();
+        SBDataset();
     };
 }
