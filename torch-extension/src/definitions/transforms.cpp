3e90b52c6e0518a7966d95ca285b4b6a626a64bc
kamran saberifard <kamisaberi@gmail.com>
2025-02-27 23:39:33 +0330

    pad transform created

--- torch-extension/src/definitions/transforms.cpp
+++ torch-extension/src/definitions/transforms.cpp
@@ -10,11 +10,28 @@ namespace torch::ext::data::transforms {
         ).squeeze(0);
     }
 
+    torch::Tensor pad_tensor(const torch::Tensor &tensor,  int size) {
+        std::vector<int64_t> ssize = {size, size};
+        return torch::nn::functional::interpolate(
+            tensor.unsqueeze(0),
+            torch::nn::functional::InterpolateFuncOptions().size(ssize).mode(torch::kBilinear).align_corners(false)
+        ).squeeze(0);
+    }
+
 
     torch::data::transforms::Lambda<torch::data::Example<>> resize(std::vector<int64_t> size) {
         return torch::data::transforms::Lambda<torch::data::Example<> >(
             [size](torch::data::Example<> example) {
                 example.data = resize_tensor(example.data, size);
+                return example;
+            }
+        );
+    }
+
+    torch::data::transforms::Lambda<torch::data::Example<>> pad(int size) {
+        return torch::data::transforms::Lambda<torch::data::Example<> >(
+            [size](torch::data::Example<> example) {
+                example.data = pad_tensor(example.data, size);
                 return example;
             }
         );
