9a67422e37a89f52b840b5f591bda9ad933efb89
kamran saberifard <kamisaberi@gmail.com>
2025-02-28 10:45:01 +0330

    lenet5 model simplified and code cleaned

--- torch-extension/src/models/cnn/lenet5.cpp
+++ torch-extension/src/models/cnn/lenet5.cpp
@@ -5,16 +5,6 @@ using namespace std;
 
 namespace torch::ext::models {
     LeNet5::LeNet5(int num_classes) {
-        // layer1 = torch::nn::Sequential();
-        // torch::nn::Conv2d conv1 = torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 6, 5).stride(1).padding(0));
-        // layer1->push_back(conv1);
-        // torch::nn::BatchNorm2d batch1 = torch::nn::BatchNorm2d(6);
-        // layer1->push_back(batch1);
-        // torch::nn::ReLU relu1 = torch::nn::ReLU();
-        // layer1->push_back(relu1);
-        // torch::nn::MaxPool2d pool1 = torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(2));
-        // layer1->push_back(pool1);
-
         layer1 = torch::nn::Sequential(
             torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 6, 5).stride(1).padding(0)),
             torch::nn::BatchNorm2d(6),
@@ -22,20 +12,20 @@ namespace torch::ext::models {
             torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(2))
         );
 
-        register_module("layer1", layer1);
-
         layer2 = torch::nn::Sequential(
             torch::nn::Conv2d(torch::nn::Conv2dOptions(6, 16, 5).stride(1).padding(0)),
             torch::nn::BatchNorm2d(16),
             torch::nn::ReLU(),
             torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(2))
         );
-        register_module("layer2", layer2);
 
         fc1 = torch::nn::Linear(400, 120);
         fc2 = torch::nn::Linear(120, 84);
         fc3 = torch::nn::Linear(84, num_classes);
 
+
+        register_module("layer1", layer1);
+        register_module("layer2", layer2);
         register_module("fc1", fc1);
         register_module("fc2", fc2);
         register_module("fc3", fc3);
