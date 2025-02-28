41a266e6b732b8fc20fd9db1ea5ab78530ffd399
kamran saberifard <kamisaberi@gmail.com>
2025-02-28 11:41:00 +0330

    lenet5 model simplified and code cleaned

--- torch-extension/include/models/cnn/lenet5.h
+++ torch-extension/include/models/cnn/lenet5.h
@@ -5,15 +5,16 @@
 
 
 using namespace std;
-namespace torch::ext::models {
 
+namespace torch::ext::models {
     struct LeNet5 : torch::nn::Module {
+    private:
         torch::nn::Sequential layer1 = nullptr, layer2 = nullptr;
         torch::nn::Linear fc1 = nullptr, fc2 = nullptr, fc3 = nullptr;
 
+    public:
         LeNet5(int num_classes);
 
         torch::Tensor forward(torch::Tensor x);
     };
 }
-
