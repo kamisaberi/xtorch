22cb41c62e9ad40ef1ca6ee7b0b7561e00e55ed1
kamran saberifard <kamisaberi@gmail.com>
2025-03-01 21:17:39 +0330

    MNIST Dataset constructor bool tarin changed to DataMode mode

--- torch-extension/mnt-t.cpp
+++ torch-extension/mnt-t.cpp
@@ -58,14 +58,48 @@ void set_random() {
 int main() {
     std::vector<int64_t> size = {32, 32};
 
+
+    // auto resize_transform = torch::data::transforms::Lambda<torch::data::Example<> >(
+    //     [size](torch::data::Example<> example) {
+    //         example.data = resize_tensor(example.data, size);
+    //         return example;
+    //     }
+    // );
+
+
     std::cout.precision(10);
     torch::Device device(torch::kCPU);
+    // Load the MNIST dataset
 
+
+    // std::vector<std::shared_ptr<torch::data::transforms::Lambda<torch::data::Example<> > > > transforms;
+    // transforms.push_back(std::make_shared<normalize_transform>());
+    //    std::vector<std::function<torch::data::Example<>>> transforms;
+    //    std::vector<torch::data::transforms::Transform<Input, Output>> transforms;
+    //    transforms.push_back(torch::data::transforms::Normalize<>(0.5,0.5));
+    //    transforms.push_back(resize_transform);
+
+    // auto dataset = torch::ext::data::datasets::MNIST("/home/kami/Documents/temp/",
+    //                                                  {.mode = DataMode::TRAIN, .download = true});
     auto dataset = torch::ext::data::datasets::MNIST("/home/kami/Documents/temp/", DataMode::TRAIN, true);
 
+    // cout << typeid(dataset).name() << endl;
+
+    // Apply the resize transform to the dataset
     auto transformed_dataset = dataset.map(torch::ext::data::transforms::resize({32, 32}))
             .map(torch::ext::data::transforms::normalize(0.5, 0.5))
             .map(torch::data::transforms::Stack<>());
+
+
+    // auto sssss = transformed_dataset.size();
+    // cout << sssss.value() << endl;
+
+    // cout << typeid(transformed_dataset).name() << endl;
+    // auto ss =  transformed_dataset.get_batch(0).data.sizes();
+    // cout << "SIZES : " << ss << endl;
+    // auto sss = transformed_dataset.dataset().size();
+    // cout << "SIZES : " << sss.value() << endl;
+
 
     vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms = {
         torch::ext::data::transforms::resize({32, 32}),
@@ -73,6 +107,44 @@ int main() {
     };
     auto dataset2 = torch::ext::data::datasets::MNIST("/home/kami/Documents/temp/",
                                                  {.mode = DataMode::TRAIN, .download = true , .transforms = transforms});
+    // for (const auto &transform: transforms) {
+    //     auto t  = dataset.map(transform);
+    //     cout << t.get_batch(0).data()->data[0] << endl;
+    // }
+    // return 0;
+
+
+    // cout << dataset.get(0).data << endl;
+    // dataset = dataset.map(torch::data::transforms::Stack<>());
+    // cout << dataset.get(0).data << endl;
+
+    // auto ld = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(std::move(dataset), 64);
+    // for (size_t epoch = 0; epoch != 10; ++epoch) {
+    //     size_t batch_index = 0;
+    //     auto ld_iter = ld->begin();
+    //     auto ld_end = ld->end();
+    //     while (ld_iter != ld_end) {
+    //         torch::Tensor data, targets;
+    //         auto batch = *ld_iter;
+    //         data = batch[0].data;
+    //         targets = batch[0].target;
+    //         cout << data << endl;
+    //         return 0;
+    //
+    //         // auto targets = batch.target;
+    //     }
+    // }
+
+
+    // auto rs = torch::ext::data::transforms::resize({32, 32});
+    // torch::data::datasets::MapDataset<torch::ext::data::datasets::MNIST, torch::data::transforms::Lambda<
+    //     torch::data::Example<> > > dataset1 = dataset.map(transforms[0]);
+    // for (int i = 1 ; i < transforms.size();i++) {
+    //     dataset1 = dataset.map(transforms[i]);
+    // }
+    // cout << dataset1.size().value() << endl;
+
+    // cout << transformed_dataset2.get_batch(0).data << endl;
 
     auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
         std::move(transformed_dataset), 64);
@@ -106,8 +178,5 @@ int main() {
             ++train_loader_interator;
         }
     }
-
-
-
     return 0;
 }
