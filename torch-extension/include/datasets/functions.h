32b3a7f61708777a0a917bf256ea0f1cb262a045
kamran saberifard <kamisaberi@gmail.com>
2025-03-04 00:58:16 +0330

    transform_dataset function created

--- torch-extension/include/datasets/functions.h
+++ torch-extension/include/datasets/functions.h
@@ -1,5 +1,29 @@
 #pragma once
 
+//#include <vector>
+#include <fstream>
+//#include <iostream>
+//#include <string>
+#include <filesystem>
+//#include <curl/curl.h>
+#include <torch/torch.h>
+#include <vector>
+#include <tuple>
+#include <map>
+//#include <fstream>
+//#include <iostream>
+//#include <string>
+//#include <filesystem>
+#include "base.h"
+#include "../utils/downloader.h"
+#include "../utils/archiver.h"
+#include "../utils/md5.h"
+#include "../exceptions/implementation.h"
+#include "../definitions/transforms.h"
+#include "../types/arguments.h"
+
+
 namespace torch::ext::data {
-    void transform_dataset();
+    torch::data::datasets::MapDataset<torch::ext::data::datasets::BaseDataset , torch::data::transforms::Stack<>()>
+    transform_dataset(torch::ext::data::datasets::BaseDataset, vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms);
 }
--- torch-extension/src/datasets/functions.cpp
+++ torch-extension/src/datasets/functions.cpp
@@ -1,7 +1,12 @@
 #include "../../include/datasets/functions.h"
 
+#include <torch/data/datasets/map.h>
+
+#include "../../include/datasets/base.h"
+
 namespace torch::ext::data {
-    void transform_dataset() {
+    torch::data::datasets::MapDataset<torch::ext::data::datasets::BaseDataset , torch::data::transforms::Stack<>()>
+    transform_dataset(torch::ext::data::datasets::BaseDataset, vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms ) {
 
     }
 
