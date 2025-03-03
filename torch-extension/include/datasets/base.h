5bbc83f4dd49f0ad8859d6918ac68c75679e9dd0
kamran saberifard <kamisaberi@gmail.com>
2025-03-04 00:16:22 +0330

    BaseDataset class created

--- torch-extension/include/datasets/base.h
+++ torch-extension/include/datasets/base.h
@@ -1,1 +1,58 @@
 #pragma once
+
+//#include <vector>
+#include <fstream>
+//#include <iostream>
+//#include <string>
+#include <filesystem>
+//#include <curl/curl.h>
+#include <torch/torch.h>
+#include <vector>
+#include <tuple>
+#include <map>
+//#include <fstream>
+//#include <iostream>
+//#include <string>
+//#include <filesystem>
+#include "../utils/downloader.h"
+#include "../utils/archiver.h"
+#include "../utils/md5.h"
+#include "../exceptions/implementation.h"
+#include "../definitions/transforms.h"
+#include "../types/arguments.h"
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace torch::ext::data::datasets {
+
+    class BaseDataset : public torch::data::Dataset<BaseDataset> {
+    public:
+        BaseDataset(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        BaseDataset(const fs::path &root, DatasetArguments args);
+
+        void read_images(const std::string &file_path, int num_images);
+
+        void read_labels(const std::string &file_path, int num_labels);
+
+        torch::data::Example<> get(size_t index) override;
+
+        torch::optional<size_t> size() const override;
+
+    protected:
+        std::vector<torch::Tensor> data; // Store image data as tensors
+        std::vector<uint8_t> labels; // Store labels
+        DataMode mode = DataMode::TRAIN;
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void transform_data(std::vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms);
+
+    private:
+        vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms = {};
+    };
+}
