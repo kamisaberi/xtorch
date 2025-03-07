20cf59b0d174394e4f9f2109366b07106feeb42c
kamran saberifard <kamisaberi@gmail.com>
2025-03-08 01:10:03 +0330

    datasets.h file completed

--- torch-extension/include/base/datasets.h
+++ torch-extension/include/base/datasets.h
@@ -2,10 +2,16 @@
 
 #include <fstream>
 #include <filesystem>
+#include <vector>
+#include <tuple>
+#include <map>
 #include <torch/torch.h>
 #include "../utils/downloader.h"
 #include "../utils/extract.h"
 #include "../utils/md5.h"
 #include "../exceptions/implementation.h"
+#include "../definitions/transforms.h"
+#include "../types/arguments.h"
+#include "../types/enums.h"
 
 
--- torch-extension/include/datasets/image-folder.h
+++ torch-extension/include/datasets/image-folder.h
@@ -1,8 +1,2 @@
-//
-// Created by kami on 3/4/25.
-//
+#pragma once
 
-#ifndef BASE_H
-#define BASE_H
-
-#endif //BASE_H
