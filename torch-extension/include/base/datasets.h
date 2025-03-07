3575a7dae8e8a017e867105318135e3042d0d83b
kamran saberifard <kamisaberi@gmail.com>
2025-03-07 23:03:31 +0330

    base folder created and datasets.h created to keep all includes files

---
+++ torch-extension/include/base/datasets.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <fstream>
+#include <filesystem>
+#include <torch/torch.h>
+#include "../utils/downloader.h"
+#include "../utils/extract.h"
+#include "../utils/md5.h"
+#include "../exceptions/implementation.h"
+
+
