7b178438773554802cf552c689851e4554b71276
kamran saberifard <kamisaberi@gmail.com>
2025-03-08 01:33:29 +0330

    datasets.h included to datasets

--- torch-extension/include/datasets/kitti.h
+++ torch-extension/include/datasets/kitti.h
@@ -1,23 +1,6 @@
 #pragma once
 
-//#include <vector>
-#include <fstream>
-//#include <iostream>
-//#include <string>
-#include <filesystem>
-//#include <curl/curl.h>
-#include <torch/torch.h>
-#include <vector>
-#include <tuple>
-#include <map>
-//#include <fstream>
-//#include <iostream>
-//#include <string>
-//#include <filesystem>
-#include "../utils/downloader.h"
-#include "../utils/archiver.h"
-#include "../utils/md5.h"
-#include "../exceptions/implementation.h"
+#include "../base/datasets.h"
 
 
 namespace torch::ext::data::datasets {
