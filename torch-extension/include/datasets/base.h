5ce5f7c4d190006100bbf11854bab2dcbf0c1d7a
kamran saberifard <kamisaberi@gmail.com>
2025-03-08 01:28:44 +0330

    datasets.h file completed

--- torch-extension/include/datasets/base.h
+++ torch-extension/include/datasets/base.h
@@ -1,26 +1,6 @@
 #pragma once
 
-//#include <vector>
-#include <fstream>
-//#include <iostream>
-//#include <string>
-#include <filesystem>
-//#include <curl/curl.h>
-#include <torch/torch.h>
-#include <vector>
-#include <tuple>
-#include <map>
-//#include <fstream>
-//#include <iostream>
-//#include <string>
-//#include <filesystem>
-#include "../utils/downloader.h"
-#include "../utils/extract.h"
-#include "../utils/md5.h"
-#include "../exceptions/implementation.h"
-#include "../definitions/transforms.h"
-#include "../types/arguments.h"
-
+#include "../base/datasets.h"
 
 using namespace std;
 namespace fs = std::filesystem;
