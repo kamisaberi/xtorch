993a39e9aa4a205711fcefee7d73f86e83f64090
kamran saberifard <kamisaberi@gmail.com>
2025-03-10 00:56:19 +0330

    Country211 inherited from BaseDatset

--- torch-extension/include/datasets/country.h
+++ torch-extension/include/datasets/country.h
@@ -3,20 +3,17 @@
 #include "base.h"
 
 
-
 using namespace std;
 namespace fs = std::filesystem;
 
 namespace torch::ext::data::datasets {
-   class Country211 : BaseDataset {
-   private:
-
-       std::string download_url = "https://openaipublic.azureedge.net/clip/data/";
-       fs::path archive_file_name = "country211.tgz";
-       std::string archive_file_md5 = "84988d7644798601126c29e9877aab6a";
-
+    class Country211 : BaseDataset {
+    private:
+        std::string download_url = "https://openaipublic.azureedge.net/clip/data/";
+        fs::path archive_file_name = "country211.tgz";
+        std::string archive_file_md5 = "84988d7644798601126c29e9877aab6a";
 
     public :
-       Country211();
+        Country211();
     };
 }
