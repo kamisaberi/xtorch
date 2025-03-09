ffbc4885020177e23b0927003b44908329be3161
kamran saberifard <kamisaberi@gmail.com>
2025-03-10 02:58:35 +0330

    Cityscapes constructors intialized

--- torch-extension/include/datasets/cityscapes.h
+++ torch-extension/include/datasets/cityscapes.h
@@ -4,9 +4,10 @@
 
 
 namespace torch::ext::data::datasets {
-   class Cityscapes : BaseDataset {
-
+    class Cityscapes : BaseDataset {
     public :
-       Cityscapes();
+        Cityscapes(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        Cityscapes(const fs::path &root, DatasetArguments args);
     };
 }
