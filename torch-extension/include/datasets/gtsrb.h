e0b2c8711bab8c973acf8bb5597575a012225975
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 02:33:05 +0330

    GTSRB constructors intialized

--- torch-extension/include/datasets/gtsrb.h
+++ torch-extension/include/datasets/gtsrb.h
@@ -1,12 +1,18 @@
 #pragma once
+#include "base.h"
 #include "../base/datasets.h"
 
 
-
 namespace torch::ext::data::datasets {
-   class GTSRB : torch::data::Dataset<GTSRB> {
-
+    class GTSRB : BaseDataset {
     public :
-       GTSRB();
+        GTSRB(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        GTSRB(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
