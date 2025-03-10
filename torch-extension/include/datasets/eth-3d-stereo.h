01e41d79bbcfc2b29ebc63d410abf0859c59a437
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 01:16:22 +0330

    ETH3DStereo constructors intialized

--- torch-extension/include/datasets/eth-3d-stereo.h
+++ torch-extension/include/datasets/eth-3d-stereo.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "../base/datasets.h"
+#include "base.h"
 
 
 
 namespace torch::ext::data::datasets {
-   class ETH3DStereo : torch::data::Dataset<ETH3DStereo> {
+   class ETH3DStereo : BaseDataset {
+
+       ETH3DStereo(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+       ETH3DStereo(const fs::path &root, DatasetArguments args);
 
     public :
        ETH3DStereo();
