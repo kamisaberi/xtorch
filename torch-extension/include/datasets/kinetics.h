24b167417c5d8ea3d436a78a1b38b6ff76e96726
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 02:53:54 +0330

    Kinetics constructors intialized

--- torch-extension/include/datasets/kinetics.h
+++ torch-extension/include/datasets/kinetics.h
@@ -1,12 +1,18 @@
 #pragma once
+#include "base.h"
 #include "../base/datasets.h"
 
 
-
 namespace torch::ext::data::datasets {
-   class Kinetics : torch::data::Dataset<Kinetics> {
-
+    class Kinetics : BaseDataset {
     public :
-       Kinetics();
+        Kinetics(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        Kinetics(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
