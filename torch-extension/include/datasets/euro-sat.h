29128dec465070ad35793e7a3d65208815786713
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 01:17:48 +0330

    EuroSAT constructors intialized

--- torch-extension/include/datasets/euro-sat.h
+++ torch-extension/include/datasets/euro-sat.h
@@ -1,20 +1,27 @@
 #pragma once
 
 #include "../base/datasets.h"
-
+#include "base.h"
 
 using namespace std;
 namespace fs = std::filesystem;
 
 
 namespace torch::ext::data::datasets {
-   class EuroSAT : torch::data::Dataset<EuroSAT> {
-   private:
-       std::string download_url = "https://huggingface.co/datasets/torchgeo/eurosat/resolve/c877bcd43f099cd0196738f714544e355477f3fd/EuroSAT.zip";
-       fs::path archive_file_name = "EuroSAT.zip";
-       std::string archive_file_md5 = "c8fa014336c82ac7804f0398fcb19387";
+    class EuroSAT : BaseDataset {
+        public :
+    EuroSAT(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+        EuroSAT(const fs::path &root, DatasetArguments args);
 
-   public :
-       EuroSAT();
+    private:
+        std::string download_url =
+                "https://huggingface.co/datasets/torchgeo/eurosat/resolve/c877bcd43f099cd0196738f714544e355477f3fd/EuroSAT.zip";
+        fs::path archive_file_name = "EuroSAT.zip";
+        std::string archive_file_md5 = "c8fa014336c82ac7804f0398fcb19387";
+
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
+
     };
 }
