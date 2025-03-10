4e82d112982da9e5a01a34fdf3e711b783a08b76
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 02:00:28 +0330

    FGVCAircraft constructors intialized

--- torch-extension/include/datasets/fgvc-aircraft.h
+++ torch-extension/include/datasets/fgvc-aircraft.h
@@ -1,12 +1,18 @@
 #pragma once
 #include "../base/datasets.h"
-
+#include "base.h"
 
 
 namespace torch::ext::data::datasets {
-   class FGVCAircraft : torch::data::Dataset<FGVCAircraft> {
-
+    class FGVCAircraft : BaseDataset {
     public :
-       FGVCAircraft();
+        FGVCAircraft(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        FGVCAircraft(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
