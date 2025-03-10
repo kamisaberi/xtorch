5feb282ad89f0344c39f81ffc598059b73ae3c72
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 00:59:31 +0330

    CREStereo constructors intialized

--- torch-extension/include/datasets/cre-stereo.h
+++ torch-extension/include/datasets/cre-stereo.h
@@ -9,16 +9,25 @@ namespace torch::ext::data::datasets {
     class CarlaStereo : BaseDataset {
     public :
         CarlaStereo(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
         CarlaStereo(const fs::path &root, DatasetArguments args);
 
     private :
         void load_data(DataMode mode = DataMode::TRAIN);
+
         void check_resources(const std::string &root, bool download = false);
     };
 
 
-    class CREStereo : torch::data::Dataset<CREStereo> {
+    class CREStereo : BaseDataset {
     public :
-        CREStereo();
+        CREStereo(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        CREStereo(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
