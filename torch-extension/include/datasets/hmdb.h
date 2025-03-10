a04356af5f7106ed1b9b1d599a65d7bf1fcfd90c
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 02:34:19 +0330

    HMDB51 constructors intialized

--- torch-extension/include/datasets/hmdb.h
+++ torch-extension/include/datasets/hmdb.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include "base.h"
 #include "../base/datasets.h"
 
 
-
 namespace torch::ext::data::datasets {
-   class HMDB51 : torch::data::Dataset<HMDB51> {
-
+    class HMDB51 : BaseDataset {
     public :
-       HMDB51();
+        HMDB51(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        HMDB51(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
