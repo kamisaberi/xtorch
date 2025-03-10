9e9a3ccd91bd01024017195e56aaeb5a46e808e2
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 01:00:45 +0330

    DTD constructors intialized

--- torch-extension/include/datasets/dtd.h
+++ torch-extension/include/datasets/dtd.h
@@ -4,11 +4,15 @@
 
 
 namespace torch::ext::data::datasets {
-   class DTD : BaseDataset {
-
+    class DTD : BaseDataset {
     public :
-    DTD(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
-       DTD(const fs::path &root, DatasetArguments args);
+        DTD(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
+        DTD(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
