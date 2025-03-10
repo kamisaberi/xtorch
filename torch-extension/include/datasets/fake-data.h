a40a06468415ae330422af7044f8a9f3a4fd8641
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 01:28:31 +0330

    FakeData constructors intialized

--- torch-extension/include/datasets/fake-data.h
+++ torch-extension/include/datasets/fake-data.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include "../base/datasets.h"
-
+#include "base.h"
 
 
 namespace torch::ext::data::datasets {
-   class FakeData : torch::data::Dataset<FakeData> {
-
+    class FakeData : BaseDataset {
     public :
-       FakeData();
+        FakeData(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        FakeData(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
