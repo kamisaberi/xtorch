cd74977236fe7ff5c8dbb462764c2d3a248bd075
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 02:23:07 +0330

    Food101 constructors intialized

--- torch-extension/include/datasets/food.h
+++ torch-extension/include/datasets/food.h
@@ -1,20 +1,25 @@
 #pragma once
+#include "base.h"
 #include "../base/datasets.h"
-
 
 
 using namespace std;
 namespace fs = std::filesystem;
 
 namespace torch::ext::data::datasets {
-   class Food101 : torch::data::Dataset<Food101> {
-   private :
-       std::string download_url  = "http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz";
-       fs::path archive_file_name = "food-101.tar.gz";
-       std::string archive_file_md5 = "85eeb15f3717b99a5da872d97d918f87";
+    class Food101 : BaseDataset {
+    public :
+        Food101(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
+        Food101(const fs::path &root, DatasetArguments args);
 
-   public :
-       Food101();
+    private :
+        std::string download_url = "http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz";
+        fs::path archive_file_name = "food-101.tar.gz";
+        std::string archive_file_md5 = "85eeb15f3717b99a5da872d97d918f87";
+
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
