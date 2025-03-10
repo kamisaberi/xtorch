a2e534c02de8ae8d4f06bb6d80602836b7613bc2
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 01:40:49 +0330

    FER2013 constructors intialized

--- torch-extension/include/datasets/fer.h
+++ torch-extension/include/datasets/fer.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include "../base/datasets.h"
+#include "base.h"
 
 
 namespace torch::ext::data::datasets {
-   class FER2013 : torch::data::Dataset<FER2013> {
-
+    class FER2013 : BaseDataset {
     public :
-       FER2013();
+        FER2013(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        FER2013(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
