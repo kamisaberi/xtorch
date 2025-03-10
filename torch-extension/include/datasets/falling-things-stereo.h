ee30cd829a9de39d918693e891175e946599b4b6
kamran saberifard <kamisaberi@gmail.com>
2025-03-11 01:29:16 +0330

    FallingThingsStereo constructors intialized

--- torch-extension/include/datasets/falling-things-stereo.h
+++ torch-extension/include/datasets/falling-things-stereo.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include "../base/datasets.h"
-
+#include "base.h"
 
 
 namespace torch::ext::data::datasets {
-   class FallingThingsStereo : torch::data::Dataset<FallingThingsStereo> {
-
+    class FallingThingsStereo : torch::data::Dataset<FallingThingsStereo> {
     public :
-       FallingThingsStereo();
+        FallingThingsStereo(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        FallingThingsStereo(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
