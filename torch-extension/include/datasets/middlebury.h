8cbef71d87f5c98b9dd6bba0109b5307da742a3a
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 02:06:28 +0330

    Middlebury2014Stereo constructors intialized

--- torch-extension/include/datasets/middlebury.h
+++ torch-extension/include/datasets/middlebury.h
@@ -1,11 +1,18 @@
 #pragma once
+#include "base.h"
 #include "../base/datasets.h"
 
 
 namespace torch::ext::data::datasets {
-   class Middlebury2014Stereo : torch::data::Dataset<Middlebury2014Stereo> {
-
+    class Middlebury2014Stereo : BaseDataset {
     public :
-       Middlebury2014Stereo();
+        Middlebury2014Stereo(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        Middlebury2014Stereo(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
