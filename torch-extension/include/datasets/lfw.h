b9cad3a7e7f0c6c941df7b2f4ed2a4c84ee44dde
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 01:50:30 +0330

    LFWPairs constructors intialized

--- torch-extension/include/datasets/lfw.h
+++ torch-extension/include/datasets/lfw.h
@@ -66,6 +66,13 @@ namespace torch::ext::data::datasets {
 
     class LFWPairs : public LFW {
     public :
-        LFWPairs();
+        LFWPairs(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        LFWPairs(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
