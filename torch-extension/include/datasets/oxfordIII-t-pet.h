2e41eba3cee089e152de794f607a869774727a64
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 02:40:07 +0330

    OxfordIIITPet constructors intialized

--- torch-extension/include/datasets/oxfordIII-t-pet.h
+++ torch-extension/include/datasets/oxfordIII-t-pet.h
@@ -1,18 +1,30 @@
 #pragma once
+#include "base.h"
 #include "../base/datasets.h"
 
 
-
 namespace torch::ext::data::datasets {
-   class OxfordIIITPet : torch::data::Dataset<OxfordIIITPet> {
+    class OxfordIIITPet : BaseDataset {
+    public :
+        OxfordIIITPet(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
-       vector<std::tuple<fs::path , std::string >> _RESOURCES = {
-               {fs::path("https://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz"), "5c4f3ee8e5d25df40f4fd59a7f44e54c"},
-               {fs::path("https://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz"), "95a8c909bbe2e81eed6a22bccdf3f68f"}
-       };
-       vector<std::string> _VALID_TARGET_TYPES = {"category", "binary-category", "segmentation"};
+        OxfordIIITPet(const fs::path &root, DatasetArguments args);
 
-   public :
-       OxfordIIITPet();
+    private :
+        vector<std::tuple<fs::path, std::string> > _RESOURCES = {
+            {
+                fs::path("https://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz"),
+                "5c4f3ee8e5d25df40f4fd59a7f44e54c"
+            },
+            {
+                fs::path("https://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz"),
+                "95a8c909bbe2e81eed6a22bccdf3f68f"
+            }
+        };
+        vector<std::string> _VALID_TARGET_TYPES = {"category", "binary-category", "segmentation"};
+
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
