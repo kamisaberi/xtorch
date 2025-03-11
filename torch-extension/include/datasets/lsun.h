0ea1c0232fadef05b3b6e2a76d722726a5ea6a3a
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 02:05:39 +0330

    LSUN constructors intialized

--- torch-extension/include/datasets/lsun.h
+++ torch-extension/include/datasets/lsun.h
@@ -1,11 +1,18 @@
 #pragma once
+#include "base.h"
 #include "../base/datasets.h"
 
 
 namespace torch::ext::data::datasets {
-   class LSUN : torch::data::Dataset<LSUN> {
-
+    class LSUN : BaseDataset {
     public :
-       LSUN();
+        LSUN(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        LSUN(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
