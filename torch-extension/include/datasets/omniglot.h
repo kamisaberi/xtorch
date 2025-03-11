8582983a4e8c0bf8e2820cdd05f4091dab6ae94b
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 02:32:29 +0330

    Omniglot constructors intialized

--- torch-extension/include/datasets/omniglot.h
+++ torch-extension/include/datasets/omniglot.h
@@ -1,12 +1,19 @@
 #pragma once
 
+#include "base.h"
 #include "../base/datasets.h"
 
 
 namespace torch::ext::data::datasets {
-   class Omniglot : torch::data::Dataset<Omniglot> {
-
+    class Omniglot : BaseDataset {
     public :
-       Omniglot();
+        Omniglot(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        Omniglot(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
