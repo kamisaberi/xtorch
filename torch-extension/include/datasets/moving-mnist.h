ae14794a7cc1a732627e9c48c9edae65bed40eb1
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 02:31:51 +0330

    MovingMNIST constructors intialized

--- torch-extension/include/datasets/moving-mnist.h
+++ torch-extension/include/datasets/moving-mnist.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include "base.h"
 #include "../base/datasets.h"
 
 
-
 namespace torch::ext::data::datasets {
-   class MovingMNIST : torch::data::Dataset<MovingMNIST> {
-
+    class MovingMNIST : BaseDataset {
     public :
-       MovingMNIST();
+        MovingMNIST(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        MovingMNIST(const fs::path &root, DatasetArguments args);
+
+    private :
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
     };
 }
