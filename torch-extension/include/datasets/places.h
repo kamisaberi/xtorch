194650171ae9cc4301a7cb9233e682832e724977
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 14:08:28 +0330

    Places365 constructor initialized

--- torch-extension/include/datasets/places.h
+++ torch-extension/include/datasets/places.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "../base/datasets.h"
+#include "base.h"
 
 
 
 namespace torch::ext::data::datasets {
-   class Places365 : torch::data::Dataset<Places365> {
+   class Places365 : BaseDataset {
+   public :
+       Places365(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
-    public :
-       Places365();
-    };
+       Places365(const fs::path &root, DatasetArguments args);
+   private :
+       void load_data(DataMode mode = DataMode::TRAIN);
+
+       void check_resources(const std::string &root, bool download = false);
+
+   };
+
 }
