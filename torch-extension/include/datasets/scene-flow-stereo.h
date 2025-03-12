d6d1574179c4941eb6399d465b962d28a886760c
saeed <saeedkohan8520@gmail.com>
2025-03-12 14:16:30 +0330

    SceneFlowStereo constructor initialized

--- torch-extension/include/datasets/scene-flow-stereo.h
+++ torch-extension/include/datasets/scene-flow-stereo.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "../base/datasets.h"
-
+#include "base.h"
 
 
 namespace torch::ext::data::datasets {
-   class SceneFlowStereo : torch::data::Dataset<SceneFlowStereo> {
+   class SceneFlowStereo : BaseDataset {
 
-    public :
-       SceneFlowStereo();
-    };
+   public :
+       SceneFlowStereo(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+       SceneFlowStereo(const fs::path &root, DatasetArguments args);
+   private :
+       void load_data(DataMode mode = DataMode::TRAIN);
+
+       void check_resources(const std::string &root, bool download = false);
+
+   };
 }
