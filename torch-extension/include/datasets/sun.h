641e8816a3e18202c13f6091ae5810a7ac8bd237
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 15:12:58 +0330

    SUN397 constructor initialized

--- torch-extension/include/datasets/sun.h
+++ torch-extension/include/datasets/sun.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../base/datasets.h"
+#include "base.h"
 
 
 using namespace std;
@@ -7,12 +8,18 @@ namespace fs = std::filesystem;
 
 
 namespace torch::ext::data::datasets {
-    class SUN397 : torch::data::Dataset<SUN397> {
+    class SUN397 : BaseDataset {
+    public :
+        SUN397(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        SUN397(const fs::path &root, DatasetArguments args);
+
     private :
         fs::path url = fs::path("http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz");
         std::string md5 = "8ca2778205c41d23104230ba66911c7a";
+        void load_data(DataMode mode = DataMode::TRAIN);
 
-    public :
-        SUN397();
+        void check_resources(const std::string &root, bool download = false);
+
     };
 }
