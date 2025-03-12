35c9cb5498fac859aa9a429e4bea5260f82f785b
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 14:06:02 +0330

    PCAM constructor initialized

--- torch-extension/include/datasets/pcam.h
+++ torch-extension/include/datasets/pcam.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "../base/datasets.h"
+#include "base.h"
 
 
 
 namespace torch::ext::data::datasets {
-    class PCAM : torch::data::Dataset<PCAM> {
+    class PCAM : BaseDataset {
+    public :
+        PCAM(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        PCAM(const fs::path &root, DatasetArguments args);
+
     private :
         std::tuple<fs::path, std::string, std::string>  t = {fs::path("camelyonpatch_level_2_split_train_x.h5"),"1Ka0XfEMiwgCYPdTI-vv6eUElOBnKFKQ2","1571f514728f59376b705fc836ff4b63"};
         std::map<std::string, std::map<std::string, std::tuple<fs::path, std::string, std::string> >> resources = {
@@ -20,10 +26,13 @@ namespace torch::ext::data::datasets {
                     {"images", {fs::path("camelyonpatch_level_2_split_valid_x.h5"),"1hgshYGWK8V-eGRy8LToWJJgDU_rXWVJ3","d5b63470df7cfa627aeec8b9dc0c066e"} },
                     {"targets", {fs::path("camelyonpatch_level_2_split_valid_y.h5"),"1bH8ZRbhSVAhScTS0p9-ZzGnX91cHT3uO","2b85f58b927af9964a4c15b8f7e8f179"} },
                 }}
+
+
         };
+        void load_data(DataMode mode = DataMode::TRAIN);
+
+        void check_resources(const std::string &root, bool download = false);
 
 
-    public :
-        PCAM();
     };
 }
