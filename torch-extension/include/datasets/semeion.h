d3a930d785cbccf91e578333cc7dc1b89f4d3de8
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 14:18:31 +0330

    SEMEION constructor initialized

--- torch-extension/include/datasets/semeion.h
+++ torch-extension/include/datasets/semeion.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../base/datasets.h"
+#include "base.h"
 
 
 using namespace std;
@@ -8,13 +9,19 @@ namespace fs = std::filesystem;
 
 
 namespace torch::ext::data::datasets {
-   class SEMEION : torch::data::Dataset<SEMEION> {
+   class SEMEION : BaseDataset {
+   public :
+       SEMEION(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+       SEMEION(const fs::path &root, DatasetArguments args);
+
    private:
        fs::path url = fs::path("http://archive.ics.uci.edu/ml/machine-learning-databases/semeion/semeion.data");
        fs::path filename = fs::path("semeion.data");
        std::string md5_checksum = "cb545d371d2ce14ec121470795a77432";
+       void load_data(DataMode mode = DataMode::TRAIN);
 
-   public :
-       SEMEION();
+       void check_resources(const std::string &root, bool download = false);
+
     };
 }
