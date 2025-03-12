7ca99ad9682883598d8c451acab0e104dcc269bd
kamran saberifard <kamisaberi@gmail.com>
2025-03-12 15:19:26 +0330

    VOCSegmentation & VOCDetection constructors initialized

--- torch-extension/include/datasets/voc.h
+++ torch-extension/include/datasets/voc.h
@@ -1,18 +1,31 @@
 #pragma once
 
 #include "../base/datasets.h"
-
+#include "base.h"
 
 
 namespace torch::ext::data::datasets {
-   class VOCSegmentation : torch::data::Dataset<VOCSegmentation> {
+   class VOCSegmentation : BaseDataset {
 
-    public :
-       VOCSegmentation();
-    };
-   class VOCDetection : torch::data::Dataset<VOCDetection> {
+   public :
+       VOCSegmentation(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
-    public :
-       VOCDetection();
+       VOCSegmentation(const fs::path &root, DatasetArguments args);
+   private :
+       void load_data(DataMode mode = DataMode::TRAIN);
+
+       void check_resources(const std::string &root, bool download = false);
+
+   };
+   class VOCDetection : BaseDataset {
+
+   public :
+       VOCDetection(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+       VOCDetection(const fs::path &root, DatasetArguments args);
+   private :
+       void load_data(DataMode mode = DataMode::TRAIN);
+
+       void check_resources(const std::string &root, bool download = false);
     };
 }
