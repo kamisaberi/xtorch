251bf4faffe621d85b089a481d1331cf86fb3a9c
saeed <saeedkohan8520@gmail.com>
2025-03-12 14:13:28 +0330

    SBDataset constructor initialized

--- torch-extension/include/datasets/sb-dataset.h
+++ torch-extension/include/datasets/sb-dataset.h
@@ -1,12 +1,18 @@
 #pragma once
 #include "../base/datasets.h"
+#include "base.h"
 
 using namespace std;
 namespace fs = std::filesystem;
 
 
 namespace torch::ext::data::datasets {
-    class SBDataset : torch::data::Dataset<SBDataset> {
+    class SBDataset : BaseDataset {
+    public :
+        SBDataset(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+        SBDataset(const fs::path &root, DatasetArguments args);
+
     private:
         fs::path url = fs::path("https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz");
         std::string md5 = "82b4d87ceb2ed10f6038a1cba92111cb";
@@ -15,8 +21,10 @@ namespace torch::ext::data::datasets {
         fs::path voc_train_url = fs::path("https://www.cs.cornell.edu/~bharathh/train_noval.txt");
         fs::path voc_split_filename = fs::path("train_noval.txt");
         std::string voc_split_md5 = "79bff800c5f0b1ec6b21080a3c066722";
+        void load_data(DataMode mode = DataMode::TRAIN);
 
-    public :
-        SBDataset();
+        void check_resources(const std::string &root, bool download = false);
+
+
     };
 }
