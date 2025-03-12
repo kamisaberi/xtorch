1897ed09fecb684ad4bbe318d17161cd0f15ad7d
saeed <saeedkohan8520@gmail.com>
2025-03-12 15:02:09 +0330

    Sintel & SintelStereo constructors initialized

--- torch-extension/include/datasets/sintel.h
+++ torch-extension/include/datasets/sintel.h
@@ -1,16 +1,30 @@
 #pragma once
 #include "../base/datasets.h"
+#include "base.h"
 
 
 namespace torch::ext::data::datasets {
-   class Sintel : torch::data::Dataset<Sintel> {
+   class Sintel : BaseDataset {
 
-    public :
-       Sintel();
-    };
+   public :
+       Sintel(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+       Sintel(const fs::path &root, DatasetArguments args);
+   private :
+       void load_data(DataMode mode = DataMode::TRAIN);
+
+       void check_resources(const std::string &root, bool download = false);
+
+   };
    class SintelStereo : torch::data::Dataset<SintelStereo> {
 
-    public :
-       SintelStereo();
+   public :
+       SintelStereo(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+       SintelStereo(const fs::path &root, DatasetArguments args);
+   private :
+       void load_data(DataMode mode = DataMode::TRAIN);
+
+       void check_resources(const std::string &root, bool download = false);
     };
 }
