6a9e4f5ade25cdfe013973cc97928e189ccc2afc
kamran saberifard <kamisaberi@gmail.com>
2025-03-15 01:55:51 +0330

    SUN397 dataset properties names refined

--- torch-extension/include/datasets/sun.h
+++ torch-extension/include/datasets/sun.h
@@ -16,8 +16,12 @@ namespace torch::ext::data::datasets {
 
     private :
         fs::path url = fs::path("http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz");
-        std::string md5 = "8ca2778205c41d23104230ba66911c7a";
+        fs::path dataset_file_name = "SUN397.tar.gz";
+        std::string dataset_file_md5 = "8ca2778205c41d23104230ba66911c7a";
+        fs::path dataset_folder_name = "sun397";
+
         void load_data(DataMode mode = DataMode::TRAIN);
+
 
         void check_resources(const std::string &root, bool download = false);
 
