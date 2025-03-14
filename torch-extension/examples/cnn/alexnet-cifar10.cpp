8a0d1ba719d478d3d4266b88cf6a4aa5ae6e687f
kamran saberifard <kamisaberi@gmail.com>
2025-03-14 23:29:41 +0330

    example files in progress

--- torch-extension/examples/cnn/alexnet-cifar10.cpp
+++ torch-extension/examples/cnn/alexnet-cifar10.cpp
@@ -1,11 +1,6 @@
-#include <torch/torch.h>
-#include <torch/data/datasets/mnist.h>
-#include <iostream>
-#include <vector>
+#include "includes/base.h"
 #include "../../include/datasets/cifar.h"
 #include "../../include/models/cnn/alexnet.h"
-#include <torch/data/transforms/base.h>
-#include <functional>
 #include "../../include/definitions/transforms.h"
 
 
