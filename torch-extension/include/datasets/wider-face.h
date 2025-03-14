85455db8b8b4ac9ee7acd138f7072bcfda6db749
kamran saberifard <kamisaberi@gmail.com>
2025-03-15 01:11:18 +0330

    widerface dataset FILE property changes to resources

--- torch-extension/include/datasets/wider-face.h
+++ torch-extension/include/datasets/wider-face.h
@@ -16,7 +16,7 @@ namespace torch::ext::data::datasets {
 
     private:
         fs::path BASE_FOLDER = fs::path("widerface");
-        std::vector<std::tuple<std::string, std::string, fs::path> > FILE_LIST = {
+        std::vector<std::tuple<std::string, std::string, fs::path> > resoutces = {
                 {"15hGDLhsx8bLgLcIRD5DhYt5iBxnjNF1M", "3fedf70df600953d25982bcd13d91ba2", fs::path("WIDER_train.zip")},
                 {"1GUCogbp16PMGa39thoMMeWxp7Rp5oM8Q", "dfa7d7e790efa35df3788964cf0bbaea", fs::path("WIDER_val.zip")},
                 {"1HIfDbVEWKmsYKJZm4lchTBDLW5N7dY5T", "e5d8f4248ed24c334bbd12f49c29dd40", fs::path("WIDER_test.zip")}
