82a352be06807981199c7620e49a61d76a91643f
kamran saberifard <kamisaberi@gmail.com>
2025-03-14 23:38:15 +0330

    example files in progress

--- torch-extension/examples/cnn/vggnet16-fashionmnist.cpp
+++ torch-extension/examples/cnn/vggnet16-fashionmnist.cpp
@@ -1,11 +1,7 @@
-#include <torch/torch.h>
-#include <torch/data/datasets/mnist.h>
-#include <iostream>
-#include <vector>
+#include "includes/base.h"
+
 #include "../../include/datasets/mnist.h"
 #include "../../include/models/cnn/vggnet16.h"
-#include <torch/data/transforms/base.h>
-#include <functional>
 #include "../../include/definitions/transforms.h"
 
 
