ca01f06108e55e964a3ad3803d05a8dc570568a3
kamran saberifard <kamisaberi@gmail.com>
2025-03-14 23:37:27 +0330

    example files in progress

--- torch-extension/examples/cnn/resnet-cifar100.cpp
+++ torch-extension/examples/cnn/resnet-cifar100.cpp
@@ -1,11 +1,7 @@
-#include <torch/torch.h>
-#include <torch/data/datasets/mnist.h>
-#include <iostream>
-#include <vector>
+#include "includes/base.h"
+
 #include "../../include/datasets/cifar.h"
 #include "../../include/models/cnn/resnet.h"
-#include <torch/data/transforms/base.h>
-#include <functional>
 #include "../../include/definitions/transforms.h"
 
 
