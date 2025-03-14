d9f824addae02c676e21317b57ecad64c64bcba4
kamran saberifard <kamisaberi@gmail.com>
2025-03-14 23:26:48 +0330

    base.h created fro examples

---
+++ torch-extension/examples/cnn/includes/base.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <torch/torch.h>
+#include <torch/data/datasets/mnist.h>
+#include <iostream>
+#include <vector>
+#include <torch/data/transforms/base.h>
+#include <functional>
+#include "../../include/datasets/cifar.h"
+#include "../../include/models/cnn/alexnet.h"
+#include "../../include/definitions/transforms.h"
+
