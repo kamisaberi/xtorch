2b7713d2ef6bc1e002edac91c1bda288ae0e004a
kamran saberifard <kamisaberi@gmail.com>
2025-03-15 01:12:19 +0330

    widerface dataset BASE_FOLDER  property changes to dataset_folder_name

--- torch-extension/include/datasets/wider-face.h
+++ torch-extension/include/datasets/wider-face.h
@@ -15,8 +15,8 @@ namespace torch::ext::data::datasets {
         WIDERFace(const fs::path &root, DatasetArguments args);
 
     private:
-        fs::path BASE_FOLDER = fs::path("widerface");
-        std::vector<std::tuple<std::string, std::string, fs::path> > resoutces = {
+        fs::path dataset_folder_name = fs::path("widerface");
+        std::vector<std::tuple<std::string, std::string, fs::path> > resources = {
                 {"15hGDLhsx8bLgLcIRD5DhYt5iBxnjNF1M", "3fedf70df600953d25982bcd13d91ba2", fs::path("WIDER_train.zip")},
                 {"1GUCogbp16PMGa39thoMMeWxp7Rp5oM8Q", "dfa7d7e790efa35df3788964cf0bbaea", fs::path("WIDER_val.zip")},
                 {"1HIfDbVEWKmsYKJZm4lchTBDLW5N7dY5T", "e5d8f4248ed24c334bbd12f49c29dd40", fs::path("WIDER_test.zip")}
