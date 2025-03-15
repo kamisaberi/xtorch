115f7071ad6d1b804445c7a48ee2fcd223f9a496
kamran saberifard <kamisaberi@gmail.com>
2025-03-16 00:46:27 +0330

    Food101 dataset properties names refined

--- torch-extension/include/datasets/food.h
+++ torch-extension/include/datasets/food.h
@@ -14,10 +14,10 @@ namespace torch::ext::data::datasets {
         Food101(const fs::path &root, DatasetArguments args);
 
     private :
-        std::string download_url = "http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz";
-        fs::path archive_file_name = "food-101.tar.gz";
-        std::string archive_file_md5 = "85eeb15f3717b99a5da872d97d918f87";
-
+        std::string url = "http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz";
+        fs::path dataset_file_name = "food-101.tar.gz";
+        std::string dataset_file_md5 = "85eeb15f3717b99a5da872d97d918f87";
+        fs::path dataset_folder_name = "food101";
         void load_data(DataMode mode = DataMode::TRAIN);
 
         void check_resources(const std::string &root, bool download = false);
