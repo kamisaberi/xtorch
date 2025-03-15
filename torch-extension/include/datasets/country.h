c4906bc1ba19082382bdac3a0af3866cf9a4514f
kamran saberifard <kamisaberi@gmail.com>
2025-03-16 00:48:21 +0330

    Country211 dataset properties names refined

--- torch-extension/include/datasets/country.h
+++ torch-extension/include/datasets/country.h
@@ -9,9 +9,10 @@ namespace fs = std::filesystem;
 namespace torch::ext::data::datasets {
     class Country211 : BaseDataset {
     private:
-        std::string download_url = "https://openaipublic.azureedge.net/clip/data/";
-        fs::path archive_file_name = "country211.tgz";
-        std::string archive_file_md5 = "84988d7644798601126c29e9877aab6a";
+        std::string url = "https://openaipublic.azureedge.net/clip/data/";
+        fs::path dataset_file_name = "country211.tgz";
+        std::string dataset_file_md5 = "84988d7644798601126c29e9877aab6a";
+        fs::path dataset_folder_name = "country211";
 
     public :
         Country211();
