9d54e7dc4b92d2e493f4de1444076378e38f39e3
kamran saberifard <kamisaberi@gmail.com>
2025-03-16 00:47:29 +0330

    EuroSat dataset properties names refined

--- torch-extension/include/datasets/euro-sat.h
+++ torch-extension/include/datasets/euro-sat.h
@@ -14,11 +14,11 @@ namespace torch::ext::data::datasets {
         EuroSAT(const fs::path &root, DatasetArguments args);
 
     private:
-        std::string download_url =
+        std::string url =
                 "https://huggingface.co/datasets/torchgeo/eurosat/resolve/c877bcd43f099cd0196738f714544e355477f3fd/EuroSAT.zip";
         fs::path archive_file_name = "EuroSAT.zip";
         std::string archive_file_md5 = "c8fa014336c82ac7804f0398fcb19387";
-
+        fs::path dataset_folder_name = "euro-sat";
         void load_data(DataMode mode = DataMode::TRAIN);
 
         void check_resources(const std::string &root, bool download = false);
