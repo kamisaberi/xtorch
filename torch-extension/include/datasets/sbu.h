51a97267ba109175bad42189f9192d22ae86bbc8
kamran saberifard <kamisaberi@gmail.com>
2025-03-15 23:53:24 +0330

    SBU dataset properties names refined

--- torch-extension/include/datasets/sbu.h
+++ torch-extension/include/datasets/sbu.h
@@ -16,8 +16,9 @@ namespace torch::ext::data::datasets {
 
     private :
         fs::path url = fs::path("https://www.cs.rice.edu/~vo9/sbucaptions/SBUCaptionedPhotoDataset.tar.gz");
-        fs::path filename = fs::path("SBUCaptionedPhotoDataset.tar.gz");
-        std::string md5_checksum = "9aec147b3488753cf758b4d493422285";
+        fs::path dataset_file_name = fs::path("SBUCaptionedPhotoDataset.tar.gz");
+        std::string dataset_file_md5 = "9aec147b3488753cf758b4d493422285";
+        fs::path dataset_folder_name = "sbu";
         void load_data(DataMode mode = DataMode::TRAIN);
 
         void check_resources(const std::string &root, bool download = false);
