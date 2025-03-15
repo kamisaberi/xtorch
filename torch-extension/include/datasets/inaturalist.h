f85b9c6d64ea75ab2310ed6f7b5f0d85038b5423
kamran saberifard <kamisaberi@gmail.com>
2025-03-16 00:44:40 +0330

    INaturalist dataset properties names refined

--- torch-extension/include/datasets/inaturalist.h
+++ torch-extension/include/datasets/inaturalist.h
@@ -59,6 +59,8 @@ namespace torch::ext::data::datasets {
             }
         };
 
+        fs::path dataset_folder_name = "inaturalist";
+
         void load_data(DataMode mode = DataMode::TRAIN);
 
         void check_resources(const std::string &root, bool download = false);
