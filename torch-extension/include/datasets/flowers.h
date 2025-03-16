d9c6173b9bf6ef22cd7fb2a0897e8337c0cd613d
saeed <kamisaberi@gmail.com>
2025-03-16 10:40:16 +0330

    Flowers102 dataset marked public inherited from BaseDataset

--- torch-extension/include/datasets/flowers.h
+++ torch-extension/include/datasets/flowers.h
@@ -4,7 +4,7 @@
 
 
 namespace torch::ext::data::datasets {
-    class Flowers102 : BaseDataset {
+    class Flowers102 : public BaseDataset {
     public :
         Flowers102(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
