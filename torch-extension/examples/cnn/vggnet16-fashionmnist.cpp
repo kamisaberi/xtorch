eb46a00320e41b89efebe7026ed12b011552adf3
kamran saberifard <kamisaberi@gmail.com>
2025-03-18 00:39:08 +0330

    examples namespace removed

--- torch-extension/examples/cnn/vggnet16-fashionmnist.cpp
+++ torch-extension/examples/cnn/vggnet16-fashionmnist.cpp
@@ -7,7 +7,6 @@
 
 using namespace std;
 using Example = torch::data::Example<torch::Tensor, torch::Tensor>;
-namespace torch::ext::examples::cnn {
 
 int main() {
     std::vector<int64_t> size = {32, 32};
@@ -54,5 +53,4 @@ int main() {
     }
 
     return 0;
-}
 }
