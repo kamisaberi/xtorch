fe2e4d8b6745d49ba52bf8494ddd5d7007072dba
kamran saberifard <kamisaberi@gmail.com>
2025-03-20 00:32:30 +0330

    alexnet-cifar100 exmaple finished

--- torch-extension/examples/cnn/alexnet-cifar100.cpp
+++ torch-extension/examples/cnn/alexnet-cifar100.cpp
@@ -8,7 +8,7 @@ using namespace std;
 using Example = torch::data::Example<torch::Tensor, torch::Tensor>;
 
 int main() {
-    std::vector<int64_t> size = {32, 32};
+    std::vector<int64_t> size = {227, 227};
 
     std::cout.precision(10);
     torch::Device device(torch::kCPU);
@@ -16,14 +16,14 @@ int main() {
     auto dataset = torch::ext::data::datasets::CIFAR100("/home/kami/Documents/temp/", DataMode::TRAIN, true);
 
     auto transformed_dataset = dataset
-            .map(torch::ext::data::transforms::resize({32, 32}))
+            .map(torch::ext::data::transforms::resize(size))
             .map(torch::ext::data::transforms::normalize(0.5, 0.5))
             .map(torch::data::transforms::Stack<>());
 
     auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
         std::move(transformed_dataset), 64);
 
-    torch::ext::models::AlexNet model(10);
+    torch::ext::models::AlexNet model(100 , 3);
     model.to(device);
     model.train();
     torch::optim::Adam optimizer(model.parameters(), torch::optim::AdamOptions(1e-3));
