b8b037f3734bf4881fdc8b6984765a7e7364d533
kamran saberifard <kamisaberi@gmail.com>
2025-03-20 16:36:40 +0330

    Imagenette dataset implementation started

--- torch-extension/src/datasets/base.cpp
+++ torch-extension/src/datasets/base.cpp
@@ -1,3 +1,11 @@
 #include "../../include/datasets/base.h"
 
+namespace torch::ext::data::datasets {
+    BaseDataset::BaseDataset(const std::string &root, DataMode mode, bool download) {
+        this->root = root;
+        this->download = download;
+        this->mode = mode;
+    }
 
+
+}
