02f92941a5802f71ea43ddefc5198f26fee34f28
kamran saberifard <kamisaberi@gmail.com>
2025-03-21 01:59:41 +0330

    CMakeLists.txt updated

--- torch-extension/include/datasets/imagenette.h
+++ torch-extension/include/datasets/imagenette.h
@@ -34,22 +34,25 @@ namespace torch::ext::data::datasets {
         Imagenette(const fs::path &root, DatasetArguments args);
 
     private:
-        std::map<string, std::tuple<fs::path, std::string> > resources = {
+        std::map<string, std::tuple<fs::path, fs::path, std::string> > resources = {
             {
                 "full", {
                     fs::path("https://s3.amazonaws.com/fast-ai-imageclas/imagenette2.tgz"),
+                    fs::path("imagenette2.tgz"),
                     "fe2fc210e6bb7c5664d602c3cd71e612"
                 }
             },
             {
                 "320px", {
                     fs::path("https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-320.tgz"),
+                    fs::path("imagenette2-320.tgz"),
                     "3df6f0d01a2c9592104656642f5e78a3"
                 }
             },
             {
                 "160px", {
                     fs::path("https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-160.tgz"),
+                    fs::path("imagenette2-160.tgz"),
                     "e793b78cc4c9e9a4ccc0c1155377a412"
                 }
             }
