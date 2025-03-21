5f40243629b7c366ffe3aa05b9fce1e75825ff0a
kamran saberifard <kamisaberi@gmail.com>
2025-03-21 16:28:32 +0330

    imagenette load_data finished and test is Ok

--- torch-extension/src/datasets/imagenette.cpp
+++ torch-extension/src/datasets/imagenette.cpp
@@ -65,11 +65,8 @@ namespace torch::ext::data::datasets {
             for (auto &p: fs::directory_iterator(path)) {
                 if (fs::is_directory(p.path())) {
                     string u = p.path().filename().string();
-                    // cout << u << endl;
                     labels_name.push_back(u);
                     for (auto &img: fs::directory_iterator(p.path())) {
-                        // string u = img.path().string();
-                        // cout << "\t" << u << endl;
                         cv::Mat image = cv::imread(img.path().string(), cv::IMREAD_COLOR);
                         if (image.empty()) {
                             throw std::runtime_error("Could not load image at: " + img.path().string());
@@ -94,35 +91,16 @@ namespace torch::ext::data::datasets {
 
                         // 6. Make sure the tensor is contiguous in memory
                         tensor = tensor.contiguous();
-                        // cout << tensor.sizes() << endl;
                         data.push_back(tensor);
                         labels.push_back(labels_name.size() - 1);
                     }
                 }
             }
 
-            //     fs::path imgs = this->dataset_path / std::get<0>(files["train"]);
-            //     fs::path lbls = this->dataset_path / std::get<1>(files["train"]);
-            //     cout << imgs.string() << "  " << lbls.string() << endl;
-            //     cout << imgs << endl;
-            //     this->read_images(imgs.string(), 50000);
-            //     this->read_labels(lbls.string(), 50000);
-            //     // auto images = read_mnist_images(imgs.string(), 50000);
-            //     // auto labels = read_mnist_labels(lbls.string(), 50000);
-            //     // cout << labels[0] << endl;
-            //     // this->data = images;
-            //     // this->labels = labels;
         } else {
             fs::path path = this->dataset_path / folder_name / fs::path("val");
-            //     fs::path imgs = this->dataset_path / std::get<0>(files["test"]);
-            //     fs::path lbls = this->dataset_path / std::get<1>(files["test"]);
-            //     cout << imgs << endl;
-            //     this->read_images(imgs.string(), 10000);
-            //     this->read_labels(imgs.string(), 10000);
-            //     // auto images = read_mnist_images(imgs.string(), 10000);
-            //     // auto labels = read_mnist_labels(lbls.string(), 10000);
-            //     // this->data = images;
-            //     // this->labels = labels;
+
+
         }
     }
 }
