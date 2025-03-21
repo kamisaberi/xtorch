3f4af7aa180c6defd0c259eb42d8f2155ab8e0e9
kamran saberifard <kamisaberi@gmail.com>
2025-03-21 20:55:54 +0330

    image-folder code started

--- torch-extension/include/datasets/image-folder.h
+++ torch-extension/include/datasets/image-folder.h
@@ -1,4 +1,28 @@
 #pragma once
 
+#include "base.h"
 #include "../base/datasets.h"
+
+
+
+namespace torch::ext::data::datasets {
+    class ImageFolder : public BaseDataset {
+        public :
+            ImageFolder(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+
+
+        ImageFolder(const fs::path &root, DatasetArguments args);
+
+    private:
+        fs::path dataset_folder_name = "imagenette";
+        vector<string> labels_name;
+        void load_data(DataMode mode = DataMode::TRAIN);
+        void check_resources(const std::string &root, bool download = false);
+
+    };
+
+
+
+
+}
 
