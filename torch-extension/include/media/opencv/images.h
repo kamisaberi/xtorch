a51815bc40bbccfa4e008b57d38b41b32173f068
kamran saberifard <kamisaberi@gmail.com>
2025-03-22 20:44:09 +0330

    convertImageToTensor created in media/opencv/images(.h & .cpp)

--- torch-extension/include/media/opencv/images.h
+++ torch-extension/include/media/opencv/images.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <filesystem>
+#include <torch/torch.h>
+using namespace std;
+
+namespace fs = std::filesystem;
+
+namespace torch::ext::media::opencv {
+
+    torch::Tensor convertImageToTensor(std::string image);
 
 
+
+}
 
 
