a3deeef1d5b666708cdaeca24c6d6331fcb94a3c
kamran saberifard <kamisaberi@gmail.com>
2025-03-23 17:30:40 +0330

    Food101 dataset constructor created

--- torch-extension/include/datasets/food.h
+++ torch-extension/include/datasets/food.h
@@ -17,7 +17,7 @@ namespace torch::ext::data::datasets {
         std::string url = "http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz";
         fs::path dataset_file_name = "food-101.tar.gz";
         std::string dataset_file_md5 = "85eeb15f3717b99a5da872d97d918f87";
-        fs::path dataset_folder_name = "food101";
+        fs::path dataset_folder_name = "food-101";
         void load_data(DataMode mode = DataMode::TRAIN);
 
         void check_resources(const std::string &root, bool download = false);
