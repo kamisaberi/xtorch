0e78521eb66a4ab0bf46a1c0ef4e16e81c26b08e
kamran saberifard <kamisaberi@gmail.com>
2025-03-23 05:01:55 +0330

    extractVideoFrames functions were written. test is ok

---
+++ torch-extension/vdtest.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include <torch/torch.h>
+#include "include/media/opencv/videos.h"
+
+
+int main() {
+//    std::vector<cv::Mat> frames;
+    fs::path videoPath("/home/kami/Documents/temp/videos/people-detection.mp4");
+    vector<torch::Tensor> frames = torch::ext::media::opencv::videos::extractVideoFramesAsTensor(videoPath);
+    cout << frames.size() << endl;
+    cout << frames[0] << endl;
+
+    return 0;
+}
