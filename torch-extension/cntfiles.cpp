0229422fce7e213c87eb66f8ace5e7ff4d904d2d
kamran saberifard <kamisaberi@gmail.com>
2025-03-23 19:52:38 +0330

    filesystem(.h & .cpp) created in utils directory

--- torch-extension/cntfiles.cpp
+++ torch-extension/cntfiles.cpp
@@ -2,20 +2,23 @@
 #include <algorithm>
 #include <iostream>
 
-std::size_t count_files_recursively(const std::filesystem::path& path) {
-    return std::count_if(
-        std::filesystem::recursive_directory_iterator(path),
-        std::filesystem::recursive_directory_iterator{},
-        [](const std::filesystem::directory_entry& entry) {
-            return entry.is_regular_file();
-        }
-    );
-}
+#include "include/utils/downloader.h"
+#include "include/utils/filesystem.h"
+
+// std::size_t count_files_recursively(const std::filesystem::path& path) {
+//     return std::count_if(
+//         std::filesystem::recursive_directory_iterator(path),
+//         std::filesystem::recursive_directory_iterator{},
+//         [](const std::filesystem::directory_entry& entry) {
+//             return entry.is_regular_file();
+//         }
+//     );
+// }
 
 int main() {
     std::filesystem::path directory_path = "/home/kami/Documents/temp/videos/";
     try {
-        std::size_t total_files = count_files_recursively(directory_path);
+        std::size_t total_files = torch::est::utils::filesystem::countFiles(directory_path, true);
         std::cout << "Total number of files: " << total_files << std::endl;
     } catch (const std::filesystem::filesystem_error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
