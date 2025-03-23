da0c8f4ef40d4330cd41ce95e6dc14655d97bab0
kamran saberifard <kamisaberi@gmail.com>
2025-03-23 19:47:11 +0330

    filesystem(.h & .cpp) created in utils directory

--- torch-extension/src/utils/filesystem.cpp
+++ torch-extension/src/utils/filesystem.cpp
@@ -4,20 +4,25 @@ namespace torch::est::utils::filesystem {
 
     std::size_t countFiles(const fs::path& path , bool recursive){
 
-      if (recursive) {
-
-          return std::count_if(
-      std::filesystem::recursive_directory_iterator(path),
-      std::filesystem::recursive_directory_iterator{},
-      [](const std::filesystem::directory_entry& entry) {
-          return entry.is_regular_file();
-      }
-  );
-
-      }
+        if (recursive) {
+            return std::count_if(
+                std::filesystem::recursive_directory_iterator(path),
+                std::filesystem::recursive_directory_iterator{},
+                [](const std::filesystem::directory_entry& entry) {
+                    return entry.is_regular_file();
+                }
+            );
 
 
+        }else {
+            return std::count_if(
+                std::filesystem::directory_iterator(path),
+                std::filesystem::directory_iterator{},
+                [](const std::filesystem::directory_entry& entry) {
+                    return entry.is_regular_file();
+                }
+            );
+        }
     }
-
 }
 
