81c47c7483cf714a999bfcec3e4ce99aa3271b7b
kamran saberifard <kamisaberi@gmail.com>
2025-03-23 18:13:13 +0330

    Food101 dataset constructor created

--- torch-extension/src/datasets/food.cpp
+++ torch-extension/src/datasets/food.cpp
@@ -7,6 +7,22 @@ namespace torch::ext::data::datasets {
 
     }
 
+    void Food101::check_resources() {
+
+        fs::path archive_file_abs_path =this->root / this->dataset_file_name;
+        this->dataset_path = this->root / this->dataset_folder_name;
+        if (! fs::exists(archive_file_abs_path)) {
+            //TODO Check folder and files inside it and after that go to download file
+        }
+
+
+
+
+
+
+
+
+    }
     // Food101::Food101() {
     //     throw NotImplementedException();
     // }
