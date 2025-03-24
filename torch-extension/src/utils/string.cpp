1e2c46f2ec238c40d0d914f6983fe1d496fc54a6
kamran saberifard <kamisaberi@gmail.com>
2025-03-24 04:53:18 +0330

    string utility file created

---
+++ torch-extension/src/utils/string.cpp
@@ -0,0 +1,20 @@
+#include "../../include/utils/string.h"
+
+namespace torch::ext::utils::string {
+    vector<std::string> split(const std::string& str, const std::string& delim)
+    {
+        vector<std::string> tokens;
+        size_t prev = 0, pos = 0;
+        do
+        {
+            pos = str.find(delim, prev);
+            if (pos == std::string::npos) pos = str.length();
+            std::string token = str.substr(prev, pos-prev);
+            if (!token.empty()) tokens.push_back(token);
+            prev = pos + delim.length();
+        }
+        while (pos < str.length() && prev < str.length());
+        return tokens;
+    }
+
+}
