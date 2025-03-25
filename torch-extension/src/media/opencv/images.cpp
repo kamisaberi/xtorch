4337a67c9fda424b28634b2308bc1a4f18a44ba8
kamran saberifard <kamisaberi@gmail.com>
2025-03-25 03:52:56 +0330

    Food101 dataset will have image_size as constructor parameter to keep Memory free

--- torch-extension/src/media/opencv/images.cpp
+++ torch-extension/src/media/opencv/images.cpp
@@ -2,7 +2,7 @@
 
 
 namespace torch::ext::media::opencv {
-    torch::Tensor convertImageToTensor(fs::path img) {
+    torch::Tensor convertImageToTensor(fs::path img, vector<int> size) {
         cv::Mat image = cv::imread(img.string(), cv::IMREAD_COLOR);
         if (image.empty()) {
             throw std::runtime_error("Could not load image at: " + img.string());
@@ -13,13 +13,16 @@ namespace torch::ext::media::opencv {
         // 3. Convert image data to float and normalize to [0, 1]
         // image.convertTo(image, CV_32F, 1.0 / 255.0);
         image.convertTo(image, CV_8U);
+        cout << "BE:" << image.rows << "  " << image.cols << "  " << image.channels() << endl;
+        cv::resize(image, image, cv::Size(size[0], size[1]), 0, 0, cv::INTER_LINEAR);
+        cout << "AF:" << image.rows << "  " << image.cols << "  " << image.channels() << endl;
 
         // 4. Create a tensor from the image data
         // OpenCV uses HWC (Height, Width, Channels) format
         torch::Tensor tensor = torch::from_blob(image.data, {image.rows, image.cols, image.channels()},
                                                 torch::kInt8);
 
-        cout << image.rows << "  " <<  image.cols << "------" << tensor.numel() * tensor.element_size() << endl;
+
         // 5. Permute to CHW (Channels, Height, Width) format, which is PyTorch's default
         tensor = tensor.permute({2, 0, 1});
 
