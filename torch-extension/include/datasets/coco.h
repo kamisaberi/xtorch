00d91dea5f457279bf37ebbcbc1ebb927ccf4c1b
kamran saberifard <kamisaberi@gmail.com>
2025-03-27 06:37:39 +0330

    torch::ext::data::datasets changed to xt::data::datasets and still changeing the dataset classes

--- torch-extension/include/datasets/coco.h
+++ torch-extension/include/datasets/coco.h
@@ -3,7 +3,7 @@
 #include "base.h"
 
 
-namespace torch::ext::data::datasets {
+namespace xt::data::datasets {
     class CocoDetection : public BaseDataset {
     public :
         CocoDetection(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
