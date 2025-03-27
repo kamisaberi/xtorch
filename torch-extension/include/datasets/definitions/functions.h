270e1cf37b22532f16e6a811e3c69cee0115c6cf
kamran saberifard <kamisaberi@gmail.com>
2025-03-27 20:29:49 +0330

    some bugs fixed

--- torch-extension/include/datasets/definitions/functions.h
+++ torch-extension/include/datasets/definitions/functions.h
@@ -4,7 +4,7 @@
 #include "../base.h"
 
 
-namespace torch::ext::data {
-    torch::data::datasets::MapDataset<torch::ext::data::datasets::BaseDataset , torch::data::transforms::Stack<>()>
-    transform_dataset(torch::ext::data::datasets::BaseDataset dataset, vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms);
+namespace xt::data {
+    torch::data::datasets::MapDataset<xt::data::datasets::BaseDataset , torch::data::transforms::Stack<>()>
+    transform_dataset(xt::data::datasets::BaseDataset dataset, vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms);
 }
