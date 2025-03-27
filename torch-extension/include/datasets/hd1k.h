725089c708258095b4740b309dad35f07be3520f
kamran saberifard <kamisaberi@gmail.com>
2025-03-27 07:20:54 +0330

    torch::ext::data::datasets changed to xt::data::datasets and still changeing the dataset classes

--- torch-extension/include/datasets/hd1k.h
+++ torch-extension/include/datasets/hd1k.h
@@ -3,7 +3,7 @@
 #include "../headers/datasets.h"
 
 
-namespace torch::ext::data::datasets {
+namespace xt::data::datasets {
     class HD1K : BaseDataset {
     public :
         HD1K(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
