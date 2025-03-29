7ee90518de5211fe3baae22bc44816bb3e355c3c
kamran saberifard <kamisaberi@gmail.com>
2025-03-29 06:59:52 +0330

    transforms namespace changed to xt::data::transforms and examples updated

--- torch-extension/examples/cnn/lenet5-fashionmnist.cpp
+++ torch-extension/examples/cnn/lenet5-fashionmnist.cpp
@@ -16,8 +16,8 @@ int main() {
     auto dataset = xt::data::datasets::FashionMNIST("/home/kami/Documents/temp/", DataMode::TRAIN, true);
 
     auto transformed_dataset = dataset
-            .map(torch::ext::data::transforms::resize({32, 32}))
-            .map(torch::ext::data::transforms::normalize(0.5, 0.5))
+            .map(xt::data::transforms::resize({32, 32}))
+            .map(xt::data::transforms::normalize(0.5, 0.5))
             .map(torch::data::transforms::Stack<>());
 
     auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
