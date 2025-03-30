547bca0d1fb64bd1184a541918337503e10e04a2
kamran saberifard <kamisaberi@gmail.com>
2025-03-31 03:02:42 +0330

    ResNet model inherited of BaseModel

--- torch-extension/include/models/cnn/resnet.h
+++ torch-extension/include/models/cnn/resnet.h
@@ -12,7 +12,7 @@ using namespace std;
 namespace torch::ext::models {
 
     namespace {
-        struct ResidualBlock : BaseModel {
+        struct ResidualBlock : torch::nn::Module {
             torch::nn::Sequential conv1 = nullptr, conv2 = nullptr, downsample = nullptr;
             int out_channels;
             torch::nn::ReLU relu = nullptr;
@@ -26,12 +26,12 @@ namespace torch::ext::models {
     }
 
     struct ResNet : BaseModel {
-        int inplanes = 64;
-        torch::nn::Sequential conv1 = nullptr;
-        torch::nn::MaxPool2d maxpool = nullptr;
-        torch::nn::AvgPool2d avgpool = nullptr;
-        torch::nn::Sequential layer0 = nullptr, layer1 = nullptr, layer2 = nullptr, layer3 = nullptr;
-        torch::nn::Linear fc = nullptr;
+        mutable  int inplanes = 64;
+        mutable  torch::nn::Sequential conv1 = nullptr;
+        mutable  torch::nn::MaxPool2d maxpool = nullptr;
+        mutable  torch::nn::AvgPool2d avgpool = nullptr;
+        mutable  torch::nn::Sequential layer0 = nullptr, layer1 = nullptr, layer2 = nullptr, layer3 = nullptr;
+        mutable  torch::nn::Linear fc = nullptr;
 
         ResNet(vector<int> layers, int num_classes = 10, int in_channels = 3 /* input channels */);
 
