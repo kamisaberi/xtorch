b752021171a3aae776493029d181f74816e98656
kamran saberifard <kamisaberi@gmail.com>
2025-03-31 03:07:35 +0330

    VggNet16 model inherited of BaseModel

--- torch-extension/include/models/cnn/vggnet.h
+++ torch-extension/include/models/cnn/vggnet.h
@@ -4,12 +4,14 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include "../base.h"
+
 
 using namespace std;
 namespace torch::ext::models {
 
 
-    struct VggNet16 : torch::nn::Module {
+    struct VggNet16 : BaseModel {
         torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 = nullptr;
         torch::nn::Sequential layer6 = nullptr, layer7 = nullptr, layer8 = nullptr, layer9 = nullptr, layer10 = nullptr;
         torch::nn::Sequential layer11 = nullptr, layer12 = nullptr, layer13 = nullptr;
@@ -17,6 +19,6 @@ namespace torch::ext::models {
 
         VggNet16(int num_classes /* classes */, int in_channels = 3 /* input channels */);
 
-        torch::Tensor forward(torch::Tensor x);
+        torch::Tensor forward(torch::Tensor x) const override;
     };
 }
