731b5baa901628bdbbd9880ec0b784e262d6f5cf
kamran saberifard <kamisaberi@gmail.com>
2025-03-30 06:42:06 +0330

    LeNet5 properties tagged as mutable

--- torch-extension/include/models/cnn/lenet5.h
+++ torch-extension/include/models/cnn/lenet5.h
@@ -10,8 +10,8 @@ using namespace std;
 namespace torch::ext::models {
     struct LeNet5 : torch::ext::models::BaseModel {
     protected:
-        torch::nn::Sequential layer1 = nullptr, layer2 = nullptr;
-        torch::nn::Linear fc1 = nullptr, fc2 = nullptr, fc3 = nullptr;
+        mutable  torch::nn::Sequential layer1 = nullptr, layer2 = nullptr;
+        mutable  torch::nn::Linear fc1 = nullptr, fc2 = nullptr, fc3 = nullptr;
 
     public:
         LeNet5(int num_classes/* classes */, int in_channels = 1/*  input channels */);
