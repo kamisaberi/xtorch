59c4041ad23a34efcdb9ff5df0d17237353df6d2
kamran saberifard <kamisaberi@gmail.com>
2025-03-30 06:32:04 +0330

    MNISTBase and MNIST new constructor created

--- torch-extension/src/models/base.cpp
+++ torch-extension/src/models/base.cpp
@@ -3,10 +3,12 @@
 namespace torch::ext::models {
 
     BaseModel::BaseModel() = default;
-    Model::Model():BaseModel() {}
+    Model::Model(int a):BaseModel()  {
+      this->a = a;
+    }
 
     torch::Tensor Model::forward(torch::Tensor input) const {
-      std::cout << input << std::endl;
-      return input * 3;
+//      std::cout << input << std::endl;
+      return input * a;
     }
 }
