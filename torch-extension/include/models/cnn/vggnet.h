4680b59dfb9de057d8457eeec957d5dab4acfc53
kamran saberifard <kamisaberi@gmail.com>
2025-03-31 03:11:15 +0330

    VggNet16 model inherited of BaseModel

--- torch-extension/include/models/cnn/vggnet.h
+++ torch-extension/include/models/cnn/vggnet.h
@@ -8,14 +8,15 @@
 
 
 using namespace std;
+
 namespace torch::ext::models {
-
-
     struct VggNet16 : BaseModel {
-        torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 = nullptr;
-        torch::nn::Sequential layer6 = nullptr, layer7 = nullptr, layer8 = nullptr, layer9 = nullptr, layer10 = nullptr;
-        torch::nn::Sequential layer11 = nullptr, layer12 = nullptr, layer13 = nullptr;
-        torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                        nullptr;
+        mutable torch::nn::Sequential layer6 = nullptr, layer7 = nullptr, layer8 = nullptr, layer9 = nullptr, layer10 =
+                        nullptr;
+        mutable torch::nn::Sequential layer11 = nullptr, layer12 = nullptr, layer13 = nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
 
         VggNet16(int num_classes /* classes */, int in_channels = 3 /* input channels */);
 
