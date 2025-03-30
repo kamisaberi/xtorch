e309e956807ad0bd81566197575986fecb885964
kamran saberifard <kamisaberi@gmail.com>
2025-03-30 11:35:19 +0330

    Trainer class almost finished but still has several bugs

--- torch-extension/temp/mntcomp2.cpp
+++ torch-extension/temp/mntcomp2.cpp
@@ -28,7 +28,9 @@ int main() {
                                                  torch::data::transforms::Normalize<>(0.5, 0.5)
                                              });
     auto transformed_dataset =  dataset.map(torch::data::transforms::Stack<>());
+
     xt::DataLoader<decltype(transformed_dataset)> loader(std::move(transformed_dataset), torch::data::DataLoaderOptions().batch_size(64).drop_last(false), /*shuffle=*/true);
+//    xt::DataLoader<decltype(dataset)> loader(std::move(dataset), torch::data::DataLoaderOptions().batch_size(64).drop_last(false), /*shuffle=*/true);
 
 
 
@@ -44,25 +46,25 @@ int main() {
 
     trainer.fit<decltype(transformed_dataset)>(&model ,loader );
 
-    for (size_t epoch = 0; epoch != 10; ++epoch) {
-        cout << "epoch: " << epoch << endl;
-        for (auto& batch : loader) {
-
-            torch::Tensor data, targets;
-            data = batch.data;
-            targets = batch.target;
-            optimizer.zero_grad();
-            torch::Tensor output;
-            output = model.forward(data);
-            torch::Tensor loss;
-            loss = torch::nll_loss(output, targets);
-            loss.backward();
-            optimizer.step();
-            //                std::cout << "Epoch: " << epoch << " | Batch: " <<  " | Loss: " << loss.item<float>() <<                            std::endl;
-
-            //            }
-
-        }
-    }
+//    for (size_t epoch = 0; epoch != 10; ++epoch) {
+//        cout << "epoch: " << epoch << endl;
+//        for (auto& batch : loader) {
+//
+//            torch::Tensor data, targets;
+//            data = batch.data;
+//            targets = batch.target;
+//            optimizer.zero_grad();
+//            torch::Tensor output;
+//            output = model.forward(data);
+//            torch::Tensor loss;
+//            loss = torch::nll_loss(output, targets);
+//            loss.backward();
+//            optimizer.step();
+//            //                std::cout << "Epoch: " << epoch << " | Batch: " <<  " | Loss: " << loss.item<float>() <<                            std::endl;
+//
+//            //            }
+//
+//        }
+//    }
     return 0;
 }
