502adb07819127a87a0daea8f1d06157e0c3c8bb
kamran saberifard <kamisaberi@gmail.com>
2025-03-31 21:09:17 +0330

    ImageFolder constructor modified

--- torch-extension/include/datasets/image-folder.h
+++ torch-extension/include/datasets/image-folder.h
@@ -13,9 +13,16 @@ namespace xt::data::datasets {
 
     class ImageFolder : public BaseDataset {
     public :
-        ImageFolder(const std::string &root,bool load_sub_folders =  false , DataMode mode = DataMode::TRAIN, LabelsType label_type = LabelsType::BY_FOLDER );
 
-        ImageFolder(const fs::path &root, DatasetArguments args);
+        ImageFolder(const std::string &root);
+        ImageFolder(const std::string &root,bool load_sub_folders);
+        ImageFolder(const std::string &root,bool load_sub_folders, DataMode mode);
+        ImageFolder(const std::string &root,bool load_sub_folders, DataMode mode , LabelsType label_type);
+        ImageFolder(const std::string &root,bool load_sub_folders, DataMode mode , LabelsType label_type, vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
+
+        // ImageFolder(const std::string &root,bool load_sub_folders , DataMode mode , LabelsType label_type = LabelsType::BY_FOLDER );
+
+        // ImageFolder(const fs::path &root, DatasetArguments args);
 
     private:
         fs::path dataset_folder_name = "imagenette";
