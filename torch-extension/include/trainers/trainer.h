2028fc7839cf34758e01d7d3dc482cf597461edb
kamran saberifard <kamisaberi@gmail.com>
2025-03-31 12:59:57 +0330

    Imagenette constructor modified

--- torch-extension/include/trainers/trainer.h
+++ torch-extension/include/trainers/trainer.h
@@ -23,10 +23,10 @@ namespace xt {
         Trainer();
 
         // Setter methods with fluent interface
-        Trainer& setMaxEpochs(int maxEpochs);
-        Trainer& setOptimizer(torch::optim::Optimizer *optimizer);
-        Trainer& setLossFn(std::function<torch::Tensor(torch::Tensor, torch::Tensor)> lossFn);
-        Trainer& enableCheckpoint(const std::string& path, int interval);
+        Trainer& set_max_epochs(int maxEpochs);
+        Trainer& set_optimizer(torch::optim::Optimizer *optimizer);
+        Trainer& set_loss_fn(std::function<torch::Tensor(torch::Tensor, torch::Tensor)> lossFn);
+        Trainer& enable_checkpoint(const std::string& path, int interval);
         template <typename Dataset>
 //        void fit(torch::ext::models::BaseModel *model , xt::DataLoader<Dataset>&  train_loader);
 //        template <typename Dataset>
@@ -35,7 +35,7 @@ namespace xt {
             torch::Device device(torch::kCPU);
             model->to(device);
             model->train();
-            for (size_t epoch = 0; epoch != this->maxEpochs_; ++epoch) {
+            for (size_t epoch = 0; epoch != this->max_epochs_; ++epoch) {
                 cout << "epoch: " << epoch << endl;
                 int a = 1;
                 for (auto& batch : train_loader) {
@@ -46,7 +46,7 @@ namespace xt {
                     torch::Tensor output;
                     output = model->forward(data);
                     torch::Tensor loss;
-                    loss = this->lossFn_(output, targets);
+                    loss = this->loss_fn_(output, targets);
                     loss.backward();
                     this->optimizer_->step();
                     a++;
@@ -58,13 +58,13 @@ namespace xt {
 
 
     private:
-        int maxEpochs_;                         // Maximum number of training epochs
+        int max_epochs_;                         // Maximum number of training epochs
         torch::optim::Optimizer *optimizer_;  // Optimizer object
-        std::function<torch::Tensor(torch::Tensor, torch::Tensor)> lossFn_;
+        std::function<torch::Tensor(torch::Tensor, torch::Tensor)> loss_fn_;
 //        std::shared_ptr<torch::nn::CrossEntropyLoss> lossFn_;  // Loss function object
-        bool checkpointEnabled_;                // Flag for checkpointing status
-        std::string checkpointPath_;            // Path for saving checkpoints
-        int checkpointInterval_;                // Interval for checkpointing
+        bool checkpoint_enabled_;                // Flag for checkpointing status
+        std::string checkpoint_path_;            // Path for saving checkpoints
+        int checkpoint_interval_;                // Interval for checkpointing
     };
 
 } // namespace xt
