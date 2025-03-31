0ddb5e380d10e6c9dfa907f8a428066ee4674e86
kamran saberifard <kamisaberi@gmail.com>
2025-03-31 04:18:54 +0330

    BaseDataset constructors recreated and optional parameters replaced by overloaded constructors

--- torch-extension/include/datasets/base.h
+++ torch-extension/include/datasets/base.h
@@ -9,9 +9,15 @@ namespace fs = std::filesystem;
 namespace xt::data::datasets {
     class BaseDataset : public torch::data::Dataset<BaseDataset> {
     public:
-        explicit BaseDataset(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
-        BaseDataset(const fs::path &root, DatasetArguments args);
+        BaseDataset(const std::string &root);
+        BaseDataset(const std::string &root, DataMode mode);
+        BaseDataset(const std::string &root, DataMode mode , bool download);
+        BaseDataset(const std::string &root, DataMode mode  , bool download, std::shared_ptr<xt::data::transforms::Compose> compose);
+        BaseDataset(const std::string &root, DataMode mode , bool download , vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
+
+
+        // BaseDataset(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
         torch::data::Example<> get(size_t index) override;
 
@@ -27,10 +33,11 @@ namespace xt::data::datasets {
         bool download = false;
         fs::path root;
         fs::path dataset_path;
+        xt::data::transforms::Compose compose;
 
-        void transform_data(std::vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms);
 
     private:
-        vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms = {};
+        // vector<torch::data::transforms::Lambda<torch::data::Example<> > > transforms = {};
+        vector<std::function<torch::Tensor(torch::Tensor)>> transforms = {};
     };
 }
