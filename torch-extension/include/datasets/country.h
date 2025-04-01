7cb1a2f91a338dba8ba8eb105b05bf08b3893c4e
kamran saberifard <kamisaberi@gmail.com>
2025-04-01 15:12:14 +0330

    datasets constructors recreated

--- torch-extension/include/datasets/country.h
+++ torch-extension/include/datasets/country.h
@@ -8,13 +8,18 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets {
     class Country211 : public BaseDataset {
+    public:
+        Country211(const std::string &root);
+        Country211(const std::string &root, DataMode mode);
+        Country211(const std::string &root, DataMode mode , bool download);
+        Country211(const std::string &root, DataMode mode , bool download, vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
+
+
     private:
         std::string url = "https://openaipublic.azureedge.net/clip/data/";
         fs::path dataset_file_name = "country211.tgz";
         std::string dataset_file_md5 = "84988d7644798601126c29e9877aab6a";
         fs::path dataset_folder_name = "country211";
 
-    public :
-        Country211();
     };
 }
