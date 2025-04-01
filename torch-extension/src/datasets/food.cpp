d600840cf4e8b13af2ff9a7e4ae93988c7aab0ab
kamran saberifard <kamisaberi@gmail.com>
2025-04-01 13:24:32 +0330

    Food101 constructor modified

--- torch-extension/src/datasets/food.cpp
+++ torch-extension/src/datasets/food.cpp
@@ -1,11 +1,30 @@
 #include "../../include/datasets/food.h"
 
 namespace xt::data::datasets {
+    Food101::Food101(const std::string &root): Food101::Food101(root, DataMode::TRAIN, false) {
+    }
+
+    Food101::Food101(const std::string &root, DataMode mode): Food101::Food101(root, mode, false) {
+    }
+
     Food101::Food101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
         check_resources();
         load_classes();
         load_data();
     }
+
+    Food101::Food101(const std::string &root, DataMode mode, bool download,
+                     vector<std::function<torch::Tensor(torch::Tensor)> > transforms) : BaseDataset(
+        root, mode, download, transforms) {
+        this->transforms = transforms;
+        if (!transforms.empty()) {
+            this->compose = xt::data::transforms::Compose(transforms);
+        }
+        check_resources();
+        load_classes();
+        load_data();
+    }
+
 
     void Food101::check_resources() {
         fs::path archive_file_abs_path = this->root / this->dataset_file_name;
@@ -89,7 +108,6 @@ namespace xt::data::datasets {
                 this->data.push_back(tensor);
                 this->labels.push_back(classes_map[label]);
             }
-
         }
     }
 
