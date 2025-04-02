8250920d4528c31e751dd4d6d0155167bc4ef783
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:07:50 +0330

    PCAM constructors throw  NotImplementedError

--- torch-extension/src/datasets/pcam.cpp
+++ torch-extension/src/datasets/pcam.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // PCAM::PCAM() {
-    //     throw NotImplementedException();
-    // }
+    PCAM::PCAM(const std::string &root): PCAM::PCAM(root, DataMode::TRAIN, false) {
+    }
+
+    PCAM::PCAM(const std::string &root, DataMode mode): PCAM::PCAM(root, mode, false) {
+    }
+
+    PCAM::PCAM(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("PCAM: PCAM not implemented");
+    }
+
+
+    PCAM::PCAM(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("PCAM: PCAM not implemented");
+    }
+
 
 }
