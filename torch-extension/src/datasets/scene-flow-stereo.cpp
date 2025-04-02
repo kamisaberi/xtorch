7cc09cf63a3f467fc048c92b539e1aec1d097f59
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:34:28 +0330

    SceneFlowStereo constructors throw  NotImplementedError

--- torch-extension/src/datasets/scene-flow-stereo.cpp
+++ torch-extension/src/datasets/scene-flow-stereo.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // SceneFlowStereo::SceneFlowStereo() {
-    //     throw NotImplementedException();
-    // }
+    SceneFlowStereo::SceneFlowStereo(const std::string &root): SceneFlowStereo::SceneFlowStereo(root, DataMode::TRAIN, false) {
+    }
+
+    SceneFlowStereo::SceneFlowStereo(const std::string &root, DataMode mode): SceneFlowStereo::SceneFlowStereo(root, mode, false) {
+    }
+
+    SceneFlowStereo::SceneFlowStereo(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("SceneFlowStereo: SceneFlowStereo not implemented");
+    }
+
+
+    SceneFlowStereo::SceneFlowStereo(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("SceneFlowStereo: SceneFlowStereo not implemented");
+    }
+
 
 }
