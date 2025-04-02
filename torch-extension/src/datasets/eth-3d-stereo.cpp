77d1526dca8e6058f2273b2a3e1e0a1f34aaf522
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 17:57:00 +0330

    ETH3DStereo constructors throw  NotImplementedError

--- torch-extension/src/datasets/eth-3d-stereo.cpp
+++ torch-extension/src/datasets/eth-3d-stereo.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // ETH3DStereo::ETH3DStereo() {
-    //     throw NotImplementedException();
-    // }
+    ETH3DStereo::ETH3DStereo(const std::string &root): ETH3DStereo::ETH3DStereo(root, DataMode::TRAIN, false) {
+    }
+
+    ETH3DStereo::ETH3DStereo(const std::string &root, DataMode mode): ETH3DStereo::ETH3DStereo(root, mode, false) {
+    }
+
+    ETH3DStereo::ETH3DStereo(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("ETH3DStereo: ETH3DStereo not implemented");
+    }
+
+
+    ETH3DStereo::ETH3DStereo(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("ETH3DStereo: ETH3DStereo not implemented");
+    }
 
 }
