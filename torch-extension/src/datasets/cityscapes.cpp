9bd5699512d8caf5782177b776d45eab070bfe1d
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 16:43:53 +0330

    Cityscapes constructors throw  NotImplementedError

--- torch-extension/src/datasets/cityscapes.cpp
+++ torch-extension/src/datasets/cityscapes.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // Cityscapes::Cityscapes() {
-    //     throw NotImplementedException();
-    // }
+    Cityscapes::Cityscapes(const std::string &root): Cityscapes::Cityscapes(root, DataMode::TRAIN, false) {
+    }
+
+    Cityscapes::Cityscapes(const std::string &root, DataMode mode): Cityscapes::Cityscapes(root, mode, false) {
+    }
+
+    Cityscapes::Cityscapes(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Cityscapes: Cityscapes not implemented");
+    }
+
+
+    Cityscapes::Cityscapes(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Cityscapes: Cityscapes not implemented");
+    }
 
 }
