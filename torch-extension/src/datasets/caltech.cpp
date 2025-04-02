bb2db7adc92a92ec6beaff2adc0c36be669c7d89
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 16:34:19 +0330

    Caltech256 constructors throw  NotImplementedError

--- torch-extension/src/datasets/caltech.cpp
+++ torch-extension/src/datasets/caltech.cpp
@@ -1,6 +1,10 @@
 #include "../../include/datasets/caltech.h"
 
 namespace xt::data::datasets {
+
+
+    // ---------------------- Caltech101 ---------------------- //
+
     Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
     }
 
@@ -17,10 +21,25 @@ namespace xt::data::datasets {
         throw std::runtime_error("Caltech101: Caltech101 not implemented");
     }
 
-    // Caltech101::Caltech101() {
-    //     throw NotImplementedException();
-    // }
-    // Caltech256::Caltech256() {
-    //     throw NotImplementedException();
-    // }
+
+
+    // ---------------------- Caltech256 ---------------------- //
+
+    Caltech256::Caltech256(const std::string &root): Caltech256::Caltech256(root, DataMode::TRAIN, false) {
+    }
+
+    Caltech256::Caltech256(const std::string &root, DataMode mode): Caltech256::Caltech256(root, mode, false) {
+    }
+
+    Caltech256::Caltech256(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Caltech256: Caltech256 not implemented");
+    }
+
+
+    Caltech256::Caltech256(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
 }
