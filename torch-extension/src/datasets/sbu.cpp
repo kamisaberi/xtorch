d419d598ab373052bcb244c2e21ef2dc9ac8edd6
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:32:05 +0330

    SBU constructors throw  NotImplementedError

--- torch-extension/src/datasets/sbu.cpp
+++ torch-extension/src/datasets/sbu.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // SBU::SBU() {
-    //     throw NotImplementedException();
-    // }
+    SBU::SBU(const std::string &root): SBU::SBU(root, DataMode::TRAIN, false) {
+    }
+
+    SBU::SBU(const std::string &root, DataMode mode): SBU::SBU(root, mode, false) {
+    }
+
+    SBU::SBU(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("SBU: SBU not implemented");
+    }
+
+
+    SBU::SBU(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("SBU: SBU not implemented");
+    }
+
 
 }
