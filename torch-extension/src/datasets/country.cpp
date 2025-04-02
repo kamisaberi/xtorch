5351a7fed205919aef4a37e70d18d5d40c6d6fbe
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 17:16:19 +0330

    Country211 constructors throw  NotImplementedError

--- torch-extension/src/datasets/country.cpp
+++ torch-extension/src/datasets/country.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // Country211::Country211() {
-    //     throw NotImplementedException();
-    // }
+    Country211::Country211(const std::string &root): Country211::Country211(root, DataMode::TRAIN, false) {
+    }
+
+    Country211::Country211(const std::string &root, DataMode mode): Country211::Country211(root, mode, false) {
+    }
+
+    Country211::Country211(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Country211: Country211 not implemented");
+    }
+
+
+    Country211::Country211(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Country211: Country211 not implemented");
+    }
 
 }
