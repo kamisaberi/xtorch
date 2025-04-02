2e555f5efff04591f4f951acca8c9b822cdab254
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 01:28:02 +0330

    Kitti constructors throw  NotImplementedError

--- torch-extension/src/datasets/kitti.cpp
+++ torch-extension/src/datasets/kitti.cpp
@@ -2,17 +2,83 @@
 
 namespace xt::data::datasets {
 
-    // Kitti::Kitti() {
-    //     throw NotImplementedException();
-    // }
-    // KittiFlow::KittiFlow() {
-    //     throw NotImplementedException();
-    // }
-    // Kitti2012Stereo::Kitti2012Stereo() {
-    //     throw NotImplementedException();
-    // }
-    // Kitti2015Stereo::Kitti2015Stereo() {
-    //     throw NotImplementedException();
-    // }
+    // ---------------------- Caltech101 ---------------------- //
+    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+
+
+
+    // ---------------------- Caltech101 ---------------------- //
+    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+
+
+    // ---------------------- Caltech101 ---------------------- //
+    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+
+
+    // ---------------------- Caltech101 ---------------------- //
+    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
 
 }
