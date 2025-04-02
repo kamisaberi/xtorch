627b24b5cde29b4300b0885ed92571418e1efe84
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:56:31 +0330

    SVHN constructors recreated

--- torch-extension/include/datasets/svhn.h
+++ torch-extension/include/datasets/svhn.h
@@ -8,6 +8,31 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets {
     class SVHN : BaseDataset {
+        /*
+        """`SVHN <http://ufldl.stanford.edu/housenumbers/>`_ Dataset.
+    Note: The SVHN dataset assigns the label `10` to the digit `0`. However, in this Dataset,
+    we assign the label `0` to the digit `0` to be compatible with PyTorch loss functions which
+    expect the class labels to be in the range `[0, C-1]`
+
+    .. warning::
+
+        This class needs `scipy <https://docs.scipy.org/doc/>`_ to load data from `.mat` format.
+
+    Args:
+        root (str or ``pathlib.Path``): Root directory of the dataset where the data is stored.
+        split (string): One of {'train', 'test', 'extra'}.
+            Accordingly dataset is selected. 'extra' is Extra training set.
+        transform (callable, optional): A function/transform that takes in a PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+
+    """
+
+         */
     public :
         SVHN(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
