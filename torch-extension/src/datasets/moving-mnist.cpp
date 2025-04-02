66ca5cb3d507de682d19900ac40523a1c77739f7
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 01:57:06 +0330

    MovingMNIST constructors throw  NotImplementedError

--- torch-extension/src/datasets/moving-mnist.cpp
+++ torch-extension/src/datasets/moving-mnist.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // MovingMNIST::MovingMNIST() {
-    //     throw NotImplementedException();
-    // }
+    MovingMNIST::MovingMNIST(const std::string &root): MovingMNIST::MovingMNIST(root, DataMode::TRAIN, false) {
+    }
+
+    MovingMNIST::MovingMNIST(const std::string &root, DataMode mode): MovingMNIST::MovingMNIST(root, mode, false) {
+    }
+
+    MovingMNIST::MovingMNIST(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("MovingMNIST: MovingMNIST not implemented");
+    }
+
+
+    MovingMNIST::MovingMNIST(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("MovingMNIST: MovingMNIST not implemented");
+    }
+
 
 }
