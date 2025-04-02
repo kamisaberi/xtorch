3591f4ec6e88f7f3d03780e2ef749d864d12921f
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 17:34:20 +0330

    CarlaStereo and CREStereo constructors throw  NotImplementedError

--- torch-extension/src/datasets/cre-stereo.cpp
+++ torch-extension/src/datasets/cre-stereo.cpp
@@ -1,12 +1,39 @@
 #include "../../include/datasets/cre-stereo.h"
 
 namespace xt::data::datasets {
+    // ---------------------- CarlaStereo ---------------------- //
+    CarlaStereo::CarlaStereo(const std::string &root): CarlaStereo::CarlaStereo(root, DataMode::TRAIN, false) {
+    }
 
-    // CarlaStereo::CarlaStereo() {
-    //     throw NotImplementedException();
-    // }
-    // CREStereo::CREStereo() {
-    //     throw NotImplementedException();
-    // }
+    CarlaStereo::CarlaStereo(const std::string &root, DataMode mode): CarlaStereo::CarlaStereo(root, mode, false) {
+    }
 
+    CarlaStereo::CarlaStereo(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("CarlaStereo: CarlaStereo not implemented");
+    }
+
+
+    CarlaStereo::CarlaStereo(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("CarlaStereo: CarlaStereo not implemented");
+    }
+
+
+    // ---------------------- CREStereo ---------------------- //
+
+    CREStereo::CREStereo(const std::string &root): CREStereo::CREStereo(root, DataMode::TRAIN, false) {
+    }
+
+    CREStereo::CREStereo(const std::string &root, DataMode mode): CREStereo::CREStereo(root, mode, false) {
+    }
+
+    CREStereo::CREStereo(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("CREStereo: CREStereo not implemented");
+    }
+
+
+    CREStereo::CREStereo(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("CREStereo: CREStereo not implemented");
+    }
 }
