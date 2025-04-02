61e98aa873ccb3a9d28c19c2406d4434147a22cd
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 01:50:30 +0330

    LFW constructors throw  NotImplementedError

--- torch-extension/src/datasets/lfw.cpp
+++ torch-extension/src/datasets/lfw.cpp
@@ -21,27 +21,27 @@ namespace xt::data::datasets {
 
 
 
+    // ---------------------- LFWPeople ---------------------- //
+    LFWPeople::LFWPeople(const std::string &root): LFWPeople::LFWPeople(root, DataMode::TRAIN, false) {
+    }
+
+    LFWPeople::LFWPeople(const std::string &root, DataMode mode): LFWPeople::LFWPeople(root, mode, false) {
+    }
+
+    LFWPeople::LFWPeople(const std::string &root, DataMode mode, bool download) : LFW(root, mode, download) {
+        throw std::runtime_error("LFWPeople: LFWPeople not implemented");
+    }
+
+
+    LFWPeople::LFWPeople(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : LFW(root, mode, download, transforms) {
+        throw std::runtime_error("LFWPeople: LFWPeople not implemented");
+    }
+
+
+
     // ---------------------- Caltech101 ---------------------- //
-    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
-    }
-
-    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
-    }
-
-    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
-    }
-
-
-    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
-    }
-
-
-
-    // ---------------------- Caltech101 ---------------------- //
-    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    LFWPairs::LFWPairs(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
     }
 
     Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
