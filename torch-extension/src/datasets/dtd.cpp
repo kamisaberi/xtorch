90e07041a646311ba2f58cf25ef22ced67646b44
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 17:55:35 +0330

    DTD constructors throw  NotImplementedError

--- torch-extension/src/datasets/dtd.cpp
+++ torch-extension/src/datasets/dtd.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // DTD::DTD() {
-    //     throw NotImplementedException();
-    // }
+    DTD::DTD(const std::string &root): DTD::DTD(root, DataMode::TRAIN, false) {
+    }
+
+    DTD::DTD(const std::string &root, DataMode mode): DTD::DTD(root, mode, false) {
+    }
+
+    DTD::DTD(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("DTD: DTD not implemented");
+    }
+
+
+    DTD::DTD(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("DTD: DTD not implemented");
+    }
 
 }
