655d70980b934d7a9ca731ba83410f99db1243a6
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 14:48:34 +0330

    Caltech101 constructors implemented

--- torch-extension/src/datasets/caltech.cpp
+++ torch-extension/src/datasets/caltech.cpp
@@ -1,6 +1,20 @@
 #include "../../include/datasets/caltech.h"
 
 namespace xt::data::datasets {
+    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+    }
+
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
+                           vector<std::function<torch::Tensor(torch::Tensor)> > transforms) : BaseDataset(
+        root, mode, download, transforms) {
+    }
 
     // Caltech101::Caltech101() {
     //     throw NotImplementedException();
@@ -8,5 +22,4 @@ namespace xt::data::datasets {
     // Caltech256::Caltech256() {
     //     throw NotImplementedException();
     // }
-
 }
