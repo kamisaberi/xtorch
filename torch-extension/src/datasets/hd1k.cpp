e70290213d6f757d331fe21a9b821b9975840c54
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 00:45:48 +0330

    HD1K constructors throw  NotImplementedError

--- torch-extension/src/datasets/hd1k.cpp
+++ torch-extension/src/datasets/hd1k.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // HD1K::HD1K() {
-    //     throw NotImplementedException();
-    // }
+    HD1K::HD1K(const std::string &root): HD1K::HD1K(root, DataMode::TRAIN, false) {
+    }
+
+    HD1K::HD1K(const std::string &root, DataMode mode): HD1K::HD1K(root, mode, false) {
+    }
+
+    HD1K::HD1K(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+    HD1K::HD1K(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
 
 }
