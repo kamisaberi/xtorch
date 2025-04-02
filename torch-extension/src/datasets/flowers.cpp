99a95621af08d1329a68e8a58f6e5bb8233b4b0f
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 00:10:36 +0330

    Flowers102 constructors throw  NotImplementedError

--- torch-extension/src/datasets/flowers.cpp
+++ torch-extension/src/datasets/flowers.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // Flowers102::Flowers102() {
-    //     throw NotImplementedException();
-    // }
+    Flowers102::Flowers102(const std::string &root): Flowers102::Flowers102(root, DataMode::TRAIN, false) {
+    }
+
+    Flowers102::Flowers102(const std::string &root, DataMode mode): Flowers102::Flowers102(root, mode, false) {
+    }
+
+    Flowers102::Flowers102(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Flowers102: Flowers102 not implemented");
+    }
+
+
+    Flowers102::Flowers102(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Flowers102: Flowers102 not implemented");
+    }
 
 }
