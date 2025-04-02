88d075cfc91918f9fe8505f36f60e3bd6bd034c2
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 00:20:08 +0330

    GTSRB constructors throw  NotImplementedError

--- torch-extension/src/datasets/gtsrb.cpp
+++ torch-extension/src/datasets/gtsrb.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // GTSRB::GTSRB() {
-    //     throw NotImplementedException();
-    // }
+    GTSRB::GTSRB(const std::string &root): GTSRB::GTSRB(root, DataMode::TRAIN, false) {
+    }
+
+    GTSRB::GTSRB(const std::string &root, DataMode mode): GTSRB::GTSRB(root, mode, false) {
+    }
+
+    GTSRB::GTSRB(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("GTSRB: GTSRB not implemented");
+    }
+
+
+    GTSRB::GTSRB(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("GTSRB: GTSRB not implemented");
+    }
+
 
 }
