36bbe0087f8b155d6882dc5a5fa111c957dcc9be
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 18:13:58 +0330

    FER2013 constructors throw  NotImplementedError

--- torch-extension/src/datasets/fer.cpp
+++ torch-extension/src/datasets/fer.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // FER2013::FER2013() {
-    //     throw NotImplementedException();
-    // }
+    FER2013::FER2013(const std::string &root): FER2013::FER2013(root, DataMode::TRAIN, false) {
+    }
+
+    FER2013::FER2013(const std::string &root, DataMode mode): FER2013::FER2013(root, mode, false) {
+    }
+
+    FER2013::FER2013(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("FER2013: FER2013 not implemented");
+    }
+
+
+    FER2013::FER2013(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("FER2013: FER2013 not implemented");
+    }
 
 }
