840aa38df776cf0a74ec0683deaa0ac0ef6dd87a
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 08:17:07 +0330

    datasets constructors recreated

--- torch-extension/include/datasets/fake-data.h
+++ torch-extension/include/datasets/fake-data.h
@@ -5,13 +5,21 @@
 
 
 namespace xt::data::datasets {
-    class FakeData :public BaseDataset {
+    class FakeData : public BaseDataset {
     public :
-        FakeData(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+        FakeData(const std::string &root);
 
-        FakeData(const fs::path &root, DatasetArguments args);
+        FakeData(const std::string &root, DataMode mode);
+
+        FakeData(const std::string &root, DataMode mode, bool download);
+
+        FakeData(const std::string &root, DataMode mode, bool download,
+                 vector<std::function<torch::Tensor(torch::Tensor)> > transforms);
 
     private :
+        size_t size_;
+        vector<int64_t> shape_ = {3, 24, 24};
+
         void load_data(DataMode mode = DataMode::TRAIN);
 
         void check_resources(const std::string &root, bool download = false);
