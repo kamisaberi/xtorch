d81d36d00983672dc330b5bfa515cab74dd0e02e
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 18:14:24 +0330

    FER2013 constructors throw  NotImplementedError

--- torch-extension/src/datasets/falling-things-stereo.cpp
+++ torch-extension/src/datasets/falling-things-stereo.cpp
@@ -9,13 +9,13 @@ namespace xt::data::datasets {
     }
 
     FallingThingsStereo::FallingThingsStereo(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+        throw std::runtime_error("FallingThingsStereo: FallingThingsStereo not implemented");
     }
 
 
     FallingThingsStereo::FallingThingsStereo(const std::string &root, DataMode mode, bool download,
                            TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+        throw std::runtime_error("FallingThingsStereo: FallingThingsStereo not implemented");
     }
 
 }
