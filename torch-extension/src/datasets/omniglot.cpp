ac5e809d8432bcf2727cb6dfb1ac6ca993483893
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:01:43 +0330

    Omniglot constructors throw  NotImplementedError

--- torch-extension/src/datasets/omniglot.cpp
+++ torch-extension/src/datasets/omniglot.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // Omniglot::Omniglot() {
-    //     throw NotImplementedException();
-    // }
+    Omniglot::Omniglot(const std::string &root): Omniglot::Omniglot(root, DataMode::TRAIN, false) {
+    }
+
+    Omniglot::Omniglot(const std::string &root, DataMode mode): Omniglot::Omniglot(root, mode, false) {
+    }
+
+    Omniglot::Omniglot(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Omniglot: Omniglot not implemented");
+    }
+
+
+    Omniglot::Omniglot(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Omniglot: Omniglot not implemented");
+    }
+
 
 }
