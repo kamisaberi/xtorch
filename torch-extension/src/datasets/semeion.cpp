4d70d77f432383b4f7e2582a6afacf300d9bb2ba
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:41:37 +0330

    SEMEION constructors throw  NotImplementedError

--- torch-extension/src/datasets/semeion.cpp
+++ torch-extension/src/datasets/semeion.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // SEMEION::SEMEION() {
-    //     throw NotImplementedException();
-    // }
+    SEMEION::SEMEION(const std::string &root): SEMEION::SEMEION(root, DataMode::TRAIN, false) {
+    }
+
+    SEMEION::SEMEION(const std::string &root, DataMode mode): SEMEION::SEMEION(root, mode, false) {
+    }
+
+    SEMEION::SEMEION(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("SEMEION: SEMEION not implemented");
+    }
+
+
+    SEMEION::SEMEION(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("SEMEION: SEMEION not implemented");
+    }
+
 
 }
