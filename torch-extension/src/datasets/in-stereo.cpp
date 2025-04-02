bb33ec3807b8930dd59215c052c53d918cecb674
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 00:54:20 +0330

    InStereo2k constructors throw  NotImplementedError

--- torch-extension/src/datasets/in-stereo.cpp
+++ torch-extension/src/datasets/in-stereo.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // InStereo2k::InStereo2k() {
-    //     throw NotImplementedException();
-    // }
+    InStereo2k::InStereo2k(const std::string &root): InStereo2k::InStereo2k(root, DataMode::TRAIN, false) {
+    }
+
+    InStereo2k::InStereo2k(const std::string &root, DataMode mode): InStereo2k::InStereo2k(root, mode, false) {
+    }
+
+    InStereo2k::InStereo2k(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("InStereo2k: InStereo2k not implemented");
+    }
+
+
+    InStereo2k::InStereo2k(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("InStereo2k: InStereo2k not implemented");
+    }
+
 
 }
