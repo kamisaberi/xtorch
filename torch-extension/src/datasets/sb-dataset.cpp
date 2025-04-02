d46c1a13af7761bd5d815279e4e8655e76eadc1f
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:26:43 +0330

    SBDataset constructors throw  NotImplementedError

--- torch-extension/src/datasets/sb-dataset.cpp
+++ torch-extension/src/datasets/sb-dataset.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // SBDataset::SBDataset() {
-    //     throw NotImplementedException();
-    // }
+    SBDataset::SBDataset(const std::string &root): SBDataset::SBDataset(root, DataMode::TRAIN, false) {
+    }
+
+    SBDataset::SBDataset(const std::string &root, DataMode mode): SBDataset::SBDataset(root, mode, false) {
+    }
+
+    SBDataset::SBDataset(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("SBDataset: SBDataset not implemented");
+    }
+
+
+    SBDataset::SBDataset(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("SBDataset: SBDataset not implemented");
+    }
+
 
 }
