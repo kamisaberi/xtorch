48febd72574a098eef15bb1faa619d78feafda07
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 01:51:22 +0330

    LFW constructors throw  NotImplementedError

--- torch-extension/src/datasets/lfw.cpp
+++ torch-extension/src/datasets/lfw.cpp
@@ -40,21 +40,21 @@ namespace xt::data::datasets {
 
 
 
-    // ---------------------- Caltech101 ---------------------- //
-    LFWPairs::LFWPairs(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    // ---------------------- LFWPairs ---------------------- //
+    LFWPairs::LFWPairs(const std::string &root): LFWPairs::LFWPairs(root, DataMode::TRAIN, false) {
     }
 
-    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    LFWPairs::LFWPairs(const std::string &root, DataMode mode): LFWPairs::LFWPairs(root, mode, false) {
     }
 
-    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    LFWPairs::LFWPairs(const std::string &root, DataMode mode, bool download) : LFW(root, mode, download) {
+        throw std::runtime_error("LFWPairs: LFWPairs not implemented");
     }
 
 
-    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    LFWPairs::LFWPairs(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : LFW(root, mode, download, transforms) {
+        throw std::runtime_error("LFWPairs: LFWPairs not implemented");
     }
 
 
