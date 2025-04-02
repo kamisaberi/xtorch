ed8ae00ccbb3cca8157ca6871fd9c2a8ca8eb1c9
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 01:25:02 +0330

    Kinetics constructors throw  NotImplementedError

--- torch-extension/src/datasets/kinetics.cpp
+++ torch-extension/src/datasets/kinetics.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // Kinetics::Kinetics() {
-    //     throw NotImplementedException();
-    // }
+    Kinetics::Kinetics(const std::string &root): Kinetics::Kinetics(root, DataMode::TRAIN, false) {
+    }
+
+    Kinetics::Kinetics(const std::string &root, DataMode mode): Kinetics::Kinetics(root, mode, false) {
+    }
+
+    Kinetics::Kinetics(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Kinetics: Kinetics not implemented");
+    }
+
+
+    Kinetics::Kinetics(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Kinetics: Kinetics not implemented");
+    }
+
 
 }
