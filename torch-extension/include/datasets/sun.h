7f76281d5a5d09dd116937f8d8fc58ed3ddc1f24
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:55:16 +0330

    SUN397 constructors recreated

--- torch-extension/include/datasets/sun.h
+++ torch-extension/include/datasets/sun.h
@@ -9,6 +9,23 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets {
     class SUN397 : BaseDataset {
+        /*
+        """`The SUN397 Data Set <https://vision.princeton.edu/projects/2010/SUN/>`_.
+
+    The SUN397 or Scene UNderstanding (SUN) is a dataset for scene recognition consisting of
+    397 categories with 108'754 images.
+
+    Args:
+        root (str or ``pathlib.Path``): Root directory of the dataset.
+        transform (callable, optional): A function/transform that takes in a PIL image and returns a transformed
+            version. E.g, ``transforms.RandomCrop``.
+        target_transform (callable, optional): A function/transform that takes in the target and transforms it.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+    """
+
+         */
     public :
         explicit SUN397(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
 
