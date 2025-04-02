d4093ca7b530b11b022733d4cf58a761d34f7efb
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 18:43:52 +0330

    Middlebury2014Stereo constructors throw  NotImplementedError

--- torch-extension/src/datasets/middlebury.cpp
+++ torch-extension/src/datasets/middlebury.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // Middlebury2014Stereo::Middlebury2014Stereo() {
-    //     throw NotImplementedException();
-    // }
+    Middlebury2014Stereo::Middlebury2014Stereo(const std::string &root): Middlebury2014Stereo::Middlebury2014Stereo(root, DataMode::TRAIN, false) {
+    }
+
+    Middlebury2014Stereo::Middlebury2014Stereo(const std::string &root, DataMode mode): Middlebury2014Stereo::Middlebury2014Stereo(root, mode, false) {
+    }
+
+    Middlebury2014Stereo::Middlebury2014Stereo(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Middlebury2014Stereo: Middlebury2014Stereo not implemented");
+    }
+
+
+    Middlebury2014Stereo::Middlebury2014Stereo(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Middlebury2014Stereo: Middlebury2014Stereo not implemented");
+    }
+
 
 }
