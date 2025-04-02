7accc3589e79de0692e997c3b61a186106e01319
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:12:12 +0330

    PhotoTour constructors throw  NotImplementedError

--- torch-extension/src/datasets/photo-tour.cpp
+++ torch-extension/src/datasets/photo-tour.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // PhotoTour::PhotoTour() {
-    //     throw NotImplementedException();
-    // }
+    PhotoTour::PhotoTour(const std::string &root): PhotoTour::PhotoTour(root, DataMode::TRAIN, false) {
+    }
+
+    PhotoTour::PhotoTour(const std::string &root, DataMode mode): PhotoTour::PhotoTour(root, mode, false) {
+    }
+
+    PhotoTour::PhotoTour(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("PhotoTour: PhotoTour not implemented");
+    }
+
+
+    PhotoTour::PhotoTour(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("PhotoTour: PhotoTour not implemented");
+    }
 
 }
