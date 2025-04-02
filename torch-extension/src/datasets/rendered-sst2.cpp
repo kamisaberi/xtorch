20596345f802e603dfc773a4e41195ee0e043b84
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:15:54 +0330

    RenderedSST2 constructors throw  NotImplementedError

--- torch-extension/src/datasets/rendered-sst2.cpp
+++ torch-extension/src/datasets/rendered-sst2.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // RenderedSST2::RenderedSST2() {
-    //     throw NotImplementedException();
-    // }
+    RenderedSST2::RenderedSST2(const std::string &root): RenderedSST2::RenderedSST2(root, DataMode::TRAIN, false) {
+    }
+
+    RenderedSST2::RenderedSST2(const std::string &root, DataMode mode): RenderedSST2::RenderedSST2(root, mode, false) {
+    }
+
+    RenderedSST2::RenderedSST2(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("RenderedSST2: RenderedSST2 not implemented");
+    }
+
+
+    RenderedSST2::RenderedSST2(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("RenderedSST2: RenderedSST2 not implemented");
+    }
+
 
 }
