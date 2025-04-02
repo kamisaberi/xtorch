48ad38008baa47dd671de37f62c1f2f3016ea68a
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 16:42:06 +0330

    CelebA constructors throw  NotImplementedError

--- torch-extension/src/datasets/celeba.cpp
+++ torch-extension/src/datasets/celeba.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // CelebA::CelebA() {
-    //     throw NotImplementedException();
-    // }
+    CelebA::CelebA(const std::string &root): CelebA::CelebA(root, DataMode::TRAIN, false) {
+    }
+
+    CelebA::CelebA(const std::string &root, DataMode mode): CelebA::CelebA(root, mode, false) {
+    }
+
+    CelebA::CelebA(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("CelebA: CelebA not implemented");
+    }
+
+
+    CelebA::CelebA(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("CelebA: CelebA not implemented");
+    }
 
 }
