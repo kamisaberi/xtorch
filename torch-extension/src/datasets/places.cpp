62df9a6d07cc32ce2c5cf49eac18cf407c4f1eb0
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:14:30 +0330

    Places365 constructors throw  NotImplementedError

--- torch-extension/src/datasets/places.cpp
+++ torch-extension/src/datasets/places.cpp
@@ -2,8 +2,22 @@
 
 namespace xt::data::datasets {
 
-    // Places365::Places365() {
-    //     throw NotImplementedException();
-    // }
+    Places365::Places365(const std::string &root): Places365::Places365(root, DataMode::TRAIN, false) {
+    }
+
+    Places365::Places365(const std::string &root, DataMode mode): Places365::Places365(root, mode, false) {
+    }
+
+    Places365::Places365(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Places365: Places365 not implemented");
+    }
+
+
+    Places365::Places365(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Places365: Places365 not implemented");
+    }
+
+
 
 }
