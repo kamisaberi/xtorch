b907a653c8d643452fd714267dd02d15dcbb2d10
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 01:15:34 +0330

    INaturalist constructors throw  NotImplementedError

--- torch-extension/src/datasets/inaturalist.cpp
+++ torch-extension/src/datasets/inaturalist.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // INaturalist::INaturalist() {
-    //     throw NotImplementedException();
-    // }
+    INaturalist::INaturalist(const std::string &root): INaturalist::INaturalist(root, DataMode::TRAIN, false) {
+    }
+
+    INaturalist::INaturalist(const std::string &root, DataMode mode): INaturalist::INaturalist(root, mode, false) {
+    }
+
+    INaturalist::INaturalist(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("INaturalist: INaturalist not implemented");
+    }
+
+
+    INaturalist::INaturalist(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("INaturalist: INaturalist not implemented");
+    }
 
 }
