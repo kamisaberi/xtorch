a20bdd564ba0ec9ad7e4d5a4f9453f09ccc98d60
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 00:48:55 +0330

    ImageNet constructors throw  NotImplementedError

--- torch-extension/include/datasets/imagenet.h
+++ torch-extension/include/datasets/imagenet.h
@@ -6,13 +6,15 @@
 namespace xt::data::datasets {
     class ImageNet : BaseDataset {
     public :
-        ImageNet(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+        ImageNet(const std::string &root);
+        ImageNet(const std::string &root, DataMode mode);
+        ImageNet(const std::string &root, DataMode mode , bool download);
+        ImageNet(const std::string &root, DataMode mode , bool download, TransformType transforms);
 
-        ImageNet(const fs::path &root, DatasetArguments args);
 
     private :
-        void load_data(DataMode mode = DataMode::TRAIN);
+        void load_data();
 
-        void check_resources(const std::string &root, bool download = false);
+        void check_resources();
     };
 }
