5312040c88e3a3ef0e43b11cc19345512c96f08c
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 17:01:47 +0330

    CocoDetection and CocoCaptions constructors throw  NotImplementedError

--- torch-extension/src/datasets/coco.cpp
+++ torch-extension/src/datasets/coco.cpp
@@ -2,11 +2,40 @@
 
 namespace xt::data::datasets {
 
-    // CocoDetection::CocoDetection() {
-    //     throw NotImplementedException();
-    // }
-    // CocoCaptions::CocoCaptions() {
-    //     throw NotImplementedException();
-    // }
+    // ---------------------- CocoDetection ---------------------- //
+    CocoDetection::CocoDetection(const std::string &root): CocoDetection::CocoDetection(root, DataMode::TRAIN, false) {
+    }
+
+    CocoDetection::CocoDetection(const std::string &root, DataMode mode): CocoDetection::CocoDetection(root, mode, false) {
+    }
+
+    CocoDetection::CocoDetection(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("CocoDetection: CocoDetection not implemented");
+    }
+
+
+    CocoDetection::CocoDetection(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("CocoDetection: CocoDetection not implemented");
+    }
+
+
+    // ---------------------- CocoCaptions ---------------------- //
+    CocoCaptions::CocoCaptions(const std::string &root): CocoCaptions::CocoCaptions(root, DataMode::TRAIN, false) {
+    }
+
+    CocoCaptions::CocoCaptions(const std::string &root, DataMode mode): CocoCaptions::CocoCaptions(root, mode, false) {
+    }
+
+    CocoCaptions::CocoCaptions(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("CocoCaptions: CocoCaptions not implemented");
+    }
+
+
+    CocoCaptions::CocoCaptions(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("CocoCaptions: CocoCaptions not implemented");
+    }
+
 
 }
