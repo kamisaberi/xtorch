99a7436f2815943068163eb08b984257e873db16
kamran saberifard <kamisaberi@gmail.com>
2025-04-02 14:13:41 +0330

    SBU constructors recreated

--- torch-extension/include/datasets/sbu.h
+++ torch-extension/include/datasets/sbu.h
@@ -26,18 +26,20 @@ namespace xt::data::datasets {
 
          */
     public :
-        SBU(const std::string &root, DataMode mode = DataMode::TRAIN, bool download = false);
+        SBU(const std::string &root);
+        SBU(const std::string &root, DataMode mode);
+        SBU(const std::string &root, DataMode mode , bool download);
+        SBU(const std::string &root, DataMode mode , bool download, vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
 
-        SBU(const fs::path &root, DatasetArguments args);
 
     private :
         fs::path url = fs::path("https://www.cs.rice.edu/~vo9/sbucaptions/SBUCaptionedPhotoDataset.tar.gz");
         fs::path dataset_file_name = fs::path("SBUCaptionedPhotoDataset.tar.gz");
         std::string dataset_file_md5 = "9aec147b3488753cf758b4d493422285";
         fs::path dataset_folder_name = "sbu";
-        void load_data(DataMode mode = DataMode::TRAIN);
+        void load_data();
 
-        void check_resources(const std::string &root, bool download = false);
+        void check_resources();
 
 
     };
