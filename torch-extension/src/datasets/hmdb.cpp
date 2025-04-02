d998edd196f9d0601382d8b5b534b315bb483feb
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 00:47:09 +0330

    HMDB51 constructors throw  NotImplementedError

--- torch-extension/src/datasets/hmdb.cpp
+++ torch-extension/src/datasets/hmdb.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // HMDB51::HMDB51() {
-    //     throw NotImplementedException();
-    // }
+    HMDB51::HMDB51(const std::string &root): HMDB51::HMDB51(root, DataMode::TRAIN, false) {
+    }
+
+    HMDB51::HMDB51(const std::string &root, DataMode mode): HMDB51::HMDB51(root, mode, false) {
+    }
+
+    HMDB51::HMDB51(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("HMDB51: HMDB51 not implemented");
+    }
+
+
+    HMDB51::HMDB51(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("HMDB51: HMDB51 not implemented");
+    }
 
 }
