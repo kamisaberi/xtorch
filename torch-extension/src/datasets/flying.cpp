73df3c4c43d049f9c0f2b942dbf4068b32e89770
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 00:16:04 +0330

    FlyingChairs and FlyingThings3D constructors throw  NotImplementedError

--- torch-extension/src/datasets/flying.cpp
+++ torch-extension/src/datasets/flying.cpp
@@ -2,11 +2,46 @@
 
 namespace xt::data::datasets {
 
-    // FlyingChairs::FlyingChairs() {
-    //     throw NotImplementedException();
-    // }
-    // FlyingThings3D::FlyingThings3D() {
-    //     throw NotImplementedException();
-    // }
+    // ---------------------- FlyingChairs ---------------------- //
+
+    FlyingChairs::FlyingChairs(const std::string &root): FlyingChairs::FlyingChairs(root, DataMode::TRAIN, false) {
+    }
+
+    FlyingChairs::FlyingChairs(const std::string &root, DataMode mode): FlyingChairs::FlyingChairs(root, mode, false) {
+    }
+
+    FlyingChairs::FlyingChairs(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("FlyingChairs: FlyingChairs not implemented");
+    }
+
+
+    FlyingChairs::FlyingChairs(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("FlyingChairs: FlyingChairs not implemented");
+    }
+
+
+
+
+
+
+    // ---------------------- Caltech101 ---------------------- //
+
+
+    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
 
 }
