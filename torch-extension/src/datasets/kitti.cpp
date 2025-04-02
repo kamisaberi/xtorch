8e2f635640f93c0f3ca277b0f1adcd3ac7d0f629
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 01:28:45 +0330

    Kitti constructors throw  NotImplementedError

--- torch-extension/src/datasets/kitti.cpp
+++ torch-extension/src/datasets/kitti.cpp
@@ -2,21 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // ---------------------- Caltech101 ---------------------- //
-    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    // ---------------------- Kitti ---------------------- //
+    Kitti::Kitti(const std::string &root): Kitti::Kitti(root, DataMode::TRAIN, false) {
     }
 
-    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    Kitti::Kitti(const std::string &root, DataMode mode): Kitti::Kitti(root, mode, false) {
     }
 
-    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    Kitti::Kitti(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Kitti: Kitti not implemented");
     }
 
 
-    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
+    Kitti::Kitti(const std::string &root, DataMode mode, bool download,
                            TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+        throw std::runtime_error("Kitti: Kitti not implemented");
     }
 
 
