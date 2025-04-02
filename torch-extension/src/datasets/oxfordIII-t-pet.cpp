cbff0ad523d8bfdf0ffb99e6cb899de6f637436c
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 02:04:30 +0330

    OxfordIIITPet constructors throw  NotImplementedError

--- torch-extension/src/datasets/oxfordIII-t-pet.cpp
+++ torch-extension/src/datasets/oxfordIII-t-pet.cpp
@@ -2,8 +2,20 @@
 
 namespace xt::data::datasets {
 
-    // OxfordIIITPet::OxfordIIITPet() {
-    //     throw NotImplementedException();
-    // }
+    OxfordIIITPet::OxfordIIITPet(const std::string &root): OxfordIIITPet::OxfordIIITPet(root, DataMode::TRAIN, false) {
+    }
+
+    OxfordIIITPet::OxfordIIITPet(const std::string &root, DataMode mode): OxfordIIITPet::OxfordIIITPet(root, mode, false) {
+    }
+
+    OxfordIIITPet::OxfordIIITPet(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("OxfordIIITPet: OxfordIIITPet not implemented");
+    }
+
+
+    OxfordIIITPet::OxfordIIITPet(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("OxfordIIITPet: OxfordIIITPet not implemented");
+    }
 
 }
