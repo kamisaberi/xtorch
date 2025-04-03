6bb4310de0670f226946f6a6ca268f4f96d4be2b
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 03:31:32 +0330

    StanfordCars constructors throw  NotImplementedError

--- torch-extension/src/datasets/stanford-cars.cpp
+++ torch-extension/src/datasets/stanford-cars.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // StanfordCars::StanfordCars() {
-    //     throw NotImplementedException();
-    // }
+    StanfordCars::StanfordCars(const std::string &root): StanfordCars::StanfordCars(root, DataMode::TRAIN, false) {
+    }
+
+    StanfordCars::StanfordCars(const std::string &root, DataMode mode): StanfordCars::StanfordCars(root, mode, false) {
+    }
+
+    StanfordCars::StanfordCars(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("StanfordCars: StanfordCars not implemented");
+    }
+
+
+    StanfordCars::StanfordCars(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("StanfordCars: StanfordCars not implemented");
+    }
+
 
 }
