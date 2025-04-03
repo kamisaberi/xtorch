4d43b3fb34ef25f83659f96b7cdc293d7f2beee4
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 03:50:45 +0330

    VOCDetection constructors throw  NotImplementedError

--- torch-extension/src/datasets/voc.cpp
+++ torch-extension/src/datasets/voc.cpp
@@ -2,12 +2,40 @@
 
 namespace xt::data::datasets {
 
-    // VOCSegmentation::VOCSegmentation() {
-    //     throw NotImplementedException();
-    // }
-    //
-    // VOCDetection::VOCDetection() {
-    //     throw NotImplementedException();
-    // }
+    // ---------------------- VOCDetection ---------------------- //
+    VOCDetection::VOCDetection(const std::string &root): VOCDetection::VOCDetection(root, DataMode::TRAIN, false) {
+    }
+
+    VOCDetection::VOCDetection(const std::string &root, DataMode mode): VOCDetection::VOCDetection(root, mode, false) {
+    }
+
+    VOCDetection::VOCDetection(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("VOCDetection: VOCDetection not implemented");
+    }
+
+
+    VOCDetection::VOCDetection(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("VOCDetection: VOCDetection not implemented");
+    }
+
+
+
+    // ---------------------- Caltech101 ---------------------- //
+    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
+    }
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
+
+
+    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
+    }
 
 }
