6b34ab802968b16476977de240abbf1f5574da85
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 03:47:38 +0330

    USPS constructors throw  NotImplementedError

--- torch-extension/src/datasets/usps.cpp
+++ torch-extension/src/datasets/usps.cpp
@@ -1,9 +1,21 @@
 #include "../../include/datasets/usps.h"
 
 namespace xt::data::datasets {
+    USPS::USPS(const std::string &root): USPS::USPS(root, DataMode::TRAIN, false) {
+    }
 
-    // USPS::USPS() {
-    //     throw NotImplementedException();
-    // }
+    USPS::USPS(const std::string &root, DataMode mode): USPS::USPS(root, mode, false) {
+    }
+
+    USPS::USPS(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("USPS: USPS not implemented");
+    }
+
+
+    USPS::USPS(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("USPS: USPS not implemented");
+    }
+
 
 }
