d2db1f1bd8f1a6b75a1deaf8ee09ef2c12642795
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 03:48:41 +0330

    WIDERFace constructors throw  NotImplementedError

--- torch-extension/src/datasets/wider-face.cpp
+++ torch-extension/src/datasets/wider-face.cpp
@@ -2,8 +2,21 @@
 
 namespace xt::data::datasets {
 
-    // WIDERFace::WIDERFace() {
-    //     throw NotImplementedException();
-    // }
+    WIDERFace::WIDERFace(const std::string &root): WIDERFace::WIDERFace(root, DataMode::TRAIN, false) {
+    }
+
+    WIDERFace::WIDERFace(const std::string &root, DataMode mode): WIDERFace::WIDERFace(root, mode, false) {
+    }
+
+    WIDERFace::WIDERFace(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("WIDERFace: WIDERFace not implemented");
+    }
+
+
+    WIDERFace::WIDERFace(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("WIDERFace: WIDERFace not implemented");
+    }
+
 
 }
