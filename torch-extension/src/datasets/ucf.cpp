63017d2b299994e9d6b2a9610a441e0c2db56c98
kamran saberifard <kamisaberi@gmail.com>
2025-04-03 03:41:10 +0330

    UCF101 constructors throw  NotImplementedError

--- torch-extension/src/datasets/ucf.cpp
+++ torch-extension/src/datasets/ucf.cpp
@@ -2,18 +2,23 @@
 
 namespace xt::data::datasets {
 
-//     UCF101::UCF101(const std::string  &root) {
-//         std::cout << root<<std::endl;
-// //        throw NotImplementedException();
-//     }
-//     torch::data::Example<> UCF101::get(size_t index) {
-//         return {};
-//     }
-//
-// //
-//     torch::optional<size_t> UCF101::size() const {
-//         return 0;
-//     }
+
+    UCF101::UCF101(const std::string &root): UCF101::UCF101(root, DataMode::TRAIN, false) {
+    }
+
+    UCF101::UCF101(const std::string &root, DataMode mode): UCF101::UCF101(root, mode, false) {
+    }
+
+    UCF101::UCF101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("UCF101: UCF101 not implemented");
+    }
+
+
+    UCF101::UCF101(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("UCF101: UCF101 not implemented");
+    }
+
 
 
 }
