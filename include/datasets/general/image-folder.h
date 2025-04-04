7363f3d48239848a7c425fbeef1df229538bde11
kamran saberifard <kamisaberi@gmail.com>
2025-04-04 05:47:02 +0330

    csv and edf datasets files created

--- include/datasets/general/image-folder.h
+++ include/datasets/general/image-folder.h
@@ -20,18 +20,10 @@ namespace xt::data::datasets {
         ImageFolder(const std::string &root,bool load_sub_folders, DataMode mode , LabelsType label_type);
         ImageFolder(const std::string &root,bool load_sub_folders, DataMode mode , LabelsType label_type, vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
 
-        // ImageFolder(const std::string &root,bool load_sub_folders , DataMode mode , LabelsType label_type = LabelsType::BY_FOLDER );
-
-        // ImageFolder(const fs::path &root, DatasetArguments args);
-
     private:
-        fs::path dataset_folder_name = "imagenette";
         vector<string> labels_name;
         LabelsType label_type = LabelsType::BY_FOLDER;
         bool load_sub_folders = false;
-
         void load_data();
-
-        // void check_resources(const std::string &root, bool download = false);
     };
 }
