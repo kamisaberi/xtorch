0d1e1a4e1435cb88403c6a9b5b522aaeccb8cbc1
kamran saberifard <kamisaberi@gmail.com>
2025-04-04 04:28:33 +0330

    examples custom normalize removed

--- examples/cnn/lenet5-cifar10.cpp
+++ examples/cnn/lenet5-cifar10.cpp
@@ -20,19 +20,19 @@ int main() {
 
     auto transformed_dataset = dataset
             .map(xt::data::transforms::resize(size))
-            .map(xt::data::transforms::normalize(0.5, 0.5))
+            .map(torch::data::transforms::Normalize<>(0.5, 0.5))
             .map(torch::data::transforms::Stack<>());
 
 
     auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
         std::move(transformed_dataset), 64);
 
-    xt::models::LeNet5 model(10,3);
+    xt::models::LeNet5 model(10, 3);
     model.to(device);
     model.train();
     torch::optim::Adam optimizer(model.parameters(), torch::optim::AdamOptions(1e-3));
     for (size_t epoch = 0; epoch != 10; ++epoch) {
-//      cout << "epoch: " << epoch << endl;
+        //      cout << "epoch: " << epoch << endl;
         size_t batch_index = 0;
         auto train_loader_interator = train_loader->begin();
         auto train_loader_end = train_loader->end();
