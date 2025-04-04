58ebd16b64538fff529c84a7af42b704d7a289ea
kamran saberifard <kamisaberi@gmail.com>
2025-04-05 01:15:56 +0330

    csv datasets constructors created

--- include/datasets/general/csv-dataset.h
+++ include/datasets/general/csv-dataset.h
@@ -6,18 +6,29 @@
 
 
 namespace xt::data::datasets {
-
-
-
     class CSVDataset : public BaseDataset {
     public :
         CSVDataset(const std::string &file_path);
-        CSVDataset(const std::string &file_path,DataMode mode);
-        CSVDataset(const std::string &file_path,DataMode mode , vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
+
+        CSVDataset(const std::string &file_path, DataMode mode);
+
+        CSVDataset(const std::string &file_path, DataMode mode, vector<int> x_indices, int y_index);
+
+        CSVDataset(const std::string &file_path, DataMode mode, vector<int> x_indices, vector<int> y_indices);
+
+        CSVDataset(const std::string &file_path, DataMode mode, vector<string> x_titles, string y_title);
+
+        CSVDataset(const std::string &file_path, DataMode mode, vector<string> x_titles, vector<string> y_titles);
+
+        CSVDataset(const std::string &file_path, DataMode mode,
+                   vector<std::function<torch::Tensor(torch::Tensor)> > transforms);
 
     private:
+        vector<int> x_indices;
+        vector<int> y_indices;
         vector<string> labels_name;
         bool load_sub_folders = false;
+
         void load_data();
     };
 
@@ -25,16 +36,18 @@ namespace xt::data::datasets {
     class StackedCSVDataset : public BaseDataset {
     public :
         StackedCSVDataset(const std::string &folder_path);
-        StackedCSVDataset(const std::string &folder_path,DataMode mode);
-        StackedCSVDataset(const std::string &folder_path,DataMode mode, bool load_sub_folders);
-        StackedCSVDataset(const std::string &folder_path,DataMode mode, bool load_sub_folders , vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
+
+        StackedCSVDataset(const std::string &folder_path, DataMode mode);
+
+        StackedCSVDataset(const std::string &folder_path, DataMode mode, bool load_sub_folders);
+
+        StackedCSVDataset(const std::string &folder_path, DataMode mode, bool load_sub_folders,
+                          vector<std::function<torch::Tensor(torch::Tensor)> > transforms);
 
     private:
         vector<string> labels_name;
         bool load_sub_folders = false;
+
         void load_data();
     };
-
-
-
 }
