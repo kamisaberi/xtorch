2afc5f704d33675bbb906e960b78e5cfbed0d73d
kamran saberifard <kamisaberi@gmail.com>
2025-04-04 03:43:43 +0330

    Resize transform struct creatd and Doxigen-style docstring created

--- include/definitions/transforms.h
+++ include/definitions/transforms.h
@@ -11,24 +11,8 @@ namespace xt::data::transforms {
     std::function<torch::Tensor(torch::Tensor input)> create_resize_transform(std::vector<int64_t> size);
 
     torch::Tensor resize_tensor(const torch::Tensor &tensor, const std::vector<int64_t> &size);
-
-    torch::Tensor pad_tensor(const torch::Tensor &tensor, const int size);
-
-    torch::Tensor grayscale_image(const torch::Tensor &tensor);
-
-    torch::Tensor grayscale_to_rgb(const torch::Tensor &tensor);
-
-
     torch::data::transforms::Lambda<torch::data::Example<> > resize(std::vector<int64_t> size);
-
-    torch::data::transforms::Lambda<torch::data::Example<> > pad(int size);
-
-    torch::data::transforms::Lambda<torch::data::Example<> > grayscale();
-
     torch::data::transforms::Lambda<torch::data::Example<> > normalize(double mean, double stddev);
-
-    torch::data::transforms::Lambda<torch::data::Example<> > grayscaleToRGB();
-
 
     /**
      * @class Compose
--- src/definitions/transforms.cpp
+++ src/definitions/transforms.cpp
@@ -24,26 +24,6 @@ namespace xt::data::transforms {
         ).squeeze(0);
     }
 
-    torch::Tensor pad_tensor(const torch::Tensor &tensor, int size) {
-        std::vector<int64_t> ssize = {size, size};
-        return torch::nn::functional::interpolate(
-            tensor.unsqueeze(0),
-            torch::nn::functional::InterpolateFuncOptions().size(ssize).mode(torch::kBilinear).align_corners(false)
-        ).squeeze(0);
-    }
-
-    torch::Tensor grayscale_image(const torch::Tensor &tensor) {
-        return torch::nn::functional::interpolate(
-            tensor.unsqueeze(0),
-            torch::nn::functional::InterpolateFuncOptions().mode(torch::kBilinear).align_corners(false)
-        ).squeeze(0);
-    }
-
-    torch::Tensor grayscale_to_rgb(const torch::Tensor &tensor) {
-        torch::Tensor gray = tensor.dim() == 3 ? tensor.unsqueeze(1) : tensor; // Ensure [N, 1, H, W]
-        return gray.repeat({1, 3, 1, 1}); // [N, 1, H, W] -> [N, 3, H, W]
-    }
-
     torch::data::transforms::Lambda<torch::data::Example<> > resize(std::vector<int64_t> size) {
         return torch::data::transforms::Lambda<torch::data::Example<> >(
             [size](torch::data::Example<> example) {
@@ -53,38 +33,10 @@ namespace xt::data::transforms {
         );
     }
 
-    torch::data::transforms::Lambda<torch::data::Example<> > pad(int size) {
-        return torch::data::transforms::Lambda<torch::data::Example<> >(
-            [size](torch::data::Example<> example) {
-                example.data = pad_tensor(example.data, size);
-                return example;
-            }
-        );
-    }
-
-    torch::data::transforms::Lambda<torch::data::Example<> > grayscale() {
-        return torch::data::transforms::Lambda<torch::data::Example<> >(
-            [](torch::data::Example<> example) {
-                example.data = grayscale_image(example.data);
-                return example;
-            }
-        );
-    }
-
     torch::data::transforms::Lambda<torch::data::Example<> > normalize(double mean, double stddev) {
         return torch::data::transforms::Lambda<torch::data::Example<> >(
             [mean, stddev](torch::data::Example<> example) {
                 example.data = example.data.to(torch::kFloat32).div(255);
-                return example;
-            }
-        );
-    }
-
-
-    torch::data::transforms::Lambda<torch::data::Example<> > grayscaleToRGB() {
-        return torch::data::transforms::Lambda<torch::data::Example<> >(
-            [](torch::data::Example<> example) {
-                example.data = grayscale_image(example.data);
                 return example;
             }
         );
