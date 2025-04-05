bc679cc35b7358c9b966e03a0cc58a84f0cb197e
kamran saberifard <kamisaberi@gmail.com>
2025-04-05 20:41:07 +0330

    Normalize transform created

--- include/definitions/transforms.h
+++ include/definitions/transforms.h
@@ -334,5 +334,15 @@ namespace xt::data::transforms {
     };
 
 
+    struct Normalize {
+    public:
+        Normalize(std::vector<float> mean, std::vector<float> std);
+        torch::Tensor operator()(const torch::Tensor& tensor) const;
+    private:
+        std::vector<float> mean;
+        std::vector<float> std;
+
+    };
+
 
 }
