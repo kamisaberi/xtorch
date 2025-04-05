7d4174732eea769a492fc44a647bcf5c999fb7de
kamran saberifard <kamisaberi@gmail.com>
2025-04-05 21:24:08 +0330

    headers/transforms.h created

--- include/transforms/compose.h
+++ include/transforms/compose.h
@@ -1,16 +1,6 @@
 #pragma once
 
-#include <torch/torch.h>
-#include <torch/data/datasets/mnist.h>
-#include <iostream>
-#include <vector>
-#include <torch/data/transforms/base.h>
-#include <functional>
-#include <stdexcept>
-#include <cmath>
-#include <opencv2/opencv.hpp>
-#include <random>
-
+#include "../headers/transforms.h"
 
 namespace xt::data::transforms {
 
