b7b6b2991abcac29cb7eecca861d2ddc36f2b2b0
kamran saberifard <kamisaberi@gmail.com>
2025-04-05 21:08:08 +0330

    Compose class transfered to compose{.h & .cpp} files

--- include/transforms/compose.h
+++ include/transforms/compose.h
@@ -1,8 +1,72 @@
-//
-// Created by kami on 4/5/25.
-//
+#pragma once
 
-#ifndef COMPOSE_H
-#define COMPOSE_H
 
-#endif //COMPOSE_H
+#include <torch/torch.h>
+#include <torch/data/datasets/mnist.h>
+#include <iostream>
+#include <vector>
+#include <torch/data/transforms/base.h>
+#include <functional>
+#include <stdexcept>
+#include <cmath>
+#include <opencv2/opencv.hpp>
+#include <random>
+
+
+namespace xt::data::transforms {
+
+    /**
+     * @class Compose
+     * @brief A class to compose multiple tensor transformations into a single callable pipeline.
+     *
+     * The Compose class allows chaining of multiple transformation functions, each operating on a
+     * `torch::Tensor`, into a single operation. It is designed to facilitate preprocessing or
+     * augmentation of tensor data (e.g., images) by applying a sequence of transforms in the order
+     * they are provided. The transformations are stored as a vector of function objects and applied
+     * via the call operator.
+     */
+    class Compose {
+    public:
+        /**
+         * @brief Alias for a transformation function that takes a tensor and returns a tensor.
+         *
+         * This type alias defines a function signature for transformations that operate on
+         * `torch::Tensor` objects, enabling flexible composition of operations.
+         */
+        using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+
+        /**
+         * @brief Default constructor, initializing an empty transformation pipeline.
+         *
+         * Creates a Compose object with no transformations, allowing subsequent addition of transforms
+         * if needed.
+         */
+        Compose();
+
+        /**
+         * @brief Constructs a Compose object with a vector of transformation functions.
+         * @param transforms A vector of TransformFunc objects specifying the sequence of transformations.
+         *
+         * Initializes the Compose object with a predefined set of transformations to be applied in order.
+         */
+        Compose(std::vector<TransformFunc> transforms);
+
+        /**
+         * @brief Applies the sequence of transformations to the input tensor.
+         * @param input The input tensor to be transformed.
+         * @return A tensor resulting from applying all transformations in sequence.
+         *
+         * This operator applies each transformation in the `transforms` vector to the input tensor,
+         * passing the output of one transformation as the input to the next, and returns the final result.
+         */
+        torch::Tensor operator()(torch::Tensor input) const;
+
+    private:
+        /**
+         * @brief Vector storing the sequence of transformation functions.
+         *
+         * This member holds the list of transformations to be applied when the object is called.
+         */
+        std::vector<TransformFunc> transforms;
+    };
+}
