b8f7b917f75b3a16ee60878e65348b8257e53842
kamran saberifard <kamisaberi@gmail.com>
2025-04-05 23:06:12 +0330

    cutout.h file created

--- include/transforms/cutout.h
+++ include/transforms/cutout.h
@@ -5,5 +5,15 @@
 namespace xt::data::transforms {
 
 
+    struct Cutout {
+    public:
+        Cutout(int num_holes = 1, int hole_size = 16);
+
+        torch::Tensor operator()(const torch::Tensor& input_tensor) const;
+    private:
+        int num_holes;
+        int hole_size;
+
+    };
 
 }
