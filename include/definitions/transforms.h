55de478b2907d67fe57319f50825df3f9bb9455c
kamran saberifard <kamisaberi@gmail.com>
2025-04-05 09:12:43 +0330

    CenterCrop transform created

--- include/definitions/transforms.h
+++ include/definitions/transforms.h
@@ -169,4 +169,15 @@ namespace xt::data::transforms {
          */
         std::vector<int64_t> padding;
     };
+
+
+    struct CenterCrop {
+    public:
+        CenterCrop(std::vector<int64_t> size) ;
+
+        torch::Tensor operator()(torch::Tensor input);
+
+    private:
+        std::vector<int64_t> size;
+    };
 }
