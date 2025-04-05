6a46a17fd794a3bac1dab1993c487335a14d65c6
kamran saberifard <kamisaberi@gmail.com>
2025-04-06 00:31:46 +0330

    transforms reordered

--- include/definitions/transforms.h
+++ include/definitions/transforms.h
@@ -19,15 +19,6 @@ namespace xt::data::transforms {
 
 
 
-    struct Lambda {
-    public:
-        Lambda(std::function<torch::Tensor(torch::Tensor)> transform);
-
-        torch::Tensor operator()(torch::Tensor input);
-
-    private:
-        std::function<torch::Tensor(torch::Tensor)> transform;
-    };
 
 
     // struct Rotation {
--- include/transforms/lambda.h
+++ include/transforms/lambda.h
@@ -3,5 +3,15 @@
 
 namespace xt::data::transforms {
 
+    struct Lambda {
+    public:
+        Lambda(std::function<torch::Tensor(torch::Tensor)> transform);
+
+        torch::Tensor operator()(torch::Tensor input);
+
+    private:
+        std::function<torch::Tensor(torch::Tensor)> transform;
+    };
+
 
 }
