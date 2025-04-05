f53e07eff42d947968f293045fbfc796d5e46933
kamran saberifard <kamisaberi@gmail.com>
2025-04-06 00:14:21 +0330

    transforms reordered

--- src/transforms/resize.cpp
+++ src/transforms/resize.cpp
@@ -1,3 +1,41 @@
-//
-// Created by kami on 4/5/25.
-//
+#include "../../include/transforms/resize.h"
+
+namespace xt::data::transforms {
+
+    /**
+     * @brief Constructs a Resize object with the target size.
+     * @param size A vector of 64-bit integers specifying the target dimensions (e.g., {height, width}).
+     *
+     * Initializes the Resize object by storing the provided size vector, which will be used
+     * to resize input tensors in subsequent calls to the operator() function.
+     */
+    Resize::Resize(std::vector<int64_t> size) : size(size) {
+    }
+
+    /**
+     * @brief Resizes the input tensor image to the target size using bilinear interpolation.
+     * @param img The input tensor image to be resized, typically in format [C, H, W] (channels, height, width).
+     * @return A new tensor with the resized dimensions, in format [C, H', W'] where H' and W' match the target size.
+     *
+     * This function applies bilinear interpolation to resize the input image tensor to the dimensions
+     * specified in the constructor. It adds a batch dimension before interpolation (making the tensor
+     * [1, C, H, W]), resizes it using torch::nn::functional::interpolate, and removes the batch dimension
+     * afterward to return a tensor in the original format [C, H', W']. The interpolation is performed
+     * with bilinear mode and align_corners set to false for smooth and standard resizing behavior.
+     */
+    torch::Tensor Resize::operator()(torch::Tensor img) {
+        img = img.unsqueeze(0); // Add batch dimension
+        img = torch::nn::functional::interpolate(
+            img,
+            torch::nn::functional::InterpolateFuncOptions()
+            .size(std::vector<int64_t>({size[0], size[1]}))
+            .mode(torch::kBilinear)
+            .align_corners(false)
+        );
+        return img.squeeze(0); // Remove batch dimension
+    }
+
+
+
+
+}
