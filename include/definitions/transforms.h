ee02af44df7df0a3f2000a0508bdbee15bc5ab52
kamran saberifard <kamisaberi@gmail.com>
2025-04-05 20:01:04 +0330

    RandomGaussianBlur bug fixed

--- include/definitions/transforms.h
+++ include/definitions/transforms.h
@@ -9,6 +9,7 @@
 #include <stdexcept>
 #include <cmath>
 #include <opencv2/opencv.hpp>
+#include <random>
 
 
 namespace xt::data::transforms {
@@ -217,6 +218,19 @@ namespace xt::data::transforms {
     private:
         cv::Size kernel_size;
         double sigma;
+    };
+
+
+    struct RandomGaussianBlur {
+    private:
+        std::vector<int> kernel_sizes; // List of odd kernel sizes to choose from
+        double sigma_min;
+        double sigma_max;
+
+    public:
+        RandomGaussianBlur(std::vector<int> sizes = {3, 5}, double sigma_min = 0.1, double sigma_max = 2.0);
+
+        torch::Tensor operator()(const torch::Tensor &input_tensor);
     };
 
 
