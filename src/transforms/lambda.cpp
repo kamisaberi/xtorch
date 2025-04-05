983f30664ba445ee0355b8de11e1a1b3e16bc7cf
kamran saberifard <kamisaberi@gmail.com>
2025-04-06 00:32:04 +0330

    transforms reordered

--- src/transforms/lambda.cpp
+++ src/transforms/lambda.cpp
@@ -3,6 +3,14 @@
 namespace xt::data::transforms {
 
 
+    Lambda::Lambda(std::function<torch::Tensor(torch::Tensor)> transform)
+        : transform(transform) {
+    }
+
+    torch::Tensor Lambda::operator()(torch::Tensor input) {
+        return transform(input);
+    }
+
 
 
 }
