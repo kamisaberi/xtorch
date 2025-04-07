96bdabc6fd1691ccdae62e00cda2e51372ffd7d7
kamran saberifard <kamisaberi@gmail.com>
2025-04-08 00:49:13 +0330

    convert.cpp transform reordered

--- src/transforms/convert.cpp
+++ src/transforms/convert.cpp
@@ -1,3 +1,32 @@
-//
-// Created by kami on 4/5/25.
-//
+#include "../../include/transforms/crop.h"
+
+namespace xt::data::transforms {
+
+    torch::Tensor ToTensor::operator()(const cv::Mat &image) const {
+        cv::Mat img;
+
+        // Convert grayscale to 3 channels if needed
+        if (image.channels() == 1) {
+            cv::cvtColor(image, img, cv::COLOR_GRAY2RGB);
+        } else if (image.channels() == 4) {
+            cv::cvtColor(image, img, cv::COLOR_BGRA2RGB);
+        } else {
+            cv::cvtColor(image, img, cv::COLOR_BGR2RGB); // Assume BGR
+        }
+
+        // Convert uint8 -> float32 and normalize to [0, 1]
+        img.convertTo(img, CV_32F, 1.0 / 255.0);
+
+        // Create tensor from OpenCV Mat
+        auto tensor = torch::from_blob(img.data, {img.rows, img.cols, 3}, torch::kFloat32).clone();
+
+        // HWC -> CHW
+        tensor = tensor.permute({2, 0, 1});
+        return tensor;
+    }
+
+
+
+
+
+}
