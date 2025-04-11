0e1e4818c70bbef7f8d623ac82d588bb47cf3f41
kamran saberifard <kamisaberi@gmail.com>
2025-04-11 20:26:03 +0330

    ResNet model parametrized

--- include/models/cnn/resnet.h
+++ include/models/cnn/resnet.h
@@ -10,7 +10,6 @@
 using namespace std;
 
 namespace xt::models {
-
     namespace {
         struct ResidualBlock : torch::nn::Module {
             torch::nn::Sequential conv1 = nullptr, conv2 = nullptr, downsample = nullptr;
@@ -26,18 +25,19 @@ namespace xt::models {
     }
 
     struct ResNet : BaseModel {
-        mutable  int inplanes = 64;
-        mutable  torch::nn::Sequential conv1 = nullptr;
-        mutable  torch::nn::MaxPool2d maxpool = nullptr;
-        mutable  torch::nn::AvgPool2d avgpool = nullptr;
-        mutable  torch::nn::Sequential layer0 = nullptr, layer1 = nullptr, layer2 = nullptr, layer3 = nullptr;
-        mutable  torch::nn::Linear fc = nullptr;
+        mutable int inplanes = 64;
+        mutable torch::nn::Sequential conv1 = nullptr;
+        mutable torch::nn::MaxPool2d maxpool = nullptr;
+        mutable torch::nn::AvgPool2d avgpool = nullptr;
+        mutable torch::nn::Sequential layer0 = nullptr, layer1 = nullptr, layer2 = nullptr, layer3 = nullptr;
+        mutable torch::nn::Linear fc = nullptr;
 
         ResNet(vector<int> layers, int num_classes = 10, int in_channels = 3 /* input channels */);
+
+        ResNet(std::vector<int> layers, int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::nn::Sequential makeLayerFromResidualBlock(int planes, int blocks, int stride = 1);
 
         torch::Tensor forward(torch::Tensor x) const override;
     };
-
 }
