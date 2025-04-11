341da015cd39baf3249a05f906f78cd4020c5e0a
kamran saberifard <kamisaberi@gmail.com>
2025-04-11 20:20:09 +0330

    AlexNet model parametrized

--- include/models/cnn/alexnet.h
+++ include/models/cnn/alexnet.h
@@ -11,11 +11,15 @@ using namespace std;
 
 
 namespace xt::models {
-   struct AlexNet :BaseModel {
-        mutable  torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 = nullptr;
-        mutable  torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+    struct AlexNet : BaseModel {
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                        nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+
     public:
-        AlexNet(int num_classes /* classes */, int in_channels =3/* input channels */);
+        AlexNet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        AlexNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::Tensor forward(torch::Tensor x) const override;
     };
