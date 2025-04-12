20526046c3ae937b128d3e2ab160db8b92c7c5ea
kamran saberifard <kamisaberi@gmail.com>
2025-04-12 10:48:47 +0330

    MobileNetV3 interface created

--- include/models/cnn/mobilenet.h
+++ include/models/cnn/mobilenet.h
@@ -144,6 +144,15 @@ namespace xt::models {
     //
     //        return output
 
+    struct Bottleneck : torch::nn::Module {
+    public:
+        Bottleneck(int input_channels,int kernel,int stride,int expansion,int output_channels,torch::nn::Module activation ,bool se = false);
+        torch::Tensor forward(torch::Tensor x);
+
+    private:
+
+    };
+
 
     //---------------------------------------------------------------------------
 
