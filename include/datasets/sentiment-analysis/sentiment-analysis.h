89629bd52f4cd4336e3aa50b93b69edc36bfa148
kamran saberifard <kamisaberi@gmail.com>
2025-04-18 00:23:05 +0330

    any foler in include folder will gets a header file with same name of folder to keep all include files inside it #8

---
+++ include/datasets/sentiment-analysis/sentiment-analysis.h
@@ -0,0 +1,8 @@
+//
+// Created by kami on 4/18/25.
+//
+
+#ifndef SENTIMENT_ANALYSIS_H
+#define SENTIMENT_ANALYSIS_H
+
+#endif //SENTIMENT_ANALYSIS_H
---
+++ include/datasets/specific/specific.h
@@ -0,0 +1,8 @@
+//
+// Created by kami on 4/18/25.
+//
+
+#ifndef SPECIFIC_H
+#define SPECIFIC_H
+
+#endif //SPECIFIC_H
