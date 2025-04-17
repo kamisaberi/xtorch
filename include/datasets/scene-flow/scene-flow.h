9dbf0c175621cd0358846d22cca20b4bd44be3fe
kamran saberifard <kamisaberi@gmail.com>
2025-04-18 00:23:05 +0330

    any foler in include folder will gets a header file with same name of folder to keep all include files inside it #8

---
+++ include/datasets/scene-flow/scene-flow.h
@@ -0,0 +1,8 @@
+//
+// Created by kami on 4/18/25.
+//
+
+#ifndef SCENE_FLOW_H
+#define SCENE_FLOW_H
+
+#endif //SCENE_FLOW_H
---
+++ include/datasets/sequence-tagging/sequence-tagging.h
@@ -0,0 +1,8 @@
+//
+// Created by kami on 4/18/25.
+//
+
+#ifndef SEQUENCE_TAGGING_H
+#define SEQUENCE_TAGGING_H
+
+#endif //SEQUENCE_TAGGING_H
