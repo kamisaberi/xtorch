b938e584095c5c58d2966cbe70a16b76434a61e6
kamran saberifard <kamisaberi@gmail.com>
2025-04-18 00:23:35 +0330

    any foler in include folder will gets a header file with same name of folder to keep all include files inside it #9

---
+++ include/datasets/text-classification/text-classification.h
@@ -0,0 +1,8 @@
+//
+// Created by kami on 4/18/25.
+//
+
+#ifndef TEXT_CLASSIFICATION_H
+#define TEXT_CLASSIFICATION_H
+
+#endif //TEXT_CLASSIFICATION_H
---
+++ include/datasets/video/video.h
@@ -0,0 +1,8 @@
+//
+// Created by kami on 4/18/25.
+//
+
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#endif //VIDEO_H
