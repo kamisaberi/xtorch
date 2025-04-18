a0056cce1f9ad1b56b0607bf5cfbc1bfd80deda4
kamran saberifard <kamisaberi@gmail.com>
2025-04-18 14:05:17 +0330

    sudo make uninstall added to library

---
+++ xtorch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "include/data-loaders/data-loaders.h"
+#include "include/datasets/datasets.h"
+#include "include/media/media.h"
+#include "include/models/models.h"
+#include "include/trainers/trainers.h"
+#include "include/transforms/transforms.h"
+#include "include/types/types.h"
+#include "include/utils/utils.h"
+
+
+
+
+
