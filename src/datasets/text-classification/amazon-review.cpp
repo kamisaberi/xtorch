58ad20721edffcd941285bcf9fec24253fcd1623
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:05:05 +0330

    amazon-review dataset constructors created

--- src/datasets/text-classification/amazon-review.cpp
+++ src/datasets/text-classification/amazon-review.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/text-classification/amazon-review.h"
+
+namespace xt::data::datasets {
+
+    AmazonReview::AmazonReview(const std::string &root): AmazonReview::AmazonReview(root, DataMode::TRAIN, false) {
+    }
+
+    AmazonReview::AmazonReview(const std::string &root, DataMode mode): AmazonReview::AmazonReview(root, mode, false) {
+    }
+
+    AmazonReview::AmazonReview(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("AmazonReview: AmazonReview not implemented");
+    }
+
+
+    AmazonReview::AmazonReview(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("AmazonReview: AmazonReview not implemented");
+    }
+
+
+}
