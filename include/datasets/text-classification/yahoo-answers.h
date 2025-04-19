e660124a755eba17037e2d6a15ff703cb20f744a
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:19:01 +0330

    yahoo-answers dataset header file created

--- include/datasets/text-classification/yahoo-answers.h
+++ include/datasets/text-classification/yahoo-answers.h
@@ -1,8 +1,19 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
 
-#ifndef YAHOO_ANSWERS_H
-#define YAHOO_ANSWERS_H
 
-#endif //YAHOO_ANSWERS_H
+namespace xt::data::datasets {
+    class YahooAnswers : BaseDataset {
+        public :
+            explicit YahooAnswers(const std::string &root);
+        YahooAnswers(const std::string &root, DataMode mode);
+        YahooAnswers(const std::string &root, DataMode mode , bool download);
+        YahooAnswers(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
