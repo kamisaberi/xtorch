e9b9e49075580304392d357e3f1f69d0d43c6a42
kamran saberifard <kamisaberi@gmail.com>
2025-04-19 21:06:45 +0330

    cola dataset constructors created

--- src/datasets/natural-language-inference/cola.cpp
+++ src/datasets/natural-language-inference/cola.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/natural-language-inference/cola.h"
+
+namespace xt::data::datasets {
+
+    COLA::COLA(const std::string &root): COLA::COLA(root, DataMode::TRAIN, false) {
+    }
+
+    COLA::COLA(const std::string &root, DataMode mode): COLA::COLA(root, mode, false) {
+    }
+
+    COLA::COLA(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("COLA: COLA not implemented");
+    }
+
+
+    COLA::COLA(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("COLA: COLA not implemented");
+    }
+
+
+}
