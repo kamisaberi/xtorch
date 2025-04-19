a1c61795f7764e009dcf67b2074420fbfbcca246
kamran saberifard <kamisaberi@gmail.com>
2025-04-19 21:18:24 +0330

    qqp dataset header file created

--- include/datasets/natural-language-inference/qqp.h
+++ include/datasets/natural-language-inference/qqp.h
@@ -1,8 +1,19 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
 
-#ifndef QQP_H
-#define QQP_H
 
-#endif //QQP_H
+namespace xt::data::datasets {
+    class QQP : BaseDataset {
+        public :
+            explicit QQP(const std::string &root);
+        QQP(const std::string &root, DataMode mode);
+        QQP(const std::string &root, DataMode mode , bool download);
+        QQP(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
