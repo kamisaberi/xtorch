b82ce75ccf30bf78ede02c473372d3ca0a6dc176
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:22:37 +0330

    yelp-review dataset constructors created

--- src/datasets/text-classification/yelp-review.cpp
+++ src/datasets/text-classification/yelp-review.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/text-classification/yelp-review.h"
+
+namespace xt::data::datasets {
+
+    YelpReview::YelpReview(const std::string &root): YelpReview::YelpReview(root, DataMode::TRAIN, false) {
+    }
+
+    YelpReview::YelpReview(const std::string &root, DataMode mode): YelpReview::YelpReview(root, mode, false) {
+    }
+
+    YelpReview::YelpReview(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("YelpReview: YelpReview not implemented");
+    }
+
+
+    YelpReview::YelpReview(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("YelpReview: YelpReview not implemented");
+    }
+
+
+}
