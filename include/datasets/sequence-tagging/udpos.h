271e0e0e1e94e56f5c987ecdbeccd64f86c7f6a9
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:40:46 +0330

    udpos dataset header file created

--- include/datasets/sequence-tagging/udpos.h
+++ include/datasets/sequence-tagging/udpos.h
@@ -1,8 +1,19 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
 
-#ifndef UDPOS_H
-#define UDPOS_H
 
-#endif //UDPOS_H
+namespace xt::data::datasets {
+    class UDPOS : BaseDataset {
+        public :
+            explicit UDPOS(const std::string &root);
+        UDPOS(const std::string &root, DataMode mode);
+        UDPOS(const std::string &root, DataMode mode , bool download);
+        UDPOS(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
