006ee29077ef0028deb9a09f3024178f1f6b2e60
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:23:14 +0330

    stsb dataset constructors created

--- src/datasets/natural-language-inference/stsb.cpp
+++ src/datasets/natural-language-inference/stsb.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/natural-language-inference/stsb.h"
+
+namespace xt::data::datasets {
+
+    STSB::STSB(const std::string &root): STSB::STSB(root, DataMode::TRAIN, false) {
+    }
+
+    STSB::STSB(const std::string &root, DataMode mode): STSB::STSB(root, mode, false) {
+    }
+
+    STSB::STSB(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("STSB: STSB not implemented");
+    }
+
+
+    STSB::STSB(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("STSB: STSB not implemented");
+    }
+
+
+}
