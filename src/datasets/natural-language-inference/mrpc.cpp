4625e38428eeb9e542111abf60fcdf1b06d02ce1
kamran saberifard <kamisaberi@gmail.com>
2025-04-19 21:11:59 +0330

    mrpc dataset constructors created

--- src/datasets/natural-language-inference/mrpc.cpp
+++ src/datasets/natural-language-inference/mrpc.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/natural-language-inference/mrpc.h"
+
+namespace xt::data::datasets {
+
+    MRPC::MRPC(const std::string &root): MRPC::MRPC(root, DataMode::TRAIN, false) {
+    }
+
+    MRPC::MRPC(const std::string &root, DataMode mode): MRPC::MRPC(root, mode, false) {
+    }
+
+    MRPC::MRPC(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("MRPC: MRPC not implemented");
+    }
+
+
+    MRPC::MRPC(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("MRPC: MRPC not implemented");
+    }
+
+
+}
