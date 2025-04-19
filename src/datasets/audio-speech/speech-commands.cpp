a4ed6f360579bc4b51b1b438835da1f207b00846
saeed <kamisaberi@gmail.com>
2025-04-19 15:41:06 +0330

    speech-commands dataset constructors created

--- src/datasets/audio-speech/speech-commands.cpp
+++ src/datasets/audio-speech/speech-commands.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/audio-speech/speech-commands.h"
+
+namespace xt::data::datasets {
+
+    SpeechCommands::SpeechCommands(const std::string &root): CMUArctic::CMUArctic(root, DataMode::TRAIN, false) {
+    }
+
+    SpeechCommands::SpeechCommands(const std::string &root, DataMode mode): CMUArctic::CMUArctic(root, mode, false) {
+    }
+
+    SpeechCommands::SpeechCommands(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("SpeechCommands: SpeechCommands not implemented");
+    }
+
+
+    SpeechCommands::SpeechCommands(const std::string &root, DataMode mode, bool download,
+                         TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("SpeechCommands: SpeechCommands not implemented");
+    }
+
+
+}
