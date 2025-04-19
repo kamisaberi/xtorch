a42edb1554fb536bceff35f7928dc2cb95424a25
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:09:26 +0330

    db-pedia dataset constructors created

--- src/datasets/text-classification/db-pedia.cpp
+++ src/datasets/text-classification/db-pedia.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/text-classification/db-pedia.h"
+
+namespace xt::data::datasets {
+
+    DBPedia::DBPedia(const std::string &root): DBPedia::DBPedia(root, DataMode::TRAIN, false) {
+    }
+
+    DBPedia::DBPedia(const std::string &root, DataMode mode): DBPedia::DBPedia(root, mode, false) {
+    }
+
+    DBPedia::DBPedia(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("DBPedia: DBPedia not implemented");
+    }
+
+
+    DBPedia::DBPedia(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("DBPedia: DBPedia not implemented");
+    }
+
+
+}
