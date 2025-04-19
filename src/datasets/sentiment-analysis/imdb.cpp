d6b3a9d537fc06eb9b651eee77dfb85014d34228
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:31:33 +0330

    imdb dataset constructors created

--- src/datasets/sentiment-analysis/imdb.cpp
+++ src/datasets/sentiment-analysis/imdb.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/sentiment-analysis/imdb.h"
+
+namespace xt::data::datasets {
+
+    IMDB::IMDB(const std::string &root): IMDB::IMDB(root, DataMode::TRAIN, false) {
+    }
+
+    IMDB::IMDB(const std::string &root, DataMode mode): IMDB::IMDB(root, mode, false) {
+    }
+
+    IMDB::IMDB(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("IMDB: IMDB not implemented");
+    }
+
+
+    IMDB::IMDB(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("IMDB: IMDB not implemented");
+    }
+
+
+}
