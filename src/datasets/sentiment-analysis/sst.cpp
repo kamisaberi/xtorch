e50506ac19028468adabba2b57479b0dc943c476
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:34:46 +0330

    sst dataset constructors created

--- src/datasets/sentiment-analysis/sst.cpp
+++ src/datasets/sentiment-analysis/sst.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/sentiment-analysis/sst.h"
+
+namespace xt::data::datasets {
+
+    SST::SST(const std::string &root): SST::SST(root, DataMode::TRAIN, false) {
+    }
+
+    SST::SST(const std::string &root, DataMode mode): SST::SST(root, mode, false) {
+    }
+
+    SST::SST(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("SST: SST not implemented");
+    }
+
+
+    SST::SST(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("SST: SST not implemented");
+    }
+
+
+}
