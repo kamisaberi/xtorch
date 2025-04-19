91e40148ff355c59670d9de517862260b7f4998e
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:03:57 +0330

    amazon-review dataset header file created

--- include/datasets/text-classification/amazon-review.h
+++ include/datasets/text-classification/amazon-review.h
@@ -1,8 +1,19 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
 
-#ifndef AMAZON_REVIEW_H
-#define AMAZON_REVIEW_H
 
-#endif //AMAZON_REVIEW_H
+namespace xt::data::datasets {
+    class AmazonReview : BaseDataset {
+        public :
+            explicit AmazonReview(const std::string &root);
+        AmazonReview(const std::string &root, DataMode mode);
+        AmazonReview(const std::string &root, DataMode mode , bool download);
+        AmazonReview(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
