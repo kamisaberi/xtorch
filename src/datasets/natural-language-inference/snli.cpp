205edacf80bf379115c363029f0df72e4ec86d2f
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:21:05 +0330

    snli dataset constructors created

--- src/datasets/natural-language-inference/snli.cpp
+++ src/datasets/natural-language-inference/snli.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/natural-language-inference/snli.h"
+
+namespace xt::data::datasets {
+
+    SNLI::SNLI(const std::string &root): SNLI::SNLI(root, DataMode::TRAIN, false) {
+    }
+
+    SNLI::SNLI(const std::string &root, DataMode mode): SNLI::SNLI(root, mode, false) {
+    }
+
+    SNLI::SNLI(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("SNLI: SNLI not implemented");
+    }
+
+
+    SNLI::SNLI(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("SNLI: SNLI not implemented");
+    }
+
+
+}
