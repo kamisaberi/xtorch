532a8abe8e674869b3a3cd50c4f27370cdb3d697
kamran saberifard <kamisaberi@gmail.com>
2025-04-19 20:41:04 +0330

    multi dataset header file created

--- include/datasets/machine-translation/multi.h
+++ include/datasets/machine-translation/multi.h
@@ -1,8 +1,19 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
 
-#ifndef MULTI_H
-#define MULTI_H
 
-#endif //MULTI_H
+namespace xt::data::datasets {
+    class MULTI : BaseDataset {
+        public :
+            explicit MULTI(const std::string &root);
+        MULTI(const std::string &root, DataMode mode);
+        MULTI(const std::string &root, DataMode mode , bool download);
+        MULTI(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
