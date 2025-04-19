773ca1893dbd17c50ba9b316fba47021a4caf12c
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:28:04 +0330

    wnli dataset constructors created

--- src/datasets/natural-language-inference/wnli.cpp
+++ src/datasets/natural-language-inference/wnli.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/natural-language-inference/wnli.h"
+
+namespace xt::data::datasets {
+
+    WNLI::WNLI(const std::string &root): WNLI::WNLI(root, DataMode::TRAIN, false) {
+    }
+
+    WNLI::WNLI(const std::string &root, DataMode mode): WNLI::WNLI(root, mode, false) {
+    }
+
+    WNLI::WNLI(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("WNLI: WNLI not implemented");
+    }
+
+
+    WNLI::WNLI(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("WNLI: WNLI not implemented");
+    }
+
+
+}
