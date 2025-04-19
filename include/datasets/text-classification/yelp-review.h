b6b73ff4bada0ab3cdeb1d6388e3f0b48df0bd1c
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:21:32 +0330

    yelp-review dataset header file created

--- include/datasets/text-classification/yelp-review.h
+++ include/datasets/text-classification/yelp-review.h
@@ -1,8 +1,19 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
 
-#ifndef YELP_REVIEW_H
-#define YELP_REVIEW_H
 
-#endif //YELP_REVIEW_H
+namespace xt::data::datasets {
+    class YelpReview : BaseDataset {
+        public :
+            explicit YelpReview(const std::string &root);
+        YelpReview(const std::string &root, DataMode mode);
+        YelpReview(const std::string &root, DataMode mode , bool download);
+        YelpReview(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
