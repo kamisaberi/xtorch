e3e7dd42bccbae9dcdc958fbfc2db9ebe127d6b0
kamran saberifard <kamisaberi@gmail.com>
2025-04-19 21:19:35 +0330

    qqp dataset constructors created

--- src/datasets/natural-language-inference/qqp.cpp
+++ src/datasets/natural-language-inference/qqp.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/natural-language-inference/qqp.h"
+
+namespace xt::data::datasets {
+
+    QQP::QQP(const std::string &root): QQP::QQP(root, DataMode::TRAIN, false) {
+    }
+
+    QQP::QQP(const std::string &root, DataMode mode): QQP::QQP(root, mode, false) {
+    }
+
+    QQP::QQP(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("QQP: QQP not implemented");
+    }
+
+
+    QQP::QQP(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("QQP: QQP not implemented");
+    }
+
+
+}
