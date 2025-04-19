7dac5a05bfcb946cebb05f74b2f05a2eec2ab2f9
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:20:35 +0330

    yahoo-answers dataset constructors created

--- src/datasets/text-classification/yahoo-answers.cpp
+++ src/datasets/text-classification/yahoo-answers.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/text-classification/yahoo-answers.h"
+
+namespace xt::data::datasets {
+
+    YahooAnswers::YahooAnswers(const std::string &root): YahooAnswers::YahooAnswers(root, DataMode::TRAIN, false) {
+    }
+
+    YahooAnswers::YahooAnswers(const std::string &root, DataMode mode): YahooAnswers::YahooAnswers(root, mode, false) {
+    }
+
+    YahooAnswers::YahooAnswers(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("YahooAnswers: YahooAnswers not implemented");
+    }
+
+
+    YahooAnswers::YahooAnswers(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("YahooAnswers: YahooAnswers not implemented");
+    }
+
+
+}
