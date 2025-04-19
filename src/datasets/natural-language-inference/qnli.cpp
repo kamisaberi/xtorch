7025540b7692e37bbe9ee257a9e5aad6df3827a2
kamran saberifard <kamisaberi@gmail.com>
2025-04-19 21:15:34 +0330

    qnli dataset constructors created

--- src/datasets/natural-language-inference/qnli.cpp
+++ src/datasets/natural-language-inference/qnli.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/natural-language-inference/qnli.h"
+
+namespace xt::data::datasets {
+
+    QNLI::QNLI(const std::string &root): QNLI::QNLI(root, DataMode::TRAIN, false) {
+    }
+
+    QNLI::QNLI(const std::string &root, DataMode mode): QNLI::QNLI(root, mode, false) {
+    }
+
+    QNLI::QNLI(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("QNLI: QNLI not implemented");
+    }
+
+
+    QNLI::QNLI(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("QNLI: QNLI not implemented");
+    }
+
+
+}
