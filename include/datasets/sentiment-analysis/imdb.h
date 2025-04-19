0297586958ad00399f5e8990ed0ee6b73eeb5246
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:30:20 +0330

    imdb dataset header file created

--- include/datasets/sentiment-analysis/imdb.h
+++ include/datasets/sentiment-analysis/imdb.h
@@ -1,8 +1,19 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
 
-#ifndef IMDB_H
-#define IMDB_H
 
-#endif //IMDB_H
+namespace xt::data::datasets {
+    class IMDB : BaseDataset {
+        public :
+            explicit IMDB(const std::string &root);
+        IMDB(const std::string &root, DataMode mode);
+        IMDB(const std::string &root, DataMode mode , bool download);
+        IMDB(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
