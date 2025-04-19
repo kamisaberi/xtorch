140f718572b586e6076cba6809aae6868a7513a5
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:42:02 +0330

    udpos dataset constructors created

--- src/datasets/sequence-tagging/udpos.cpp
+++ src/datasets/sequence-tagging/udpos.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/sequence-tagging/udpos.h"
+
+namespace xt::data::datasets {
+
+    UDPOS::UDPOS(const std::string &root): UDPOS::UDPOS(root, DataMode::TRAIN, false) {
+    }
+
+    UDPOS::UDPOS(const std::string &root, DataMode mode): UDPOS::UDPOS(root, mode, false) {
+    }
+
+    UDPOS::UDPOS(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("UDPOS: UDPOS not implemented");
+    }
+
+
+    UDPOS::UDPOS(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("UDPOS: UDPOS not implemented");
+    }
+
+
+}
