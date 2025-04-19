4f4d77e5d60e338783b1bd909e1e1cabd918a4e6
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:18:44 +0330

    rte dataset constructors created

--- src/datasets/natural-language-inference/rte.cpp
+++ src/datasets/natural-language-inference/rte.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/natural-language-inference/rte.h"
+
+namespace xt::data::datasets {
+
+    RTE::RTE(const std::string &root): RTE::RTE(root, DataMode::TRAIN, false) {
+    }
+
+    RTE::RTE(const std::string &root, DataMode mode): RTE::RTE(root, mode, false) {
+    }
+
+    RTE::RTE(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("RTE: RTE not implemented");
+    }
+
+
+    RTE::RTE(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("RTE: RTE not implemented");
+    }
+
+
+}
