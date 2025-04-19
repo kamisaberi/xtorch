8acb4d306369044076822b3c0d267689daafefea
kamran saberifard <kamisaberi@gmail.com>
2025-04-19 20:26:53 +0330

    wiki-text dataset constructors created

--- src/datasets/language-modeling/wiki-text.cpp
+++ src/datasets/language-modeling/wiki-text.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/language-modeling/wiki-text.h"
+
+namespace xt::data::datasets {
+
+    WikiText::WikiText(const std::string &root): WikiText::WikiText(root, DataMode::TRAIN, false) {
+    }
+
+    WikiText::WikiText(const std::string &root, DataMode mode): WikiText::WikiText(root, mode, false) {
+    }
+
+    WikiText::WikiText(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("WikiText: WikiText not implemented");
+    }
+
+
+    WikiText::WikiText(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("WikiText: WikiText not implemented");
+    }
+
+
+}
