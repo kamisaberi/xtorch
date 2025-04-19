fd8879e6b7a3b782e59d0b2da8386532f3885c03
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:38:20 +0330

    con-ll-2000-chunking dataset header file created

--- src/datasets/sequence-tagging/con-ll-2000-chunking.cpp
+++ src/datasets/sequence-tagging/con-ll-2000-chunking.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/sequence-tagging/con-ll-2000-chunking.h"
+
+namespace xt::data::datasets {
+
+    CoNLL2000Chunking::CoNLL2000Chunking(const std::string &root): CoNLL2000Chunking::CoNLL2000Chunking(root, DataMode::TRAIN, false) {
+    }
+
+    CoNLL2000Chunking::CoNLL2000Chunking(const std::string &root, DataMode mode): CoNLL2000Chunking::CoNLL2000Chunking(root, mode, false) {
+    }
+
+    CoNLL2000Chunking::CoNLL2000Chunking(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("CoNLL2000Chunking: CoNLL2000Chunking not implemented");
+    }
+
+
+    CoNLL2000Chunking::CoNLL2000Chunking(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("CoNLL2000Chunking: CoNLL2000Chunking not implemented");
+    }
+
+
+}
