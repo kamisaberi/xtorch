4e49898cf27ee9ccb4ff5f839bb517438b74e7c3
saeed <kamisaberi@gmail.com>
2025-04-19 15:16:59 +0330

    crema-d dataset header file created

--- src/datasets/audio-speech/crema-d.cpp
+++ src/datasets/audio-speech/crema-d.cpp
@@ -1,3 +1,23 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+
+#include "../base/base.h"
+#include "../../headers/datasets.h"
+
+
+namespace xt::data::datasets {
+    class CremaD : BaseDataset {
+    public :
+        explicit CremaD(const std::string &root);
+
+        CremaD(const std::string &root, DataMode mode);
+
+        CremaD(const std::string &root, DataMode mode, bool download);
+
+        CremaD(const std::string &root, DataMode mode, bool download, TransformType transforms);
+
+    private :
+        void load_data();
+
+        void check_resources();
+    };
+}
