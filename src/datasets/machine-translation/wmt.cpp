29310c7f476bbefa0620c7946fa6d716dedb2073
kamran saberifard <kamisaberi@gmail.com>
2025-04-19 20:45:04 +0330

    wmt dataset constructors created

--- src/datasets/machine-translation/wmt.cpp
+++ src/datasets/machine-translation/wmt.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/machine-translation/wmt.h"
+
+namespace xt::data::datasets {
+
+    WMT::WMT(const std::string &root): WMT::WMT(root, DataMode::TRAIN, false) {
+    }
+
+    WMT::WMT(const std::string &root, DataMode mode): WMT::WMT(root, mode, false) {
+    }
+
+    WMT::WMT(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("WMT: WMT not implemented");
+    }
+
+
+    WMT::WMT(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("WMT: WMT not implemented");
+    }
+
+
+}
