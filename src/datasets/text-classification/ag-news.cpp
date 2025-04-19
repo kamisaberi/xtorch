d0770d38adc4b0899ed3743e247f2eac90129200
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:00:07 +0330

    ag-news dataset constructors created

--- src/datasets/text-classification/ag-news.cpp
+++ src/datasets/text-classification/ag-news.cpp
@@ -1,4 +1,22 @@
-//
-// Created by pc on 4/14/2025.
-//
+#include "../../../include/datasets/text-classification/ag-news.h"
 
+namespace xt::data::datasets {
+
+    AgNews::AgNews(const std::string &root): AgNews::AgNews(root, DataMode::TRAIN, false) {
+    }
+
+    AgNews::AgNews(const std::string &root, DataMode mode): AgNews::AgNews(root, mode, false) {
+    }
+
+    AgNews::AgNews(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("AgNews: AgNews not implemented");
+    }
+
+
+    AgNews::AgNews(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("AgNews: AgNews not implemented");
+    }
+
+
+}
