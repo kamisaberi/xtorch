fbc32717b2b71b228bf392693816195d7ce65040
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 01:12:28 +0330

    sogou-news dataset constructors created

--- src/datasets/text-classification/sogou-news.cpp
+++ src/datasets/text-classification/sogou-news.cpp
@@ -1,3 +1,22 @@
-//
-// Created by kami on 4/15/25.
-//
+#include "../../../include/datasets/text-classification/sogou-news.h"
+
+namespace xt::data::datasets {
+
+    SogouNews::SogouNews(const std::string &root): SogouNews::SogouNews(root, DataMode::TRAIN, false) {
+    }
+
+    SogouNews::SogouNews(const std::string &root, DataMode mode): SogouNews::SogouNews(root, mode, false) {
+    }
+
+    SogouNews::SogouNews(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("SogouNews: SogouNews not implemented");
+    }
+
+
+    SogouNews::SogouNews(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("SogouNews: SogouNews not implemented");
+    }
+
+
+}
