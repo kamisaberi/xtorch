81d0ea5178c5ae9ee0fa2983f166e77af5b23482
kamran saberifard <kamisaberi@gmail.com>
2025-04-20 00:26:48 +0330

    wnli dataset header file created

--- include/datasets/natural-language-inference/wnli.h
+++ include/datasets/natural-language-inference/wnli.h
@@ -1,8 +1,19 @@
-//
-// Created by kami on 4/15/25.
-//
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
 
-#ifndef WNLI_H
-#define WNLI_H
 
-#endif //WNLI_H
+namespace xt::data::datasets {
+    class WNLI : BaseDataset {
+        public :
+            explicit WNLI(const std::string &root);
+        WNLI(const std::string &root, DataMode mode);
+        WNLI(const std::string &root, DataMode mode , bool download);
+        WNLI(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
