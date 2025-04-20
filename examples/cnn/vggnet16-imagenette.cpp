3debbda1aaeb4dcb6e4cc83908721d7325228527
saeed <kamisaberi@gmail.com>
2025-04-20 15:12:58 +0330

    vggnet model moved to models/cnn/vggnet

--- examples/cnn/vggnet16-imagenette.cpp
+++ examples/cnn/vggnet16-imagenette.cpp
@@ -1,6 +1,6 @@
 #include "includes/base.h"
 #include "../../include/datasets/image-classification/imagenette.h"
-#include "../../include/models/cnn/vggnet.h"
+#include "../../include/models/cnn/vggnet/vggnet.h"
 #include "../../include/definitions/transforms.h"
 
 
