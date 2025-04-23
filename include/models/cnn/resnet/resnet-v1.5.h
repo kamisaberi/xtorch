1df6b325f78d44f454bb2715e6117c05267cc012
saeed <kamisaberi@gmail.com>
2025-04-23 13:23:28 +0330

    ResNetV15 model created

--- include/models/cnn/resnet/resnet-v1.5.h
+++ include/models/cnn/resnet/resnet-v1.5.h
@@ -24,7 +24,7 @@ namespace xt::models {
         };
     }
 
-    struct ResNet : BaseModel {
+    struct ResNetV15 : BaseModel {
         mutable int inplanes = 64;
         mutable torch::nn::Sequential conv1 = nullptr;
         mutable torch::nn::MaxPool2d maxpool = nullptr;
@@ -33,9 +33,9 @@ namespace xt::models {
         mutable torch::nn::Sequential layer0 = nullptr, layer1 = nullptr, layer2 = nullptr, layer3 = nullptr;
         mutable torch::nn::Linear fc = nullptr;
 
-        ResNet(vector<int> layers, int num_classes = 10, int in_channels = 3 /* input channels */);
+        ResNetV15(vector<int> layers, int num_classes = 10, int in_channels = 3 /* input channels */);
 
-        ResNet(std::vector<int> layers, int num_classes, int in_channels, std::vector<int64_t> input_shape);
+        ResNetV15(std::vector<int> layers, int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::nn::Sequential makeLayerFromResidualBlock(int planes, int blocks, int stride = 1);
 
