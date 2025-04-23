e1b82860139b3f9f31597f07bfed89c94bd89e1e
saeed <kamisaberi@gmail.com>
2025-04-23 14:48:28 +0330

    ShuffleNetV1 model interface created

--- include/models/cnn/shuffle-net/shuffle-net-v1.h
+++ include/models/cnn/shuffle-net/shuffle-net-v1.h
@@ -1,8 +1,26 @@
-//
-// Created by kami on 4/21/25.
-//
+#pragma once
+#include <torch/torch.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include "../../base.h"
 
-#ifndef SHUFFLE_NET_V1_H
-#define SHUFFLE_NET_V1_H
 
-#endif //SHUFFLE_NET_V1_H
+using namespace std;
+
+
+namespace xt::models {
+    struct ShuffleNetV1 : BaseModel {
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+
+    public:
+        ShuffleNetV1(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        ShuffleNetV1(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+    };
+}
