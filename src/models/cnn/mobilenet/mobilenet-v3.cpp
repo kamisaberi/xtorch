6bac90b844034c1db91a12467e2324e65d0a825f
kamisaberi <kamisaberi@gmail.com>
2025-04-23 23:55:29 +0330

    some bugs fixed

--- src/models/cnn/mobilenet/mobilenet-v3.cpp
+++ src/models/cnn/mobilenet/mobilenet-v3.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/models/cnn/mobilenet/mobilenet.h"
+#include "../../../../include/models/cnn/mobilenet/mobilenet-v3.h"
 
 namespace xt::models {
     HSigmoid::HSigmoid() {
