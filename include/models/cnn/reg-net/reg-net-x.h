06b25f5a6dd2fce696773b6558fc6bed9b2d93e6
saeed <kamisaberi@gmail.com>
2025-04-23 14:39:01 +0330

    RegNetX model interface created

--- include/models/cnn/reg-net/reg-net-x.h
+++ include/models/cnn/reg-net/reg-net-x.h
@@ -1,8 +1,26 @@
-//
-// Created by kami on 4/21/25.
-//
+#pragma once
+#include <torch/torch.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include "../../base.h"
 
-#ifndef REG_NET_X_H
-#define REG_NET_X_H
 
-#endif //REG_NET_X_H
+using namespace std;
+
+
+namespace xt::models {
+    struct RegNetX : BaseModel {
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+
+    public:
+        RegNetX(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        RegNetX(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+    };
+}
