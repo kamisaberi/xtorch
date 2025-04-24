05750fa3f34b91646a2f52af3d1a8cfa594c8eb9
kamisaberi <kamisaberi@gmail.com>
2025-04-25 00:14:10 +0330

    include/datasets/image_classification base header file includes fixed

--- src/datasets/image_classification/omniglot.cpp
+++ src/datasets/image_classification/omniglot.cpp
@@ -1,14 +1,15 @@
-#include "../../../include/datasets/image-classification/omniglot.h"
+#include "../../../include/datasets/image_classification/omniglot.h"
 
 namespace xt::data::datasets {
 
-    Omniglot::Omniglot(const std::string &root): Omniglot::Omniglot(root, DataMode::TRAIN, false) {
+    Omniglot::Omniglot(const std::string &root): Omniglot(root, DataMode::TRAIN, false) {
     }
 
     Omniglot::Omniglot(const std::string &root, DataMode mode): Omniglot::Omniglot(root, mode, false) {
     }
 
-    Omniglot::Omniglot(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+    Omniglot::Omniglot(const std::string& root, DataMode mode, bool download) : BaseDataset(root, mode, download)
+    {
         throw std::runtime_error("Omniglot: Omniglot not implemented");
     }
 
