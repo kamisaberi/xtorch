c3eecd7eb3d43a9dfb68a29618db3333655761db
kamisaberi <kamisaberi@gmail.com>
2025-04-25 00:26:39 +0330

    include/datasets/stereo_matching_depth_estimation base header file includes fixed

--- include/datasets/stereo_matching_depth_estimation/stereo_matching_depth_estimation.h
+++ include/datasets/stereo_matching_depth_estimation/stereo_matching_depth_estimation.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "cre-stereo.h"
-#include "eth-3d-stereo.h"
-#include "falling-things-stereo.h"
-#include "in-stereo.h"
+#include "cre_stereo.h"
+#include "eth_3d_stereo.h"
+#include "falling_things_stereo.h"
+#include "in_stereo.h"
 #include "middlebury.h"
-#include "scene-flow-stereo.h"
+#include "scene_flow_stereo.h"
