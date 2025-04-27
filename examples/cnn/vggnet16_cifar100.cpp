ddd2486fa4e0c4e072e7a60f60d78e652d2cf397
kamisaberi <kamisaberi@gmail.com>
2025-04-27 23:59:35 +0330

    cifar_10.h and cifar_100.h files created

--- examples/cnn/vggnet16_cifar100.cpp
+++ examples/cnn/vggnet16_cifar100.cpp
@@ -1,5 +1,5 @@
 #include "includes/base.h"
-#include "../../include/datasets/image_classification/cifar.h"
+#include "../../include/datasets/image_classification/cifar.h01"
 #include "../../include/models/cnn/vggnet/vggnet16.h"
 #include "../../include/definitions/transforms.h"
 
