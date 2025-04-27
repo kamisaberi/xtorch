b9337a9c7a53bb47ab9d478c4e76e1297cdd66e2
saeed <kamisaberi@gmail.com>
2025-04-27 15:04:04 +0330

    xtorch.h file documented

--- xtorch.h
+++ xtorch.h
@@ -1,3 +1,36 @@
+/**
+ * @file include.h
+ * @brief Main include file that aggregates all core components of the framework
+ *
+ * This header serves as the central include point for all major components
+ * of the framework. It provides forward includes for data handling, models,
+ * training, transformations, and utility functions.
+ *
+ * @author Kamran Saberifard
+ * @email kamisaberi@gmail.com
+ * @github https://github.com/kamisaberi
+ *
+ * @defgroup CoreComponents Core Framework Components
+ * @{
+ *   @defgroup DataLoading Data Loading and Processing
+ *   @defgroup Datasets Dataset Implementations
+ *   @defgroup Media Media Handling
+ *   @defgroup Models Model Architectures
+ *   @defgroup Training Training Algorithms
+ *   @defgroup Transforms Data Transformations
+ *   @defgroup Types Core Types
+ *   @defgroup Utilities Utility Functions
+ *   @defgroup Temp Temporary Components
+ * @}
+ *
+ * @mainpage Framework Overview
+ * This framework provides a comprehensive suite of tools for machine learning
+ * and data processing. The core components are organized into logical modules
+ * that can be included individually or through this master include file.
+ *
+ * @note For most applications, including this single header is sufficient to
+ * access all framework functionality.
+ */
 #pragma once
 
 #include "include/data_loaders/data_loaders.h"
@@ -9,8 +42,3 @@
 #include "include/types/types.h"
 #include "include/utils/utils.h"
 #include "include/temp/temp.h"
-
-
-
-
-
