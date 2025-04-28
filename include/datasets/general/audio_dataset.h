13ec051137d44cf73e77d9ce27dc52593231e162
saeed <kamisaberi@gmail.com>
2025-04-28 15:52:28 +0330

    src/trainers/trainer.cpp documented

--- include/datasets/general/audio_dataset.h
+++ include/datasets/general/audio_dataset.h
@@ -15,11 +15,9 @@
 namespace xt::data::datasets {
 
 
-
-
     class AudioDataset : public torch::data::datasets::Dataset<AudioDataset> {
     public:
-        AudioDataset(const std::string& audio_dir, const std::string& label_file) {
+        AudioDataset(const std::string &audio_dir, const std::string &label_file) {
             std::ifstream label_stream(label_file);
             std::string line;
             while (std::getline(label_stream, line)) {
@@ -36,7 +34,7 @@ namespace xt::data::datasets {
             std::vector<float> samples(file.frames());
             file.read(&samples[0], file.frames());
             auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU);
-            torch::Tensor waveform = torch::from_blob(samples.data(), {1, (int64_t)samples.size()}, options).clone();
+            torch::Tensor waveform = torch::from_blob(samples.data(), {1, (int64_t) samples.size()}, options).clone();
             torch::Tensor label = torch::full({1}, labels[index], torch::kLong);
             return {waveform, label};
         }
@@ -46,23 +44,23 @@ namespace xt::data::datasets {
         }
 
     private:
-        std::vector<std::string> file_paths;
+        std::vector <std::string> file_paths;
         std::vector<int> labels;
     };
 
     class StackedAudioDataset : public BaseDataset {
-        public :
-            StackedAudioDataset(const std::string &folder_path);
+    public :
+        StackedAudioDataset(const std::string &folder_path);
 
         StackedAudioDataset(const std::string &folder_path, DataMode mode);
 
         StackedAudioDataset(const std::string &folder_path, DataMode mode, bool load_sub_folders);
 
         StackedAudioDataset(const std::string &folder_path, DataMode mode, bool load_sub_folders,
-                          vector<std::function<torch::Tensor(torch::Tensor)> > transforms);
+                            vector <std::function<torch::Tensor(torch::Tensor)>> transforms);
 
     private:
-        vector<string> labels_name;
+        vector <string> labels_name;
         bool load_sub_folders = false;
 
         void load_data();
