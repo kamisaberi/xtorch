4d20c67c3ad7dd5d034b2b376e32e3b18ba9fc04
kamisaberi <kamisaberi@gmail.com>
2025-04-28 22:38:55 +0330

    mnist.h separated to individual files

---
+++ include/datasets/base/mnist_base.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace xt::data::datasets
+{
+    class MNISTBase : public BaseDataset
+    {
+    public:
+        explicit MNISTBase(const std::string& root);
+        MNISTBase(const std::string& root, DataMode mode);
+        MNISTBase(const std::string& root, DataMode mode, bool download);
+        MNISTBase(const std::string& root, DataMode mode, bool download,
+                  vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
+        void read_images(const std::string& file_path, int num_images);
+        void read_labels(const std::string& file_path, int num_labels);
+    };
+}
