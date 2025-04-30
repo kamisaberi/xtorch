89d020fbd6bc497d989606cb5b2bc5b9fb758fb0
kamisaberi <kamisaberi@gmail.com>
2025-04-30 23:57:00 +0330

    temp/CMakeLists.txt updated

--- temp/dltest3.cpp
+++ temp/dltest3.cpp
@@ -1,4 +1,4 @@
-#include "../include/datasets/image-classification/mnist.h"
+#include "../include/datasets/image_classification/mnist.h"
 #include "../include/models/cnn/lenet/lenet5.h"
 #include "../include/definitions/transforms.h"
 #include <torch/torch.h>
