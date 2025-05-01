b218f6a80f26a763d030392dc25b664ea3a3ba70
kamisaberi <kamisaberi@gmail.com>
2025-05-02 01:10:27 +0330

    temp/02/dp-03.cpp created

--- temp/02/dp-03.cpp
+++ temp/02/dp-03.cpp
@@ -267,7 +267,8 @@ int main() {
         // Define devices (2 GPUs)
         std::vector<torch::Device> devices = {
             torch::Device(torch::kCUDA, 0),
-            torch::Device(torch::kCUDA, 1)
+            // torch::Device(torch::kCUDA, 1),
+            torch::Device(torch::kCPU)
         };
 
         // Create DataParallel (batch size divisible by 2)
