7f2cde639792884cdbfd1cef3361e87e362d1dcc
kamisaberi <kamisaberi@gmail.com>
2025-05-02 04:26:16 +0330

    DataParallel class interface and implementation created

--- include/data_parallels/data_parallel.h
+++ include/data_parallels/data_parallel.h
@@ -9,6 +9,28 @@
 #include <chrono>
 #include <stdexcept>
 
+
+// Custom neural network module inheriting from torch::nn::Cloneable
+struct CustomNet : torch::nn::Cloneable<CustomNet> {
+    CustomNet() {
+        reset();
+    }
+
+    void reset() override {
+        fc1 = register_module("fc1", torch::nn::Linear(torch::nn::LinearOptions(784, 256).bias(true)));
+        fc2 = register_module("fc2", torch::nn::Linear(torch::nn::LinearOptions(256, 10).bias(true)));
+    }
+
+    torch::Tensor forward(torch::Tensor x) {
+        x = torch::relu(fc1->forward(x));
+        x = fc2->forward(x);
+        return x;
+    }
+
+    torch::nn::Linear fc1{nullptr}, fc2{nullptr};
+};
+
+
 // Optimized DataParallel class for multi-GPU training
 class DataParallel
 {
