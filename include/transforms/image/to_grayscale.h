1cffae62c9f270104d1028b1d6a39d5968b145a6
kamisaberi <kamisaberi@gmail.com>
2025-05-04 02:00:54 +0330

    grayscale_to_rgb.cpp transform moved to src/transforms/image

--- include/transforms/image/to_grayscale.h
+++ include/transforms/image/to_grayscale.h
@@ -6,14 +6,7 @@
 
 namespace xt::transforms::image {
 
-
     struct Grayscale {
-    public:
-        Grayscale();
-        torch::Tensor operator()(torch::Tensor input);
-    };
-
-    struct ToGray {
         torch::Tensor operator()(const torch::Tensor& color_tensor) const;
     };
 
