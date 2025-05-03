1eb9693111a97c4968cec4295375d8fe09c2adaf
kamisaberi <kamisaberi@gmail.com>
2025-05-04 03:03:45 +0330

    random_rotation.cpp created

--- include/transforms/image/rotation.h
+++ include/transforms/image/rotation.h
@@ -4,27 +4,6 @@
 
 namespace xt::transforms::image {
 
-    struct Rotation {
-    public:
-        Rotation(double angle_deg);
-
-        torch::Tensor operator()(const torch::Tensor &input_tensor);
-
-        private :
-            double angle; // Rotation angle in degrees
-    };
-
-
-    // struct Rotation {
-    // public:
-    //     Rotation(float angle);
-    //
-    //     torch::Tensor operator()(torch::Tensor input);
-    //
-    // private:
-    //     float angle;
-    // };
-
 
 
 }
