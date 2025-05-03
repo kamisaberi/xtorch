2fc889caec6ef60e56c42c530650aefae28bc391
kamisaberi <kamisaberi@gmail.com>
2025-05-04 01:12:16 +0330

    random_flip.h transform moved to include/transforms/image

--- include/transforms/image/random_flip.h
+++ include/transforms/image/random_flip.h
@@ -1,26 +1,11 @@
+#pragma once
 
 #include "../../headers/transforms.h"
 
-namespace xt::data::transforms {
-
-    struct HorizontalFlip {
-    public:
-        HorizontalFlip();
-
-        torch::Tensor operator()(torch::Tensor input);
-    };
-
-
-    struct VerticalFlip {
-    public:
-        VerticalFlip();
-
-        torch::Tensor operator()(torch::Tensor input);
-    };
-
-
-
-    struct RandomFlip {
+namespace xt::transforms::image
+{
+    struct RandomFlip
+    {
     private:
         double horizontal_prob;
         double vertical_prob;
@@ -28,9 +13,6 @@ namespace xt::data::transforms {
     public:
         RandomFlip(double h_prob = 0.5, double v_prob = 0.0);
 
-        torch::Tensor operator()(const torch::Tensor &input_tensor);
+        torch::Tensor operator()(const torch::Tensor& input_tensor);
     };
-
-
-
 }
