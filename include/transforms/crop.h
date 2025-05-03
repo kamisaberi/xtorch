a50c3448a5c5dfb6c81e89f378420e12a5a0561d
kamisaberi <kamisaberi@gmail.com>
2025-05-04 00:46:22 +0330

    random_crop.cpp transform moved to src/transforms/image

--- include/transforms/crop.h
+++ include/transforms/crop.h
@@ -7,27 +7,6 @@ namespace xt::data::transforms {
 
 
 
-    struct RandomCrop {
-    public:
-        RandomCrop(std::vector<int64_t> size);
-
-        torch::Tensor operator()(torch::Tensor input);
-
-    private:
-        std::vector<int64_t> size;
-    };
-
-
-    struct RandomCrop2 {
-    private:
-        int crop_height;
-        int crop_width;
-
-    public:
-        RandomCrop2(int height, int width);
-
-        torch::Tensor operator()(const torch::Tensor &input_tensor);
-    };
 
 
 
