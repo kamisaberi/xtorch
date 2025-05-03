afb4ef26eae44ba49dd19fc32fadc692255d1285
kamisaberi <kamisaberi@gmail.com>
2025-05-04 00:33:22 +0330

    center_crop.h transform moved to include/transforms/image

--- include/transforms/crop.h
+++ include/transforms/crop.h
@@ -5,15 +5,6 @@
 namespace xt::data::transforms {
 
 
-    struct CenterCrop {
-    public:
-        CenterCrop(std::vector<int64_t> size);
-
-        torch::Tensor operator()(torch::Tensor input);
-
-    private:
-        std::vector<int64_t> size;
-    };
 
 
     struct RandomCrop {
