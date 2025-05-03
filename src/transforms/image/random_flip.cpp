505e597ad1b6b9c9a6ea168176a73ae5d253ebf0
kamisaberi <kamisaberi@gmail.com>
2025-05-04 01:17:18 +0330

    random_flip.cpp transform moved to src/transforms/image

--- src/transforms/image/random_flip.cpp
+++ src/transforms/image/random_flip.cpp
@@ -2,36 +2,6 @@
 
 namespace xt::transforms::image {
 
-
-    HorizontalFlip::HorizontalFlip() {
-    }
-
-    torch::Tensor HorizontalFlip::operator()(torch::Tensor input) {
-        int64_t input_dims = input.dim();
-        if (input_dims < 2) {
-            throw std::runtime_error("Input tensor must have at least 2 dimensions (e.g., [H, W]).");
-        }
-
-        // Flip along the last dimension (width)
-        return torch::flip(input, {-1});
-    }
-
-
-    VerticalFlip::VerticalFlip() {
-    }
-
-    torch::Tensor VerticalFlip::operator()(torch::Tensor input) {
-        int64_t input_dims = input.dim();
-        if (input_dims < 2) {
-            throw std::runtime_error("Input tensor must have at least 2 dimensions (e.g., [H, W]).");
-        }
-
-        // Flip along the second-to-last dimension (height)
-        return torch::flip(input, {-2});
-    }
-
-
-
     RandomFlip::RandomFlip(double h_prob, double v_prob)
         : horizontal_prob(h_prob), vertical_prob(v_prob) {
     }
