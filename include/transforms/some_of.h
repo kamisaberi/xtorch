eb12a1330ceed2298e44594b8ee01b59f655f595
kamisaberi <kamisaberi@gmail.com>
2025-05-07 02:06:12 +0330

    transforms/SomeOf class initialized

--- include/transforms/some_of.h
+++ include/transforms/some_of.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "../headers/transforms.h"
+
+namespace xt::transforms
+{
+    class SomeOf : xt::Module
+    {
+    public:
+        using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+        SomeOf();
+        explicit SomeOf(std::vector<TransformFunc> transforms);
+        torch::Tensor operator()(torch::Tensor input) const override;
+
+    private:
+        std::vector<TransformFunc> transforms;
+    };
+}
