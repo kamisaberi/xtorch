d535039367d20707841f884211c32ed02bee6612
kamisaberi <kamisaberi@gmail.com>
2025-05-07 03:12:14 +0330

    transforms/Sometimes implemented

--- src/transforms/sometimes.cpp
+++ src/transforms/sometimes.cpp
@@ -1,3 +1,21 @@
-//
-// Created by kami on 5/6/25.
-//
+#include <utility>
+
+#include "../../include/transforms/sometimes.h"
+
+namespace xt::transforms
+{
+    using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+
+    Sometimes::Sometimes() = default;
+
+    Sometimes::Sometimes(std::vector<TransformFunc> transforms): xt::Module(), transforms(std::move(transforms))
+    {
+    }
+
+    torch::Tensor Sometimes::operator()(torch::Tensor input) const
+    {
+        int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
+        input = transforms[index](std::move(input));
+        return input;
+    }
+}
