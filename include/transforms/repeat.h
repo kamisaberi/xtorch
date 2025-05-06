6eb56319a20ba3297b4b8f9e6be45a5a7a5cdb38
kamisaberi <kamisaberi@gmail.com>
2025-05-07 00:23:55 +0330

    transforms/Repeat class initialized transform created

--- include/transforms/repeat.h
+++ include/transforms/repeat.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "../headers/transforms.h"
+
+namespace xt::transforms
+{
+    class Repeat : xt::Module
+    {
+    public:
+        using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+        Repeat();
+        explicit Repeat(TransformFunc transform);
+        torch::Tensor operator()(torch::Tensor input) const;
+
+    private:
+        TransformFunc transform;
+    };
+}
