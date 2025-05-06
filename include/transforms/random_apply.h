a7b6f5ca69fcc669c1140a6bdb3580084a48ea4a
kamisaberi <kamisaberi@gmail.com>
2025-05-07 02:08:53 +0330

    transforms/RandomApply initialized

--- include/transforms/random_apply.h
+++ include/transforms/random_apply.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "../headers/transforms.h"
+
+namespace xt::transforms
+{
+    class RandomApply : xt::Module
+    {
+    public:
+        using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+        RandomApply();
+        explicit RandomApply(std::vector<TransformFunc> transforms);
+        torch::Tensor operator()(torch::Tensor input) const override;
+
+    private:
+        std::vector<TransformFunc> transforms;
+    };
+}
