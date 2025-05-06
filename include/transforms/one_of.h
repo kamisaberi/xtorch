80ed83d426b3aae1a23ff6995c27cc980ec6eca6
kamisaberi <kamisaberi@gmail.com>
2025-05-07 01:59:21 +0330

    transforms/OneOf class initialized

--- include/transforms/one_of.h
+++ include/transforms/one_of.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "../headers/transforms.h"
+
+namespace xt::transforms
+{
+    class OneOf : xt::Module
+    {
+    public:
+        using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+        OneOf();
+        explicit OneOf(std::vector<TransformFunc> transforms);
+        torch::Tensor operator()(torch::Tensor input) const override;
+
+    private:
+        std::vector<TransformFunc> transforms;
+    };
+}
