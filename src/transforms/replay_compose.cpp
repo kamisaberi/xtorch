72bfddff6b583a72b4568cff46b8ee696d74a6ef
kamisaberi <kamisaberi@gmail.com>
2025-05-07 02:44:04 +0330

    transforms/ReplayCompose implemented

--- src/transforms/replay_compose.cpp
+++ src/transforms/replay_compose.cpp
@@ -1,3 +1,21 @@
-//
-// Created by kami on 5/6/25.
-//
+#include <utility>
+
+#include "../../include/transforms/replay_compose.h"
+
+namespace xt::transforms
+{
+    using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+
+    ReplayCompose::ReplayCompose() = default;
+
+    ReplayCompose::ReplayCompose(std::vector<TransformFunc> transforms): xt::Module(), transforms(std::move(transforms))
+    {
+    }
+
+    torch::Tensor ReplayCompose::operator()(torch::Tensor input) const
+    {
+        int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
+        input = transforms[index](std::move(input));
+        return input;
+    }
+}
