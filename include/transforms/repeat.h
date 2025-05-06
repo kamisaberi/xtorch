0764c6d1d0c03c17fa0ca6d8157f67a9749e745e
kamisaberi <kamisaberi@gmail.com>
2025-05-07 01:47:52 +0330

    transforms/Repeat class implemented

--- include/transforms/repeat.h
+++ include/transforms/repeat.h
@@ -4,13 +4,13 @@
 
 namespace xt::transforms
 {
-    class Repeat : xt::Module
+    class Repeat : public xt::Module
     {
     public:
         using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
         Repeat();
         explicit Repeat(TransformFunc transform, int n_times);
-        torch::Tensor operator()(torch::Tensor input) const;
+        torch::Tensor operator()(torch::Tensor input)  const override;
 
     private:
         TransformFunc transform;
