8de614f15682f1b3691329f499ea113ed6afe7a8
kamisaberi <kamisaberi@gmail.com>
2025-05-07 03:29:56 +0330

    transforms/Palindrome transform initialized

--- include/transforms/palindrome.h
+++ include/transforms/palindrome.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "../headers/transforms.h"
+
+namespace xt::transforms
+{
+    class Palindrome : xt::Module
+    {
+    public:
+        using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+        Palindrome();
+        explicit Palindrome(std::vector<TransformFunc> transforms);
+        torch::Tensor operator()(torch::Tensor input) const override;
+
+    private:
+        std::vector<TransformFunc> transforms;
+    };
+}
