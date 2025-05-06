eacb3e33f03d592546e9e53b3e12a60c3a96ca41
kamisaberi <kamisaberi@gmail.com>
2025-05-07 02:43:24 +0330

    transforms/ReplayCompose initialized

--- include/transforms/replay_compose.h
+++ include/transforms/replay_compose.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "../headers/transforms.h"
+
+namespace xt::transforms
+{
+    class ReplayCompose : xt::Module
+    {
+    public:
+        using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+        ReplayCompose();
+        explicit ReplayCompose(std::vector<TransformFunc> transforms);
+        torch::Tensor operator()(torch::Tensor input) const override;
+
+    private:
+        std::vector<TransformFunc> transforms;
+    };
+}
