4f0824ae15e49df338450160d88d9ec29eb15b5d
kamisaberi <kamisaberi@gmail.com>
2025-05-08 02:41:01 +0330

    resnext.cpp file moved to computer_vision/image_classification

--- src/models/cnn/res_net/resnext.cpp
+++ src/models/cnn/res_net/resnext.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/models/cnn/resnet/resnext.h"
+#include "../../../../include/models/computer_vision/image_classification/resnext.h"
 
 namespace xt::models {
     namespace {
