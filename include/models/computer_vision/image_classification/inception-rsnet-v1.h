e35ee97c755f4dd7c1563fd110d068607a577a0d
kamisaberi <kamisaberi@gmail.com>
2025-05-08 03:12:08 +0330

    inception-rsnet-v1.h file moved to computer_vision/image_classification

---
+++ include/models/computer_vision/image_classification/inception-rsnet-v1.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <torch/torch.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include "../../base.h"
+
+
+using namespace std;
+
+
+namespace xt::models {
+    struct InceptionResNet : BaseModel {
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+
+    public:
+        InceptionResNet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        InceptionResNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+    };
+}
