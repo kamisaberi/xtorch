6dd6c379fe51f42e421fc0f50609e7d6201800c9
kamisaberi <kamisaberi@gmail.com>
2025-05-08 01:37:31 +0330

    vggnet16.h file moved to computer_vision/image_classification

--- examples/cnn/vggnet16_mnist.cpp
+++ examples/cnn/vggnet16_mnist.cpp
@@ -1,6 +1,6 @@
 #include "includes/base.h"
 #include "../../include/datasets/image_classification/mnist.h"
-#include "../../include/models/cnn/vggnet/vggnet16.h"
+#include "../../include/models/computer_vision/image_classification/vggnet16.h"
 #include "../../include/definitions/transforms.h"
 
 using namespace std;
--- include/models/cnn/cnn.h
+++ include/models/cnn/cnn.h
@@ -6,4 +6,4 @@
 #include "mobilenet/mobilenet-v3.h"
 #include "resnet/resnet18.h"
 #include "unet/unet.h"
-#include "vggnet/vggnet16.h"
+#include "../computer_vision/image_classification/vggnet16.h"
