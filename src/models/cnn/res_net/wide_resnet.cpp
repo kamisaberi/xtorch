e69dec15faaae40e72e2a396a385c62fb3a8d326
kamisaberi <kamisaberi@gmail.com>
2025-05-08 02:41:53 +0330

    wide-resnet.h file moved to computer_vision/image_classification

--- src/models/cnn/res_net/wide_resnet.cpp
+++ src/models/cnn/res_net/wide_resnet.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/models/cnn/resnet/wide-resnet.h"
+#include "../../../../include/models/computer_vision/image_classification/wide-resnet.h"
 
 namespace xt::models {
     namespace {
