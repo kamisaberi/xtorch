b0ad383405d8302e7068b1e53ed399b4c11f55d1
kamisaberi <kamisaberi@gmail.com>
2025-05-07 03:32:10 +0330

    transforms/Palindrome transform implemented

--- src/transforms/palindrome.cpp
+++ src/transforms/palindrome.cpp
@@ -1,3 +1,28 @@
-//
-// Created by kami on 5/7/25.
-//
+#include <utility>
+
+#include "../../include/transforms/palindrome.h"
+
+namespace xt::transforms
+{
+    using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
+
+    Palindrome::Palindrome() = default;
+
+    Palindrome::Palindrome(std::vector<TransformFunc> transforms): xt::Module(), transforms(std::move(transforms))
+    {
+    }
+
+    torch::Tensor Palindrome::operator()(torch::Tensor input) const
+    {
+        for (const auto& transform : this->transforms)
+        {
+            input = transform(std::move(input));
+        }
+        for (int i = this->transforms.size() - 1; i >= 0; i--)
+        {
+            input = transforms[i](std::move(input));
+        }
+
+        return input;
+    }
+}
