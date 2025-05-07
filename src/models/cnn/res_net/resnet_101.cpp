2905f632ef07653fd38b03cecdaabdf5ccdfd738
kamisaberi <kamisaberi@gmail.com>
2025-05-08 01:48:55 +0330

    resnet101.h file moved to computer_vision/image_classification

--- src/models/cnn/res_net/resnet_101.cpp
+++ src/models/cnn/res_net/resnet_101.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/models/cnn/resnet/resnet101.h"
+#include "../../../../include/models/computer_vision/image_classification/resnet101.h"
 
 namespace xt::models {
     namespace {
