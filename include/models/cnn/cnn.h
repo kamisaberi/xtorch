2bfe84964fd5eda0cb53ff4c13d046148aed356f
kamisaberi <kamisaberi@gmail.com>
2025-05-09 00:04:32 +0330

    reg_net marked for unknown category

--- include/models/cnn/cnn.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-#include "../computer_vision/image_classification/alexnet.h"
-#include "../computer_vision/image_classification/lenet5.h"
-#include "../computer_vision/image_classification/mobilenet-v3.h"
-#include "../computer_vision/image_classification/resnet18.h"
-#include "../computer_vision/object_detection/unet.h"
-#include "../computer_vision/image_classification/vggnet16.h"
---
+++ include/models/computer_vision/computer_vision.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#include "image_classification/alexnet.h"
+#include "image_classification/lenet5.h"
+#include "image_classification/mobilenet-v3.h"
+#include "image_classification/resnet18.h"
+#include "object_detection/unet.h"
+#include "image_classification/vggnet16.h"
