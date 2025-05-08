420c9e4be8aa7d959e2f7282c5081f3a2913a417
kamisaberi <kamisaberi@gmail.com>
2025-05-09 02:03:02 +0330

    all Inception models moved to inception.h file created in computer_vision/image_classification

---
+++ include/models/computer_vision/image_classification/inception.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <torch/torch.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include "../../base.h"
+
+
+using namespace std;
+
+
+namespace xt::models
+{
+    struct InceptionV1 : BaseModel
+    {
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                                          nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+
+    public:
+        InceptionV1(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        InceptionV1(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+    };
+
+    struct InceptionV2 : BaseModel
+    {
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                                          nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+
+    public:
+        InceptionV2(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        InceptionV2(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+    };
+
+    struct InceptionV3 : BaseModel
+    {
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                                          nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+
+    public:
+        InceptionV3(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        InceptionV3(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+    };
+
+    struct InceptionV4 : BaseModel
+    {
+        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
+                                          nullptr;
+        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+
+    public:
+        InceptionV4(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        InceptionV4(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+    };
+}
