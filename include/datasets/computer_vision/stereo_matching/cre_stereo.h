25866774d8e2e5c9dbbfeeb6744a2a67b68fcc0b
kamisaberi <kamisaberi@gmail.com>
2025-05-10 01:40:59 +0330

    datasets moved to appropriate folders

--- include/datasets/computer_vision/stereo_matching/cre_stereo.h
+++ include/datasets/computer_vision/stereo_matching/cre_stereo.h
@@ -6,19 +6,6 @@
 
 
 namespace xt::data::datasets {
-    class CarlaStereo : public  BaseDataset {
-    public :
-        explicit  CarlaStereo(const std::string &root);
-        CarlaStereo(const std::string &root, DataMode mode);
-        CarlaStereo(const std::string &root, DataMode mode , bool download);
-        CarlaStereo(const std::string &root, DataMode mode , bool download, TransformType transforms);
-
-
-    private :
-        void load_data();
-
-        void check_resources();
-    };
 
 
     class CREStereo : public BaseDataset {
---
+++ src/datasets/computer_vision/stereo_matching/crala_stereo.cpp
@@ -0,0 +1,23 @@
+#include "../../../../include/datasets/computer_vision/stereo_matching/cre_stereo.h"
+
+namespace xt::data::datasets {
+    // ---------------------- CarlaStereo ---------------------- //
+    CarlaStereo::CarlaStereo(const std::string &root): CarlaStereo::CarlaStereo(root, DataMode::TRAIN, false) {
+    }
+
+    CarlaStereo::CarlaStereo(const std::string &root, DataMode mode): CarlaStereo::CarlaStereo(root, mode, false) {
+    }
+
+    CarlaStereo::CarlaStereo(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("CarlaStereo: CarlaStereo not implemented");
+    }
+
+
+    CarlaStereo::CarlaStereo(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("CarlaStereo: CarlaStereo not implemented");
+    }
+
+
+
+}
