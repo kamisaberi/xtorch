661ce7af1ee50333310e9efd5ab1f3210caf30ef
kamisaberi <kamisaberi@gmail.com>
2025-05-10 02:05:10 +0330

    datasets moved to appropriate folders

--- include/datasets/stereo_matching_depth_estimation/stereo_matching_depth_estimation.h
+++ include/datasets/stereo_matching_depth_estimation/stereo_matching_depth_estimation.h
@@ -4,5 +4,5 @@
 #include "../computer_vision/stereo_matching/eth_3d_stereo.h"
 #include "../computer_vision/stereo_matching/falling_things_stereo.h"
 #include "../computer_vision/stereo_matching/in_stereo.h"
-#include "middlebury.h"
+#include "../computer_vision/stereo_matching/middlebury.h"
 #include "../computer_vision/stereo_matching/scene_flow_stereo.h"
