08a3054af955d177779596c3f8e053d6b4e54ec3
kamisaberi <kamisaberi@gmail.com>
2025-05-10 00:24:54 +0330

    datasets moved to appropriate folders

--- src/datasets/computer_vision/image_classification/emnist.cpp
+++ src/datasets/computer_vision/image_classification/emnist.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/image_classification/emnist.h"
+#include "../../../../include/datasets/computer_vision/image_classification/emnist.h"
 
 namespace xt::data::datasets
 {
--- src/datasets/computer_vision/image_classification/fashion_mnist.cpp
+++ src/datasets/computer_vision/image_classification/fashion_mnist.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/image_classification/fashion_mnist.h"
+#include "../../../../include/datasets/computer_vision/image_classification/fashion_mnist.h"
 
 namespace xt::data::datasets {
 
