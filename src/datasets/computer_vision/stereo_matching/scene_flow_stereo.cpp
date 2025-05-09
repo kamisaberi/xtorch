8bf4c1a842ef7d40c27ee2c60550fbae738d426d
kamisaberi <kamisaberi@gmail.com>
2025-05-10 02:02:02 +0330

    datasets moved to appropriate folders

--- src/datasets/computer_vision/stereo_matching/scene_flow_stereo.cpp
+++ src/datasets/computer_vision/stereo_matching/scene_flow_stereo.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/stereo_matching_depth_estimation/scene_flow_stereo.h"
+#include "../../../../include/datasets/computer_vision/stereo_matching/scene_flow_stereo.h"
 
 namespace xt::data::datasets {
 
