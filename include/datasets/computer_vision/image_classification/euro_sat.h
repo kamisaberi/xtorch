3c9f171995a59315713311196eb4b3a9e95df9ce
kamisaberi <kamisaberi@gmail.com>
2025-05-10 00:24:54 +0330

    datasets moved to appropriate folders

--- include/datasets/computer_vision/image_classification/euro_sat.h
+++ include/datasets/computer_vision/image_classification/euro_sat.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "../base/base.h"
-#include "../../headers/datasets.h"
+#include "../../base/base.h"
+#include "../../../headers/datasets.h"
 
 using namespace std;
 namespace fs = std::filesystem;
