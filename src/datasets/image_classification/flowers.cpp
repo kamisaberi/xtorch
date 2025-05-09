d9814bae3353f334760bf202b7474c2b8c384e21
kamisaberi <kamisaberi@gmail.com>
2025-05-10 00:25:22 +0330

    datasets moved to appropriate folders

--- src/datasets/image_classification/flowers.cpp
+++ src/datasets/image_classification/flowers.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/image_classification/flowers.h"
+#include "../../../include/datasets/computer_vision/image_classification/flowers.h"
 
 namespace xt::data::datasets {
 
