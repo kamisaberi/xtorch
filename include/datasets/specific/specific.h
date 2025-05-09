3d59b7a4b3cb902029c661aac8cbe2b5a155d9ef
kamisaberi <kamisaberi@gmail.com>
2025-05-10 02:03:24 +0330

    datasets moved to appropriate folders

--- include/datasets/specific/specific.h
+++ include/datasets/specific/specific.h
@@ -2,5 +2,5 @@
 #include "../computer_vision/stereo_matching/kitti_2012_stereo.h"
 #include "lsun.h"
 #include "../computer_vision/image_classification/sbu.h"
-#include "sintel.h"
+#include "../computer_vision/stereo_matching/sintel_stereo.h"
 #include "../computer_vision/semantic_segmentation/voc_segmentation.h"
