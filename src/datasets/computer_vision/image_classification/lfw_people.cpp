bf674e356877e50d0d6affb5e88d0a374f35d2ae
kamisaberi <kamisaberi@gmail.com>
2025-05-10 00:51:38 +0330

    datasets moved to appropriate folders

---
+++ src/datasets/computer_vision/image_classification/lfw_people.cpp
@@ -0,0 +1,61 @@
+#include "../../../include/datasets/face_recognition/lfw.h"
+
+namespace xt::data::datasets {
+
+    // ---------------------- LFW ---------------------- //
+    LFW::LFW(const std::string &root): LFW::LFW(root, DataMode::TRAIN, false) {
+    }
+
+    LFW::LFW(const std::string &root, DataMode mode): LFW::LFW(root, mode, false) {
+    }
+
+    LFW::LFW(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("LFW: LFW not implemented");
+    }
+
+
+    LFW::LFW(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("LFW: LFW not implemented");
+    }
+
+
+
+    // ---------------------- LFWPeople ---------------------- //
+    LFWPeople::LFWPeople(const std::string &root): LFWPeople::LFWPeople(root, DataMode::TRAIN, false) {
+    }
+
+    LFWPeople::LFWPeople(const std::string &root, DataMode mode): LFWPeople::LFWPeople(root, mode, false) {
+    }
+
+    LFWPeople::LFWPeople(const std::string &root, DataMode mode, bool download) : LFW(root, mode, download) {
+        throw std::runtime_error("LFWPeople: LFWPeople not implemented");
+    }
+
+
+    LFWPeople::LFWPeople(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : LFW(root, mode, download, transforms) {
+        throw std::runtime_error("LFWPeople: LFWPeople not implemented");
+    }
+
+
+
+    // ---------------------- LFWPairs ---------------------- //
+    LFWPairs::LFWPairs(const std::string &root): LFWPairs::LFWPairs(root, DataMode::TRAIN, false) {
+    }
+
+    LFWPairs::LFWPairs(const std::string &root, DataMode mode): LFWPairs::LFWPairs(root, mode, false) {
+    }
+
+    LFWPairs::LFWPairs(const std::string &root, DataMode mode, bool download) : LFW(root, mode, download) {
+        throw std::runtime_error("LFWPairs: LFWPairs not implemented");
+    }
+
+
+    LFWPairs::LFWPairs(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : LFW(root, mode, download, transforms) {
+        throw std::runtime_error("LFWPairs: LFWPairs not implemented");
+    }
+
+
+}
