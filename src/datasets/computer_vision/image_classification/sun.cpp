491ad345fa3970c8b6ffd5b60d5a46cbfc0f7bdb
kamisaberi <kamisaberi@gmail.com>
2025-05-10 00:31:55 +0330

    datasets moved to appropriate folders

--- src/datasets/computer_vision/image_classification/sun.cpp
+++ src/datasets/computer_vision/image_classification/sun.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/image_classification/sun.h"
+#include "../../../../include/datasets/computer_vision/image_classification/sun.h"
 
 namespace xt::data::datasets {
 
--- src/datasets/computer_vision/image_classification/svhn.cpp
+++ src/datasets/computer_vision/image_classification/svhn.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/image_classification/svhn.h"
+#include "../../../../include/datasets/computer_vision/image_classification/svhn.h"
 
 namespace xt::data::datasets {
 
