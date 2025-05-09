02b4e09adff7b299c36cf608a7b5d0b6bd56066b
kamisaberi <kamisaberi@gmail.com>
2025-05-10 00:25:34 +0330

    datasets moved to appropriate folders

--- src/datasets/computer_vision/image_classification/flickr.cpp
+++ src/datasets/computer_vision/image_classification/flickr.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/computer_vision/image_classification/flickr.h"
+#include "../../../../include/datasets/computer_vision/image_classification/flickr.h"
 
 namespace xt::data::datasets {
 
