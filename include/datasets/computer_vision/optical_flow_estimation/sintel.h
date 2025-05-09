7d9fd42fd851cb6ce375f05ce0523415fd11c3dc
kamisaberi <kamisaberi@gmail.com>
2025-05-10 01:25:45 +0330

    datasets moved to appropriate folders

---
+++ include/datasets/computer_vision/optical_flow_estimation/sintel.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "../base/base.h"
+#include "../../headers/datasets.h"
+
+
+namespace xt::data::datasets {
+   class Sintel : BaseDataset {
+/*
+       """`Sintel <http://sintel.is.tue.mpg.de/>`_ Dataset for optical flow.
+
+           The dataset is expected to have the following structure: ::
+
+               root
+                   Sintel
+                       testing
+                           clean
+                               scene_1
+                               scene_2
+                               ...
+                           final
+                               scene_1
+                               scene_2
+                               ...
+                       training
+                           clean
+                               scene_1
+                               scene_2
+                               ...
+                           final
+                               scene_1
+                               scene_2
+                               ...
+                           flow
+                               scene_1
+                               scene_2
+                               ...
+
+           Args:
+               root (str or ``pathlib.Path``): Root directory of the Sintel Dataset.
+               split (string, optional): The dataset split, either "train" (default) or "test"
+               pass_name (string, optional): The pass to use, either "clean" (default), "final", or "both". See link above for
+                   details on the different passes.
+               transforms (callable, optional): A function/transform that takes in
+                   ``img1, img2, flow, valid_flow_mask`` and returns a transformed version.
+                   ``valid_flow_mask`` is expected for consistency with other datasets which
+                   return a built-in valid mask, such as :class:`~torchvision.datasets.KittiFlow`.
+           """
+
+ */
+   public :
+       explicit Sintel(const std::string &root);
+       Sintel(const std::string &root, DataMode mode);
+       Sintel(const std::string &root, DataMode mode , bool download);
+       Sintel(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+   private :
+       void load_data();
+
+       void check_resources();
+
+   };
+}
