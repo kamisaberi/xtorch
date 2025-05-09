705f61ab6d486e1c68ec5f3ed10c26046d458aed
kamisaberi <kamisaberi@gmail.com>
2025-05-10 01:17:52 +0330

    datasets moved to appropriate folders

--- include/datasets/specific/specific.h
+++ include/datasets/specific/specific.h
@@ -3,4 +3,4 @@
 #include "lsun.h"
 #include "../computer_vision/image_classification/sbu.h"
 #include "sintel.h"
-#include "voc.h"
+#include "../computer_vision/semantic_segmentation/voc_segmentation.h"
