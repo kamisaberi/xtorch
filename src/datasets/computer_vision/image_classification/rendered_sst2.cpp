b27ad82d28a080bbb4bc8caa9b02f892ee86216c
kamisaberi <kamisaberi@gmail.com>
2025-05-10 00:30:10 +0330

    datasets moved to appropriate folders

--- src/datasets/computer_vision/image_classification/rendered_sst2.cpp
+++ src/datasets/computer_vision/image_classification/rendered_sst2.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/image_classification/rendered_sst2.h"
+#include "../../../../include/datasets/computer_vision/image_classification/rendered_sst2.h"
 
 namespace xt::data::datasets {
 
--- src/datasets/computer_vision/image_classification/semeion.cpp
+++ src/datasets/computer_vision/image_classification/semeion.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/image_classification/semeion.h"
+#include "../../../../include/datasets/computer_vision/image_classification/semeion.h"
 
 namespace xt::data::datasets {
 
