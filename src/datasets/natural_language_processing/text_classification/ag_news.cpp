6f2f23a071d8994e613c0c62b0833811a3b34be5
kamran saberifard <kamisaberi@gmail.com>
2025-05-11 14:57:57 +0330

    some dataset files cread in natural_language_processing/text_classification

--- src/datasets/natural_language_processing/text_classification/ag_news.cpp
+++ src/datasets/natural_language_processing/text_classification/ag_news.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/text_classification/ag_news.h"
+#include "../../../../include/datasets/natural_language_processing/text_classification/ag_news.h"
 
 namespace xt::data::datasets {
 
