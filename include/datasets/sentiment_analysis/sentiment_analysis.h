2039f250683dbdbd4354e541dbfd645194c7f2b3
kamran saberifard <kamisaberi@gmail.com>
2025-05-11 15:15:11 +0330

    some dataset files cread in natural_language_processing/text_classification

--- include/datasets/sentiment_analysis/sentiment_analysis.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "../natural_language_processing/text_classification/imdb.h"
-#include "../natural_language_processing/text_classification/sst2.h"
--- include/datasets/text_classification/text_classification.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../natural_language_processing/text_classification/ag_news.h"
-#include "../natural_language_processing/text_classification/amazon_review_polarity.h"
-#include "../natural_language_processing/text_classification/db_pedia.h"
-#include "../natural_language_processing/text_classification/sogou_news.h"
-#include "../natural_language_processing/text_classification/yahoo_answers.h"
-#include "../natural_language_processing/text_classification/yelp_review_polarity.h"
