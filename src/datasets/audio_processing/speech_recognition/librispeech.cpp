b8858d88483627bfd9d21956e78591519d4f6c39
kamran saberifard <kamisaberi@gmail.com>
2025-05-11 14:19:33 +0330

    some dataset files cread in audio_processing/speech_recognition

--- src/datasets/audio_processing/speech_recognition/librispeech.cpp
+++ src/datasets/audio_processing/speech_recognition/librispeech.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/audio_speech/librispeech.h"
+#include "../../../../include/datasets/audio_processing/speech_recognition/librispeech.h"
 
 namespace xt::data::datasets {
 
