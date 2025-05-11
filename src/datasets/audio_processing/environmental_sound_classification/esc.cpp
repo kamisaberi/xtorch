3b8d861691044e1bc3d347fdbd665d42ca44f968
kamran saberifard <kamisaberi@gmail.com>
2025-05-11 14:48:07 +0330

    some dataset files cread in audio_processing/environmental_sound_classification

--- src/datasets/audio_processing/environmental_sound_classification/esc.cpp
+++ src/datasets/audio_processing/environmental_sound_classification/esc.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/audio_speech/esc.h"
+#include "../../../../include/datasets/audio_processing/environmental_sound_classification/esc.h"
 
 namespace xt::data::datasets {
 
--- src/datasets/audio_processing/environmental_sound_classification/urban_sound.cpp
+++ src/datasets/audio_processing/environmental_sound_classification/urban_sound.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/audio_speech/urban_sound.h"
+#include "../../../../include/datasets/audio_processing/environmental_sound_classification/urban_sound.h"
 
 namespace xt::data::datasets {
 
