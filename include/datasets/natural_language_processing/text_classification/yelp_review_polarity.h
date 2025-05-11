f79d3f7f62283e31f2a4e320f959ace7f91b5037
kamran saberifard <kamisaberi@gmail.com>
2025-05-11 15:12:39 +0330

    some dataset files cread in natural_language_processing/text_classification

---
+++ include/datasets/natural_language_processing/text_classification/yelp_review_polarity.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../../base/base.h"
+#include "../../../headers/datasets.h"
+
+
+namespace xt::data::datasets {
+    class YelpReview : BaseDataset {
+        public :
+            explicit YelpReview(const std::string &root);
+        YelpReview(const std::string &root, DataMode mode);
+        YelpReview(const std::string &root, DataMode mode , bool download);
+        YelpReview(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
---
+++ src/datasets/natural_language_processing/text_classification/yelp_review_polarity.cpp
@@ -0,0 +1,22 @@
+#include "../../../../include/datasets/natural_language_processing/text_classification/yelp_review_polarity.h"
+
+namespace xt::data::datasets {
+
+    YelpReview::YelpReview(const std::string &root): YelpReview::YelpReview(root, DataMode::TRAIN, false) {
+    }
+
+    YelpReview::YelpReview(const std::string &root, DataMode mode): YelpReview::YelpReview(root, mode, false) {
+    }
+
+    YelpReview::YelpReview(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+        throw std::runtime_error("YelpReview: YelpReview not implemented");
+    }
+
+
+    YelpReview::YelpReview(const std::string &root, DataMode mode, bool download,
+                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
+        throw std::runtime_error("YelpReview: YelpReview not implemented");
+    }
+
+
+}
