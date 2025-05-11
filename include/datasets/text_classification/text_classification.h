fcaff0240a6a60182f004969863555d017f71a43
kamran saberifard <kamisaberi@gmail.com>
2025-05-11 15:09:35 +0330

    some dataset files cread in natural_language_processing/text_classification

--- include/datasets/text_classification/text_classification.h
+++ include/datasets/text_classification/text_classification.h
@@ -3,6 +3,6 @@
 #include "../natural_language_processing/text_classification/ag_news.h"
 #include "../natural_language_processing/text_classification/amazon_review_polarity.h"
 #include "../natural_language_processing/text_classification/db_pedia.h"
-#include "sogou_news.h"
+#include "../natural_language_processing/text_classification/sogou_news.h"
 #include "yahoo_answers.h"
 #include "yelp_review.h"
