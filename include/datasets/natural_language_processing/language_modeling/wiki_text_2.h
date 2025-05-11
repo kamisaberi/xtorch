a4f19f97c3b9f596718e86f5d4bd67b7d36530ac
kamran saberifard <kamisaberi@gmail.com>
2025-05-11 15:24:45 +0330

    some dataset files cread in natural_language_processing/text_classification

---
+++ include/datasets/natural_language_processing/language_modeling/wiki_text_2.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../../base/base.h"
+#include "../../../headers/datasets.h"
+
+
+namespace xt::data::datasets {
+    class WikiText : BaseDataset {
+        public :
+            explicit WikiText(const std::string &root);
+        WikiText(const std::string &root, DataMode mode);
+        WikiText(const std::string &root, DataMode mode , bool download);
+        WikiText(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        private :
+            void load_data();
+
+        void check_resources();
+    };
+}
