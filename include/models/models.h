a3b9684306196443e6f51cc40868bb025328afcd
kamisaberi <kamisaberi@gmail.com>
2025-05-13 00:02:57 +0330

    header files fixed

--- include/models/models.h
+++ include/models/models.h
@@ -2,3 +2,10 @@
 
 #include "base.h"
 #include "computer_vision/computer_vision.h"
+#include "generative_models/generative_models.h"
+#include "graph_neural_networks/graph_neural_networks.h"
+#include "multimodal/multimodal.h"
+#include "graph_neural_networks/graph_neural_networks.h"
+#include "others/others.h"
+#include "reinforcement_learning/reinforcement_learning.h"
+#include "time_series_and_sequence/time_series_and_sequence.h"
