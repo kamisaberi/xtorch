891d3b90b6c2e1db02df37c394e1a5f4dfb3327d
kamran saberifard <kamisaberi@gmail.com>
2025-05-12 15:37:16 +0330

    files cleaned

--- include/datasets/computer_vision/stereo_matching/stereo_matching.h
+++ include/datasets/computer_vision/stereo_matching/stereo_matching.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "carla_stereo.h"
 #include "cre_stereo.h"
 #include "eth_3d_stereo.h"
 #include "falling_things_stereo.h"
 #include "in_stereo.h"
+#include "kitti_2012_stereo.h"
+#include "kitti_2015_stereo.h"
 #include "middlebury.h"
 #include "scene_flow_stereo.h"
+#include "sintel_stereo.h"
