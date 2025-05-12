72d583e845343e719188a9cf5403ad7e4d05e675
kamran saberifard <kamisaberi@gmail.com>
2025-05-12 15:51:57 +0330

    files cleaned

---
+++ include/datasets/natural_language_processing/language_modeling/language_modeling.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "en_wik9.h"
+#include "penn_treebank.h"
+#include "wiki_text103.h"
+#include "wiki_text_2.h"
---
+++ include/datasets/natural_language_processing/machine_translation/machine_translation.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "iwslt2016.h"
+#include "iwslt2017.h"
+#include "multi30k.h"
+#include "wmt14.h"
