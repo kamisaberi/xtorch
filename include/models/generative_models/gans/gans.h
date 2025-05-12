65e7956677d6c88c4529c84038376de08f0cd789
kamisaberi <kamisaberi@gmail.com>
2025-05-13 01:33:54 +0330

    header files fixed

--- include/models/generative_models/gans/gans.h
+++ include/models/generative_models/gans/gans.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "big_gan.h"
+#include "cycle_gan.h"
+#include "dcgan.h"
+#include "gan.h"
+#include "pix2pix.h"
+#include "pro_gan.h"
+#include "star_gan.h"
+#include "style_gan.h"
+#include "wgan.h"
+#include "wgan_gp.h"
