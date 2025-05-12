dd3c09956989d3e107710bd9a1174eae30a13484
kamran saberifard <kamisaberi@gmail.com>
2025-05-12 15:57:56 +0330

    files cleaned

--- include/datasets/natural_language_processing/natural_language_processing.h
+++ include/datasets/natural_language_processing/natural_language_processing.h
@@ -1,1 +1,12 @@
 #pragma once
+
+
+#include "dialogue_generation/dialogue_generation.h"
+#include "language_modeling/language_modeling.h"
+#include "machine_translation/machine_translation.h"
+#include "math_word_problems/math_word_problems.h"
+#include "question_answering/question_answering.h"
+#include "sequence_tagging/sequence_tagging.h"
+#include "text_classification/text_classification.h"
+#include "text_summarization/text_summarization.h"
+#include "unsupervised_learning/unsupervised_learning.h"
