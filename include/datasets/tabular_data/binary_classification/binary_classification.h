291dcfb93dc35a1a54e538db3127b6ff079ddf6b
kamran saberifard <kamisaberi@gmail.com>
2025-05-12 16:47:01 +0330

    files cleaned

---
+++ include/datasets/tabular_data/binary_classification/binary_classification.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "adult_census_income.h"
+#include "banknote_authentication.h"
+#include "breast_cancer_wisconsin.h"
+#include "credit_approval.h"
+#include "german_credit.h"
+#include "german_credit.h"
+#include "habermans_survival.h"
+#include "ionosphere.h"
+#include "mushroom_dataset.h"
+#include "pima_indians_diabetes.h"
+#include "sonar_mines_vs_rocks.h"
+#include "statlog_heart.h"
+#include "titanic_dataset.h"
