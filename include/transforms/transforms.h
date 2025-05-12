4da69edb44212b306e54718bc31193d694428a82
kamisaberi <kamisaberi@gmail.com>
2025-05-13 02:47:41 +0330

    header files fixed

--- include/transforms/transforms.h
+++ include/transforms/transforms.h
@@ -1,4 +1,15 @@
 #pragma once
 
 
+#include  "appliers/appliers.h"
+#include  "general/general.h"
+#include  "graph/graph.h"
+#include "image/image.h"
+#include "signal/signal.h"
+#include "target/target.h"
+#include "text/text.h"
+#include "video/video.h"
+#include "weather/weather.h"
+
+
 
