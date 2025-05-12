ec2acf31de538571db5dbe80778386a5bca579aa
kamisaberi <kamisaberi@gmail.com>
2025-05-13 02:52:33 +0330

    header files fixed

--- include/transforms/appliers/appliers.h
+++ include/transforms/appliers/appliers.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "compose.h"
+#include "one_of.h"
+#include "palindrome.h"
+#include "random_apply.h"
+#include "repeat.h"
+#include "replay_compose.h"
+#include "some_of.h"
+#include "sometimes.h"
