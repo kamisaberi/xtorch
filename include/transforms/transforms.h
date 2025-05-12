bbcd41052ad2e2f4b8506e5a20e7d8f1fc7090a4
kamisaberi <kamisaberi@gmail.com>
2025-05-13 02:38:54 +0330

    header files fixed

--- include/transforms/transforms.h
+++ include/transforms/transforms.h
@@ -1,18 +1,4 @@
 #pragma once
 
-#include "appliers/compose.h"
-#include "convert.h"
-#include "crop.h"
-#include "image/cutout.h"
-#include "image/random_flip.h"
-#include "gaussian.h"
-#include "grayscale.h"
-#include "image/color_jitter.h"
-#include "general/lambda.h"
-#include "general/normalize.h"
-#include "image/pad.h"
-#include "image/resize.h"
-#include "image/rotation.h"
-
 
 
