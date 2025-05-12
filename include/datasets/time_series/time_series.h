7a6f7491b2a460c4c9c0648e01e39207ab06a2e6
kamran saberifard <kamisaberi@gmail.com>
2025-05-12 17:47:43 +0330

    files cleaned

--- include/datasets/time_series/time_series.h
+++ include/datasets/time_series/time_series.h
@@ -1,1 +1,5 @@
 #pragma once
+
+#include "anomaly_detection/anomaly_detection.h"
+#include "time_series_classification/time_series_classification.h"
+#include "time_series_forecasting/time_series_forecasting.h"
---
+++ include/datasets/time_series/time_series_forecasting/time_series_forecasting.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "electricity_load_diagrams.h"
+#include "m4_competition.h"
