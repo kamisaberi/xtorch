94d601c15b3bbb1460af2119870cb8b40e5d9333
kamisaberi <kamisaberi@gmail.com>
2025-05-13 02:36:05 +0330

    header files fixed

--- src/transforms/appliers/one_of.cpp
+++ src/transforms/appliers/one_of.cpp
@@ -1,18 +1,18 @@
 #include <utility>
 
-#include "../../include/transforms/some_of.h"
+#include "../../../include/transforms/one_of.h"
 
 namespace xt::transforms
 {
     using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
 
-    SomeOf::SomeOf() = default;
+    OneOf::OneOf() = default;
 
-    SomeOf::SomeOf(std::vector<TransformFunc> transforms): xt::Module(), transforms(std::move(transforms))
+    OneOf::OneOf(std::vector<TransformFunc> transforms): xt::Module(), transforms(std::move(transforms))
     {
     }
 
-    torch::Tensor SomeOf::operator()(torch::Tensor input) const
+    torch::Tensor OneOf::operator()(torch::Tensor input) const
     {
         int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
         input = transforms[index](std::move(input));
--- src/transforms/appliers/sometimes.cpp
+++ src/transforms/appliers/sometimes.cpp
@@ -1,18 +1,18 @@
 #include <utility>
 
-#include "../../include/transforms/one_of.h"
+#include "../../../include/transforms/sometimes.h"
 
 namespace xt::transforms
 {
     using TransformFunc = std::function<torch::Tensor(torch::Tensor)>;
 
-    OneOf::OneOf() = default;
+    Sometimes::Sometimes() = default;
 
-    OneOf::OneOf(std::vector<TransformFunc> transforms): xt::Module(), transforms(std::move(transforms))
+    Sometimes::Sometimes(std::vector<TransformFunc> transforms): xt::Module(), transforms(std::move(transforms))
     {
     }
 
-    torch::Tensor OneOf::operator()(torch::Tensor input) const
+    torch::Tensor Sometimes::operator()(torch::Tensor input) const
     {
         int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
         input = transforms[index](std::move(input));
