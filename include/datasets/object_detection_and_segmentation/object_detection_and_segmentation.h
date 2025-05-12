818b3c889aca8e9e0c06bcd81e457eb527ebe385
kamran saberifard <kamisaberi@gmail.com>
2025-05-12 14:16:19 +0330

    files cleaned

--- include/datasets/object_detection_and_segmentation/object_detection_and_segmentation.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "../computer_vision/semantic_segmentation/cityscapes.h"
-#include "../computer_vision/image_captioning/coco_captions.h"
-#include "../computer_vision/semantic_segmentation/oxfordIII_t_pet.h"
-#include "../computer_vision/semantic_segmentation/sb_dataset.h"
