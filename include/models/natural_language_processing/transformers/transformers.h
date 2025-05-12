c8518d220116c3841684f6a570f2c2df11bc49c5
kamisaberi <kamisaberi@gmail.com>
2025-05-13 01:57:20 +0330

    header files fixed

--- include/models/natural_language_processing/transformers/transformers.h
+++ include/models/natural_language_processing/transformers/transformers.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "albert.h"
+#include "bart.h"
+#include "bert.h"
+#include "big_bird.h"
+#include "deepseek.h"
+#include "distil_bert.h"
+#include "electra.h"
+#include "gpt.h"
+#include "grok.h"
+#include "llama.h"
+#include "long_former.h"
+#include "mistral.h"
+#include "reformer.h"
+#include "roberta.h"
+#include "t5.h"
+#include "xlnet.h"
