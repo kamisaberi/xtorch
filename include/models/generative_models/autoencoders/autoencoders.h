8f981e8af2aaf5f3ced8033c9f31fd505d422b98
kamisaberi <kamisaberi@gmail.com>
2025-05-13 01:26:40 +0330

    header files fixed

--- include/models/generative_models/autoencoders/autoencoders.h
+++ include/models/generative_models/autoencoders/autoencoders.h
@@ -1,1 +1,8 @@
 #pragma once
+
+
+#include "autoencoder.h"
+#include "contractive_autoencoder.h"
+#include "denoising_autoencoder.h"
+#include "sparse_autoencoder.h"
+#include "variational_autoencoder.h"
