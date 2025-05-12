6c2888271c503aab8288463f25242b93e8e09adb
kamisaberi <kamisaberi@gmail.com>
2025-05-13 01:22:45 +0330

    header files fixed

--- include/models/computer_vision/vision_transformers/vision_transformers.h
+++ include/models/computer_vision/vision_transformers/vision_transformers.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include "beit.h"
+#include "clip_vit.h"
+#include "deit.h"
+#include "mvit.h"
+#include "pvt.h"
+#include "swin_transformer.h"
+#include "t2t_vit.h"
+#include "vit.h"
 
