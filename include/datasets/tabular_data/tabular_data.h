43a4b4737b6d218d6930989af8966dbf273a1f56
kamran saberifard <kamisaberi@gmail.com>
2025-05-12 17:44:46 +0330

    files cleaned

--- include/datasets/tabular_data/tabular_data.h
+++ include/datasets/tabular_data/tabular_data.h
@@ -1,1 +1,6 @@
 #pragma once
+
+#include "binary_classification/binary_classification.h"
+#include "classification/classification.h"
+#include "regression/regression.h"
+#include "regression_classification/regression_classification.h"
