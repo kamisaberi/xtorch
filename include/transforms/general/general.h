b509a0dbead28ef686d03970f36814168aa301a1
kamisaberi <kamisaberi@gmail.com>
2025-05-13 02:55:21 +0330

    header files fixed

--- include/transforms/general/general.h
+++ include/transforms/general/general.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "convert_dtype.h"
+#include "lambda.h"
+#include "normalize.h"
+#include "oversampling.h"
+#include "pca.h"
+#include "random_oversampling.h"
+#include "random_undersampling.h"
+#include "to_tensor.h"
+#include "undersampling.h"
