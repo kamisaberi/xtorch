e6d49cfd9cb37ea9a5dd7c2eb987eba4957bc61c
kamisaberi <kamisaberi@gmail.com>
2025-05-14 03:19:51 +0330

    Imagenette dataset bugs fixed

--- src/datasets/computer_vision/image_classification/lfw_people.cpp
+++ src/datasets/computer_vision/image_classification/lfw_people.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/face_recognition/lfw.h"
+#include "../../../../include/datasets/computer_vision/image_classification/lfw_people.h"
 
 namespace xt::data::datasets {
 
