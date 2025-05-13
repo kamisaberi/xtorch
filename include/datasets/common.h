1f9e37796a597b37a7c62b253b01b566fe8ce9ba
kamisaberi <kamisaberi@gmail.com>
2025-05-14 01:01:09 +0330

    header files fixed

---
+++ include/datasets/common.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <fstream>
+#include <filesystem>
+#include <vector>
+#include <tuple>
+#include <map>
+#include <torch/torch.h>
+#include "../utils/downloader.h"
+#include "../utils/extract.h"
+#include "../utils/md5.h"
+#include "../exceptions/implementation.h"
+#include "../definitions/transforms.h"
+#include "../types/arguments.h"
+#include "../types/enums.h"
+#include <opencv2/opencv.hpp>
+#include "../media/opencv/images.h"
+#include "../transforms/transforms.h"
