ece3281c0e04550a3945078e0ed304f4171f38ed
kamisaberi <kamisaberi@gmail.com>
2025-05-14 03:14:25 +0330

    Caltech256 dataset bugs fixed

--- src/datasets/computer_vision/image_classification/caltech256.cpp
+++ src/datasets/computer_vision/image_classification/caltech256.cpp
@@ -5,26 +5,6 @@ namespace xt::data::datasets {
 
     // ---------------------- Caltech101 ---------------------- //
 
-    Caltech101::Caltech101(const std::string &root): Caltech101::Caltech101(root, DataMode::TRAIN, false) {
-    }
-
-    Caltech101::Caltech101(const std::string &root, DataMode mode): Caltech101::Caltech101(root, mode, false) {
-    }
-
-    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
-    }
-
-
-    Caltech101::Caltech101(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Caltech101: Caltech101 not implemented");
-    }
-
-
-
-    // ---------------------- Caltech256 ---------------------- //
-
     Caltech256::Caltech256(const std::string &root): Caltech256::Caltech256(root, DataMode::TRAIN, false) {
     }
 
@@ -32,7 +12,7 @@ namespace xt::data::datasets {
     }
 
     Caltech256::Caltech256(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Caltech256: Caltech256 not implemented");
+        throw std::runtime_error("Caltech101: Caltech101 not implemented");
     }
 
 
@@ -40,6 +20,9 @@ namespace xt::data::datasets {
                            TransformType transforms) : BaseDataset(root, mode, download, transforms) {
         throw std::runtime_error("Caltech101: Caltech101 not implemented");
     }
+
+
+
 
 
 }
