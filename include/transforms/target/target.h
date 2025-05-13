efd12a171c728c169af1f2fce9ba4eb551c3d05e
kamisaberi <kamisaberi@gmail.com>
2025-05-14 00:22:27 +0330

    header files fixed

--- include/transforms/target/target.h
+++ include/transforms/target/target.h
@@ -1,1 +1,14 @@
 #pragma once
+
+
+#include "binarizer.h"
+#include "binary_encoder.h"
+#include "class_weighter.h"
+#include "clipper.h"
+#include "discretizer.h"
+#include ""
+#include ""
+#include ""
+#include ""
+#include ""
+#include ""
