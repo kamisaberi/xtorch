cfa9161a794b33c64a3c6215112680840da3e0f1
kamisaberi <kamisaberi@gmail.com>
2025-05-14 03:23:47 +0330

    LFWPeople dataset bugs fixed

--- include/datasets/computer_vision/image_classification/lfw_people.h
+++ include/datasets/computer_vision/image_classification/lfw_people.h
@@ -1,24 +1,46 @@
 #pragma once
-#include "../base/base.h"
-#include "../../headers/datasets.h"
+#include "datasets/base/base.h"
+// #include "../../headers/datasets.h"
 
 
 using namespace std;
 namespace fs = std::filesystem;
 
-namespace xt::data::datasets {
-    class LFW : public BaseDataset {
-    public :
-        explicit LFW(const std::string &root);
-        LFW(const std::string &root, DataMode mode);
-        LFW(const std::string &root, DataMode mode , bool download);
-        LFW(const std::string &root, DataMode mode , bool download, TransformType transforms);
+namespace xt::data::datasets
+{
+    class LFWPeople : public BaseDataset
+    {
+        /*
+        """`LFW <http://vis-www.cs.umass.edu/lfw/>`_ Dataset.
 
+        Args:
+        root (str or ``pathlib.Path``): Root directory of dataset where directory
+            ``lfw-py`` exists or will be saved to if download is set to True.
+        split (string, optional): The image split to use. Can be one of ``train``, ``test``,
+            ``10fold`` (default).
+        image_set (str, optional): Type of image funneling to use, ``original``, ``funneled`` or
+            ``deepfunneled``. Defaults to ``funneled``.
+        transform (callable, optional): A function/transform that  takes in a PIL image
+            and returns a transformed version. E.g, ``transforms.RandomRotation``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+
+        """
+        */
+
+    public :
+        explicit LFWPeople(const std::string& root);
+        LFWPeople(const std::string& root, DataMode mode);
+        LFWPeople(const std::string& root, DataMode mode, bool download);
+        LFWPeople(const std::string& root, DataMode mode, bool download, TransformType transforms);
 
     private :
         fs::path dataset_folder_name = fs::path("lfw-py");
         fs::path url = fs::path("http://vis-www.cs.umass.edu/lfw/");
-        std::map<std::string, std::tuple<std::string, fs::path, std::string> > file_dict = {
+        std::map<std::string, std::tuple<std::string, fs::path, std::string>> file_dict = {
             {"original", {"lfw", fs::path("lfw.tgz"), "a17d05bd522c52d84eca14327a23d494"}},
             {"funneled", {"lfw_funneled", fs::path("lfw-funneled.tgz"), "1b42dfed7d15c9b2dd63d5e5840c86ad"}},
             {
@@ -49,74 +71,4 @@ namespace xt::data::datasets {
         void check_resources();
     };
 
-    class LFWPeople : public LFW {
-        /*
-        """`LFW <http://vis-www.cs.umass.edu/lfw/>`_ Dataset.
-
-    Args:
-        root (str or ``pathlib.Path``): Root directory of dataset where directory
-            ``lfw-py`` exists or will be saved to if download is set to True.
-        split (string, optional): The image split to use. Can be one of ``train``, ``test``,
-            ``10fold`` (default).
-        image_set (str, optional): Type of image funneling to use, ``original``, ``funneled`` or
-            ``deepfunneled``. Defaults to ``funneled``.
-        transform (callable, optional): A function/transform that  takes in a PIL image
-            and returns a transformed version. E.g, ``transforms.RandomRotation``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        download (bool, optional): If true, downloads the dataset from the internet and
-            puts it in root directory. If dataset is already downloaded, it is not
-            downloaded again.
-
-    """
-
-         */
-    public :
-        explicit LFWPeople(const std::string &root);
-        LFWPeople(const std::string &root, DataMode mode);
-        LFWPeople(const std::string &root, DataMode mode , bool download);
-        LFWPeople(const std::string &root, DataMode mode , bool download, TransformType transforms);
-
-
-    private :
-        void load_data();
-
-        void check_resources();
-    };
-
-    class LFWPairs : public LFW {
-        /*
-        """`LFW <http://vis-www.cs.umass.edu/lfw/>`_ Dataset.
-
-    Args:
-        root (str or ``pathlib.Path``): Root directory of dataset where directory
-            ``lfw-py`` exists or will be saved to if download is set to True.
-        split (string, optional): The image split to use. Can be one of ``train``, ``test``,
-            ``10fold``. Defaults to ``10fold``.
-        image_set (str, optional): Type of image funneling to use, ``original``, ``funneled`` or
-            ``deepfunneled``. Defaults to ``funneled``.
-        transform (callable, optional): A function/transform that takes in a PIL image
-            and returns a transformed version. E.g, ``transforms.RandomRotation``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        download (bool, optional): If true, downloads the dataset from the internet and
-            puts it in root directory. If dataset is already downloaded, it is not
-            downloaded again.
-
-    """
-
-
-         */
-    public :
-        explicit  LFWPairs(const std::string &root);
-        LFWPairs(const std::string &root, DataMode mode);
-        LFWPairs(const std::string &root, DataMode mode , bool download);
-        LFWPairs(const std::string &root, DataMode mode , bool download, TransformType transforms);
-
-
-    private :
-        void load_data();
-
-        void check_resources();
-    };
 }
