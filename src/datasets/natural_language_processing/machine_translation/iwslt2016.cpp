c1dfb9a39fe32bbd6b520af30447884c8921e0e8
kamisaberi <kamisaberi@gmail.com>
2025-05-14 22:12:39 +0330

    datasets includes path changed to  absolute

--- src/datasets/natural_language_processing/machine_translation/iwslt2016.cpp
+++ src/datasets/natural_language_processing/machine_translation/iwslt2016.cpp
@@ -1,19 +1,19 @@
-#include "../../../../include/datasets/natural_language_processing/machine_translation/iwslt2017.h"
+#include "datasets/natural_language_processing/machine_translation/iwslt2016.h"
 
 namespace xt::data::datasets {
 
-    IWSLT::IWSLT(const std::string &root): IWSLT::IWSLT(root, DataMode::TRAIN, false) {
+    IWSLT20169::IWSLT20169(const std::string &root): IWSLT20169::IWSLT20169(root, DataMode::TRAIN, false) {
     }
 
-    IWSLT::IWSLT(const std::string &root, DataMode mode): IWSLT::IWSLT(root, mode, false) {
+    IWSLT20169::IWSLT20169(const std::string &root, DataMode mode): IWSLT20169::IWSLT20169(root, mode, false) {
     }
 
-    IWSLT::IWSLT(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+    IWSLT20169::IWSLT20169(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
         throw std::runtime_error("IWSLT: IWSLT not implemented");
     }
 
 
-    IWSLT::IWSLT(const std::string &root, DataMode mode, bool download,
+    IWSLT20169::IWSLT20169(const std::string &root, DataMode mode, bool download,
                            TransformType transforms) : BaseDataset(root, mode, download, transforms) {
         throw std::runtime_error("IWSLT: IWSLT not implemented");
     }
