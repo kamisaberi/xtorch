99e920f8a0c642234cd1ab68eddb313671a46406
kamisaberi <kamisaberi@gmail.com>
2025-05-14 22:05:01 +0330

    datasets includes path changed to  absolute

--- src/datasets/natural_language_processing/language_modeling/penn_treebank.cpp
+++ src/datasets/natural_language_processing/language_modeling/penn_treebank.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/datasets/language_modeling/penn_treebank.h"
+#include "datasets/natural_language_processing/language_modeling/penn_treebank.h"
 
 namespace xt::data::datasets {
 
