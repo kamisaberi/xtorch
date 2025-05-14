486de0e4ae347b6784f94d7727e70cda9e770576
kamisaberi <kamisaberi@gmail.com>
2025-05-14 21:59:20 +0330

    datasets includes path changed to  absolute

--- src/datasets/computer_vision/stereo_matching/carla_stereo.cpp
+++ src/datasets/computer_vision/stereo_matching/carla_stereo.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/datasets/computer_vision/stereo_matching/cre_stereo.h"
+#include "datasets/computer_vision/stereo_matching/carla_stereo.h"
 
 namespace xt::data::datasets {
     // ---------------------- CarlaStereo ---------------------- //
--- src/datasets/computer_vision/stereo_matching/eth_3d_stereo.cpp
+++ src/datasets/computer_vision/stereo_matching/eth_3d_stereo.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/datasets/computer_vision/stereo_matching/eth_3d_stereo.h"
+#include "datasets/computer_vision/stereo_matching/eth_3d_stereo.h"
 
 namespace xt::data::datasets {
 
