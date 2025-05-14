9a2b8b7beca4039d06044144c9b70cc11363e47d
kamisaberi <kamisaberi@gmail.com>
2025-05-14 21:49:21 +0330

    datasets includes path changed to  absolute

--- src/datasets/computer_vision/image_classification/celeba.cpp
+++ src/datasets/computer_vision/image_classification/celeba.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/datasets/computer_vision/image_classification/celeba.h"
+#include "datasets/computer_vision/image_classification/celeba.h"
 
 namespace xt::data::datasets {
 
--- src/datasets/computer_vision/image_classification/country.cpp
+++ src/datasets/computer_vision/image_classification/country.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/datasets/computer_vision/image_classification/country.h"
+#include "datasets/computer_vision/image_classification/country.h"
 
 namespace xt::data::datasets {
 
