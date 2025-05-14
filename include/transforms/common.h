7b4dfe7f0678f43e93206301f762f81e23633e18
kamisaberi <kamisaberi@gmail.com>
2025-05-15 01:46:08 +0330

    header files fixed

---
+++ include/transforms/common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <torch/torch.h>
+#include <torch/data/datasets/mnist.h>
+#include <iostream>
+#include <vector>
+#include <torch/data/transforms/base.h>
+#include <functional>
+#include <stdexcept>
+#include <cmath>
+#include <opencv2/opencv.hpp>
+#include <random>
+#include "base/base.h"
