64f10ab2a202f5240ba2a7639ab1a13f10eabbdc
kamisaberi <kamisaberi@gmail.com>
2025-05-14 22:56:34 +0330

    datasets includes path changed to  absolute

--- src/datasets/natural_language_processing/text_classification/imdb.cpp
+++ src/datasets/natural_language_processing/text_classification/imdb.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/datasets/natural_language_processing/text_classification/imdb.h"
+#include "datasets/natural_language_processing/text_classification/imdb.h"
 
 namespace xt::data::datasets {
 
