71e7a9c6cc9ff030ca5358a437d9b6e18ac0daca
kamisaberi <kamisaberi@gmail.com>
2025-05-14 21:54:54 +0330

    datasets includes path changed to  absolute

--- src/datasets/computer_vision/image_classification/places.cpp
+++ src/datasets/computer_vision/image_classification/places.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/datasets/computer_vision/image_classification/places.h"
+#include "datasets/computer_vision/image_classification/places.h"
 
 namespace xt::data::datasets {
 
