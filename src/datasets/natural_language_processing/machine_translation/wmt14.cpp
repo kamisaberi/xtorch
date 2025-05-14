b1c871ec19b928da28af2a674c3b121b10cdab4d
kamisaberi <kamisaberi@gmail.com>
2025-05-14 22:15:03 +0330

    datasets includes path changed to  absolute

--- src/datasets/natural_language_processing/machine_translation/wmt14.cpp
+++ src/datasets/natural_language_processing/machine_translation/wmt14.cpp
@@ -1,19 +1,19 @@
-#include "../../../../include/datasets/natural_language_processing/machine_translation/wmt14.h"
+#include "datasets/natural_language_processing/machine_translation/wmt14.h"
 
 namespace xt::data::datasets {
 
-    WMT::WMT(const std::string &root): WMT::WMT(root, DataMode::TRAIN, false) {
+    WMT14::WMT14(const std::string &root): WMT14::WMT14(root, DataMode::TRAIN, false) {
     }
 
-    WMT::WMT(const std::string &root, DataMode mode): WMT::WMT(root, mode, false) {
+    WMT14::WMT14(const std::string &root, DataMode mode): WMT14::WMT14(root, mode, false) {
     }
 
-    WMT::WMT(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+    WMT14::WMT14(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
         throw std::runtime_error("WMT: WMT not implemented");
     }
 
 
-    WMT::WMT(const std::string &root, DataMode mode, bool download,
+    WMT14::WMT14(const std::string &root, DataMode mode, bool download,
                            TransformType transforms) : BaseDataset(root, mode, download, transforms) {
         throw std::runtime_error("WMT: WMT not implemented");
     }
