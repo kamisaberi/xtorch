5c913f5fe003da997e0ba4e850f8f2ed9137e784
kamisaberi <kamisaberi@gmail.com>
2025-05-14 22:01:25 +0330

    datasets includes path changed to  absolute

--- src/datasets/computer_vision/video_classification/hmdb.cpp
+++ src/datasets/computer_vision/video_classification/hmdb.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/datasets/computer_vision/video_classification/hmdb.h"
+#include "datasets/computer_vision/video_classification/hmdb.h"
 
 namespace xt::data::datasets {
 
