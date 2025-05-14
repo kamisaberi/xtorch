15e1afdecd51cc15b99a764f0bbb900156bd2fec
kamisaberi <kamisaberi@gmail.com>
2025-05-14 23:02:55 +0330

    datasets includes path changed to  absolute

--- include/datasets/natural_language_processing/text_classification/yelp_review_full.h
+++ include/datasets/natural_language_processing/text_classification/yelp_review_full.h
@@ -1,18 +1,19 @@
 #pragma once
-#include "../base/base.h"
-#include "../../headers/datasets.h"
+#include "datasets/base/base.h"
 
 
-namespace xt::data::datasets {
-    class YelpReview : BaseDataset {
-        public :
-            explicit YelpReview(const std::string &root);
-        YelpReview(const std::string &root, DataMode mode);
-        YelpReview(const std::string &root, DataMode mode , bool download);
-        YelpReview(const std::string &root, DataMode mode , bool download, TransformType transforms);
+namespace xt::data::datasets
+{
+    class YelpReviewFull : BaseDataset
+    {
+    public :
+        explicit YelpReviewFull(const std::string& root);
+        YelpReviewFull(const std::string& root, DataMode mode);
+        YelpReviewFull(const std::string& root, DataMode mode, bool download);
+        YelpReviewFull(const std::string& root, DataMode mode, bool download, TransformType transforms);
 
-        private :
-            void load_data();
+    private :
+        void load_data();
 
         void check_resources();
     };
