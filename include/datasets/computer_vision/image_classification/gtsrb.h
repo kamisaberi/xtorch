616a8cf88f8a240cf4b2f7a9f1c84c65961fca3b
kamisaberi <kamisaberi@gmail.com>
2025-05-14 23:19:57 +0330

    datasets includes path changed to  absolute

--- include/datasets/computer_vision/image_classification/gtsrb.h
+++ include/datasets/computer_vision/image_classification/gtsrb.h
@@ -1,7 +1,6 @@
 #pragma once
-#include "../../base/base.h"
-#include "../../../headers/datasets.h"
 
+#include "datasets/base/base.h"
 
 namespace xt::data::datasets {
     class GTSRB : BaseDataset {
