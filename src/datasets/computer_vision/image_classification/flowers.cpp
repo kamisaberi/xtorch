9514166206547045100093efd6d3349c8eb49c40
kamisaberi <kamisaberi@gmail.com>
2025-05-14 21:50:59 +0330

    datasets includes path changed to  absolute

--- src/datasets/computer_vision/image_classification/flowers.cpp
+++ src/datasets/computer_vision/image_classification/flowers.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/datasets/computer_vision/image_classification/flowers.h"
+#include "datasets/computer_vision/image_classification/flowers.h"
 
 namespace xt::data::datasets {
 
