1a0ca1709c9f0d607bcfe2f419ac3bba9b61c92c
kamisaberi <kamisaberi@gmail.com>
2025-05-14 22:10:46 +0330

    datasets includes path changed to  absolute

--- src/datasets/natural_language_processing/language_modeling/wiki_text_2.cpp
+++ src/datasets/natural_language_processing/language_modeling/wiki_text_2.cpp
@@ -1,19 +1,19 @@
-#include "../../../include/datasets/language_modeling/wiki_text.h"
+#include "datasets/natural_language_processing/language_modeling/wiki_text_2.h"
 
 namespace xt::data::datasets {
 
-    WikiText::WikiText(const std::string &root): WikiText::WikiText(root, DataMode::TRAIN, false) {
+    WikiTextV2::WikiTextV2(const std::string &root): WikiTextV2::WikiTextV2(root, DataMode::TRAIN, false) {
     }
 
-    WikiText::WikiText(const std::string &root, DataMode mode): WikiText::WikiText(root, mode, false) {
+    WikiTextV2::WikiTextV2(const std::string &root, DataMode mode): WikiTextV2::WikiTextV2(root, mode, false) {
     }
 
-    WikiText::WikiText(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
+    WikiTextV2::WikiTextV2(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
         throw std::runtime_error("WikiText: WikiText not implemented");
     }
 
 
-    WikiText::WikiText(const std::string &root, DataMode mode, bool download,
+    WikiTextV2::WikiTextV2(const std::string &root, DataMode mode, bool download,
                            TransformType transforms) : BaseDataset(root, mode, download, transforms) {
         throw std::runtime_error("WikiText: WikiText not implemented");
     }
