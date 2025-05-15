866183d79b03dba2575c7cfee834011983112030
kamisaberi <kamisaberi@gmail.com>
2025-05-16 01:24:33 +0330

    si=ome bugs fixed

--- include/datasets/audio_processing/binary_speech_classification/yes_no.h
+++ include/datasets/audio_processing/binary_speech_classification/yes_no.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "datasets/base/base.h"
-#include "datasets/common.h"
+// #include "datasets/common.h"
 
 
-namespace xt::data::datasets {
-    class YesNo : BaseDataset {
-        public :
-            explicit YesNo(const std::string &root);
-        YesNo(const std::string &root, DataMode mode);
-        YesNo(const std::string &root, DataMode mode , bool download);
-        YesNo(const std::string &root, DataMode mode , bool download, TransformType transforms);
+namespace xt::data::datasets
+{
+    class YesNo : public BaseDataset
+    {
+    public :
+        explicit YesNo(const std::string& root);
+        YesNo(const std::string& root, DataMode mode);
+        YesNo(const std::string& root, DataMode mode, bool download);
+        YesNo(const std::string& root, DataMode mode, bool download, TransformType transforms);
 
-        private :
-            void load_data();
+    private :
+        void load_data();
 
         void check_resources();
     };
