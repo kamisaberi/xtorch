e3c2bf57a52debcb78b2a32b5ea63c4253988ea5
kamisaberi <kamisaberi@gmail.com>
2025-05-16 00:53:06 +0330

    si=ome bugs fixed

--- include/datasets/general/tensor_dataset.h
+++ include/datasets/general/tensor_dataset.h
@@ -3,24 +3,19 @@
 
 #include "datasets/base/base.h"
 
-namespace xt::data::datasets {
-
-
-
-    class TensorDataset : public BaseDataset {
+namespace xt::data::datasets
+{
+    class TensorDataset : public BaseDataset
+    {
     public :
-        TensorDataset(const std::string &file_path);
-        TensorDataset(const std::string &file_path,DataMode mode);
-        TensorDataset(const std::string &file_path,DataMode mode , vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
+        TensorDataset(const std::string& file_path);
+        TensorDataset(const std::string& file_path, DataMode mode);
+        TensorDataset(const std::string& file_path, DataMode mode,
+                      vector<std::function<torch::Tensor(torch::Tensor)>> transforms);
 
     private:
         vector<string> labels_name;
         bool load_sub_folders = false;
         void load_data();
     };
-
-
-
-
-
 }
