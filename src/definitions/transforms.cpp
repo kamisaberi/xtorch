6a0cb23e13e1e2ae244cec578b8e6205d481fc06
kamisaberi <kamisaberi@gmail.com>
2025-05-16 18:31:15 +0330

    /headers/transforms.h removed

--- src/definitions/transforms.cpp
+++ src/definitions/transforms.cpp
@@ -1,50 +1,50 @@
 #include "../../include/definitions/transforms.h"
 
 namespace xt::data::transforms {
-    std::function<torch::Tensor(torch::Tensor input)> create_resize_transform(std::vector<int64_t> size) {
-        auto resize_fn = [size](torch::Tensor img) -> torch::Tensor {
-            img = img.unsqueeze(0); // Add batch dimension
-            img = torch::nn::functional::interpolate(
-                img,
-                torch::nn::functional::InterpolateFuncOptions()
-                .size(std::vector<int64_t>({size[0], size[1]}))
-                .mode(torch::kBilinear)
-                .align_corners(false)
-            );
-            return img.squeeze(0); // Remove batch dimension
-        };
-        return resize_fn;
-    }
-
-
-    torch::Tensor resize_tensor(const torch::Tensor &tensor, const std::vector<int64_t> &size) {
-        return torch::nn::functional::interpolate(
-            tensor.unsqueeze(0),
-            torch::nn::functional::InterpolateFuncOptions().size(size).mode(torch::kBilinear).align_corners(false)
-        ).squeeze(0);
-    }
-
-    torch::data::transforms::Lambda<torch::data::Example<> > resize(std::vector<int64_t> size) {
-        return torch::data::transforms::Lambda<torch::data::Example<> >(
-            [size](torch::data::Example<> example) {
-                example.data = resize_tensor(example.data, size);
-                return example;
-            }
-        );
-    }
-
-    torch::data::transforms::Lambda<torch::data::Example<> > normalize(double mean, double stddev) {
-        return torch::data::transforms::Lambda<torch::data::Example<> >(
-            [mean, stddev](torch::data::Example<> example) {
-                example.data = example.data.to(torch::kFloat32).div(255);
-                return example;
-            }
-        );
-    }
-
-
-
-
+    // std::function<torch::Tensor(torch::Tensor input)> create_resize_transform(std::vector<int64_t> size) {
+    //     auto resize_fn = [size](torch::Tensor img) -> torch::Tensor {
+    //         img = img.unsqueeze(0); // Add batch dimension
+    //         img = torch::nn::functional::interpolate(
+    //             img,
+    //             torch::nn::functional::InterpolateFuncOptions()
+    //             .size(std::vector<int64_t>({size[0], size[1]}))
+    //             .mode(torch::kBilinear)
+    //             .align_corners(false)
+    //         );
+    //         return img.squeeze(0); // Remove batch dimension
+    //     };
+    //     return resize_fn;
+    // }
+    //
+    //
+    // torch::Tensor resize_tensor(const torch::Tensor &tensor, const std::vector<int64_t> &size) {
+    //     return torch::nn::functional::interpolate(
+    //         tensor.unsqueeze(0),
+    //         torch::nn::functional::InterpolateFuncOptions().size(size).mode(torch::kBilinear).align_corners(false)
+    //     ).squeeze(0);
+    // }
+    //
+    // torch::data::transforms::Lambda<torch::data::Example<> > resize(std::vector<int64_t> size) {
+    //     return torch::data::transforms::Lambda<torch::data::Example<> >(
+    //         [size](torch::data::Example<> example) {
+    //             example.data = resize_tensor(example.data, size);
+    //             return example;
+    //         }
+    //     );
+    // }
+    //
+    // torch::data::transforms::Lambda<torch::data::Example<> > normalize(double mean, double stddev) {
+    //     return torch::data::transforms::Lambda<torch::data::Example<> >(
+    //         [mean, stddev](torch::data::Example<> example) {
+    //             example.data = example.data.to(torch::kFloat32).div(255);
+    //             return example;
+    //         }
+    //     );
+    // }
+    //
+    //
+    //
+    //
 
 
 
