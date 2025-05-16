f4299dac107e9cf994810261f5d092bbe6893ad8
kamisaberi <kamisaberi@gmail.com>
2025-05-17 02:36:57 +0330

    clonable.h module created

---
+++ include/base/cloneable.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <torch/torch.h>
+#include <iostream>
+#include <torch/script.h>
+#include <string>
+#include <vector>
+
+
+namespace xt
+{
+    class Module: public torch::nn::Module {
+    public:
+        Module();
+        virtual  torch::Tensor forward(torch::Tensor input) const = 0;
+        torch::Tensor operator()(torch::Tensor input) const ;
+    };
+}
