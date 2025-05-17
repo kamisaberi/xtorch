e77eb4ba13f25e146b48e3988b3d59eca91ffe12
kamran saberifard <kamisaberi@gmail.com>
2025-05-17 14:21:01 +0330

    RandomOverSampling general tramsform created

--- src/transforms/general/random_oversampling.cpp
+++ src/transforms/general/random_oversampling.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/10/25.
-//
+#include "transforms/general/random_oversampling.h"
+
+namespace xt::transforms::general {
+
+    RandomOverSampling::RandomOverSampling() = default;
+
+
+    RandomOverSampling::RandomOverSampling(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    torch::Tensor Lambda::forward(torch::Tensor input) {
+        return transform(input);
+    }
+
+}
