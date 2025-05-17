e98568491ac6729aa22b8b440467ade0cfa09066
kamran saberifard <kamisaberi@gmail.com>
2025-05-17 14:22:03 +0330

    RandomUnderSampling general tramsform created

--- include/transforms/general/random_undersampling.h
+++ include/transforms/general/random_undersampling.h
@@ -1,2 +1,17 @@
 #pragma once
 #include "transforms/common.h"
+
+namespace xt::transforms::general {
+
+    struct RandomUnderSampling final : xt::Module {
+    public:
+        RandomUnderSampling();
+        RandomUnderSampling(std::function<torch::Tensor(torch::Tensor)> transform);
+        torch::Tensor forward(torch::Tensor input) const override;
+
+    private:
+        std::function<torch::Tensor(torch::Tensor)> transform;
+    };
+
+
+}
