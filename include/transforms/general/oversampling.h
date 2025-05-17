4e7723f9773dfe8502e673471f496dcf89cf3027
kamran saberifard <kamisaberi@gmail.com>
2025-05-17 14:17:28 +0330

    oversampling general tramsform created

--- include/transforms/general/oversampling.h
+++ include/transforms/general/oversampling.h
@@ -1,2 +1,19 @@
 #pragma once
 #include "transforms/common.h"
+
+
+
+namespace xt::transforms::general {
+
+    struct OverSampling final : xt::Module {
+    public:
+        OverSampling();
+        OverSampling(std::function<torch::Tensor(torch::Tensor)> transform);
+        torch::Tensor forward(torch::Tensor input) const override;
+
+    private:
+        std::function<torch::Tensor(torch::Tensor)> transform;
+    };
+
+
+}
