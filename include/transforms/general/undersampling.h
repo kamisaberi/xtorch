5b6a77011cbff06a1a0259dd4df2a5df567c80fb
kamran saberifard <kamisaberi@gmail.com>
2025-05-17 14:37:08 +0330

    UnderSampling general tramsform created

--- include/transforms/general/undersampling.h
+++ include/transforms/general/undersampling.h
@@ -1,3 +1,19 @@
 #pragma once
-
 #include "transforms/common.h"
+
+
+
+namespace xt::transforms::general {
+
+    struct UnderSampling final : xt::Module {
+    public:
+        UnderSampling();
+        UnderSampling(std::function<torch::Tensor(torch::Tensor)> transform);
+        torch::Tensor forward(torch::Tensor input) const override;
+
+    private:
+        std::function<torch::Tensor(torch::Tensor)> transform;
+    };
+
+
+}
--- src/transforms/general/undersampling.cpp
+++ src/transforms/general/undersampling.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/10/25.
-//
+#include "transforms/general/undersampling.h"
+
+namespace xt::transforms::general {
+
+    UnderSampling::UnderSampling() = default;
+
+
+    UnderSampling::UnderSampling(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    torch::Tensor Lambda::forward(torch::Tensor input) {
+        return transform(input);
+    }
+
+}
