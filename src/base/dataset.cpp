ff34d1954327ca3407857f7fc7a53255522b657a
kamran saberifard <kamisaberi@gmail.com>
2025-05-17 15:37:40 +0330

    base/Dataset class is under reconstruction

--- src/base/dataset.cpp
+++ src/base/dataset.cpp
@@ -1,33 +1,26 @@
 #include "base/dataset.h"
 
 namespace xt::datasets {
-    Dataset::Dataset(const std::string &root): Dataset::Dataset(root, DataMode::TRAIN, false) {
-    }
 
-    Dataset::Dataset(const std::string &root, DataMode mode): Dataset::Dataset(root, mode, false) {
-    }
+    Dataset::Dataset();
 
-    Dataset::Dataset(const std::string &root, DataMode mode, bool download) : root(root), mode(mode),
-        download(download) {
-    }
+    Dataset::Dataset(DataMode mode);
 
-    Dataset::Dataset(const std::string &root, DataMode mode, bool download,
-                             xt::Module transformer) : Dataset::Dataset(root, mode, download) {
-        this->transformer = transformer;
-    }
+    Dataset::Dataset(DataMode mode, xt::Module transformer);
 
-
+    Dataset::Dataset(DataMode mode, xt::Module transformer, xt::Module target_transformer)
+            : mode(mode),
+              transformer(transformer),
+              target_transformer(target_transformer) {}
 
     // BaseDataset::~BaseDataset() {}
-
-
 
     torch::data::Example<> Dataset::get(size_t index) {
         return {data[index], torch::tensor(labels[index])};
     }
 
     // Override `size` method to return the number of samples
-    torch::optional<size_t> Dataset::size() const {
+    torch::optional <size_t> Dataset::size() const {
         return data.size();
     }
 }
