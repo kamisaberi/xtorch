d5ec12b4218afd3a0ff01db4356908c1763b9eb3
kamran saberifard <kamisaberi@gmail.com>
2025-05-17 14:17:28 +0330

    oversampling general tramsform created

--- src/transforms/general/oversampling.cpp
+++ src/transforms/general/oversampling.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/10/25.
-//
+#include "transforms/general/oversampling.h"
+
+namespace xt::transforms::general {
+
+    OverSampling::OverSampling() = default;
+
+
+    OverSampling::OverSampling(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    torch::Tensor Lambda::forward(torch::Tensor input) {
+        return transform(input);
+    }
+
+}
