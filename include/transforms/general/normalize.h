ee1ae4a58e311ea687c44173d16ac9722105493f
kamran saberifard <kamisaberi@gmail.com>
2025-05-17 14:14:14 +0330

    lambda general tramsform created

--- include/transforms/general/normalize.h
+++ include/transforms/general/normalize.h
@@ -2,13 +2,18 @@
 
 #include "transforms/common.h"
 
-namespace xt::transforms
-{
-    struct Normalize
-    {
+namespace xt::transforms::general {
+
+
+    struct Normalize::xt::Module {
     public:
-        Normalize(std::vector<float> mean, std::vector<float> std);
-        torch::Tensor operator()(const torch::Tensor& tensor) const;
+        Normalize();
+
+//        Normalize(float mean, float std);
+
+        Normalize(std::vector<float>  mean, std::vector<float> std);
+
+        torch::Tensor forward(torch::Tensor input) const override;
 
     private:
         std::vector<float> mean;
