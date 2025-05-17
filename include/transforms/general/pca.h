3f919c8439033689d937a60e83dc6faab4ad8289
kamran saberifard <kamisaberi@gmail.com>
2025-05-17 14:19:07 +0330

    pca general tramsform created

--- include/transforms/general/pca.h
+++ include/transforms/general/pca.h
@@ -1,2 +1,16 @@
 #pragma once
 #include "transforms/common.h"
+namespace xt::transforms::general {
+
+    struct PCA final : xt::Module {
+    public:
+        PCA();
+        PCA(std::function<torch::Tensor(torch::Tensor)> transform);
+        torch::Tensor forward(torch::Tensor input) const override;
+
+    private:
+        std::function<torch::Tensor(torch::Tensor)> transform;
+    };
+
+
+}
--- src/transforms/general/pca.cpp
+++ src/transforms/general/pca.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/10/25.
-//
+#include "transforms/general/pca.h"
+
+namespace xt::transforms::general {
+
+    PCA::PCA() = default;
+
+    PCA::PCA(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    torch::Tensor Lambda::forward(torch::Tensor input) {
+        return transform(input);
+    }
+
+}
