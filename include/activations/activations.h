5bf1e032fa88ee293a232b9ad5cec55f3ca86282
kamisaberi <kamisaberi@gmail.com>
2025-05-19 02:24:24 +0330

    activations/activations.h created

---
+++ include/activations/activations.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "activations/a_m_lines.h"
+#include "activations/aglu.h"
+#include "activations/ahaf.h"
+#include "activations/andhra.h"
+#include "activations/aria.h"
+#include "activations/"
+#include "activations/"
+#include "activations/"
+#include "activations/"
+#include "activations/"
