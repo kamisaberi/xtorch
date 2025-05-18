9573517e75d31406c078abcad8c736004ce7e7fb
kamisaberi <kamisaberi@gmail.com>
2025-05-19 02:17:51 +0330

    some bugs fixed

--- include/models/computer_vision/image_classification/lenet5.h
+++ include/models/computer_vision/image_classification/lenet5.h
@@ -5,18 +5,19 @@
 
 using namespace std;
 
-namespace xt::models {
-    struct LeNet5 : xt::Module , torch::nn::Cloneable<LeNet5> {
+namespace xt::models
+{
+    struct LeNet5 : xt::Cloneable<LeNet5>
+    {
     protected:
-        mutable  torch::nn::Sequential layer1 = {nullptr}, layer2 = {nullptr};
-        mutable  torch::nn::Linear fc1 = nullptr, fc2 = nullptr, fc3 = nullptr;
+        mutable torch::nn::Sequential layer1 = {nullptr}, layer2 = {nullptr};
+        mutable torch::nn::Linear fc1 = nullptr, fc2 = nullptr, fc3 = nullptr;
 
     public:
-        LeNet5(int num_classes/* classes */, int in_channels = 1/*  input channels */);
+        explicit LeNet5(int num_classes/* classes */, int in_channels = 1/*  input channels */);
         LeNet5(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::Tensor forward(torch::Tensor x) const override;
         void reset() override;
-
     };
 }
