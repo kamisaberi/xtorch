fb370f6f096dca945de5930bd2489581eee99d01
kamisaberi <kamisaberi@gmail.com>
2025-05-20 03:12:13 +0330

    CBAM model base file created

--- include/models/computer_vision/image_classification/cbam.h
+++ include/models/computer_vision/image_classification/cbam.h
@@ -1,3 +1,20 @@
 #pragma once
 
 #include "models/common.h"
+
+using namespace std;
+
+namespace xt::models
+{
+    struct CBAM : xt::Cloneable<CBAM>
+    {
+    protected:
+
+    public:
+        explicit CBAM(int num_classes/* classes */, int in_channels = 1/*  input channels */);
+        CBAM(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+}
