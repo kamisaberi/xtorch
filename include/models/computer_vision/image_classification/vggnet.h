9255d675930d0149168dcf9b5d6b91f0c946ed77
kamisaberi <kamisaberi@gmail.com>
2025-05-20 02:46:28 +0330

    RVggNet*  model right now is cloneable

--- include/models/computer_vision/image_classification/vggnet.h
+++ include/models/computer_vision/image_classification/vggnet.h
@@ -4,8 +4,10 @@
 
 using namespace std;
 
-namespace xt::models {
-    struct VggNet11 : xt::Module {
+namespace xt::models
+{
+    struct VggNet11 : xt::Cloneable<VggNet11>
+    {
         mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr;
         mutable torch::nn::Sequential layer5 = nullptr;
         mutable torch::nn::Sequential layer6 = nullptr, layer7 = nullptr, layer8 = nullptr, layer9 = nullptr;
@@ -13,13 +15,15 @@ namespace xt::models {
         mutable torch::nn::Sequential layer11 = nullptr, layer12 = nullptr, layer13 = nullptr;
         mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
 
-        VggNet11(int num_classes /* classes */, int in_channels  /* input channels */);
+        VggNet11(int num_classes /* classes */, int in_channels /* input channels */);
         VggNet11(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
     };
 
-    struct VggNet13 : xt::Module {
+    struct VggNet13 : xt::Cloneable<VggNet13>
+    {
         mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr;
         mutable torch::nn::Sequential layer5 = nullptr;
         mutable torch::nn::Sequential layer6 = nullptr, layer7 = nullptr, layer8 = nullptr, layer9 = nullptr;
@@ -27,14 +31,15 @@ namespace xt::models {
         mutable torch::nn::Sequential layer11 = nullptr, layer12 = nullptr, layer13 = nullptr;
         mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
 
-        VggNet13(int num_classes /* classes */, int in_channels  /* input channels */);
+        VggNet13(int num_classes /* classes */, int in_channels /* input channels */);
         VggNet13(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::Tensor forward(torch::Tensor x) const override;
     };
 
 
-    struct VggNet16 : xt::Module {
+    struct VggNet16 : xt::Cloneable<VggNet16>
+    {
         mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr;
         mutable torch::nn::Sequential layer5 = nullptr;
         mutable torch::nn::Sequential layer6 = nullptr, layer7 = nullptr, layer8 = nullptr, layer9 = nullptr;
@@ -42,16 +47,15 @@ namespace xt::models {
         mutable torch::nn::Sequential layer11 = nullptr, layer12 = nullptr, layer13 = nullptr;
         mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
 
-        VggNet16(int num_classes /* classes */, int in_channels  /* input channels */);
+        VggNet16(int num_classes /* classes */, int in_channels /* input channels */);
         VggNet16(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::Tensor forward(torch::Tensor x) const override;
     };
 
 
-
-
-    struct VggNet19 : xt::Module {
+    struct VggNet19 : xt::Cloneable<VggNet19>
+    {
         mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr;
         mutable torch::nn::Sequential layer5 = nullptr;
         mutable torch::nn::Sequential layer6 = nullptr, layer7 = nullptr, layer8 = nullptr, layer9 = nullptr;
@@ -59,12 +63,9 @@ namespace xt::models {
         mutable torch::nn::Sequential layer11 = nullptr, layer12 = nullptr, layer13 = nullptr;
         mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
 
-        VggNet19(int num_classes /* classes */, int in_channels  /* input channels */);
+        VggNet19(int num_classes /* classes */, int in_channels /* input channels */);
         VggNet19(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::Tensor forward(torch::Tensor x) const override;
     };
-
-
-
 }
