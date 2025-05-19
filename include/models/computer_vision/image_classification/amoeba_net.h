a758dc06d245cee7650f7bfca2ec192b99345783
kamisaberi <kamisaberi@gmail.com>
2025-05-20 03:04:12 +0330

    AmoabaNet model base file created

--- include/models/computer_vision/image_classification/amoeba_net.h
+++ include/models/computer_vision/image_classification/amoeba_net.h
@@ -1,3 +1,20 @@
 #pragma once
 
 #include "models/common.h"
+
+using namespace std;
+
+namespace xt::models
+{
+    struct AmoabaNet : xt::Cloneable<AmoabaNet>
+    {
+    protected:
+
+    public:
+        explicit AmoabaNet(int num_classes/* classes */, int in_channels = 1/*  input channels */);
+        AmoabaNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+}
