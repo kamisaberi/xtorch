b3512e35b01271f861ea7d171c4001290b6a9107
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:52:39 +0330

    DeepLabV1 model base file created

--- include/models/computer_vision/image_segmentation/deep_lab.h
+++ include/models/computer_vision/image_segmentation/deep_lab.h
@@ -1,2 +1,21 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+
+    struct DeepLabV1 : xt::Cloneable<DeepLabV1>
+    {
+    private:
+
+    public:
+        DeepLabV1(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DeepLabV1(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+
+
+}
