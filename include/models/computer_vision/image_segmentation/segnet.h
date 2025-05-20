09a31edbee303810e6784f16d415bd993beb4e8e
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:57:12 +0330

    SegNet model base file created

--- include/models/computer_vision/image_segmentation/segnet.h
+++ include/models/computer_vision/image_segmentation/segnet.h
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "models/common.h"
+namespace xt::models
+{
+    struct SegNet : xt::Cloneable<SegNet>
+    {
+    private:
+
+    public:
+        SegNet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        SegNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
