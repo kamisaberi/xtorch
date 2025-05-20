718f82a924a20d5790a3ba652d67809cd8adca40
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:12:54 +0330

    EfficientDet model base file created

--- include/models/computer_vision/object_detection/efficient_det.h
+++ include/models/computer_vision/object_detection/efficient_det.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct EfficientDet : xt::Cloneable<EfficientDet>
+    {
+    private:
+
+    public:
+        EfficientDet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        EfficientDet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
