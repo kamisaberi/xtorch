900adf737c6bd2267ad72c5714a850e349ba99cd
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:28:10 +0330

    WideResNet model base file created

--- src/models/computer_vision/image_classification/resnext.cpp
+++ src/models/computer_vision/image_classification/resnext.cpp
@@ -1,4 +1,4 @@
-#include "../../../../include/models/computer_vision/image_classification/resnext.h"
+#include "models/computer_vision/image_classification/resnext.h"
 
 namespace xt::models {
     namespace {
