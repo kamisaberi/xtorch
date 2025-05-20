6b02fd4a44c5ba01aa7a7dade3e5614399f2f224
kamisaberi <kamisaberi@gmail.com>
2025-05-20 03:47:22 +0330

    GoogLeNet model base file created

--- include/models/computer_vision/image_classification/google_net.h
+++ include/models/computer_vision/image_classification/google_net.h
@@ -1,2 +1,23 @@
 #pragma once
 #include "models/common.h"
+
+
+
+
+namespace xt::models
+{
+    struct GoogLeNet : xt::Cloneable<GoogLeNet>
+    {
+    private:
+
+    public:
+        GoogLeNet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        GoogLeNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+
+}
