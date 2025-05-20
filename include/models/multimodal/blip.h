cfdb7318c55740d4a3a40c91efc79bc0fa212837
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:21:21 +0330

    BLIP model base file created

--- include/models/multimodal/blip.h
+++ include/models/multimodal/blip.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct BLIP : xt::Cloneable<BLIP>
+    {
+    private:
+
+    public:
+        BLIP(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        BLIP(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
