fac009cb88336f9d0ae305e7a1c7155f17c4f10b
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:27:36 +0330

    multimodal models base file created

--- include/models/multimodal/llava.h
+++ include/models/multimodal/llava.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct LLAVA : xt::Cloneable<LLAVA>
+    {
+    private:
+
+    public:
+        LLAVA(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        LLAVA(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
