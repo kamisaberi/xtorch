d79b5b77893802d25aef5279a666307358df9437
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:19:30 +0330

    NetworkInNetwork model base file created

--- include/models/computer_vision/image_classification/network_in_network.h
+++ include/models/computer_vision/image_classification/network_in_network.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct NetworkInNetwork : xt::Cloneable<NetworkInNetwork>
+    {
+    private:
+
+    public:
+        NetworkInNetwork(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        NetworkInNetwork(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
