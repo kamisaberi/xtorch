81baaa985e900cd8993056bb09db3ac210b11acf
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:28:28 +0330

    YoloV1 model base file created

--- include/models/computer_vision/object_detection/yolo.h
+++ include/models/computer_vision/object_detection/yolo.h
@@ -1,2 +1,22 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct YoloV1 : xt::Cloneable<YoloV1>
+    {
+    private:
+
+    public:
+        YoloV1(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        YoloV1(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+
+
+
+
+}
