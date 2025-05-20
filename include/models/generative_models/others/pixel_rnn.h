da8a295d389b3d10f1f6c62eed3b1ea26b10abcc
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 14:51:14 +0330

    PixelRNN model base file created

--- include/models/generative_models/others/pixel_rnn.h
+++ include/models/generative_models/others/pixel_rnn.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct PixelRNN : xt::Cloneable<PixelRNN>
+    {
+    private:
+
+    public:
+        PixelRNN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        PixelRNN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
