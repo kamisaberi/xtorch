f77e784fb81770cec151b120e52b3fe1205ab654
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 14:30:42 +0330

    CycleGAN model base file created

--- include/models/generative_models/gans/cycle_gan.h
+++ include/models/generative_models/gans/cycle_gan.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct CycleGAN : xt::Cloneable<CycleGAN>
+    {
+    private:
+
+    public:
+        CycleGAN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        CycleGAN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
