1ecba02f0fe3176b41e6927330e3aadb929bbfdf
kamisaberi <kamisaberi@gmail.com>
2025-05-20 03:50:42 +0330

    InceptionV4 model base file created

--- include/models/computer_vision/image_classification/inception.h
+++ include/models/computer_vision/image_classification/inception.h
@@ -47,6 +47,18 @@ namespace xt::models
         void reset() override;
     };
 
+    struct InceptionV4 : xt::Cloneable<InceptionV4>
+    {
+    private:
+
+    public:
+        InceptionV4(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        InceptionV4(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
 
 
 }
