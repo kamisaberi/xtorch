135f02e0bd26c279a3ef7dcd2ee7a61d58bdf0fd
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 14:32:32 +0330

    Pix2Pix model base file created

--- include/models/generative_models/gans/pix2pix.h
+++ include/models/generative_models/gans/pix2pix.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct Pix2Pix : xt::Cloneable<Pix2Pix>
+    {
+    private:
+
+    public:
+        Pix2Pix(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        Pix2Pix(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
