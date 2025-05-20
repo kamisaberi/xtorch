e2a52d79af4e0c323c2b3fce115dc02bbe8d364a
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:15:20 +0330

    MaskRCNN model base file created

--- include/models/computer_vision/object_detection/mask_rcnn.h
+++ include/models/computer_vision/object_detection/mask_rcnn.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct MaskRCNN : xt::Cloneable<MaskRCNN>
+    {
+    private:
+
+    public:
+        MaskRCNN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        MaskRCNN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
