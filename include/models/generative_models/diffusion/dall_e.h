a3f0e2763d811060e90bd22c4cf5b1a36115c08d
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:57:25 +0330

    DallEV1,2,3 model base file created

--- include/models/generative_models/diffusion/dall_e.h
+++ include/models/generative_models/diffusion/dall_e.h
@@ -1,1 +1,46 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct DallEV1 : xt::Cloneable<DallEV1>
+    {
+    private:
+
+    public:
+        DallEV1(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DallEV1(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+    struct DallEV2 : xt::Cloneable<DallEV2>
+    {
+    private:
+
+    public:
+        DallEV2(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DallEV2(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+    struct DallEV3 : xt::Cloneable<DallEV3>
+    {
+    private:
+
+    public:
+        DallEV3(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DallEV3(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
