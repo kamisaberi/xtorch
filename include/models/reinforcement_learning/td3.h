3fe0644e7d9858fbd35b998c75579a8fee126a19
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 17:41:43 +0330

    reinforcement_learning models base file created

--- include/models/reinforcement_learning/td3.h
+++ include/models/reinforcement_learning/td3.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models {
+    struct TD3 : xt::Cloneable<TD3> {
+    private:
+
+    public:
+        TD3(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        TD3(int num_classes, int in_channels, std::vector <int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+        void reset() override;
+    };
+
+}
