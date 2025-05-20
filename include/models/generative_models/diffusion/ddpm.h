a7d988bd90a1eba8d293ce355e17aa707e1d20bb
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:58:26 +0330

    DDPM model base file created

--- include/models/generative_models/diffusion/ddpm.h
+++ include/models/generative_models/diffusion/ddpm.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct DDPM : xt::Cloneable<DDPM>
+    {
+    private:
+
+    public:
+        DDPM(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DDPM(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
