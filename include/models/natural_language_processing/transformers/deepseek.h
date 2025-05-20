1a9aea71ad80f23ca59924af8cbf286e9a4745e6
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:43:12 +0330

    npl/transformers models base file created

--- include/models/natural_language_processing/transformers/deepseek.h
+++ include/models/natural_language_processing/transformers/deepseek.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct DeepSeek : xt::Cloneable<DeepSeek>
+    {
+    private:
+
+    public:
+        DeepSeek(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DeepSeek(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
--- include/models/natural_language_processing/transformers/llama.h
+++ include/models/natural_language_processing/transformers/llama.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct LLAMA : xt::Cloneable<LLAMA>
+    {
+    private:
+
+    public:
+        LLAMA(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        LLAMA(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
