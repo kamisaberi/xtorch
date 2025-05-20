eaede253f251476c6f69e5efc4119239ec7e94bb
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 18:10:06 +0330

    reinforcement_learning models base file created

--- include/models/time_series_and_sequence/informer.h
+++ include/models/time_series_and_sequence/informer.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct Informer : xt::Cloneable<Informer>
+    {
+    private:
+
+    public:
+        Informer(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        Informer(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
