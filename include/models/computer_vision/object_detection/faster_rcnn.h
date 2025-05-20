1a4d40ad74efbb52759fb4d4a96d0200c0587d52
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:14:49 +0330

    FasterRCNN model base file created

--- include/models/computer_vision/object_detection/faster_rcnn.h
+++ include/models/computer_vision/object_detection/faster_rcnn.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct FasterRCNN : xt::Cloneable<FasterRCNN>
+    {
+    private:
+
+    public:
+        FasterRCNN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        FasterRCNN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
