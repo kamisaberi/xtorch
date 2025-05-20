7c08aeabd95af089e98698f852a19b14d3fe345c
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:02:47 +0330

    GCN, GIN model base file created

--- include/models/graph_neural_networks/gcn.h
+++ include/models/graph_neural_networks/gcn.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct GCN : xt::Cloneable<GCN>
+    {
+    private:
+
+    public:
+        GCN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        GCN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
