ba24eee6a81931800ec8c9f8a1100bacfdf098c7
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:37:28 +0330

    PVTV2 model base file created

--- include/models/computer_vision/vision_transformers/pvt.h
+++ include/models/computer_vision/vision_transformers/pvt.h
@@ -15,4 +15,17 @@ namespace xt::models
         void reset() override;
     };
 
+    struct PVTV2 : xt::Cloneable<PVTV2>
+    {
+    private:
+
+    public:
+        PVTV2(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        PVTV2(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
 }
