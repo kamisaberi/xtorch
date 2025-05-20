611838388bd72d8e31358c9bcd2edc5ff125bd67
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:01:53 +0330

    GAT model base file created

--- include/models/graph_neural_networks/gat.h
+++ include/models/graph_neural_networks/gat.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct GAT : xt::Cloneable<GAT>
+    {
+    private:
+
+    public:
+        GAT(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        GAT(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
