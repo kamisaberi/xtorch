24086aad2116cd9d79434ace550adf3292f946a0
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:38:45 +0330

    SwinTransformerV1 model base file created

--- include/models/computer_vision/vision_transformers/swin_transformer.h
+++ include/models/computer_vision/vision_transformers/swin_transformer.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct SwinTransformerV1 : xt::Cloneable<SwinTransformerV1>
+    {
+    private:
+
+    public:
+        SwinTransformerV1(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        SwinTransformerV1(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
