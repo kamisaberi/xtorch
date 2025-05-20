a52f8618ab78d3a1fbd208fb23633f2c0333b1e8
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:33:52 +0330

    CLIPViT model base file created

--- include/models/computer_vision/vision_transformers/clip_vit.h
+++ include/models/computer_vision/vision_transformers/clip_vit.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct CLIPViT : xt::Cloneable<CLIPViT>
+    {
+    private:
+
+    public:
+        CLIPViT(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        CLIPViT(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
