3283c55f9dff1f1c070964dd296b0e6e3c0e582c
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:38:16 +0330

    T2TViT model base file created

--- include/models/computer_vision/vision_transformers/t2t_vit.h
+++ include/models/computer_vision/vision_transformers/t2t_vit.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct T2TViT : xt::Cloneable<T2TViT>
+    {
+    private:
+
+    public:
+        T2TViT(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        T2TViT(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
