5e70c39115f7ef8e87dd3ca3731233141ce2c154
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:04:07 +0330

    GraphUNet model base file created

--- include/models/graph_neural_networks/graph_unet.h
+++ include/models/graph_neural_networks/graph_unet.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct GraphUNet	 : xt::Cloneable<GraphUNet>
+    {
+    private:
+
+    public:
+        GraphUNet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        GraphUNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
