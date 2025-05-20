484d8d322c2ff79b6413ff8d268bbade8aefbebd
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 14:52:31 +0330

    Glow model base file created

--- include/models/generative_models/others/glow.h
+++ include/models/generative_models/others/glow.h
@@ -1,2 +1,20 @@
 #pragma once
 
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct Glow : xt::Cloneable<Glow>
+    {
+    private:
+
+    public:
+        Glow(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        Glow(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
