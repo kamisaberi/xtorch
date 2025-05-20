6fd07dd7d4f51a131c977974c018aefbd704e6bb
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 14:41:32 +0330

    StyleGAN model base file created

--- include/models/generative_models/gans/style_gan.h
+++ include/models/generative_models/gans/style_gan.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct StyleGAN : xt::Cloneable<StyleGAN>
+    {
+    private:
+
+    public:
+        StyleGAN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        StyleGAN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
