70f00619795efeab5b1f566ed36b3270863bdf71
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:26:11 +0330

    CLIP model base file created

--- include/models/multimodal/clip.h
+++ include/models/multimodal/clip.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct CLIP : xt::Cloneable<CLIP>
+    {
+    private:
+
+    public:
+        CLIP(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        CLIP(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
