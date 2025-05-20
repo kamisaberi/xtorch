191957d038304901653321370e0512f22baccf2d
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 18:08:45 +0330

    reinforcement_learning models base file created

--- include/models/reinforcement_learning/sac.h
+++ include/models/reinforcement_learning/sac.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct SAC : xt::Cloneable<SAC>
+    {
+    private:
+
+    public:
+        SAC(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        SAC(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
