bc59dc2262a1f017d501faf6f73ec26f8ac07f47
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:51:21 +0330

    SparseAutoEncoder model base file created

--- include/models/generative_models/autoencoders/sparse_autoencoder.h
+++ include/models/generative_models/autoencoders/sparse_autoencoder.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct SparseAutoEncoder : xt::Cloneable<SparseAutoEncoder>
+    {
+    private:
+
+    public:
+        SparseAutoEncoder(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        SparseAutoEncoder(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
