46d5b3ce6aa4e69042178b026fc704c1d6cc5958
kamisaberi <kamisaberi@gmail.com>
2025-05-20 03:49:24 +0330

    InceptionV1 model base file created

--- include/models/computer_vision/image_classification/inception.h
+++ include/models/computer_vision/image_classification/inception.h
@@ -8,11 +8,9 @@ using namespace std;
 
 namespace xt::models
 {
-    struct InceptionV1 : xt::Module
+    struct InceptionV1 : xt::Cloneable<InceptionV1>
     {
-        mutable torch::nn::Sequential layer1 = nullptr, layer2 = nullptr, layer3 = nullptr, layer4 = nullptr, layer5 =
-                                          nullptr;
-        mutable torch::nn::Sequential fc = nullptr, fc1 = nullptr, fc2 = nullptr;
+    private:
 
     public:
         InceptionV1(int num_classes /* classes */, int in_channels = 3/* input channels */);
@@ -20,7 +18,9 @@ namespace xt::models
         InceptionV1(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
     };
+
 
     struct InceptionV2 : BaseModel
     {
