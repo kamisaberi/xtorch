624fe7e08534952d4aea315b04729e5515fc5e8f
kamisaberi <kamisaberi@gmail.com>
2025-05-20 03:34:57 +0330

    EfficientNetB7 model base file created

--- include/models/computer_vision/image_classification/efficient_net.h
+++ include/models/computer_vision/image_classification/efficient_net.h
@@ -101,5 +101,17 @@ namespace xt::models
         void reset() override;
     };
 
+    struct EfficientNetB7 : xt::Cloneable<EfficientNetB7>
+    {
+    private:
+
+    public:
+        EfficientNetB7(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        EfficientNetB7(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
 
 }
