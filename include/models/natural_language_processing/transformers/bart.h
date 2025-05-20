9f0f7113b7bcfa9becbff5ff092edd61f974e713
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:41:25 +0330

    npl/transformers models base file created

--- include/models/natural_language_processing/transformers/bart.h
+++ include/models/natural_language_processing/transformers/bart.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct BART : xt::Cloneable<BART>
+    {
+    private:
+
+    public:
+        BART(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        BART(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
