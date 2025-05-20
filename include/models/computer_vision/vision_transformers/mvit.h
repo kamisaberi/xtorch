ec6c99022b235963101b8cd5d6fc9562556fb762
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:35:43 +0330

    MViT model base file created

--- include/models/computer_vision/vision_transformers/mvit.h
+++ include/models/computer_vision/vision_transformers/mvit.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct MViT : xt::Cloneable<MViT>
+    {
+    private:
+
+    public:
+        MViT(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        MViT(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
