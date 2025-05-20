80f97e5c29db5a0887d03a8be61b003a4c7a861b
kamisaberi <kamisaberi@gmail.com>
2025-05-20 03:48:17 +0330

    HighwayNetwork model base file created

--- include/models/computer_vision/image_classification/highway_network.h
+++ include/models/computer_vision/image_classification/highway_network.h
@@ -1,3 +1,21 @@
 #pragma once
 
 #include "models/common.h"
+namespace  xt::models
+{
+
+    struct HighwayNetwork : xt::Cloneable<HighwayNetwork>
+    {
+    private:
+
+    public:
+        HighwayNetwork(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        HighwayNetwork(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+
+}
