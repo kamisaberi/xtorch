bb6a17b0e4a0464254c6e6f0b5f27ec9c41be82b
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 17:42:39 +0330

    reinforcement_learning models base file created

--- include/models/reinforcement_learning/dueling_dqn.h
+++ include/models/reinforcement_learning/dueling_dqn.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct DuelingDQN : xt::Cloneable<DuelingDQN>
+    {
+    private:
+
+    public:
+        DuelingDQN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DuelingDQN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
--- include/models/reinforcement_learning/impala.h
+++ include/models/reinforcement_learning/impala.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct IMPALA : xt::Cloneable<IMPALA>
+    {
+    private:
+
+    public:
+        IMPALA(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        IMPALA(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
