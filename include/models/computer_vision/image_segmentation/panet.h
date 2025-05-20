25d7f4d2c8c7c7ab2102c38a2f3379c70b2c586b
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:56:17 +0330

    PANet model base file created

--- include/models/computer_vision/image_segmentation/panet.h
+++ include/models/computer_vision/image_segmentation/panet.h
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "models/common.h"
+namespace  xt::models
+{
+    struct PANet : xt::Cloneable<PANet>
+    {
+    private:
+
+    public:
+        PANet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        PANet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
