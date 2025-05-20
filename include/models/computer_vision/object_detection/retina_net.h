70d1a003a603a292ff5ed3b9268a7a0506d93b8c
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:16:25 +0330

    RetinaNet model base file created

--- include/models/computer_vision/object_detection/retina_net.h
+++ include/models/computer_vision/object_detection/retina_net.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+    struct RetinaNet : xt::Cloneable<RetinaNet>
+    {
+    private:
+
+    public:
+        RetinaNet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        RetinaNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
