7fa81200c53e888234936f402017461305421a71
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:20:34 +0330

    PyramidalNet model base file created

--- include/models/computer_vision/image_classification/pyramidal_net.h
+++ include/models/computer_vision/image_classification/pyramidal_net.h
@@ -1,3 +1,21 @@
 #pragma once
 
 #include "models/common.h"
+
+
+namespace xt::models
+{
+    struct PyramidalNet : xt::Cloneable<PyramidalNet>
+    {
+    private:
+
+    public:
+        PyramidalNet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        PyramidalNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
