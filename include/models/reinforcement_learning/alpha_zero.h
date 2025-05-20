0d60d3a9eeeba19e98ff3333cdb7abcf4848f7ca
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 17:41:43 +0330

    reinforcement_learning models base file created

--- include/models/reinforcement_learning/alpha_zero.h
+++ include/models/reinforcement_learning/alpha_zero.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct AlphaZero : xt::Cloneable<AlphaZero>
+    {
+    private:
+
+    public:
+        AlphaZero(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        AlphaZero(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
--- include/models/reinforcement_learning/ddpg.h
+++ include/models/reinforcement_learning/ddpg.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models {
+    struct DDPG : xt::Cloneable<DDPG> {
+    private:
+
+    public:
+        DDPG(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DDPG(int num_classes, int in_channels, std::vector <int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+        void reset() override;
+    };
+
+}
