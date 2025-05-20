768c05ffb15c6a9207585a921ddfb8886792e7b9
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:39:20 +0330

    npl/others rnn base file created

--- include/models/natural_language_processing/rnn/seq2seq.h
+++ include/models/natural_language_processing/rnn/seq2seq.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct Seq2Seq : xt::Cloneable<Seq2Seq>
+    {
+    private:
+
+    public:
+        Seq2Seq(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        Seq2Seq(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
