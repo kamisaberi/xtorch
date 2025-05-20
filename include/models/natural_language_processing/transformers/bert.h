c1460c66f31e2e36eb7aec3f44ae179fe71427f8
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:41:25 +0330

    npl/transformers models base file created

--- include/models/natural_language_processing/transformers/bert.h
+++ include/models/natural_language_processing/transformers/bert.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct BERT : xt::Cloneable<BERT>
+    {
+    private:
+
+    public:
+        BERT(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        BERT(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
--- include/models/natural_language_processing/transformers/big_bird.h
+++ include/models/natural_language_processing/transformers/big_bird.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct BigBird : xt::Cloneable<BigBird>
+    {
+    private:
+
+    public:
+        BigBird(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        BigBird(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
