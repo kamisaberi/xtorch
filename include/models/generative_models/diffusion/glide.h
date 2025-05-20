68302331c0467fae7aa34a259d7fab27a6b30103
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:58:55 +0330

    GLIDE model base file created

--- include/models/generative_models/diffusion/glide.h
+++ include/models/generative_models/diffusion/glide.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct GLIDE : xt::Cloneable<GLIDE>
+    {
+    private:
+
+    public:
+        GLIDE(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        GLIDE(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
