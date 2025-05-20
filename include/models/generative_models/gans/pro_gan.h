124a4078481c7f0839e8ea9f73da0309e17d38e9
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 14:33:04 +0330

    ProGAN model base file created

--- include/models/generative_models/gans/pro_gan.h
+++ include/models/generative_models/gans/pro_gan.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct ProGAN : xt::Cloneable<ProGAN>
+    {
+    private:
+
+    public:
+        ProGAN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        ProGAN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
