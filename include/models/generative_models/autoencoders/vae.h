385f38745895e470f8688aafb75d39687637a564
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:51:49 +0330

    VAE model base file created

--- include/models/generative_models/autoencoders/vae.h
+++ include/models/generative_models/autoencoders/vae.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct VAE : xt::Cloneable<VAE>
+    {
+    private:
+
+    public:
+        VAE(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        VAE(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
