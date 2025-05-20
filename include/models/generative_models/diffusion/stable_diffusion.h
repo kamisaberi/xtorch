587a0725c259f56b84a58ad9caec5f24efbef72c
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 14:00:04 +0330

    StableDiffusion model base file created

--- include/models/generative_models/diffusion/stable_diffusion.h
+++ include/models/generative_models/diffusion/stable_diffusion.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct StableDiffusion	 : xt::Cloneable<StableDiffusion>
+    {
+    private:
+
+    public:
+        StableDiffusion(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        StableDiffusion(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
