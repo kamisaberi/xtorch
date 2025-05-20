7dd460492b620add703a801fda73311246ade19f
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 14:31:55 +0330

    GAN model base file created

--- include/models/generative_models/gans/gan.h
+++ include/models/generative_models/gans/gan.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct GAN : xt::Cloneable<GAN>
+    {
+    private:
+
+    public:
+        GAN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        GAN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
