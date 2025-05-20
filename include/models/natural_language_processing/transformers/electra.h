1e823befcb36264038661ca9240442aa60893e73
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:43:12 +0330

    npl/transformers models base file created

--- include/models/natural_language_processing/transformers/electra.h
+++ include/models/natural_language_processing/transformers/electra.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct ELECTRA : xt::Cloneable<ELECTRA>
+    {
+    private:
+
+    public:
+        ELECTRA(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        ELECTRA(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
