bd7178f4c783e8f9029b8c16879eebad7df37455
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 13:50:45 +0330

    DAE model base file created

--- include/models/generative_models/autoencoders/dae.h
+++ include/models/generative_models/autoencoders/dae.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct DAE : xt::Cloneable<DAE>
+    {
+    private:
+
+    public:
+        DAE(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DAE(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
