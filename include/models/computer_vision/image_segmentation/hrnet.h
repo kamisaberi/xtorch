3933c47a370725a8c7699816dfceafa56e6182b3
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:55:39 +0330

    HRNet model base file created

--- include/models/computer_vision/image_segmentation/hrnet.h
+++ include/models/computer_vision/image_segmentation/hrnet.h
@@ -1,3 +1,20 @@
 #pragma once
 
 #include "models/common.h"
+
+namespace xt::models
+{
+    struct HRNet : xt::Cloneable<HRNet>
+    {
+    private:
+
+    public:
+        HRNet(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        HRNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
