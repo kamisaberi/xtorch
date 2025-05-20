f62ff5b343394b81f7e8bcce30ec80ed790ce274
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 17:39:30 +0330

    reinforcement_learning models base file created

--- include/models/reinforcement_learning/a3c.h
+++ include/models/reinforcement_learning/a3c.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct A3C  : xt::Cloneable<A3C >
+    {
+    private:
+
+    public:
+        A3C (int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        A3C (int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
