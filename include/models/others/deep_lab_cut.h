39055b87ccc99668c51f3996d2acba5482642744
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 17:36:56 +0330

    npl/others models base file created

--- include/models/others/deep_lab_cut.h
+++ include/models/others/deep_lab_cut.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct DeepLabCut : xt::Cloneable<DeepLabCut>
+    {
+    private:
+
+    public:
+        DeepLabCut(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        DeepLabCut(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
