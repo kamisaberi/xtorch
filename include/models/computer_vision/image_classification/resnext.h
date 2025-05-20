3408c39fe9d8c6d8aee940d8c6d68a5dad940d13
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:41:14 +0330

    ResNeXt model base file created

--- include/models/computer_vision/image_classification/resnext.h
+++ include/models/computer_vision/image_classification/resnext.h
@@ -5,35 +5,18 @@
 using namespace std;
 
 namespace xt::models {
-    namespace {
-        struct ResidualBlock : torch::nn::Module {
-            torch::nn::Sequential conv1 = nullptr, conv2 = nullptr, downsample = nullptr;
-            int out_channels;
-            torch::nn::ReLU relu = nullptr;
-            torch::Tensor residual;
+    struct ResNeXt : xt::Cloneable<ResNeXt>
+    {
+    private:
 
-            ResidualBlock(int in_channels, int out_channels, int stride = 1,
-                          torch::nn::Sequential downsample = nullptr);
+    public:
+        ResNeXt(int num_classes /* classes */, int in_channels = 3/* input channels */);
 
-            torch::Tensor forward(torch::Tensor x);
-        };
-    }
-
-    struct ResNext : xt::Module {
-        mutable int inplanes = 64;
-        mutable torch::nn::Sequential conv1 = nullptr;
-        mutable torch::nn::MaxPool2d maxpool = nullptr;
-        mutable torch::nn::AvgPool2d avgpool = nullptr;
-
-        mutable torch::nn::Sequential layer0 = nullptr, layer1 = nullptr, layer2 = nullptr, layer3 = nullptr;
-        mutable torch::nn::Linear fc = nullptr;
-
-        ResNext(vector<int> layers, int num_classes = 10, int in_channels = 3 /* input channels */);
-
-        ResNext(std::vector<int> layers, int num_classes, int in_channels, std::vector<int64_t> input_shape);
-
-        torch::nn::Sequential makeLayerFromResidualBlock(int planes, int blocks, int stride = 1);
+        ResNeXt(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
         torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
     };
+
+
 }
