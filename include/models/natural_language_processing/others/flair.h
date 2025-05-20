e84292bb39c6a8af0388c267164fa6293ff2ea7d
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:38:06 +0330

    npl/others models base file created

--- include/models/natural_language_processing/others/flair.h
+++ include/models/natural_language_processing/others/flair.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct Flair : xt::Cloneable<Flair>
+    {
+    private:
+
+    public:
+        Flair(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        Flair(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
--- include/models/natural_language_processing/others/word2vec.h
+++ include/models/natural_language_processing/others/word2vec.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct Word2Vec : xt::Cloneable<Word2Vec>
+    {
+    private:
+
+    public:
+        Word2Vec(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        Word2Vec(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
