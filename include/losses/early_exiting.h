d57b3f09be49bf1d5d41be908ca10f180a545370
kamisaberi <kamisaberi@gmail.com>
2025-05-21 00:50:51 +0330

    losses base files created

--- include/losses/early_exiting.h
+++ include/losses/early_exiting.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "losses/common.h"
+
+
+namespace xt::losses
+{
+    class EarlyExiting : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
--- include/losses/elastic_face.h
+++ include/losses/elastic_face.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "losses/common.h"
+
+
+namespace xt::losses
+{
+    class ElasticFace : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
