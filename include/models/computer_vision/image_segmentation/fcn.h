af3b8472c100f2746045d19220ee9541b2603e24
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:54:44 +0330

    FCN model base file created

--- include/models/computer_vision/image_segmentation/fcn.h
+++ include/models/computer_vision/image_segmentation/fcn.h
@@ -1,2 +1,19 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+
+    struct FCN : xt::Cloneable<FCN>
+    {
+    private:
+
+    public:
+        FCN(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        FCN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
