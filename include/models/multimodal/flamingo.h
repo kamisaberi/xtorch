3e6486c9a710cd45dbd4b1f666ce27a8769b7989
kamran saberifard <kamisaberi@gmail.com>
2025-05-20 15:27:36 +0330

    multimodal models base file created

--- include/models/multimodal/flamingo.h
+++ include/models/multimodal/flamingo.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct Flamingo : xt::Cloneable<Flamingo>
+    {
+    private:
+
+    public:
+        Flamingo(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        Flamingo(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
--- include/models/multimodal/mert.h
+++ include/models/multimodal/mert.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "models/common.h"
+
+namespace xt::models
+{
+    struct MERT : xt::Cloneable<MERT>
+    {
+    private:
+
+    public:
+        MERT(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        MERT(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
