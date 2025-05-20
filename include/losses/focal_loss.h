7e7bd03604bfdcd80ef4989ed23345b61bfd97ba
kamisaberi <kamisaberi@gmail.com>
2025-05-21 00:51:51 +0330

    losses base files created

--- include/losses/focal_loss.h
+++ include/losses/focal_loss.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "losses/common.h"
+
+
+namespace xt::losses
+{
+    class FocalLoss : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
--- include/losses/gan_hinge_loss.h
+++ include/losses/gan_hinge_loss.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "losses/common.h"
+
+
+namespace xt::losses
+{
+    class GANHingeLoss : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
