ceed78c46d7d90bd2188f00f46b101a7c7a76532
kamisaberi <kamisaberi@gmail.com>
2025-05-20 04:21:40 +0330

    Xception model base file created

--- include/models/computer_vision/image_classification/xception.h
+++ include/models/computer_vision/image_classification/xception.h
@@ -1,2 +1,20 @@
 #pragma once
 #include "models/common.h"
+namespace xt::models
+{
+
+
+    struct Xception : xt::Cloneable<Xception>
+    {
+    private:
+
+    public:
+        Xception(int num_classes /* classes */, int in_channels = 3/* input channels */);
+
+        Xception(int num_classes, int in_channels, std::vector<int64_t> input_shape);
+
+        torch::Tensor forward(torch::Tensor x) const override;
+        void reset() override;
+    };
+
+}
