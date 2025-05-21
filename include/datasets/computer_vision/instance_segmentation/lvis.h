f03b0087cc8fe404f364c59324d2880d121fd2cb
kamisaberi <kamisaberi@gmail.com>
2025-05-22 00:59:36 +0330

    datasets base files implementation created

--- include/datasets/computer_vision/instance_segmentation/lvis.h
+++ include/datasets/computer_vision/instance_segmentation/lvis.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "datasets/common.h"
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace xt::data::datasets
+{
+    class LVIS : public xt::datasets::Dataset
+    {
+    public :
+        explicit LVIS(const std::string& root);
+        LVIS(const std::string& root, xt::datasets::DataMode mode);
+        LVIS(const std::string& root, xt::datasets::DataMode mode, bool download);
+        LVIS(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        LVIS(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+
+
+    };
+}
