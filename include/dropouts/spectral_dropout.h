fd71a0a027c96de3be3a85f777f20d11a913ba02
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 12:46:37 +0330

    dropouts files interfaces is creating

--- include/dropouts/spectral_dropout.h
+++ include/dropouts/spectral_dropout.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "dropouts/common.h"
+
+
+namespace xt::dropouts
+{
+    class SpectralDropout : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
--- include/dropouts/targeted_dropout.h
+++ include/dropouts/targeted_dropout.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "dropouts/common.h"
+
+
+namespace xt::dropouts
+{
+    class TargetedDropout : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
