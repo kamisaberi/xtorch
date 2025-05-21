cbe2001e7b6a60739e0f011ffdb2cea7ab4a25ad
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:14:28 +0330

    datasets base files implementation created

--- src/datasets/computer_vision/autonomous_driving_perception/nu_scenes.cpp
+++ src/datasets/computer_vision/autonomous_driving_perception/nu_scenes.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "datasets/computer_vision/autonomous_driving_perception/nu_scenes.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- NuScenes ---------------------- //
+
+    NuScenes::NuScenes(const std::string& root): NuScenes::NuScenes(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    NuScenes::NuScenes(const std::string& root, xt::datasets::DataMode mode): NuScenes::NuScenes(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    NuScenes::NuScenes(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        NuScenes::NuScenes(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    NuScenes::NuScenes(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : NuScenes::NuScenes(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    NuScenes::NuScenes(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void NuScenes::load_data()
+    {
+
+    }
+
+    void NuScenes::check_resources()
+    {
+
+    }
+}
