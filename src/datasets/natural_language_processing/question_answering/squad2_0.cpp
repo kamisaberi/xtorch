d4e3136fc4ebcf91a9d6ae3915b9c8698f8fe8c9
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:28:54 +0330

    datasets base files implementation created

--- src/datasets/natural_language_processing/question_answering/squad2_0.cpp
+++ src/datasets/natural_language_processing/question_answering/squad2_0.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/natural_language_processing/question_answering/squad2_0.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- SQuAD20 ---------------------- //
+
+    SQuAD20::SQuAD20(const std::string& root): SQuAD20::SQuAD20(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    SQuAD20::SQuAD20(const std::string& root, xt::datasets::DataMode mode): SQuAD20::SQuAD20(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    SQuAD20::SQuAD20(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        SQuAD20::SQuAD20(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    SQuAD20::SQuAD20(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : SQuAD20::SQuAD20(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    SQuAD20::SQuAD20(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void SQuAD20::load_data()
+    {
+
+    }
+
+    void SQuAD20::check_resources()
+    {
+
+    }
+}
