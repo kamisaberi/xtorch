35ca158babe52ec01db303ceb18f810f79637a5f
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 14:45:02 +0330

    datasets interface files is creating

--- include/datasets/computer_vision/autonomous_driving_perception/waymo_open_dataset.h
+++ include/datasets/computer_vision/autonomous_driving_perception/waymo_open_dataset.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class WaymoOpenDataset : public xt::datasets::Dataset
+    {
+    public :
+        explicit WaymoOpenDataset(const std::string& root);
+        WaymoOpenDataset(const std::string& root, xt::datasets::DataMode mode);
+        WaymoOpenDataset(const std::string& root, xt::datasets::DataMode mode, bool download);
+        WaymoOpenDataset(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        WaymoOpenDataset(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
