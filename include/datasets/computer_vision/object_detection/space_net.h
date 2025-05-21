9cf5af4964d6ffef45a8edd5b9411b3205c4ab46
kamisaberi <kamisaberi@gmail.com>
2025-05-22 01:01:38 +0330

    datasets base files implementation created

--- include/datasets/computer_vision/object_detection/space_net.h
+++ include/datasets/computer_vision/object_detection/space_net.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "datasets/common.h"
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace xt::data::datasets
+{
+    class SpaceNet : public xt::datasets::Dataset
+    {
+    public :
+        explicit SpaceNet(const std::string& root);
+        SpaceNet(const std::string& root, xt::datasets::DataMode mode);
+        SpaceNet(const std::string& root, xt::datasets::DataMode mode, bool download);
+        SpaceNet(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        SpaceNet(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+
+
+    };
+}
