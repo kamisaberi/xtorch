e54bb351fae5f2f620f01d8e5acbc20a25553982
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:13:23 +0330

    datasets base files implementation created

--- src/datasets/computer_vision/3d_shape_generation/shapenet.cpp
+++ src/datasets/computer_vision/3d_shape_generation/shapenet.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "datasets/computer_vision/3d_shape_generation/shapenet.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- ShapeNet ---------------------- //
+
+    ShapeNet::ShapeNet(const std::string& root): ShapeNet::ShapeNet(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    ShapeNet::ShapeNet(const std::string& root, xt::datasets::DataMode mode): ShapeNet::ShapeNet(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    ShapeNet::ShapeNet(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        ShapeNet::ShapeNet(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    ShapeNet::ShapeNet(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : ShapeNet::ShapeNet(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    ShapeNet::ShapeNet(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void ShapeNet::load_data()
+    {
+
+    }
+
+    void ShapeNet::check_resources()
+    {
+
+    }
+}
