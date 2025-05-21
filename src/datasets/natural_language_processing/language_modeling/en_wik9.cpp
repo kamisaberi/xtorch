d97a00c514ce31f35c503f811454fb526da3a811
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:24:08 +0330

    datasets base files implementation created

--- src/datasets/natural_language_processing/language_modeling/en_wik9.cpp
+++ src/datasets/natural_language_processing/language_modeling/en_wik9.cpp
@@ -1,7 +1,48 @@
 #include "datasets/natural_language_processing/language_modeling/en_wik9.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- EnWik9 ---------------------- //
+
+    EnWik9::EnWik9(const std::string& root): EnWik9::EnWik9(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    EnWik9::EnWik9(const std::string& root, xt::datasets::DataMode mode): EnWik9::EnWik9(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    EnWik9::EnWik9(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        EnWik9::EnWik9(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    EnWik9::EnWik9(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : EnWik9::EnWik9(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    EnWik9::EnWik9(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
 
 
+    void EnWik9::load_data()
+    {
 
+    }
+
+    void EnWik9::check_resources()
+    {
+
+    }
 }
