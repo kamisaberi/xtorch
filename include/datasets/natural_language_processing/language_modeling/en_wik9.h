1c1f88eb278465cdf203ed0c14532a2bdcd27307
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:21:36 +0330

    datasets interface files is creating

--- include/datasets/natural_language_processing/language_modeling/en_wik9.h
+++ include/datasets/natural_language_processing/language_modeling/en_wik9.h
@@ -1,2 +1,35 @@
 #pragma once
 
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class EnWik9 : public xt::datasets::Dataset
+    {
+    public :
+        explicit EnWik9(const std::string& root);
+        EnWik9(const std::string& root, xt::datasets::DataMode mode);
+        EnWik9(const std::string& root, xt::datasets::DataMode mode, bool download);
+        EnWik9(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        EnWik9(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
--- include/datasets/natural_language_processing/math_word_problems/gsm8k.h
+++ include/datasets/natural_language_processing/math_word_problems/gsm8k.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class GSM8K : public xt::datasets::Dataset
+    {
+    public :
+        explicit GSM8K(const std::string& root);
+        GSM8K(const std::string& root, xt::datasets::DataMode mode);
+        GSM8K(const std::string& root, xt::datasets::DataMode mode, bool download);
+        GSM8K(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        GSM8K(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
