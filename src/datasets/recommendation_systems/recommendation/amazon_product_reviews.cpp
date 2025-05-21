615328e138e9db2aa0eba32001b8933446be045e
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:07:59 +0330

    datasets base files implementation created

--- src/datasets/recommendation_systems/recommendation/amazon_product_reviews.cpp
+++ src/datasets/recommendation_systems/recommendation/amazon_product_reviews.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/recommendation_systems/recommendation/amazon_product_reviews.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- AmazonProductReviews ---------------------- //
+
+    AmazonProductReviews::AmazonProductReviews(const std::string& root): AmazonProductReviews::AmazonProductReviews(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    AmazonProductReviews::AmazonProductReviews(const std::string& root, xt::datasets::DataMode mode): AmazonProductReviews::AmazonProductReviews(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    AmazonProductReviews::AmazonProductReviews(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        AmazonProductReviews::AmazonProductReviews(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    AmazonProductReviews::AmazonProductReviews(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : AmazonProductReviews::AmazonProductReviews(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    AmazonProductReviews::AmazonProductReviews(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void AmazonProductReviews::load_data()
+    {
+
+    }
+
+    void AmazonProductReviews::check_resources()
+    {
+
+    }
+}
