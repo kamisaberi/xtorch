3bcb800966e3ea447173f9d56c4432b43a0c341e
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 12:37:22 +0330

    dropouts files interfaces is creating

--- include/dropouts/drop_block.h
+++ include/dropouts/drop_block.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "dropouts/common.h"
+
+
+namespace xt::dropouts
+{
+    class DropBlock : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
--- include/dropouts/drop_path.h
+++ include/dropouts/drop_path.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "dropouts/common.h"
+
+
+namespace xt::dropouts
+{
+    class DropPath : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
