087e71e946c924b96a256cbe6bae4ee60f929f44
kamisaberi <kamisaberi@gmail.com>
2025-05-22 01:51:23 +0330

    datasets base files implementation created

--- src/datasets/audio_processing/speech_recognition/libri_light_limited.cpp
+++ src/datasets/audio_processing/speech_recognition/libri_light_limited.cpp
@@ -1,4 +1,47 @@
-//
-// Created by pc on 5/11/2025.
-//
+#include "datasets/audio_processing/speech_recognition/libri_light_limited.h"
 
+namespace xt::data::datasets
+{
+
+    LibriLightLimited::LibriLightLimited(const std::string& root): LibriLightLimited::LibriLightLimited(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    LibriLightLimited::LibriLightLimited(const std::string& root, xt::datasets::DataMode mode): LibriLightLimited::LibriLightLimited(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    LibriLightLimited::LibriLightLimited(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        LibriLightLimited::LibriLightLimited(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    LibriLightLimited::LibriLightLimited(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : LibriLightLimited::LibriLightLimited(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    LibriLightLimited::LibriLightLimited(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void LibriLightLimited::load_data()
+    {
+
+    }
+
+    void LibriLightLimited::check_resources()
+    {
+
+    }
+}
