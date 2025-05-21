19ec720ae514e1047c179164aae4f53efbe8cf3b
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 12:34:14 +0330

    dropouts files interfaces is creating

--- include/dropouts/band_dropout.h
+++ include/dropouts/band_dropout.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "dropouts/common.h"
+
+
+namespace xt::dropouts
+{
+    class BandDropout : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
--- include/dropouts/checkerboard_dropout.h
+++ include/dropouts/checkerboard_dropout.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "dropouts/common.h"
+
+
+namespace xt::dropouts
+{
+    class CheckerboardDropout : xt::Module
+    {
+    public:
+
+    private:
+    };
+}
