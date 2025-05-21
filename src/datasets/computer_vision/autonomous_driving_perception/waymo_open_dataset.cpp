20302777ba1c272fb1b902eab4c7d830eea422fb
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:15:04 +0330

    datasets base files implementation created

--- src/datasets/computer_vision/autonomous_driving_perception/waymo_open_dataset.cpp
+++ src/datasets/computer_vision/autonomous_driving_perception/waymo_open_dataset.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "datasets/computer_vision/autonomous_driving_perception/waymo_open_dataset.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- WaymoOpenDataset ---------------------- //
+
+    WaymoOpenDataset::WaymoOpenDataset(const std::string& root): WaymoOpenDataset::WaymoOpenDataset(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    WaymoOpenDataset::WaymoOpenDataset(const std::string& root, xt::datasets::DataMode mode): WaymoOpenDataset::WaymoOpenDataset(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    WaymoOpenDataset::WaymoOpenDataset(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        WaymoOpenDataset::WaymoOpenDataset(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    WaymoOpenDataset::WaymoOpenDataset(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : WaymoOpenDataset::WaymoOpenDataset(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    WaymoOpenDataset::WaymoOpenDataset(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void WaymoOpenDataset::load_data()
+    {
+
+    }
+
+    void WaymoOpenDataset::check_resources()
+    {
+
+    }
+}
