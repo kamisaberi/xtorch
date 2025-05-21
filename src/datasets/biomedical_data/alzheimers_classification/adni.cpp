7560ed7fa1bf555b548371950d367057cff84f32
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:10:43 +0330

    datasets base files implementation created

--- src/datasets/biomedical_data/alzheimers_classification/adni.cpp
+++ src/datasets/biomedical_data/alzheimers_classification/adni.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/biomedical_data/alzheimers_classification/adni.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- ADNI ---------------------- //
+
+    ADNI::ADNI(const std::string& root): ADNI::ADNI(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    ADNI::ADNI(const std::string& root, xt::datasets::DataMode mode): ADNI::ADNI(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    ADNI::ADNI(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        ADNI::ADNI(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    ADNI::ADNI(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : ADNI::ADNI(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    ADNI::ADNI(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void ADNI::load_data()
+    {
+
+    }
+
+    void ADNI::check_resources()
+    {
+
+    }
+}
