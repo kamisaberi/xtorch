2e47ba0b981fa539028c8673f9f482015ff06c70
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 14:54:54 +0330

    datasets interface files is creating

--- include/datasets/computer_vision/image_generation/ffhq.h
+++ include/datasets/computer_vision/image_generation/ffhq.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class FFHQ : public xt::datasets::Dataset
+    {
+    public :
+        explicit FFHQ(const std::string& root);
+        FFHQ(const std::string& root, xt::datasets::DataMode mode);
+        FFHQ(const std::string& root, xt::datasets::DataMode mode, bool download);
+        FFHQ(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        FFHQ(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
