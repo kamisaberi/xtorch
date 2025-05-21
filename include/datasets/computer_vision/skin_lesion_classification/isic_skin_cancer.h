007fdd98a45db7414761ec56fc4c23f498f2fa96
kamisaberi <kamisaberi@gmail.com>
2025-05-22 01:09:41 +0330

    datasets base files implementation created

--- include/datasets/computer_vision/skin_lesion_classification/isic_skin_cancer.h
+++ include/datasets/computer_vision/skin_lesion_classification/isic_skin_cancer.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "datasets/common.h"
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace xt::data::datasets
+{
+    class ISICSkinCancer : public xt::datasets::Dataset
+    {
+    public :
+        explicit ISICSkinCancer(const std::string& root);
+        ISICSkinCancer(const std::string& root, xt::datasets::DataMode mode);
+        ISICSkinCancer(const std::string& root, xt::datasets::DataMode mode, bool download);
+        ISICSkinCancer(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        ISICSkinCancer(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+
+
+    };
+}
