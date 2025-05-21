22c1ae08f62aeb114d6799733be45e163a338be4
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:13:58 +0330

    datasets base files implementation created

--- src/datasets/computer_vision/anomaly_detection/mvtec_ad.cpp
+++ src/datasets/computer_vision/anomaly_detection/mvtec_ad.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "datasets/computer_vision/anomaly_detection/mvtec_ad.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- MVTecAD ---------------------- //
+
+    MVTecAD::MVTecAD(const std::string& root): MVTecAD::MVTecAD(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    MVTecAD::MVTecAD(const std::string& root, xt::datasets::DataMode mode): MVTecAD::MVTecAD(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    MVTecAD::MVTecAD(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        MVTecAD::MVTecAD(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    MVTecAD::MVTecAD(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : MVTecAD::MVTecAD(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    MVTecAD::MVTecAD(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void MVTecAD::load_data()
+    {
+
+    }
+
+    void MVTecAD::check_resources()
+    {
+
+    }
+}
