d767cfcf2238d8bf80f486de086d9c41b9a4f167
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:23:36 +0330

    datasets base files implementation created

--- src/datasets/natural_language_processing/dialogue_generation/persona_chat.cpp
+++ src/datasets/natural_language_processing/dialogue_generation/persona_chat.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/image_classification/caltech101.h"
+#include "datasets/natural_language_processing/dialogue_generation/persona_chat.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- Caltech101 ---------------------- //
+    // ---------------------- PersonaChat ---------------------- //
 
-    Caltech101::Caltech101(const std::string& root): Caltech101::Caltech101(
+    PersonaChat::PersonaChat(const std::string& root): PersonaChat::PersonaChat(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    Caltech101::Caltech101(const std::string& root, xt::datasets::DataMode mode): Caltech101::Caltech101(
+    PersonaChat::PersonaChat(const std::string& root, xt::datasets::DataMode mode): PersonaChat::PersonaChat(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    Caltech101::Caltech101(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        Caltech101::Caltech101(
+    PersonaChat::PersonaChat(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        PersonaChat::PersonaChat(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    Caltech101::Caltech101(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : Caltech101::Caltech101(
+    PersonaChat::PersonaChat(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : PersonaChat::PersonaChat(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    Caltech101::Caltech101(const std::string& root, xt::datasets::DataMode mode, bool download,
+    PersonaChat::PersonaChat(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void Caltech101::load_data()
+    void PersonaChat::load_data()
     {
 
     }
 
-    void Caltech101::check_resources()
+    void PersonaChat::check_resources()
     {
 
     }
