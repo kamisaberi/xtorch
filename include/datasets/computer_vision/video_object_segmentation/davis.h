39d0821a2be780d97664d8a6d34b8938676fc2ca
kamisaberi <kamisaberi@gmail.com>
2025-05-22 01:20:24 +0330

    datasets base files implementation created

--- include/datasets/computer_vision/video_object_segmentation/davis.h
+++ include/datasets/computer_vision/video_object_segmentation/davis.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "datasets/common.h"
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace xt::data::datasets
+{
+    class DAVIS : public xt::datasets::Dataset
+    {
+    public :
+        explicit DAVIS(const std::string& root);
+        DAVIS(const std::string& root, xt::datasets::DataMode mode);
+        DAVIS(const std::string& root, xt::datasets::DataMode mode, bool download);
+        DAVIS(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        DAVIS(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+
+
+    };
+}
