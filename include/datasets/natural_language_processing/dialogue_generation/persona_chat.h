6a72f988abb3b8fc3f5657e4178cea8f12adeed3
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:19:15 +0330

    datasets interface files is creating

--- include/datasets/natural_language_processing/dialogue_generation/persona_chat.h
+++ include/datasets/natural_language_processing/dialogue_generation/persona_chat.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class PersonaChat : public xt::datasets::Dataset
+    {
+    public :
+        explicit PersonaChat(const std::string& root);
+        PersonaChat(const std::string& root, xt::datasets::DataMode mode);
+        PersonaChat(const std::string& root, xt::datasets::DataMode mode, bool download);
+        PersonaChat(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        PersonaChat(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
