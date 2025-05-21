a0ac7f71ccae2822797ef55d7086e42dce88ce37
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:12:01 +0330

    datasets base files implementation created

--- src/datasets/time_series/time_series_forecasting/electricity_load_diagrams.cpp
+++ src/datasets/time_series/time_series_forecasting/electricity_load_diagrams.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/time_series/time_series_forecasting/electricity_load_diagrams.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- ElectricityLoadDiagrams ---------------------- //
+
+    ElectricityLoadDiagrams::ElectricityLoadDiagrams(const std::string& root): ElectricityLoadDiagrams::ElectricityLoadDiagrams(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    ElectricityLoadDiagrams::ElectricityLoadDiagrams(const std::string& root, xt::datasets::DataMode mode): ElectricityLoadDiagrams::ElectricityLoadDiagrams(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    ElectricityLoadDiagrams::ElectricityLoadDiagrams(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        ElectricityLoadDiagrams::ElectricityLoadDiagrams(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    ElectricityLoadDiagrams::ElectricityLoadDiagrams(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : ElectricityLoadDiagrams::ElectricityLoadDiagrams(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    ElectricityLoadDiagrams::ElectricityLoadDiagrams(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void ElectricityLoadDiagrams::load_data()
+    {
+
+    }
+
+    void ElectricityLoadDiagrams::check_resources()
+    {
+
+    }
+}
