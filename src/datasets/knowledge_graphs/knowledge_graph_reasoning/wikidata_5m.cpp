28ffd14cd9225447c6c29d536949585d1a3c2f50
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:00:03 +0330

    datasets base files implementation created

---
+++ src/datasets/knowledge_graphs/knowledge_graph_reasoning/wikidata_5m.cpp
@@ -0,0 +1,48 @@
+#include "datasets/knowledge_graphs/knowledge_graph_reasoning/wikidata_5m.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- Wikidata5M ---------------------- //
+
+    Wikidata5M::Wikidata5M(const std::string& root): Wikidata5M::Wikidata5M(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    Wikidata5M::Wikidata5M(const std::string& root, xt::datasets::DataMode mode): Wikidata5M::Wikidata5M(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    Wikidata5M::Wikidata5M(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        Wikidata5M::Wikidata5M(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    Wikidata5M::Wikidata5M(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : Wikidata5M::Wikidata5M(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    Wikidata5M::Wikidata5M(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void Wikidata5M::load_data()
+    {
+
+    }
+
+    void Wikidata5M::check_resources()
+    {
+
+    }
+}
