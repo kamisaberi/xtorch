c1e5070f9b255bb8c8742a1ffc5a19b1ffd3f78d
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:08:54 +0330

    datasets interface files is creating

--- include/datasets/audio_processing/speech_separation/libri_mix.h
+++ include/datasets/audio_processing/speech_separation/libri_mix.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class LibriMix : public xt::datasets::Dataset
+    {
+    public :
+        explicit LibriMix(const std::string& root);
+        LibriMix(const std::string& root, xt::datasets::DataMode mode);
+        LibriMix(const std::string& root, xt::datasets::DataMode mode, bool download);
+        LibriMix(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        LibriMix(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
