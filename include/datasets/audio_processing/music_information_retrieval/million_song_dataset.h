fc61596f9b3fe4e4514627f6e6515d94fd0d0d28
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:02:13 +0330

    datasets interface files is creating

--- include/datasets/audio_processing/music_information_retrieval/million_song_dataset.h
+++ include/datasets/audio_processing/music_information_retrieval/million_song_dataset.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets {
+    class MillionSongDataset : public xt::datasets::Dataset {
+    public :
+        explicit MillionSongDataset(const std::string &root);
+
+        MillionSongDataset(const std::string &root, xt::datasets::DataMode mode);
+
+        MillionSongDataset(const std::string &root, xt::datasets::DataMode mode, bool download);
+
+        MillionSongDataset(const std::string &root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr <xt::Module> transformer);
+
+        MillionSongDataset(const std::string &root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr <xt::Module> transformer,
+                           std::unique_ptr <xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
