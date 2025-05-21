5b07d63114c13ca06d2082b9934d8b0a9cd57f54
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:09:14 +0330

    datasets base files implementation created

--- src/datasets/reinforcement_learning/continuous_control/mu_jo_co_gym.cpp
+++ src/datasets/reinforcement_learning/continuous_control/mu_jo_co_gym.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/reinforcement_learning/continuous_control/mu_jo_co_gym.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- MuJoCo ---------------------- //
+
+    MuJoCo::MuJoCo(const std::string& root): MuJoCo::MuJoCo(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    MuJoCo::MuJoCo(const std::string& root, xt::datasets::DataMode mode): MuJoCo::MuJoCo(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    MuJoCo::MuJoCo(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        MuJoCo::MuJoCo(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    MuJoCo::MuJoCo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : MuJoCo::MuJoCo(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    MuJoCo::MuJoCo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void MuJoCo::load_data()
+    {
+
+    }
+
+    void MuJoCo::check_resources()
+    {
+
+    }
+}
