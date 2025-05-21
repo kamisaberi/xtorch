7382a64d233912e978eeb32a33e67a271c729f71
kamisaberi <kamisaberi@gmail.com>
2025-05-22 00:59:05 +0330

    datasets base files implementation created

--- include/datasets/computer_vision/image_text_pretraining/laion_5b.h
+++ include/datasets/computer_vision/image_text_pretraining/laion_5b.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "datasets/common.h"
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace xt::data::datasets
+{
+    class LAION5B : public xt::datasets::Dataset
+    {
+    public :
+        explicit LAION5B(const std::string& root);
+        LAION5B(const std::string& root, xt::datasets::DataMode mode);
+        LAION5B(const std::string& root, xt::datasets::DataMode mode, bool download);
+        LAION5B(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        LAION5B(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+
+
+    };
+}
