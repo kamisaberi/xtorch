97657b8f0653dc8ab561b2361ba1663d11c4fca2
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:34:53 +0330

    datasets interface files is creating

--- include/datasets/natural_language_processing/text_summarization/cnn_daily_mail.h
+++ include/datasets/natural_language_processing/text_summarization/cnn_daily_mail.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class CNNDailyMail    : public xt::datasets::Dataset
+    {
+    public :
+        explicit CNNDailyMail(const std::string& root);
+        CNNDailyMail(const std::string& root, xt::datasets::DataMode mode);
+        CNNDailyMail(const std::string& root, xt::datasets::DataMode mode, bool download);
+        CNNDailyMail(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        CNNDailyMail(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
