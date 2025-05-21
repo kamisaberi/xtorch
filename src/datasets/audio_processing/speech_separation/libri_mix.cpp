76f15d2bf60bf49870de1cdd1a67f60730c71594
kamisaberi <kamisaberi@gmail.com>
2025-05-22 01:53:08 +0330

    datasets base files implementation created

--- src/datasets/audio_processing/speech_separation/libri_mix.cpp
+++ src/datasets/audio_processing/speech_separation/libri_mix.cpp
@@ -1,4 +1,47 @@
-//
-// Created by pc on 5/11/2025.
-//
+#include "datasets/audio_processing/speech_separation/libri_mix.h"
 
+namespace xt::data::datasets
+{
+    
+    LibriMix::LibriMix(const std::string& root): LibriMix::LibriMix(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    LibriMix::LibriMix(const std::string& root, xt::datasets::DataMode mode): LibriMix::LibriMix(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    LibriMix::LibriMix(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        LibriMix::LibriMix(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    LibriMix::LibriMix(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : LibriMix::LibriMix(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    LibriMix::LibriMix(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void LibriMix::load_data()
+    {
+
+    }
+
+    void LibriMix::check_resources()
+    {
+
+    }
+}
