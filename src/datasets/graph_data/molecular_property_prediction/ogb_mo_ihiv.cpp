ee10cb4420a797876b34900488ab7c84f090d101
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:50:45 +0330

    datasets base files implementation created

--- src/datasets/graph_data/molecular_property_prediction/ogb_mo_ihiv.cpp
+++ src/datasets/graph_data/molecular_property_prediction/ogb_mo_ihiv.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/graph_data/molecular_property_prediction/ogb_mo_ihiv.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- OGBMolHIV ---------------------- //
+
+    OGBMolHIV::OGBMolHIV(const std::string& root): OGBMolHIV::OGBMolHIV(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    OGBMolHIV::OGBMolHIV(const std::string& root, xt::datasets::DataMode mode): OGBMolHIV::OGBMolHIV(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    OGBMolHIV::OGBMolHIV(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        OGBMolHIV::OGBMolHIV(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    OGBMolHIV::OGBMolHIV(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : OGBMolHIV::OGBMolHIV(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    OGBMolHIV::OGBMolHIV(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void OGBMolHIV::load_data()
+    {
+
+    }
+
+    void OGBMolHIV::check_resources()
+    {
+
+    }
+}
