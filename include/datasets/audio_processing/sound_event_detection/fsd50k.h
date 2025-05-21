02af0d3c9251a7b5ce9b4b5aab4aaf255c738026
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:05:10 +0330

    datasets interface files is creating

--- include/datasets/audio_processing/sound_event_detection/fsd50k.h
+++ include/datasets/audio_processing/sound_event_detection/fsd50k.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class FSD50K : public xt::datasets::Dataset
+    {
+    public :
+        explicit FSD50K(const std::string& root);
+        FSD50K(const std::string& root, xt::datasets::DataMode mode);
+        FSD50K(const std::string& root, xt::datasets::DataMode mode, bool download);
+        FSD50K(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        FSD50K(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
