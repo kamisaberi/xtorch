c7d315af9d9ecb860ce71ba2c15fe1631a3f4db8
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 14:53:04 +0330

    datasets interface files is creating

--- include/datasets/computer_vision/image_captioning/coco_captions.h
+++ include/datasets/computer_vision/image_captioning/coco_captions.h
@@ -1,17 +1,34 @@
 #pragma once
-#include "datasets/base/base.h"
+
 #include "datasets/common.h"
 
 
 namespace xt::data::datasets {
-    class CocoCaptions : public BaseDataset {
+    class CocoCaptions : public xt::datasets::Dataset {
     public :
-        explicit  CocoCaptions(const std::string &root);
-        CocoCaptions(const std::string &root, DataMode mode);
-        CocoCaptions(const std::string &root, DataMode mode , bool download);
-        CocoCaptions(const std::string &root, DataMode mode , bool download, TransformType transforms);
+        explicit CocoCaptions(const std::string &root);
 
-    private :
+        CocoCaptions(const std::string &root, xt::datasets::DataMode mode);
+
+        CocoCaptions(const std::string &root, xt::datasets::DataMode mode, bool download);
+
+        CocoCaptions(const std::string &root, xt::datasets::DataMode mode, bool download,
+                     std::unique_ptr <xt::Module> transformer);
+
+        CocoCaptions(const std::string &root, xt::datasets::DataMode mode, bool download,
+                     std::unique_ptr <xt::Module> transformer,
+                     std::unique_ptr <xt::Module> target_transformer);
+
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
