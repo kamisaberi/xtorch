feb32218fa5644813bba579d9ac86aa98bd383f4
kamisaberi <kamisaberi@gmail.com>
2025-05-22 01:45:49 +0330

    datasets base files implementation created

--- src/datasets/audio_processing/pronunciation_dictionary/cmu_dict.cpp
+++ src/datasets/audio_processing/pronunciation_dictionary/cmu_dict.cpp
@@ -1,4 +1,47 @@
-//
-// Created by pc on 5/11/2025.
-//
+#include "datasets/audio_processing/pronunciation_dictionary/cmu_dict.h"
 
+namespace xt::data::datasets
+{
+
+    CMUDict::CMUDict(const std::string& root): CMUDict::CMUDict(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    CMUDict::CMUDict(const std::string& root, xt::datasets::DataMode mode): CMUDict::CMUDict(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    CMUDict::CMUDict(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        CMUDict::CMUDict(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    CMUDict::CMUDict(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : CMUDict::CMUDict(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    CMUDict::CMUDict(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void CMUDict::load_data()
+    {
+
+    }
+
+    void CMUDict::check_resources()
+    {
+
+    }
+}
