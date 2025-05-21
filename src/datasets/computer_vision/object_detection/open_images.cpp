5d2a822be7326218128b8408006c3df12bd3cdb9
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:32:30 +0330

    datasets base files implementation created

--- src/datasets/computer_vision/object_detection/open_images.cpp
+++ src/datasets/computer_vision/object_detection/open_images.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/10/25.
-//
+#include "datasets/computer_vision/object_detection/open_images.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- OpenImages ---------------------- //
+
+    OpenImages::OpenImages(const std::string& root): OpenImages::OpenImages(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    OpenImages::OpenImages(const std::string& root, xt::datasets::DataMode mode): OpenImages::OpenImages(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    OpenImages::OpenImages(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        OpenImages::OpenImages(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    OpenImages::OpenImages(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : OpenImages::OpenImages(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    OpenImages::OpenImages(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void OpenImages::load_data()
+    {
+
+    }
+
+    void OpenImages::check_resources()
+    {
+
+    }
+}
