58bea078ca4c0ef926339851e724d66eed1c73d9
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:17:58 +0330

    datasets interface files is creating

--- include/datasets/knowledge_graphs/knowledge_graph_reasoning/wikidata_5m.h
+++ include/datasets/knowledge_graphs/knowledge_graph_reasoning/wikidata_5m.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets {
+    class Wikidata5M : public xt::datasets::Dataset {
+    public :
+        explicit Wikidata5M(const std::string &root);
+
+        Wikidata5M(const std::string &root, xt::datasets::DataMode mode);
+
+        Wikidata5M(const std::string &root, xt::datasets::DataMode mode, bool download);
+
+        Wikidata5M(const std::string &root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr <xt::Module> transformer);
+
+        Wikidata5M(const std::string &root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr <xt::Module> transformer,
+                   std::unique_ptr <xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
