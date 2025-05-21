189befc3fdab55826bd34977097c90d3e1df0594
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:33:37 +0330

    datasets base files implementation created

--- src/datasets/computer_vision/object_detection/xview.cpp
+++ src/datasets/computer_vision/object_detection/xview.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/10/25.
-//
+#include "datasets/computer_vision/object_detection/xview.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- XView ---------------------- //
+
+    XView::XView(const std::string& root): XView::XView(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    XView::XView(const std::string& root, xt::datasets::DataMode mode): XView::XView(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    XView::XView(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        XView::XView(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    XView::XView(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : XView::XView(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    XView::XView(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void XView::load_data()
+    {
+
+    }
+
+    void XView::check_resources()
+    {
+
+    }
+}
