c0cef93b96208a598022d4bf2e7de58b8289613e
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:23:05 +0330

    datasets base files implementation created

--- src/datasets/natural_language_processing/dialogue_generation/daily_dialog.cpp
+++ src/datasets/natural_language_processing/dialogue_generation/daily_dialog.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/natural_language_processing/dialogue_generation/daily_dialog.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- DailyDialog ---------------------- //
+
+    DailyDialog::DailyDialog(const std::string& root): DailyDialog::DailyDialog(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    DailyDialog::DailyDialog(const std::string& root, xt::datasets::DataMode mode): DailyDialog::DailyDialog(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    DailyDialog::DailyDialog(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        DailyDialog::DailyDialog(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    DailyDialog::DailyDialog(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : DailyDialog::DailyDialog(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    DailyDialog::DailyDialog(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void DailyDialog::load_data()
+    {
+
+    }
+
+    void DailyDialog::check_resources()
+    {
+
+    }
+}
--- src/datasets/natural_language_processing/dialogue_generation/persona_chat.cpp
+++ src/datasets/natural_language_processing/dialogue_generation/persona_chat.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/computer_vision/image_classification/caltech101.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- Caltech101 ---------------------- //
+
+    Caltech101::Caltech101(const std::string& root): Caltech101::Caltech101(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    Caltech101::Caltech101(const std::string& root, xt::datasets::DataMode mode): Caltech101::Caltech101(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    Caltech101::Caltech101(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        Caltech101::Caltech101(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    Caltech101::Caltech101(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : Caltech101::Caltech101(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    Caltech101::Caltech101(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void Caltech101::load_data()
+    {
+
+    }
+
+    void Caltech101::check_resources()
+    {
+
+    }
+}
