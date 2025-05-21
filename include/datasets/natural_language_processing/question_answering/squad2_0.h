960aa3bd19d4618900dbd1a08846389443ce011e
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:24:04 +0330

    datasets interface files is creating

--- include/datasets/natural_language_processing/question_answering/squad2_0.h
+++ include/datasets/natural_language_processing/question_answering/squad2_0.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class SQuAD20 : public xt::datasets::Dataset
+    {
+    public :
+        explicit SQuAD20(const std::string& root);
+        SQuAD20(const std::string& root, xt::datasets::DataMode mode);
+        SQuAD20(const std::string& root, xt::datasets::DataMode mode, bool download);
+        SQuAD20(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        SQuAD20(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
