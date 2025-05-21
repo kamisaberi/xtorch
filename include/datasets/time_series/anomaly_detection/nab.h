86d52f27b57b3f9270d79613b38e333307084a17
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:40:36 +0330

    datasets interface files is creating

--- include/datasets/time_series/anomaly_detection/nab.h
+++ include/datasets/time_series/anomaly_detection/nab.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets {
+    class NAB : public xt::datasets::Dataset {
+    public :
+        explicit NAB(const std::string &root);
+
+        NAB(const std::string &root, xt::datasets::DataMode mode);
+
+        NAB(const std::string &root, xt::datasets::DataMode mode, bool download);
+
+        NAB(const std::string &root, xt::datasets::DataMode mode, bool download,
+            std::unique_ptr <xt::Module> transformer);
+
+        NAB(const std::string &root, xt::datasets::DataMode mode, bool download,
+            std::unique_ptr <xt::Module> transformer,
+            std::unique_ptr <xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
