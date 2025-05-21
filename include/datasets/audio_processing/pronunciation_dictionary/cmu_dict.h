e273d3a2b80b537beeb41e381458155e47f93186
kamran saberifard <kamisaberi@gmail.com>
2025-05-21 15:04:20 +0330

    datasets interface files is creating

--- include/datasets/audio_processing/pronunciation_dictionary/cmu_dict.h
+++ include/datasets/audio_processing/pronunciation_dictionary/cmu_dict.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class CMUDict : public xt::datasets::Dataset
+    {
+    public :
+        explicit CMUDict(const std::string& root);
+        CMUDict(const std::string& root, xt::datasets::DataMode mode);
+        CMUDict(const std::string& root, xt::datasets::DataMode mode, bool download);
+        CMUDict(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        CMUDict(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
