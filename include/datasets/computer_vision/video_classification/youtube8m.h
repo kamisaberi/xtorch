6673f250b30cad45f9673bf4f7aa962b16b51162
kamisaberi <kamisaberi@gmail.com>
2025-05-22 01:19:39 +0330

    datasets base files implementation created

--- include/datasets/computer_vision/video_classification/youtube8m.h
+++ include/datasets/computer_vision/video_classification/youtube8m.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "datasets/common.h"
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace xt::data::datasets
+{
+    class YouTube8M : public xt::datasets::Dataset
+    {
+    public :
+        explicit YouTube8M(const std::string& root);
+        YouTube8M(const std::string& root, xt::datasets::DataMode mode);
+        YouTube8M(const std::string& root, xt::datasets::DataMode mode, bool download);
+        YouTube8M(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        YouTube8M(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+
+
+    };
+}
