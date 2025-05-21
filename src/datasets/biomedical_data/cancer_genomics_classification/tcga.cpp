251ca461aed63f6b46afbd4f836718e4b9b1f1f3
kamisaberi <kamisaberi@gmail.com>
2025-05-22 02:11:12 +0330

    datasets base files implementation created

--- src/datasets/biomedical_data/cancer_genomics_classification/tcga.cpp
+++ src/datasets/biomedical_data/cancer_genomics_classification/tcga.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/biomedical_data/cancer_genomics_classification/tcga.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- TCGA ---------------------- //
+
+    TCGA::TCGA(const std::string& root): TCGA::TCGA(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    TCGA::TCGA(const std::string& root, xt::datasets::DataMode mode): TCGA::TCGA(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    TCGA::TCGA(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        TCGA::TCGA(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    TCGA::TCGA(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : TCGA::TCGA(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    TCGA::TCGA(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void TCGA::load_data()
+    {
+
+    }
+
+    void TCGA::check_resources()
+    {
+
+    }
+}
