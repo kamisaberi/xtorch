09f69658d992e4bdec64b3576f2caded7296ba61
kamisaberi <kamisaberi@gmail.com>
2025-05-22 01:55:51 +0330

    datasets base files implementation created

--- src/datasets/audio_processing/speech_synthesis/vctk_092.cpp
+++ src/datasets/audio_processing/speech_synthesis/vctk_092.cpp
@@ -1,4 +1,48 @@
-//
-// Created by pc on 5/11/2025.
-//
+#include "datasets/audio_processing/speech_synthesis/vctk_092.h"
 
+namespace xt::data::datasets
+{
+    // ---------------------- VCTK092 ---------------------- //
+
+    VCTK092::VCTK092(const std::string& root): VCTK092::VCTK092(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    VCTK092::VCTK092(const std::string& root, xt::datasets::DataMode mode): VCTK092::VCTK092(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    VCTK092::VCTK092(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        VCTK092::VCTK092(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    VCTK092::VCTK092(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : VCTK092::VCTK092(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    VCTK092::VCTK092(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void VCTK092::load_data()
+    {
+
+    }
+
+    void VCTK092::check_resources()
+    {
+
+    }
+}
