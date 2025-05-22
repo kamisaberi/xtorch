296fd1787cd7422e4dfaa94ea15374089d3bf87c
kamisaberi <kamisaberi@gmail.com>
2025-05-23 03:26:40 +0330

    datasets base files changed

--- include/datasets/audio_processing/speaker_identification_and_verification/vox_celeb.h
+++ include/datasets/audio_processing/speaker_identification_and_verification/vox_celeb.h
@@ -1,18 +1,34 @@
 #pragma once
-#include "datasets/base/base.h"
+
 #include "datasets/common.h"
 
+using namespace std;
+namespace fs = std::filesystem;
 
-namespace xt::data::datasets {
-    class VoxCeleb : BaseDataset {
-        public :
-            explicit VoxCeleb(const std::string &root);
-        VoxCeleb(const std::string &root, DataMode mode);
-        VoxCeleb(const std::string &root, DataMode mode , bool download);
-        VoxCeleb(const std::string &root, DataMode mode , bool download, TransformType transforms);
 
-        private :
-            void load_data();
+namespace xt::data::datasets
+{
+    class VoxCeleb : xt::datasets::Dataset
+    {
+    public :
+        explicit VoxCeleb(const std::string& root);
+        VoxCeleb(const std::string& root, xt::datasets::DataMode mode);
+        VoxCeleb(const std::string& root, xt::datasets::DataMode mode, bool download);
+        VoxCeleb(const std::string& root, xt::datasets::DataMode mode, bool download,
+                 std::unique_ptr<xt::Module> transformer);
+        VoxCeleb(const std::string& root, xt::datasets::DataMode mode, bool download,
+                 std::unique_ptr<xt::Module> transformer,
+                 std::unique_ptr<xt::Module> target_transformer);
+
+    private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
 
         void check_resources();
     };
--- src/datasets/audio_processing/speaker_identification_and_verification/vox_celeb.cpp
+++ src/datasets/audio_processing/speaker_identification_and_verification/vox_celeb.cpp
@@ -1,22 +1,48 @@
-#include "../../../../include/datasets/audio_processing/speaker_identification_and_verification/vox_celeb.h"
+#include "datasets/audio_processing/speaker_identification_and_verification/vox_celeb.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- VoxCeleb ---------------------- //
 
-    VoxCeleb::VoxCeleb(const std::string &root): VoxCeleb::VoxCeleb(root, DataMode::TRAIN, false) {
+    VoxCeleb::VoxCeleb(const std::string& root): VoxCeleb::VoxCeleb(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    VoxCeleb::VoxCeleb(const std::string &root, DataMode mode): VoxCeleb::VoxCeleb(root, mode, false) {
+    VoxCeleb::VoxCeleb(const std::string& root, xt::datasets::DataMode mode): VoxCeleb::VoxCeleb(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    VoxCeleb::VoxCeleb(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("VoxCeleb: VoxCeleb not implemented");
+    VoxCeleb::VoxCeleb(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        VoxCeleb::VoxCeleb(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    VoxCeleb::VoxCeleb(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : VoxCeleb::VoxCeleb(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    VoxCeleb::VoxCeleb(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    VoxCeleb::VoxCeleb(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("VoxCeleb: VoxCeleb not implemented");
+    void VoxCeleb::load_data()
+    {
+
     }
 
+    void VoxCeleb::check_resources()
+    {
 
+    }
 }
