fa4e69fd69ad1b5530b3ff4c293cb871f7c19891
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:32:30 +0330

    datasets base files changed

--- src/datasets/computer_vision/stereo_matching/kitti_2015_stereo.cpp
+++ src/datasets/computer_vision/stereo_matching/kitti_2015_stereo.cpp
@@ -1,24 +1,48 @@
 #include "datasets/computer_vision/stereo_matching/kitti_2015_stereo.h"
 
-namespace xt::data::datasets {
-
-
+namespace xt::data::datasets
+{
     // ---------------------- Kitti2015Stereo ---------------------- //
-    Kitti2015Stereo::Kitti2015Stereo(const std::string &root): Kitti2015Stereo::Kitti2015Stereo(root, DataMode::TRAIN, false) {
+
+    Kitti2015Stereo::Kitti2015Stereo(const std::string& root): Kitti2015Stereo::Kitti2015Stereo(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    Kitti2015Stereo::Kitti2015Stereo(const std::string &root, DataMode mode): Kitti2015Stereo::Kitti2015Stereo(root, mode, false) {
+    Kitti2015Stereo::Kitti2015Stereo(const std::string& root, xt::datasets::DataMode mode): Kitti2015Stereo::Kitti2015Stereo(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    Kitti2015Stereo::Kitti2015Stereo(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Kitti2015Stereo: Kitti2015Stereo not implemented");
+    Kitti2015Stereo::Kitti2015Stereo(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        Kitti2015Stereo::Kitti2015Stereo(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    Kitti2015Stereo::Kitti2015Stereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : Kitti2015Stereo::Kitti2015Stereo(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    Kitti2015Stereo::Kitti2015Stereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    Kitti2015Stereo::Kitti2015Stereo(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Kitti2015Stereo: Kitti2015Stereo not implemented");
+    void Kitti2015Stereo::load_data()
+    {
+
     }
 
+    void Kitti2015Stereo::check_resources()
+    {
 
+    }
 }
