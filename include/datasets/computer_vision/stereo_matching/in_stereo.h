d2ee7193a5a5c8fdabb620fc85150bd565265265
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:21:36 +0330

    datasets base files changed

--- include/datasets/computer_vision/stereo_matching/in_stereo.h
+++ include/datasets/computer_vision/stereo_matching/in_stereo.h
@@ -1,8 +1,14 @@
 #pragma once
-#include "datasets/base/base.h"
 
-namespace xt::data::datasets {
-    class InStereo2k : BaseDataset {
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class InStereo2k : xt::datasets::Dataset
+    {
         /*
         """`InStereo2k <https://github.com/YuhuaXu/StereoDataset>`_ dataset.
 
@@ -36,13 +42,25 @@ namespace xt::data::datasets {
         """
          */
     public :
-        explicit  InStereo2k(const std::string &root);
-        InStereo2k(const std::string &root, DataMode mode);
-        InStereo2k(const std::string &root, DataMode mode , bool download);
-        InStereo2k(const std::string &root, DataMode mode , bool download, TransformType transforms);
+        explicit InStereo2k(const std::string& root);
+        InStereo2k(const std::string& root, xt::datasets::DataMode mode);
+        InStereo2k(const std::string& root, xt::datasets::DataMode mode, bool download);
+        InStereo2k(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        InStereo2k(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
 
     private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
+
         void check_resources();
     };
 }
