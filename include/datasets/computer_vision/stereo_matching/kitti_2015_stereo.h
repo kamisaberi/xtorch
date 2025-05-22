6c6f5e4c04f9368de94e0d4b2b8b2066585bf36f
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:31:57 +0330

    datasets base files changed

--- include/datasets/computer_vision/stereo_matching/kitti_2015_stereo.h
+++ include/datasets/computer_vision/stereo_matching/kitti_2015_stereo.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#include "datasets/base/base.h"
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
 
 namespace xt::data::datasets {
-    class Kitti2015Stereo : BaseDataset {
+    class Kitti2015Stereo : xt::datasets::Dataset {
         /*
         """
     KITTI dataset from the `2015 stereo evaluation benchmark <http://www.cvlibs.net/datasets/kitti/eval_scene_flow.php>`_.
@@ -48,15 +51,28 @@ namespace xt::data::datasets {
 
          */
     public :
-        explicit Kitti2015Stereo(const std::string &root);
-        Kitti2015Stereo(const std::string &root, DataMode mode);
-        Kitti2015Stereo(const std::string &root, DataMode mode , bool download);
-        Kitti2015Stereo(const std::string &root, DataMode mode , bool download, TransformType transforms);
+        explicit Kitti2015Stereo(const std::string& root);
+        Kitti2015Stereo(const std::string& root, xt::datasets::DataMode mode);
+        Kitti2015Stereo(const std::string& root, xt::datasets::DataMode mode, bool download);
+        Kitti2015Stereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        Kitti2015Stereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
 
 
     private :
+
+        // TODO fs::path dataset_folder_name
+fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
+
     };
 }
