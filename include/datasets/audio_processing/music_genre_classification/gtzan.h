8acb28ba6ae4eafcf9b7eef978597e1f611b00ba
kamisaberi <kamisaberi@gmail.com>
2025-05-23 03:15:35 +0330

    datasets base files changed

--- include/datasets/audio_processing/music_genre_classification/gtzan.h
+++ include/datasets/audio_processing/music_genre_classification/gtzan.h
@@ -1,21 +1,33 @@
 #pragma once
 
-#include "datasets/base/base.h"
 #include "datasets/common.h"
 
+using namespace std;
+namespace fs = std::filesystem;
 
-namespace xt::data::datasets {
-    class GTZAN : BaseDataset {
+
+namespace xt::data::datasets
+{
+    class GTZAN : xt::datasets::Dataset
+    {
     public :
-        explicit GTZAN(const std::string &root);
-
-        GTZAN(const std::string &root, DataMode mode);
-
-        GTZAN(const std::string &root, DataMode mode, bool download);
-
-        GTZAN(const std::string &root, DataMode mode, bool download, TransformType transforms);
+        explicit GTZAN(const std::string& root);
+        GTZAN(const std::string& root, xt::datasets::DataMode mode);
+        GTZAN(const std::string& root, xt::datasets::DataMode mode, bool download);
+        GTZAN(const std::string& root, xt::datasets::DataMode mode, bool download,
+              std::unique_ptr<xt::Module> transformer);
+        GTZAN(const std::string& root, xt::datasets::DataMode mode, bool download,
+              std::unique_ptr<xt::Module> transformer,
+              std::unique_ptr<xt::Module> target_transformer);
 
     private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
