b98f725ff0b089194f743e36234b12c9d07f8584
kamisaberi <kamisaberi@gmail.com>
2025-05-22 03:38:31 +0330

    datasets base files implementation created

--- src/datasets/natural_language_processing/unsupervised_learning/cc100.cpp
+++ src/datasets/natural_language_processing/unsupervised_learning/cc100.cpp
@@ -1,3 +1,48 @@
-//
-// Created by kami on 5/12/25.
-//
+#include "datasets/natural_language_processing/unsupervised_learning/cc100.h"
+
+namespace xt::data::datasets
+{
+    // ---------------------- CC100 ---------------------- //
+
+    CC100::CC100(const std::string& root): CC100::CC100(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
+    }
+
+    CC100::CC100(const std::string& root, xt::datasets::DataMode mode): CC100::CC100(
+        root, mode, false, nullptr, nullptr)
+    {
+    }
+
+    CC100::CC100(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        CC100::CC100(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    CC100::CC100(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : CC100::CC100(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    CC100::CC100(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
+    }
+
+
+    void CC100::load_data()
+    {
+
+    }
+
+    void CC100::check_resources()
+    {
+
+    }
+}
