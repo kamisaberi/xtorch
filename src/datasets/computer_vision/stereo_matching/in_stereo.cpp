7a6e3fa3e4375f192ab615a2fa8cb12222edb0cf
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:21:36 +0330

    datasets base files changed

--- src/datasets/computer_vision/stereo_matching/in_stereo.cpp
+++ src/datasets/computer_vision/stereo_matching/in_stereo.cpp
@@ -1,22 +1,48 @@
 #include "datasets/computer_vision/stereo_matching/in_stereo.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- InStereo2k ---------------------- //
 
-    InStereo2k::InStereo2k(const std::string &root): InStereo2k::InStereo2k(root, DataMode::TRAIN, false) {
+    InStereo2k::InStereo2k(const std::string& root): InStereo2k::InStereo2k(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    InStereo2k::InStereo2k(const std::string &root, DataMode mode): InStereo2k::InStereo2k(root, mode, false) {
+    InStereo2k::InStereo2k(const std::string& root, xt::datasets::DataMode mode): InStereo2k::InStereo2k(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    InStereo2k::InStereo2k(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("InStereo2k: InStereo2k not implemented");
+    InStereo2k::InStereo2k(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        InStereo2k::InStereo2k(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    InStereo2k::InStereo2k(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : InStereo2k::InStereo2k(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    InStereo2k::InStereo2k(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    InStereo2k::InStereo2k(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("InStereo2k: InStereo2k not implemented");
+    void InStereo2k::load_data()
+    {
+
     }
 
+    void InStereo2k::check_resources()
+    {
 
+    }
 }
