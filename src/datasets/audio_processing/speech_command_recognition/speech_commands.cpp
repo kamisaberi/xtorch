4514e4ab91c931a0d3ad4fd3c3ecd763f9e49dfe
kamisaberi <kamisaberi@gmail.com>
2025-05-23 03:29:29 +0330

    datasets base files changed

--- src/datasets/audio_processing/speech_command_recognition/speech_commands.cpp
+++ src/datasets/audio_processing/speech_command_recognition/speech_commands.cpp
@@ -1,22 +1,48 @@
-#include "../../../../include/datasets/audio_processing/speech_command_recognition/speech_commands.h"
+#include "datasets/audio_processing/speech_command_recognition/speech_commands.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- SpeechCommands ---------------------- //
 
-    SpeechCommands::SpeechCommands(const std::string &root): SpeechCommands::SpeechCommands(root, DataMode::TRAIN, false) {
+    SpeechCommands::SpeechCommands(const std::string& root): SpeechCommands::SpeechCommands(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    SpeechCommands::SpeechCommands(const std::string &root, DataMode mode): SpeechCommands::SpeechCommands(root, mode, false) {
+    SpeechCommands::SpeechCommands(const std::string& root, xt::datasets::DataMode mode): SpeechCommands::SpeechCommands(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    SpeechCommands::SpeechCommands(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("SpeechCommands: SpeechCommands not implemented");
+    SpeechCommands::SpeechCommands(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        SpeechCommands::SpeechCommands(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    SpeechCommands::SpeechCommands(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : SpeechCommands::SpeechCommands(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    SpeechCommands::SpeechCommands(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    SpeechCommands::SpeechCommands(const std::string &root, DataMode mode, bool download,
-                         TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("SpeechCommands: SpeechCommands not implemented");
+    void SpeechCommands::load_data()
+    {
+
     }
 
+    void SpeechCommands::check_resources()
+    {
 
+    }
 }
