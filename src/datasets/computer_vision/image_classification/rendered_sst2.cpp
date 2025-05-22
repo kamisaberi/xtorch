3ba8cd907e0185d68e16beb9c23c6b8157a47142
kamisaberi <kamisaberi@gmail.com>
2025-05-23 01:25:25 +0330

    datasets base files changed

--- src/datasets/computer_vision/image_classification/rendered_sst2.cpp
+++ src/datasets/computer_vision/image_classification/rendered_sst2.cpp
@@ -1,22 +1,48 @@
 #include "datasets/computer_vision/image_classification/rendered_sst2.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- RenderedSST2 ---------------------- //
 
-    RenderedSST2::RenderedSST2(const std::string &root): RenderedSST2::RenderedSST2(root, DataMode::TRAIN, false) {
+    RenderedSST2::RenderedSST2(const std::string& root): RenderedSST2::RenderedSST2(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    RenderedSST2::RenderedSST2(const std::string &root, DataMode mode): RenderedSST2::RenderedSST2(root, mode, false) {
+    RenderedSST2::RenderedSST2(const std::string& root, xt::datasets::DataMode mode): RenderedSST2::RenderedSST2(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    RenderedSST2::RenderedSST2(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("RenderedSST2: RenderedSST2 not implemented");
+    RenderedSST2::RenderedSST2(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        RenderedSST2::RenderedSST2(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    RenderedSST2::RenderedSST2(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : RenderedSST2::RenderedSST2(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    RenderedSST2::RenderedSST2(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    RenderedSST2::RenderedSST2(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("RenderedSST2: RenderedSST2 not implemented");
+    void RenderedSST2::load_data()
+    {
+
     }
 
+    void RenderedSST2::check_resources()
+    {
 
+    }
 }
