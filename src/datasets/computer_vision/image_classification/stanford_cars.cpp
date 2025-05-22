31eba12ee5edbd8e2b3075e2a24546c0e2cadf05
kamisaberi <kamisaberi@gmail.com>
2025-05-23 01:35:23 +0330

    datasets base files changed

--- src/datasets/computer_vision/image_classification/stanford_cars.cpp
+++ src/datasets/computer_vision/image_classification/stanford_cars.cpp
@@ -1,22 +1,48 @@
 #include "datasets/computer_vision/image_classification/stanford_cars.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- StanfordCars ---------------------- //
 
-    StanfordCars::StanfordCars(const std::string &root): StanfordCars::StanfordCars(root, DataMode::TRAIN, false) {
+    StanfordCars::StanfordCars(const std::string& root): StanfordCars::StanfordCars(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    StanfordCars::StanfordCars(const std::string &root, DataMode mode): StanfordCars::StanfordCars(root, mode, false) {
+    StanfordCars::StanfordCars(const std::string& root, xt::datasets::DataMode mode): StanfordCars::StanfordCars(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    StanfordCars::StanfordCars(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("StanfordCars: StanfordCars not implemented");
+    StanfordCars::StanfordCars(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        StanfordCars::StanfordCars(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    StanfordCars::StanfordCars(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : StanfordCars::StanfordCars(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    StanfordCars::StanfordCars(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    StanfordCars::StanfordCars(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("StanfordCars: StanfordCars not implemented");
+    void StanfordCars::load_data()
+    {
+
     }
 
+    void StanfordCars::check_resources()
+    {
 
+    }
 }
