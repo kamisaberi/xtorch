5db8d478f46d86bb1a325208d2c29e41e883690e
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:04:58 +0330

    datasets base files changed

--- include/datasets/computer_vision/stereo_matching/cre_stereo.h
+++ include/datasets/computer_vision/stereo_matching/cre_stereo.h
@@ -1,22 +1,36 @@
 #pragma once
 
+#include "datasets/common.h"
 
-#include "datasets/base/base.h"
+using namespace std;
+namespace fs = std::filesystem;
 
 namespace xt::data::datasets {
 
 
-    class CREStereo : public BaseDataset {
+    class CREStereo : public xt::datasets::Dataset {
     public :
-        explicit  CREStereo(const std::string &root);
-        CREStereo(const std::string &root, DataMode mode);
-        CREStereo(const std::string &root, DataMode mode , bool download);
-        CREStereo(const std::string &root, DataMode mode , bool download, TransformType transforms);
+        explicit CREStereo(const std::string& root);
+        CREStereo(const std::string& root, xt::datasets::DataMode mode);
+        CREStereo(const std::string& root, xt::datasets::DataMode mode, bool download);
+        CREStereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        CREStereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
 
 
     private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
+
     };
 }
