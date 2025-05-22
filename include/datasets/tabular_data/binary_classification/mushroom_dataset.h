a5e1185b6cee6cbf83f748f26bdf8ad940b0869a
kamisaberi <kamisaberi@gmail.com>
2025-05-22 04:02:08 +0330

    datasets base files implementation created

--- include/datasets/tabular_data/binary_classification/mushroom_dataset.h
+++ include/datasets/tabular_data/binary_classification/mushroom_dataset.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class ModelNet40 : public xt::datasets::Dataset
+    {
+    public :
+        explicit ModelNet40(const std::string& root);
+        ModelNet40(const std::string& root, xt::datasets::DataMode mode);
+        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download);
+        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private:
+
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
+
+        void check_resources();
+    };
+}
