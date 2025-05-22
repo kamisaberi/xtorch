e8a75141812f2a78774bcb93a4bc147874d3d3df
kamisaberi <kamisaberi@gmail.com>
2025-05-22 23:07:45 +0330

    datasets base files changed

--- src/datasets/computer_vision/image_classification/fake_data.cpp
+++ src/datasets/computer_vision/image_classification/fake_data.cpp
@@ -1,48 +1,42 @@
 #include "datasets/computer_vision/image_classification/fake_data.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
     // ---------------------- FakeData ---------------------- //
 
-    FakeData::FakeData(const std::string& root): FakeData::FakeData(
-        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    FakeData::FakeData(size_t size): FakeData::FakeData(
+        size, xt::datasets::DataMode::TRAIN, {3, 24, 24}, nullptr, nullptr)
     {
     }
 
-    FakeData::FakeData(const std::string& root, xt::datasets::DataMode mode): FakeData::FakeData(
-        root, mode, false, nullptr, nullptr)
+    FakeData::FakeData(size_t size, xt::datasets::DataMode mode): FakeData::FakeData(
+        size, mode, {3, 24, 24}, nullptr, nullptr)
     {
     }
 
-    FakeData::FakeData(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        FakeData::FakeData(
-            root, mode, download, nullptr, nullptr)
+    FakeData::FakeData(size_t size, xt::datasets::DataMode mode, vector<int64_t> shape) : FakeData::FakeData(
+        size, mode, shape, nullptr, nullptr)
     {
     }
 
-    FakeData::FakeData(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : FakeData::FakeData(
-        root, mode, download, std::move(transformer), nullptr)
+
+    FakeData::FakeData(size_t size, xt::datasets::DataMode mode, vector<int64_t> shape,
+                       std::unique_ptr<xt::Module> transformer) : FakeData::FakeData(
+        size, mode, shape, std::move(transformer), nullptr)
     {
     }
 
-    FakeData::FakeData(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+    FakeData::FakeData(size_t size, xt::datasets::DataMode mode, vector<int64_t> shape,
+                       std::unique_ptr<xt::Module> transformer,
+                       std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
-        check_resources();
-        load_data();
-
+        generate_data();
     }
 
 
-    void FakeData::load_data()
+
+    void FakeData::generate_data()
     {
-
     }
-
-    void FakeData::check_resources()
-    {
-
-    }
-
 }
