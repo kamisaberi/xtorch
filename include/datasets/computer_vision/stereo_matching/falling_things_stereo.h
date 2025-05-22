278bc34b674a435bb97975c88f0769073e4a4271
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:11:38 +0330

    datasets base files changed

--- include/datasets/computer_vision/stereo_matching/falling_things_stereo.h
+++ include/datasets/computer_vision/stereo_matching/falling_things_stereo.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#include "datasets/base/base.h"
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
 
 namespace xt::data::datasets {
-    class FallingThingsStereo : public BaseDataset {
+    class FallingThingsStereo : public xt::datasets::Dataset {
         /*
         `FallingThings <https://research.nvidia.com/publication/2018-06_falling-things-synthetic-dataset-3d-object-detection-and-pose-estimation>`_ dataset.
         The dataset is expected to have the following structure: ::
@@ -42,17 +45,32 @@ namespace xt::data::datasets {
                     ...
          */
     public :
-        explicit  FallingThingsStereo(const std::string &root);
-        FallingThingsStereo(const std::string &root, DataMode mode);
-        FallingThingsStereo(const std::string &root, DataMode mode , bool download);
-        FallingThingsStereo(const std::string &root, DataMode mode , bool download, TransformType transforms);
+
+        explicit FallingThingsStereo(const std::string& root);
+        FallingThingsStereo(const std::string& root, xt::datasets::DataMode mode);
+        FallingThingsStereo(const std::string& root, xt::datasets::DataMode mode, bool download);
+        FallingThingsStereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        FallingThingsStereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
 
     private :
         fs::path  url ="https://drive.google.com/open?id=1y4h9T6D9rf6dAmsRwEtfzJdcghCnI_01";
         fs::path dataset_file_name = "fat.zip";
         std::string dataset_file_md5 = "????????????????????????????????";
         fs::path dataset_folder_name = "FallingThings";
+
+
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
+
         void check_resources();
+
     };
 }
