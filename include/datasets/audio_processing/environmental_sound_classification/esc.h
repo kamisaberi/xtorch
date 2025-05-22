29b6c8ea2e02dfcc0572bd45196dab91d4a7916b
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:51:58 +0330

    datasets base files changed

--- include/datasets/audio_processing/environmental_sound_classification/esc.h
+++ include/datasets/audio_processing/environmental_sound_classification/esc.h
@@ -1,21 +1,33 @@
 #pragma once
 
-#include "datasets/base/base.h"
 #include "datasets/common.h"
 
+using namespace std;
+namespace fs = std::filesystem;
 
-namespace xt::data::datasets {
-    class ESC : BaseDataset {
+
+namespace xt::data::datasets
+{
+    class ESC : xt::datasets::Dataset
+    {
     public :
-        explicit ESC(const std::string &root);
-
-        ESC(const std::string &root, DataMode mode);
-
-        ESC(const std::string &root, DataMode mode, bool download);
-
-        ESC(const std::string &root, DataMode mode, bool download, TransformType transforms);
+        explicit ESC(const std::string& root);
+        ESC(const std::string& root, xt::datasets::DataMode mode);
+        ESC(const std::string& root, xt::datasets::DataMode mode, bool download);
+        ESC(const std::string& root, xt::datasets::DataMode mode, bool download,
+            std::unique_ptr<xt::Module> transformer);
+        ESC(const std::string& root, xt::datasets::DataMode mode, bool download,
+            std::unique_ptr<xt::Module> transformer,
+            std::unique_ptr<xt::Module> target_transformer);
 
     private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
