1ee1d87df8d0e19df5aca22a1f8d29585916b204
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:54:30 +0330

    datasets base files changed

--- src/datasets/audio_processing/environmental_sound_classification/urban_sound.cpp
+++ src/datasets/audio_processing/environmental_sound_classification/urban_sound.cpp
@@ -1,22 +1,48 @@
-#include "../../../../include/datasets/audio_processing/environmental_sound_classification/urban_sound.h"
+#include "datasets/audio_processing/environmental_sound_classification/urban_sound.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- UrbanSound ---------------------- //
 
-    UrbanSound::UrbanSound(const std::string &root): UrbanSound::UrbanSound(root, DataMode::TRAIN, false) {
+    UrbanSound::UrbanSound(const std::string& root): UrbanSound::UrbanSound(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    UrbanSound::UrbanSound(const std::string &root, DataMode mode): UrbanSound::UrbanSound(root, mode, false) {
+    UrbanSound::UrbanSound(const std::string& root, xt::datasets::DataMode mode): UrbanSound::UrbanSound(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    UrbanSound::UrbanSound(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("UrbanSound: UrbanSound not implemented");
+    UrbanSound::UrbanSound(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        UrbanSound::UrbanSound(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    UrbanSound::UrbanSound(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : UrbanSound::UrbanSound(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    UrbanSound::UrbanSound(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    UrbanSound::UrbanSound(const std::string &root, DataMode mode, bool download,
-                         TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("UrbanSound: UrbanSound not implemented");
+    void UrbanSound::load_data()
+    {
+
     }
 
+    void UrbanSound::check_resources()
+    {
 
+    }
 }
