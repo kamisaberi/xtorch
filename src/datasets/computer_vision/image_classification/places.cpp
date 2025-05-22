6b8a3949dfa3a08e8009f142f27c196fc97b74ab
kamisaberi <kamisaberi@gmail.com>
2025-05-23 01:23:18 +0330

    datasets base files changed

--- src/datasets/computer_vision/image_classification/places.cpp
+++ src/datasets/computer_vision/image_classification/places.cpp
@@ -1,23 +1,48 @@
 #include "datasets/computer_vision/image_classification/places.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- Places365 ---------------------- //
 
-    Places365::Places365(const std::string &root): Places365::Places365(root, DataMode::TRAIN, false) {
+    Places365::Places365(const std::string& root): Places365::Places365(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    Places365::Places365(const std::string &root, DataMode mode): Places365::Places365(root, mode, false) {
+    Places365::Places365(const std::string& root, xt::datasets::DataMode mode): Places365::Places365(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    Places365::Places365(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Places365: Places365 not implemented");
+    Places365::Places365(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        Places365::Places365(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    Places365::Places365(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : Places365::Places365(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    Places365::Places365(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    Places365::Places365(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Places365: Places365 not implemented");
+    void Places365::load_data()
+    {
+
     }
 
+    void Places365::check_resources()
+    {
 
-
+    }
 }
