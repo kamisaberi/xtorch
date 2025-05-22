9cd8a5287e4e38a7752c384845e8b4d89ca04224
kamisaberi <kamisaberi@gmail.com>
2025-05-23 02:43:05 +0330

    datasets base files changed

--- src/datasets/computer_vision/stereo_matching/sintel_stereo.cpp
+++ src/datasets/computer_vision/stereo_matching/sintel_stereo.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/"
+#include "datasets/computer_vision/stereo_matching/sintel_stereo.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- SintelStereo ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    SintelStereo::SintelStereo(const std::string& root): SintelStereo::SintelStereo(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    SintelStereo::SintelStereo(const std::string& root, xt::datasets::DataMode mode): SintelStereo::SintelStereo(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    SintelStereo::SintelStereo(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        SintelStereo::SintelStereo(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    SintelStereo::SintelStereo(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : SintelStereo::SintelStereo(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    SintelStereo::SintelStereo(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void SintelStereo::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void SintelStereo::check_resources()
     {
 
     }
