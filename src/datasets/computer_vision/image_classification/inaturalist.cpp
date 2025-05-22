f999c6fcd58fea3cccf39cf0e16f34721bf5d74a
kamisaberi <kamisaberi@gmail.com>
2025-05-23 01:11:46 +0330

    datasets base files changed

--- src/datasets/computer_vision/image_classification/inaturalist.cpp
+++ src/datasets/computer_vision/image_classification/inaturalist.cpp
@@ -1,21 +1,48 @@
 #include "datasets/computer_vision/image_classification/inaturalist.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- INaturalist ---------------------- //
 
-    INaturalist::INaturalist(const std::string &root): INaturalist::INaturalist(root, DataMode::TRAIN, false) {
+    INaturalist::INaturalist(const std::string& root): INaturalist::INaturalist(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    INaturalist::INaturalist(const std::string &root, DataMode mode): INaturalist::INaturalist(root, mode, false) {
+    INaturalist::INaturalist(const std::string& root, xt::datasets::DataMode mode): INaturalist::INaturalist(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    INaturalist::INaturalist(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("INaturalist: INaturalist not implemented");
+    INaturalist::INaturalist(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        INaturalist::INaturalist(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    INaturalist::INaturalist(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : INaturalist::INaturalist(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    INaturalist::INaturalist(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    INaturalist::INaturalist(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("INaturalist: INaturalist not implemented");
+    void INaturalist::load_data()
+    {
+
     }
 
+    void INaturalist::check_resources()
+    {
+
+    }
 }
