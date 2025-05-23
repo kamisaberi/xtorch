a555e46c5a81b85a8cbd01b81fc4747aa630be7e
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:53:05 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/sst2.cpp
+++ src/datasets/natural_language_processing/text_classification/sst2.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/sst2.h"
 
-namespace xt::data::datasets {
 
-    SST2::SST2(const std::string &root): SST2::SST2(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- SST2 ---------------------- //
+
+    SST2::SST2(const std::string& root): SST2::SST2(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    SST2::SST2(const std::string &root, DataMode mode): SST2::SST2(root, mode, false) {
+    SST2::SST2(const std::string& root, xt::datasets::DataMode mode): SST2::SST2(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    SST2::SST2(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("SST: SST not implemented");
+    SST2::SST2(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        SST2::SST2(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    SST2::SST2(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : SST2::SST2(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    SST2::SST2(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    SST2::SST2(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("SST: SST not implemented");
+    void SST2::load_data()
+    {
+
     }
 
+    void SST2::check_resources()
+    {
 
+    }
 }
