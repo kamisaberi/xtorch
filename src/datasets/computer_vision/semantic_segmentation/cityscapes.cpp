41b0cf696924c6da3793c6c35175bdf7787004b9
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:46:08 +0330

    datasets base files changed

--- src/datasets/computer_vision/semantic_segmentation/cityscapes.cpp
+++ src/datasets/computer_vision/semantic_segmentation/cityscapes.cpp
@@ -1,21 +1,49 @@
 #include "datasets/computer_vision/semantic_segmentation/cityscapes.h"
 
-namespace xt::data::datasets {
 
-    Cityscapes::Cityscapes(const std::string &root): Cityscapes::Cityscapes(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- Cityscapes ---------------------- //
+
+    Cityscapes::Cityscapes(const std::string& root): Cityscapes::Cityscapes(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    Cityscapes::Cityscapes(const std::string &root, DataMode mode): Cityscapes::Cityscapes(root, mode, false) {
+    Cityscapes::Cityscapes(const std::string& root, xt::datasets::DataMode mode): Cityscapes::Cityscapes(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    Cityscapes::Cityscapes(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Cityscapes: Cityscapes not implemented");
+    Cityscapes::Cityscapes(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        Cityscapes::Cityscapes(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    Cityscapes::Cityscapes(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : Cityscapes::Cityscapes(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    Cityscapes::Cityscapes(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    Cityscapes::Cityscapes(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Cityscapes: Cityscapes not implemented");
+    void Cityscapes::load_data()
+    {
+
     }
 
+    void Cityscapes::check_resources()
+    {
+
+    }
 }
