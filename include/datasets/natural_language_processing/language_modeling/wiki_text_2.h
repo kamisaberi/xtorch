e08d161750aef6bdc3e698575c1174b638751390
kamisaberi <kamisaberi@gmail.com>
2025-05-23 06:04:25 +0330

    datasets base files changed

--- include/datasets/natural_language_processing/language_modeling/wiki_text_2.h
+++ include/datasets/natural_language_processing/language_modeling/wiki_text_2.h
@@ -1,17 +1,33 @@
 #pragma once
 
-#include "datasets/base/base.h"
+#include "datasets/common.h"
 
-namespace xt::data::datasets {
-    class WikiTextV2 : BaseDataset {
-        public :
-            explicit WikiTextV2(const std::string &root);
-        WikiTextV2(const std::string &root, DataMode mode);
-        WikiTextV2(const std::string &root, DataMode mode , bool download);
-        WikiTextV2(const std::string &root, DataMode mode , bool download, TransformType transforms);
+using namespace std;
+namespace fs = std::filesystem;
 
-        private :
-            void load_data();
+namespace xt::data::datasets
+{
+    class WikiTextV2 : public xt::datasets::Dataset
+    {
+    public :
+        explicit WikiTextV2(const std::string& root);
+        WikiTextV2(const std::string& root, xt::datasets::DataMode mode);
+        WikiTextV2(const std::string& root, xt::datasets::DataMode mode, bool download);
+        WikiTextV2(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        WikiTextV2(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
+
+    private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
 
         void check_resources();
     };
--- src/datasets/natural_language_processing/language_modeling/wiki_text_2.cpp
+++ src/datasets/natural_language_processing/language_modeling/wiki_text_2.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/language_modeling/wiki_text_2.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- WikiTextV2 ---------------------- //
 
-    WikiTextV2::WikiTextV2(const std::string &root): WikiTextV2::WikiTextV2(root, DataMode::TRAIN, false) {
+    WikiTextV2::WikiTextV2(const std::string& root): WikiTextV2::WikiTextV2(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    WikiTextV2::WikiTextV2(const std::string &root, DataMode mode): WikiTextV2::WikiTextV2(root, mode, false) {
+    WikiTextV2::WikiTextV2(const std::string& root, xt::datasets::DataMode mode): WikiTextV2::WikiTextV2(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    WikiTextV2::WikiTextV2(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("WikiText: WikiText not implemented");
+    WikiTextV2::WikiTextV2(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        WikiTextV2::WikiTextV2(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    WikiTextV2::WikiTextV2(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : WikiTextV2::WikiTextV2(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    WikiTextV2::WikiTextV2(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    WikiTextV2::WikiTextV2(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("WikiText: WikiText not implemented");
+    void WikiTextV2::load_data()
+    {
+
     }
 
+    void WikiTextV2::check_resources()
+    {
 
+    }
 }
+
