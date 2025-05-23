e664d1f40638d7b1aae6da9936c857655ff3317a
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:49:41 +0330

    datasets base files changed

--- include/datasets/natural_language_processing/text_classification/snli.h
+++ include/datasets/natural_language_processing/text_classification/snli.h
@@ -1,16 +1,33 @@
 #pragma once
-#include "datasets/base/base.h"
 
-namespace xt::data::datasets {
-    class SNLI : BaseDataset {
-        public :
-            explicit SNLI(const std::string &root);
-        SNLI(const std::string &root, DataMode mode);
-        SNLI(const std::string &root, DataMode mode , bool download);
-        SNLI(const std::string &root, DataMode mode , bool download, TransformType transforms);
+#include "datasets/common.h"
 
-        private :
-            void load_data();
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class SNLI : public xt::datasets::Dataset
+    {
+    public :
+        explicit SNLI(const std::string& root);
+        SNLI(const std::string& root, xt::datasets::DataMode mode);
+        SNLI(const std::string& root, xt::datasets::DataMode mode, bool download);
+        SNLI(const std::string& root, xt::datasets::DataMode mode, bool download,
+             std::unique_ptr<xt::Module> transformer);
+        SNLI(const std::string& root, xt::datasets::DataMode mode, bool download,
+             std::unique_ptr<xt::Module> transformer,
+             std::unique_ptr<xt::Module> target_transformer);
+
+    private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
 
         void check_resources();
     };
