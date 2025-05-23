0cd3d44cf3cea85e6ab914531046ffa9c78421e2
kamisaberi <kamisaberi@gmail.com>
2025-05-23 06:44:20 +0330

    datasets base files changed

--- include/datasets/general/audio_dataset.h
+++ include/datasets/general/audio_dataset.h
@@ -1,26 +1,23 @@
 #pragma once
 
-#include <torch/torch.h>
-#include <torch/data/datasets.h>
-#include <vector>
-#include <string>
+#include "datasets/common.h"
 #include <sndfile.hh>
-#include <fstream>
-#include <sstream>
 
-#include "../base/base.h"
-// #include "../../headers/datasets.h"
+using namespace std;
+namespace fs = std::filesystem;
 
 
-namespace xt::data::datasets {
-
-
-    class AudioDataset : public torch::data::datasets::Dataset<AudioDataset> {
+namespace xt::data::datasets
+{
+    class AudioDataset : public xt::datasets::Dataset
+    {
     public:
-        AudioDataset(const std::string &audio_dir, const std::string &label_file) {
+        AudioDataset(const std::string& audio_dir, const std::string& label_file)
+        {
             std::ifstream label_stream(label_file);
             std::string line;
-            while (std::getline(label_stream, line)) {
+            while (std::getline(label_stream, line))
+            {
                 std::istringstream iss(line);
                 std::string filename, label_str;
                 iss >> filename >> label_str;
@@ -29,42 +26,47 @@ namespace xt::data::datasets {
             }
         }
 
-        torch::data::Example<> get(size_t index) override {
+        torch::data::Example<> get(size_t index) override
+        {
             SndfileHandle file(file_paths[index]);
             std::vector<float> samples(file.frames());
             file.read(&samples[0], file.frames());
             auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU);
-            torch::Tensor waveform = torch::from_blob(samples.data(), {1, (int64_t) samples.size()}, options).clone();
+            torch::Tensor waveform = torch::from_blob(samples.data(), {1, (int64_t)samples.size()}, options).clone();
             torch::Tensor label = torch::full({1}, labels[index], torch::kLong);
             return {waveform, label};
         }
 
-        torch::optional<size_t> size() const override {
+        torch::optional<size_t> size() const override
+        {
             return file_paths.size();
         }
 
     private:
-        std::vector <std::string> file_paths;
+        std::vector<std::string> file_paths;
         std::vector<int> labels;
     };
 
-    class StackedAudioDataset : public BaseDataset {
+    class StackedAudioDataset : public BaseDataset
+    {
     public :
-        StackedAudioDataset(const std::string &folder_path);
+        StackedAudioDataset(const std::string& folder_path);
 
-        StackedAudioDataset(const std::string &folder_path, DataMode mode);
+        StackedAudioDataset(const std::string& folder_path, DataMode mode);
 
-        StackedAudioDataset(const std::string &folder_path, DataMode mode, bool load_sub_folders);
+        StackedAudioDataset(const std::string& folder_path, DataMode mode, bool load_sub_folders);
 
-        StackedAudioDataset(const std::string &folder_path, DataMode mode, bool load_sub_folders,
-                            vector <std::function<torch::Tensor(torch::Tensor)>> transforms);
+        StackedAudioDataset(const std::string& folder_path, DataMode mode, bool load_sub_folders,
+                            std::unique_ptr<xt::Module> transformer);
+
+        StackedAudioDataset(const std::string& folder_path, DataMode mode, bool load_sub_folders,
+                            std::unique_ptr<xt::Module> transformer,
+                            std::unique_ptr<xt::Module> target_transformer);
 
     private:
-        vector <string> labels_name;
+        vector<string> labels_name;
         bool load_sub_folders = false;
 
         void load_data();
     };
-
-
 }
