bd47b3e7a305dfc8a86aa4d699e82de93d0d135d
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:09:48 +0330

    datasets base files changed

--- src/datasets/audio_processing/speech_recognition/timit.cpp
+++ src/datasets/audio_processing/speech_recognition/timit.cpp
@@ -1,22 +1,49 @@
-#include "../../../../include/datasets/audio_processing/speech_recognition/timit.h"
+#include "datasets/audio_processing/speech_recognition/timit.h"
 
-namespace xt::data::datasets {
 
-    TIMIT::TIMIT(const std::string &root): TIMIT::TIMIT(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- TIMIT ---------------------- //
+
+    TIMIT::TIMIT(const std::string& root): TIMIT::TIMIT(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    TIMIT::TIMIT(const std::string &root, DataMode mode): TIMIT::TIMIT(root, mode, false) {
+    TIMIT::TIMIT(const std::string& root, xt::datasets::DataMode mode): TIMIT::TIMIT(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    TIMIT::TIMIT(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("TIMIT: TIMIT not implemented");
+    TIMIT::TIMIT(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        TIMIT::TIMIT(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    TIMIT::TIMIT(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : TIMIT::TIMIT(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    TIMIT::TIMIT(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    TIMIT::TIMIT(const std::string &root, DataMode mode, bool download,
-                         TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("TIMIT: TIMIT not implemented");
+    void TIMIT::load_data()
+    {
+
     }
 
+    void TIMIT::check_resources()
+    {
 
+    }
 }
