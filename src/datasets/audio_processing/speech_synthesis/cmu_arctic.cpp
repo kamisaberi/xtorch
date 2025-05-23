48870c7ecd3448a70ca70e63a080fbaf90dbb73c
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:16:49 +0330

    datasets base files changed

--- src/datasets/audio_processing/speech_synthesis/cmu_arctic.cpp
+++ src/datasets/audio_processing/speech_synthesis/cmu_arctic.cpp
@@ -1,22 +1,49 @@
-#include "../../../../include/datasets/audio_processing/speech_synthesis/cmu_arctic.h"
+#include "datasets/audio_processing/speech_synthesis/cmu_arctic.h"
 
-namespace xt::data::datasets {
 
-    CMUArctic::CMUArctic(const std::string &root): CMUArctic::CMUArctic(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- CMUArctic ---------------------- //
+
+    CMUArctic::CMUArctic(const std::string& root): CMUArctic::CMUArctic(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    CMUArctic::CMUArctic(const std::string &root, DataMode mode): CMUArctic::CMUArctic(root, mode, false) {
+    CMUArctic::CMUArctic(const std::string& root, xt::datasets::DataMode mode): CMUArctic::CMUArctic(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    CMUArctic::CMUArctic(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("CMUArctic: CMUArctic not implemented");
+    CMUArctic::CMUArctic(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        CMUArctic::CMUArctic(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    CMUArctic::CMUArctic(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : CMUArctic::CMUArctic(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    CMUArctic::CMUArctic(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    CMUArctic::CMUArctic(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("CMUArctic: CMUArctic not implemented");
+    void CMUArctic::load_data()
+    {
+
     }
 
+    void CMUArctic::check_resources()
+    {
 
+    }
 }
