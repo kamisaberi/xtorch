f95f0e96087f13c2a13556dc1a3841a8dc35e4d9
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:51:45 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/sogou_news.cpp
+++ src/datasets/natural_language_processing/text_classification/sogou_news.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/sogou_news.h"
 
-namespace xt::data::datasets {
 
-    SogouNews::SogouNews(const std::string &root): SogouNews::SogouNews(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- SogouNews ---------------------- //
+
+    SogouNews::SogouNews(const std::string& root): SogouNews::SogouNews(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    SogouNews::SogouNews(const std::string &root, DataMode mode): SogouNews::SogouNews(root, mode, false) {
+    SogouNews::SogouNews(const std::string& root, xt::datasets::DataMode mode): SogouNews::SogouNews(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    SogouNews::SogouNews(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("SogouNews: SogouNews not implemented");
+    SogouNews::SogouNews(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        SogouNews::SogouNews(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    SogouNews::SogouNews(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : SogouNews::SogouNews(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    SogouNews::SogouNews(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    SogouNews::SogouNews(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("SogouNews: SogouNews not implemented");
+    void SogouNews::load_data()
+    {
+
     }
 
+    void SogouNews::check_resources()
+    {
 
+    }
 }
