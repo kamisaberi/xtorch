c11b7e7ffc5308b2a2968399210e76ff90707556
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:48:54 +0330

    datasets base files changed

--- src/datasets/computer_vision/semantic_segmentation/sb_dataset.cpp
+++ src/datasets/computer_vision/semantic_segmentation/sb_dataset.cpp
@@ -1,22 +1,49 @@
 #include "datasets/computer_vision/semantic_segmentation/sb_dataset.h"
 
-namespace xt::data::datasets {
 
-    SBDataset::SBDataset(const std::string &root): SBDataset::SBDataset(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- SBDataset ---------------------- //
+
+    SBDataset::SBDataset(const std::string& root): SBDataset::SBDataset(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    SBDataset::SBDataset(const std::string &root, DataMode mode): SBDataset::SBDataset(root, mode, false) {
+    SBDataset::SBDataset(const std::string& root, xt::datasets::DataMode mode): SBDataset::SBDataset(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    SBDataset::SBDataset(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("SBDataset: SBDataset not implemented");
+    SBDataset::SBDataset(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        SBDataset::SBDataset(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    SBDataset::SBDataset(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : SBDataset::SBDataset(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    SBDataset::SBDataset(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    SBDataset::SBDataset(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("SBDataset: SBDataset not implemented");
+    void SBDataset::load_data()
+    {
+
     }
 
+    void SBDataset::check_resources()
+    {
 
+    }
 }
