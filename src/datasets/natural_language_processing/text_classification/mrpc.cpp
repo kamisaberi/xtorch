b0e018335c77a976dd8db9903706da4fcf2bbff0
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:43:57 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/mrpc.cpp
+++ src/datasets/natural_language_processing/text_classification/mrpc.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/mrpc.h"
 
-namespace xt::data::datasets {
 
-    MRPC::MRPC(const std::string &root): MRPC::MRPC(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- MRPC ---------------------- //
+
+    MRPC::MRPC(const std::string& root): MRPC::MRPC(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    MRPC::MRPC(const std::string &root, DataMode mode): MRPC::MRPC(root, mode, false) {
+    MRPC::MRPC(const std::string& root, xt::datasets::DataMode mode): MRPC::MRPC(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    MRPC::MRPC(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("MRPC: MRPC not implemented");
+    MRPC::MRPC(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        MRPC::MRPC(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    MRPC::MRPC(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : MRPC::MRPC(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    MRPC::MRPC(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    MRPC::MRPC(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("MRPC: MRPC not implemented");
+    void MRPC::load_data()
+    {
+
     }
 
+    void MRPC::check_resources()
+    {
 
+    }
 }
