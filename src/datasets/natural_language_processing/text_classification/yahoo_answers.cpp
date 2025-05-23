f7b8ced530c49cae4d4a476a3eb7ba271a55269a
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:02:51 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/yahoo_answers.cpp
+++ src/datasets/natural_language_processing/text_classification/yahoo_answers.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/yahoo_answers.h"
 
-namespace xt::data::datasets {
 
-    YahooAnswers::YahooAnswers(const std::string &root): YahooAnswers::YahooAnswers(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- YahooAnswers ---------------------- //
+
+    YahooAnswers::YahooAnswers(const std::string& root): YahooAnswers::YahooAnswers(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    YahooAnswers::YahooAnswers(const std::string &root, DataMode mode): YahooAnswers::YahooAnswers(root, mode, false) {
+    YahooAnswers::YahooAnswers(const std::string& root, xt::datasets::DataMode mode): YahooAnswers::YahooAnswers(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    YahooAnswers::YahooAnswers(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("YahooAnswers: YahooAnswers not implemented");
+    YahooAnswers::YahooAnswers(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        YahooAnswers::YahooAnswers(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    YahooAnswers::YahooAnswers(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : YahooAnswers::YahooAnswers(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    YahooAnswers::YahooAnswers(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    YahooAnswers::YahooAnswers(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("YahooAnswers: YahooAnswers not implemented");
+    void YahooAnswers::load_data()
+    {
+
     }
 
+    void YahooAnswers::check_resources()
+    {
 
+    }
 }
