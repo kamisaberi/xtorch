8b9ec1aedaf1b7a6d5e5152495428e137e2d85ed
kamisaberi <kamisaberi@gmail.com>
2025-05-24 03:23:44 +0330

    dataset base files updated

--- include/datasets/tabular_data/binary_classification/habermans_survival.h
+++ include/datasets/tabular_data/binary_classification/habermans_survival.h
@@ -7,15 +7,15 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets
 {
-    class ModelNet40 : public xt::datasets::Dataset
+    class HabermansSurvival : public xt::datasets::Dataset
     {
     public :
-        explicit ModelNet40(const std::string& root);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        explicit HabermansSurvival(const std::string& root);
+        HabermansSurvival(const std::string& root, xt::datasets::DataMode mode);
+        HabermansSurvival(const std::string& root, xt::datasets::DataMode mode, bool download);
+        HabermansSurvival(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        HabermansSurvival(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer,
                    std::unique_ptr<xt::Module> target_transformer);
 
--- src/datasets/tabular_data/binary_classification/habermans_survival.cpp
+++ src/datasets/tabular_data/binary_classification/habermans_survival.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/3d_point_cloud_analysis/model_net40.h"
+#include "datasets/tabular_data/binary_classification/habermans_survival.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- HabermansSurvival ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    HabermansSurvival::HabermansSurvival(const std::string& root): HabermansSurvival::HabermansSurvival(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    HabermansSurvival::HabermansSurvival(const std::string& root, xt::datasets::DataMode mode): HabermansSurvival::HabermansSurvival(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    HabermansSurvival::HabermansSurvival(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        HabermansSurvival::HabermansSurvival(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    HabermansSurvival::HabermansSurvival(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : HabermansSurvival::HabermansSurvival(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    HabermansSurvival::HabermansSurvival(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void HabermansSurvival::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void HabermansSurvival::check_resources()
     {
 
     }
