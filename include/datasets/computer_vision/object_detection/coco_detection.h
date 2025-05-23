390be91cc6e7474ff888ebef504f77303d13086f
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:32:56 +0330

    datasets base files changed

--- include/datasets/computer_vision/object_detection/coco_detection.h
+++ include/datasets/computer_vision/object_detection/coco_detection.h
@@ -1,21 +1,35 @@
 #pragma once
-#include "datasets/base/base.h"
+
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
 
 
-namespace xt::data::datasets {
-    class CocoDetection : public BaseDataset {
+namespace xt::data::datasets
+{
+    class CocoDetection : public xt::datasets::Dataset
+    {
     public :
-        explicit  CocoDetection(const std::string &root);
-        CocoDetection(const std::string &root, DataMode mode);
-        CocoDetection(const std::string &root, DataMode mode , bool download);
-        CocoDetection(const std::string &root, DataMode mode , bool download, TransformType transforms);
-
+        explicit CocoDetection(const std::string& root);
+        CocoDetection(const std::string& root, xt::datasets::DataMode mode);
+        CocoDetection(const std::string& root, xt::datasets::DataMode mode, bool download);
+        CocoDetection(const std::string& root, xt::datasets::DataMode mode, bool download,
+                      std::unique_ptr<xt::Module> transformer);
+        CocoDetection(const std::string& root, xt::datasets::DataMode mode, bool download,
+                      std::unique_ptr<xt::Module> transformer,
+                      std::unique_ptr<xt::Module> target_transformer);
 
     private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
     };
-
-
 }
