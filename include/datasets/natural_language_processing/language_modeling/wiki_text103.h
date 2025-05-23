ab5dcd9929045775072873ccf96ec20c58ee6da2
kamisaberi <kamisaberi@gmail.com>
2025-05-23 06:02:45 +0330

    datasets base files changed

--- include/datasets/natural_language_processing/language_modeling/wiki_text103.h
+++ include/datasets/natural_language_processing/language_modeling/wiki_text103.h
@@ -1,18 +1,32 @@
 #pragma once
 
-#include "datasets/base/base.h"
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
 
 namespace xt::data::datasets
 {
-    class WikiText103 : BaseDataset
+    class WikiText103 : public xt::datasets::Dataset
     {
     public :
         explicit WikiText103(const std::string& root);
-        WikiText103(const std::string& root, DataMode mode);
-        WikiText103(const std::string& root, DataMode mode, bool download);
-        WikiText103(const std::string& root, DataMode mode, bool download, TransformType transforms);
+        WikiText103(const std::string& root, xt::datasets::DataMode mode);
+        WikiText103(const std::string& root, xt::datasets::DataMode mode, bool download);
+        WikiText103(const std::string& root, xt::datasets::DataMode mode, bool download,
+                    std::unique_ptr<xt::Module> transformer);
+        WikiText103(const std::string& root, xt::datasets::DataMode mode, bool download,
+                    std::unique_ptr<xt::Module> transformer,
+                    std::unique_ptr<xt::Module> target_transformer);
 
     private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
--- src/datasets/natural_language_processing/language_modeling/wiki_text103.cpp
+++ src/datasets/natural_language_processing/language_modeling/wiki_text103.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/language_modeling/wiki_text103.h"
 
-namespace xt::data::datasets {
 
-    WikiText103::WikiText103(const std::string &root): WikiText103::WikiText103(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- WikiText103 ---------------------- //
+
+    WikiText103::WikiText103(const std::string& root): WikiText103::WikiText103(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    WikiText103::WikiText103(const std::string &root, DataMode mode): WikiText103::WikiText103(root, mode, false) {
+    WikiText103::WikiText103(const std::string& root, xt::datasets::DataMode mode): WikiText103::WikiText103(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    WikiText103::WikiText103(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("WikiText: WikiText not implemented");
+    WikiText103::WikiText103(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        WikiText103::WikiText103(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    WikiText103::WikiText103(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : WikiText103::WikiText103(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    WikiText103::WikiText103(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    WikiText103::WikiText103(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("WikiText: WikiText not implemented");
+    void WikiText103::load_data()
+    {
+
     }
 
+    void WikiText103::check_resources()
+    {
 
+    }
 }
