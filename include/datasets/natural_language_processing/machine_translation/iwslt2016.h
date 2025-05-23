55e2ac790740542f8e04cbeffb4d10cfa8253978
kamisaberi <kamisaberi@gmail.com>
2025-05-23 06:11:05 +0330

    datasets base files changed

--- include/datasets/natural_language_processing/machine_translation/iwslt2016.h
+++ include/datasets/natural_language_processing/machine_translation/iwslt2016.h
@@ -1,16 +1,34 @@
 #pragma once
-#include "datasets/base/base.h"
 
-namespace xt::data::datasets {
-    class IWSLT20169 : BaseDataset {
-        public :
-            explicit IWSLT20169(const std::string &root);
-        IWSLT20169(const std::string &root, DataMode mode);
-        IWSLT20169(const std::string &root, DataMode mode , bool download);
-        IWSLT20169(const std::string &root, DataMode mode , bool download, TransformType transforms);
+#include "datasets/common.h"
 
-        private :
-            void load_data();
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class IWSLT2016 : public xt::datasets::Dataset
+    {
+    public :
+        explicit IWSLT2016(const std::string& root);
+        IWSLT2016(const std::string& root, xt::datasets::DataMode mode);
+        IWSLT2016(const std::string& root, xt::datasets::DataMode mode, bool download);
+        IWSLT2016(const std::string& root, xt::datasets::DataMode mode, bool download,
+                  std::unique_ptr<xt::Module> transformer);
+        IWSLT2016(const std::string& root, xt::datasets::DataMode mode, bool download,
+                  std::unique_ptr<xt::Module> transformer,
+                  std::unique_ptr<xt::Module> target_transformer);
+
+    private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+
+        void load_data();
 
         void check_resources();
     };
