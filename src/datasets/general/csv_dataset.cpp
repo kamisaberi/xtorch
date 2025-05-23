7caba68c335d67b769fcf1e8d7b1724d8b537af7
kamisaberi <kamisaberi@gmail.com>
2025-05-23 06:53:30 +0330

    datasets base files changed

--- src/datasets/general/csv_dataset.cpp
+++ src/datasets/general/csv_dataset.cpp
@@ -1,25 +1,27 @@
-
 #include "datasets/general/csv_dataset.h"
 
-namespace xt::data::datasets {
-
-
-    CSVDataset::CSVDataset(const std::string &file_path): BaseDataset(file_path) {
-
+namespace xt::data::datasets
+{
+    CSVDataset::CSVDataset(const std::string& file_path): xt::datasets::Dataset()
+    {
+        this->file_path_ = file_path;
         csv2::Reader<csv2::delimiter<','>,
-             csv2::quote_character<'"'>,
-             csv2::first_row_is_header<true>,
-             csv2::trim_policy::trim_whitespace> csv;
+                     csv2::quote_character<'"'>,
+                     csv2::first_row_is_header<true>,
+                     csv2::trim_policy::trim_whitespace> csv;
 
 
-        if (csv.mmap(file_path)) {
+        if (csv.mmap(file_path))
+        {
             // Retrieve the header row (optional)
             const auto header = csv.header();
 
             // Iterate over each row in the CSV file
-            for (const auto row : csv) {
+            for (const auto row : csv)
+            {
                 // Iterate over each cell in the row
-                for (const auto cell : row) {
+                for (const auto cell : row)
+                {
                     std::string value;
                     // Read the cell value
                     cell.read_value(value);
@@ -27,10 +29,10 @@ namespace xt::data::datasets {
                 }
                 std::cout << std::endl; // Newline after each row
             }
-        } else {
+        }
+        else
+        {
             std::cerr << "Failed to open foo.csv" << std::endl;
         }
-
-
     }
 }
