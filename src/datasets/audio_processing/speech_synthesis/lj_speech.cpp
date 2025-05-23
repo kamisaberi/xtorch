f3c543a6cdd238dd5f9f8ce540f7b46b206d4348
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:19:59 +0330

    datasets base files changed

--- src/datasets/audio_processing/speech_synthesis/lj_speech.cpp
+++ src/datasets/audio_processing/speech_synthesis/lj_speech.cpp
@@ -1,22 +1,49 @@
-#include "../../../../include/datasets/audio_processing/speech_synthesis/lj_speech.h"
+#include "datasets/audio_processing/speech_synthesis/lj_speech.h"
 
-namespace xt::data::datasets {
 
-    LjSpeech::LjSpeech(const std::string &root): LjSpeech::LjSpeech(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- LjSpeech ---------------------- //
+
+    LjSpeech::LjSpeech(const std::string& root): LjSpeech::LjSpeech(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    LjSpeech::LjSpeech(const std::string &root, DataMode mode): LjSpeech::LjSpeech(root, mode, false) {
+    LjSpeech::LjSpeech(const std::string& root, xt::datasets::DataMode mode): LjSpeech::LjSpeech(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    LjSpeech::LjSpeech(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("LjSpeech: LjSpeech not implemented");
+    LjSpeech::LjSpeech(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        LjSpeech::LjSpeech(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    LjSpeech::LjSpeech(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : LjSpeech::LjSpeech(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    LjSpeech::LjSpeech(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    LjSpeech::LjSpeech(const std::string &root, DataMode mode, bool download,
-                         TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("LjSpeech: LjSpeech not implemented");
+    void LjSpeech::load_data()
+    {
+
     }
 
+    void LjSpeech::check_resources()
+    {
 
+    }
 }
