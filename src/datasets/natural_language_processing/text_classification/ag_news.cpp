bf62872ca43a6ddfc113b3618fef86c9ac288821
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:27:47 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/ag_news.cpp
+++ src/datasets/natural_language_processing/text_classification/ag_news.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/ag_news.h"
 
-namespace xt::data::datasets {
 
-    AgNews::AgNews(const std::string &root): AgNews::AgNews(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- AgNews ---------------------- //
+
+    AgNews::AgNews(const std::string& root): AgNews::AgNews(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    AgNews::AgNews(const std::string &root, DataMode mode): AgNews::AgNews(root, mode, false) {
+    AgNews::AgNews(const std::string& root, xt::datasets::DataMode mode): AgNews::AgNews(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    AgNews::AgNews(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("AgNews: AgNews not implemented");
+    AgNews::AgNews(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        AgNews::AgNews(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    AgNews::AgNews(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : AgNews::AgNews(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    AgNews::AgNews(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    AgNews::AgNews(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("AgNews: AgNews not implemented");
+    void AgNews::load_data()
+    {
+
     }
 
+    void AgNews::check_resources()
+    {
 
+    }
 }
