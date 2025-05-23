3503544918c241dd57e982e7079254367b324037
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:09:48 +0330

    datasets base files changed

--- include/datasets/audio_processing/speech_recognition/timit.h
+++ include/datasets/audio_processing/speech_recognition/timit.h
@@ -1,21 +1,33 @@
 #pragma once
 
-#include "datasets/base/base.h"
 #include "datasets/common.h"
 
+using namespace std;
+namespace fs = std::filesystem;
 
-namespace xt::data::datasets {
-    class TIMIT : BaseDataset {
+
+namespace xt::data::datasets
+{
+    class TIMIT : public xt::datasets::Dataset
+    {
     public :
-        explicit TIMIT(const std::string &root);
-
-        TIMIT(const std::string &root, DataMode mode);
-
-        TIMIT(const std::string &root, DataMode mode, bool download);
-
-        TIMIT(const std::string &root, DataMode mode, bool download, TransformType transforms);
+        explicit TIMIT(const std::string& root);
+        TIMIT(const std::string& root, xt::datasets::DataMode mode);
+        TIMIT(const std::string& root, xt::datasets::DataMode mode, bool download);
+        TIMIT(const std::string& root, xt::datasets::DataMode mode, bool download,
+              std::unique_ptr<xt::Module> transformer);
+        TIMIT(const std::string& root, xt::datasets::DataMode mode, bool download,
+              std::unique_ptr<xt::Module> transformer,
+              std::unique_ptr<xt::Module> target_transformer);
 
     private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
