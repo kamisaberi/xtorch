257177cab217b5f9ed53ec10d8a26fbdc8297ccf
kamisaberi <kamisaberi@gmail.com>
2025-05-24 03:20:50 +0330

    dataset base files updated

--- include/datasets/tabular_data/binary_classification/breast_cancer_wisconsin.h
+++ include/datasets/tabular_data/binary_classification/breast_cancer_wisconsin.h
@@ -7,15 +7,15 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets
 {
-    class ModelNet40 : public xt::datasets::Dataset
+    class BreastCancerWisconsin : public xt::datasets::Dataset
     {
     public :
-        explicit ModelNet40(const std::string& root);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        explicit BreastCancerWisconsin(const std::string& root);
+        BreastCancerWisconsin(const std::string& root, xt::datasets::DataMode mode);
+        BreastCancerWisconsin(const std::string& root, xt::datasets::DataMode mode, bool download);
+        BreastCancerWisconsin(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        BreastCancerWisconsin(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer,
                    std::unique_ptr<xt::Module> target_transformer);
 
