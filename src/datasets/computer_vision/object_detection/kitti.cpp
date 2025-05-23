31e7fd8917c19c6bde0a739e2b40e01e7dbcd9af
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:34:52 +0330

    datasets base files changed

--- src/datasets/computer_vision/object_detection/kitti.cpp
+++ src/datasets/computer_vision/object_detection/kitti.cpp
@@ -1,26 +1,49 @@
 #include "datasets/computer_vision/object_detection/kitti.h"
 
-namespace xt::data::datasets {
 
+namespace xt::data::datasets
+{
     // ---------------------- Kitti ---------------------- //
-    Kitti::Kitti(const std::string &root): Kitti::Kitti(root, DataMode::TRAIN, false) {
+
+    Kitti::Kitti(const std::string& root): Kitti::Kitti(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    Kitti::Kitti(const std::string &root, DataMode mode): Kitti::Kitti(root, mode, false) {
+    Kitti::Kitti(const std::string& root, xt::datasets::DataMode mode): Kitti::Kitti(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    Kitti::Kitti(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("Kitti: Kitti not implemented");
+    Kitti::Kitti(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        Kitti::Kitti(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    Kitti::Kitti(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : Kitti::Kitti(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    Kitti::Kitti(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    Kitti::Kitti(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("Kitti: Kitti not implemented");
+    void Kitti::load_data()
+    {
+
     }
 
+    void Kitti::check_resources()
+    {
 
-
-
-
+    }
 }
