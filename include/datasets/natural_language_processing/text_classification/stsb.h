bf8eb8bf8ba6f3a5e0f6caab77dd142021339955
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:00:18 +0330

    datasets base files changed

--- include/datasets/natural_language_processing/text_classification/stsb.h
+++ include/datasets/natural_language_processing/text_classification/stsb.h
@@ -1,17 +1,35 @@
 #pragma once
-#include "datasets/base/base.h"
 
-namespace xt::data::datasets {
-    class STSB : BaseDataset {
-        public :
-            explicit STSB(const std::string &root);
-        STSB(const std::string &root, DataMode mode);
-        STSB(const std::string &root, DataMode mode , bool download);
-        STSB(const std::string &root, DataMode mode , bool download, TransformType transforms);
+#include "datasets/common.h"
 
-        private :
-            void load_data();
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class STSB : public xt::datasets::Dataset
+    {
+    public :
+        explicit STSB(const std::string& root);
+        STSB(const std::string& root, xt::datasets::DataMode mode);
+        STSB(const std::string& root, xt::datasets::DataMode mode, bool download);
+        STSB(const std::string& root, xt::datasets::DataMode mode, bool download,
+             std::unique_ptr<xt::Module> transformer);
+        STSB(const std::string& root, xt::datasets::DataMode mode, bool download,
+             std::unique_ptr<xt::Module> transformer,
+             std::unique_ptr<xt::Module> target_transformer);
+
+    private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
 
         void check_resources();
+
     };
 }
--- src/datasets/natural_language_processing/text_classification/stsb.cpp
+++ src/datasets/natural_language_processing/text_classification/stsb.cpp
@@ -1,22 +1,48 @@
 #include "datasets/natural_language_processing/text_classification/stsb.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- STSB ---------------------- //
 
-    STSB::STSB(const std::string &root): STSB::STSB(root, DataMode::TRAIN, false) {
+    STSB::STSB(const std::string& root): STSB::STSB(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    STSB::STSB(const std::string &root, DataMode mode): STSB::STSB(root, mode, false) {
+    STSB::STSB(const std::string& root, xt::datasets::DataMode mode): STSB::STSB(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    STSB::STSB(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("STSB: STSB not implemented");
+    STSB::STSB(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        STSB::STSB(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    STSB::STSB(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : STSB::STSB(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    STSB::STSB(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    STSB::STSB(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("STSB: STSB not implemented");
+    void STSB::load_data()
+    {
+
     }
 
+    void STSB::check_resources()
+    {
 
+    }
 }
