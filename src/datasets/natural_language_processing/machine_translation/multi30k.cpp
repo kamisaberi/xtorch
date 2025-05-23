95d6633df0d5ac6f4a141e96e0a47b3b47b3c1c8
kamisaberi <kamisaberi@gmail.com>
2025-05-23 06:13:35 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/machine_translation/multi30k.cpp
+++ src/datasets/natural_language_processing/machine_translation/multi30k.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/machine_translation/multi30k.h"
 
-namespace xt::data::datasets {
 
-    MULTI30k::MULTI30k(const std::string &root): MULTI30k::MULTI30k(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- MULTI30k ---------------------- //
+
+    MULTI30k::MULTI30k(const std::string& root): MULTI30k::MULTI30k(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    MULTI30k::MULTI30k(const std::string &root, DataMode mode): MULTI30k::MULTI30k(root, mode, false) {
+    MULTI30k::MULTI30k(const std::string& root, xt::datasets::DataMode mode): MULTI30k::MULTI30k(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    MULTI30k::MULTI30k(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("MULTI: MULTI not implemented");
+    MULTI30k::MULTI30k(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        MULTI30k::MULTI30k(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    MULTI30k::MULTI30k(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : MULTI30k::MULTI30k(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    MULTI30k::MULTI30k(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    MULTI30k::MULTI30k(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("MULTI: MULTI not implemented");
+    void MULTI30k::load_data()
+    {
+
     }
 
+    void MULTI30k::check_resources()
+    {
 
+    }
 }
