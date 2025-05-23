9988e46e623a3185242000755891e31413ca5412
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:50:25 +0330

    datasets base files changed

--- src/datasets/computer_vision/semantic_segmentation/voc_segmentation.cpp
+++ src/datasets/computer_vision/semantic_segmentation/voc_segmentation.cpp
@@ -1,23 +1,49 @@
 #include "datasets/computer_vision/semantic_segmentation/voc_segmentation.h"
 
-namespace xt::data::datasets {
 
-
+namespace xt::data::datasets
+{
     // ---------------------- VOCSegmentation ---------------------- //
-    VOCSegmentation::VOCSegmentation(const std::string &root): VOCSegmentation::VOCSegmentation(root, DataMode::TRAIN, false) {
+
+    VOCSegmentation::VOCSegmentation(const std::string& root): VOCSegmentation::VOCSegmentation(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    VOCSegmentation::VOCSegmentation(const std::string &root, DataMode mode): VOCSegmentation::VOCSegmentation(root, mode, false) {
+    VOCSegmentation::VOCSegmentation(const std::string& root, xt::datasets::DataMode mode): VOCSegmentation::VOCSegmentation(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    VOCSegmentation::VOCSegmentation(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("VOCSegmentation: VOCSegmentation not implemented");
+    VOCSegmentation::VOCSegmentation(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        VOCSegmentation::VOCSegmentation(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    VOCSegmentation::VOCSegmentation(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : VOCSegmentation::VOCSegmentation(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    VOCSegmentation::VOCSegmentation(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    VOCSegmentation::VOCSegmentation(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("VOCSegmentation: VOCSegmentation not implemented");
+    void VOCSegmentation::load_data()
+    {
+
     }
 
+    void VOCSegmentation::check_resources()
+    {
+
+    }
 }
