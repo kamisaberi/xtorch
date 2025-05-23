c8b0c4b6e1aa2424150ade225e7ee9fd62d4aef0
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:19:59 +0330

    datasets base files changed

--- include/datasets/audio_processing/speech_synthesis/lj_speech.h
+++ include/datasets/audio_processing/speech_synthesis/lj_speech.h
@@ -1,21 +1,33 @@
 #pragma once
 
-#include "datasets/base/base.h"
 #include "datasets/common.h"
 
+using namespace std;
+namespace fs = std::filesystem;
 
-namespace xt::data::datasets {
-    class LjSpeech : BaseDataset {
+
+namespace xt::data::datasets
+{
+    class LjSpeech : public xt::datasets::Dataset
+    {
     public :
-        explicit LjSpeech(const std::string &root);
-
-        LjSpeech(const std::string &root, DataMode mode);
-
-        LjSpeech(const std::string &root, DataMode mode, bool download);
-
-        LjSpeech(const std::string &root, DataMode mode, bool download, TransformType transforms);
+        explicit LjSpeech(const std::string& root);
+        LjSpeech(const std::string& root, xt::datasets::DataMode mode);
+        LjSpeech(const std::string& root, xt::datasets::DataMode mode, bool download);
+        LjSpeech(const std::string& root, xt::datasets::DataMode mode, bool download,
+                 std::unique_ptr<xt::Module> transformer);
+        LjSpeech(const std::string& root, xt::datasets::DataMode mode, bool download,
+                 std::unique_ptr<xt::Module> transformer,
+                 std::unique_ptr<xt::Module> target_transformer);
 
     private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
