ac5e90fa3a20afdf133c334b052f51b456c74739
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:05:28 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/yelp_review_polarity.cpp
+++ src/datasets/natural_language_processing/text_classification/yelp_review_polarity.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/yelp_review_polarity.h"
 
-namespace xt::data::datasets {
 
-    YelpReviewPolarity::YelpReviewPolarity(const std::string &root): YelpReviewPolarity::YelpReviewPolarity(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- YelpReviewPolarity ---------------------- //
+
+    YelpReviewPolarity::YelpReviewPolarity(const std::string& root): YelpReviewPolarity::YelpReviewPolarity(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    YelpReviewPolarity::YelpReviewPolarity(const std::string &root, DataMode mode): YelpReviewPolarity::YelpReviewPolarity(root, mode, false) {
+    YelpReviewPolarity::YelpReviewPolarity(const std::string& root, xt::datasets::DataMode mode): YelpReviewPolarity::YelpReviewPolarity(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    YelpReviewPolarity::YelpReviewPolarity(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("YelpReview: YelpReview not implemented");
+    YelpReviewPolarity::YelpReviewPolarity(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        YelpReviewPolarity::YelpReviewPolarity(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    YelpReviewPolarity::YelpReviewPolarity(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : YelpReviewPolarity::YelpReviewPolarity(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    YelpReviewPolarity::YelpReviewPolarity(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    YelpReviewPolarity::YelpReviewPolarity(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("YelpReview: YelpReview not implemented");
+    void YelpReviewPolarity::load_data()
+    {
+
     }
 
+    void YelpReviewPolarity::check_resources()
+    {
 
+    }
 }
