b19d45a45b38ea51f72bbe0e7e1cc0c08c70a591
kamisaberi <kamisaberi@gmail.com>
2025-05-24 03:23:00 +0330

    dataset base files updated

--- src/datasets/tabular_data/binary_classification/german_credit.cpp
+++ src/datasets/tabular_data/binary_classification/german_credit.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/3d_point_cloud_analysis/model_net40.h"
+#include "datasets/tabular_data/binary_classification/german_credit.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- GermanCredit ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    GermanCredit::GermanCredit(const std::string& root): GermanCredit::GermanCredit(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    GermanCredit::GermanCredit(const std::string& root, xt::datasets::DataMode mode): GermanCredit::GermanCredit(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    GermanCredit::GermanCredit(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        GermanCredit::GermanCredit(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    GermanCredit::GermanCredit(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : GermanCredit::GermanCredit(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    GermanCredit::GermanCredit(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void GermanCredit::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void GermanCredit::check_resources()
     {
 
     }
