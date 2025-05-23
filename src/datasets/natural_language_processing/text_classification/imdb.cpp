5cea2c6cea006a74a0e702b7e6a4d54eac62dd36
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:41:20 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/imdb.cpp
+++ src/datasets/natural_language_processing/text_classification/imdb.cpp
@@ -1,22 +1,48 @@
 #include "datasets/natural_language_processing/text_classification/imdb.h"
 
-namespace xt::data::datasets {
+namespace xt::data::datasets
+{
+    // ---------------------- IMDB ---------------------- //
 
-    IMDB::IMDB(const std::string &root): IMDB::IMDB(root, DataMode::TRAIN, false) {
+    IMDB::IMDB(const std::string& root): IMDB::IMDB(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    IMDB::IMDB(const std::string &root, DataMode mode): IMDB::IMDB(root, mode, false) {
+    IMDB::IMDB(const std::string& root, xt::datasets::DataMode mode): IMDB::IMDB(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    IMDB::IMDB(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("IMDB: IMDB not implemented");
+    IMDB::IMDB(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        IMDB::IMDB(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    IMDB::IMDB(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : IMDB::IMDB(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    IMDB::IMDB(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    IMDB::IMDB(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("IMDB: IMDB not implemented");
+    void IMDB::load_data()
+    {
+
     }
 
+    void IMDB::check_resources()
+    {
 
+    }
 }
