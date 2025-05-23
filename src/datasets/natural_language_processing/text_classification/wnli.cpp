e38b90b3871e361dcb8b197543a8b81e15378d8a
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:01:37 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/wnli.cpp
+++ src/datasets/natural_language_processing/text_classification/wnli.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/wnli.h"
 
-namespace xt::data::datasets {
 
-    WNLI::WNLI(const std::string &root): WNLI::WNLI(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- WNLI ---------------------- //
+
+    WNLI::WNLI(const std::string& root): WNLI::WNLI(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    WNLI::WNLI(const std::string &root, DataMode mode): WNLI::WNLI(root, mode, false) {
+    WNLI::WNLI(const std::string& root, xt::datasets::DataMode mode): WNLI::WNLI(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    WNLI::WNLI(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("WNLI: WNLI not implemented");
+    WNLI::WNLI(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        WNLI::WNLI(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    WNLI::WNLI(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : WNLI::WNLI(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    WNLI::WNLI(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    WNLI::WNLI(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("WNLI: WNLI not implemented");
+    void WNLI::load_data()
+    {
+
     }
 
+    void WNLI::check_resources()
+    {
 
+    }
 }
