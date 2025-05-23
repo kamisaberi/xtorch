868a64c9a7ac52f9a222e0ee086661eddf988f1c
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:50:02 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/snli.cpp
+++ src/datasets/natural_language_processing/text_classification/snli.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/snli.h"
 
-namespace xt::data::datasets {
 
-    SNLI::SNLI(const std::string &root): SNLI::SNLI(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- SNLI ---------------------- //
+
+    SNLI::SNLI(const std::string& root): SNLI::SNLI(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    SNLI::SNLI(const std::string &root, DataMode mode): SNLI::SNLI(root, mode, false) {
+    SNLI::SNLI(const std::string& root, xt::datasets::DataMode mode): SNLI::SNLI(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    SNLI::SNLI(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("SNLI: SNLI not implemented");
+    SNLI::SNLI(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        SNLI::SNLI(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    SNLI::SNLI(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : SNLI::SNLI(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    SNLI::SNLI(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    SNLI::SNLI(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("SNLI: SNLI not implemented");
+    void SNLI::load_data()
+    {
+
     }
 
+    void SNLI::check_resources()
+    {
 
+    }
 }
