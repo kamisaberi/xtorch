1af87577d0ad90f7e409bdc45caa0ba1ee6ba75e
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:31:21 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/text_classification/amazon_review_polarity.cpp
+++ src/datasets/natural_language_processing/text_classification/amazon_review_polarity.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/amazon_review_polarity.h"
 
-namespace xt::data::datasets {
 
-    AmazonReviewPolarity::AmazonReviewPolarity(const std::string &root): AmazonReviewPolarity::AmazonReviewPolarity(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- AmazonReviewPolarity ---------------------- //
+
+    AmazonReviewPolarity::AmazonReviewPolarity(const std::string& root): AmazonReviewPolarity::AmazonReviewPolarity(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    AmazonReviewPolarity::AmazonReviewPolarity(const std::string &root, DataMode mode): AmazonReviewPolarity::AmazonReviewPolarity(root, mode, false) {
+    AmazonReviewPolarity::AmazonReviewPolarity(const std::string& root, xt::datasets::DataMode mode): AmazonReviewPolarity::AmazonReviewPolarity(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    AmazonReviewPolarity::AmazonReviewPolarity(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("AmazonReview: AmazonReview not implemented");
+    AmazonReviewPolarity::AmazonReviewPolarity(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        AmazonReviewPolarity::AmazonReviewPolarity(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    AmazonReviewPolarity::AmazonReviewPolarity(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : AmazonReviewPolarity::AmazonReviewPolarity(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    AmazonReviewPolarity::AmazonReviewPolarity(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    AmazonReviewPolarity::AmazonReviewPolarity(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("AmazonReview: AmazonReview not implemented");
+    void AmazonReviewPolarity::load_data()
+    {
+
     }
 
+    void AmazonReviewPolarity::check_resources()
+    {
 
+    }
 }
