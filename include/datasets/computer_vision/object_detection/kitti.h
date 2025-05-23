c92ed74f3518b5aa7669b3760cdc6883cdf18343
kamisaberi <kamisaberi@gmail.com>
2025-05-23 05:34:52 +0330

    datasets base files changed

--- include/datasets/computer_vision/object_detection/kitti.h
+++ include/datasets/computer_vision/object_detection/kitti.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#include "datasets/base/base.h"
+#include "datasets/common.h"
+
+using namespace std;
+namespace fs = std::filesystem;
 
 namespace xt::data::datasets {
-    class Kitti : BaseDataset {
+    class Kitti : public xt::datasets::Dataset{
         /*
         """`KITTI <http://www.cvlibs.net/datasets/kitti/eval_object.php?obj_benchmark>`_ Dataset.
 
@@ -39,10 +42,14 @@ namespace xt::data::datasets {
 
          */
     public :
-        explicit Kitti(const std::string &root);
-        Kitti(const std::string &root, DataMode mode);
-        Kitti(const std::string &root, DataMode mode , bool download);
-        Kitti(const std::string &root, DataMode mode , bool download, TransformType transforms);
+        explicit Kitti(const std::string& root);
+        Kitti(const std::string& root, xt::datasets::DataMode mode);
+        Kitti(const std::string& root, xt::datasets::DataMode mode, bool download);
+        Kitti(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer);
+        Kitti(const std::string& root, xt::datasets::DataMode mode, bool download,
+                   std::unique_ptr<xt::Module> transformer,
+                   std::unique_ptr<xt::Module> target_transformer);
 
 
     private:
@@ -55,9 +62,15 @@ namespace xt::data::datasets {
         fs::path labels_dir_name = fs::path("label_2");
         fs::path dataset_folder_name = "kitti";
 
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
         void load_data();
 
         void check_resources();
+
     };
 
 
