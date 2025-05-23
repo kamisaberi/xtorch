4204ef7269f8e482dbe0f0af0f6e70479e94905f
kamisaberi <kamisaberi@gmail.com>
2025-05-23 03:50:58 +0330

    datasets base files changed

--- src/datasets/computer_vision/optical_flow_estimation/kitti_flow.cpp
+++ src/datasets/computer_vision/optical_flow_estimation/kitti_flow.cpp
@@ -1,24 +1,48 @@
 #include "datasets/computer_vision/optical_flow_estimation/kitti_flow.h"
 
-namespace xt::data::datasets {
-
-
+namespace xt::data::datasets
+{
     // ---------------------- KittiFlow ---------------------- //
-    KittiFlow::KittiFlow(const std::string &root): KittiFlow::KittiFlow(root, DataMode::TRAIN, false) {
+
+    KittiFlow::KittiFlow(const std::string& root): KittiFlow::KittiFlow(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    KittiFlow::KittiFlow(const std::string &root, DataMode mode): KittiFlow::KittiFlow(root, mode, false) {
+    KittiFlow::KittiFlow(const std::string& root, xt::datasets::DataMode mode): KittiFlow::KittiFlow(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    KittiFlow::KittiFlow(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("KittiFlow: KittiFlow not implemented");
+    KittiFlow::KittiFlow(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        KittiFlow::KittiFlow(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    KittiFlow::KittiFlow(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : KittiFlow::KittiFlow(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    KittiFlow::KittiFlow(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    KittiFlow::KittiFlow(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("KittiFlow: KittiFlow not implemented");
+    void KittiFlow::load_data()
+    {
+
     }
 
+    void KittiFlow::check_resources()
+    {
 
+    }
 }
