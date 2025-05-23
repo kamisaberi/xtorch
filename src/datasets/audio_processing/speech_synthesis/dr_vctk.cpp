81404faf7331343aa79155d5587685fa25418d01
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:18:38 +0330

    datasets base files changed

--- src/datasets/audio_processing/speech_synthesis/dr_vctk.cpp
+++ src/datasets/audio_processing/speech_synthesis/dr_vctk.cpp
@@ -1,22 +1,49 @@
-#include "../../../../include/datasets/audio_processing/speech_synthesis/dr_vctk.h"
+#include "datasets/audio_processing/speech_synthesis/dr_vctk.h"
 
-namespace xt::data::datasets {
 
-    VCTK::VCTK(const std::string &root): VCTK::VCTK(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- VCTK ---------------------- //
+
+    VCTK::VCTK(const std::string& root): VCTK::VCTK(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    VCTK::VCTK(const std::string &root, DataMode mode): VCTK::VCTK(root, mode, false) {
+    VCTK::VCTK(const std::string& root, xt::datasets::DataMode mode): VCTK::VCTK(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    VCTK::VCTK(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("VCTK: VCTK not implemented");
+    VCTK::VCTK(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        VCTK::VCTK(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    VCTK::VCTK(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : VCTK::VCTK(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    VCTK::VCTK(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    VCTK::VCTK(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("VCTK: VCTK not implemented");
+    void VCTK::load_data()
+    {
+
     }
 
+    void VCTK::check_resources()
+    {
 
+    }
 }
