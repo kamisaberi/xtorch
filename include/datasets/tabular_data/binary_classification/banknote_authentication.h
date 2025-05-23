260c33ddb50aa6dadbc3d6f05ea482c356f07999
kamisaberi <kamisaberi@gmail.com>
2025-05-24 03:19:53 +0330

    dataset base files updated

--- include/datasets/tabular_data/binary_classification/banknote_authentication.h
+++ include/datasets/tabular_data/binary_classification/banknote_authentication.h
@@ -7,15 +7,15 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets
 {
-    class ModelNet40 : public xt::datasets::Dataset
+    class BanknoteAuthentication : public xt::datasets::Dataset
     {
     public :
-        explicit ModelNet40(const std::string& root);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        explicit BanknoteAuthentication(const std::string& root);
+        BanknoteAuthentication(const std::string& root, xt::datasets::DataMode mode);
+        BanknoteAuthentication(const std::string& root, xt::datasets::DataMode mode, bool download);
+        BanknoteAuthentication(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        BanknoteAuthentication(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer,
                    std::unique_ptr<xt::Module> target_transformer);
 
--- src/datasets/tabular_data/binary_classification/banknote_authentication.cpp
+++ src/datasets/tabular_data/binary_classification/banknote_authentication.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/3d_point_cloud_analysis/model_net40.h"
+#include "datasets/tabular_data/binary_classification/banknote_authentication.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- BanknoteAuthentication ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    BanknoteAuthentication::BanknoteAuthentication(const std::string& root): BanknoteAuthentication::BanknoteAuthentication(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    BanknoteAuthentication::BanknoteAuthentication(const std::string& root, xt::datasets::DataMode mode): BanknoteAuthentication::BanknoteAuthentication(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    BanknoteAuthentication::BanknoteAuthentication(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        BanknoteAuthentication::BanknoteAuthentication(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    BanknoteAuthentication::BanknoteAuthentication(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : BanknoteAuthentication::BanknoteAuthentication(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    BanknoteAuthentication::BanknoteAuthentication(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void BanknoteAuthentication::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void BanknoteAuthentication::check_resources()
     {
 
     }
