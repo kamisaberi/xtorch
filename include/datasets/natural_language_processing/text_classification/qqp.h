68f52a63f572a0a376a8f6c8fe4e6ab6dd524f16
kamisaberi <kamisaberi@gmail.com>
2025-05-23 04:46:23 +0330

    datasets base files changed

--- include/datasets/natural_language_processing/text_classification/qqp.h
+++ include/datasets/natural_language_processing/text_classification/qqp.h
@@ -1,16 +1,33 @@
 #pragma once
-#include "datasets/base/base.h"
 
-namespace xt::data::datasets {
-    class QQP : BaseDataset {
-        public :
-            explicit QQP(const std::string &root);
-        QQP(const std::string &root, DataMode mode);
-        QQP(const std::string &root, DataMode mode , bool download);
-        QQP(const std::string &root, DataMode mode , bool download, TransformType transforms);
+#include "datasets/common.h"
 
-        private :
-            void load_data();
+using namespace std;
+namespace fs = std::filesystem;
+
+namespace xt::data::datasets
+{
+    class QQP : public xt::datasets::Dataset
+    {
+    public :
+        explicit QQP(const std::string& root);
+        QQP(const std::string& root, xt::datasets::DataMode mode);
+        QQP(const std::string& root, xt::datasets::DataMode mode, bool download);
+        QQP(const std::string& root, xt::datasets::DataMode mode, bool download,
+            std::unique_ptr<xt::Module> transformer);
+        QQP(const std::string& root, xt::datasets::DataMode mode, bool download,
+            std::unique_ptr<xt::Module> transformer,
+            std::unique_ptr<xt::Module> target_transformer);
+
+    private :
+        // TODO fs::path dataset_folder_name
+        fs::path dataset_folder_name = "?";
+
+        bool download = false;
+        fs::path root;
+        fs::path dataset_path;
+
+        void load_data();
 
         void check_resources();
     };
--- src/datasets/natural_language_processing/text_classification/qqp.cpp
+++ src/datasets/natural_language_processing/text_classification/qqp.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/text_classification/qqp.h"
 
-namespace xt::data::datasets {
 
-    QQP::QQP(const std::string &root): QQP::QQP(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- QQP ---------------------- //
+
+    QQP::QQP(const std::string& root): QQP::QQP(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    QQP::QQP(const std::string &root, DataMode mode): QQP::QQP(root, mode, false) {
+    QQP::QQP(const std::string& root, xt::datasets::DataMode mode): QQP::QQP(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    QQP::QQP(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("QQP: QQP not implemented");
+    QQP::QQP(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        QQP::QQP(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    QQP::QQP(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : QQP::QQP(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    QQP::QQP(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    QQP::QQP(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("QQP: QQP not implemented");
+    void QQP::load_data()
+    {
+
     }
 
+    void QQP::check_resources()
+    {
 
+    }
 }
