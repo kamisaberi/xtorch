6310fa01bd249707b67844604e48e2905230786e
kamisaberi <kamisaberi@gmail.com>
2025-05-23 06:12:17 +0330

    datasets base files changed

--- src/datasets/natural_language_processing/machine_translation/iwslt2017.cpp
+++ src/datasets/natural_language_processing/machine_translation/iwslt2017.cpp
@@ -1,22 +1,49 @@
 #include "datasets/natural_language_processing/machine_translation/iwslt2017.h"
 
-namespace xt::data::datasets {
 
-    IWSLT2017::IWSLT2017(const std::string &root): IWSLT2017::IWSLT2017(root, DataMode::TRAIN, false) {
+namespace xt::data::datasets
+{
+    // ---------------------- IWSLT2017 ---------------------- //
+
+    IWSLT2017::IWSLT2017(const std::string& root): IWSLT2017::IWSLT2017(
+        root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
+    {
     }
 
-    IWSLT2017::IWSLT2017(const std::string &root, DataMode mode): IWSLT2017::IWSLT2017(root, mode, false) {
+    IWSLT2017::IWSLT2017(const std::string& root, xt::datasets::DataMode mode): IWSLT2017::IWSLT2017(
+        root, mode, false, nullptr, nullptr)
+    {
     }
 
-    IWSLT2017::IWSLT2017(const std::string &root, DataMode mode, bool download) : BaseDataset(root, mode, download) {
-        throw std::runtime_error("IWSLT: IWSLT not implemented");
+    IWSLT2017::IWSLT2017(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        IWSLT2017::IWSLT2017(
+            root, mode, download, nullptr, nullptr)
+    {
+    }
+
+    IWSLT2017::IWSLT2017(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : IWSLT2017::IWSLT2017(
+        root, mode, download, std::move(transformer), nullptr)
+    {
+    }
+
+    IWSLT2017::IWSLT2017(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
+        xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
+    {
+        check_resources();
+        load_data();
+
     }
 
 
-    IWSLT2017::IWSLT2017(const std::string &root, DataMode mode, bool download,
-                           TransformType transforms) : BaseDataset(root, mode, download, transforms) {
-        throw std::runtime_error("IWSLT: IWSLT not implemented");
+    void IWSLT2017::load_data()
+    {
+
     }
 
+    void IWSLT2017::check_resources()
+    {
 
+    }
 }
