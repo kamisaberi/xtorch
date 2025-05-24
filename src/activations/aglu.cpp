2f50869aed3bd96ad1dded404aaf0d14aeed5262
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 14:16:25 +0330

    include path updated

--- src/activations/aglu.cpp
+++ src/activations/aglu.cpp
@@ -1,11 +1,10 @@
 #include "include/activations/aglu.h"
 
-namespace xt::activations
-{
-    torch::Tensor aglu(torch::Tensor x){
+namespace xt::activations {
+    torch::Tensor aglu(torch::Tensor x) {
     }
 
-    torch::Tensor AGLU::forward(torch::Tensor x) const{
+    torch::Tensor AGLU::forward(torch::Tensor x) const {
         return xt::activations::aglu(x);
     }
 
