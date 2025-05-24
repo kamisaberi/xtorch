fbf8f9e0f5ff4ef0f1edcf4eecc90d267aec5c9c
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 11:01:32 +0330

    dataset files base updated

--- include/datasets/tabular_data/classification/yeast.h
+++ include/datasets/tabular_data/classification/yeast.h
@@ -7,15 +7,15 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets
 {
-    class ModelNet40 : public xt::datasets::Dataset
+    class Yeast : public xt::datasets::Dataset
     {
     public :
-        explicit ModelNet40(const std::string& root);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        explicit Yeast(const std::string& root);
+        Yeast(const std::string& root, xt::datasets::DataMode mode);
+        Yeast(const std::string& root, xt::datasets::DataMode mode, bool download);
+        Yeast(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        Yeast(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer,
                    std::unique_ptr<xt::Module> target_transformer);
 
