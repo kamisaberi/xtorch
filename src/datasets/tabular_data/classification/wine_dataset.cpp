076b1d5b95a1c8a3b8d10984edb9bba94b1b2306
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 11:01:05 +0330

    dataset files base updated

--- src/datasets/tabular_data/classification/wine_dataset.cpp
+++ src/datasets/tabular_data/classification/wine_dataset.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/3d_point_cloud_analysis/model_net40.h"
+#include "datasets/tabular_data/classification/wine_dataset.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- WineDataset ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    WineDataset::WineDataset(const std::string& root): WineDataset::WineDataset(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    WineDataset::WineDataset(const std::string& root, xt::datasets::DataMode mode): WineDataset::WineDataset(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    WineDataset::WineDataset(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        WineDataset::WineDataset(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    WineDataset::WineDataset(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : WineDataset::WineDataset(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    WineDataset::WineDataset(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void WineDataset::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void WineDataset::check_resources()
     {
 
     }
