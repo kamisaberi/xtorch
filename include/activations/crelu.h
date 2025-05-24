049fd10f4fb4b7603e9b851a1d374358ad46a03b
kamisaberi <kamisaberi@gmail.com>
2025-05-25 00:05:50 +0330

    activation crelu created

--- include/activations/crelu.h
+++ include/activations/crelu.h
@@ -4,10 +4,15 @@
 
 namespace xt::activations
 {
+    torch::Tensor crelu(torch::Tensor x);
+
     struct CReLU : xt::Module
     {
+    public:
+        CReLU() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
-
-
-
