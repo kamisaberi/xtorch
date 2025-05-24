11ec23f9b5dd003727d694d66e3b06fc3760ae2c
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 10:51:11 +0330

    dataset files base updated

--- src/datasets/tabular_data/classification/balance_scale.cpp
+++ src/datasets/tabular_data/classification/balance_scale.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/3d_point_cloud_analysis/model_net40.h"
+#include "datasets/tabular_data/classification/balance_scale.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- BalanceScale ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    BalanceScale::BalanceScale(const std::string& root): BalanceScale::BalanceScale(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    BalanceScale::BalanceScale(const std::string& root, xt::datasets::DataMode mode): BalanceScale::BalanceScale(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    BalanceScale::BalanceScale(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        BalanceScale::BalanceScale(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    BalanceScale::BalanceScale(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : BalanceScale::BalanceScale(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    BalanceScale::BalanceScale(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void BalanceScale::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void BalanceScale::check_resources()
     {
 
     }
