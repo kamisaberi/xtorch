4524ea37b0d42f2f5381e2d5dfc157c7e390ba89
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 15:01:56 +0330

    activation asu created

--- src/activations/asu.cpp
+++ src/activations/asu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/asu.h"
+
+namespace xt::activations {
+    torch::Tensor asu(torch::Tensor x) {
+    }
+
+    torch::Tensor ASU::forward(torch::Tensor x) const {
+        return xt::activations::asu(x);
+    }
+
+}
