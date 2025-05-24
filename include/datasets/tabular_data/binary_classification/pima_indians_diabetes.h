5959fccbb8051d35e1852120b524d4ab6fd22676
kamisaberi <kamisaberi@gmail.com>
2025-05-24 03:37:19 +0330

    dataset base files updated

--- include/datasets/tabular_data/binary_classification/pima_indians_diabetes.h
+++ include/datasets/tabular_data/binary_classification/pima_indians_diabetes.h
@@ -7,15 +7,15 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets
 {
-    class ModelNet40 : public xt::datasets::Dataset
+    class PimaIndiansDiabetes : public xt::datasets::Dataset
     {
     public :
-        explicit ModelNet40(const std::string& root);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        explicit PimaIndiansDiabetes(const std::string& root);
+        PimaIndiansDiabetes(const std::string& root, xt::datasets::DataMode mode);
+        PimaIndiansDiabetes(const std::string& root, xt::datasets::DataMode mode, bool download);
+        PimaIndiansDiabetes(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        PimaIndiansDiabetes(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer,
                    std::unique_ptr<xt::Module> target_transformer);
 
--- src/datasets/tabular_data/binary_classification/pima_indians_diabetes.cpp
+++ src/datasets/tabular_data/binary_classification/pima_indians_diabetes.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/3d_point_cloud_analysis/model_net40.h"
+#include "datasets/tabular_data/binary_classification/pima_indians_diabetes.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- PimaIndiansDiabetes ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    PimaIndiansDiabetes::PimaIndiansDiabetes(const std::string& root): PimaIndiansDiabetes::PimaIndiansDiabetes(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    PimaIndiansDiabetes::PimaIndiansDiabetes(const std::string& root, xt::datasets::DataMode mode): PimaIndiansDiabetes::PimaIndiansDiabetes(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    PimaIndiansDiabetes::PimaIndiansDiabetes(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        PimaIndiansDiabetes::PimaIndiansDiabetes(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    PimaIndiansDiabetes::PimaIndiansDiabetes(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : PimaIndiansDiabetes::PimaIndiansDiabetes(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    PimaIndiansDiabetes::PimaIndiansDiabetes(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void PimaIndiansDiabetes::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void PimaIndiansDiabetes::check_resources()
     {
 
     }
