d9a12ce68dd42eae1e3de153ef1ab58821341282
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 14:39:14 +0330

    include path updated

--- src/activations/andhra.cpp
+++ src/activations/andhra.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/andhra.h"
+
+namespace xt::activations {
+    torch::Tensor andhra(torch::Tensor x) {
+    }
+
+    torch::Tensor ANDHRA::forward(torch::Tensor x) const {
+        return xt::activations::andhra(x);
+    }
+
+}
