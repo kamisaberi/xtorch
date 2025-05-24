934b5a7a9a5d6a164862062c5a2824a2c207df38
kamisaberi <kamisaberi@gmail.com>
2025-05-25 00:42:31 +0330

    activation evo_norms created

--- src/activations/elish.cpp
+++ src/activations/elish.cpp
@@ -1,11 +1,13 @@
 #include "include/activations/elish.h"
 
-namespace xt::activations {
-    torch::Tensor elish(torch::Tensor x) {
+namespace xt::activations
+{
+    torch::Tensor elish(torch::Tensor x)
+    {
     }
 
-    torch::Tensor ELiSH::forward(torch::Tensor x) const {
+    torch::Tensor ELiSH::forward(torch::Tensor x) const
+    {
         return xt::activations::elish(x);
     }
-
 }
