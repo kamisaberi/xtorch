6efb305e5e6b98469fd22ebc6425b490f06f56d7
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 13:35:17 +0330

    include path updated

--- src/datasets/natural_language_processing/text_classification/stsb.cpp
+++ src/datasets/natural_language_processing/text_classification/stsb.cpp
@@ -1,4 +1,4 @@
-#include "datasets/natural_language_processing/text_classification/stsb.h"
+#include "include/datasets/natural_language_processing/text_classification/stsb.h"
 
 namespace xt::data::datasets
 {
