9af8d03d20135ad836444091f94d96bcdaaadc88
kamisaberi <kamisaberi@gmail.com>
2025-05-25 00:55:27 +0330

    activation gcu created

--- src/activations/gcu.cpp
+++ src/activations/gcu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/gcu.h"
+
+namespace xt::activations {
+    torch::Tensor gcu(torch::Tensor x) {
+    }
+
+    torch::Tensor GCU::forward(torch::Tensor x) const {
+        return xt::activations::gcu(x);
+    }
+
+}
