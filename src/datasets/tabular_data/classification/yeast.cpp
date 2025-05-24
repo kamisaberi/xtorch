007a3be17db0466ebf9c207fb5250d1456f8504d
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 11:01:58 +0330

    dataset files base updated

--- src/datasets/tabular_data/classification/yeast.cpp
+++ src/datasets/tabular_data/classification/yeast.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/3d_point_cloud_analysis/model_net40.h"
+#include "datasets/tabular_data/classification/yeast.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- Yeast ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    Yeast::Yeast(const std::string& root): Yeast::Yeast(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    Yeast::Yeast(const std::string& root, xt::datasets::DataMode mode): Yeast::Yeast(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    Yeast::Yeast(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        Yeast::Yeast(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    Yeast::Yeast(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : Yeast::Yeast(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    Yeast::Yeast(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void Yeast::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void Yeast::check_resources()
     {
 
     }
