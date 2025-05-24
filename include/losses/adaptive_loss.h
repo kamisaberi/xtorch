2f33f30d89ff6c780e3d3bb89e162c64b3467f6a
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 12:49:49 +0330

    include path updated

--- include/losses/adaptive_loss.h
+++ include/losses/adaptive_loss.h
@@ -1,13 +1,17 @@
 #pragma once
+
 #include "common.h"
 
 
-namespace xt::losses
-{
-    class AdaptiveLoss : xt::Module
-    {
+namespace xt::losses {
+    class AdaptiveLoss : xt::Module {
     public:
+        AdaptiveLoss() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
+
+    torch::Tensor adaptive_loss(torch::Tensor x);
 }
