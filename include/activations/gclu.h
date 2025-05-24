e039e4d8090bdf1e7762ea45adf66ba6ee8a5901
kamisaberi <kamisaberi@gmail.com>
2025-05-25 00:52:59 +0330

    activation gclu created

--- include/activations/gclu.h
+++ include/activations/gclu.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct GCLU	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor gclu(torch::Tensor x);
+
+    struct GCLU : xt::Module {
+    public:
+        GCLU() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
--- src/activations/gclu.cpp
+++ src/activations/gclu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/gclu.h"
+
+namespace xt::activations {
+    torch::Tensor gclu(torch::Tensor x) {
+    }
+
+    torch::Tensor GCLU::forward(torch::Tensor x) const {
+        return xt::activations::gclu(x);
+    }
+
+}
