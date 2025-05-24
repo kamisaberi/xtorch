506eb5093896507a62126a66b83521495438cf46
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 11:05:31 +0330

    dataset files base updated

--- src/datasets/tabular_data/regression_classification/abalone.cpp
+++ src/datasets/tabular_data/regression_classification/abalone.cpp
@@ -1,32 +1,32 @@
-#include "datasets/computer_vision/3d_point_cloud_analysis/model_net40.h"
+#include "datasets/tabular_data/regression_classification/abalone.h"
 
 namespace xt::data::datasets
 {
-    // ---------------------- ModelNet40 ---------------------- //
+    // ---------------------- Abalone ---------------------- //
 
-    ModelNet40::ModelNet40(const std::string& root): ModelNet40::ModelNet40(
+    Abalone::Abalone(const std::string& root): Abalone::Abalone(
         root, xt::datasets::DataMode::TRAIN, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode): ModelNet40::ModelNet40(
+    Abalone::Abalone(const std::string& root, xt::datasets::DataMode mode): Abalone::Abalone(
         root, mode, false, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download) :
-        ModelNet40::ModelNet40(
+    Abalone::Abalone(const std::string& root, xt::datasets::DataMode mode, bool download) :
+        Abalone::Abalone(
             root, mode, download, nullptr, nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
-                           std::unique_ptr<xt::Module> transformer) : ModelNet40::ModelNet40(
+    Abalone::Abalone(const std::string& root, xt::datasets::DataMode mode, bool download,
+                           std::unique_ptr<xt::Module> transformer) : Abalone::Abalone(
         root, mode, download, std::move(transformer), nullptr)
     {
     }
 
-    ModelNet40::ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+    Abalone::Abalone(const std::string& root, xt::datasets::DataMode mode, bool download,
                            std::unique_ptr<xt::Module> transformer, std::unique_ptr<xt::Module> target_transformer):
         xt::datasets::Dataset(mode, std::move(transformer), std::move(target_transformer))
     {
@@ -36,12 +36,12 @@ namespace xt::data::datasets
     }
 
 
-    void ModelNet40::load_data()
+    void Abalone::load_data()
     {
 
     }
 
-    void ModelNet40::check_resources()
+    void Abalone::check_resources()
     {
 
     }
