8fffff3ce5ec67a559c85dc94ea42fc1b26eaa01
kamran saberifard <kamisaberi@gmail.com>
2025-05-24 11:03:52 +0330

    dataset files base updated

--- include/datasets/tabular_data/regression/boston_housing.h
+++ include/datasets/tabular_data/regression/boston_housing.h
@@ -7,15 +7,15 @@ namespace fs = std::filesystem;
 
 namespace xt::data::datasets
 {
-    class ModelNet40 : public xt::datasets::Dataset
+    class BostonHousing : public xt::datasets::Dataset
     {
     public :
-        explicit ModelNet40(const std::string& root);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        explicit BostonHousing(const std::string& root);
+        BostonHousing(const std::string& root, xt::datasets::DataMode mode);
+        BostonHousing(const std::string& root, xt::datasets::DataMode mode, bool download);
+        BostonHousing(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer);
-        ModelNet40(const std::string& root, xt::datasets::DataMode mode, bool download,
+        BostonHousing(const std::string& root, xt::datasets::DataMode mode, bool download,
                    std::unique_ptr<xt::Module> transformer,
                    std::unique_ptr<xt::Module> target_transformer);
 
