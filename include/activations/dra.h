04b23e6ea8e67859590206d2bb06e62ec75fa546
kamisaberi <kamisaberi@gmail.com>
2025-05-25 00:16:00 +0330

    activation delu created

--- include/activations/dra.h
+++ include/activations/dra.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct DRA : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor dra(torch::Tensor x);
+
+    struct DRA : xt::Module {
+    public:
+        DRA() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
--- src/activations/dra.cpp
+++ src/activations/dra.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/aglu.h"
+
+namespace xt::activations {
+    torch::Tensor dra(torch::Tensor x) {
+    }
+
+    torch::Tensor DRA::forward(torch::Tensor x) const {
+        return xt::activations::dra(x);
+    }
+
+}
