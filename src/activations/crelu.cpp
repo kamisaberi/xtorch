318cd67185d0a5fe77cae4bd8257e0c4b512f987
kamisaberi <kamisaberi@gmail.com>
2025-05-25 00:05:50 +0330

    activation crelu created

--- src/activations/crelu.cpp
+++ src/activations/crelu.cpp
@@ -1,3 +1,13 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/crelu.h"
+
+namespace xt::activations
+{
+    torch::Tensor crelu(torch::Tensor x)
+    {
+    }
+
+    torch::Tensor CReLU::forward(torch::Tensor x) const
+    {
+        return xt::activations::crelu(x);
+    }
+}
