514ca7ca5ee380c97561fe627cfe560101b2f242
kamisaberi <kamisaberi@gmail.com>
2025-05-25 00:04:31 +0330

    activation coslu created

--- src/activations/coslu.cpp
+++ src/activations/coslu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/coslu.h"
+
+namespace xt::activations {
+    torch::Tensor coslu(torch::Tensor x) {
+    }
+
+    torch::Tensor CosLU::forward(torch::Tensor x) const {
+        return xt::activations::coslu(x);
+    }
+
+}
