4c5aed9013a8e5cc197b49c529cee7a8f0f77a5a
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:41:11 +0330

    dropout scheduled_drop_path created

--- include/dropouts/scheduled_drop_path.h
+++ include/dropouts/scheduled_drop_path.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor scheduled_drop_path(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class ScheduledDropPath : xt::Module
-    {
+    struct ScheduledDropPath : xt::Module {
     public:
+        ScheduledDropPath() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
