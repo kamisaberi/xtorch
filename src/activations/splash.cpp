f894c4a726c06f253bc6d20068a3bd9a736aa328
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:43:39 +0330

    activation splash created

--- src/activations/splash.cpp
+++ src/activations/splash.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/splash.h"
+
+namespace xt::activations {
+    torch::Tensor splash(torch::Tensor x) {
+    }
+
+    torch::Tensor SPLASH::forward(torch::Tensor x) const {
+        return xt::activations::splash(x);
+    }
+
+}
