821f77405a54c2b8fdc40ab07328528a8c227aec
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:35:49 +0330

    dropout fraternal_dropout created

--- include/dropouts/fraternal_dropout.h
+++ include/dropouts/fraternal_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor fraternal_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class FraternalDropout : xt::Module
-    {
+    struct FraternalDropout : xt::Module {
     public:
+        FraternalDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
