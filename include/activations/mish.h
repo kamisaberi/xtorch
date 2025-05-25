52604008fcb2db9030996a1b2f52415b6d172ed2
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:29:53 +0330

    activation maxout created

--- include/activations/mish.h
+++ include/activations/mish.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct Mish	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor mish(torch::Tensor x);
+
+    struct Mish : xt::Module {
+    public:
+        Mish() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
