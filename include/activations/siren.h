493f287fb535c3d0762f0ff89689229ab073588d
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:37:40 +0330

    activation siren created

--- include/activations/siren.h
+++ include/activations/siren.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct Siren	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor siren(torch::Tensor x);
+
+    struct Siren : xt::Module {
+    public:
+        Siren() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
