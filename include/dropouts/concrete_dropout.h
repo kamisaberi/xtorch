a736812f82ab8ce7a3644aeed7d52cb82d80d782
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:24:24 +0330

    dropout concrete_dropout created

--- include/dropouts/concrete_dropout.h
+++ include/dropouts/concrete_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor concrete_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class ConcreteDropout : xt::Module
-    {
+    struct ConcreteDropout : xt::Module {
     public:
+        ConcreteDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
