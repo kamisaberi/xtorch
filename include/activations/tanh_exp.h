00f46ddfebac494428a403d4e61768603cb25440
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:57:12 +0330

    activation tanh_exp created

--- include/activations/tanh_exp.h
+++ include/activations/tanh_exp.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct TanhExp	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor tanh_exp(torch::Tensor x);
+
+    struct TanhExp : xt::Module {
+    public:
+        TanhExp() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
--- src/activations/tanh_exp.cpp
+++ src/activations/tanh_exp.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/tanh_exp.h"
+
+namespace xt::activations {
+    torch::Tensor tanh_exp(torch::Tensor x) {
+    }
+
+    torch::Tensor TanhExp::forward(torch::Tensor x) const {
+        return xt::activations::tanh_exp(x);
+    }
+
+}
