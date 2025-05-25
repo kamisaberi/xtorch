64e365ec0460f0adff9b6bc92ef679270896df60
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:29:06 +0330

    dropout early_dropout created

--- include/dropouts/early_dropout.h
+++ include/dropouts/early_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor early_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class EarlyDropout : xt::Module
-    {
+    struct EarlyDropout : xt::Module {
     public:
+        EarlyDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
