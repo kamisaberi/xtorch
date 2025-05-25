21fcb05c31107246ea4040548e926f554ace351b
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:48:10 +0330

    activation norm_lin_comb created

--- src/activations/norm_lin_comb.cpp
+++ src/activations/norm_lin_comb.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/norm_lin_comb.h"
+
+namespace xt::activations {
+    torch::Tensor norm_lin_comb(torch::Tensor x) {
+    }
+
+    torch::Tensor NormLinComb::forward(torch::Tensor x) const {
+        return xt::activations::norm_lin_comb(x);
+    }
+
+}
