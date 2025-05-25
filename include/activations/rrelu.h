c058fcd9075a80e988b1596b3ab2a5993cbfdc52
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 16:29:41 +0330

    activation rrelu created

--- include/activations/rrelu.h
+++ include/activations/rrelu.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct RReLU	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor rrelu(torch::Tensor x);
+
+    struct RReLU : xt::Module {
+    public:
+        RReLU() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
