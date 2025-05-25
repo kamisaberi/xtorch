994581434376e6ca5c4e626cae0506d724cb5290
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:29:41 +0330

    dropout embedding_dropout created

--- include/dropouts/embedding_dropout.h
+++ include/dropouts/embedding_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor embedding_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class EmbeddingDropout : xt::Module
-    {
+    struct EmbeddingDropout : xt::Module {
     public:
+        EmbeddingDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
