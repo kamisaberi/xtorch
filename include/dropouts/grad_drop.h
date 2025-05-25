e24917087dfee0d830f254ce95031d539cefdc72
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:37:42 +0330

    dropout GradDrop created

--- include/dropouts/grad_drop.h
+++ include/dropouts/grad_drop.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor grad_drop(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class GradDrop : xt::Module
-    {
+    struct GradDrop : xt::Module {
     public:
+        GradDrop() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
