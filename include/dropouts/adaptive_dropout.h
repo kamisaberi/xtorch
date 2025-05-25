ea70c745cee060d23e6be2d0f990566ee850a25f
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:54:48 +0330

    dropout temporal_dropout created

--- include/dropouts/adaptive_dropout.h
+++ include/dropouts/adaptive_dropout.h
@@ -3,11 +3,11 @@
 #include "common.h"
 
 namespace xt::dropouts {
-    torch::Tensor adaptive_dropout(torch::Tensor x);
+    torch::Tensor temporal_dropout(torch::Tensor x);
 
-    struct AdaptiveDropout : xt::Module {
+    struct TemporalDropout : xt::Module {
     public:
-        AdaptiveDropout() = default;
+        TemporalDropout() = default;
 
         torch::Tensor forward(torch::Tensor x) const override;
 
