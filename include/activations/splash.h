2abfed85c9033b3a25eb3b2cac0161848bafa368
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:43:39 +0330

    activation splash created

--- include/activations/splash.h
+++ include/activations/splash.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct SPLASH	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor splash(torch::Tensor x);
+
+    struct SPLASH : xt::Module {
+    public:
+        SPLASH() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
