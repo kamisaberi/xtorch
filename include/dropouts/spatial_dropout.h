f5a5e1bbb6d123a731b0df471c4425bb7394261d
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:53:03 +0330

    dropout spatial_dropout created

--- include/dropouts/spatial_dropout.h
+++ include/dropouts/spatial_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor spatial_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class SpatialDropout : xt::Module
-    {
+    struct SpatialDropout : xt::Module {
     public:
+        SpatialDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
