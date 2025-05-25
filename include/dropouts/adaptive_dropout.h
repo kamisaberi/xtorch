ec17d36aef6638a922d80350b90ae43e19f5c298
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:16:40 +0330

    dropout adaptive_dropout created

--- include/dropouts/adaptive_dropout.h
+++ include/dropouts/adaptive_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor adaptive_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class AdaptiveDropout : xt::Module
-    {
+    struct AdaptiveDropout : xt::Module {
     public:
+        AdaptiveDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
