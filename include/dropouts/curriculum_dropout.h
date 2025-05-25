335da8e83ab7ae500f1c4c707b0130777c519be1
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:25:22 +0330

    dropout curriculum_dropout created

--- include/dropouts/curriculum_dropout.h
+++ include/dropouts/curriculum_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor curriculum_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class CurriculumDropout : xt::Module
-    {
+    struct CurriculumDropout : xt::Module {
     public:
+        CurriculumDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
