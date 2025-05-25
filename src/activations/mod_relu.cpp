daf73ca96e3a555e59263eaf80c93fdc8591eb58
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:31:50 +0330

    activation mod_relu created

--- src/activations/mod_relu.cpp
+++ src/activations/mod_relu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/mod_relu.h"
+
+namespace xt::activations {
+    torch::Tensor mod_relu(torch::Tensor x) {
+    }
+
+    torch::Tensor ModReLU::forward(torch::Tensor x) const {
+        return xt::activations::mod_relu(x);
+    }
+
+}
