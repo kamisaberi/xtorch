f99b8b2c55faaee17ab7f84f937b19ba8407aed8
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:40:45 +0330

    activation smooth_step created

--- include/activations/smooth_step.h
+++ include/activations/smooth_step.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct SmoothStep	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor smooth_step(torch::Tensor x);
+
+    struct SmoothStep : xt::Module {
+    public:
+        SmoothStep() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
