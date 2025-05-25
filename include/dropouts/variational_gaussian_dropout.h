8c638d2caa79f91edf445dd897caafa56819d176
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:56:13 +0330

    dropout variational_gaussian_dropout created

--- include/dropouts/variational_gaussian_dropout.h
+++ include/dropouts/variational_gaussian_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor variational_gaussian_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class VariationalGaussianDropout : xt::Module
-    {
+    struct VariationalGaussianDropout : xt::Module {
     public:
+        VariationalGaussianDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
