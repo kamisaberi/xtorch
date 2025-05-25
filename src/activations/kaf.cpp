c26f805de49150949baeda7afd327848c9c2ebed
kamisaberi <kamisaberi@gmail.com>
2025-05-25 09:46:37 +0330

    activation kaf created

--- src/activations/kaf.cpp
+++ src/activations/kaf.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/kaf.h"
+
+namespace xt::activations {
+    torch::Tensor kaf(torch::Tensor x) {
+    }
+
+    torch::Tensor KAF::forward(torch::Tensor x) const {
+        return xt::activations::kaf(x);
+    }
+
+}
