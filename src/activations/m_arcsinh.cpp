2ccb88b84ffd2094ee5a929c9483e0acd601454a
kamisaberi <kamisaberi@gmail.com>
2025-05-25 09:54:37 +0330

    activation MArcsinh created

--- src/activations/m_arcsinh.cpp
+++ src/activations/m_arcsinh.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/m_arcsinh.h"
+
+namespace xt::activations {
+    torch::Tensor m_arcsinh(torch::Tensor x) {
+    }
+
+    torch::Tensor MArcsinh::forward(torch::Tensor x) const {
+        return xt::activations::m_arcsinh(x);
+    }
+
+}
