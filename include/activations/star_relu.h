f1a714aefd68c64758ea1d65abb75827c94c52ea
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:50:45 +0330

    activation star_relu created

--- include/activations/star_relu.h
+++ include/activations/star_relu.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct StarReLU	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor star_relu(torch::Tensor x);
+
+    struct StarReLU: xt::Module {
+    public:
+        StarReLU() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
--- src/activations/star_relu.cpp
+++ src/activations/star_relu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/star_relu.h"
+
+namespace xt::activations {
+    torch::Tensor star_relu(torch::Tensor x) {
+    }
+
+    torch::Tensor StarReLU::forward(torch::Tensor x) const {
+        return xt::activations::star_relu(x);
+    }
+
+}
