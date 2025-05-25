3654eedc076063dccae4da10ee8dfc5e791e15c7
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:55:30 +0330

    dropout variational_dropout created

--- include/dropouts/variational_dropout.h
+++ include/dropouts/variational_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor variational_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class VariationalDropout : xt::Module
-    {
+    struct VariationalDropout : xt::Module {
     public:
+        VariationalDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
