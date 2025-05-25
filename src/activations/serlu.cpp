69a87e41b04417c2005cd8c2bdff3c0a2440816a
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:33:16 +0330

    activation serlu created

--- src/activations/serlu.cpp
+++ src/activations/serlu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/serlu.h"
+
+namespace xt::activations {
+    torch::Tensor serlu(torch::Tensor x) {
+    }
+
+    torch::Tensor SERLU::forward(torch::Tensor x) const {
+        return xt::activations::serlu(x);
+    }
+
+}
