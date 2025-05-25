8cd35e59d5fa58837da61d290ddd7660654f0b67
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:54:30 +0330

    activation swish created

--- src/activations/swish.cpp
+++ src/activations/swish.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/swish.h"
+
+namespace xt::activations {
+    torch::Tensor swish(torch::Tensor x) {
+    }
+
+    torch::Tensor Swish::forward(torch::Tensor x) const {
+        return xt::activations::swish(x);
+    }
+
+}
