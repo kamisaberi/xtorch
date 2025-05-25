71787115712290bc072418907ed8ad83748db2db
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:33:08 +0330

    activation nail_or created

--- include/activations/nail_or.h
+++ include/activations/nail_or.h
@@ -2,9 +2,18 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct NailOr : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor nail_or(torch::Tensor x);
+
+    struct NailOr : xt::Module {
+    public:
+        NailOr() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
+
+
+
