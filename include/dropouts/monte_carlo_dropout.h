d61c81ec437e16bdfb81f5dfefac1fa673ee25d5
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:38:49 +0330

    dropout monte_carlo_dropout created

--- include/dropouts/monte_carlo_dropout.h
+++ include/dropouts/monte_carlo_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor monte_carlo_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class MonteCarloDropout : xt::Module
-    {
+    struct MonteCarloDropout : xt::Module {
     public:
+        MonteCarloDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
