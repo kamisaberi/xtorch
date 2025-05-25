e2cd32c060ab4f51cee36ea966d8d88cc1a56161
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:27:11 +0330

    dropout drop_connect created

--- include/dropouts/drop_connect.h
+++ include/dropouts/drop_connect.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor drop_connect(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class DropConnect : xt::Module
-    {
+    struct DropConnect : xt::Module {
     public:
+        DropConnect() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
