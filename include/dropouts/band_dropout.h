40ef25a18a6662a4b8dfaa98dcf03b7ea335af28
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:22:55 +0330

    dropout band_dropout created

--- include/dropouts/band_dropout.h
+++ include/dropouts/band_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor band_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class BandDropout : xt::Module
-    {
+    struct BandDropout : xt::Module {
     public:
+        BandDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
