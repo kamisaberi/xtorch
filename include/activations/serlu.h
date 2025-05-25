f03ed30b45146fc67329edfb0ff38973fac93979
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:33:16 +0330

    activation serlu created

--- include/activations/serlu.h
+++ include/activations/serlu.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct SERLU	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor serlu(torch::Tensor x);
+
+    struct SERLU : xt::Module {
+    public:
+        SERLU() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
