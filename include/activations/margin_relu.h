8ceb1e1cdd35bd222169244934c148e9de00bab0
kamisaberi <kamisaberi@gmail.com>
2025-05-25 10:15:53 +0330

    activation MarginReLU created

--- include/activations/margin_relu.h
+++ include/activations/margin_relu.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct MarginReLU	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor margin_relu(torch::Tensor x);
+
+    struct MarginReLU : xt::Module {
+    public:
+        MarginReLU() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
--- src/activations/margin_relu.cpp
+++ src/activations/margin_relu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/margin_relu.h"
+
+namespace xt::activations {
+    torch::Tensor margin_relu(torch::Tensor x) {
+    }
+
+    torch::Tensor MarginReLU::forward(torch::Tensor x) const {
+        return xt::activations::margin_relu(x);
+    }
+
+}
