7af93a36bc79f8fba481f360290454114834545c
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:22:19 +0330

    dropout auto_dropout created

--- include/dropouts/auto_dropout.h
+++ include/dropouts/auto_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor auto_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class AutoDropout : xt::Module
-    {
+    struct AutoDropout : xt::Module {
     public:
+        AutoDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
