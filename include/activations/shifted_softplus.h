cdc1e6aad5ac45a05964828a07d0041e3ce33f8f
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:34:50 +0330

    activation shifted_softplus created

--- include/activations/shifted_softplus.h
+++ include/activations/shifted_softplus.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct ShiftedSoftplus	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor shifted_softplus(torch::Tensor x);
+
+    struct ShiftedSoftplus : xt::Module {
+    public:
+        ShiftedSoftplus() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
--- src/activations/shifted_softplus.cpp
+++ src/activations/shifted_softplus.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/shifted_softplus.h"
+
+namespace xt::activations {
+    torch::Tensor shifted_softplus(torch::Tensor x) {
+    }
+
+    torch::Tensor ShiftedSoftplus::forward(torch::Tensor x) const {
+        return xt::activations::shifted_softplus(x);
+    }
+
+}
