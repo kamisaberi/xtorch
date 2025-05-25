356556782792e6ae1d4ce1500d8232c43514c45c
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:40:45 +0330

    activation smooth_step created

--- src/activations/smooth_step.cpp
+++ src/activations/smooth_step.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/smooth_step.h"
+
+namespace xt::activations {
+    torch::Tensor smooth_step(torch::Tensor x) {
+    }
+
+    torch::Tensor SmoothStep::forward(torch::Tensor x) const {
+        return xt::activations::smooth_step(x);
+    }
+
+}
