ce57b6dba59db5a094c6a28d1ceec663b8178328
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:55:22 +0330

    activation reglu created

--- include/activations/reglu.h
+++ include/activations/reglu.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct ReGLU	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor reglu(torch::Tensor x);
+
+    struct ReGLU : xt::Module {
+    public:
+        ReGLU() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
