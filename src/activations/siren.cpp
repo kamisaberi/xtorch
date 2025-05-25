41b149ca00b4043f88dbd7c7899e5f6c7e46ff37
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:37:40 +0330

    activation siren created

--- src/activations/siren.cpp
+++ src/activations/siren.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/siren.h"
+
+namespace xt::activations {
+    torch::Tensor siren(torch::Tensor x) {
+    }
+
+    torch::Tensor Siren::forward(torch::Tensor x) const {
+        return xt::activations::siren(x);
+    }
+
+}
