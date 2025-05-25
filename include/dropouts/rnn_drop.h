ff205bb26dd0bec4c2bbe210c8a7181487df4d41
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:40:33 +0330

    dropout rnn_drop created

--- include/dropouts/rnn_drop.h
+++ include/dropouts/rnn_drop.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor rnn_drop(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class RnnDrop : xt::Module
-    {
+    struct RnnDrop : xt::Module {
     public:
+        RnnDrop() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
