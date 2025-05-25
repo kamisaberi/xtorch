9c40e6a5ccd73559ca655989871c099cbf0a35f8
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:50:19 +0330

    activation phish created

--- include/activations/phish.h
+++ include/activations/phish.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct Phish	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor phish(torch::Tensor x);
+
+    struct Phish : xt::Module {
+    public:
+        Phish() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
--- src/activations/phish.cpp
+++ src/activations/phish.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/phish.h"
+
+namespace xt::activations {
+    torch::Tensor phish(torch::Tensor x) {
+    }
+
+    torch::Tensor Phish::forward(torch::Tensor x) const {
+        return xt::activations::phish(x);
+    }
+
+}
