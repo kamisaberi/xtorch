12459546f03a0bdfd9a552aab6781352ec028efe
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:27:45 +0330

    dropout drop_path created

--- include/dropouts/drop_path.h
+++ include/dropouts/drop_path.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor drop_path(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class DropPath : xt::Module
-    {
+    struct DropPath : xt::Module {
     public:
+        DropPath() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
