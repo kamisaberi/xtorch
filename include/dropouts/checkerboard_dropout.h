7ffa616222b831a5d78bffc892476ac66901eac1
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:23:40 +0330

    dropout checkerboard_dropout_dropout created

--- include/dropouts/checkerboard_dropout.h
+++ include/dropouts/checkerboard_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor checkerboard_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class CheckerboardDropout : xt::Module
-    {
+    struct CheckerboardDropout : xt::Module {
     public:
+        CheckerboardDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
