18b35556d8097abf5c96a947622cdf8ba1b594ff
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:45:04 +0330

    activation squared_relu created

--- include/activations/squared_relu.h
+++ include/activations/squared_relu.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct SquaredReLU	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor squared_relu(torch::Tensor x);
+
+    struct SquaredReLU : xt::Module {
+    public:
+        SquaredReLU() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
--- src/activations/squared_relu.cpp
+++ src/activations/squared_relu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/squared_relu.h"
+
+namespace xt::activations {
+    torch::Tensor squared_relu(torch::Tensor x) {
+    }
+
+    torch::Tensor SquaredReLU::forward(torch::Tensor x) const {
+        return xt::activations::squared_relu(x);
+    }
+
+}
