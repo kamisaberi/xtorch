7548a0e77770164f67091786fb15dd1ad4b1ff1a
kamisaberi <kamisaberi@gmail.com>
2025-05-25 09:50:35 +0330

    activation leaf created

--- include/activations/leaf.h
+++ include/activations/leaf.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct LEAF	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor leaf(torch::Tensor x);
+
+    struct LEAF : xt::Module {
+    public:
+        LEAF() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
