dc9cbc4a0fe00aa92550663586d8bcd15549476a
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:54:08 +0330

    activation rational created

--- include/activations/rational.h
+++ include/activations/rational.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct Rational	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor rational(torch::Tensor x);
+
+    struct Rational : xt::Module {
+    public:
+        Rational() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
