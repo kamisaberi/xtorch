7e647387c0bcea695fd78e857f0ebd1907c2cf02
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:55:22 +0330

    activation reglu created

--- src/activations/reglu.cpp
+++ src/activations/reglu.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/reglu.h"
+
+namespace xt::activations {
+    torch::Tensor reglu(torch::Tensor x) {
+    }
+
+    torch::Tensor ReGLU::forward(torch::Tensor x) const {
+        return xt::activations::reglu(x);
+    }
+
+}
