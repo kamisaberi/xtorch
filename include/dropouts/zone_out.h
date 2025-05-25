1935f7045c46ce950132e41414fa172ee6871cfa
kamisaberi <kamisaberi@gmail.com>
2025-05-26 02:09:51 +0330

    dropout zone_out created

--- include/dropouts/zone_out.h
+++ include/dropouts/zone_out.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor zone_out(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class ZoneOut : xt::Module
-    {
+    struct ZoneOut : xt::Module {
     public:
+        ZoneOut() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
