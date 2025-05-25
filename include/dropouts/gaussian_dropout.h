37998f488c903b05812a8800840742116013a8f4
kamisaberi <kamisaberi@gmail.com>
2025-05-26 01:36:38 +0330

    dropout gaussian_dropout created

--- include/dropouts/gaussian_dropout.h
+++ include/dropouts/gaussian_dropout.h
@@ -1,13 +1,19 @@
 #pragma once
+
 #include "common.h"
 
+namespace xt::dropouts {
+    torch::Tensor gaussian_dropout(torch::Tensor x);
 
-namespace xt::dropouts
-{
-    class GaussianDropout : xt::Module
-    {
+    struct GaussianDropout : xt::Module {
     public:
+        GaussianDropout() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
 
     private:
     };
 }
+
+
+
