ebee5816258df3168fe078bc46d860c6561d423d
kamisaberi <kamisaberi@gmail.com>
2025-05-25 09:47:49 +0330

    activation kan created

--- src/activations/kan.cpp
+++ src/activations/kan.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/kan.h"
+
+namespace xt::activations {
+    torch::Tensor kan(torch::Tensor x) {
+    }
+
+    torch::Tensor KAN::forward(torch::Tensor x) const {
+        return xt::activations::kan(x);
+    }
+
+}
