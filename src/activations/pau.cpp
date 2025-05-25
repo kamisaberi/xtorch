092098484841237262f70e00de309d8cb8df7a19
kamran saberifard <kamisaberi@gmail.com>
2025-05-25 13:49:17 +0330

    activation pau created

--- src/activations/pau.cpp
+++ src/activations/pau.cpp
@@ -1,3 +1,11 @@
-//
-// Created by kami on 5/16/25.
-//
+#include "include/activations/pau.h"
+
+namespace xt::activations {
+    torch::Tensor pau(torch::Tensor x) {
+    }
+
+    torch::Tensor PAU::forward(torch::Tensor x) const {
+        return xt::activations::pau(x);
+    }
+
+}
