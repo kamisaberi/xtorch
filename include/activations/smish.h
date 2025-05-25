3a06a0a53748967efb64c78941b4629b5257e1c4
kamisaberi <kamisaberi@gmail.com>
2025-05-26 00:39:31 +0330

    activation smish created

--- include/activations/smish.h
+++ include/activations/smish.h
@@ -2,10 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations
-{
-    struct Smish	 : xt::Module
-    {
+namespace xt::activations {
+    torch::Tensor smish(torch::Tensor x);
+
+    struct Smish : xt::Module {
+    public:
+        Smish() = default;
+
+        torch::Tensor forward(torch::Tensor x) const override;
+
+    private:
     };
 }
 
