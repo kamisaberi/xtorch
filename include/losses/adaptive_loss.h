b141aaa8e7d43437710e313e4df4e14c8e8f9da2
kamisaberi <kamisaberi@gmail.com>
2025-05-26 11:06:44 +0330

    dropout zone_out created

--- include/losses/adaptive_loss.h
+++ include/losses/adaptive_loss.h
@@ -7,7 +7,6 @@ namespace xt::losses {
     class AdaptiveLoss : xt::Module {
     public:
         AdaptiveLoss() = default;
-
         torch::Tensor forward(torch::Tensor x) const override;
 
     private:
