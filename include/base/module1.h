e8134de8ecf9a7875a8853cc7aa69046b8bca4a1
kamisaberi <kamisaberi@gmail.com>
2025-05-27 03:19:55 +0330

    dropout zone_out created

--- include/base/module1.h
+++ include/base/module1.h
@@ -8,16 +8,16 @@ namespace xt
     class Module1 : public torch::nn::Module
     {
     public:
-        // Variadic template forward method (declaration only)
+        // Variadic template forward method (non-virtual, as templates cannot be virtual)
         template <typename... Args>
         torch::Tensor forward(Args... args);
 
-        // Pure virtual forward method for vector of tensors
-        virtual torch::Tensor forward(std::vector<torch::Tensor> tensors) = 0;
-
-        // Virtual destructor for proper cleanup
-        virtual ~Module1() = default;
+        // Pure virtual destructor to make the class abstract
+        ~Module1() override = 0;
     };
+
+    // Pure virtual destructor definition
+    inline Module1::~Module1() = default;
 
     // Explicit template instantiation declarations
     extern template torch::Tensor Module1::forward(torch::Tensor);
