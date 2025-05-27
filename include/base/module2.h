d7de1c5e1fb2f8fdbeecbb97f6302408a5348f46
kamisaberi <kamisaberi@gmail.com>
2025-05-27 10:27:29 +0330

    new xt::Module created

--- include/base/module2.h
+++ include/base/module2.h
@@ -6,18 +6,18 @@
 
 namespace xt {
 
-    class Module : public torch::nn::Module {
+    class Module2 : public torch::nn::Module {
     public:
         // Variadic template forward method with auto return type
         template<typename... Args>
         auto forward(Args... args) -> torch::Tensor;
 
         // Pure virtual destructor to make the class abstract
-        virtual ~Module() = 0;
+        virtual ~Module2() = 0;
     };
 
     // Pure virtual destructor definition
-    inline Module::~Module() = default;
+    inline Module2::~Module2() = default;
 
 } // namespace xt
 
