2fcdc880e1f1ae646b16b6031e9d57942961eae7
kamran saberifard <kamisaberi@gmail.com>
2025-05-28 13:14:37 +0330

    palindrome transfomrsapplier updated

--- src/transforms/appliers/one_of.cpp
+++ src/transforms/appliers/one_of.cpp
@@ -14,7 +14,7 @@ namespace xt::transforms {
         std::vector <torch::Tensor> tensor_vec(tensors);
         input = tensor_vec[0];
         int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
-        input = std::any_cast<torch::Tensor>(transforms[index](std::move(input)));
+        input = std::any_cast<torch::Tensor>(transforms[index]({input}));
         return input;
     }
 
