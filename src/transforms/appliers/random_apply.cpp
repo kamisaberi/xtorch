cfb8a388c58a45ecb4f3c2e8d28109de8416f525
kamran saberifard <kamisaberi@gmail.com>
2025-05-28 13:29:49 +0330

    random_apply transfomrsapplier updated

--- src/transforms/appliers/random_apply.cpp
+++ src/transforms/appliers/random_apply.cpp
@@ -11,20 +11,14 @@ namespace xt::transforms
     }
 
 
-    torch::Tensor RandomApply::forward(torch::Tensor input) const
-    {
-        int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
-        input = transforms[index](std::move(input));
-        return input;
-    }
+    auto RandomApply::forward(std::initializer_list<torch::Tensor> tensors) -> std::any {
 
-
-    auto Compose::forward(std::initializer_list<torch::Tensor> tensors) -> std::any {
+        std::vector<torch::Tensor> tensor_vec(tensors);
+        torch::Tensor input = tensor_vec[0];
 
         int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
-        input = transforms[index](std::move(input));
+        input = std::any_cast<torch::Tensor>(transforms[index]({input}));
         return input;
-
 
     }
 
