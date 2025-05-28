22df2a4e7450935fbf0e194f8fc55a8304d2495b
kamisaberi <kamisaberi@gmail.com>
2025-05-29 02:14:41 +0330

    MOdule class forward method changed

--- src/transforms/appliers/one_of.cpp
+++ src/transforms/appliers/one_of.cpp
@@ -9,10 +9,17 @@ namespace xt::transforms {
     }
 
 
-    auto OneOf::forward(std::initializer_list <torch::Tensor> tensors) -> std::any {
+    auto OneOf::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
 
-        std::vector <torch::Tensor> tensor_vec(tensors);
-        input = tensor_vec[0];
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor input = tensor_vec[0];
         int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
         input = std::any_cast<torch::Tensor>(transforms[index]({input}));
         return input;
