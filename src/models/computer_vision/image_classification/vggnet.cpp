a06c64caa773a734a3b3a14965ae61552db5d063
kamisaberi <kamisaberi@gmail.com>
2025-05-29 01:59:37 +0330

    MOdule class forward method changed

--- src/models/computer_vision/image_classification/vggnet.cpp
+++ src/models/computer_vision/image_classification/vggnet.cpp
@@ -995,8 +995,17 @@ namespace xt::models
     }
 
 
-    torch::Tensor VggNet16::forward(torch::Tensor x) const
+    auto VggNet16::forward(std::initializer_list<std::any> tensors) -> std::any
     {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
         x = layer1->forward(x);
         x = layer2->forward(x);
         x = layer3->forward(x);
@@ -1336,8 +1345,18 @@ namespace xt::models
     }
 
 
-    torch::Tensor VggNet19::forward(torch::Tensor x) const
+
+    auto VggNet19::forward(std::initializer_list<std::any> tensors) -> std::any
     {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
         x = layer1->forward(x);
         x = layer2->forward(x);
         x = layer3->forward(x);
