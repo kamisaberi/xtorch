f06f0cd24160b7c8ddee8f7edf70c1caaa32c5e7
kamran saberifard <kamisaberi@gmail.com>
2025-05-28 12:57:45 +0330

    one_f transfomrsapplier updated

--- src/transforms/appliers/one_of.cpp
+++ src/transforms/appliers/one_of.cpp
@@ -1,29 +1,21 @@
 
 #include "include/transforms/appliers/one_of.h"
 
-namespace xt::transforms
-{
+namespace xt::transforms {
 
     OneOf::OneOf() = default;
 
-    OneOf::OneOf(std::vector<xt::Module> transforms): xt::Module(), transforms(std::move(transforms))
-    {
+    OneOf::OneOf(std::vector <xt::Module> transforms) : xt::Module(), transforms(std::move(transforms)) {
     }
 
 
+    auto Compose::forward(std::initializer_list <torch::Tensor> tensors) -> std::any {
 
-    auto Compose::forward(std::initializer_list<torch::Tensor> tensors) -> std::any {
-
-
-
-
-    }
-
-        torch::Tensor OneOf::forward(torch::Tensor input) const
-    {
+        std::vector <torch::Tensor> tensor_vec(tensors);
+        input = tensor_vec[0];
         int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
-        input = transforms[index](std::move(input));
+        input = std::any_cast<torch::Tensor>(transforms[index](std::move(input)));
         return input;
-
     }
+
 }
