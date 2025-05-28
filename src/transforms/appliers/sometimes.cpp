876f41dd48562a7423f9972710a2351eaf153af0
kamisaberi <kamisaberi@gmail.com>
2025-05-29 02:20:57 +0330

    MOdule class forward method changed

--- src/transforms/appliers/sometimes.cpp
+++ src/transforms/appliers/sometimes.cpp
@@ -7,12 +7,12 @@ namespace xt::transforms {
     Sometimes::Sometimes(std::vector <xt::Module> transforms) : xt::Module(), transforms(std::move(transforms)) {
     }
 
-    auto Sometimes::forward(std::initializer_list <torch::Tensor> tensors) -> std::any {
-        std::vector <torch::Tensor> tensor_vec(tensors);
-        torch::Tensor input = tensor_vec[0];
-        int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
-        input = std::any_cast<torch::Tensor>(transform[index]({input}));
-        return input;
+    auto Sometimes::forward(std::initializer_list <std::any> tensors) -> std::any {
+        // std::vector <torch::Tensor> tensor_vec(tensors);
+        // torch::Tensor input = tensor_vec[0];
+        // int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
+        // input = std::any_cast<torch::Tensor>(transform[index]({input}));
+        // return input;
 
     }
 
