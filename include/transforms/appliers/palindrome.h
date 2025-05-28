0b921d4c897e7dd7e8f21f6db1933fbfb699c78b
kamisaberi <kamisaberi@gmail.com>
2025-05-29 00:18:08 +0330

    MOdule class forward method changed

--- include/transforms/appliers/palindrome.h
+++ include/transforms/appliers/palindrome.h
@@ -10,7 +10,7 @@ namespace xt::transforms
         Palindrome();
         explicit Palindrome(std::vector<xt::Module> transforms);
 //        torch::Tensor forward(torch::Tensor input) const override;
-        auto forward(std::initializer_list<torch::Tensor> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
 
     private:
