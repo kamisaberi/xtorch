d5a97476c4cde31062f27ca2dd0f7ed13bb54a6a
kamisaberi <kamisaberi@gmail.com>
2025-05-29 01:46:05 +0330

    MOdule class forward method changed

--- src/models/computer_vision/image_classification/resnet.cpp
+++ src/models/computer_vision/image_classification/resnet.cpp
@@ -328,8 +328,17 @@ namespace xt::models
         return layers;
     }
 
-    torch::Tensor ResNet34::forward(torch::Tensor x) const
+    auto ResNet34::forward(std::initializer_list<std::any> tensors) -> std::any
     {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
         x = conv1->forward(x);
         x = maxpool->forward(x);
         x = layer0->forward(x);
@@ -470,8 +479,17 @@ namespace xt::models
         return layers;
     }
 
-    torch::Tensor ResNet50::forward(torch::Tensor x) const
+    auto ResNet50::forward(std::initializer_list<std::any> tensors) -> std::any
     {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
         x = conv1->forward(x);
         x = maxpool->forward(x);
         x = layer0->forward(x);
