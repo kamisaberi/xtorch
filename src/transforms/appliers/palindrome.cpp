91cc3b1c75eec7f58c118ce4261ccad20bb92351
kamisaberi <kamisaberi@gmail.com>
2025-05-29 02:16:07 +0330

    MOdule class forward method changed

--- src/transforms/appliers/palindrome.cpp
+++ src/transforms/appliers/palindrome.cpp
@@ -1,9 +1,7 @@
-
 #include "include/transforms/appliers/palindrome.h"
 
 namespace xt::transforms
 {
-
     Palindrome::Palindrome() = default;
 
     Palindrome::Palindrome(std::vector<xt::Module> transforms): xt::Module(), transforms(std::move(transforms))
@@ -11,10 +9,17 @@ namespace xt::transforms
     }
 
 
-    auto Palindrome::forward(std::initializer_list<torch::Tensor> tensors) -> std::any {
+    auto Palindrome::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
 
-        std::vector<torch::Tensor> tensor_vec(tensors);
-        input = tensor_vec[0];
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor input = tensor_vec[0];
 
         for (const auto& transform : this->transforms)
         {
@@ -24,26 +29,6 @@ namespace xt::transforms
         {
             input = std::any_cast<torch::Tensor>(transforms[i]({input}));
         }
-
-
-
-
-
-    }
-
-        torch::Tensor Palindrome::forward(torch::Tensor input) const
-    {
-        for (const auto& transform : this->transforms)
-        {
-            input = transform(std::move(input));
-        }
-        for (int i = this->transforms.size() - 1; i >= 0; i--)
-        {
-            input = transforms[i](std::move(input));
-        }
-
-        return input;
-
     }
 
 }
