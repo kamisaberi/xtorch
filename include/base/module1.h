d696de82278770241c40f85ac99ba28afa59a426
kamran saberifard <kamisaberi@gmail.com>
2025-05-28 14:39:46 +0330

    transforms foreardmethod prototypes is updating to new version

--- include/base/module1.h
+++ include/base/module1.h
@@ -13,10 +13,10 @@ namespace xt
     public:
         Module1();
         // Pure virtual forward with initializer_list
-        virtual auto forward(std::initializer_list<torch::Tensor> tensors) -> std::any = 0;
+        virtual auto forward(std::initializer_list<std::any> tensors) -> std::any = 0;
 
         // Operator() to call forward
-        auto operator()(std::initializer_list<torch::Tensor> tensors) -> std::any;
+        auto operator()(std::initializer_list<std::any> tensors) -> std::any;
 
         // Pure virtual destructor
         ~Module1() override = default;    };
