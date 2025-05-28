be8ed25f4960ab2a4954037dc814664a38012c22
kamisaberi <kamisaberi@gmail.com>
2025-05-29 01:39:19 +0330

    MOdule class forward method changed

--- include/models/computer_vision/image_classification/alexnet.h
+++ include/models/computer_vision/image_classification/alexnet.h
@@ -18,7 +18,8 @@ namespace xt::models
 
         AlexNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
-        torch::Tensor forward(torch::Tensor x) const override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        // torch::Tensor forward(torch::Tensor x) const override;
         void reset() override;
     };
 }
