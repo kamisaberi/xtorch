a118427db12cb96c646f5f3780388574f3e87ad8
kamisaberi <kamisaberi@gmail.com>
2025-05-29 00:18:35 +0330

    MOdule class forward method changed

--- include/transforms/appliers/repeat.h
+++ include/transforms/appliers/repeat.h
@@ -9,7 +9,7 @@ namespace xt::transforms
     public:
         Repeat();
         explicit Repeat(std::unique_ptr<xt::Module> transform, int n_times);
-        auto forward(std::initializer_list<torch::Tensor> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
     private:
         std::unique_ptr<xt::Module> transform;
         int n_times_;
