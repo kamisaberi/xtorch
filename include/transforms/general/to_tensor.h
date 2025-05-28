ee0820ed4f94d1cf933c03004c61a69d151cadf5
kamisaberi <kamisaberi@gmail.com>
2025-05-29 00:49:01 +0330

    MOdule class forward method changed

--- include/transforms/general/to_tensor.h
+++ include/transforms/general/to_tensor.h
@@ -3,9 +3,9 @@
 
 namespace xt::transforms
 {
-    struct ToTensor {
+    struct ToTensor final : xt::Module {
     public:
-        auto forward(std::initializer_list<torch::Tensor> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
         torch::Tensor operator()(const cv::Mat& image) const;
     };
 
