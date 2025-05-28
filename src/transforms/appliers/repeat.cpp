fc4a14e550c926edc692aed47d3b9c33d18465ce
kamran saberifard <kamisaberi@gmail.com>
2025-05-28 13:32:09 +0330

    repeat transfomrsapplier updated

--- src/transforms/appliers/repeat.cpp
+++ src/transforms/appliers/repeat.cpp
@@ -1,20 +1,21 @@
 #include "include/transforms/appliers/repeat.h"
 
-namespace xt::transforms
-{
+namespace xt::transforms {
     Repeat::Repeat() = default;
 
-    Repeat::Repeat(std::unique_ptr<xt::Module> transform, int n_times): xt::Module(), transform(std::move(transform)),
-                                                          n_times_(n_times)
-    {
+    Repeat::Repeat(std::unique_ptr <xt::Module> transform, int n_times) : xt::Module(), transform(std::move(transform)),
+                                                                          n_times_(n_times) {
     }
 
-    torch::Tensor Repeat::forward(torch::Tensor input) const
-    {
-        for (int i = 1; i < this->n_times_; i++)
-        {
+    auto Compose::forward(std::initializer_list <torch::Tensor> tensors) -> std::any {
+        std::vector <torch::Tensor> tensor_vec(tensors);
+        torch::Tensor input = tensor_vec[0];
+        for (int i = 1; i < this->n_times_; i++) {
             // input = transform(std::move(input));
         }
         return input;
+
+
     }
+
 }
