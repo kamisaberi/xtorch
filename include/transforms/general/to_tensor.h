85757488f1d2ef0e9cdfa45d933f99e5798dbcfb
kamran saberifard <kamisaberi@gmail.com>
2025-05-28 14:39:46 +0330

    transforms foreardmethod prototypes is updating to new version

--- include/transforms/general/to_tensor.h
+++ include/transforms/general/to_tensor.h
@@ -5,6 +5,7 @@ namespace xt::transforms
 {
     struct ToTensor {
     public:
+        auto forward(std::initializer_list<torch::Tensor> tensors) -> std::any  override;
         torch::Tensor operator()(const cv::Mat& image) const;
     };
 
--- src/base/module1.cpp
+++ src/base/module1.cpp
@@ -6,7 +6,7 @@ namespace xt
     Module1::Module1() = default;
 
     // Operator() implementation in Module
-    auto Module1::operator()(const std::initializer_list<torch::Tensor> tensors) -> std::any
+    auto Module1::operator()(const std::initializer_list<std::any> tensors) -> std::any
     {
         return forward(tensors); // Calls subclass's forward
     }
