86937b49fae9eea3c07d2a876d3480ae089123b3
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:24:04 +0330

    MOdule class forward method changed

--- include/transforms/target/log_transformer.h
+++ include/transforms/target/log_transformer.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class LogTransformer final : public xt::Module
+    {
+    public:
+        LogTransformer();
+        explicit LogTransformer(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
