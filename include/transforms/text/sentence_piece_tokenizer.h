6e193236a387b431e27afbdb67c252dad5596c1c
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:37:42 +0330

    MOdule class forward method changed

--- include/transforms/text/sentence_piece_tokenizer.h
+++ include/transforms/text/sentence_piece_tokenizer.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class SentencePieceTokenizer final : public xt::Module
+    {
+    public:
+        SentencePieceTokenizer();
+        explicit SentencePieceTokenizer(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
