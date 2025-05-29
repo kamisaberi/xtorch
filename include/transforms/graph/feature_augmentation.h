0331a69997f0273c6dff461d6234869cbca64262
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:49:57 +0330

    MOdule class forward method changed

--- include/transforms/graph/feature_augmentation.h
+++ include/transforms/graph/feature_augmentation.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class FeatureAugmentation final : public xt::Module
+    {
+    public:
+        FeatureAugmentation();
+        explicit FeatureAugmentation(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
