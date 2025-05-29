a60759041091a7f613083ab3664f6ac26d8fd0f9
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:18:36 +0330

    MOdule class forward method changed

--- include/transforms/signal/wavelet_transforms.h
+++ include/transforms/signal/wavelet_transforms.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class WaveletTransforms final : public xt::Module
+    {
+    public:
+        WaveletTransforms();
+        explicit WaveletTransforms(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
