6fb3f4d743fb4ff7e9154b1b26a2e1cf97df794e
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:47:20 +0330

    MOdule class forward method changed

--- include/transforms/image/black_white.h
+++ include/transforms/image/black_white.h
@@ -1,5 +1,16 @@
-
-
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class BlackWhite final : public xt::Module
+    {
+    public:
+        BlackWhite();
+        explicit BlackWhite(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
