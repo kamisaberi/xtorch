79ea78a1dd53987ca767103f8b02bcadcaa06aaf
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:15:33 +0330

    MOdule class forward method changed

--- include/transforms/image/grid_mask.h
+++ include/transforms/image/grid_mask.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class GridMask final : public xt::Module
+    {
+    public:
+        GridMask();
+        explicit GridMask(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
