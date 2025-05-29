480d0ab72916b3733cdb6b9f7cbf72cd9161fb78
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:02:25 +0330

    MOdule class forward method changed

--- include/transforms/signal/mel_scale.h
+++ include/transforms/signal/mel_scale.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class MelScale final : public xt::Module
+    {
+    public:
+        MelScale();
+        explicit MelScale(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
