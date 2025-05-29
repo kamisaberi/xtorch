6d9c9680191289b707faea2debe5c80a77aa4091
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:56:43 +0330

    MOdule class forward method changed

--- include/transforms/image/crop_non_empty_mask_if_exists.h
+++ include/transforms/image/crop_non_empty_mask_if_exists.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class CropNonEmptyMaskIfExists final : public xt::Module
+    {
+    public:
+        CropNonEmptyMaskIfExists();
+        explicit CropNonEmptyMaskIfExists(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
