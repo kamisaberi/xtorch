2d516a9c948b732bdb27910954a7e6cd9a807d95
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:23:39 +0330

    MOdule class forward method changed

--- include/transforms/image/piecewise_affine.h
+++ include/transforms/image/piecewise_affine.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class PiecewiseAffine final : public xt::Module
+    {
+    public:
+        PiecewiseAffine();
+        explicit PiecewiseAffine(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
