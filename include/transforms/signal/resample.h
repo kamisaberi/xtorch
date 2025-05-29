8dd403cd05b9d4892e64fa450657622529e1f37c
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:07:05 +0330

    MOdule class forward method changed

--- include/transforms/signal/resample.h
+++ include/transforms/signal/resample.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Resample final : public xt::Module
+    {
+    public:
+        Resample();
+        explicit Resample(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
