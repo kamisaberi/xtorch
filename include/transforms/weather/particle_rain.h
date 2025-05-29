075be447976599afd22e7f1dbb21264ef1e5fa94
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:23:13 +0330

    MOdule class forward method changed

--- include/transforms/weather/particle_rain.h
+++ include/transforms/weather/particle_rain.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class ParticleRain final : public xt::Module
+    {
+    public:
+        ParticleRain();
+        explicit ParticleRain(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
