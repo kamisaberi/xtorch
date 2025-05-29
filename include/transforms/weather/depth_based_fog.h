77a5cd4ac9695ee8202aa02a252db383d88aec0c
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:58:31 +0330

    MOdule class forward method changed

--- include/transforms/weather/depth_based_fog.h
+++ include/transforms/weather/depth_based_fog.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class DepthBasedFog final : public xt::Module
+    {
+    public:
+        DepthBasedFog();
+        explicit DepthBasedFog(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
