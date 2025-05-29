861dba3c8c16ae40aadf820f7e4571da5a80c7bd
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:55:27 +0330

    MOdule class forward method changed

--- include/transforms/graph/subgraph.h
+++ include/transforms/graph/subgraph.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Subgraph final : public xt::Module
+    {
+    public:
+        Subgraph();
+        explicit Subgraph(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
