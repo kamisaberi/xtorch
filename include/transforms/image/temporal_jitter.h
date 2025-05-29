ce4c0ce92ca20d495a58a88d70db64d2f3f76f70
kamisaberi <kamisaberi@gmail.com>
2025-05-30 03:01:51 +0330

    MOdule class forward method changed

--- include/transforms/image/temporal_jitter.h
+++ include/transforms/image/temporal_jitter.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class TemporalJitter final : public xt::Module
+    {
+    public:
+        TemporalJitter();
+        explicit TemporalJitter(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
