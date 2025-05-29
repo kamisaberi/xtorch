091b829767a70b6da811372412a2a8217c800d73
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:24:56 +0330

    MOdule class forward method changed

--- include/transforms/target/min_max_scaler.h
+++ include/transforms/target/min_max_scaler.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class MinMaxScaler final : public xt::Module
+    {
+    public:
+        MinMaxScaler();
+        explicit MinMaxScaler(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
