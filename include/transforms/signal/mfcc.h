80a9fbcf94cd991c519a3aa6ae7d84a3b9e59bbc
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:06:28 +0330

    MOdule class forward method changed

--- include/transforms/signal/mfcc.h
+++ include/transforms/signal/mfcc.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class MFCC final : public xt::Module
+    {
+    public:
+        MFCC();
+        explicit MFCC(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
