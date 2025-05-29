18045e38c2f7c953fe369ad48fe7f2db8073ba05
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:36:17 +0330

    MOdule class forward method changed

--- include/transforms/text/back_translation.h
+++ include/transforms/text/back_translation.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class BackTranslation final : public xt::Module
+    {
+    public:
+        BackTranslation();
+        explicit BackTranslation(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
