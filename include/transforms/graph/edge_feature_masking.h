5152ecd1b99b0fa00bf144898345b8258995099c
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:47:51 +0330

    MOdule class forward method changed

--- include/transforms/graph/edge_feature_masking.h
+++ include/transforms/graph/edge_feature_masking.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class EdgeFeatureMasking final : public xt::Module
+    {
+    public:
+        EdgeFeatureMasking();
+        explicit EdgeFeatureMasking(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
