1a569198e910ad03386432bfbf6d31b86fde82ec
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:40:21 +0330

    MOdule class forward method changed

--- include/transforms/image/ada.h
+++ include/transforms/image/ada.h
@@ -1,2 +1,16 @@
-#pragma  once
+#pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class ADA final : public xt::Module
+    {
+    public:
+        ADA();
+        explicit ADA(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
