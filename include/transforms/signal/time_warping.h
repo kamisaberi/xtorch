835f4fb02613d9590c70a7b219c86f8686ed8499
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:09:56 +0330

    MOdule class forward method changed

--- include/transforms/signal/time_warping.h
+++ include/transforms/signal/time_warping.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class TimeWarping final : public xt::Module
+    {
+    public:
+        TimeWarping();
+        explicit TimeWarping(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
