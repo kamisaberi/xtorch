ee626299bab47fa35999d817b0f91ec3403d54af
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:22:44 +0330

    MOdule class forward method changed

--- include/transforms/target/label_smoother.h
+++ include/transforms/target/label_smoother.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class LabelSmoother final : public xt::Module
+    {
+    public:
+        LabelSmoother();
+        explicit LabelSmoother(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
