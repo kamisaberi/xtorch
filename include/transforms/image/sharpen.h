556d4c57063f1ab7045d5f4b70319872cb14a0b9
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:59:37 +0330

    MOdule class forward method changed

--- include/transforms/image/sharpen.h
+++ include/transforms/image/sharpen.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Sharpen final : public xt::Module
+    {
+    public:
+        Sharpen();
+        explicit Sharpen(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
