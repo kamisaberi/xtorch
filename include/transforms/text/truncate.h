f414df4b81fcd870e153ac4d4d162fc70ce81ecb
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:39:45 +0330

    MOdule class forward method changed

--- include/transforms/text/truncate.h
+++ include/transforms/text/truncate.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Truncate final : public xt::Module
+    {
+    public:
+        Truncate();
+        explicit Truncate(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
