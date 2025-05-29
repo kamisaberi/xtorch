f70d528fae1941926ae32278032cf0d08c2c59c3
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:58:31 +0330

    MOdule class forward method changed

--- include/transforms/image/random_vertical_flip.h
+++ include/transforms/image/random_vertical_flip.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomVerticalFlip final : public xt::Module
+    {
+    public:
+        RandomVerticalFlip();
+        explicit RandomVerticalFlip(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
