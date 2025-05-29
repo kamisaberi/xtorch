79a6af5cac1fb35243616b622de14fc76abd902c
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:09:30 +0330

    MOdule class forward method changed

--- include/transforms/target/label_clusterer.h
+++ include/transforms/target/label_clusterer.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class LabelClusterer final : public xt::Module
+    {
+    public:
+        LabelClusterer();
+        explicit LabelClusterer(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
