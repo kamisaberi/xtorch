d2a48528d19703e02aeac3a3485b36f85a01730f
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:25:52 +0330

    MOdule class forward method changed

--- include/transforms/target/one_hot_encoder.h
+++ include/transforms/target/one_hot_encoder.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class OneHotEncoder final : public xt::Module
+    {
+    public:
+        OneHotEncoder();
+        explicit OneHotEncoder(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
