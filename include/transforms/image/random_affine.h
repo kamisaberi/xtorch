1e3cf35ec07ade34b0b527c8c2a1e9139d29de7d
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:27:11 +0330

    MOdule class forward method changed

--- include/transforms/image/random_affine.h
+++ include/transforms/image/random_affine.h
@@ -2,3 +2,15 @@
 
 #include "include/transforms/common.h"
 
+namespace xt::transforms
+{
+    class RandomAffine final : public xt::Module
+    {
+    public:
+        RandomAffine();
+        explicit RandomAffine(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
