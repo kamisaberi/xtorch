9a14f7b2ed39672a107da6adc3cbe2dc3ca4a03b
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:50:49 +0330

    MOdule class forward method changed

--- include/transforms/graph/graph_coarsening.h
+++ include/transforms/graph/graph_coarsening.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class GraphCoarsening final : public xt::Module
+    {
+    public:
+        GraphCoarsening();
+        explicit GraphCoarsening(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
