039cd7ff163be3ca04578142715a685f40a7aa15
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:10:34 +0330

    MOdule class forward method changed

--- include/transforms/image/grid_dropout.h
+++ include/transforms/image/grid_dropout.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class GridDropout final : public xt::Module
+    {
+    public:
+        GridDropout();
+        explicit GridDropout(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
