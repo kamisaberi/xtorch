f8760ddc2588cf5270e1b0828e37be150c8b421e
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:26:09 +0330

    MOdule class forward method changed

--- include/transforms/weather/patchy_fog.h
+++ include/transforms/weather/patchy_fog.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class PatchyFog final : public xt::Module
+    {
+    public:
+        PatchyFog();
+        explicit PatchyFog(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
