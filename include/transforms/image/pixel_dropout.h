497915bff202cd1c7acff0c9790754ae5c00135f
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:24:03 +0330

    MOdule class forward method changed

--- include/transforms/image/pixel_dropout.h
+++ include/transforms/image/pixel_dropout.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class PixelDropout final : public xt::Module
+    {
+    public:
+        PixelDropout();
+        explicit EdgeAdd(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
