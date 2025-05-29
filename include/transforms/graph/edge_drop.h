ce7fd02fb762b4ffa8a37d1d2af574c8833af46c
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:47:13 +0330

    MOdule class forward method changed

--- include/transforms/graph/edge_drop.h
+++ include/transforms/graph/edge_drop.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class EdgeDrop final : public xt::Module
+    {
+    public:
+        EdgeDrop();
+        explicit EdgeDrop(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
