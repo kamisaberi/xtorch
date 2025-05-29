44f7d007631e106c102786e624442b390e9be400
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:20:36 +0330

    MOdule class forward method changed

--- include/transforms/image/longest_max_size.h
+++ include/transforms/image/longest_max_size.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class LongestMaxSize final : public xt::Module
+    {
+    public:
+        LongestMaxSize();
+        explicit LongestMaxSize(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
