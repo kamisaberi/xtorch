8aaf02b7c46260c57c4de6ce049158720fe38ecc
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:26:39 +0330

    MOdule class forward method changed

--- include/transforms/image/random_adjust_sharpness.h
+++ include/transforms/image/random_adjust_sharpness.h
@@ -2,3 +2,15 @@
 
 #include "include/transforms/common.h"
 
+namespace xt::transforms
+{
+    class RandomAdjustSharpness final : public xt::Module
+    {
+    public:
+        RandomAdjustSharpness();
+        explicit RandomAdjustSharpness(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
