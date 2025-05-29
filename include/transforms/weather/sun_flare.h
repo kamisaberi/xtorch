aaac6139428b40ea52c1b069f58c14a207db1ae2
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:28:35 +0330

    MOdule class forward method changed

--- include/transforms/weather/sun_flare.h
+++ include/transforms/weather/sun_flare.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class SunFlare final : public xt::Module
+    {
+    public:
+        SunFlare();
+        explicit SunFlare(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
