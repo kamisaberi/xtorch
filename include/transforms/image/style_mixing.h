2cb65736f6cedab9d97f8e1da6fc999bd7713135
kamisaberi <kamisaberi@gmail.com>
2025-05-30 03:01:22 +0330

    MOdule class forward method changed

--- include/transforms/image/style_mixing.h
+++ include/transforms/image/style_mixing.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class StyleMixing final : public xt::Module
+    {
+    public:
+        StyleMixing();
+        explicit StyleMixing(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
