286a2eecf015a72159eb77f5206405b6ed2ab737
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:27:23 +0330

    MOdule class forward method changed

--- include/transforms/weather/streak_based_rain.h
+++ include/transforms/weather/streak_based_rain.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class StreakBasedRain final : public xt::Module
+    {
+    public:
+        StreakBasedRain();
+        explicit StreakBasedRain(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
