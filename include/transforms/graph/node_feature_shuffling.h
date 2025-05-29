8fd394304876d14fba27406dcef9a56740057eb9
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:54:11 +0330

    MOdule class forward method changed

--- include/transforms/graph/node_feature_shuffling.h
+++ include/transforms/graph/node_feature_shuffling.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class NodeFeatureShuffling final : public xt::Module
+    {
+    public:
+        NodeFeatureShuffling();
+        explicit NodeFeatureShuffling(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
