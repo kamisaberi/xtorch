5fd6c7c5758823a32d45e8f1a927e50c197f0bbc
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:52:01 +0330

    MOdule class forward method changed

--- include/transforms/graph/graph_diffusion.h
+++ include/transforms/graph/graph_diffusion.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class GraphDiffusion final : public xt::Module
+    {
+    public:
+        GraphDiffusion();
+        explicit GraphDiffusion(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
