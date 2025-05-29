23ec00ac9ba6ef60e61b027c157c232491cb943e
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:06:47 +0330

    MOdule class forward method changed

--- include/transforms/signal/pitch_shift.h
+++ include/transforms/signal/pitch_shift.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class PitchShift final : public xt::Module
+    {
+    public:
+        PitchShift();
+        explicit PitchShift(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
