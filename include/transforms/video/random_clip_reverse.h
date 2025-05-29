524ccfda55ca3872bfe3b64155552754ead1db32
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:53:01 +0330

    MOdule class forward method changed

--- include/transforms/video/random_clip_reverse.h
+++ include/transforms/video/random_clip_reverse.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomClipReverse final : public xt::Module
+    {
+    public:
+        RandomClipReverse();
+        explicit RandomClipReverse(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
