1fd2e76493d514d82b1a24ca98cd12ddc4a9f1e7
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:25:12 +0330

    MOdule class forward method changed

--- include/transforms/target/binary_encoder.h
+++ include/transforms/target/binary_encoder.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class BinaryEncoder final : public xt::Module
+    {
+    public:
+        BinaryEncoder();
+        explicit BinaryEncoder(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
