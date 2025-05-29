d169f75f794fd0c141c1125222697b7eb5386f0e
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:38:09 +0330

    MOdule class forward method changed

--- include/transforms/text/str_to_int_transform.h
+++ include/transforms/text/str_to_int_transform.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class StrToIntTransform final : public xt::Module
+    {
+    public:
+        StrToIntTransform();
+        explicit StrToIntTransform(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
