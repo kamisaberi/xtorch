3a758dc2251086604df77aed34ae3cbf1931b63d
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:35:54 +0330

    MOdule class forward method changed

--- include/transforms/image/random_equalize.h
+++ include/transforms/image/random_equalize.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomEqualize final : public xt::Module
+    {
+    public:
+        RandomEqualize();
+        explicit RandomEqualize(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
