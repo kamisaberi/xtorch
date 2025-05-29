078c2d82166507b0ff8a928abf1bcd47cf37cf6a
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:53:07 +0330

    MOdule class forward method changed

--- include/transforms/image/coarse_dropout.h
+++ include/transforms/image/coarse_dropout.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class CoarseDropout final : public xt::Module
+    {
+    public:
+        CoarseDropout();
+        explicit CoarseDropout(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
