1946dc57314de56d1f32eb4c64239188525d2bcd
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:48:05 +0330

    MOdule class forward method changed

--- include/transforms/image/blur.h
+++ include/transforms/image/blur.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Blur final : public xt::Module
+    {
+    public:
+        Blur();
+        explicit Blur(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
