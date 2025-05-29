b3c9d37a64060bf244f540f252389ef7a5a747cf
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:52:44 +0330

    MOdule class forward method changed

--- include/transforms/image/clahe.h
+++ include/transforms/image/clahe.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class CLAHE final : public xt::Module
+    {
+    public:
+        CLAHE();
+        explicit CLAHE(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
