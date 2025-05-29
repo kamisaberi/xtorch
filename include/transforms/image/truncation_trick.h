2d342ddfc05928d7cd68273d563007be788024df
kamisaberi <kamisaberi@gmail.com>
2025-05-30 03:02:59 +0330

    MOdule class forward method changed

--- include/transforms/image/truncation_trick.h
+++ include/transforms/image/truncation_trick.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class TruncationTrick final : public xt::Module
+    {
+    public:
+        TruncationTrick();
+        explicit TruncationTrick(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
