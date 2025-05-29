f1bb6b23406af76633ee06a8007aebfa121fe1c2
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:59:18 +0330

    MOdule class forward method changed

--- include/transforms/signal/de_reverberation.h
+++ include/transforms/signal/de_reverberation.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class DeReverberation final : public xt::Module
+    {
+    public:
+        DeReverberation();
+        explicit EdgeAdd(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
