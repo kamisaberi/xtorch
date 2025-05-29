09fb363a7c1debc2665223c92ffd8fcc58583e94
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:22:17 +0330

    MOdule class forward method changed

--- include/transforms/target/label_remapper.h
+++ include/transforms/target/label_remapper.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class LabelRemapper final : public xt::Module
+    {
+    public:
+        LabelRemapper();
+        explicit LabelRemapper(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
