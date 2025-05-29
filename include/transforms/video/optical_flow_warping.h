73024969ded73fd5d6243db15483561c1d793250
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:47:53 +0330

    MOdule class forward method changed

--- include/transforms/video/optical_flow_warping.h
+++ include/transforms/video/optical_flow_warping.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class OpticalFlowWarping final : public xt::Module
+    {
+    public:
+        OpticalFlowWarping();
+        explicit OpticalFlowWarping(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
