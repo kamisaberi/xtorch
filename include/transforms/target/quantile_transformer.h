ec4d270c34b419ee5ab075080e3c3b99bedc2a70
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:31:16 +0330

    MOdule class forward method changed

--- include/transforms/target/quantile_transformer.h
+++ include/transforms/target/quantile_transformer.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class QuantileTransformer final : public xt::Module
+    {
+    public:
+        QuantileTransformer();
+        explicit QuantileTransformer(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
