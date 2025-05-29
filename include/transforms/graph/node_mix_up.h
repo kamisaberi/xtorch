153d7b760acb7fe37732b8c7a2dc268c61267cc1
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:54:30 +0330

    MOdule class forward method changed

--- include/transforms/graph/node_mix_up.h
+++ include/transforms/graph/node_mix_up.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class NodeMixUp final : public xt::Module
+    {
+    public:
+        NodeMixUp();
+        explicit NodeMixUp(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
