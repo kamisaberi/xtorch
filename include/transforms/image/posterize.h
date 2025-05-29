b7468c0cccde6514b1149fa830b773832212fc5d
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:24:30 +0330

    MOdule class forward method changed

--- include/transforms/image/posterize.h
+++ include/transforms/image/posterize.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Posterize final : public xt::Module
+    {
+    public:
+        Posterize();
+        explicit Posterize(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
