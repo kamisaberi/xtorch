1e451a6aeee63289132e5bacdb5076b87080338c
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:51:21 +0330

    MOdule class forward method changed

--- include/transforms/image/channel_shuffle.h
+++ include/transforms/image/channel_shuffle.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class ChannelShuffle final : public xt::Module
+    {
+    public:
+        ChannelShuffle();
+        explicit ChannelShuffle(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
