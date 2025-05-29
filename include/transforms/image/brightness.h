4b29bd417ccc0cf4c093dd2a3885b6aa71445527
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:48:25 +0330

    MOdule class forward method changed

--- include/transforms/image/brightness.h
+++ include/transforms/image/brightness.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Brightness final : public xt::Module
+    {
+    public:
+        Brightness();
+        explicit Brightness(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
