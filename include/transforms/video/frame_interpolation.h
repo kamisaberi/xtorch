bce076c29b0c2bbd568e285d51368734b49f6815
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:47:24 +0330

    MOdule class forward method changed

--- include/transforms/video/frame_interpolation.h
+++ include/transforms/video/frame_interpolation.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class FrameInterpolation final : public xt::Module
+    {
+    public:
+        FrameInterpolation();
+        explicit FrameInterpolation(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
