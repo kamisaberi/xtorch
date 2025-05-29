37fc444e852835baf1f393a2833209e526af86e7
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:55:57 +0330

    MOdule class forward method changed

--- include/transforms/image/random_scale.h
+++ include/transforms/image/random_scale.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomScale final : public xt::Module
+    {
+    public:
+        RandomScale();
+        explicit RandomScale(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
