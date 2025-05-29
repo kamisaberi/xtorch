88b985862f4f3539206e4ff19f647d2fdb5abb83
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:08:01 +0330

    MOdule class forward method changed

--- include/transforms/signal/sliding_window_cmn.h
+++ include/transforms/signal/sliding_window_cmn.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class SlidingWindowCMN final : public xt::Module
+    {
+    public:
+        SlidingWindowCMN();
+        explicit SlidingWindowCMN(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
