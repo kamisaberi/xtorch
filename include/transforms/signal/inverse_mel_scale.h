38552948869179e3f8e29dd84e25175d1ba9b80e
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:02:06 +0330

    MOdule class forward method changed

--- include/transforms/signal/inverse_mel_scale.h
+++ include/transforms/signal/inverse_mel_scale.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class InverseMelScale final : public xt::Module
+    {
+    public:
+        InverseMelScale();
+        explicit InverseMelScale(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
