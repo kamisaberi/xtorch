ae24fc3cd0883d9e46f01777377c6c74d57def98
kamisaberi <kamisaberi@gmail.com>
2025-05-29 21:43:14 +0330

    MOdule class forward method changed

--- include/transforms/target/hashing_encoder.h
+++ include/transforms/target/hashing_encoder.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class HashingEncoder final : public xt::Module
+    {
+    public:
+        HashingEncoder();
+        explicit HashingEncoder(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
