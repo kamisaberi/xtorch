c2a3549a00abb03e43a2ac9aee30b2a365a34128
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:59:13 +0330

    MOdule class forward method changed

--- include/transforms/image/scale.h
+++ include/transforms/image/scale.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Scale final : public xt::Module
+    {
+    public:
+        Scale();
+        explicit Scale(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
