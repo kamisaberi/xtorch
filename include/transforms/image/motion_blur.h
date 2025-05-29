2b1c032ef33bb1bc8932e3358ae4c6a75e4f4ba1
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:28:07 +0330

    MOdule class forward method changed

--- include/transforms/image/motion_blur.h
+++ include/transforms/image/motion_blur.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class MotionNlur final : public xt::Module
+    {
+    public:
+        MotionNlur();
+        explicit MotionNlur(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
