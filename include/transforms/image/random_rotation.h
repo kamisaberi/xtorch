c3f35f214c6a8ae6c11eaa54d67786ea5f05c875
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:55:11 +0330

    MOdule class forward method changed

--- include/transforms/image/random_rotation.h
+++ include/transforms/image/random_rotation.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomRotation final : public xt::Module
+    {
+    public:
+        RandomRotation();
+        explicit RandomRotation(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
