2c3773937ede0fa9e3d658051b7f0a7d00f9dde0
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:21:02 +0330

    MOdule class forward method changed

--- include/transforms/weather/dust_sand_clouds.h
+++ include/transforms/weather/dust_sand_clouds.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class DustSandClouds final : public xt::Module
+    {
+    public:
+        DustSandClouds();
+        explicit DustSandClouds(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
