774e5ee3bf0ce9edb7d73e84b894e58ba2d120a8
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:52:51 +0330

    MOdule class forward method changed

--- include/transforms/graph/node_drop.h
+++ include/transforms/graph/node_drop.h
@@ -1,4 +1,16 @@
 #pragma once
 
-
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class NodeDrop final : public xt::Module
+    {
+    public:
+        NodeDrop();
+        explicit NodeDrop(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
