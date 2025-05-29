0b299b2a36f899953e19ef265e97c07bdba4ddc5
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:31:42 +0330

    MOdule class forward method changed

--- include/transforms/target/robust_scaler.h
+++ include/transforms/target/robust_scaler.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RobustScaler final : public xt::Module
+    {
+    public:
+        RobustScaler();
+        explicit RobustScaler(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
