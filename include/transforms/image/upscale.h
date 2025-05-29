ea6ba9acde60b398cfdfa5b9b84fb69b6381344d
kamisaberi <kamisaberi@gmail.com>
2025-05-30 03:03:30 +0330

    MOdule class forward method changed

--- include/transforms/image/upscale.h
+++ include/transforms/image/upscale.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Upscale final : public xt::Module
+    {
+    public:
+        Upscale();
+        explicit Upscale(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
