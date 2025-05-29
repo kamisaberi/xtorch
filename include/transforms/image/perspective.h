005c9e50949227b8a8d39672cceeb5c7c9e3d092
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:34:50 +0330

    MOdule class forward method changed

--- include/transforms/image/perspective.h
+++ include/transforms/image/perspective.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Perspective final : public xt::Module
+    {
+    public:
+        Perspective();
+        explicit Perspective(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
