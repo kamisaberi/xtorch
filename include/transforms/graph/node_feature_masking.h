64b83e783543d92299078e9409ea8ea4bc9ef445
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:53:40 +0330

    MOdule class forward method changed

--- include/transforms/graph/node_feature_masking.h
+++ include/transforms/graph/node_feature_masking.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class NodeFeatureMasking final : public xt::Module
+    {
+    public:
+        NodeFeatureMasking();
+        explicit NodeFeatureMasking(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
