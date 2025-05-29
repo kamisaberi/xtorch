f7dea7e0adb95333e41cfaa9cb75d7229a608faf
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:30:29 +0330

    MOdule class forward method changed

--- include/transforms/target/ordinal_encoder.h
+++ include/transforms/target/ordinal_encoder.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class OrdinalEncoder final : public xt::Module
+    {
+    public:
+        OrdinalEncoder();
+        explicit OrdinalEncoder(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
