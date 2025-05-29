f53136493822ec63edb43087a368b54c43910f6e
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:55:37 +0330

    MOdule class forward method changed

--- include/transforms/image/crop_and_pad.h
+++ include/transforms/image/crop_and_pad.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class CropAndPad final : public xt::Module
+    {
+    public:
+        CropAndPad();
+        explicit CropAndPad(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
