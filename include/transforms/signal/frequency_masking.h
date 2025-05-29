13e077a4aac686798e33edd5b11fe1ff8e080b0a
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:59:38 +0330

    MOdule class forward method changed

--- include/transforms/signal/frequency_masking.h
+++ include/transforms/signal/frequency_masking.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class FrequencyMasking final : public xt::Module
+    {
+    public:
+        FrequencyMasking();
+        explicit FrequencyMasking(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
