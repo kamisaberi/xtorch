eb22dcd63c45e2b53a688194472234bb35e0c2e4
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:26:53 +0330

    MOdule class forward method changed

--- include/transforms/image/median_blur.h
+++ include/transforms/image/median_blur.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class MedianBlur final : public xt::Module
+    {
+    public:
+        MedianBlur();
+        explicit MedianBlur(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
