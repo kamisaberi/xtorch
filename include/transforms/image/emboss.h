5652bb30596abf6c304199c059c4ad1ea3a0cff1
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:05:29 +0330

    MOdule class forward method changed

--- include/transforms/image/emboss.h
+++ include/transforms/image/emboss.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Emboss final : public xt::Module
+    {
+    public:
+        Emboss();
+        explicit Emboss(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
