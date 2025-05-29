9f812310da1fe08d3fee451cf8d98cb1b8f1f4ba
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:22:50 +0330

    MOdule class forward method changed

--- include/transforms/weather/homogeneous_fog.h
+++ include/transforms/weather/homogeneous_fog.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class HomogeneousFog final : public xt::Module
+    {
+    public:
+        HomogeneousFog();
+        explicit HomogeneousFog(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
