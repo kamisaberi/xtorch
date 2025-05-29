775d529fa1a668b0377ecbdbaa7618df3b4dba42
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:44:20 +0330

    MOdule class forward method changed

--- include/transforms/image/random_grid_shuffle.h
+++ include/transforms/image/random_grid_shuffle.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomGridShuffle final : public xt::Module
+    {
+    public:
+        RandomGridShuffle();
+        explicit RandomGridShuffle(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
