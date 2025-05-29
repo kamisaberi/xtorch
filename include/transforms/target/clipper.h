62114b4d9ada7cb837bb07f0601c7923dd1bd588
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:26:02 +0330

    MOdule class forward method changed

--- include/transforms/target/clipper.h
+++ include/transforms/target/clipper.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Clipper final : public xt::Module
+    {
+    public:
+        Clipper();
+        explicit Clipper(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
