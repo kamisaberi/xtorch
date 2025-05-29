af78c48301b296eb9ea299788e9b427176a5840f
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:06:56 +0330

    MOdule class forward method changed

--- include/transforms/image/fancy_pca.h
+++ include/transforms/image/fancy_pca.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class FancyPCA final : public xt::Module
+    {
+    public:
+        FancyPCA();
+        explicit FancyPCA(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
