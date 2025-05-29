ce517cda4f3dfd22c4021a822eacd3f3a739192e
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:31:35 +0330

    MOdule class forward method changed

--- include/transforms/image/pad.h
+++ include/transforms/image/pad.h
@@ -1,3 +1,4 @@
+//TODO SHOULD CHANGE
 #pragma once
 
 
--- include/transforms/image/pad_if_needed.h
+++ include/transforms/image/pad_if_needed.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class PadIfNeeded final : public xt::Module
+    {
+    public:
+        PadIfNeeded();
+        explicit PadIfNeeded(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
