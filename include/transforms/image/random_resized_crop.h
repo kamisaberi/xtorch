d2ec6b4a1146222aaddeb1cc4afd2dc1bb2ac33a
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:53:53 +0330

    MOdule class forward method changed

--- include/transforms/image/random_resized_crop.h
+++ include/transforms/image/random_resized_crop.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomResizedCrop final : public xt::Module
+    {
+    public:
+        RandomResizedCrop();
+        explicit RandomResizedCrop(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
