1511f81b50a5dfd2753d4a044235a49b3d5d2d66
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:30:24 +0330

    MOdule class forward method changed

--- include/transforms/weather/wet_ground.h
+++ include/transforms/weather/wet_ground.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class WetGround final : public xt::Module
+    {
+    public:
+        WetGround();
+        explicit WetGround(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
