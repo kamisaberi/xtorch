82094888f394e0a764c45e84b8ff27c16415369b
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:07:56 +0330

    MOdule class forward method changed

--- include/transforms/image/glass_blur.h
+++ include/transforms/image/glass_blur.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class GlassBlur final : public xt::Module
+    {
+    public:
+        GlassBlur();
+        explicit GlassBlur(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
