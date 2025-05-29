01a0d2ea8239c7f4f578010fb94e68641997f98a
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:30:57 +0330

    MOdule class forward method changed

--- include/transforms/image/optical_distortion.h
+++ include/transforms/image/optical_distortion.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class OpticalDistortion final : public xt::Module
+    {
+    public:
+        OpticalDistortion();
+        explicit OpticalDistortion(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
