e892a75cf0c0b8439ff899c1592693f1da3641ff
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:21:24 +0330

    MOdule class forward method changed

--- include/transforms/weather/dynamic_shadows.h
+++ include/transforms/weather/dynamic_shadows.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class DynamicShadows final : public xt::Module
+    {
+    public:
+        DynamicShadows();
+        explicit DynamicShadows(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
