7f80903a38752b0fc4b0cb28ab00e0614bdeae0b
kamisaberi <kamisaberi@gmail.com>
2025-05-30 03:02:24 +0330

    MOdule class forward method changed

--- include/transforms/image/to_sepia.h
+++ include/transforms/image/to_sepia.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class ToSepia final : public xt::Module
+    {
+    public:
+        ToSepia();
+        explicit ToSepia(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
