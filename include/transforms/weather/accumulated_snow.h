ef32ab5048ba11f9e3d0ec46f591bf18f290d8f4
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:57:08 +0330

    MOdule class forward method changed

--- include/transforms/weather/accumulated_snow.h
+++ include/transforms/weather/accumulated_snow.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class AccumulatedSnow final : public xt::Module
+    {
+    public:
+        AccumulatedSnow();
+        explicit AccumulatedSnow(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
