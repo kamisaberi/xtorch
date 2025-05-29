5baabc82e76c0e27f335a3fec6d2c366f9301812
kamisaberi <kamisaberi@gmail.com>
2025-05-29 20:25:35 +0330

    MOdule class forward method changed

--- include/transforms/target/class_weighter.h
+++ include/transforms/target/class_weighter.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class ClassWeighter final : public xt::Module
+    {
+    public:
+        ClassWeighter();
+        explicit ClassWeighter(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
