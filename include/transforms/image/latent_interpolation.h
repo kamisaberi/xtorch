449fd512598992cc3c61755c48f9ca1aedfc2b96
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:18:05 +0330

    MOdule class forward method changed

--- include/transforms/image/latent_interpolation.h
+++ include/transforms/image/latent_interpolation.h
@@ -1,2 +1,16 @@
-#pragma  once
+#pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class LatentInterpolation final : public xt::Module
+    {
+    public:
+        LatentInterpolation();
+        explicit LatentInterpolation(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
