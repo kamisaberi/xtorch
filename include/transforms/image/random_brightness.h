0865e53fc2d69359581f20e447340184dadc3911
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:28:53 +0330

    MOdule class forward method changed

--- include/transforms/image/random_brightness.h
+++ include/transforms/image/random_brightness.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomBrightness final : public xt::Module
+    {
+    public:
+        RandomBrightness();
+        explicit RandomBrightness(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
