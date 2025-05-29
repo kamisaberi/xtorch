889cccc9c9131e00812f8029c83b4f42c130b7bc
kamisaberi <kamisaberi@gmail.com>
2025-05-30 00:46:52 +0330

    MOdule class forward method changed

--- include/transforms/image/affine.h
+++ include/transforms/image/affine.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Affine final : public xt::Module
+    {
+    public:
+        Affine();
+        explicit Affine(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
