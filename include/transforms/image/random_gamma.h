2bf882f2f056fd32a280b8fab5d2786f52a90961
kamisaberi <kamisaberi@gmail.com>
2025-05-30 02:42:08 +0330

    MOdule class forward method changed

--- include/transforms/image/random_gamma.h
+++ include/transforms/image/random_gamma.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomGamma final : public xt::Module
+    {
+    public:
+        RandomGamma();
+        explicit RandomGamma(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
