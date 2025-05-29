a24c0099ad692dd64936afeae50bf184e1d9e982
kamisaberi <kamisaberi@gmail.com>
2025-05-29 19:55:02 +0330

    MOdule class forward method changed

--- include/transforms/graph/random_walk_subgraph.h
+++ include/transforms/graph/random_walk_subgraph.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class RandomWalkSubgraph final : public xt::Module
+    {
+    public:
+        RandomWalkSubgraph();
+        explicit RandomWalkSubgraph(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
