3f6e7160791622fa5ab34c2df50bd0c38269142b
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:40:07 +0330

    MOdule class forward method changed

--- include/transforms/text/vocab_transform.h
+++ include/transforms/text/vocab_transform.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class VocabTransform final : public xt::Module
+    {
+    public:
+        VocabTransform();
+        explicit VocabTransform(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
