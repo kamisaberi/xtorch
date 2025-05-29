4e4c7c6a8917833f16e09387f2807a577eef78a4
kamisaberi <kamisaberi@gmail.com>
2025-05-30 01:06:05 +0330

    MOdule class forward method changed

--- include/transforms/image/equalize.h
+++ include/transforms/image/equalize.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class Equalize final : public xt::Module
+    {
+    public:
+        Equalize();
+        explicit Equalize(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
