7a3df1ec1d658f313c5338c7c6c53f366e418631
kamisaberi <kamisaberi@gmail.com>
2025-05-29 22:38:59 +0330

    MOdule class forward method changed

--- include/transforms/text/text_style_transfer.h
+++ include/transforms/text/text_style_transfer.h
@@ -1,2 +1,16 @@
 #pragma once
+
 #include "include/transforms/common.h"
+
+namespace xt::transforms
+{
+    class TextStyleTransfer final : public xt::Module
+    {
+    public:
+        TextStyleTransfer();
+        explicit TextStyleTransfer(std::vector<xt::Module> transforms);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+    private:
+    };
+}
