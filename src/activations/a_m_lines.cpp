3b228ef8d8bcc409105c3cdb1b5ae6fa5befb91d
kamisaberi <kamisaberi@gmail.com>
2025-05-30 15:39:15 +0330

    MOdule class forward method changed

--- src/activations/a_m_lines.cpp
+++ src/activations/a_m_lines.cpp
@@ -1,8 +1,10 @@
+//TODO SHOULD IMPLEMENT
+
 #include "include/activations/a_m_lines.h"
 
 namespace xt::activations {
     torch::Tensor am_lines(torch::Tensor x) {
-
+        return  torch::zeros(10);
     }
 
     auto AMLines::forward(std::initializer_list<std::any> tensors) -> std::any
