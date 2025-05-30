65c1eb884dc30ff9f836f847eb5084a101374c92
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:42:44 +0330

    MOdule class forward method changed

--- include/losses/multi_loss.h
+++ include/losses/multi_loss.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor multi_loss(torch::Tensor x);
     class MultiLoss : xt::Module
     {
     public:
+        MultiLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
