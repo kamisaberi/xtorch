ea4f68e810f4b31ab870ba73806a50d59903571b
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:12:22 +0330

    MOdule class forward method changed

--- include/losses/dice_loss.h
+++ include/losses/dice_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor dice_loss(torch::Tensor x);
     class DiceLoss : xt::Module
     {
     public:
+
+        DiceLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
--- include/losses/dsam_loss.h
+++ include/losses/dsam_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor dsam_loss(torch::Tensor x);
     class DSAMLoss : xt::Module
     {
     public:
+
+        DSAMLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
