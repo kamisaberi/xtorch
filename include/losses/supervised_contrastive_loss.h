81cbb7569621cee8ec68384c7863e45996208811
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:52:15 +0330

    MOdule class forward method changed

--- include/losses/supervised_contrastive_loss.h
+++ include/losses/supervised_contrastive_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor supervised_contrastive_loss(torch::Tensor x);
     class SupervisedContrastiveLoss : xt::Module
     {
     public:
+        SupervisedContrastiveLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
