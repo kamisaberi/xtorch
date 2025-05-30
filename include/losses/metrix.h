0aa34e53011cacb1e21ac9d1594faf44cdfbd1f0
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:40:43 +0330

    MOdule class forward method changed

--- include/losses/metrix.h
+++ include/losses/metrix.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor metrix(torch::Tensor x);
     class Metrix : xt::Module
     {
     public:
+        Metrix() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
