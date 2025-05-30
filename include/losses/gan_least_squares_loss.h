a9cbf44f0e0b579f621f767ffcddaca640ccb9ba
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:35:28 +0330

    MOdule class forward method changed

--- include/losses/gan_least_squares_loss.h
+++ include/losses/gan_least_squares_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor gan_least_squares_loss(torch::Tensor x);
     class GANLeastSquaresLoss : xt::Module
     {
     public:
+        GANLeastSquaresLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
--- include/losses/ghm_c.h
+++ include/losses/ghm_c.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor ghmc(torch::Tensor x);
     class GHMC : xt::Module
     {
     public:
+        GHMC() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
