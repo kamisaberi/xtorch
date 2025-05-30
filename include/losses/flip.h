33fb8c8cb35f164cc92b0c764605747f62acf644
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:33:26 +0330

    MOdule class forward method changed

--- include/losses/flip.h
+++ include/losses/flip.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor flip(torch::Tensor x);
     class FLIP : xt::Module
     {
     public:
+
+        FLIP() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
