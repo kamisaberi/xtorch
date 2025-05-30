f4ad06e4a8732c6d11593e030ea74965d36bcdcd
kamisaberi <kamisaberi@gmail.com>
2025-05-30 17:56:26 +0330

    MOdule class forward method changed

--- src/activations/fem.cpp
+++ src/activations/fem.cpp
@@ -1,11 +1,14 @@
 #include "include/activations/fem.h"
 
-namespace xt::activations {
-    torch::Tensor fem(torch::Tensor x) {
+namespace xt::activations
+{
+    torch::Tensor fem(torch::Tensor x)
+    {
+        return torch::zeros(10);
     }
 
-    torch::Tensor FEM::forward(torch::Tensor x) const {
-        return xt::activations::fem(x);
+    auto FEM::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::activations::fem(torch::zeros(10));
     }
-
 }
--- src/activations/gclu.cpp
+++ src/activations/gclu.cpp
@@ -1,11 +1,14 @@
 #include "include/activations/gclu.h"
 
-namespace xt::activations {
-    torch::Tensor gclu(torch::Tensor x) {
+namespace xt::activations
+{
+    torch::Tensor gclu(torch::Tensor x)
+    {
+        return torch::zeros(10);
     }
 
-    torch::Tensor GCLU::forward(torch::Tensor x) const {
-        return xt::activations::gclu(x);
+    auto GCLU::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::activations::gclu(torch::zeros(10));
     }
-
 }
