ce1e9c810b9486161ecbf4a7a96b7f8e3c6014be
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:54:03 +0330

    MOdule class forward method changed

--- include/losses/uf_loss.h
+++ include/losses/uf_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor uf_loss(torch::Tensor x);
     class UFLoss : xt::Module
     {
     public:
+        UFLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
