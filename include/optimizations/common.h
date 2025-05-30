ced393dffa549b425700b67e03d19ded052e101e
kamisaberi <kamisaberi@gmail.com>
2025-05-31 03:05:44 +0330

    MOdule class forward method changed

--- include/optimizations/common.h
+++ include/optimizations/common.h
@@ -4,3 +4,9 @@
 #include <iostream>
 #include <vector>
 #include <any>
+#include <torch/optim/optimizer.h>
+#include <torch/torch.h> // Ensure Tensor and std::function are included
+#include <vector>
+#include <torch/optim/optimizer.h>
+#include <torch/torch.h> // Ensure Tensor and std::function are included
+#include <vector>
