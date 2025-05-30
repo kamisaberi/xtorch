12a479ff5c01ce4e37e9a0885b0d3779c1665eaa
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:40:43 +0330

    MOdule class forward method changed

--- include/losses/lovasz_softmax.h
+++ include/losses/lovasz_softmax.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor lovasz_softmax(torch::Tensor x);
     class LovaszSoftmax : xt::Module
     {
     public:
+        LovaszSoftmax() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
