c73a19634660aecfef4a26179ffef4fea72d942a
kamisaberi <kamisaberi@gmail.com>
2025-05-30 23:48:05 +0330

    MOdule class forward method changed

--- include/losses/upit.h
+++ include/losses/upit.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
-    class uPIT : xt::Module
+    torch::Tensor upit(torch::Tensor x);
+    class UPIT : xt::Module
     {
     public:
+        UPIT() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
--- include/losses/vgg_loss.h
+++ include/losses/vgg_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor vgg_loss(torch::Tensor x);
     class VGGLoss : xt::Module
     {
     public:
+        VGGLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
