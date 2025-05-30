83138124a1a0c4736eeb1290b8765e43da53d064
kamisaberi <kamisaberi@gmail.com>
2025-05-30 20:43:44 +0330

    MOdule class forward method changed

--- include/dropouts/recurrent_dropout.h
+++ include/dropouts/recurrent_dropout.h
@@ -8,8 +8,7 @@ namespace xt::dropouts {
     struct RecurrentDropout : xt::Module {
     public:
         RecurrentDropout() = default;
-
-        torch::Tensor forward(torch::Tensor x) const override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
--- src/dropouts/layer_drop.cpp
+++ src/dropouts/layer_drop.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/20/25.
-//
+#include "include/dropouts/layer_drop.h"
+
+namespace xt::dropouts
+{
+    torch::Tensor layer_drop(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto LayerDrop::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::dropouts::layer_drop(torch::zeros(10));
+    }
+}
