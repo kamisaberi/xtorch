24388e9f09ba31d57f370ac890cd5f1bd8292050
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:12:22 +0330

    MOdule class forward method changed

--- include/losses/dual_softmax_loss.h
+++ include/losses/dual_softmax_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor dual_softmax_loss(torch::Tensor x);
     class DualSoftmaxLoss : xt::Module
     {
     public:
+        DualSoftmaxLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
