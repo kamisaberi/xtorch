32d40f98e560937266d1d151f579c3ac59575568
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:16:37 +0330

    MOdule class forward method changed

--- include/losses/early_exiting.h
+++ include/losses/early_exiting.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor early_exiting(torch::Tensor x);
     class EarlyExiting : xt::Module
     {
     public:
+        EarlyExiting() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
