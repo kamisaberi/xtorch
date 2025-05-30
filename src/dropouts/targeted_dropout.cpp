282ea4f54c80b51ea2f9a19ebaaf61a8a0f84f2c
kamisaberi <kamisaberi@gmail.com>
2025-05-30 21:31:58 +0330

    MOdule class forward method changed

--- src/dropouts/targeted_dropout.cpp
+++ src/dropouts/targeted_dropout.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/20/25.
-//
+#include "include/dropouts/targeted_dropout.h"
+
+namespace xt::dropouts
+{
+    torch::Tensor targeted_dropout(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto TargetedDropout::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::dropouts::targeted_dropout(torch::zeros(10));
+    }
+}
