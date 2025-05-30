091e70bd322fa70380507d51276988d8e070391f
kamisaberi <kamisaberi@gmail.com>
2025-05-30 23:48:05 +0330

    MOdule class forward method changed

--- include/losses/varifocal_loss.h
+++ include/losses/varifocal_loss.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor varifocal_loss(torch::Tensor x);
     class VarifocalLoss : xt::Module
     {
     public:
+        VarifocalLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
