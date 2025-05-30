de975737645240d7800e03fd4e8fca426e696551
kamisaberi <kamisaberi@gmail.com>
2025-05-30 20:19:30 +0330

    MOdule class forward method changed

--- include/dropouts/auto_dropout.h
+++ include/dropouts/auto_dropout.h
@@ -8,8 +8,7 @@ namespace xt::dropouts {
     struct AutoDropout : xt::Module {
     public:
         AutoDropout() = default;
-
-        torch::Tensor forward(torch::Tensor x) const override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
--- src/dropouts/checkerboard_dropout.cpp
+++ src/dropouts/checkerboard_dropout.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/20/25.
-//
+#include "include/dropouts/checkerboard_dropout.h"
+
+namespace xt::dropouts
+{
+    torch::Tensor checkerboard_dropout(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto CheckerboardDropout::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::dropouts::checkerboard_dropout(torch::zeros(10));
+    }
+}
