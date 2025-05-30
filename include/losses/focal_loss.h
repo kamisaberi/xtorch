b535f0c84b5d7bc0e8a5d7ab0b9be870bf07c0b7
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:33:26 +0330

    MOdule class forward method changed

--- include/losses/focal_loss.h
+++ include/losses/focal_loss.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor focal_loss(torch::Tensor x);
     class FocalLoss : xt::Module
     {
     public:
+        FocalLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
--- include/losses/gan_hinge_loss.h
+++ include/losses/gan_hinge_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor gan_hinge_loss(torch::Tensor x);
     class GANHingeLoss : xt::Module
     {
     public:
+        GANHingeLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
