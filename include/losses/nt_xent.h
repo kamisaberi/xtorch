1b36510c386a1fa683a840873f6418987e781746
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:42:44 +0330

    MOdule class forward method changed

--- include/losses/nt_xent.h
+++ include/losses/nt_xent.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor nt_xent(torch::Tensor x);
     class NTXent : xt::Module
     {
     public:
+        NTXent() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
--- include/losses/oa_loss.h
+++ include/losses/oa_loss.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor oa_loss(torch::Tensor x);
     class OALoss : xt::Module
     {
     public:
+        OALoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
