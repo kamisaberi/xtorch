cddd7dc67ee20de3a7197f6a5ca99b0cca8517a0
kamisaberi <kamisaberi@gmail.com>
2025-05-31 03:09:03 +0330

    MOdule class forward method changed

--- include/optimizations/ada_hessian.h
+++ include/optimizations/ada_hessian.h
@@ -1,13 +1,27 @@
 #pragma once
+
 #include "common.h"
 
-namespace xt::models
+namespace xt::optimizations
 {
-    struct AdaHessian : torch::optim::Optimizer
-    {
-    private:
-
+    class AdaHessian : public torch::optim::Optimizer {
     public:
-    };
+        explicit AdaHessian(std::vector<torch::Tensor>&& parameters, double lr = 0.01, double momentum = 0.9);
 
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+
+        // Getter and setter for learning rate
+        double lr() const { return lr_; }
+        void lr(double lr) { lr_ = lr; }
+
+        // Getter and setter for momentum
+        double momentum() const { return momentum_; }
+        void momentum(double momentum) { momentum_ = momentum; }
+
+    private:
+        double lr_;
+        double momentum_;
+        std::vector<torch::Tensor> velocities_;
+    };
 }
