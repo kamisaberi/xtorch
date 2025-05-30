39887cdfc991318af29c8b01a3e9a6efb3268e2c
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:54:03 +0330

    MOdule class forward method changed

--- include/losses/triplet_entropy_loss.h
+++ include/losses/triplet_entropy_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor triplet_entropy_loss(torch::Tensor x);
     class TripletEntropyLoss : xt::Module
     {
     public:
+        TripletEntropyLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
--- include/losses/triplet_loss.h
+++ include/losses/triplet_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor triplet_loss(torch::Tensor x);
     class TripletLoss : xt::Module
     {
     public:
+        TripletLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
