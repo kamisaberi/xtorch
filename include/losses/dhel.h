155137459148eadf06e39c213d2c8b90a0866673
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:04:33 +0330

    MOdule class forward method changed

--- include/losses/dhel.h
+++ include/losses/dhel.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+
+    torch::Tensor dhel(torch::Tensor x);
     class DHEL : xt::Module
     {
     public:
+        DHEL() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
