62a34081ca335c1a7bb09a7ca1559d55e0c8e17d
kamisaberi <kamisaberi@gmail.com>
2025-05-30 20:21:23 +0330

    MOdule class forward method changed

--- src/dropouts/concrete_dropout.cpp
+++ src/dropouts/concrete_dropout.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/20/25.
-//
+#include "include/dropouts/concrete_dropout.h"
+
+namespace xt::dropouts
+{
+    torch::Tensor concrete_dropout(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto ConcreteDropout::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::dropouts::concrete_dropout(torch::zeros(10));
+    }
+}
