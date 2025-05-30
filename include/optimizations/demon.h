76db090805f2f45eae20495d2327549b09460f2f
kamisaberi <kamisaberi@gmail.com>
2025-05-31 03:16:59 +0330

    MOdule class forward method changed

--- include/optimizations/demon.h
+++ include/optimizations/demon.h
@@ -1,14 +1,27 @@
 #pragma once
-#pragma once
+
 #include "common.h"
 
-namespace xt::models
+namespace xt::optimizations
 {
-    struct Demon : torch::optim::Optimizer
-    {
-    private:
-
+    class Demon : public torch::optim::Optimizer {
     public:
-    };
+        explicit Demon(std::vector<torch::Tensor>&& parameters, double lr = 0.01, double momentum = 0.9);
 
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+
+        // Getter and setter for learning rate
+        double lr() const { return lr_; }
+        void lr(double lr) { lr_ = lr; }
+
+        // Getter and setter for momentum
+        double momentum() const { return momentum_; }
+        void momentum(double momentum) { momentum_ = momentum; }
+
+    private:
+        double lr_;
+        double momentum_;
+        std::vector<torch::Tensor> velocities_;
+    };
 }
