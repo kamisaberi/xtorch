d45d533342c3dbfffc8f2d0852ca4b190d7badd9
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:52:15 +0330

    MOdule class forward method changed

--- include/losses/seesaw_loss.h
+++ include/losses/seesaw_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor seesaw_loss(torch::Tensor x);
     class SeesawLoss : xt::Module
     {
     public:
+        SeesawLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
--- include/losses/self_adjusting_smooth_l1_loss.h
+++ include/losses/self_adjusting_smooth_l1_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor self_adjusting_smooth_l1_loss(torch::Tensor x);
     class SelfAdjustingSmoothL1Loss : xt::Module
     {
     public:
+        SelfAdjustingSmoothL1Loss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
