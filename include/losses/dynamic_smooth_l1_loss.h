7d89e96c882774a6b96b2e25d781eb2a4a90aebe
kamisaberi <kamisaberi@gmail.com>
2025-05-30 22:16:37 +0330

    MOdule class forward method changed

--- include/losses/dynamic_smooth_l1_loss.h
+++ include/losses/dynamic_smooth_l1_loss.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor dynamic_smooth_l1_loss(torch::Tensor x);
     class DynamicSmoothL1Loss : xt::Module
     {
     public:
+        DynamicSmoothL1Loss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
--- include/losses/elastic_face.h
+++ include/losses/elastic_face.h
@@ -4,9 +4,12 @@
 
 namespace xt::losses
 {
+    torch::Tensor elastic_face(torch::Tensor x);
     class ElasticFace : xt::Module
     {
     public:
+        ElasticFace() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
