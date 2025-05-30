b4410a745a9dd9e5391a21b86f8f475e380b48c1
kamisaberi <kamisaberi@gmail.com>
2025-05-30 04:33:47 +0330

    MOdule class forward method changed

--- include/transforms/video/video.h
+++ include/transforms/video/video.h
@@ -3,5 +3,4 @@
 #include "frame_interpolation.h"
 #include  "optical_flow_warping.h"
 #include  "random_clip_reverse.h"
-#include  "random_short_side_scale.h"
 #include  "uniform_temporal_subsample.h"
