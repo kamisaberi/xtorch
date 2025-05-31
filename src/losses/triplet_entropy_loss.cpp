b8f85e4e60d0e3dbc120ab46dec3566597b70015
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:33:42 +0330

    MOdule class forward method changed

--- src/losses/triplet_entropy_loss.cpp
+++ src/losses/triplet_entropy_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/triplet_entropy_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor triplet_entropy_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto TripletEntropyLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::triplet_entropy_loss(torch::zeros(10));
+    }
+}
--- src/losses/triplet_loss.cpp
+++ src/losses/triplet_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/triplet_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor triplet_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto TripletLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::triplet_loss(torch::zeros(10));
+    }
+}
