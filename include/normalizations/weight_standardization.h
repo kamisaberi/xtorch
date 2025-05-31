21dd76ad08d5cc02e8e792d83a2347e0054a234b
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:19:56 +0330

    MOdule class forward method changed

--- include/normalizations/weight_standardization.h
+++ include/normalizations/weight_standardization.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class WeightStandardization : xt::Module
+    struct WeightStandardization : xt::Module
     {
     public:
+        WeightStandardization() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
