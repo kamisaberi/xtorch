e5500e705c98b4c73592d250a2bd914caab323f1
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:05:48 +0330

    MOdule class forward method changed

--- src/normalizations/spectral_normalization.cpp
+++ src/normalizations/spectral_normalization.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/spectral_normalization.h"
+
+namespace xt::norm
+{
+    auto SpectralNorm::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
