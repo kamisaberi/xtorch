adfebc0c439320b15e1b523f255d1501ac61c35a
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:31:32 +0330

    MOdule class forward method changed

--- src/losses/rank_based_loss.cpp
+++ src/losses/rank_based_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/rank_based_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor rank_based_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto RankBasedLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::rank_based_loss(torch::zeros(10));
+    }
+}
--- src/losses/seesaw_loss.cpp
+++ src/losses/seesaw_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/seesaw_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor seesaw_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto SeesawLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::seesaw_loss(torch::zeros(10));
+    }
+}
