01e96e9e459cf40b285533266e60ae4d925d9f20
kamisaberi <kamisaberi@gmail.com>
2025-05-31 03:39:09 +0330

    MOdule class forward method changed

--- include/optimizations/sm3.h
+++ include/optimizations/sm3.h
@@ -1,13 +1,27 @@
 #pragma once
+
 #include "common.h"
 
-namespace xt::models
+namespace xt::optimizations
 {
-    struct SM3 : torch::optim::Optimizer
-    {
-    private:
-
+    class SM3 : public torch::optim::Optimizer {
     public:
-    };
+        explicit SM3(std::vector<torch::Tensor>&& parameters, double lr = 0.01, double momentum = 0.9);
 
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+
+        // Getter and setter for learning rate
+        double lr() const { return lr_; }
+        void lr(double lr) { lr_ = lr; }
+
+        // Getter and setter for momentum
+        double momentum() const { return momentum_; }
+        void momentum(double momentum) { momentum_ = momentum; }
+
+    private:
+        double lr_;
+        double momentum_;
+        std::vector<torch::Tensor> velocities_;
+    };
 }
