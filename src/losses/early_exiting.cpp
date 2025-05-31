d2b908d89b57c11c98c7906374915e11f0b5e516
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:49:29 +0330

    MOdule class forward method changed

--- src/losses/early_exiting.cpp
+++ src/losses/early_exiting.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/early_exiting.h"
+
+namespace xt::losses
+{
+    torch::Tensor early_exiting(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto EarlyExiting::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::early_exiting(torch::zeros(10));
+    }
+}
