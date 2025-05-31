f1cee445bc23ae6ebbbe871f31296e1f9002b453
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:18:10 +0330

    MOdule class forward method changed

--- include/normalizations/srn.h
+++ include/normalizations/srn.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class SRN : xt::Module
+    struct SRN : xt::Module
     {
     public:
+        SRN() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
