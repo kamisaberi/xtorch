cf79f74cfbe87b65d47e6dfb35cdd1312dd9330b
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:30:21 +0330

    MOdule class forward method changed

--- src/losses/nt_xent.cpp
+++ src/losses/nt_xent.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/nt_xent.h"
+
+namespace xt::losses
+{
+    torch::Tensor nt_xent(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto NTXent::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::nt_xent(torch::zeros(10));
+    }
+}
--- src/losses/oa_loss.cpp
+++ src/losses/oa_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/oa_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor oa_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto OALoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::oa_loss(torch::zeros(10));
+    }
+}
