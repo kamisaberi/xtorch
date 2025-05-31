84ae414206fd805086fcac3dc1f953f71f539754
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:51:22 +0330

    MOdule class forward method changed

--- src/losses/gan_hinge_loss.cpp
+++ src/losses/gan_hinge_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/gan_hinge_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor gan_hinge_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto GANHingeLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::gan_hinge_loss(torch::zeros(10));
+    }
+}
