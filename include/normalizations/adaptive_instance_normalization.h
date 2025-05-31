1f7a941a899e5fb5541489e7f00bb1ee9b1b23cf
kamisaberi <kamisaberi@gmail.com>
2025-05-31 03:56:38 +0330

    MOdule class forward method changed

--- include/normalizations/adaptive_instance_normalization.h
+++ include/normalizations/adaptive_instance_normalization.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class AdaptiveInstanceNorm : xt::Module
+    struct AdaptiveInstanceNorm : xt::Module
     {
     public:
+        AdaptiveInstanceNorm() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
