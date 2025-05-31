c136b93af0e5c33d8738c4c8fd383478ca3a40b7
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:46:35 +0330

    MOdule class forward method changed

--- src/losses/dsam_loss.cpp
+++ src/losses/dsam_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/dsam_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor dsam_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto DSAMLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::dsam_loss(torch::zeros(10));
+    }
+}
