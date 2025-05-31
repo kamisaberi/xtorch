9d2c71189cd51aed051970c1c3234df7c85f9190
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:31:32 +0330

    MOdule class forward method changed

--- src/losses/proxy_anchor_loss.cpp
+++ src/losses/proxy_anchor_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/proxy_anchor_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor proxy_anchor_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto ProxyAnchorLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::proxy_anchor_loss(torch::zeros(10));
+    }
+}
