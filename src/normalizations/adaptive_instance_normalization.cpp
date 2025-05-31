a7b2525bdb86cfb886a09a9460d9e631881194cc
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:49:40 +0330

    MOdule class forward method changed

--- src/normalizations/adaptive_instance_normalization.cpp
+++ src/normalizations/adaptive_instance_normalization.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/adaptive_instance_normalization.h"
+
+namespace xt::norm
+{
+    auto AdaptiveInstanceNorm::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
