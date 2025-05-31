a3b4cf0e47726bcb99208a312ab206c72116ae47
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:07:47 +0330

    MOdule class forward method changed

--- src/normalizations/weight_standardization.cpp
+++ src/normalizations/weight_standardization.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/weight_standardization.h"
+
+namespace xt::norm
+{
+    auto WeightStandardization::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
