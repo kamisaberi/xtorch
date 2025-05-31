b3c131a09809d8a0f03ccf2e6ca5e17fa56721df
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:32:21 +0330

    MOdule class forward method changed

--- src/losses/supervised_contrastive_loss.cpp
+++ src/losses/supervised_contrastive_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/supervised_contrastive_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor supervised_contrastive_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto SupervisedContrastiveLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::supervised_contrastive_loss(torch::zeros(10));
+    }
+}
