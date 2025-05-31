55441122587c1cee7489fc39424669dd7d452b5e
kamran saberifard <kamisaberi@gmail.com>
2025-05-31 15:28:35 +0330

    optimazers cpp files created

--- src/transforms/graph/edge_perturbation.cpp
+++ src/transforms/graph/edge_perturbation.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/graph/edge_perturbation.h"
+
+namespace xt::transforms::graph {
+
+    EdgePerturbation::EdgePerturbation() = default;
+
+    EdgePerturbation::EdgePerturbation(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    auto EdgePerturbation::forward(std::initializer_list <std::any> tensors) -> std::any {
+        return torch::zeroes(10);
+    }
+
+
+}
--- src/transforms/graph/graph_coarsening.cpp
+++ src/transforms/graph/graph_coarsening.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/graph/graph_coarsening.h"
+
+namespace xt::transforms::graph {
+
+    GraphCoarsening::GraphCoarsening() = default;
+
+    GraphCoarsening::GraphCoarsening(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    auto EdgeDrop::forward(std::initializer_list <std::any> tensors) -> std::any {
+        return torch::zeroes(10);
+    }
+
+
+}
