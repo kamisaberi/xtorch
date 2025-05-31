04cab9933a5d2af4c618be61cbd0ebcbd666d155
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:20:58 +0330

    MOdule class forward method changed

--- src/optimizations/ams_grad.cpp
+++ src/optimizations/ams_grad.cpp
@@ -1,3 +1,20 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/optimizations/ams_grad.h"
+
+namespace xt::optimizations
+{
+    AMSGrad::AMSGrad(std::vector<torch::Tensor>&& parameters, double lr, double momentum)
+        : Optimizer(std::move(parameters), nullptr), lr_(lr), momentum_(momentum)
+    {
+        velocities_.resize(param_groups()[0].params().size());
+        for (size_t i = 0; i < velocities_.size(); ++i)
+        {
+            velocities_[i] = torch::zeros_like(param_groups()[0].params()[i]);
+        }
+    }
+
+    using LossClosure = std::function<torch::Tensor()>;
+    torch::Tensor AMSGrad::step(LossClosure closure)
+    {
+        return torch::Tensor();
+    }
+}
