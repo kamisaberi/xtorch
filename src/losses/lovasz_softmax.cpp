343b05964476f997cfe5815de331866b12224b0d
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:29:07 +0330

    MOdule class forward method changed

--- src/losses/lovasz_softmax.cpp
+++ src/losses/lovasz_softmax.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/lovasz_softmax.h"
+
+namespace xt::losses
+{
+    torch::Tensor lovasz_softmax(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto LovaszSoftmax::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::lovasz_softmax(torch::zeros(10));
+    }
+}
--- src/losses/multi_loss.cpp
+++ src/losses/multi_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/multi_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor multi_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto MultiLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::multi_loss(torch::zeros(10));
+    }
+}
