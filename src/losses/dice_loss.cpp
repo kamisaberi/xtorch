4c05ef9af2b78f2ab94409e5e8fdc486cc8a2634
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:45:41 +0330

    MOdule class forward method changed

--- src/losses/dice_loss.cpp
+++ src/losses/dice_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/dice_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor dice_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto DiceLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::dice_loss(torch::zeros(10));
+    }
+}
