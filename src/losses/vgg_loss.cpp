dd2b9283479d1b9d24dedc22c067146ad4adaec7
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:34:53 +0330

    MOdule class forward method changed

--- src/losses/vgg_loss.cpp
+++ src/losses/vgg_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/vgg_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor vgg_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto VGGLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::vgg_loss(torch::zeros(10));
+    }
+}
