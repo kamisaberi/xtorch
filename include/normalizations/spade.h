f4d2c58e4e6b57b22f8d28b0bdcef4cca4df9e92
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:17:17 +0330

    MOdule class forward method changed

--- include/normalizations/spade.h
+++ include/normalizations/spade.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class SPADE : xt::Module
+    struct SPADE : xt::Module
     {
     public:
+        SPADE() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
