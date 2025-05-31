2ed4b18938eebe03f713ec8ae477f77f4933b710
kamisaberi <kamisaberi@gmail.com>
2025-06-01 01:45:23 +0330

    MOdule class forward method changed

--- include/transforms/image/cutout.h
+++ include/transforms/image/cutout.h
@@ -3,18 +3,17 @@
 
 #include "include/transforms/common.h"
 
-namespace xt::transforms::image {
-
-
-    struct Cutout {
+namespace xt::transforms::image
+{
+    struct Cutout
+    {
     public:
         Cutout(int num_holes = 1, int hole_size = 16);
 
         torch::Tensor operator()(const torch::Tensor& input_tensor) const;
+
     private:
         int num_holes;
         int hole_size;
-
     };
-
 }
--- include/transforms/image/elastic_transform.h
+++ include/transforms/image/elastic_transform.h
@@ -2,7 +2,7 @@
 
 #include "include/transforms/common.h"
 
-namespace xt::transforms
+namespace xt::transforms::image
 {
     class ElasticTransform final : public xt::Module
     {
