5344cabcc0a79c61ad9f01d91dc0fdbac8045226
kamran saberifard <kamisaberi@gmail.com>
2025-05-31 15:28:35 +0330

    optimazers cpp files created

--- src/transforms/graph/edge_feature_masking.cpp
+++ src/transforms/graph/edge_feature_masking.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/graph/edge_feature_masking.h"
+
+namespace xt::transforms::graph {
+
+    EdgeFeatureMasking::EdgeFeatureMasking() = default;
+
+    EdgeFeatureMasking::EdgeFeatureMasking(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    auto EdgeFeatureMasking::forward(std::initializer_list <std::any> tensors) -> std::any {
+        return torch::zeroes(10);
+    }
+
+
+}
--- src/transforms/graph/feature_dropout.cpp
+++ src/transforms/graph/feature_dropout.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/graph/feature_dropout.h"
+
+namespace xt::transforms::graph {
+
+    FeatureDropout::FeatureDropout() = default;
+
+    FeatureDropout::FeatureDropout(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    auto FeatureDropout::forward(std::initializer_list <std::any> tensors) -> std::any {
+        return torch::zeroes(10);
+    }
+
+
+}
