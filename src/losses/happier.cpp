b223faf632d45a14ebcadc9b3608914b172d8aa1
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:54:19 +0330

    MOdule class forward method changed

--- src/losses/happier.cpp
+++ src/losses/happier.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/happier.h"
+
+namespace xt::losses
+{
+    torch::Tensor happier(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto HAPPIER::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::happier(torch::zeros(10));
+    }
+}
