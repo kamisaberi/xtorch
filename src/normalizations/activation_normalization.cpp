2fc6153772c7a12996c60ee09bda60904a99160e
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:49:40 +0330

    MOdule class forward method changed

--- src/normalizations/activation_normalization.cpp
+++ src/normalizations/activation_normalization.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/activation_normalization.h"
+
+namespace xt::norm
+{
+    auto ActiveNorm::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
