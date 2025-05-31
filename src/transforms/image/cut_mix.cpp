2ae464e9de36abe1495e02cf55db9cdd172a1362
kamisaberi <kamisaberi@gmail.com>
2025-06-01 02:49:27 +0330

    MOdule class forward method changed

--- src/transforms/image/cut_mix.cpp
+++ src/transforms/image/cut_mix.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/cut_mix.h"
 
+namespace xt::transforms::image
+{
+    CutMix::CutMix() = default;
+
+    CutMix::CutMix(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto CutMix::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
