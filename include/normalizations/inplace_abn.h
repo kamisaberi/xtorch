7d38ea46de7f8d0f1a4780a9033724b61503e755
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:11:05 +0330

    MOdule class forward method changed

--- include/normalizations/inplace_abn.h
+++ include/normalizations/inplace_abn.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class InplaceABN : xt::Module
+    struct InplaceABN : xt::Module
     {
     public:
+        InplaceABN() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
