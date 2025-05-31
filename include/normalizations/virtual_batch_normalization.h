1ce1fdae78146dd405836239b595f4aac7ff7e05
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:19:02 +0330

    MOdule class forward method changed

--- include/normalizations/virtual_batch_normalization.h
+++ include/normalizations/virtual_batch_normalization.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class VirtualBatchNorm : xt::Module
+    struct VirtualBatchNorm : xt::Module
     {
     public:
+        VirtualBatchNorm() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
