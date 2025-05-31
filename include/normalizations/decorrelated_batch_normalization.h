481a276919482422249e3cae96d00617d631d9d2
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:09:24 +0330

    MOdule class forward method changed

--- include/normalizations/decorrelated_batch_normalization.h
+++ include/normalizations/decorrelated_batch_normalization.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class DecorrelatedBatchNorm : xt::Module
+    struct DecorrelatedBatchNorm : xt::Module
     {
     public:
+        DecorrelatedBatchNorm() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
