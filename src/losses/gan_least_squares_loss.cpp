4aaf3203cd2511d89a8e2fd7b92242e97f090e4f
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:51:49 +0330

    MOdule class forward method changed

--- src/losses/gan_least_squares_loss.cpp
+++ src/losses/gan_least_squares_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/gan_least_squares_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor gan_least_squares_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto GANLeastSquaresLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::gan_least_squares_loss(torch::zeros(10));
+    }
+}
