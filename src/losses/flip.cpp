152b33f31b07912c84d12f816295a962bf1e4e6d
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:50:26 +0330

    MOdule class forward method changed

--- src/losses/flip.cpp
+++ src/losses/flip.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/flip.h"
+
+namespace xt::losses
+{
+    torch::Tensor flip(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto FLIP::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::flip(torch::zeros(10));
+    }
+}
