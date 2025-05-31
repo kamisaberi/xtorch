b96f228f6f9528a3ff16d05239e64a786a3f6617
kamisaberi <kamisaberi@gmail.com>
2025-06-01 02:36:12 +0330

    MOdule class forward method changed

--- src/transforms/image/brightness.cpp
+++ src/transforms/image/brightness.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::image {
 
     Brightness::Brightness() = default;
 
-    Brightness::Brightness(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    Brightness::Brightness(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto Brightness::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
