dd99d846ba8214a0eb45c37297655db02e163d0e
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:29:07 +0330

    MOdule class forward method changed

--- src/losses/metrix.cpp
+++ src/losses/metrix.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/metrix.h"
+
+namespace xt::losses
+{
+    torch::Tensor metrix(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto Metrix::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::metrix(torch::zeros(10));
+    }
+}
