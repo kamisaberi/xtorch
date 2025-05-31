1e39f72439bd9cffe51feeb73565c1bdc9a5de96
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:19:24 +0330

    MOdule class forward method changed

--- src/optimizations/adam_w.cpp
+++ src/optimizations/adam_w.cpp
@@ -1,3 +1,20 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/optimizations/adam_w.h"
+
+namespace xt::optimizations
+{
+    AdamW::AdamW(std::vector<torch::Tensor>&& parameters, double lr, double momentum)
+        : Optimizer(std::move(parameters), nullptr), lr_(lr), momentum_(momentum)
+    {
+        velocities_.resize(param_groups()[0].params().size());
+        for (size_t i = 0; i < velocities_.size(); ++i)
+        {
+            velocities_[i] = torch::zeros_like(param_groups()[0].params()[i]);
+        }
+    }
+
+    using LossClosure = std::function<torch::Tensor()>;
+    torch::Tensor AdamW::step(LossClosure closure)
+    {
+        return torch::Tensor();
+    }
+}
