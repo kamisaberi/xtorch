e6273c63077373330f174398acfb18802b934965
kamran saberifard <kamisaberi@gmail.com>
2025-05-31 15:55:47 +0330

    optimazers cpp files created

--- src/transforms/image/advanced_blur.cpp
+++ src/transforms/image/advanced_blur.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/advanced_blur.h"
+
+namespace xt::transforms::image  {
+
+    AdvancedBlur::AdvancedBlur() = default;
+
+    AdvancedBlur::AdvancedBlur(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    auto AdvancedBlur::forward(std::initializer_list <std::any> tensors) -> std::any {
+        return torch::zeroes(10);
+    }
+
+
+}
--- src/transforms/image/black_white.cpp
+++ src/transforms/image/black_white.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/black_white.h"
+
+namespace xt::transforms::image  {
+
+    BlackWhite::BlackWhite() = default;
+
+    BlackWhite::BlackWhite(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    }
+
+    auto BlackWhite::forward(std::initializer_list <std::any> tensors) -> std::any {
+        return torch::zeroes(10);
+    }
+
+
+}
