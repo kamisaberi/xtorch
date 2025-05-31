6f82460035125cadb7e15bed140d76e11fcb1d9e
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:12:27 +0330

    MOdule class forward method changed

--- include/normalizations/local_contrast_normalization.h
+++ include/normalizations/local_contrast_normalization.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class LocalContrastNorm : xt::Module
+    struct LocalContrastNorm : xt::Module
     {
     public:
+        LocalContrastNorm() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
