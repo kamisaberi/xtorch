dd13d40b13a7cfd1cbc2c4c3af2969f5f2c7f937
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:01:42 +0330

    MOdule class forward method changed

--- src/normalizations/inplace_abn.cpp
+++ src/normalizations/inplace_abn.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/inplace_abn.h"
+
+namespace xt::norm
+{
+    auto InplaceABN::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
