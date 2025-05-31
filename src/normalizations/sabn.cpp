fe75efb7342c4043064b3e23445245788c813caa
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:05:05 +0330

    MOdule class forward method changed

--- src/normalizations/sabn.cpp
+++ src/normalizations/sabn.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/sabn.h"
+
+namespace xt::norm
+{
+    auto SABN::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
