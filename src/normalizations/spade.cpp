a15f232e182155b3b5d10f32f6607daf9cc670b6
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:05:48 +0330

    MOdule class forward method changed

--- src/normalizations/spade.cpp
+++ src/normalizations/spade.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/spade.h"
+
+namespace xt::norm
+{
+    auto SPADE::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
