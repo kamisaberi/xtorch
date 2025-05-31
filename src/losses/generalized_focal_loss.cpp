84c774ba56b8dfc61b2995df2c70d0f3650ae2dd
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:52:24 +0330

    MOdule class forward method changed

--- src/losses/generalized_focal_loss.cpp
+++ src/losses/generalized_focal_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/generalized_focal_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor generalized_focal_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto GeneralizedFocalLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::generalized_focal_loss(torch::zeros(10));
+    }
+}
