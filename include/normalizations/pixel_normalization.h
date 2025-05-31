96b3c5e0ee7c78ab0891c6bdff472982711da456
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:14:57 +0330

    MOdule class forward method changed

--- include/normalizations/pixel_normalization.h
+++ include/normalizations/pixel_normalization.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class PixelNorm : xt::Module
+    struct PixelNorm : xt::Module
     {
     public:
+        PixelNorm() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
