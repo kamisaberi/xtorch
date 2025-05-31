cd573199b152e445ce6b13d4783b36266c2b6cda
kamisaberi <kamisaberi@gmail.com>
2025-06-01 02:33:24 +0330

    MOdule class forward method changed

--- src/transforms/image/black_white.cpp
+++ src/transforms/image/black_white.cpp
@@ -1,15 +1,15 @@
 #include "include/transforms/image/black_white.h"
 
-namespace xt::transforms::image  {
-
+namespace xt::transforms::image
+{
     BlackWhite::BlackWhite() = default;
 
-    BlackWhite::BlackWhite(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    BlackWhite::BlackWhite(std::vector<xt::Module> transforms) : xt::Module()
+    {
     }
 
-    auto BlackWhite::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+    auto BlackWhite::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
     }
-
-
 }
