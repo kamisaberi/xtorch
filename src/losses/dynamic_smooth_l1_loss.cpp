142e91cfb91894e1acafa4f89dfd2284e3bf16d7
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:48:17 +0330

    MOdule class forward method changed

--- src/losses/dynamic_smooth_l1_loss.cpp
+++ src/losses/dynamic_smooth_l1_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/dynamic_smooth_l1_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor dynamic_smooth_l1_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto DynamicSmoothL1Loss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::dynamic_smooth_l1_loss(torch::zeros(10));
+    }
+}
