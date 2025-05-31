980797806795d59fdca9f4dd6c6d94be9fc747cb
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:53:32 +0330

    MOdule class forward method changed

--- src/losses/ghm_r.cpp
+++ src/losses/ghm_r.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/ghm_r.h"
+
+namespace xt::losses
+{
+    torch::Tensor ghm_r(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto GHMR::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::ghm_r(torch::zeros(10));
+    }
+}
