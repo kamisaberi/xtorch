0f02f8ac71252d144a015ed0bb6e0abe7fa78be2
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:01:42 +0330

    MOdule class forward method changed

--- src/normalizations/instance_level_meta_normalization.cpp
+++ src/normalizations/instance_level_meta_normalization.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/instance_level_meta_normalization.h"
+
+namespace xt::norm
+{
+    auto InstanceLevelMetaNorm::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
