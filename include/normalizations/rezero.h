ea0d02c30e3b975a17f974e3b4f402730f747eda
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:16:09 +0330

    MOdule class forward method changed

--- include/normalizations/rezero.h
+++ include/normalizations/rezero.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class Rezero : xt::Module
+    struct Rezero : xt::Module
     {
     public:
+        Rezero() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
