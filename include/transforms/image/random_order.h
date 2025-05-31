be82fc0f139209ff7440c8cedffa24e68b1baf82
kamisaberi <kamisaberi@gmail.com>
2025-06-01 01:55:46 +0330

    MOdule class forward method changed

--- include/transforms/image/random_order.h
+++ include/transforms/image/random_order.h
@@ -2,7 +2,7 @@
 
 #include "include/transforms/common.h"
 
-namespace xt::transforms
+namespace xt::transforms::image
 {
     class RandomOrder final : public xt::Module
     {
--- include/transforms/image/random_posterize.h
+++ include/transforms/image/random_posterize.h
@@ -2,7 +2,7 @@
 
 #include "include/transforms/common.h"
 
-namespace xt::transforms
+namespace xt::transforms::image
 {
     class RandomPosterize final : public xt::Module
     {
