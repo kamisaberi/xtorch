b1dce5311caf60e653c525304c35a4bcae8bee0f
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:15:14 +0330

    MOdule class forward method changed

--- include/normalizations/power_normalization.h
+++ include/normalizations/power_normalization.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class PowerNorm : xt::Module
+    struct PowerNorm : xt::Module
     {
     public:
+        PowerNorm() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
