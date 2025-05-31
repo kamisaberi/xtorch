1ee08dda24eb1180a39d61bec8ca73e3c669fdc5
kamisaberi <kamisaberi@gmail.com>
2025-06-01 02:38:11 +0330

    MOdule class forward method changed

--- src/transforms/image/crop_and_pad.cpp
+++ src/transforms/image/crop_and_pad.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::image {
 
     CropAndPad::CropAndPad() = default;
 
-    CropAndPad::CropAndPad(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    CropAndPad::CropAndPad(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto CropAndPad::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
