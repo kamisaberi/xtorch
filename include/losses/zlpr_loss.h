e770dbf1ecd14aa50fa780467a0c1e22af0fb3cf
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:37:24 +0330

    MOdule class forward method changed

--- include/losses/zlpr_loss.h
+++ include/losses/zlpr_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor zlpr_loss(torch::Tensor x);
     class ZLPRLoss : xt::Module
     {
     public:
+        ZLPRLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
--- src/losses/zlpr_loss.cpp
+++ src/losses/zlpr_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/zlpr_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor zlpr_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto ZLPRLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::zlpr_loss(torch::zeros(10));
+    }
+}
