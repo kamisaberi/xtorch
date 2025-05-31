ceb06a34f10c0a996c541ce9d6b9fb5f26a20da7
kamran saberifard <kamisaberi@gmail.com>
2025-05-31 14:09:52 +0330

    optimazers cpp files created

--- src/optimizations/mpso.cpp
+++ src/optimizations/mpso.cpp
@@ -1,3 +1,20 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/optimizations/mspo.h"
+
+namespace xt::optimizations
+{
+    MSPO::MSPO(std::vector<torch::Tensor>&& parameters, double lr, double momentum)
+            : Optimizer(std::move(parameters), nullptr), lr_(lr), momentum_(momentum)
+    {
+        velocities_.resize(param_groups()[0].params().size());
+        for (size_t i = 0; i < velocities_.size(); ++i)
+        {
+            velocities_[i] = torch::zeros_like(param_groups()[0].params()[i]);
+        }
+    }
+
+    using LossClosure = std::function<torch::Tensor()>;
+    torch::Tensor MSPO::step(LossClosure closure)
+    {
+        return torch::Tensor();
+    }
+}
