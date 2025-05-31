cc2ca050990401c83ee7b0c1ed167330fb0b2843
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:05:05 +0330

    MOdule class forward method changed

--- src/normalizations/rezero.cpp
+++ src/normalizations/rezero.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/rezero.h"
+
+namespace xt::norm
+{
+    auto Rezero::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
--- src/normalizations/self_norm.cpp
+++ src/normalizations/self_norm.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/self_norm.h"
+
+namespace xt::norm
+{
+    auto SelfNorm::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
