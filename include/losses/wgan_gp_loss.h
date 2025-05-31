7070614c61a1f47fe573ce5865e92d6b5f3a2c21
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:37:24 +0330

    MOdule class forward method changed

--- include/losses/wgan_gp_loss.h
+++ include/losses/wgan_gp_loss.h
@@ -4,9 +4,13 @@
 
 namespace xt::losses
 {
+    torch::Tensor wgan_gp_loss(torch::Tensor x);
     class WGANGPLoss : xt::Module
     {
     public:
+        WGANGPLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
 
     private:
     };
--- src/losses/wgan_gp_loss.cpp
+++ src/losses/wgan_gp_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/wgan_gp_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor wgan_gp_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto WGANGPLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::wgan_gp_loss(torch::zeros(10));
+    }
+}
