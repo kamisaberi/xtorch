bf020ca3c406dfebfcf2b50f43c417bb2a678aa9
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:07:08 +0330

    MOdule class forward method changed

--- src/normalizations/weight_demodulation.cpp
+++ src/normalizations/weight_demodulation.cpp
@@ -1,3 +1,9 @@
-//
-// Created by kami on 5/21/25.
-//
+#include "include/normalizations/weight_demodulation.h"
+
+namespace xt::norm
+{
+    auto WeightDemodulization::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
