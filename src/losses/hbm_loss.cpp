be2441a7d588ca72bb4421ae09599ffe1c1e31ed
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:58:17 +0330

    MOdule class forward method changed

--- src/losses/hbm_loss.cpp
+++ src/losses/hbm_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/hbm_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor hbm_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto HBMLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::hbm_loss(torch::zeros(10));
+    }
+}
