1ec07286b70632ef72129b628d839bd3b09fff15
kamisaberi <kamisaberi@gmail.com>
2025-05-31 03:39:41 +0330

    MOdule class forward method changed

--- include/optimizations/srmm.h
+++ include/optimizations/srmm.h
@@ -1,12 +1,27 @@
 #pragma once
+
 #include "common.h"
 
-namespace xt::models
+namespace xt::optimizations
 {
-    struct SRMM : torch::optim::Optimizer
-    {
-    private:
-
+    class SRMM : public torch::optim::Optimizer {
     public:
-    };
+        explicit SRMM(std::vector<torch::Tensor>&& parameters, double lr = 0.01, double momentum = 0.9);
 
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+
+        // Getter and setter for learning rate
+        double lr() const { return lr_; }
+        void lr(double lr) { lr_ = lr; }
+
+        // Getter and setter for momentum
+        double momentum() const { return momentum_; }
+        void momentum(double momentum) { momentum_ = momentum; }
+
+    private:
+        double lr_;
+        double momentum_;
+        std::vector<torch::Tensor> velocities_;
+    };
+}
