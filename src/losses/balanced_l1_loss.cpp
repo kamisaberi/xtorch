ab124e191db31f7053b432e917f6af74cc4fa994
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:39:15 +0330

    MOdule class forward method changed

--- src/losses/balanced_l1_loss.cpp
+++ src/losses/balanced_l1_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/balanced_l1_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor balanced_l1_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto BalancedL1Loss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::balanced_l1_loss(torch::zeros(10));
+    }
+}
