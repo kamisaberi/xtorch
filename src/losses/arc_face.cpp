7f15d390c513ec251de28b01d67044c398f54d40
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:36:33 +0330

    MOdule class forward method changed

--- src/losses/arc_face.cpp
+++ src/losses/arc_face.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/arc_face.h"
+
+namespace xt::losses
+{
+    torch::Tensor arc_Face(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto ArcFace::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::arc_Face(torch::zeros(10));
+    }
+}
