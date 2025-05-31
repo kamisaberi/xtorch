dd4f32f8352c560f79df22b66531de028f0c657b
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:49:55 +0330

    MOdule class forward method changed

--- src/losses/elastic_face.cpp
+++ src/losses/elastic_face.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/elastic_face.h"
+
+namespace xt::losses
+{
+    torch::Tensor elastic_face(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto ElasticFace::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::elastic_face(torch::zeros(10));
+    }
+}
