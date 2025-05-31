210b3fae05501f6168bc9470635f465753eaa006
kamisaberi <kamisaberi@gmail.com>
2025-06-01 02:37:16 +0330

    MOdule class forward method changed

--- src/transforms/image/clahe.cpp
+++ src/transforms/image/clahe.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::image {
 
     CLAHE::CLAHE() = default;
 
-    CLAHE::CLAHE(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    CLAHE::CLAHE(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto CLAHE::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
