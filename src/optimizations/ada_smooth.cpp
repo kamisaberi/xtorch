e7fd15986b9944ca37b549a515196842d488cf80
kamisaberi <kamisaberi@gmail.com>
2025-05-31 11:17:06 +0330

    MOdule class forward method changed

--- src/optimizations/ada_smooth.cpp
+++ src/optimizations/ada_smooth.cpp
@@ -1,3 +1,20 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/optimizations/ada_smooth.h"
+
+namespace xt::optimizations
+{
+    AdaSmooth::AdaSmooth(std::vector<torch::Tensor>&& parameters, double lr, double momentum)
+        : Optimizer(std::move(parameters), nullptr), lr_(lr), momentum_(momentum)
+    {
+        velocities_.resize(param_groups()[0].params().size());
+        for (size_t i = 0; i < velocities_.size(); ++i)
+        {
+            velocities_[i] = torch::zeros_like(param_groups()[0].params()[i]);
+        }
+    }
+
+    using LossClosure = std::function<torch::Tensor()>;
+    torch::Tensor AdaSmooth::step(LossClosure closure)
+    {
+        return torch::Tensor();
+    }
+}
