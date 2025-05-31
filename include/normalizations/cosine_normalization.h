1c6fc7baa4204ca09cf1a3fb8dc525c2e09d6662
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:08:48 +0330

    MOdule class forward method changed

--- include/normalizations/cosine_normalization.h
+++ include/normalizations/cosine_normalization.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class CosineNorm : xt::Module
+    struct CosineNorm : xt::Module
     {
     public:
+        CosineNorm() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
