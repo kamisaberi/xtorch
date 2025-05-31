f8b5e8ce8a86d148475d5898760adbe36e6c3173
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:30:21 +0330

    MOdule class forward method changed

--- src/losses/piou_loss.cpp
+++ src/losses/piou_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/piou_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor piou_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto PIoULoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::piou_loss(torch::zeros(10));
+    }
+}
