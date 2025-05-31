d1aa0759c8b4b9bbf3b0fb26363169e862379415
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:47:35 +0330

    MOdule class forward method changed

--- src/losses/dual_softmax_loss.cpp
+++ src/losses/dual_softmax_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/dual_softmax_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor dual_softmax_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto DualSoftmaxLoss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::dual_softmax_loss(torch::zeros(10));
+    }
+}
