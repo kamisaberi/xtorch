1af02368eef88c122efd6939c56883331c787a4b
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:58:53 +0330

    MOdule class forward method changed

--- src/losses/info_nce.cpp
+++ src/losses/info_nce.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/info_nce.h"
+
+namespace xt::losses
+{
+    torch::Tensor info_nce(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto InfoNCE::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::info_nce(torch::zeros(10));
+    }
+}
