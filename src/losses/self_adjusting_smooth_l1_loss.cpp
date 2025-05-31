731ade38518b6b6340cf384f7cc7f9787ba0b5e5
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:32:21 +0330

    MOdule class forward method changed

--- src/losses/self_adjusting_smooth_l1_loss.cpp
+++ src/losses/self_adjusting_smooth_l1_loss.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/self_adjusting_smooth_l1_loss.h"
+
+namespace xt::losses
+{
+    torch::Tensor self_adjusting_smooth_l1_loss(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto SelfAdjustingSmoothL1Loss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::self_adjusting_smooth_l1_loss(torch::zeros(10));
+    }
+}
