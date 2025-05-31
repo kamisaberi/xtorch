194e80e7840bdab9d04472ad7081c985b61a9b9e
kamisaberi <kamisaberi@gmail.com>
2025-05-31 10:34:53 +0330

    MOdule class forward method changed

--- src/losses/upit.cpp
+++ src/losses/upit.cpp
@@ -1,3 +1,14 @@
-//
-// Created by kami on 5/19/25.
-//
+#include "include/losses/upit.h"
+
+namespace xt::losses
+{
+    torch::Tensor upit(torch::Tensor x)
+    {
+        return torch::zeros(10);
+    }
+
+    auto UPIT::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return xt::losses::upit(torch::zeros(10));
+    }
+}
