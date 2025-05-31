e35c171b69bd036adc3f70519b46340f00a88630
kamisaberi <kamisaberi@gmail.com>
2025-06-01 02:25:23 +0330

    MOdule class forward method changed

--- src/transforms/image/ada.cpp
+++ src/transforms/image/ada.cpp
@@ -1,15 +1,15 @@
 #include "include/transforms/image/ada.h"
 
-namespace xt::transforms::image {
-
+namespace xt::transforms::image
+{
     ADA::ADA() = default;
 
-    ADA::ADA(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    ADA::ADA(std::vector<xt::Module> transforms) : xt::Module()
+    {
     }
 
-    auto ADA::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+    auto ADA::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
     }
-
-
 }
