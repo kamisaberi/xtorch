d0cf120ac4182269a9e0cb61b6d639aff377d107
kamisaberi <kamisaberi@gmail.com>
2025-05-31 04:18:46 +0330

    MOdule class forward method changed

--- include/normalizations/sync_bn.h
+++ include/normalizations/sync_bn.h
@@ -1,12 +1,15 @@
 #pragma once
-#include "common.h"
 
+#include "common.h"
 
 namespace xt::norm
 {
-    class SyncBN : xt::Module
+    struct SyncBN : xt::Module
     {
     public:
+        SyncBN() = default;
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
