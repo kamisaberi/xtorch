248a9201e5671ad3858fc2db630416673afa5149
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:36:17 +0330

    MOdule class forward method changed

--- src/transforms/target/min_max_scaler.cpp
+++ src/transforms/target/min_max_scaler.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/min_max_scaler.h"
 
+namespace xt::transforms::target
+{
+    MinMaxScaler::MinMaxScaler() = default;
+
+    MinMaxScaler::MinMaxScaler(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto MinMaxScaler::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
