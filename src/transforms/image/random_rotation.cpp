a7abb6bfcdaada72a33c7a27572b1e4a794d723a
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:12:27 +0330

    optimazers cpp files created

---
+++ src/transforms/image/random_rotation.cpp
@@ -0,0 +1,15 @@
+#include "include/transforms/image/random_rotation.h"
+
+namespace xt::transforms::image
+{
+    RandomRotation::RandomRotation() = default;
+
+    RandomRotation::RandomRotation(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomRotation::RandomRotation(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
