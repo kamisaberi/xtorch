d2391bd22cb9def25d0170502eb5011153b5e4a2
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:15:27 +0330

    optimazers cpp files created

--- src/transforms/image/random_thin_plate_spline.cpp
+++ src/transforms/image/random_thin_plate_spline.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/random_thin_plate_spline.h"
 
+namespace xt::transforms::image
+{
+    RandomThinPlateSpline::RandomThinPlateSpline() = default;
+
+    RandomThinPlateSpline::RandomThinPlateSpline(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomThinPlateSpline::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
