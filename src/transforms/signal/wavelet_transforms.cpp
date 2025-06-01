183e9acc270c1d5e5fa5207ca91e9e7d07f0ccc2
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:29:04 +0330

    MOdule class forward method changed

--- src/transforms/signal/wavelet_transforms.cpp
+++ src/transforms/signal/wavelet_transforms.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/wavelet_transforms.h"
 
+namespace xt::transforms::signal
+{
+    WaveletTransforms::WaveletTransforms() = default;
+
+    WaveletTransforms::WaveletTransforms(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto WaveletTransforms::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
