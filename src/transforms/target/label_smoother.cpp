b86feead2d7dbda55370ff09bf267fba69820b1f
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:34:27 +0330

    MOdule class forward method changed

--- src/transforms/target/label_smoother.cpp
+++ src/transforms/target/label_smoother.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/label_smoother.h"
 
+namespace xt::transforms::target
+{
+    LabelSmoother::LabelSmoother() = default;
+
+    LabelSmoother::LabelSmoother(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto LabelSmoother::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
