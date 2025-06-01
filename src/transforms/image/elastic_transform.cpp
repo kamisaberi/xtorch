9ab01b265583cb80a26a09fe4091e16493d2747e
kamisaberi <kamisaberi@gmail.com>
2025-06-01 04:00:33 +0330

    MOdule class forward method changed

--- src/transforms/image/elastic_transform.cpp
+++ src/transforms/image/elastic_transform.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/4/25.
-//
+#include "include/transforms/image/elastic_transform.h"
+
+namespace xt::transforms::image
+{
+    ElasticTransform::ElasticTransform() = default;
+
+    ElasticTransform::ElasticTransform(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto ElasticTransform::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
