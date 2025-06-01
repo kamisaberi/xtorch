c968f6ca4bd45dc94396e31084e00a121ea88ae6
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:42:25 +0330

    optimazers cpp files created

--- src/transforms/image/median_blur.cpp
+++ src/transforms/image/median_blur.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/median_blur.h"
+
+namespace xt::transforms::image
+{
+    MedianBlur::MedianBlur() = default;
+
+    MedianBlur::MedianBlur(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto MedianBlur::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
