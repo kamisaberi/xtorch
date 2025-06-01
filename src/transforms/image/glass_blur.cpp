9b53b1c0c0bad7180b60fc3d760bbcbf8664f6e3
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:35:56 +0330

    optimazers cpp files created

--- src/transforms/image/glass_blur.cpp
+++ src/transforms/image/glass_blur.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/glass_blur.h"
+
+namespace xt::transforms::image
+{
+    GlassBlur::GlassBlur() = default;
+
+    GlassBlur::GlassBlur(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto GlassBlur::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
