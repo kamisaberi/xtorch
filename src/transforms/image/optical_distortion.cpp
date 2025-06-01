ae85aad9c2e10436806307d5150717cab4869904
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:44:49 +0330

    optimazers cpp files created

--- src/transforms/image/optical_distortion.cpp
+++ src/transforms/image/optical_distortion.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/optical_distortion.h"
+
+namespace xt::transforms::image
+{
+    OpticalDistortion::OpticalDistortion() = default;
+
+    OpticalDistortion::OpticalDistortion(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto OpticalDistortion::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
