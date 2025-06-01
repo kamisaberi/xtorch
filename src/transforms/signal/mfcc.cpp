31cdadde98b5801b2c4b3c459eda5780b0e2d797
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:24:33 +0330

    MOdule class forward method changed

--- src/transforms/signal/mfcc.cpp
+++ src/transforms/signal/mfcc.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/mfcc.h"
 
+namespace xt::transforms::signal
+{
+    MFCC::MFCC() = default;
+
+    MFCC::MFCC(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto MFCC::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
