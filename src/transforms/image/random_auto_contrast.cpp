f39319e8815d30d1861d9701e4604d9f8cc10eb9
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:50:22 +0330

    optimazers cpp files created

--- src/transforms/image/random_auto_contrast.cpp
+++ src/transforms/image/random_auto_contrast.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/4/25.
-//
+#include "include/transforms/image/random_auto_contrast.h"
+
+namespace xt::transforms::image
+{
+    RandomAutoContrast::RandomAutoContrast() = default;
+
+    RandomAutoContrast::RandomAutoContrast(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomAutoContrast::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
