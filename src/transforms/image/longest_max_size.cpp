5da5de03c173676657155084cb8da02bd886a8f9
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:41:11 +0330

    optimazers cpp files created

--- src/transforms/image/longest_max_size.cpp
+++ src/transforms/image/longest_max_size.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/longest_max_size.h"
+
+namespace xt::transforms::image
+{
+    LongestMaxSize::LongestMaxSize() = default;
+
+    LongestMaxSize::LongestMaxSize(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto LongestMaxSize::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
