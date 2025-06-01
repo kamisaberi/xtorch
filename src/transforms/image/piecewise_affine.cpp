14383aebc6e39ab73ddef4b05f1df34633de2378
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:46:50 +0330

    optimazers cpp files created

--- src/transforms/image/piecewise_affine.cpp
+++ src/transforms/image/piecewise_affine.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/piecewise_affine.h"
+
+namespace xt::transforms::image
+{
+    PiecewiseAffine::PiecewiseAffine() = default;
+
+    PiecewiseAffine::PiecewiseAffine(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto PiecewiseAffine::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
