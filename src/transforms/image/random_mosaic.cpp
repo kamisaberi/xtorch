2996120530e5d73cdca041b9fa5a3abfafabca60
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:08:18 +0330

    optimazers cpp files created

--- src/transforms/image/random_mosaic.cpp
+++ src/transforms/image/random_mosaic.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/random_mosaic.h"
+
+namespace xt::transforms::image
+{
+    RandomMosaic::RandomMosaic() = default;
+
+    RandomMosaic::RandomMosaic(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomMosaic::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
