38aca5079b1bb1331e3d2244243b3f254a405699
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:37:56 +0330

    optimazers cpp files created

--- src/transforms/image/grid_mask.cpp
+++ src/transforms/image/grid_mask.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/grid_mask.h"
 
+namespace xt::transforms::image
+{
+    GridMask::GridMask() = default;
+
+    GridMask::GridMask(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto GridMask::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
