7aeb8c2ad1f28b09745e40febba449c8afc05024
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:24:24 +0330

    optimazers cpp files created

--- src/transforms/image/spatter.cpp
+++ src/transforms/image/spatter.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/spatter.h"
+
+namespace xt::transforms::image
+{
+    Spatter::Spatter() = default;
+
+    Spatter::Spatter(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Spatter::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
