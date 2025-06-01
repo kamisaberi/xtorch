176ffbaf2e9fd60d44c77880c1c56dfe03fa53e5
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:42:59 +0330

    optimazers cpp files created

--- src/transforms/image/mix_up.cpp
+++ src/transforms/image/mix_up.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/mix_up.h"
 
+namespace xt::transforms::image
+{
+    MixUp::MixUp() = default;
+
+    MixUp::MixUp(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto MixUp::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
