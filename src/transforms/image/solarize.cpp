c9ae563bb76d4070cf735502fce7149b34e590be
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:23:42 +0330

    optimazers cpp files created

--- src/transforms/image/solarize.cpp
+++ src/transforms/image/solarize.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/solarize.h"
+
+namespace xt::transforms::image
+{
+    Solarize::Solarize() = default;
+
+    Solarize::Solarize(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Solarize::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
