1253332992e8cacf1a63a7ba4d3e409b2587f6f0
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:56:03 +0330

    optimazers cpp files created

--- src/transforms/image/random_grayscale.cpp
+++ src/transforms/image/random_grayscale.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/4/25.
-//
+#include "include/transforms/image/random_grayscale.h"
+
+namespace xt::transforms::image
+{
+    RandomGrayscale::RandomGrayscale() = default;
+
+    RandomGrayscale::RandomGrayscale(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomGrayscale::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
