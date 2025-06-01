955ec912616847f14cfed55a5e89c2e7eedbdd93
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:25:01 +0330

    MOdule class forward method changed

--- src/transforms/signal/pitch_shift.cpp
+++ src/transforms/signal/pitch_shift.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/pitch_shift.h"
 
+namespace xt::transforms::signal
+{
+    PitchShift::PitchShift() = default;
+
+    PitchShift::PitchShift(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto PitchShift::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
