0f2364182231dcfbe540017424817abc40034c55
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:34:48 +0330

    optimazers cpp files created

--- src/transforms/image/flip.cpp
+++ src/transforms/image/flip.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/flip.h"
+
+namespace xt::transforms::image
+{
+    Flip::Flip() = default;
+
+    Flip::Flip(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Flip::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
