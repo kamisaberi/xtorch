04abf735cdfae39351fb3790e074471a38bb87ac
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:56:49 +0330

    optimazers cpp files created

--- src/transforms/signal/add_noise.cpp
+++ src/transforms/signal/add_noise.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/add_noise.h"
 
+namespace xt::transforms::signal
+{
+    AddNoise::AddNoise() = default;
+
+    AddNoise::AddNoise(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto AddNoise::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
