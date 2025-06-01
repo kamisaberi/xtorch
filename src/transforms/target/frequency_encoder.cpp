fae7c84eef83be7a6a4a52077257a549128b695e
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:30:07 +0330

    MOdule class forward method changed

--- src/transforms/target/frequency_encoder.cpp
+++ src/transforms/target/frequency_encoder.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/frequency_encoder.h"
 
+namespace xt::transforms::target
+{
+    FrequencyEncoder::FrequencyEncoder() = default;
+
+    FrequencyEncoder::FrequencyEncoder(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto FrequencyEncoder::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
