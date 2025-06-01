b48e3bc19315e3cd351a76fb0812d05bd1e4e612
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:36:43 +0330

    optimazers cpp files created

--- src/transforms/image/grid_distortion.cpp
+++ src/transforms/image/grid_distortion.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/grid_distortion.h"
+
+namespace xt::transforms::image
+{
+    GridDistortion::GridDistortion() = default;
+
+    GridDistortion::GridDistortion(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto GridDistortion::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
