8c57c77d140a3065370ea5b687a7aa306380b5c5
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:50:38 +0330

    MOdule class forward method changed

--- src/transforms/text/synonym_replacement.cpp
+++ src/transforms/text/synonym_replacement.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/text/synonym_replacement.h"
+
+namespace xt::transforms::text
+{
+    SynonymReplacement::SynonymReplacement() = default;
+
+    SynonymReplacement::SynonymReplacement(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto SynonymReplacement::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
