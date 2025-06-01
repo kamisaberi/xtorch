e2f6773ad124cb0787a5e8b78539f802c91b8dae
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:38:02 +0330

    MOdule class forward method changed

--- src/transforms/target/power_transformer.cpp
+++ src/transforms/target/power_transformer.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/power_transformer.h"
 
+namespace xt::transforms::target
+{
+    PowerTransformer::PowerTransformer() = default;
+
+    PowerTransformer::PowerTransformer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto PowerTransformer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
