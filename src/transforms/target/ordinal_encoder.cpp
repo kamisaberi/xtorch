15ebe2eafc20bda79ddfc825efbebe292674d894
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:37:38 +0330

    MOdule class forward method changed

--- src/transforms/target/ordinal_encoder.cpp
+++ src/transforms/target/ordinal_encoder.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/ordinal_encoder.h"
 
+namespace xt::transforms::target
+{
+    OrdinalEncoder::OrdinalEncoder() = default;
+
+    OrdinalEncoder::OrdinalEncoder(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto OrdinalEncoder::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
