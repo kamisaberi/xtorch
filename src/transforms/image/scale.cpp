e6560b44c98ffe53fc31c31d82428f832994e8d4
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:21:51 +0330

    optimazers cpp files created

--- src/transforms/image/scale.cpp
+++ src/transforms/image/scale.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/image/scale.h"
 
+namespace xt::transforms::image
+{
+    Scale::Scale() = default;
+
+    Scale::Scale(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Scale::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
