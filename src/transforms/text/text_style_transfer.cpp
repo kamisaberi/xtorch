5e2be70af5c5a5858d0571c9a8a438c12583faee
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:51:07 +0330

    MOdule class forward method changed

--- src/transforms/text/text_style_transfer.cpp
+++ src/transforms/text/text_style_transfer.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/text/text_style_transfer.h"
+
+namespace xt::transforms::text
+{
+    TextStyleTransfer::TextStyleTransfer() = default;
+
+    TextStyleTransfer::TextStyleTransfer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto TextStyleTransfer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
