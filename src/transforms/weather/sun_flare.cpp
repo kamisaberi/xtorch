fd53b76b3ab8c5d8cbd0a6ff6639d6a1cedc1ed7
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:30:08 +0330

    MOdule class forward method changed

--- src/transforms/weather/sun_flare.cpp
+++ src/transforms/weather/sun_flare.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/sun_flare.h"
+
+namespace xt::transforms::weather
+{
+    SunFlare::SunFlare() = default;
+
+    SunFlare::SunFlare(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto SunFlare::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
