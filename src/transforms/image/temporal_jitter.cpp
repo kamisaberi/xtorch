9cae797ac2f6ccd67c8f4d221e5dbee6924b1320
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:52:32 +0330

    optimazers cpp files created

--- src/transforms/image/temporal_jitter.cpp
+++ src/transforms/image/temporal_jitter.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/temporal_jitter.h"
+
+namespace xt::transforms::image
+{
+    TemporalJitter::TemporalJitter() = default;
+
+    TemporalJitter::TemporalJitter(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto TemporalJitter::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
