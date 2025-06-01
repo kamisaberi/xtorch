bc41e7bd6b034707d0078d352ac8d68915574873
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:35:22 +0330

    MOdule class forward method changed

--- src/transforms/target/log_transformer.cpp
+++ src/transforms/target/log_transformer.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/log_transformer.h"
 
+namespace xt::transforms::target
+{
+    LogTransformer::LogTransformer() = default;
+
+    LogTransformer::LogTransformer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto LogTransformer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
