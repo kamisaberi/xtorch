7640fe95480bd721eb42aa290d73a51836059efa
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:27:40 +0330

    MOdule class forward method changed

--- src/transforms/weather/falling_snow.cpp
+++ src/transforms/weather/falling_snow.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/falling_snow.h"
+
+namespace xt::transforms::weather
+{
+    FallingSnow::FallingSnow() = default;
+
+    FallingSnow::FallingSnow(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto FallingSnow::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
