a2120a7f4d0ab1303f0a92a31783bca11e65fd2c
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:26:54 +0330

    MOdule class forward method changed

--- src/transforms/weather/dust_sand_clouds.cpp
+++ src/transforms/weather/dust_sand_clouds.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/dust_sand_clouds.h"
+
+namespace xt::transforms::weather
+{
+    DustSandClouds::DustSandClouds() = default;
+
+    DustSandClouds::DustSandClouds(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto DustSandClouds::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
