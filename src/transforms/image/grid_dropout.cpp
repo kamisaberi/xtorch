bbe72eff8bc153ba31f1a4f509fefdd3b6bafa30
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:37:20 +0330

    optimazers cpp files created

--- src/transforms/image/grid_dropout.cpp
+++ src/transforms/image/grid_dropout.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/grid_dropout.h"
+
+namespace xt::transforms::image
+{
+    GridDropout::GridDropout() = default;
+
+    GridDropout::GridDropout(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto GridDropout::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
