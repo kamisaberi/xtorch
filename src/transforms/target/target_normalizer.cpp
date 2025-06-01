39f0985269a449bf289c6a7da759b7337f110f71
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:40:06 +0330

    MOdule class forward method changed

--- src/transforms/target/target_normalizer.cpp
+++ src/transforms/target/target_normalizer.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/target_normalizer.h"
 
+namespace xt::transforms::target
+{
+    TargetNormalizer::TargetNormalizer() = default;
+
+    TargetNormalizer::TargetNormalizer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto TargetNormalizer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
