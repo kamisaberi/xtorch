257412735d6ca7dabb7e9d7e889052e0df723cc3
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:22:29 +0330

    optimazers cpp files created

--- src/transforms/image/sharpen.cpp
+++ src/transforms/image/sharpen.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/sharpen.h"
+
+namespace xt::transforms::image
+{
+    Sharpen::Sharpen() = default;
+
+    Sharpen::Sharpen(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Sharpen::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
