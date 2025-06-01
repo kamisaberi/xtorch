e696d6b399faade4334b927231969d3f975f0d22
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:46:02 +0330

    optimazers cpp files created

--- src/transforms/image/perspective.cpp
+++ src/transforms/image/perspective.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/perspective.h"
+
+namespace xt::transforms::image
+{
+    Perspective::Perspective() = default;
+
+    Perspective::Perspective(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Perspective::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
