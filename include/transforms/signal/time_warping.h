87627b8e5ebc358a1ba6332b2bd39e0c9e3800b4
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:17:13 +0330

    MOdule class forward method changed

--- include/transforms/signal/time_warping.h
+++ include/transforms/signal/time_warping.h
@@ -2,7 +2,7 @@
 
 #include "include/transforms/common.h"
 
-namespace xt::transforms
+namespace xt::transforms::signal
 {
     class TimeWarping final : public xt::Module
     {
--- include/transforms/signal/wavelet_transforms.h
+++ include/transforms/signal/wavelet_transforms.h
@@ -2,7 +2,7 @@
 
 #include "include/transforms/common.h"
 
-namespace xt::transforms
+namespace xt::transforms::signal
 {
     class WaveletTransforms final : public xt::Module
     {
