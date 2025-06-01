0fd82387f1e837bd27f31414882f9bf3ca53bd7d
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:30:35 +0330

    MOdule class forward method changed

--- src/transforms/target/hashing_encoder.cpp
+++ src/transforms/target/hashing_encoder.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/hashing_encoder.h"
 
+namespace xt::transforms::target
+{
+    HashingEncoder::HashingEncoder() = default;
+
+    HashingEncoder::HashingEncoder(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto HashingEncoder::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
