ca315e5ca52f807bdbb30322241319b4dbd615b7
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:51:42 +0330

    optimazers cpp files created

--- src/transforms/image/random_brightness_contrast.cpp
+++ src/transforms/image/random_brightness_contrast.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/random_brightness_contrast.h"
+
+namespace xt::transforms::image
+{
+    RandomBrightnessContrast::RandomBrightnessContrast() = default;
+
+    RandomBrightnessContrast::RandomBrightnessContrast(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomBrightnessContrast::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
