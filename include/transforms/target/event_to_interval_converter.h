2e530e6f91b535c80960c180ef2f5a37726bca94
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:21:03 +0330

    MOdule class forward method changed

--- include/transforms/target/event_to_interval_converter.h
+++ include/transforms/target/event_to_interval_converter.h
@@ -2,7 +2,7 @@
 
 #include "include/transforms/common.h"
 
-namespace xt::transforms
+namespace xt::transforms::target
 {
     class EventToIntervalConverter final : public xt::Module
     {
--- include/transforms/target/frequency_encoder.h
+++ include/transforms/target/frequency_encoder.h
@@ -2,7 +2,7 @@
 
 #include "include/transforms/common.h"
 
-namespace xt::transforms
+namespace xt::transforms::target
 {
     class FrequencyEncoder final : public xt::Module
     {
