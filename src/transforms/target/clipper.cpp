1466bf105ba8bc5c3b443df8a0e3982f941b577c
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:26:55 +0330

    MOdule class forward method changed

--- src/transforms/target/clipper.cpp
+++ src/transforms/target/clipper.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/clipper.h"
 
+namespace xt::transforms::target
+{
+    Clipper::Clipper() = default;
+
+    Clipper::Clipper(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Clipper::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
