0405fea239904f5ecfaa2312ff9360c45c4f9654
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:25:00 +0330

    optimazers cpp files created

--- src/transforms/image/style_mixing.cpp
+++ src/transforms/image/style_mixing.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/style_mixing.h"
 
+namespace xt::transforms::image
+{
+    StyleMixing::StyleMixing() = default;
+
+    StyleMixing::StyleMixing(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto StyleMixing::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
