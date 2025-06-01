10c0e2ea90cfa6df0d489627057b269af9e8cff6
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:23:12 +0330

    optimazers cpp files created

--- src/transforms/image/smallest_max_size.cpp
+++ src/transforms/image/smallest_max_size.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/smallest_max_size.h"
+
+namespace xt::transforms::image
+{
+    SmallestMaxSize::SmallestMaxSize() = default;
+
+    SmallestMaxSize::SmallestMaxSize(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto SmallestMaxSize::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
