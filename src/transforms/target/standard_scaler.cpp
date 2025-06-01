4b44c95e1b683e4878a42532f518f1c08390c7b5
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:39:15 +0330

    MOdule class forward method changed

--- src/transforms/target/standard_scaler.cpp
+++ src/transforms/target/standard_scaler.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/standard_scaler.h"
 
+namespace xt::transforms::target
+{
+    StandardScaler::StandardScaler() = default;
+
+    StandardScaler::StandardScaler(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto StandardScaler::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
