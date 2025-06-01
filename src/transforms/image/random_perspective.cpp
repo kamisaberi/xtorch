0cb629e46a9b5072b389bfc75f6baff8ff645802
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:09:25 +0330

    optimazers cpp files created

--- src/transforms/image/random_perspective.cpp
+++ src/transforms/image/random_perspective.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/4/25.
-//
+#include "include/transforms/image/random_perspective.h"
+
+namespace xt::transforms::image
+{
+    RandomPerspective::RandomPerspective() = default;
+
+    RandomPerspective::RandomPerspective(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomPerspective::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
