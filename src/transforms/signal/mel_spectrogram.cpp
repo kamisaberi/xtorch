9caf2292e026389523b33961cb399d5c4bf71fef
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:23:10 +0330

    MOdule class forward method changed

--- src/transforms/signal/mel_spectrogram.cpp
+++ src/transforms/signal/mel_spectrogram.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/mel_spectrogram.h"
 
+namespace xt::transforms::signal
+{
+    MelSpectrogram::MelSpectrogram() = default;
+
+    MelSpectrogram::MelSpectrogram(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto MelSpectrogram::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
