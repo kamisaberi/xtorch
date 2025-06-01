0d77565690d1bb1336caf7cbf3d99b35cc45a525
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:28:26 +0330

    MOdule class forward method changed

--- src/transforms/target/event_rate_calculator.cpp
+++ src/transforms/target/event_rate_calculator.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/event_rate_calculator.h"
 
+namespace xt::transforms::target
+{
+    EventRateCalculator::EventRateCalculator() = default;
+
+    EventRateCalculator::EventRateCalculator(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto EventRateCalculator::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
