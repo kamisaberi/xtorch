17630fb84e625b2b4dc74384cbd90f65e34b8898
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:28:04 +0330

    MOdule class forward method changed

--- src/transforms/weather/foggy_rain.cpp
+++ src/transforms/weather/foggy_rain.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/foggy_rain.h"
+
+namespace xt::transforms::weather
+{
+    FoggyRain::FoggyRain() = default;
+
+    FoggyRain::FoggyRain(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto FoggyRain::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
