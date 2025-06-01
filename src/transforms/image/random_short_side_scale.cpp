5e4c79f3081fd55322eae42a529bb4d6635a3e1b
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:14:14 +0330

    optimazers cpp files created

--- src/transforms/image/random_short_side_scale.cpp
+++ src/transforms/image/random_short_side_scale.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/10/25.
-//
+#include "include/transforms/image/random_short_side_scale.h"
+
+namespace xt::transforms::image
+{
+    RandomShortSideScale::RandomShortSideScale() = default;
+
+    RandomShortSideScale::RandomShortSideScale(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomShortSideScale::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
