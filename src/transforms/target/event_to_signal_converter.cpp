1a59c21035382ef57da9e5cbeae50c734892f6dd
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:29:20 +0330

    MOdule class forward method changed

--- src/transforms/target/event_to_signal_converter.cpp
+++ src/transforms/target/event_to_signal_converter.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/event_to_signal_converter.h"
 
+namespace xt::transforms::target
+{
+    EventToSignalConverter::EventToSignalConverter() = default;
+
+    EventToSignalConverter::EventToSignalConverter(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto EventToSignalConverter::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
