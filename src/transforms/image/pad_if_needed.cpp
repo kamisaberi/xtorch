18c0658a50d1c9dd745e23c86461ac5d2579462c
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:45:31 +0330

    optimazers cpp files created

--- src/transforms/image/pad_if_needed.cpp
+++ src/transforms/image/pad_if_needed.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/pad_if_needed.h"
+
+namespace xt::transforms::image
+{
+    PadIfNeeded::PadIfNeeded() = default;
+
+    PadIfNeeded::PadIfNeeded(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto PadIfNeeded::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
