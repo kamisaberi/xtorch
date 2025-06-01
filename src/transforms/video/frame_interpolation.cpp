534c34318d0e8250ee5248035b30554a26743d79
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:06:14 +0330

    MOdule class forward method changed

--- src/transforms/video/frame_interpolation.cpp
+++ src/transforms/video/frame_interpolation.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/video/frame_interpolation.h"
 
+namespace xt::transforms::video
+{
+    FrameInterpolation::FrameInterpolation() = default;
+
+    FrameInterpolation::FrameInterpolation(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto FrameInterpolation::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
