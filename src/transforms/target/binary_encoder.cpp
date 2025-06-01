0d82172df92c189e3da7eee413aff7f4ef53967b
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:25:07 +0330

    MOdule class forward method changed

--- src/transforms/target/binary_encoder.cpp
+++ src/transforms/target/binary_encoder.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/binary_encoder.h"
 
+namespace xt::transforms::target
+{
+    BinaryEncoder::BinaryEncoder() = default;
+
+    BinaryEncoder::BinaryEncoder(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto BinaryEncoder::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
