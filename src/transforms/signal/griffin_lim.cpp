eccfb7e73a895e11f58097af26207fea102ff267
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:21:17 +0330

    MOdule class forward method changed

--- src/transforms/signal/griffin_lim.cpp
+++ src/transforms/signal/griffin_lim.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/griffin_lim.h"
 
+namespace xt::transforms::signal
+{
+    GriffinLim::GriffinLim() = default;
+
+    GriffinLim::GriffinLim(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto GriffinLim::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
