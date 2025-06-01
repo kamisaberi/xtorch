4fb3e9f980401884ee32bd48ce7899d0f2fe84f6
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:19:02 +0330

    MOdule class forward method changed

--- src/transforms/signal/background_noise_addition.cpp
+++ src/transforms/signal/background_noise_addition.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/background_noise_addition.h"
 
+namespace xt::transforms::signal
+{
+    BackgroundNoiseAddition::BackgroundNoiseAddition() = default;
+
+    BackgroundNoiseAddition::BackgroundNoiseAddition(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto BackgroundNoiseAddition::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
