ab47e26f7654c6a1283c2cd277085472910db977
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:53:48 +0330

    optimazers cpp files created

--- src/transforms/image/random_crop_near_bbox.cpp
+++ src/transforms/image/random_crop_near_bbox.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/random_crop_near_bbox.h"
+
+namespace xt::transforms::image
+{
+    RandomCropNearBbox::RandomCropNearBbox() = default;
+
+    RandomCropNearBbox::RandomCropNearBbox(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomCropNearBbox::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
