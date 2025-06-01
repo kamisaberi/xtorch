e8251c5dd3f4d367554a73dbca6f72cec14441b6
kamisaberi <kamisaberi@gmail.com>
2025-06-01 04:01:44 +0330

    MOdule class forward method changed

--- src/transforms/image/equalize.cpp
+++ src/transforms/image/equalize.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/equalize.h"
+
+namespace xt::transforms::image
+{
+    Equalize::Equalize() = default;
+
+    Equalize::Equalize(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Equalize::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
