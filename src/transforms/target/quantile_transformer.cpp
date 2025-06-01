b3d293297f039145c34c205586aadbf577364e45
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:38:25 +0330

    MOdule class forward method changed

--- src/transforms/target/quantile_transformer.cpp
+++ src/transforms/target/quantile_transformer.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/quantile_transformer.h"
 
+namespace xt::transforms::target
+{
+    QuantileTransformer::QuantileTransformer() = default;
+
+    QuantileTransformer::QuantileTransformer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto QuantileTransformer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
