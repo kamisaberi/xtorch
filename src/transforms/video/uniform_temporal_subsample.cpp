8f5134c0309ccadf11c1a46b9bd65cf064984d60
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:07:32 +0330

    MOdule class forward method changed

--- src/transforms/video/uniform_temporal_subsample.cpp
+++ src/transforms/video/uniform_temporal_subsample.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/video/uniform_temporal_subsample.h"
 
+namespace xt::transforms::video
+{
+    UniformTemporalSubsample::UniformTemporalSubsample() = default;
+
+    UniformTemporalSubsample::UniformTemporalSubsample(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto UniformTemporalSubsample::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
