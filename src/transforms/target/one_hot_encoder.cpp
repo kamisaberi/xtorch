28db3471e8ce37ed46378e745b1f7371ea274eb1
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:37:13 +0330

    MOdule class forward method changed

--- src/transforms/target/one_hot_encoder.cpp
+++ src/transforms/target/one_hot_encoder.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/one_hot_encoder.h"
 
+namespace xt::transforms::target
+{
+    OneHotEncoder::OneHotEncoder() = default;
+
+    OneHotEncoder::OneHotEncoder(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto OneHotEncoder::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
