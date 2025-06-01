5592d24438e45f204e79991e39973b992a62d259
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:51:02 +0330

    optimazers cpp files created

--- src/transforms/image/random_brightness.cpp
+++ src/transforms/image/random_brightness.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/random_brightness.h"
+
+namespace xt::transforms::image
+{
+    RandomBrightness::RandomBrightness() = default;
+
+    RandomBrightness::RandomBrightness(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomBrightness::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
