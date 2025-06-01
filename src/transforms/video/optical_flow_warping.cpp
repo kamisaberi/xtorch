0f4186dd73e4a379819f741cde707177d1638b22
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:06:41 +0330

    MOdule class forward method changed

--- src/transforms/video/optical_flow_warping.cpp
+++ src/transforms/video/optical_flow_warping.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/video/optical_flow_warping.h"
 
+namespace xt::transforms::video
+{
+    OpticalFlowWarping::OpticalFlowWarping() = default;
+
+    OpticalFlowWarping::OpticalFlowWarping(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto OpticalFlowWarping::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
