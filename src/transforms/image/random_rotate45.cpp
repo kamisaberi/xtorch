605e495f9fd1273a75e0c9eb0b997fd85efe05c5
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:11:15 +0330

    optimazers cpp files created

--- src/transforms/image/random_rotate45.cpp
+++ src/transforms/image/random_rotate45.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/random_rotate45.h"
+
+namespace xt::transforms::image
+{
+    RandomRotate45::RandomRotate45() = default;
+
+    RandomRotate45::RandomRotate45(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomRotate45::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
