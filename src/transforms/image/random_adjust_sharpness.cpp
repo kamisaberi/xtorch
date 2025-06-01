c1f602d390460fa7dc31f93d0c69a7c568109f72
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:48:33 +0330

    optimazers cpp files created

--- src/transforms/image/random_adjust_sharpness.cpp
+++ src/transforms/image/random_adjust_sharpness.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/4/25.
-//
+#include "include/transforms/image/random_adjust_sharpness.h"
+
+namespace xt::transforms::image
+{
+    RandomAdjustSharpness::RandomAdjustSharpness() = default;
+
+    RandomAdjustSharpness::RandomAdjustSharpness(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomAdjustSharpness::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
