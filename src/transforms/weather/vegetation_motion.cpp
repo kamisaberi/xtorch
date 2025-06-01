bdca0e97450a43778ae6a0cfb659e8edda490f16
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:30:34 +0330

    MOdule class forward method changed

--- src/transforms/weather/vegetation_motion.cpp
+++ src/transforms/weather/vegetation_motion.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/vegetation_motion.h"
+
+namespace xt::transforms::weather
+{
+    VegetationMotion::VegetationMotion() = default;
+
+    VegetationMotion::VegetationMotion(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto VegetationMotion::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
