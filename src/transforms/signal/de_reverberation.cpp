7f61360868eb1b3d8966e6d52460344bb5c84987
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:20:07 +0330

    MOdule class forward method changed

--- src/transforms/signal/de_reverberation.cpp
+++ src/transforms/signal/de_reverberation.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/de_reverberation.h"
 
+namespace xt::transforms::signal
+{
+    DeReverberation::DeReverberation() = default;
+
+    DeReverberation::DeReverberation(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto DeReverberation::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
