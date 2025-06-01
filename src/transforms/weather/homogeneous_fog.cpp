91b539cfd4b338763b36cd1d204cacac8c8b7809
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:28:30 +0330

    MOdule class forward method changed

--- src/transforms/weather/homogeneous_fog.cpp
+++ src/transforms/weather/homogeneous_fog.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/homogeneous_fog.h"
+
+namespace xt::transforms::weather
+{
+    HomogeneousFog::HomogeneousFog() = default;
+
+    HomogeneousFog::HomogeneousFog(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto HomogeneousFog::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
