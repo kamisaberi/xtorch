721c41f020e008ea0e88e9f07cd774813566dcdd
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:09:59 +0330

    optimazers cpp files created

--- src/transforms/image/random_posterize.cpp
+++ src/transforms/image/random_posterize.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/random_posterize.h"
 
+namespace xt::transforms::image
+{
+    RandomPosterize::RandomPosterize() = default;
+
+    RandomPosterize::RandomPosterize(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomPosterize::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
