dbb50dd3add44598d3da8602bf25aa21fa552af3
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:48:56 +0330

    MOdule class forward method changed

--- src/transforms/text/bert_tokenizer.cpp
+++ src/transforms/text/bert_tokenizer.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/text/bert_tokenizer.h"
+
+namespace xt::transforms::text
+{
+    BertTokenizer::BertTokenizer() = default;
+
+    BertTokenizer::BertTokenizer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto BertTokenizer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
