60465b703a1455619a65f42072f3fe55f6f85e98
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:49:20 +0330

    MOdule class forward method changed

--- src/transforms/text/pad_transform.cpp
+++ src/transforms/text/pad_transform.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/text/pad_transform.h"
 
+namespace xt::transforms::text
+{
+    PadTransform::PadTransform() = default;
+
+    PadTransform::PadTransform(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto PadTransform::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
