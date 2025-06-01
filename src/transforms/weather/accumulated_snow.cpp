4798d5ffecb3a39b482b058088eacb66a1fe6532
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:20:19 +0330

    MOdule class forward method changed

--- src/transforms/weather/accumulated_snow.cpp
+++ src/transforms/weather/accumulated_snow.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/accumulated_snow.h"
+
+namespace xt::transforms::weather
+{
+    AccumulatedSnow::AccumulatedSnow() = default;
+
+    AccumulatedSnow::AccumulatedSnow(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto AccumulatedSnow::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
