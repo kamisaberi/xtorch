56b8cb060fb69943c504ab0a2a28d0d01f78a91c
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:44:17 +0330

    optimazers cpp files created

--- src/transforms/image/noise_injection.cpp
+++ src/transforms/image/noise_injection.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/noise_injection.h"
 
+namespace xt::transforms::image
+{
+    NoiseInjection::NoiseInjection() = default;
+
+    NoiseInjection::NoiseInjection(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto NoiseInjection::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
