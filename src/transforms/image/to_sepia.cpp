02c551b0720cdc5e45fe9d1aecba7baa7136d0ed
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:53:09 +0330

    optimazers cpp files created

--- src/transforms/image/to_sepia.cpp
+++ src/transforms/image/to_sepia.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/to_sepia.h"
+
+namespace xt::transforms::image
+{
+    ToSepia::ToSepia() = default;
+
+    ToSepia::ToSepia(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto ToSepia::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
