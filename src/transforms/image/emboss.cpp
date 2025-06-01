ab6f351e493c4caab6e2269b69613bbeb4045fe2
kamisaberi <kamisaberi@gmail.com>
2025-06-01 04:01:17 +0330

    MOdule class forward method changed

--- src/transforms/image/emboss.cpp
+++ src/transforms/image/emboss.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/emboss.h"
+
+namespace xt::transforms::image
+{
+    Emboss::Emboss() = default;
+
+    Emboss::Emboss(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Emboss::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
