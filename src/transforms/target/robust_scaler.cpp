e52779d8a3d180d5d6474db8cfca534a359dca6f
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:38:51 +0330

    MOdule class forward method changed

--- src/transforms/target/robust_scaler.cpp
+++ src/transforms/target/robust_scaler.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/robust_scaler.h"
 
+namespace xt::transforms::target
+{
+    RobustScaler::RobustScaler() = default;
+
+    RobustScaler::RobustScaler(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RobustScaler::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
