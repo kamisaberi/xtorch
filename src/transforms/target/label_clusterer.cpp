a551f5c7a12a3410c552db2cd6632fbeb1fb2467
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:31:48 +0330

    MOdule class forward method changed

--- src/transforms/target/label_clusterer.cpp
+++ src/transforms/target/label_clusterer.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/label_clusterer.h"
 
+namespace xt::transforms::target
+{
+    LabelClusterer::LabelClusterer() = default;
+
+    LabelClusterer::LabelClusterer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto LabelClusterer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
