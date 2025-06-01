a18f2aca947129e3e7c6e1e40c1ab533b7152d5c
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:50:14 +0330

    MOdule class forward method changed

--- src/transforms/text/str_to_int_transform.cpp
+++ src/transforms/text/str_to_int_transform.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/text/str_to_int_transform.h"
+
+namespace xt::transforms::text
+{
+    StrToIntTransform::StrToIntTransform() = default;
+
+    StrToIntTransform::StrToIntTransform(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto StrToIntTransform::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
