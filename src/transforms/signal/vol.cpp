6bbd72f159d82f7029c933d1c24ce72b2e8e5d4e
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:28:36 +0330

    MOdule class forward method changed

--- src/transforms/signal/vol.cpp
+++ src/transforms/signal/vol.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/vol.h"
 
+namespace xt::transforms::signal
+{
+    Vol::Vol() = default;
+
+    Vol::Vol(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Vol::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
