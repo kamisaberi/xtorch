200e473668e568013af5105b149b316d3ed609ab
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:40:34 +0330

    optimazers cpp files created

--- src/transforms/image/latent_projection.cpp
+++ src/transforms/image/latent_projection.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/latent_projection.h"
 
+namespace xt::transforms::image
+{
+    LatentProjection::LatentProjection() = default;
+
+    LatentProjection::LatentProjection(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto LatentProjection::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
