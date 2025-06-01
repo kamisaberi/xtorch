8e269c0409b92cfbb3e3361b4f6d21c55c95b867
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:48:30 +0330

    MOdule class forward method changed

--- src/transforms/text/back_translation.cpp
+++ src/transforms/text/back_translation.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/text/back_translation.h"
+
+namespace xt::transforms::text
+{
+    BackTranslation::BackTranslation() = default;
+
+    BackTranslation::BackTranslation(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto BackTranslation::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
