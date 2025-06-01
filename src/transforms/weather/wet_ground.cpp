95d3a2e8d554dada9b9d46c3238cac9ebdd517f2
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:30:59 +0330

    MOdule class forward method changed

--- src/transforms/weather/wet_ground.cpp
+++ src/transforms/weather/wet_ground.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/wet_ground.h"
+
+namespace xt::transforms::weather
+{
+    WetGround::WetGround() = default;
+
+    WetGround::WetGround(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto WetGround::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
