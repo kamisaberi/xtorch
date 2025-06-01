9278e12f8491fd7e4f35757a5e919aad3b51a21e
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:26:01 +0330

    MOdule class forward method changed

--- src/transforms/signal/sliding_window_cmn.cpp
+++ src/transforms/signal/sliding_window_cmn.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/sliding_window_cmn.h"
 
+namespace xt::transforms::signal
+{
+    SlidingWindowCMN::SlidingWindowCMN() = default;
+
+    SlidingWindowCMN::SlidingWindowCMN(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto SlidingWindowCMN::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
