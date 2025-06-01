bde1d06b42df6dde593a56990ee483bb86d429e9
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:05:46 +0330

    optimazers cpp files created

--- src/transforms/image/random_grid_dropout.cpp
+++ src/transforms/image/random_grid_dropout.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/random_grid_dropout.h"
+
+namespace xt::transforms::image
+{
+    RandomGridDropout::RandomGridDropout() = default;
+
+    RandomGridDropout::RandomGridDropout(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomGridDropout::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
