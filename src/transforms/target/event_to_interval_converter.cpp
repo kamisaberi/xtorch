91cd9c3eeae230668c0620b7044cceb0cf6e55cf
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:28:51 +0330

    MOdule class forward method changed

--- src/transforms/target/event_to_interval_converter.cpp
+++ src/transforms/target/event_to_interval_converter.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/event_to_interval_converter.h"
 
+namespace xt::transforms::target
+{
+    EventToIntervalConverter::EventToIntervalConverter() = default;
+
+    EventToIntervalConverter::EventToIntervalConverter(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto EventToIntervalConverter::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
