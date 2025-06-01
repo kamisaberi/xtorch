b5d4b9bd3c8d245e41376d4364dee32d8aa93ccb
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:26:30 +0330

    MOdule class forward method changed

--- src/transforms/weather/depth_based_fog.cpp
+++ src/transforms/weather/depth_based_fog.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/depth_based_fog.h"
+
+namespace xt::transforms::weather
+{
+    DepthBasedFog::DepthBasedFog() = default;
+
+    DepthBasedFog::DepthBasedFog(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto DepthBasedFog::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
