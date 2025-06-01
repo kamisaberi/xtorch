e59b074a0dd90b980c6a69beebfb577583cad78e
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:24:02 +0330

    MOdule class forward method changed

--- src/transforms/target/binarizer.cpp
+++ src/transforms/target/binarizer.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/binarizer.h"
 
+namespace xt::transforms::target
+{
+    Binarizer::Binarizer() = default;
+
+    Binarizer::Binarizer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Binarizer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
