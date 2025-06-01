fbec77b7a9ecd70ca47c1d84e0176f19c6d01ede
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:34:57 +0330

    MOdule class forward method changed

--- src/transforms/target/leave_one_out_encoder.cpp
+++ src/transforms/target/leave_one_out_encoder.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/leave_one_out_encoder.h"
 
+namespace xt::transforms::target
+{
+    LeaveOneOutEncoder::LeaveOneOutEncoder() = default;
+
+    LeaveOneOutEncoder::LeaveOneOutEncoder(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto LeaveOneOutEncoder::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
