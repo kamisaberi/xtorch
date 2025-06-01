a63b916d6f0055bb0c6a66ab2173790ce1444ed1
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:54:52 +0330

    optimazers cpp files created

--- src/transforms/image/zoom_blur.cpp
+++ src/transforms/image/zoom_blur.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/zoom_blur.h"
+
+namespace xt::transforms::image
+{
+    ZoomBlur::ZoomBlur() = default;
+
+    ZoomBlur::ZoomBlur(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto ZoomBlur::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
