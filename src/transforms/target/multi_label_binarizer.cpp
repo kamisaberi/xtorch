a01c6f78ec81bdb67f366c342682db996a2efdf2
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:36:42 +0330

    MOdule class forward method changed

--- src/transforms/target/multi_label_binarizer.cpp
+++ src/transforms/target/multi_label_binarizer.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/multi_label_binarizer.h"
 
+namespace xt::transforms::target
+{
+    MultiLabelBinarizer::MultiLabelBinarizer() = default;
+
+    MultiLabelBinarizer::MultiLabelBinarizer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto MultiLabelBinarizer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
