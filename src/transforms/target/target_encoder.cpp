de4b27f27a028cc7918fe76db99e94f980ae638d
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:39:42 +0330

    MOdule class forward method changed

--- src/transforms/target/target_encoder.cpp
+++ src/transforms/target/target_encoder.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/target_encoder.h"
 
+namespace xt::transforms::target
+{
+    TargetEncoder::TargetEncoder() = default;
+
+    TargetEncoder::TargetEncoder(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto TargetEncoder::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
