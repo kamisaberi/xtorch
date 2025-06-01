a80622eac26272516211afa6ef5cdd7cb80695f9
kamisaberi <kamisaberi@gmail.com>
2025-06-01 04:18:26 +0330

    MOdule class forward method changed

--- src/transforms/image/fancy_pca.cpp
+++ src/transforms/image/fancy_pca.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/10/25.
-//
+#include "include/transforms/image/fancy_pca.h"
+
+namespace xt::transforms::image
+{
+    FancyPCA::FancyPCA() = default;
+
+    FancyPCA::FancyPCA(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto FancyPCA::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
