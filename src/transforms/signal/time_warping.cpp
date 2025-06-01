25e00d01349a9c1b9acf6e7996027105658f7a0f
kamisaberi <kamisaberi@gmail.com>
2025-06-01 23:28:09 +0330

    MOdule class forward method changed

--- src/transforms/signal/time_warping.cpp
+++ src/transforms/signal/time_warping.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/signal/time_warping.h"
 
+namespace xt::transforms::signal
+{
+    TimeWarping::TimeWarping() = default;
+
+    TimeWarping::TimeWarping(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto TimeWarping::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
