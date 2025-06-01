b416415d6349c95104bf4c96169a4eeb444f507b
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:49:07 +0330

    optimazers cpp files created

--- src/transforms/image/random_affine.cpp
+++ src/transforms/image/random_affine.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/4/25.
-//
+#include "include/transforms/image/random_affine.h"
+
+namespace xt::transforms::image
+{
+    RandomAffine::RandomAffine() = default;
+
+    RandomAffine::RandomAffine(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomAffine::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
