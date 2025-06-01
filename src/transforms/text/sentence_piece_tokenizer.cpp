39dbc1bbe922653aa94413da894c437156fe9da1
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:49:46 +0330

    MOdule class forward method changed

--- src/transforms/text/sentence_piece_tokenizer.cpp
+++ src/transforms/text/sentence_piece_tokenizer.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/6/25.
-//
+#include "include/transforms/text/sentence_piece_tokenizer.h"
+
+namespace xt::transforms::text
+{
+    SentencePieceTokenizer::SentencePieceTokenizer() = default;
+
+    SentencePieceTokenizer::SentencePieceTokenizer(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto SentencePieceTokenizer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
