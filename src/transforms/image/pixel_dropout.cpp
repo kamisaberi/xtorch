6ddbe2a946138ab10f53c31005e90f87779f6379
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:47:28 +0330

    optimazers cpp files created

--- src/transforms/image/pixel_dropout.cpp
+++ src/transforms/image/pixel_dropout.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/pixel_dropout.h"
+
+namespace xt::transforms::image
+{
+    PixelDropout::PixelDropout() = default;
+
+    PixelDropout::PixelDropout(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto PixelDropout::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
