2270c71c5a29d2b3e9e3212ddb41df18ca31756d
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:38:28 +0330

    optimazers cpp files created

--- src/transforms/image/grid_shuffle.cpp
+++ src/transforms/image/grid_shuffle.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/grid_shuffle.h"
+
+namespace xt::transforms::image
+{
+    GridShuffle::GridShuffle() = default;
+
+    GridShuffle::GridShuffle(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto GridShuffle::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
