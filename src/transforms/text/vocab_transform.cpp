327670ca07c98e42d56b07e2dbd98e7512259872
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:52:10 +0330

    MOdule class forward method changed

--- src/transforms/text/vocab_transform.cpp
+++ src/transforms/text/vocab_transform.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/5/2025.
-//
+#include "include/transforms/text/vocab_transform.h"
 
+namespace xt::transforms::text
+{
+    VocabTransform::VocabTransform() = default;
+
+    VocabTransform::VocabTransform(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto VocabTransform::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
