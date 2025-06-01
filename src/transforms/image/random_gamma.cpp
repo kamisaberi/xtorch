d0662f13daf53cc0cba5da336f8f00818553bb7f
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:55:14 +0330

    optimazers cpp files created

--- src/transforms/image/random_gamma.cpp
+++ src/transforms/image/random_gamma.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/random_gamma.h"
+
+namespace xt::transforms::image
+{
+    RandomGamma::RandomGamma() = default;
+
+    RandomGamma::RandomGamma(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomGamma::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
