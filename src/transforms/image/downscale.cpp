11fcf1cef8300ff35ffa337eed1686d7c3c0fa21
kamisaberi <kamisaberi@gmail.com>
2025-06-01 03:58:46 +0330

    MOdule class forward method changed

--- src/transforms/image/downscale.cpp
+++ src/transforms/image/downscale.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/downscale.h"
+
+namespace xt::transforms::image
+{
+    Downscale::Downscale() = default;
+
+    Downscale::Downscale(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Downscale::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
