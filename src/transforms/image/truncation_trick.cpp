0eefde241981c93f2b5292f3b435d4058252bf0c
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:53:43 +0330

    optimazers cpp files created

--- src/transforms/image/truncation_trick.cpp
+++ src/transforms/image/truncation_trick.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/4/2025.
-//
+#include "include/transforms/image/truncation_trick.h"
 
+namespace xt::transforms::image
+{
+    TruncationTrick::TruncationTrick() = default;
+
+    TruncationTrick::TruncationTrick(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto TruncationTrick::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
