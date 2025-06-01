6789c6ef6f1bcf5b7d36bbcd252c4c7069b702f5
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:54:17 +0330

    optimazers cpp files created

--- src/transforms/image/upscale.cpp
+++ src/transforms/image/upscale.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/upscale.h"
+
+namespace xt::transforms::image
+{
+    Upscale::Upscale() = default;
+
+    Upscale::Upscale(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto Upscale::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
