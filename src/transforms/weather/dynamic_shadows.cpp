f1214d8c7a029c5a83c0428d0159c9968cc595ad
kamisaberi <kamisaberi@gmail.com>
2025-06-02 01:27:17 +0330

    MOdule class forward method changed

--- src/transforms/weather/dynamic_shadows.cpp
+++ src/transforms/weather/dynamic_shadows.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/5/25.
-//
+#include "include/transforms/weather/dynamic_shadows.h"
+
+namespace xt::transforms::weather
+{
+    DynamicShadows::DynamicShadows() = default;
+
+    DynamicShadows::DynamicShadows(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto DynamicShadows::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
