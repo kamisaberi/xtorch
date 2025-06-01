ff256f8ecc8d42953807ca11d06e8b7e3db522b9
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:34:00 +0330

    MOdule class forward method changed

--- src/transforms/target/label_remapper.cpp
+++ src/transforms/target/label_remapper.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/label_remapper.h"
 
+namespace xt::transforms::target
+{
+    LabelRemapper::LabelRemapper() = default;
+
+    LabelRemapper::LabelRemapper(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto LabelRemapper::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
