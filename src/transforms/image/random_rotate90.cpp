b3ba63d433a57df14b168dcc9352b750a07be66c
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 14:11:50 +0330

    optimazers cpp files created

--- src/transforms/image/random_rotate90.cpp
+++ src/transforms/image/random_rotate90.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/random_rotate90.h"
+
+namespace xt::transforms::image
+{
+    RandomRotate90::RandomRotate90() = default;
+
+    RandomRotate90::RandomRotate90(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomRotate90::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
