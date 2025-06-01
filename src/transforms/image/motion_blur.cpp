360a5ee8c19e3c86b98044d3625415239dfefb9a
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:43:30 +0330

    optimazers cpp files created

--- src/transforms/image/motion_blur.cpp
+++ src/transforms/image/motion_blur.cpp
@@ -1,3 +1,15 @@
-//
-// Created by kami on 5/11/25.
-//
+#include "include/transforms/image/motion_blur.h"
+
+namespace xt::transforms::image
+{
+    MotionBlur::MotionBlur() = default;
+
+    MotionBlur::MotionBlur(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto MotionBlur::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
