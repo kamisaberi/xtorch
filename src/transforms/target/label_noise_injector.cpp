73cf54f34e558e63b439cc99e0425da72f0cd7ed
kamisaberi <kamisaberi@gmail.com>
2025-06-02 00:32:41 +0330

    MOdule class forward method changed

--- src/transforms/target/label_noise_injector.cpp
+++ src/transforms/target/label_noise_injector.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/target/label_noise_injector.h"
 
+namespace xt::transforms::target
+{
+    LabelNoiseInjector::LabelNoiseInjector() = default;
+
+    LabelNoiseInjector::LabelNoiseInjector(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto LabelNoiseInjector::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
