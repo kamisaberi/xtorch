7256046c4cc0878bf424fdb98ab5d8a67bd3c7f9
kamran saberifard <kamisaberi@gmail.com>
2025-06-01 13:49:45 +0330

    optimazers cpp files created

--- src/transforms/image/random_augment.cpp
+++ src/transforms/image/random_augment.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 5/10/2025.
-//
+#include "include/transforms/image/random_augment.h"
 
+namespace xt::transforms::image
+{
+    RandomAugment::RandomAugment() = default;
+
+    RandomAugment::RandomAugment(std::vector<xt::Module> transforms) : xt::Module()
+    {
+    }
+
+    auto RandomAugment::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        return torch::zeros(10);
+    }
+}
