87b424c022bf92d04e6c36484d6831f61b753828
kamisaberi <kamisaberi@gmail.com>
2025-06-02 06:00:45 +0330

    new DataLoader with buffer system added

--- src/transforms/graph/feature_dropout.cpp
+++ src/transforms/graph/feature_dropout.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::graph {
 
     FeatureDropout::FeatureDropout() = default;
 
-    FeatureDropout::FeatureDropout(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    FeatureDropout::FeatureDropout(std::vector<xt::Module> transforms) : xt::Module(), transform(transform) {
     }
 
     auto FeatureDropout::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
