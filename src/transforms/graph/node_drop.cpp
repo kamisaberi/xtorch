d0253a8d9ff4d5a919e258a54553e30fe5a2456e
kamisaberi <kamisaberi@gmail.com>
2025-06-02 06:01:14 +0330

    new DataLoader with buffer system added

--- src/transforms/graph/node_drop.cpp
+++ src/transforms/graph/node_drop.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::graph {
 
     NodeDrop::NodeDrop() = default;
 
-    NodeDrop::NodeDrop(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    NodeDrop::NodeDrop(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto NodeDrop::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
