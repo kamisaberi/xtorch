7f43d552f3dea9e7a94f38da33eec15911bcfc14
kamisaberi <kamisaberi@gmail.com>
2025-06-02 05:17:42 +0330

    new DataLoader with buffer system added

--- src/transforms/general/normalize.cpp
+++ src/transforms/general/normalize.cpp
@@ -1,29 +1,39 @@
 #include "include/transforms/general/normalize.h"
 
-namespace xt::transforms::general {
-
+namespace xt::transforms::general
+{
     Normalize::Normalize(std::vector<float> mean_, std::vector<float> std_)
-            : mean(std::move(mean_)), std(std::move(std_)) {
-        if (mean.size() != std.size()) {
+        : mean(std::move(mean_)), std(std::move(std_))
+    {
+        if (mean.size() != std.size())
+        {
             throw std::invalid_argument("Mean and std must have the same number of channels");
         }
     }
 
     auto Normalize::forward(std::initializer_list<std::any> tensors) -> std::any
     {
-        // std::vector<torch::Tensor> tensor_vec(tensors);
-        // torch::Tensor input = tensor_vec[0];
-        // if (tensor.dim() != 3 || tensor.size(0) != static_cast<long>(mean.size())) {
-        //     throw std::invalid_argument("Input tensor must be CHW with matching number of channels");
-        // }
-        //
-        // torch::Tensor out = tensor.clone();
-        // for (size_t c = 0; c < mean.size(); ++c) {
-        //     out[c] = (out[c] - mean[c]) / std[c];
-        // }
-        //
-        // return out;
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor input = tensor_vec[0];
+
+        if (input.dim() != 3 || input.size(0) != static_cast<long>(mean.size()))
+        {
+            throw std::invalid_argument("Input tensor must be CHW with matching number of channels");
+        }
+
+        torch::Tensor out = input.clone();
+        for (size_t c = 0; c < mean.size(); ++c)
+        {
+            out[c] = (out[c] - mean[c]) / std[c];
+        }
+
+        return out;
     }
-
-
 }
