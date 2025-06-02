b5a160e634ebd1ac53feb4b515e6e1466861a5f7
kamisaberi <kamisaberi@gmail.com>
2025-06-02 06:01:14 +0330

    new DataLoader with buffer system added

--- src/transforms/graph/graph_mix_up.cpp
+++ src/transforms/graph/graph_mix_up.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::graph {
 
     GraphMixUp::GraphMixUp() = default;
 
-    GraphMixUp::GraphMixUp(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    GraphMixUp::GraphMixUp(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto GraphMixUp::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
