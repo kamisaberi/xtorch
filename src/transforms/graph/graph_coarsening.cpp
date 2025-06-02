fe38b1998d5dc61bc265cc7374a49bc078076d97
kamisaberi <kamisaberi@gmail.com>
2025-06-02 06:00:45 +0330

    new DataLoader with buffer system added

--- src/transforms/graph/graph_coarsening.cpp
+++ src/transforms/graph/graph_coarsening.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::graph {
 
     GraphCoarsening::GraphCoarsening() = default;
 
-    GraphCoarsening::GraphCoarsening(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    GraphCoarsening::GraphCoarsening(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto EdgeDrop::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
