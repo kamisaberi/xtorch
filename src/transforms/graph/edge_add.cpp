90bbbd7bc035a6e0936f6418fd47924d0ee4c8b1
kamisaberi <kamisaberi@gmail.com>
2025-06-02 05:59:33 +0330

    new DataLoader with buffer system added

--- src/transforms/graph/edge_add.cpp
+++ src/transforms/graph/edge_add.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::graph {
 
     EdgeAdd::EdgeAdd() = default;
 
-    EdgeAdd::EdgeAdd(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    EdgeAdd::EdgeAdd(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto EdgeDrop::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
--- src/transforms/graph/edge_drop.cpp
+++ src/transforms/graph/edge_drop.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::graph {
 
     EdgeDrop::EdgeDrop() = default;
 
-    EdgeDrop::EdgeDrop(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    EdgeDrop::EdgeDrop(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto EdgeDrop::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
