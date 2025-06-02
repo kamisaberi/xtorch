edb8be784f6f8bc0aa2f83015b4530ff8643c645
kamisaberi <kamisaberi@gmail.com>
2025-06-02 19:54:20 +0330

    new DataLoader with buffer system added

--- temp/ds/mnist.cpp
+++ temp/ds/mnist.cpp
@@ -7,9 +7,19 @@ int main()
 {
     std::cout.precision(10);
 
-    auto dataset = xt::datasets::MNIST(
-        "/home/kami/Documents/datasets/", xt::datasets::DataMode::TRAIN, true );
+    auto transforms = {
+        xt::transforms::image::Resize({32, 32}),
+        xt::transforms::general::Normalize({0.5}, {0.5})
+    };
+    xt::transforms::Compose compose(transforms);
 
+
+    auto dataset = xt::datasets::MNIST(
+        "/home/kami/Documents/datasets/", xt::datasets::DataMode::TRAIN, false);
+
+
+    auto datum = dataset.get(0);
+    cout << datum.data.sizes() << endl;
     // xt::DataLoader<decltype(dataset)> loader(
     //     std::move(dataset),
     //     torch::data::DataLoaderOptions().batch_size(64).drop_last(false),
