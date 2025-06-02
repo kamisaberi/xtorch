51dac085f1776a917f4082eb02c63885aa365dca
kamisaberi <kamisaberi@gmail.com>
2025-06-02 06:00:14 +0330

    new DataLoader with buffer system added

--- src/transforms/graph/edge_perturbation.cpp
+++ src/transforms/graph/edge_perturbation.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::graph {
 
     EdgePerturbation::EdgePerturbation() = default;
 
-    EdgePerturbation::EdgePerturbation(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    EdgePerturbation::EdgePerturbation(std::vector<xt::Module> transforms) : xt::Module() {
     }
 
     auto EdgePerturbation::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
--- src/transforms/graph/feature_augmentation.cpp
+++ src/transforms/graph/feature_augmentation.cpp
@@ -4,11 +4,11 @@ namespace xt::transforms::graph {
 
     FeatureAugmentation::FeatureAugmentation() = default;
 
-    FeatureAugmentation::FeatureAugmentation(std::function<torch::Tensor(torch::Tensor)> transform) : xt::Module(), transform(transform) {
+    FeatureAugmentation::FeatureAugmentation(std::vector<xt::Module> transforms) : xt::Module(), transform(transform) {
     }
 
     auto FeatureAugmentation::forward(std::initializer_list <std::any> tensors) -> std::any {
-        return torch::zeroes(10);
+        return torch::zeros(10);
     }
 
 
