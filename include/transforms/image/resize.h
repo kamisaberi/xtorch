a90d5e0fb42dc939ad3f55a79f64527d9b39a7eb
kamisaberi <kamisaberi@gmail.com>
2025-06-02 03:38:56 +0330

    MOdule class forward method changed

--- include/transforms/image/resize.h
+++ include/transforms/image/resize.h
@@ -4,32 +4,14 @@
 
 namespace xt::transforms::image
 {
-    /**
-     * @struct Resize
-     * @brief A functor to resize a tensor image to a specified size.
-     *
-     * This struct provides a callable object that resizes a `torch::Tensor` representing an image
-     * to a target size specified as a vector of 64-bit integers. It uses the call operator to
-     * perform the resizing operation, making it suitable for use in functional pipelines or
-     * transformations.
-     */
     struct Resize
     {
     public:
-        /**
-         * @brief Constructs a Resize object with the target size.
-         * @param size A vector of 64-bit integers specifying the target dimensions (e.g., {height, width}).
-         */
         explicit Resize(std::vector<int64_t> size);
 
-        /**
-         * @brief Resizes the input tensor image to the target size.
-         * @param img The input tensor image to be resized.
-         * @return A new tensor with the resized dimensions.
-         */
         torch::Tensor operator()(torch::Tensor img);
 
     private:
-        std::vector<int64_t> size; ///< The target size for resizing (e.g., {height, width}).
+        std::vector<int64_t> size;
     };
 }
