0c3265de4097b5dadcc35abee4df19dabbfb1305
kamisaberi <kamisaberi@gmail.com>
2025-06-04 19:26:12 +0330

    some bugs fixed

--- temp/ddp/nccl.cpp
+++ temp/ddp/nccl.cpp
@@ -1,7 +1,8 @@
 #include <torch/torch.h>
 #include <torch/csrc/distributed/c10d/ProcessGroupNCCL.hpp>
-#include <torch/csrc/distributed/c10d/Store.hpp>
+#include <torch/csrc/distributed/c10d/TCPStore.hpp>
 #include <torch/csrc/distributed/c10d/PrefixStore.hpp>
+#include <cuda_runtime.h>
 #include <mpi.h>
 #include <iostream>
 
@@ -14,11 +15,20 @@ int main(int argc, char* argv[]) {
         MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
         // Set CUDA device
-        torch::Device device(torch::kCUDA, rank);
-        torch::cuda::set_device(rank);
+        cudaError_t err = cudaSetDevice(rank);
+        if (err != cudaSuccess) {
+            std::cerr << "CUDA error setting device " << rank << ": " << cudaGetErrorString(err) << std::endl;
+            return 1;
+        }
+
+        // Create TCPStore options
+        c10d::TCPStoreOptions opts;
+        opts.isServer = (rank == 0);
+        opts.numWorkers = world_size;
+        opts.port = 29500;
 
         // Create store
-        auto store = c10::make_intrusive<c10d::TCPStore>("localhost", 29500, world_size, rank == 0);
+        auto store = c10::make_intrusive<c10d::TCPStore>("localhost", opts);
 
         // Create options
         auto options = c10d::ProcessGroupNCCL::Options::create();
@@ -27,7 +37,7 @@ int main(int argc, char* argv[]) {
         auto pg = c10::make_intrusive<c10d::ProcessGroupNCCL>(store, rank, world_size, options);
 
         // Create tensor
-        torch::Tensor tensor = torch::ones({5}).to(device) * (rank + 1);
+        torch::Tensor tensor = torch::ones({5}, torch::device(torch::kCUDA)) * (rank + 1);
 
         // Perform all-reduce
         std::vector<torch::Tensor> tensors = {tensor};
@@ -37,7 +47,6 @@ int main(int argc, char* argv[]) {
         // Print result
         if (rank == 0) {
             std::cout << "All-reduce result: " << tensors[0] << std::endl;
-            // Expected: [10, 10, 10, 10, 10] for 4 GPUs
         }
 
         // Cleanup
