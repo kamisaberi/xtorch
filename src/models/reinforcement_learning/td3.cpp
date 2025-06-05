3d4eb4312467442eede55007a14c6a736347b881
kamisaberi <kamisaberi@gmail.com>
2025-06-05 20:42:33 +0330

    some bugs fixed

--- src/models/reinforcement_learning/td3.cpp
+++ src/models/reinforcement_learning/td3.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/reinforcement_learning/td3.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    TD3::TD3(int num_classes, int in_channels)
+    {
+    }
+
+    TD3::TD3(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void TD3::reset()
+    {
+    }
+
+    auto TD3::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
