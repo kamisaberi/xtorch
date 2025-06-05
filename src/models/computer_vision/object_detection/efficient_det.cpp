8deaba7ac13a7f80d5bbed56c9575d33dd41f8f9
kamisaberi <kamisaberi@gmail.com>
2025-06-05 15:08:56 +0330

    some bugs fixed

--- src/models/computer_vision/object_detection/efficient_det.cpp
+++ src/models/computer_vision/object_detection/efficient_det.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/computer_vision/object_detection/efficient_det.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    EfficientDet::EfficientDet(int num_classes, int in_channels)
+    {
+    }
+
+    EfficientDet::EfficientDet(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void EfficientDet::reset()
+    {
+    }
+
+    auto EfficientDet::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
