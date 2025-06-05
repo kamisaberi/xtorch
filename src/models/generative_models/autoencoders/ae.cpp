a0245aa92cfd0aab587f20d0113807d49bfe01d5
kamisaberi <kamisaberi@gmail.com>
2025-06-05 15:39:59 +0330

    some bugs fixed

--- src/models/generative_models/autoencoders/ae.cpp
+++ src/models/generative_models/autoencoders/ae.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/generative_models/autoencoders/ae.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    AE::AE(int num_classes, int in_channels)
+    {
+    }
+
+    AE::AE(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void AE::reset()
+    {
+    }
+
+    auto AE::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
