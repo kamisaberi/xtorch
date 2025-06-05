cdb9f1116cc4b0fa20b4b427ed4d8372c0d9792a
kamisaberi <kamisaberi@gmail.com>
2025-06-05 06:46:29 +0330

    some bugs fixed

--- src/models/computer_vision/image_classification/dense_net.cpp
+++ src/models/computer_vision/image_classification/dense_net.cpp
@@ -31,4 +31,102 @@ namespace xt::models
 
         return x;
     }
+
+
+
+
+
+
+    DenseNet169::DenseNet169(int num_classes, int in_channels)
+    {
+    }
+
+    DenseNet169::DenseNet169(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void DenseNet169::reset()
+    {
+    }
+
+    auto DenseNet169::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+
+
+
+    DenseNet201::DenseNet201(int num_classes, int in_channels)
+    {
+    }
+
+    DenseNet201::DenseNet201(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void DenseNet201::reset()
+    {
+    }
+
+    auto DenseNet201::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+
+
+
+    DenseNet264::DenseNet264(int num_classes, int in_channels)
+    {
+    }
+
+    DenseNet264::DenseNet264(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void DenseNet264::reset()
+    {
+    }
+
+    auto DenseNet264::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+
+
+
+
+
+
+
+
 }
