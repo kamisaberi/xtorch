47950b3ccf8168d281bd10f2c3910428a3cc7dfe
kamisaberi <kamisaberi@gmail.com>
2025-06-05 19:51:19 +0330

    some bugs fixed

--- src/models/graph_neural_networks/graph_sage.cpp
+++ src/models/graph_neural_networks/graph_sage.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/graph_neural_networks/graph_sage.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    GraphSAGE::GraphSAGE(int num_classes, int in_channels)
+    {
+    }
+
+    GraphSAGE::GraphSAGE(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void GraphSAGE::reset()
+    {
+    }
+
+    auto GraphSAGE::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
