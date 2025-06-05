ba01a2388fd734efc225fcb613ddfa4d496f552e
kamisaberi <kamisaberi@gmail.com>
2025-06-05 18:03:54 +0330

    some bugs fixed

--- src/models/generative_models/diffusion/ddim.cpp
+++ src/models/generative_models/diffusion/ddim.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/generative_models/diffusion/ddim.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    DDIM::DDIM(int num_classes, int in_channels)
+    {
+    }
+
+    DDIM::DDIM(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void DDIM::reset()
+    {
+    }
+
+    auto DDIM::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
--- src/models/generative_models/diffusion/imagen.cpp
+++ src/models/generative_models/diffusion/imagen.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/generative_models/diffusion/imagen.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    Imagen::Imagen(int num_classes, int in_channels)
+    {
+    }
+
+    Imagen::Imagen(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void Imagen::reset()
+    {
+    }
+
+    auto Imagen::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
