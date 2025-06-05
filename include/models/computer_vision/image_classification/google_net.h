9d4f5945924d7312bb66ff28d522da94b3467692
kamisaberi <kamisaberi@gmail.com>
2025-06-05 07:22:51 +0330

    some bugs fixed

--- include/models/computer_vision/image_classification/google_net.h
+++ include/models/computer_vision/image_classification/google_net.h
@@ -16,7 +16,7 @@ namespace xt::models
 
         GoogLeNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
-        torch::Tensor forward(torch::Tensor x) const override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
         void reset() override;
     };
 
