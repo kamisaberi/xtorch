12e7001cd53b099257613df8dcc3062a6c1d7441
kamisaberi <kamisaberi@gmail.com>
2025-06-05 06:49:32 +0330

    some bugs fixed

--- src/models/computer_vision/image_classification/efficient_net.cpp
+++ src/models/computer_vision/image_classification/efficient_net.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/computer_vision/image_classification/efficient_net.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    CBAM::CBAM(int num_classes, int in_channels)
+    {
+    }
+
+    CBAM::CBAM(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void CBAM::reset()
+    {
+    }
+
+    auto CBAM::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
