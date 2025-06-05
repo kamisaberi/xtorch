3ecf24b4d19cee6af0e3abadb39ed6292f463cae
kamisaberi <kamisaberi@gmail.com>
2025-06-05 18:07:32 +0330

    some bugs fixed

--- src/models/generative_models/gans/pix2pix.cpp
+++ src/models/generative_models/gans/pix2pix.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/generative_models/gans/pix2pix.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    Pix2Pix::Pix2Pix(int num_classes, int in_channels)
+    {
+    }
+
+    Pix2Pix::Pix2Pix(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void Pix2Pix::reset()
+    {
+    }
+
+    auto Pix2Pix::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
