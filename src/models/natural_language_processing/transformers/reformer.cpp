ca84e085440a52b6778df70e8ff834457effad18
kamisaberi <kamisaberi@gmail.com>
2025-06-05 20:29:40 +0330

    some bugs fixed

--- src/models/natural_language_processing/transformers/reformer.cpp
+++ src/models/natural_language_processing/transformers/reformer.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/natural_language_processing/transformers/reformer.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    Reformer::Reformer(int num_classes, int in_channels)
+    {
+    }
+
+    Reformer::Reformer(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void Reformer::reset()
+    {
+    }
+
+    auto Reformer::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
