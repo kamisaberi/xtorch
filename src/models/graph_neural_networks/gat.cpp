d98233892d512d75d50e1b4ef4ae8c6664eac000
kamisaberi <kamisaberi@gmail.com>
2025-06-05 19:50:36 +0330

    some bugs fixed

--- src/models/graph_neural_networks/gat.cpp
+++ src/models/graph_neural_networks/gat.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/graph_neural_networks/gat.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    GAT::GAT(int num_classes, int in_channels)
+    {
+    }
+
+    GAT::GAT(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void GAT::reset()
+    {
+    }
+
+    auto GAT::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
