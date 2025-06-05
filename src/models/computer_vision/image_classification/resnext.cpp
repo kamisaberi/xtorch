7f1cd4ec27b55e647943e5846b34f157d35cf0c3
kamisaberi <kamisaberi@gmail.com>
2025-06-05 08:14:02 +0330

    some bugs fixed

--- src/models/computer_vision/image_classification/resnext.cpp
+++ src/models/computer_vision/image_classification/resnext.cpp
@@ -1,4 +1,34 @@
 #include "include/models/computer_vision/image_classification/resnext.h"
 
-namespace xt::models {
+
+using namespace std;
+
+namespace xt::models
+{
+    ResNeXt::ResNeXt(int num_classes, int in_channels)
+    {
+    }
+
+    ResNeXt::ResNeXt(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void ResNeXt::reset()
+    {
+    }
+
+    auto ResNeXt::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
 }
