9858ac3cc7d048aa2644691de7a56cb1cebb3915
kamisaberi <kamisaberi@gmail.com>
2025-06-05 07:50:40 +0330

    some bugs fixed

--- include/models/computer_vision/image_classification/network_in_network.h
+++ include/models/computer_vision/image_classification/network_in_network.h
@@ -12,7 +12,8 @@ namespace xt::models
 
         NetworkInNetwork(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
-        torch::Tensor forward(torch::Tensor x) const override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
         void reset() override;
     };
 
