473ceda397ce505a71ef40d1f441ba5a905a1a65
kamisaberi <kamisaberi@gmail.com>
2025-06-05 20:28:59 +0330

    some bugs fixed

--- src/models/natural_language_processing/transformers/llama.cpp
+++ src/models/natural_language_processing/transformers/llama.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/natural_language_processing/transformers/llama.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    LLAMA::LLAMA(int num_classes, int in_channels)
+    {
+    }
+
+    LLAMA::LLAMA(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void LLAMA::reset()
+    {
+    }
+
+    auto LLAMA::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
