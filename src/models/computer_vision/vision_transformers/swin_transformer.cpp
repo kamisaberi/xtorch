892a8d928459175cff711e2e61f8e94d1f73e99b
kamisaberi <kamisaberi@gmail.com>
2025-06-05 15:29:38 +0330

    some bugs fixed

--- src/models/computer_vision/vision_transformers/swin_transformer.cpp
+++ src/models/computer_vision/vision_transformers/swin_transformer.cpp
@@ -1,3 +1,62 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/computer_vision/vision_transformers/swin_transformer.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    SwinTransformerV1::SwinTransformerV1(int num_classes, int in_channels)
+    {
+    }
+
+    SwinTransformerV1::SwinTransformerV1(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void SwinTransformerV1::reset()
+    {
+    }
+
+    auto SwinTransformerV1::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+
+    SwinTransformerV2::SwinTransformerV2(int num_classes, int in_channels)
+    {
+    }
+
+    SwinTransformerV2::SwinTransformerV2(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void SwinTransformerV2::reset()
+    {
+    }
+
+    auto SwinTransformerV2::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+
+}
