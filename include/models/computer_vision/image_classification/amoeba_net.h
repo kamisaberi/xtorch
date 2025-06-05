af2bd0d5aa4589e4d267fde271f67a2ee191de04
kamisaberi <kamisaberi@gmail.com>
2025-06-05 06:42:36 +0330

    some bugs fixed

--- include/models/computer_vision/image_classification/amoeba_net.h
+++ include/models/computer_vision/image_classification/amoeba_net.h
@@ -15,7 +15,8 @@ namespace xt::models
         explicit AmoabaNet(int num_classes/* classes */, int in_channels = 1/*  input channels */);
         AmoabaNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
-        torch::Tensor forward(torch::Tensor x) const override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
         void reset() override;
     };
 }
