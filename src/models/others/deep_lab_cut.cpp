3542c18a9026b7ee09a50166a6db5a339dba1097
kamisaberi <kamisaberi@gmail.com>
2025-06-05 20:31:25 +0330

    some bugs fixed

--- src/models/others/deep_lab_cut.cpp
+++ src/models/others/deep_lab_cut.cpp
@@ -1,3 +1,34 @@
-//
-// Created by kami on 5/9/25.
-//
+#include "include/models/others/deep_lab_cut.h"
+
+
+using namespace std;
+
+namespace xt::models
+{
+    DeepLabCut::DeepLabCut(int num_classes, int in_channels)
+    {
+    }
+
+    DeepLabCut::DeepLabCut(int num_classes, int in_channels, std::vector<int64_t> input_shape)
+    {
+    }
+
+    void DeepLabCut::reset()
+    {
+    }
+
+    auto DeepLabCut::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
+        std::vector<std::any> any_vec(tensors);
+
+        std::vector<torch::Tensor> tensor_vec;
+        for (const auto& item : any_vec)
+        {
+            tensor_vec.push_back(std::any_cast<torch::Tensor>(item));
+        }
+
+        torch::Tensor x = tensor_vec[0];
+
+        return x;
+    }
+}
