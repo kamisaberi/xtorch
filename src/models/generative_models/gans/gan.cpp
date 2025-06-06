68a0de845949a9f2034d3cce5635a576ad17d4cc
kamisaberi <kamisaberi@gmail.com>
2025-06-07 01:25:41 +0330

    some bugs fixed

--- src/models/generative_models/gans/gan.cpp
+++ src/models/generative_models/gans/gan.cpp
@@ -34,4 +34,52 @@ namespace xt::models
 
 
 
+    Generator(int nz, int nhidden, int nout)
+    : fc1(torch::nn::Linear(nz, nhidden)),
+      fc2(torch::nn::Linear(nhidden, nhidden)),
+      fc3(torch::nn::Linear(nhidden, nout)) {
+        register_module("fc1", fc1);
+        register_module("fc2", fc2);
+        register_module("fc3", fc3);
+
+        // Initialize weights
+        apply([](torch::nn::Module& module) {
+            if (auto* linear = dynamic_cast<torch::nn::LinearImpl*>(&module)) {
+                torch::nn::init::normal_(linear->weight, 0.0, 0.02);
+                torch::nn::init::constant_(linear->bias, 0.0);
+            }
+        });
+    }
+
+    torch::Tensor forward(torch::Tensor x) {
+        x = torch::relu(fc1(x));
+        x = torch::relu(fc2(x));
+        x = torch::tanh(fc3(x));
+        return x;
+    }
+    Discriminator(int nin, int nhidden)
+        : fc1(torch::nn::Linear(nin, nhidden)),
+          fc2(torch::nn::Linear(nhidden, nhidden)),
+          fc3(torch::nn::Linear(nhidden, 1)) {
+        register_module("fc1", fc1);
+        register_module("fc2", fc2);
+        register_module("fc3", fc3);
+
+        // Initialize weights
+        apply([](torch::nn::Module& module) {
+            if (auto* linear = dynamic_cast<torch::nn::LinearImpl*>(&module)) {
+                torch::nn::init::normal_(linear->weight, 0.0, 0.02);
+                torch::nn::init::constant_(linear->bias, 0.0);
+            }
+        });
+    }
+
+    torch::Tensor forward(torch::Tensor x) {
+        x = torch::leaky_relu(fc1(x), 0.2);
+        x = torch::leaky_relu(fc2(x), 0.2);
+        x = torch::sigmoid(fc3(x));
+        return x;
+    }
+
+
 }
