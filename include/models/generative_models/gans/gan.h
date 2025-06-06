977c7e67bf6ddb025574013f2306261de93a6252
kamisaberi <kamisaberi@gmail.com>
2025-06-07 01:26:30 +0330

    some bugs fixed

--- include/models/generative_models/gans/gan.h
+++ include/models/generative_models/gans/gan.h
@@ -14,69 +14,30 @@ namespace xt::models
 
         GAN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
         void reset() override;
 
-        struct Generator : torch::nn::Module {
-            Generator(int nz, int nhidden, int nout)
-                : fc1(torch::nn::Linear(nz, nhidden)),
-                  fc2(torch::nn::Linear(nhidden, nhidden)),
-                  fc3(torch::nn::Linear(nhidden, nout)) {
-                register_module("fc1", fc1);
-                register_module("fc2", fc2);
-                register_module("fc3", fc3);
+        struct Generator : torch::nn::Module
+        {
+        public:
+            Generator(int nz, int nhidden, int nout);
 
-                // Initialize weights
-                apply([](torch::nn::Module& module) {
-                    if (auto* linear = dynamic_cast<torch::nn::LinearImpl*>(&module)) {
-                        torch::nn::init::normal_(linear->weight, 0.0, 0.02);
-                        torch::nn::init::constant_(linear->bias, 0.0);
-                    }
-                });
-            }
+            torch::Tensor forward(torch::Tensor x);
 
-            torch::Tensor forward(torch::Tensor x) {
-                x = torch::relu(fc1(x));
-                x = torch::relu(fc2(x));
-                x = torch::tanh(fc3(x));
-                return x;
-            }
-
+        private:
             torch::nn::Linear fc1, fc2, fc3;
         };
 
         // Discriminator Network
-        struct Discriminator : torch::nn::Module {
-            Discriminator(int nin, int nhidden)
-                : fc1(torch::nn::Linear(nin, nhidden)),
-                  fc2(torch::nn::Linear(nhidden, nhidden)),
-                  fc3(torch::nn::Linear(nhidden, 1)) {
-                register_module("fc1", fc1);
-                register_module("fc2", fc2);
-                register_module("fc3", fc3);
+        struct Discriminator : torch::nn::Module
+        {
+        public:
+            Discriminator(int nin, int nhidden);
 
-                // Initialize weights
-                apply([](torch::nn::Module& module) {
-                    if (auto* linear = dynamic_cast<torch::nn::LinearImpl*>(&module)) {
-                        torch::nn::init::normal_(linear->weight, 0.0, 0.02);
-                        torch::nn::init::constant_(linear->bias, 0.0);
-                    }
-                });
-            }
+            torch::Tensor forward(torch::Tensor x);
 
-            torch::Tensor forward(torch::Tensor x) {
-                x = torch::leaky_relu(fc1(x), 0.2);
-                x = torch::leaky_relu(fc2(x), 0.2);
-                x = torch::sigmoid(fc3(x));
-                return x;
-            }
-
+        private:
             torch::nn::Linear fc1, fc2, fc3;
         };
-
-
-
-
     };
-
 }
