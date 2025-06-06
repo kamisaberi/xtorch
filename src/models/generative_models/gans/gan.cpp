ad9056d799d440144896a4a25cd372646b7cfda9
kamisaberi <kamisaberi@gmail.com>
2025-06-07 01:27:12 +0330

    some bugs fixed

--- src/models/generative_models/gans/gan.cpp
+++ src/models/generative_models/gans/gan.cpp
@@ -33,53 +33,59 @@ namespace xt::models
     }
 
 
-
-    Generator(int nz, int nhidden, int nout)
-    : fc1(torch::nn::Linear(nz, nhidden)),
-      fc2(torch::nn::Linear(nhidden, nhidden)),
-      fc3(torch::nn::Linear(nhidden, nout)) {
+    GAN::Generator::Generator(int nz, int nhidden, int nout)
+        : fc1(torch::nn::Linear(nz, nhidden)),
+          fc2(torch::nn::Linear(nhidden, nhidden)),
+          fc3(torch::nn::Linear(nhidden, nout))
+    {
         register_module("fc1", fc1);
         register_module("fc2", fc2);
         register_module("fc3", fc3);
 
         // Initialize weights
-        apply([](torch::nn::Module& module) {
-            if (auto* linear = dynamic_cast<torch::nn::LinearImpl*>(&module)) {
+        apply([](torch::nn::Module& module)
+        {
+            if (auto* linear = dynamic_cast<torch::nn::LinearImpl*>(&module))
+            {
                 torch::nn::init::normal_(linear->weight, 0.0, 0.02);
                 torch::nn::init::constant_(linear->bias, 0.0);
             }
         });
     }
 
-    torch::Tensor forward(torch::Tensor x) {
+    torch::Tensor GAN::Generator::forward(torch::Tensor x)
+    {
         x = torch::relu(fc1(x));
         x = torch::relu(fc2(x));
         x = torch::tanh(fc3(x));
         return x;
     }
-    Discriminator(int nin, int nhidden)
+
+    GAN::Discriminator::Discriminator(int nin, int nhidden)
         : fc1(torch::nn::Linear(nin, nhidden)),
           fc2(torch::nn::Linear(nhidden, nhidden)),
-          fc3(torch::nn::Linear(nhidden, 1)) {
+          fc3(torch::nn::Linear(nhidden, 1))
+    {
         register_module("fc1", fc1);
         register_module("fc2", fc2);
         register_module("fc3", fc3);
 
         // Initialize weights
-        apply([](torch::nn::Module& module) {
-            if (auto* linear = dynamic_cast<torch::nn::LinearImpl*>(&module)) {
+        apply([](torch::nn::Module& module)
+        {
+            if (auto* linear = dynamic_cast<torch::nn::LinearImpl*>(&module))
+            {
                 torch::nn::init::normal_(linear->weight, 0.0, 0.02);
                 torch::nn::init::constant_(linear->bias, 0.0);
             }
         });
     }
 
-    torch::Tensor forward(torch::Tensor x) {
+    torch::Tensor GAN::Discriminator::forward(torch::Tensor x)
+    {
         x = torch::leaky_relu(fc1(x), 0.2);
         x = torch::leaky_relu(fc2(x), 0.2);
         x = torch::sigmoid(fc3(x));
         return x;
     }
-
-
 }
