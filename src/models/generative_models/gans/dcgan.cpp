788a01b2af5520fa94324715b5d481aa0cb7f28e
kamisaberi <kamisaberi@gmail.com>
2025-06-06 23:31:03 +0330

    some bugs fixed

--- src/models/generative_models/gans/dcgan.cpp
+++ src/models/generative_models/gans/dcgan.cpp
@@ -79,52 +79,47 @@ namespace xt::models
         return x;
     }
 
-                DCGAN::Discriminator::Discriminator(int nc, int ndf)
-                : conv1(torch::nn::Conv2dOptions(nc, ndf, 4).stride(2).padding(1).bias(false)),
-                  conv2(torch::nn::Conv2dOptions(ndf, ndf * 2, 4).stride(2).padding(1).bias(false)),
-                  conv3(torch::nn::Conv2dOptions(ndf * 2, ndf * 4, 4).stride(2).padding(1).bias(false)),
-                  conv4(torch::nn::Conv2dOptions(ndf * 4, ndf * 8, 4).stride(2).padding(1).bias(false)),
-                  conv5(torch::nn::Conv2dOptions(ndf * 8, 1, 4).stride(1).padding(0).bias(false)),
-                  bn1(torch::nn::BatchNorm2d(ndf * 2)),
-                  bn2(torch::nn::BatchNorm2d(ndf * 4)),
-                  bn3(torch::nn::BatchNorm2d(ndf * 8))
+    DCGAN::Discriminator::Discriminator(int nc, int ndf)
+        : conv1(torch::nn::Conv2dOptions(nc, ndf, 4).stride(2).padding(1).bias(false)),
+          conv2(torch::nn::Conv2dOptions(ndf, ndf * 2, 4).stride(2).padding(1).bias(false)),
+          conv3(torch::nn::Conv2dOptions(ndf * 2, ndf * 4, 4).stride(2).padding(1).bias(false)),
+          conv4(torch::nn::Conv2dOptions(ndf * 4, ndf * 8, 4).stride(2).padding(1).bias(false)),
+          conv5(torch::nn::Conv2dOptions(ndf * 8, 1, 4).stride(1).padding(0).bias(false)),
+          bn1(torch::nn::BatchNorm2d(ndf * 2)),
+          bn2(torch::nn::BatchNorm2d(ndf * 4)),
+          bn3(torch::nn::BatchNorm2d(ndf * 8))
+    {
+        register_module("conv1", conv1);
+        register_module("conv2", conv2);
+        register_module("conv3", conv3);
+        register_module("conv4", conv4);
+        register_module("conv5", conv5);
+        register_module("bn1", bn1);
+        register_module("bn2", bn2);
+        register_module("bn3", bn3);
+
+        // Initialize weights
+        apply([](torch::nn::Module& module)
+        {
+            if (auto* conv = dynamic_cast<torch::nn::Conv2dImpl*>(&module))
             {
-                register_module("conv1", conv1);
-                register_module("conv2", conv2);
-                register_module("conv3", conv3);
-                register_module("conv4", conv4);
-                register_module("conv5", conv5);
-                register_module("bn1", bn1);
-                register_module("bn2", bn2);
-                register_module("bn3", bn3);
-
-                // Initialize weights
-                apply([](torch::nn::Module& module)
-                {
-                    if (auto* conv = dynamic_cast<torch::nn::Conv2dImpl*>(&module))
-                    {
-                        torch::nn::init::normal_(conv->weight, 0.0, 0.02);
-                    }
-                    else if (auto* bn = dynamic_cast<torch::nn::BatchNorm2dImpl*>(&module))
-                    {
-                        torch::nn::init::normal_(bn->weight, 1.0, 0.02);
-                        torch::nn::init::constant_(bn->bias, 0.0);
-                    }
-                });
+                torch::nn::init::normal_(conv->weight, 0.0, 0.02);
             }
-
-            torch::Tensor DCGAN::Discriminator::forward(torch::Tensor x)
+            else if (auto* bn = dynamic_cast<torch::nn::BatchNorm2dImpl*>(&module))
             {
-                x = torch::leaky_relu(conv1(x), 0.2);
-                x = torch::leaky_relu(bn1(conv2(x)), 0.2);
-                x = torch::leaky_relu(bn2(conv3(x)), 0.2);
-                x = torch::leaky_relu(bn3(conv4(x)), 0.2);
-                x = torch::sigmoid(conv5(x));
-                return x;
+                torch::nn::init::normal_(bn->weight, 1.0, 0.02);
+                torch::nn::init::constant_(bn->bias, 0.0);
             }
+        });
+    }
 
-
-
-
-
+    torch::Tensor DCGAN::Discriminator::forward(torch::Tensor x)
+    {
+        x = torch::leaky_relu(conv1(x), 0.2);
+        x = torch::leaky_relu(bn1(conv2(x)), 0.2);
+        x = torch::leaky_relu(bn2(conv3(x)), 0.2);
+        x = torch::leaky_relu(bn3(conv4(x)), 0.2);
+        x = torch::sigmoid(conv5(x));
+        return x;
+    }
 }
