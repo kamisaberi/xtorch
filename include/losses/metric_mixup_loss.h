6429653787fce04a30fc27957277eb7c136d5881
kamisaberi <kamisaberi@gmail.com>
2025-06-10 03:07:27 +0330

    generalized_focal_loss loss function created

--- include/losses/metric_mixup_loss.h
+++ include/losses/metric_mixup_loss.h
@@ -4,12 +4,13 @@
 
 namespace xt::losses
 {
-    torch::Tensor metrix(torch::Tensor x);
-    class Metrix : xt::Module
+    torch::Tensor metric_mixup_loss(torch::Tensor x);
+
+    class MetricMixupLoss : xt::Module
     {
     public:
-        Metrix() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        MetricMixupLoss() = default;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
