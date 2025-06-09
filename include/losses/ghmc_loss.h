b90637502483e241f25d83cffd461a83cb0f3bdd
kamisaberi <kamisaberi@gmail.com>
2025-06-10 02:04:48 +0330

    generalized_focal_loss loss function created

--- include/losses/ghmc_loss.h
+++ include/losses/ghmc_loss.h
@@ -4,11 +4,11 @@
 
 namespace xt::losses
 {
-    torch::Tensor ghmc(torch::Tensor x);
-    class GHMC : xt::Module
+    torch::Tensor ghmc_loss(torch::Tensor x);
+    class GHMCLoss : xt::Module
     {
     public:
-        GHMC() = default;
+        GHMCLoss() = default;
         auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
