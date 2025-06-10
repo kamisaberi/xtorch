0652dad1d9e7f87b42ac2ad0f2375606d5c2365f
kamisaberi <kamisaberi@gmail.com>
2025-06-10 14:33:57 +0330

    fem activation function created

--- include/activations/gclu.h
+++ include/activations/gclu.h
@@ -3,7 +3,7 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor gclu(torch::Tensor x);
+    torch::Tensor gclu(torch::Tensor x, int64_t dim = 1);
 
     struct GCLU : xt::Module {
     public:
--- src/activations/gclu.cpp
+++ src/activations/gclu.cpp
@@ -2,9 +2,15 @@
 
 namespace xt::activations
 {
-    torch::Tensor gclu(torch::Tensor x)
+    torch::Tensor gclu(const torch::Tensor x, int64_t dim )
     {
-        return torch::zeros(10);
+        TORCH_CHECK(x.size(dim) % 2 == 0, "Dimension ", dim, " size (", x.size(dim), ") must be even for GCLU split.");
+
+        auto chunks = torch::chunk(x, 2, dim);
+        torch::Tensor x_a = chunks[0];
+        torch::Tensor x_b = chunks[1];
+
+        return x_a * torch::cos(x_b);
     }
 
     auto GCLU::forward(std::initializer_list<std::any> tensors) -> std::any
