fc8f07e8a19abfaf3ebc3d3b87aa76f7ff7adf3a
kamisaberi <kamisaberi@gmail.com>
2025-06-10 22:50:48 +0330

    serlu activation function created

--- include/activations/serlu.h
+++ include/activations/serlu.h
@@ -2,18 +2,16 @@
 
 #include "common.h"
 
-namespace xt::activations {
-    torch::Tensor serlu(torch::Tensor x);
+namespace xt::activations
+{
+    torch::Tensor serlu(const torch::Tensor& x, double lambda_serlu = 1.0507, double alpha_serlu = 1.67326);
 
-    struct SERLU : xt::Module {
+    struct SERLU : xt::Module
+    {
     public:
         SERLU() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
 }
-
-
-
