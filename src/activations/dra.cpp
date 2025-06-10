bbe883552b1ea0b623084cdc3a0b757652b9053d
kamisaberi <kamisaberi@gmail.com>
2025-06-10 14:14:19 +0330

    dra activation function created

--- src/activations/dra.cpp
+++ src/activations/dra.cpp
@@ -2,11 +2,13 @@
 
 namespace xt::activations
 {
-    torch::Tensor dra(torch::Tensor x)
-    {
-        return torch::zeros(10);
+    torch::Tensor dra(const torch::Tensor& x, double alpha ) {
+        torch::Tensor x_cubed = torch::pow(x, 3.0);
+        torch::Tensor arg_sigmoid = alpha * x_cubed;
+        torch::Tensor sig_val = torch::sigmoid(arg_sigmoid);
+        torch::Tensor result = x * sig_val;
+        return result;
     }
-
 
     auto DRA::forward(std::initializer_list<std::any> tensors) -> std::any
     {
