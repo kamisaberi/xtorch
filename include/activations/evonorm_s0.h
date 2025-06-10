b52bc034e4fb18025b218bc2faf5409b80d8a5fd
kamisaberi <kamisaberi@gmail.com>
2025-06-10 14:29:46 +0330

    elish activation function created

--- include/activations/evonorm_s0.h
+++ include/activations/evonorm_s0.h
@@ -2,18 +2,19 @@
 
 #include "common.h"
 
-namespace xt::activations {
-    torch::Tensor evonorm_s0(torch::Tensor x);
+namespace xt::activations
+{
+    torch::Tensor evonorm_s0(const torch::Tensor& x, const torch::Tensor& gamma, const torch::Tensor& beta,
+                             const torch::Tensor& v_param, int64_t num_groups,
+                             double eps = 1e-5);
 
-    struct EvonormS0 : xt::Module {
+    struct EvonormS0 : xt::Module
+    {
     public:
         EvonormS0() = default;
 
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
 }
-
-
-
