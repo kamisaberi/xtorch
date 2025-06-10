0e53624bf57401d05ab1ebf2ed2f0d14c2b4ea5a
kamisaberi <kamisaberi@gmail.com>
2025-06-10 22:53:28 +0330

    shifted_softplus activation function created

--- include/activations/shifted_softplus.h
+++ include/activations/shifted_softplus.h
@@ -2,17 +2,18 @@
 
 #include "common.h"
 
-namespace xt::activations {
-    torch::Tensor shifted_softplus(torch::Tensor x);
+namespace xt::activations
+{
+    const double LN_2 = std::log(2.0); // More portable way to get log(2)
 
-    struct ShiftedSoftplus : xt::Module {
+    torch::Tensor shifted_softplus(const torch::Tensor& x, double shift_val = LN_2);
+
+    struct ShiftedSoftplus : xt::Module
+    {
     public:
         ShiftedSoftplus() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
 }
-
-
-
