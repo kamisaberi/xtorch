83f77b931443ee95bfc68d0021e1a995d08f909d
kamisaberi <kamisaberi@gmail.com>
2025-06-10 05:14:36 +0330

    aria activation function created

--- src/activations/aria.cpp
+++ src/activations/aria.cpp
@@ -2,9 +2,13 @@
 
 namespace xt::activations
 {
-    torch::Tensor aria(torch::Tensor x)
+    torch::Tensor aria(const torch::Tensor& x, double alpha, double beta)
     {
-         return  torch::zeros(10);
+        torch::Tensor x_pow_alpha = torch::pow(x, alpha);
+        torch::Tensor arg_tanh = beta * x_pow_alpha;
+        torch::Tensor tanh_val = torch::tanh(arg_tanh);
+        torch::Tensor gate = (1.0 + tanh_val) / 2.0;
+        return x * gate;
     }
 
     auto ARiA::forward(std::initializer_list<std::any> tensors) -> std::any
