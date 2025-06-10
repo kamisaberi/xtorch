a800f7ffae326b060ae17e6097a741923b3a2228
kamisaberi <kamisaberi@gmail.com>
2025-06-10 14:48:32 +0330

    hermite activation function created

--- src/activations/hermite.cpp
+++ src/activations/hermite.cpp
@@ -2,11 +2,29 @@
 
 namespace xt::activations
 {
-    torch::Tensor hermite(torch::Tensor x)
-    {
-        return torch::zeros(10);
-    }
+    torch::Tensor hermite(const torch::Tensor& x) {
+        torch::Tensor x_abs = torch::abs(x);
 
+        torch::Tensor y_gt_2 = x;
+
+        torch::Tensor y_1_to_2_abs = x_abs * (2.0 - x_abs);
+        torch::Tensor y_1_to_2 = torch::copysign(y_1_to_2_abs, x);
+
+        torch::Tensor y_lt_1_abs = (2.0 * x_abs - 3.0) * x_abs * x_abs + 1.0;
+        torch::Tensor y_lt_1 = torch::copysign(y_lt_1_abs, x);
+
+
+        torch::Tensor result = torch::where(
+            x_abs > 2.0,
+            y_gt_2,
+            torch::where(
+                x_abs > 1.0,
+                y_1_to_2,
+                y_lt_1
+            )
+        );
+        return result;
+    }
     auto Hermite::forward(std::initializer_list<std::any> tensors) -> std::any
     {
         return xt::activations::hermite(torch::zeros(10));
