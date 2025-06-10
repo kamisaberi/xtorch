197cb69b0b10d4005c819af7662a11a0abef7372
kamisaberi <kamisaberi@gmail.com>
2025-06-10 23:44:50 +0330

    splash activation function created

--- src/activations/splash.cpp
+++ src/activations/splash.cpp
@@ -2,9 +2,12 @@
 
 namespace xt::activations
 {
-    torch::Tensor splash(torch::Tensor x)
+    torch::Tensor splash(const torch::Tensor& x, double S , double R , double B )
     {
-        return torch::zeros(10);
+        torch::Tensor log_term_arg = B * torch::pow(torch::abs(S * x), R) + 1.0;
+        torch::Tensor result = x * (torch::log(log_term_arg) / torch::log(torch::tensor(2.0, x.options())));
+        // log base 2
+        return result;
     }
 
     auto SPLASH::forward(std::initializer_list<std::any> tensors) -> std::any
