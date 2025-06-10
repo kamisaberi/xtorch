71320f1167b7aff899311b5b0057b3a7ed161177
kamisaberi <kamisaberi@gmail.com>
2025-06-11 00:00:13 +0330

    squared_relu activation function created

--- src/activations/squared_relu.cpp
+++ src/activations/squared_relu.cpp
@@ -2,9 +2,10 @@
 
 namespace xt::activations
 {
-    torch::Tensor squared_relu(torch::Tensor x)
+    torch::Tensor squared_relu(const torch::Tensor& x)
     {
-        return torch::zeros(10);
+        torch::Tensor relu_x = torch::relu(x);
+        return relu_x * relu_x;
     }
 
     auto SquaredReLU::forward(std::initializer_list<std::any> tensors) -> std::any
