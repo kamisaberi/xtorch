06b77ffd9d755ea142fd2011c05e9eb01b8c30ac
kamisaberi <kamisaberi@gmail.com>
2025-06-10 22:53:28 +0330

    shifted_softplus activation function created

--- src/activations/shifted_softplus.cpp
+++ src/activations/shifted_softplus.cpp
@@ -2,9 +2,11 @@
 
 namespace xt::activations
 {
-    torch::Tensor shifted_softplus(torch::Tensor x)
+
+    torch::Tensor shifted_softplus(const torch::Tensor& x, double shift_val )
     {
-        return torch::zeros(10);
+        return torch::softplus(x) - shift_val;
+        // torch::softplus(x) = torch::log1p(torch::exp(x))
     }
 
     auto ShiftedSoftplus::forward(std::initializer_list<std::any> tensors) -> std::any
