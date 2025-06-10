575cc2482220e708cdbec1aa5978aa8f834f6811
kamisaberi <kamisaberi@gmail.com>
2025-06-10 05:31:31 +0330

    coslu activation function created

--- src/activations/coslu.cpp
+++ src/activations/coslu.cpp
@@ -2,9 +2,12 @@
 
 namespace xt::activations
 {
-    torch::Tensor coslu(torch::Tensor x)
+    torch::Tensor coslu(torch::Tensor& x)
     {
-        return torch::zeros(10);
+        torch::Tensor positive_part = x * torch::cos(x);
+        torch::Tensor negative_part = x * torch::exp(x);
+        torch::Tensor result = torch::where(x >= 0, positive_part, negative_part);
+        return result;
     }
 
     auto CosLU::forward(std::initializer_list<std::any> tensors) -> std::any
