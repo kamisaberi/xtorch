9635346707b01e47606c0375c6af45e143476ee8
kamisaberi <kamisaberi@gmail.com>
2025-06-10 21:15:55 +0330

    m_arcsinh activation function created

--- include/activations/lin_comb.h
+++ include/activations/lin_comb.h
@@ -2,22 +2,21 @@
 
 #include "common.h"
 
-namespace xt::activations {
+namespace xt::activations
+{
     torch::Tensor lin_comb(
         const torch::Tensor& x,
         const std::vector<std::function<torch::Tensor(const torch::Tensor&)>>& base_functions,
         const torch::Tensor& coefficients // Shape (num_base_functions)
     );
 
-    struct LinComb : xt::Module {
+    struct LinComb : xt::Module
+    {
     public:
         LinComb() = default;
 
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
 }
-
-
-
