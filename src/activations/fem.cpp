a73ff7abcad9a837a67f871e29e07cdd6e78d771
kamisaberi <kamisaberi@gmail.com>
2025-06-10 14:31:50 +0330

    fem activation function created

--- src/activations/fem.cpp
+++ src/activations/fem.cpp
@@ -2,11 +2,13 @@
 
 namespace xt::activations
 {
-    torch::Tensor fem(torch::Tensor x)
-    {
-        return torch::zeros(10);
+    torch::Tensor fem(const torch::Tensor& x, double alpha , double beta ) {
+        torch::Tensor x_plus_alpha = x + alpha;
+        torch::Tensor softplus_term = torch::softplus(x_plus_alpha); // softplus(y) = ln(1 + exp(y))
+        torch::Tensor tanh_term = torch::tanh(softplus_term);
+        torch::Tensor result = x * tanh_term + beta * x;
+        return result;
     }
-
     auto FEM::forward(std::initializer_list<std::any> tensors) -> std::any
     {
         return xt::activations::fem(torch::zeros(10));
