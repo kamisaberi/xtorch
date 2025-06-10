659b3d4ee36620f44aa20af8187e5f56eae94971
kamisaberi <kamisaberi@gmail.com>
2025-06-10 05:19:19 +0330

    asu activation function created

--- src/activations/asu.cpp
+++ src/activations/asu.cpp
@@ -1,14 +1,17 @@
 #include "include/activations/asu.h"
 
-namespace xt::activations {
-    torch::Tensor asu(torch::Tensor x) {
-        return  torch::zeros(10);
+namespace xt::activations
+{
+    torch::Tensor asu(const torch::Tensor& x, double alpha, double beta, double gamma)
+    {
+        torch::Tensor beta_x = beta * x;
+        torch::Tensor sig_beta_x = torch::sigmoid(beta_x);
+        torch::Tensor result = alpha * x * sig_beta_x + gamma;
+        return result;
     }
 
     auto ASU::forward(std::initializer_list<std::any> tensors) -> std::any
     {
-
         return xt::activations::asu(torch::zeros(10));
     }
-
 }
