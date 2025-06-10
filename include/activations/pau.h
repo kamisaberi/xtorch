d1c77ed7b939d48abee8ca95876163bea87f743a
kamisaberi <kamisaberi@gmail.com>
2025-06-10 21:38:02 +0330

    pau activation function created

--- include/activations/pau.h
+++ include/activations/pau.h
@@ -4,7 +4,13 @@
 
 namespace xt::activations
 {
-    torch::Tensor pau(torch::Tensor x);
+    torch::Tensor pau(
+        const torch::Tensor& x,
+        const torch::Tensor& P_coeffs, // Numerator coefficients [p_m, p_{m-1}, ..., p_1, p_0]
+        const torch::Tensor& Q_coeffs, // Denominator coefficients [q_n, q_{n-1}, ..., q_1, 1.0] (q_0 is fixed to 1)
+        double epsilon = 1e-7
+    );
+
 
     struct PAU : xt::Module
     {
