c2a56f75af0bd449b27b6eaebd7a4796f996f9c4
kamisaberi <kamisaberi@gmail.com>
2025-06-10 21:48:35 +0330

    rational activation function created

--- include/activations/rational.h
+++ include/activations/rational.h
@@ -2,18 +2,22 @@
 
 #include "common.h"
 
-namespace xt::activations {
-    torch::Tensor rational(torch::Tensor x);
+namespace xt::activations
+{
+    torch::Tensor rational(
+        const torch::Tensor& x,
+        const torch::Tensor& P_coeffs, // Numerator coefficients [p_m, ..., p_1, p_0]
+        const torch::Tensor& Q_coeffs, // Denominator coefficients [q_n, ..., q_1] (q_0 is fixed to 1)
+        double epsilon = 1e-7 // For denominator stability
+    );
 
-    struct Rational : xt::Module {
+
+    struct Rational : xt::Module
+    {
     public:
         Rational() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
 }
-
-
-
