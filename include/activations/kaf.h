a8067440b78e6687aca5b457f75ae4b521bbe4a9
kamisaberi <kamisaberi@gmail.com>
2025-06-10 14:52:09 +0330

    kaf activation function created

--- include/activations/kaf.h
+++ include/activations/kaf.h
@@ -2,17 +2,19 @@
 
 #include "common.h"
 
-namespace xt::activations {
-    torch::Tensor kaf(torch::Tensor x);
+namespace xt::activations
+{
+    torch::Tensor kaf(const torch::Tensor& x,
+                      const torch::Tensor& dictionary_coefs, // Shape [D] or [1, D] for broadcasting
+                      const torch::Tensor& boundary_params // Shape [D-1] or [1, D-1] for broadcasting
+    );
 
-    struct KAF : xt::Module {
+    struct KAF : xt::Module
+    {
     public:
         KAF() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
 }
-
-
-
