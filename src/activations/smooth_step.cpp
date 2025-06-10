8ce73d30d707bb3d95187c4580513b7472d6070c
kamisaberi <kamisaberi@gmail.com>
2025-06-10 23:09:11 +0330

    smish activation function created

--- src/activations/smooth_step.cpp
+++ src/activations/smooth_step.cpp
@@ -2,9 +2,13 @@
 
 namespace xt::activations
 {
-    torch::Tensor smooth_step(torch::Tensor x)
+    torch::Tensor smooth_step(const torch::Tensor& x, double edge0, double edge1)
     {
-        return torch::zeros(10);
+        TORCH_CHECK(edge0 < edge1, "edge0 must be less than edge1 for smoothstep.");
+
+        torch::Tensor t = torch::clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+        torch::Tensor result = t * t * (3.0 - 2.0 * t);
+        return result;
     }
 
     auto SmoothStep::forward(std::initializer_list<std::any> tensors) -> std::any
