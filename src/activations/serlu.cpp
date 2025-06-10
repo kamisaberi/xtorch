7dde1a7597a0934d98dc42e6cf8d017c6b32fa24
kamisaberi <kamisaberi@gmail.com>
2025-06-10 22:50:48 +0330

    serlu activation function created

--- src/activations/serlu.cpp
+++ src/activations/serlu.cpp
@@ -1,15 +1,18 @@
 #include "include/activations/serlu.h"
 
-namespace xt::activations {
-    torch::Tensor serlu(torch::Tensor x) {
-        return  torch::zeros(10);
+namespace xt::activations
+{
+    torch::Tensor serlu(const torch::Tensor& x, double lambda_serlu, double alpha_serlu)
+    {
+        torch::Tensor positive_part = lambda_serlu * x;
+        torch::Tensor negative_part = lambda_serlu * alpha_serlu * (torch::exp(x) - 1.0);
+
+        torch::Tensor result = torch::where(x > 0, positive_part, negative_part);
+        return result;
     }
 
     auto SERLU::forward(std::initializer_list<std::any> tensors) -> std::any
     {
-
-
         return xt::activations::serlu(torch::zeros(10));
     }
-
 }
