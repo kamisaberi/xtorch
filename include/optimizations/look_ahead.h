90668fb9b458d3324d889b1a4c39e7895e78f620
kamisaberi <kamisaberi@gmail.com>
2025-06-13 03:05:21 +0330

    some bugs during build fixed

--- include/optimizations/look_ahead.h
+++ include/optimizations/look_ahead.h
@@ -61,7 +61,7 @@ struct LookaheadParamState : torch::optim::OptimizerParamState {
     TORCH_ARG(torch::Tensor, exp_avg);      // m_t
     TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t
 
-    LookaheadParamState() = default;
+    // LookaheadParamState() = default;
     void serialize(torch::serialize::OutputArchive& archive) const override;
     void deserialize(torch::serialize::InputArchive& archive) ;
     std::unique_ptr<OptimizerParamState> clone() const override;
