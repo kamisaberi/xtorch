26acaf9a15a72934fd15d8250bfe097655169bfe
kamisaberi <kamisaberi@gmail.com>
2025-06-13 19:05:09 +0330

    dropouts are under construction

--- include/dropouts/temporal_dropout.h
+++ include/dropouts/temporal_dropout.h
@@ -3,14 +3,17 @@
 #include "common.h"
 
 namespace xt::dropouts {
-    torch::Tensor temporal_dropout(torch::Tensor x);
 
     struct TemporalDropout : xt::Module {
     public:
-        TemporalDropout() = default;
+        TemporalDropout(double p_drop_timestep = 0.1, int time_dim = 1);
         auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
+        double p_drop_timestep_; // Probability of dropping an entire time step.
+        int time_dim_;           // The dimension index representing time/sequence length.
+        double epsilon_ = 1e-7;
+
     };
 }
 
