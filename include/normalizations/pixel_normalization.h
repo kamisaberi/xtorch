0bb0a72a3dd6083c006e72f94f02dcae877ce847
kamisaberi <kamisaberi@gmail.com>
2025-06-14 00:38:56 +0330

    normalizations are under construction

--- include/normalizations/pixel_normalization.h
+++ include/normalizations/pixel_normalization.h
@@ -7,10 +7,11 @@ namespace xt::norm
     struct PixelNorm : xt::Module
     {
     public:
-        PixelNorm() = default;
+        PixelNorm(double eps = 1e-8);
 
         auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        double eps_; // Small epsilon for numerical stability
     };
 }
