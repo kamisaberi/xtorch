e2f37e40994afd5e5ed44df16dddda4608e61e07
kamisaberi <kamisaberi@gmail.com>
2025-06-13 18:03:18 +0330

    dropouts are under construction

--- include/dropouts/grad_drop.h
+++ include/dropouts/grad_drop.h
@@ -3,15 +3,21 @@
 #include "common.h"
 
 namespace xt::dropouts {
-    torch::Tensor grad_drop(torch::Tensor x);
 
     struct GradDrop : xt::Module {
     public:
-        GradDrop() = default;
 
+        GradDrop(double p_drop_at_zero_activation = 0.5, double sensitivity = 1.0);
         auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
+
+
+        double p_drop_at_zero_activation_; // Dropout probability if activation is zero
+        double sensitivity_;               // How fast keep_prob increases with abs(activation)
+        double base_logit_keep_at_zero_;   // Precomputed logit(1 - p_drop_at_zero_activation_)
+        double epsilon_ = 1e-7;           // For numerical stability
+
     };
 }
 
