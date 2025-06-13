87048392964870a2d7910a9af2fd4c6d58dcb4ea
kamisaberi <kamisaberi@gmail.com>
2025-06-13 15:22:29 +0330

    attention_dropout finished

--- include/dropouts/auto_dropout.h
+++ include/dropouts/auto_dropout.h
@@ -2,17 +2,17 @@
 
 #include "common.h"
 
-namespace xt::dropouts {
+namespace xt::dropouts
+{
     torch::Tensor auto_dropout(torch::Tensor x);
 
-    struct AutoDropout : xt::Module {
+    struct AutoDropout : xt::Module
+    {
     public:
-        AutoDropout() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        AutoDropout(c10::IntArrayRef probability_shape = {}, double initial_dropout_rate = 0.05);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        torch::Tensor log_alpha_;
     };
 }
-
-
-
