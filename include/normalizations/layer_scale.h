672f1374af9daadae8d09b72ad142887c598be6c
kamisaberi <kamisaberi@gmail.com>
2025-06-13 23:44:43 +0330

    normalizations are under construction

--- include/normalizations/layer_scale.h
+++ include/normalizations/layer_scale.h
@@ -7,10 +7,16 @@ namespace xt::norm
     struct LayerScale : xt::Module
     {
     public:
-        LayerScale() = default;
+        LayerScale(int64_t dim, double initial_value = 1e-4);
 
         auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        int64_t dim_; // The feature dimension (number of channels)
+        double initial_value_; // Initial value for the learnable scaling factors
+
+        // Learnable scaling parameter (lambda)
+        // It's a vector of size 'dim_', one scale factor per channel.
+        torch::Tensor lambda_;
     };
 }
