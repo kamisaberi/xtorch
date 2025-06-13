06938815d10cfc12eb756f14e456d3b2d091f1f0
kamisaberi <kamisaberi@gmail.com>
2025-06-13 18:38:59 +0330

    dropouts are under construction

--- include/dropouts/sensor_dropout.h
+++ include/dropouts/sensor_dropout.h
@@ -2,18 +2,18 @@
 
 #include "common.h"
 
-namespace xt::dropouts {
-    torch::Tensor sensor_dropout(torch::Tensor x);
-
-    struct SensorDropout : xt::Module {
+namespace xt::dropouts
+{
+    struct SensorDropout : xt::Module
+    {
     public:
-        SensorDropout() = default;
-
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        SensorDropout(const std::vector<int64_t>& sensor_splits, double p_drop_sensor = 0.1);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        double p_drop_sensor_; // Probability of dropping any given sensor
+        std::vector<int64_t> sensor_splits_; // Number of features for each sensor, e.g., {10, 5, 15} for 3 sensors
+        int64_t total_features_;
+        double epsilon_ = 1e-7;
     };
 }
-
-
-
