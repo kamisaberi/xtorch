7368492e3d2e3344468cfded25915fbf48aba4c2
kamisaberi <kamisaberi@gmail.com>
2025-06-13 17:48:45 +0330

    dropouts are under construction

--- include/dropouts/gaussian_dropout.h
+++ include/dropouts/gaussian_dropout.h
@@ -3,14 +3,16 @@
 #include "common.h"
 
 namespace xt::dropouts {
-    torch::Tensor gaussian_dropout(torch::Tensor x);
 
     struct GaussianDropout : xt::Module {
     public:
-        GaussianDropout() = default;
+        GaussianDropout(double p_rate = 0.1);
         auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
+        double p_rate_; // Corresponds to the 'p' in standard dropout, used to calculate alpha (variance)
+        double alpha_; // Variance of the multiplicative noise (noise ~ N(0, alpha_)), so multiplier ~ N(1, alpha_)
+
     };
 }
 
