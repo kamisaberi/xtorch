1756fa0dc3165dbaeb0a561e1ceef384cdda5967
kamisaberi <kamisaberi@gmail.com>
2025-06-13 15:54:03 +0330

    attention_dropout finished

--- include/dropouts/checkerboard_dropout.h
+++ include/dropouts/checkerboard_dropout.h
@@ -2,18 +2,19 @@
 
 #include "common.h"
 
-namespace xt::dropouts {
+namespace xt::dropouts
+{
     torch::Tensor checkerboard_dropout(torch::Tensor x);
 
-    struct CheckerboardDropout : xt::Module {
+    struct CheckerboardDropout : xt::Module
+    {
     public:
-        CheckerboardDropout() = default;
+        CheckerboardDropout(bool drop_even_sum_indices = true);
 
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        bool drop_even_sum_indices_; // If true, drop elements where sum of relevant indices is even.
+        // If false, drop elements where sum of relevant indices is odd.
     };
 }
-
-
-
