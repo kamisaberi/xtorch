57e8810e656c20d3c09b53b2245d1df444c8b333
kamisaberi <kamisaberi@gmail.com>
2025-06-13 19:28:28 +0330

    dropouts are under construction

--- include/dropouts/variational_gaussian_dropout.h
+++ include/dropouts/variational_gaussian_dropout.h
@@ -2,17 +2,19 @@
 
 #include "common.h"
 
-namespace xt::dropouts {
-    torch::Tensor variational_gaussian_dropout(torch::Tensor x);
+namespace xt::dropouts
+{
 
-    struct VariationalGaussianDropout : xt::Module {
+    struct VariationalGaussianDropout : xt::Module
+    {
     public:
-        VariationalGaussianDropout() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        VariationalGaussianDropout(c10::IntArrayRef alpha_shape = {},
+                                   double initial_dropout_rate_for_alpha_init = 0.05);
+
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        torch::Tensor log_alpha_;
+        double epsilon_ = 1e-8; // Small constant for numerical stability, e.g., in sqrt(alpha)
     };
 }
-
-
-
