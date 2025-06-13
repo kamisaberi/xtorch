be469b1c1bc782975fb852aec65707b555e44369
kamisaberi <kamisaberi@gmail.com>
2025-06-13 17:22:10 +0330

    dropouts are under construction

--- include/dropouts/drop_pathway.h
+++ include/dropouts/drop_pathway.h
@@ -3,15 +3,17 @@
 #include "common.h"
 
 namespace xt::dropouts {
-    torch::Tensor drop_pathway(torch::Tensor x);
 
     struct DropPathway : xt::Module {
     public:
-        DropPathway() = default;
+        DropPathway(double p_drop = 0.1);
 
         auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
+        double p_drop_; // Probability of dropping the pathway (input tensor)
+        double epsilon_ = 1e-7; // For numerical stability in division
+
     };
 }
 
