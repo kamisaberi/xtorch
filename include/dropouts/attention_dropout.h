fd5faca6764a1804474d9e337acb743a2d27b337
kamisaberi <kamisaberi@gmail.com>
2025-06-13 15:09:45 +0330

    adaptive_dropout finished

--- include/dropouts/attention_dropout.h
+++ include/dropouts/attention_dropout.h
@@ -2,17 +2,15 @@
 
 #include "common.h"
 
-namespace xt::dropouts {
-    torch::Tensor attention_dropout(torch::Tensor x);
-
-    struct AttentionDropout : xt::Module {
+namespace xt::dropouts
+{
+    struct AttentionDropout : xt::Module
+    {
     public:
-        AttentionDropout() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        AttentionDropout(double p = 0.1);
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        double p_; // Probability of an element to be zeroed.
     };
 }
-
-
-
