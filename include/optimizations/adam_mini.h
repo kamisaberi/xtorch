145ed254f0efd7054fcb517d498ccf1b11f53e81
kamisaberi <kamisaberi@gmail.com>
2025-06-15 05:51:11 +0330

    optimizations are under construction

--- include/optimizations/adam_mini.h
+++ include/optimizations/adam_mini.h
@@ -21,9 +21,10 @@ struct AdamMiniOptions : torch::optim::OptimizerOptions {
     TORCH_ARG(double, beta2) = 0.999;
     TORCH_ARG(double, eps) = 1e-8;
     TORCH_ARG(double, weight_decay) = 0.0;
+    TORCH_ARG(double ,  lr) = 1e-6;
 
     void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) override;
+    void deserialize(torch::serialize::InputArchive& archive) ;
     std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
 };
 
@@ -35,7 +36,7 @@ struct AdamMiniParamState : torch::optim::OptimizerParamState {
 
     AdamMiniParamState() = default;
     void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) override;
+    void deserialize(torch::serialize::InputArchive& archive) ;
     std::unique_ptr<OptimizerParamState> clone() const override;
 };
 
@@ -51,7 +52,7 @@ public:
     void load(torch::serialize::InputArchive& archive) override;
 
 protected:
-    std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() override;
+    std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
 };
 
 #endif // ADAM_MINI_OPTIMIZER_HPP
