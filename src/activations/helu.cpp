b731e22af036eb57053224bcaa37541fa9a27191
kamisaberi <kamisaberi@gmail.com>
2025-06-17 02:10:29 +0330

    some bugs fixed

--- src/activations/helu.cpp
+++ src/activations/helu.cpp
@@ -2,7 +2,7 @@
 
 namespace xt::activations
 {
-    torch::Tensor helu(const torch::Tensor& x, double alpha, double lambda_param)
+    torch::Tensor helu(const torch::Tensor x, double alpha, double lambda_param)
     {
         torch::Tensor positive_part = x;
         torch::Tensor negative_part = alpha * (torch::exp(x / lambda_param) - 1.0);
@@ -13,6 +13,7 @@ namespace xt::activations
 
     auto HeLU::forward(std::initializer_list<std::any> tensors) -> std::any
     {
-        return xt::activations::helu(torch::zeros(10));
+
+        return xt::activations::helu(torch::zeros({10}));
     }
 }
