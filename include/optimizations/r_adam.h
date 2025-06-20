dd27bbd195dfb75165a0c692ce37d1c41e5af7da
kamisaberi <kamisaberi@gmail.com>
2025-06-20 18:15:29 +0330

    some bugs fixed

--- include/optimizations/r_adam.h
+++ include/optimizations/r_adam.h
@@ -2,49 +2,50 @@
 
 
 #include "common.h"
+namespace xt::optim
+{
+    // --- Options for RAdam Optimizer ---
+    struct RAdamOptions : torch::optim::OptimizerOptions {
+        explicit RAdamOptions(double learning_rate = 1e-3)
+            : torch::optim::OptimizerOptions() {
+            this->lr(learning_rate);
+        }
 
-// --- Options for RAdam Optimizer ---
-struct RAdamOptions : torch::optim::OptimizerOptions {
-    explicit RAdamOptions(double learning_rate = 1e-3)
-        : torch::optim::OptimizerOptions() {
-        this->lr(learning_rate);
-    }
+        TORCH_ARG(double, beta1) = 0.9;
+        TORCH_ARG(double, beta2) = 0.999;
+        TORCH_ARG(double, eps) = 1e-8;
+        TORCH_ARG(double, weight_decay) = 0.0;
+        TORCH_ARG(double, lr) = 1e-6;
 
-    TORCH_ARG(double, beta1) = 0.9;
-    TORCH_ARG(double, beta2) = 0.999;
-    TORCH_ARG(double, eps) = 1e-8;
-    TORCH_ARG(double, weight_decay) = 0.0;
-    TORCH_ARG(double, lr) = 1e-6;
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
+    };
 
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
-};
+    // --- Parameter State for RAdam Optimizer ---
+    struct RAdamParamState : torch::optim::OptimizerParamState {
+        TORCH_ARG(torch::Tensor, step);
+        TORCH_ARG(torch::Tensor, exp_avg);      // m_t
+        TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t
 
-// --- Parameter State for RAdam Optimizer ---
-struct RAdamParamState : torch::optim::OptimizerParamState {
-    TORCH_ARG(torch::Tensor, step);
-    TORCH_ARG(torch::Tensor, exp_avg);      // m_t
-    TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t
+        // RAdamParamState() = default;
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<OptimizerParamState> clone() const override;
+    };
 
-    // RAdamParamState() = default;
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<OptimizerParamState> clone() const override;
-};
+    // --- RAdam Optimizer Class ---
+    class RAdam : public torch::optim::Optimizer {
+    public:
+        RAdam(std::vector<torch::Tensor> params, RAdamOptions options);
+        explicit RAdam(std::vector<torch::Tensor> params, double lr = 1e-3);
 
-// --- RAdam Optimizer Class ---
-class RAdam : public torch::optim::Optimizer {
-public:
-    RAdam(std::vector<torch::Tensor> params, RAdamOptions options);
-    explicit RAdam(std::vector<torch::Tensor> params, double lr = 1e-3);
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+        void save(torch::serialize::OutputArchive& archive) const override;
+        void load(torch::serialize::InputArchive& archive) override;
 
-    using LossClosure = std::function<torch::Tensor()>;
-    torch::Tensor step(LossClosure closure = nullptr) override;
-    void save(torch::serialize::OutputArchive& archive) const override;
-    void load(torch::serialize::InputArchive& archive) override;
-
-protected:
-    std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
-};
-
+    protected:
+        std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
+    };
+}
