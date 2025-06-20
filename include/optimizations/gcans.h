d5a3b9c5941eb0d1618d78ba17b032b6851368fa
kamisaberi <kamisaberi@gmail.com>
2025-06-20 17:49:40 +0330

    some bugs fixed

--- include/optimizations/gcans.h
+++ include/optimizations/gcans.h
@@ -1,15 +1,7 @@
-#ifndef GCANS_OPTIMIZER_HPP
-#define GCANS_OPTIMIZER_HPP
+#pragma once
 
-#include <torch/torch.h>
-#include <torch/serialize/archive.h>
 
-#include <cmath>
-#include <vector>
-#include <memory>
-#include <string>
-#include <cstdint>
-
+#include "common.h"
 // --- Options for GCANS Optimizer ---
 struct GCANSOptions : torch::optim::OptimizerOptions {
     double lr;
@@ -58,4 +50,3 @@ protected:
     std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
 };
 
-#endif // GCANS_OPTIMIZER_HPP
