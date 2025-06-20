653bf84d81d5193db74450229d234147835410d6
kamisaberi <kamisaberi@gmail.com>
2025-06-20 17:58:20 +0330

    some bugs fixed

--- include/optimizations/power_sgd.h
+++ include/optimizations/power_sgd.h
@@ -1,15 +1,7 @@
-#ifndef POWERSGD_OPTIMIZER_HPP
-#define POWERSGD_OPTIMIZER_HPP
+#pragma once
 
-#include <torch/torch.h>
-#include <torch/serialize/archive.h>
 
-#include <cmath>
-#include <vector>
-#include <memory>
-#include <string>
-#include <cstdint>
-
+#include "common.h"
 // --- Options for PowerSGD Optimizer ---
 struct PowerSGDOptions : torch::optim::OptimizerOptions
 {
@@ -58,4 +50,3 @@ protected:
     std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
 };
 
-#endif // POWERSGD_OPTIMIZER_HPP
