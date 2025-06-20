bff77890bec829483abb249fd31292190086df91
kamisaberi <kamisaberi@gmail.com>
2025-06-20 18:07:16 +0330

    some bugs fixed

--- include/optimizations/ams_grad.h
+++ include/optimizations/ams_grad.h
@@ -2,49 +2,51 @@
 
 
 #include "common.h"
-// --- Options for AMSGrad Optimizer ---
-struct AMSGradOptions : torch::optim::OptimizerOptions {
-    explicit AMSGradOptions(double learning_rate = 1e-3)
-        : torch::optim::OptimizerOptions() {
-        this->lr(learning_rate);
-    }
+namespace xt::optim
+{
+    // --- Options for AMSGrad Optimizer ---
+    struct AMSGradOptions : torch::optim::OptimizerOptions {
+        explicit AMSGradOptions(double learning_rate = 1e-3)
+            : torch::optim::OptimizerOptions() {
+            this->lr(learning_rate);
+        }
 
-    TORCH_ARG(double, beta1) = 0.9;
-    TORCH_ARG(double, beta2) = 0.999;
-    TORCH_ARG(double, eps) = 1e-8;
-    TORCH_ARG(double, weight_decay) = 0.0;
-    TORCH_ARG(double ,  lr) = 1e-6;
+        TORCH_ARG(double, beta1) = 0.9;
+        TORCH_ARG(double, beta2) = 0.999;
+        TORCH_ARG(double, eps) = 1e-8;
+        TORCH_ARG(double, weight_decay) = 0.0;
+        TORCH_ARG(double ,  lr) = 1e-6;
 
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
-};
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
+    };
 
-// --- Parameter State for AMSGrad Optimizer ---
-struct AMSGradParamState : torch::optim::OptimizerParamState {
-    TORCH_ARG(torch::Tensor, step);
-    TORCH_ARG(torch::Tensor, exp_avg);      // m_t
-    TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t
-    TORCH_ARG(torch::Tensor, max_exp_avg_sq); // The key AMSGrad state
+    // --- Parameter State for AMSGrad Optimizer ---
+    struct AMSGradParamState : torch::optim::OptimizerParamState {
+        TORCH_ARG(torch::Tensor, step);
+        TORCH_ARG(torch::Tensor, exp_avg);      // m_t
+        TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t
+        TORCH_ARG(torch::Tensor, max_exp_avg_sq); // The key AMSGrad state
 
-    // AMSGradParamState() = default;
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<OptimizerParamState> clone() const override;
-};
+        // AMSGradParamState() = default;
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<OptimizerParamState> clone() const override;
+    };
 
-// --- AMSGrad Optimizer Class ---
-class AMSGrad : public torch::optim::Optimizer {
-public:
-    AMSGrad(std::vector<torch::Tensor> params, AMSGradOptions options);
-    explicit AMSGrad(std::vector<torch::Tensor> params, double lr = 1e-3);
+    // --- AMSGrad Optimizer Class ---
+    class AMSGrad : public torch::optim::Optimizer {
+    public:
+        AMSGrad(std::vector<torch::Tensor> params, AMSGradOptions options);
+        explicit AMSGrad(std::vector<torch::Tensor> params, double lr = 1e-3);
 
-    using LossClosure = std::function<torch::Tensor()>;
-    torch::Tensor step(LossClosure closure = nullptr) override;
-    void save(torch::serialize::OutputArchive& archive) const override;
-    void load(torch::serialize::InputArchive& archive) override;
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+        void save(torch::serialize::OutputArchive& archive) const override;
+        void load(torch::serialize::InputArchive& archive) override;
 
-protected:
-    std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
-};
-
+    protected:
+        std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
+    };
+}
