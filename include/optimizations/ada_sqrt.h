c18356c3c8b547c0dc80a613b2d5f8366f0ae29e
kamisaberi <kamisaberi@gmail.com>
2025-06-20 17:44:51 +0330

    some bugs fixed

--- include/optimizations/ada_sqrt.h
+++ include/optimizations/ada_sqrt.h
@@ -1,15 +1,7 @@
-#ifndef ADASQRT_OPTIMIZER_HPP
-#define ADASQRT_OPTIMIZER_HPP
+#pragma once
 
-#include <torch/torch.h>
-#include <torch/serialize/archive.h>
 
-#include <cmath>
-#include <vector>
-#include <memory>
-#include <string>
-#include <cstdint>
-
+#include "common.h"
 // --- Options for AdaSqrt Optimizer ---
 struct AdaSqrtOptions : torch::optim::OptimizerOptions {
     explicit AdaSqrtOptions(double learning_rate = 1e-3)
@@ -55,4 +47,3 @@ protected:
     std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
 };
 
-#endif // ADASQRT_OPTIMIZER_HPP
