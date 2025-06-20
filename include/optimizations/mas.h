32dc98507e0d7e24e32d53c4ecb88b663bd1a5f4
kamisaberi <kamisaberi@gmail.com>
2025-06-20 17:56:56 +0330

    some bugs fixed

--- include/optimizations/mas.h
+++ include/optimizations/mas.h
@@ -1,17 +1,7 @@
-#ifndef MAS_OPTIMIZER_HPP
-#define MAS_OPTIMIZER_HPP
+#pragma once
 
-#include <torch/torch.h>
-#include <torch/serialize/archive.h>
 
-#include <cmath>
-#include <vector>
-#include <memory>
-#include <string>
-#include <cstdint>
-
-#include "../data_loaders/extended_data_loader.h"
-
+#include "common.h"
 // --- Options for MAS Optimizer ---
 struct MASOptions : torch::optim::OptimizerOptions {
     explicit MASOptions(double learning_rate = 1e-3)
@@ -69,4 +59,3 @@ protected:
     std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
 };
 
-#endif // MAS_OPTIMIZER_HPP
