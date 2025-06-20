b789adb8a27bbb75500a87a4131feb79e1b53143
kamisaberi <kamisaberi@gmail.com>
2025-06-20 18:11:34 +0330

    some bugs fixed

--- include/optimizations/gcans.h
+++ include/optimizations/gcans.h
@@ -2,51 +2,53 @@
 
 
 #include "common.h"
-// --- Options for GCANS Optimizer ---
-struct GCANSOptions : torch::optim::OptimizerOptions {
-    double lr;
-    explicit GCANSOptions(double learning_rate = 0.1) // SGD often uses a higher LR
-        : torch::optim::OptimizerOptions() {
-        this->lr= learning_rate;
-    }
+namespace xt::optim
+{
+    // --- Options for GCANS Optimizer ---
+    struct GCANSOptions : torch::optim::OptimizerOptions {
+        double lr;
+        explicit GCANSOptions(double learning_rate = 0.1) // SGD often uses a higher LR
+            : torch::optim::OptimizerOptions() {
+            this->lr= learning_rate;
+        }
 
-    // Base SGD with Momentum parameters
-    TORCH_ARG(double, momentum) = 0.9;
-    TORCH_ARG(double, weight_decay) = 1e-4;
+        // Base SGD with Momentum parameters
+        TORCH_ARG(double, momentum) = 0.9;
+        TORCH_ARG(double, weight_decay) = 1e-4;
 
-    // GCANS parameters
-    TORCH_ARG(double, compression_ratio) = 0.01; // Select top 1% of gradients
-    TORCH_ARG(double, sampling_beta) = 0.99; // EMA decay for sampling probabilities
+        // GCANS parameters
+        TORCH_ARG(double, compression_ratio) = 0.01; // Select top 1% of gradients
+        TORCH_ARG(double, sampling_beta) = 0.99; // EMA decay for sampling probabilities
 
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
-};
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
+    };
 
-// --- Parameter State for GCANS ---
-struct GCANSParamState : torch::optim::OptimizerParamState {
-    TORCH_ARG(torch::Tensor, step);
-    TORCH_ARG(torch::Tensor, momentum_buffer); // For inner SGD
-    TORCH_ARG(torch::Tensor, error_feedback);  // Error accumulation
-    TORCH_ARG(torch::Tensor, sampling_probs);  // Adaptive sampling probabilities
-public:
-    // GCANSParamState() = default;
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<OptimizerParamState> clone() const override;
-};
+    // --- Parameter State for GCANS ---
+    struct GCANSParamState : torch::optim::OptimizerParamState {
+        TORCH_ARG(torch::Tensor, step);
+        TORCH_ARG(torch::Tensor, momentum_buffer); // For inner SGD
+        TORCH_ARG(torch::Tensor, error_feedback);  // Error accumulation
+        TORCH_ARG(torch::Tensor, sampling_probs);  // Adaptive sampling probabilities
+    public:
+        // GCANSParamState() = default;
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<OptimizerParamState> clone() const override;
+    };
 
-// --- GCANS Optimizer Class ---
-class GCANS : public torch::optim::Optimizer {
-public:
-    GCANS(std::vector<torch::Tensor> params, GCANSOptions options);
+    // --- GCANS Optimizer Class ---
+    class GCANS : public torch::optim::Optimizer {
+    public:
+        GCANS(std::vector<torch::Tensor> params, GCANSOptions options);
 
-    using LossClosure = std::function<torch::Tensor()>;
-    torch::Tensor step(LossClosure closure = nullptr) override;
-    void save(torch::serialize::OutputArchive& archive) const override;
-    void load(torch::serialize::InputArchive& archive) override;
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+        void save(torch::serialize::OutputArchive& archive) const override;
+        void load(torch::serialize::InputArchive& archive) override;
 
-protected:
-    std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
-};
-
+    protected:
+        std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
+    };
+}
