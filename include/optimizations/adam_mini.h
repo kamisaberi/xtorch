696e3dfcd4c464ef41a6039c8ddd8ce962b37853
kamisaberi <kamisaberi@gmail.com>
2025-06-20 18:05:21 +0330

    some bugs fixed

--- include/optimizations/adam_mini.h
+++ include/optimizations/adam_mini.h
@@ -3,48 +3,50 @@
 
 #include "common.h"
 
-// --- Options for AdamMini Optimizer ---
-struct AdamMiniOptions : torch::optim::OptimizerOptions {
-    explicit AdamMiniOptions(double learning_rate = 1e-3)
-        : torch::optim::OptimizerOptions() {
-        this->lr(learning_rate);
-    }
+namespace xt::optim
+{
+    // --- Options for AdamMini Optimizer ---
+    struct AdamMiniOptions : torch::optim::OptimizerOptions {
+        explicit AdamMiniOptions(double learning_rate = 1e-3)
+            : torch::optim::OptimizerOptions() {
+            this->lr(learning_rate);
+        }
 
-    TORCH_ARG(double, beta1) = 0.9;
-    TORCH_ARG(double, beta2) = 0.999;
-    TORCH_ARG(double, eps) = 1e-8;
-    TORCH_ARG(double, weight_decay) = 0.0;
-    TORCH_ARG(double ,  lr) = 1e-6;
+        TORCH_ARG(double, beta1) = 0.9;
+        TORCH_ARG(double, beta2) = 0.999;
+        TORCH_ARG(double, eps) = 1e-8;
+        TORCH_ARG(double, weight_decay) = 0.0;
+        TORCH_ARG(double ,  lr) = 1e-6;
 
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
-};
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
+    };
 
-// --- Parameter State for AdamMini Optimizer ---
-struct AdamMiniParamState : torch::optim::OptimizerParamState {
-    TORCH_ARG(torch::Tensor, step);
-    TORCH_ARG(torch::Tensor, exp_avg);      // m_t (full float32 precision)
-    TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t (stored in float16)
+    // --- Parameter State for AdamMini Optimizer ---
+    struct AdamMiniParamState : torch::optim::OptimizerParamState {
+        TORCH_ARG(torch::Tensor, step);
+        TORCH_ARG(torch::Tensor, exp_avg);      // m_t (full float32 precision)
+        TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t (stored in float16)
 
-    // AdamMiniParamState() = default;
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<OptimizerParamState> clone() const override;
-};
+        // AdamMiniParamState() = default;
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<OptimizerParamState> clone() const override;
+    };
 
-// --- AdamMini Optimizer Class ---
-class AdamMini : public torch::optim::Optimizer {
-public:
-    AdamMini(std::vector<torch::Tensor> params, AdamMiniOptions options);
-    explicit AdamMini(std::vector<torch::Tensor> params, double lr = 1e-3);
+    // --- AdamMini Optimizer Class ---
+    class AdamMini : public torch::optim::Optimizer {
+    public:
+        AdamMini(std::vector<torch::Tensor> params, AdamMiniOptions options);
+        explicit AdamMini(std::vector<torch::Tensor> params, double lr = 1e-3);
 
-    using LossClosure = std::function<torch::Tensor()>;
-    torch::Tensor step(LossClosure closure = nullptr) override;
-    void save(torch::serialize::OutputArchive& archive) const override;
-    void load(torch::serialize::InputArchive& archive) override;
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+        void save(torch::serialize::OutputArchive& archive) const override;
+        void load(torch::serialize::InputArchive& archive) override;
 
-protected:
-    std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
-};
-
+    protected:
+        std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
+    };
+}
