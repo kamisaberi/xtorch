d69010c3953d934ae4b21b344edc0bad7c5804cb
kamisaberi <kamisaberi@gmail.com>
2025-06-20 17:45:19 +0330

    some bugs fixed

--- include/optimizations/adam_w.h
+++ include/optimizations/adam_w.h
@@ -1,14 +1,7 @@
-#ifndef ADAMW_OPTIMIZER_HPP
-#define ADAMW_OPTIMIZER_HPP
+#pragma once
 
-#include <torch/torch.h>
-#include <torch/serialize/archive.h>
 
-#include <cmath>
-#include <vector>
-#include <memory>
-#include <string>
-#include <cstdint>
+#include "common.h"
 
 // --- Options for AdamW Optimizer ---
 // The options are identical to Adam, the difference is in the implementation.
@@ -59,4 +52,3 @@ protected:
     std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
 };
 
-#endif // ADAMW_OPTIMIZER_HPP
