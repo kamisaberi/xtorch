946ce4cab3c30a4925947b1a06e3f291580eda70
kamisaberi <kamisaberi@gmail.com>
2025-06-20 18:16:17 +0330

    some bugs fixed

--- include/optimizations/stochastic_weight_averaging.h
+++ include/optimizations/stochastic_weight_averaging.h
@@ -2,29 +2,31 @@
 
 
 #include "common.h"
-// --- StochasticWeightAveraging Wrapper Class ---
-// This is a wrapper, not a direct optimizer. It contains a base optimizer.
-class StochasticWeightAveraging {
-public:
-    // The constructor takes an existing, fully-configured optimizer.
-    StochasticWeightAveraging(std::unique_ptr<torch::optim::Optimizer> base_optimizer,
-                               int swa_start_step,
-                               int swa_update_frequency = 1);
+namespace xt::optim
+{
+    // --- StochasticWeightAveraging Wrapper Class ---
+    // This is a wrapper, not a direct optimizer. It contains a base optimizer.
+    class StochasticWeightAveraging {
+    public:
+        // The constructor takes an existing, fully-configured optimizer.
+        StochasticWeightAveraging(std::unique_ptr<torch::optim::Optimizer> base_optimizer,
+                                   int swa_start_step,
+                                   int swa_update_frequency = 1);
 
-    // Standard optimizer methods that delegate to the base optimizer
-    void zero_grad();
-    torch::Tensor step(torch::optim::Optimizer::LossClosure closure = nullptr);
+        // Standard optimizer methods that delegate to the base optimizer
+        void zero_grad();
+        torch::Tensor step(torch::optim::Optimizer::LossClosure closure = nullptr);
 
-    // SWA-specific methods
-    // Call this at the end of training to use the averaged weights
-    void swap_swa_weights();
+        // SWA-specific methods
+        // Call this at the end of training to use the averaged weights
+        void swap_swa_weights();
 
-private:
-    std::unique_ptr<torch::optim::Optimizer> base_optimizer_;
-    std::vector<torch::Tensor> swa_params_; // Stores the running average of weights
-    int64_t swa_start_step_;
-    int64_t swa_update_frequency_;
-    int64_t n_averaged_ = 0; // Number of models we have averaged so far
-    int64_t global_step_ = 0;
-};
-
+    private:
+        std::unique_ptr<torch::optim::Optimizer> base_optimizer_;
+        std::vector<torch::Tensor> swa_params_; // Stores the running average of weights
+        int64_t swa_start_step_;
+        int64_t swa_update_frequency_;
+        int64_t n_averaged_ = 0; // Number of models we have averaged so far
+        int64_t global_step_ = 0;
+    };
+}
