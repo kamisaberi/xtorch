9340b886a45b9ca9ad8f3c04a1697258c7444276
kamisaberi <kamisaberi@gmail.com>
2025-06-20 18:12:35 +0330

    some bugs fixed

--- include/optimizations/lamb.h
+++ include/optimizations/lamb.h
@@ -2,49 +2,50 @@
 
 
 #include "common.h"
+namespace xt::optim
+{
+    // --- Options for LAMB Optimizer ---
+    struct LambOptions : torch::optim::OptimizerOptions {
+        explicit LambOptions(double learning_rate = 1e-3)
+            : torch::optim::OptimizerOptions() {
+            this->lr(learning_rate);
+        }
 
-// --- Options for LAMB Optimizer ---
-struct LambOptions : torch::optim::OptimizerOptions {
-    explicit LambOptions(double learning_rate = 1e-3)
-        : torch::optim::OptimizerOptions() {
-        this->lr(learning_rate);
-    }
+        // Standard Adam parameters
+        TORCH_ARG(double, beta1) = 0.9;
+        TORCH_ARG(double, beta2) = 0.999;
+        TORCH_ARG(double, eps) = 1e-6; // LAMB often uses a slightly larger eps
+        TORCH_ARG(double, weight_decay) = 0.01; // LAMB often has a default weight decay
+        TORCH_ARG(double ,  lr) = 1e-6;   // For momentum on the gradient "force"
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
+    };
 
-    // Standard Adam parameters
-    TORCH_ARG(double, beta1) = 0.9;
-    TORCH_ARG(double, beta2) = 0.999;
-    TORCH_ARG(double, eps) = 1e-6; // LAMB often uses a slightly larger eps
-    TORCH_ARG(double, weight_decay) = 0.01; // LAMB often has a default weight decay
-    TORCH_ARG(double ,  lr) = 1e-6;   // For momentum on the gradient "force"
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
-};
+    // --- Parameter State for LAMB Optimizer ---
+    struct LambParamState : torch::optim::OptimizerParamState {
+        TORCH_ARG(torch::Tensor, step);
+        TORCH_ARG(torch::Tensor, exp_avg);      // m_t (EMA of gradients)
+        TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t (EMA of squared gradients)
 
-// --- Parameter State for LAMB Optimizer ---
-struct LambParamState : torch::optim::OptimizerParamState {
-    TORCH_ARG(torch::Tensor, step);
-    TORCH_ARG(torch::Tensor, exp_avg);      // m_t (EMA of gradients)
-    TORCH_ARG(torch::Tensor, exp_avg_sq);   // v_t (EMA of squared gradients)
+        // LambParamState() = default;
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<OptimizerParamState> clone() const override;
+    };
 
-    // LambParamState() = default;
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<OptimizerParamState> clone() const override;
-};
+    // --- LAMB Optimizer Class ---
+    class LAMB : public torch::optim::Optimizer {
+    public:
+        LAMB(std::vector<torch::Tensor> params, LambOptions options);
+        explicit LAMB(std::vector<torch::Tensor> params, double lr = 1e-3);
 
-// --- LAMB Optimizer Class ---
-class LAMB : public torch::optim::Optimizer {
-public:
-    LAMB(std::vector<torch::Tensor> params, LambOptions options);
-    explicit LAMB(std::vector<torch::Tensor> params, double lr = 1e-3);
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+        void save(torch::serialize::OutputArchive& archive) const override;
+        void load(torch::serialize::InputArchive& archive) override;
 
-    using LossClosure = std::function<torch::Tensor()>;
-    torch::Tensor step(LossClosure closure = nullptr) override;
-    void save(torch::serialize::OutputArchive& archive) const override;
-    void load(torch::serialize::InputArchive& archive) override;
-
-protected:
-    std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
-};
-
+    protected:
+        std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
+    };
+}
