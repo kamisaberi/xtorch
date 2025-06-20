c73325313fdfce18392653b8ef8d99cce87398e7
kamisaberi <kamisaberi@gmail.com>
2025-06-20 18:08:14 +0330

    some bugs fixed

--- include/optimizations/demon.h
+++ include/optimizations/demon.h
@@ -2,51 +2,53 @@
 
 
 #include "common.h"
-// --- Options for Demon Optimizer ---
-struct DemonOptions : torch::optim::OptimizerOptions {
-    explicit DemonOptions(double learning_rate = 0.1) // SGD-like LR
-        : torch::optim::OptimizerOptions() {
-        this->lr(learning_rate);
-    }
+namespace xt::optim
+{
+    // --- Options for Demon Optimizer ---
+    struct DemonOptions : torch::optim::OptimizerOptions {
+        explicit DemonOptions(double learning_rate = 0.1) // SGD-like LR
+            : torch::optim::OptimizerOptions() {
+            this->lr(learning_rate);
+        }
 
-    // The momentum will decay from beta_initial to beta_final
-    TORCH_ARG(double, beta_initial) = 0.99;
-    TORCH_ARG(double, beta_final) = 0.5;
+        // The momentum will decay from beta_initial to beta_final
+        TORCH_ARG(double, beta_initial) = 0.99;
+        TORCH_ARG(double, beta_final) = 0.5;
 
-    // The total number of steps over which the decay occurs
-    TORCH_ARG(long, total_steps) = 100000;
+        // The total number of steps over which the decay occurs
+        TORCH_ARG(long, total_steps) = 100000;
 
-    TORCH_ARG(double, weight_decay) = 1e-4;
-    TORCH_ARG(double ,  lr) = 1e-6;
+        TORCH_ARG(double, weight_decay) = 1e-4;
+        TORCH_ARG(double ,  lr) = 1e-6;
 
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
-};
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<torch::optim::OptimizerOptions> clone() const override;
+    };
 
-// --- Parameter State for Demon Optimizer ---
-struct DemonParamState : torch::optim::OptimizerParamState {
-    TORCH_ARG(torch::Tensor, step);
-    TORCH_ARG(torch::Tensor, momentum_buffer);
+    // --- Parameter State for Demon Optimizer ---
+    struct DemonParamState : torch::optim::OptimizerParamState {
+        TORCH_ARG(torch::Tensor, step);
+        TORCH_ARG(torch::Tensor, momentum_buffer);
 
-    // DemonParamState() = default;
-    void serialize(torch::serialize::OutputArchive& archive) const override;
-    void deserialize(torch::serialize::InputArchive& archive) ;
-    std::unique_ptr<OptimizerParamState> clone() const override;
-};
+        // DemonParamState() = default;
+        void serialize(torch::serialize::OutputArchive& archive) const override;
+        void deserialize(torch::serialize::InputArchive& archive) ;
+        std::unique_ptr<OptimizerParamState> clone() const override;
+    };
 
-// --- Demon Optimizer Class ---
-class Demon : public torch::optim::Optimizer {
-public:
-    Demon(std::vector<torch::Tensor> params, DemonOptions options);
-    explicit Demon(std::vector<torch::Tensor> params, double lr = 0.1);
+    // --- Demon Optimizer Class ---
+    class Demon : public torch::optim::Optimizer {
+    public:
+        Demon(std::vector<torch::Tensor> params, DemonOptions options);
+        explicit Demon(std::vector<torch::Tensor> params, double lr = 0.1);
 
-    using LossClosure = std::function<torch::Tensor()>;
-    torch::Tensor step(LossClosure closure = nullptr) override;
-    void save(torch::serialize::OutputArchive& archive) const override;
-    void load(torch::serialize::InputArchive& archive) override;
+        using LossClosure = std::function<torch::Tensor()>;
+        torch::Tensor step(LossClosure closure = nullptr) override;
+        void save(torch::serialize::OutputArchive& archive) const override;
+        void load(torch::serialize::InputArchive& archive) override;
 
-protected:
-    std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
-};
-
+    protected:
+        std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
+    };
+}
