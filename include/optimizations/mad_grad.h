8db6b3b7cb9399d67969efd74829dd2ab9b7daed
kamisaberi <kamisaberi@gmail.com>
2025-06-20 17:56:27 +0330

    some bugs fixed

--- include/optimizations/mad_grad.h
+++ include/optimizations/mad_grad.h
@@ -1,14 +1,7 @@
-#ifndef MADGRAD_OPTIMIZER_HPP
-#define MADGRAD_OPTIMIZER_HPP
+#pragma once
 
-#include <torch/torch.h>
-#include <torch/serialize/archive.h>
 
-#include <cmath>
-#include <vector>
-#include <memory>
-#include <string>
-#include <cstdint>
+#include "common.h"
 
 // --- Options for MADGRAD Optimizer ---
 struct MADGRADOptions : torch::optim::OptimizerOptions {
@@ -56,4 +49,3 @@ protected:
     std::unique_ptr<torch::optim::OptimizerParamState> make_param_state() ;
 };
 
-#endif // MADGRAD_OPTIMIZER_HPP
