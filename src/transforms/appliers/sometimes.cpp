a26049b079dd62f2f72a6630be9523904f3ba203
kamisaberi <kamisaberi@gmail.com>
2025-06-25 22:29:05 +0330

    Sometimes applier tagged as 'is not yet implemented.'

--- src/transforms/appliers/sometimes.cpp
+++ src/transforms/appliers/sometimes.cpp
@@ -1,20 +1,20 @@
-
 #include "include/transforms/appliers/sometimes.h"
 
-namespace xt::transforms {
+namespace xt::transforms
+{
     Sometimes::Sometimes() = default;
 
-    Sometimes::Sometimes(std::vector <xt::Module> transforms) : xt::Module(), transforms(std::move(transforms)) {
+    Sometimes::Sometimes(std::vector<xt::Module> transforms) : xt::Module(), transforms(std::move(transforms))
+    {
+        throw std::runtime_error("Sometimes applier is not yet implemented.");
     }
 
-    auto Sometimes::forward(std::initializer_list <std::any> tensors) -> std::any {
+    auto Sometimes::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
         // std::vector <torch::Tensor> tensor_vec(tensors);
         // torch::Tensor input = tensor_vec[0];
         // int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
         // input = std::any_cast<torch::Tensor>(transform[index]({input}));
         // return input;
-
     }
-
-
 }
