f11180c9b7b972b1ecb6586c67401c8ba1746983
kamisaberi <kamisaberi@gmail.com>
2025-06-25 22:27:06 +0330

    ReplayCompose applier tagged as 'is not yet implemented.'

--- src/transforms/appliers/replay_compose.cpp
+++ src/transforms/appliers/replay_compose.cpp
@@ -1,21 +1,20 @@
-
 #include "include/transforms/appliers/replay_compose.h"
 
 namespace xt::transforms
 {
-
     ReplayCompose::ReplayCompose() = default;
 
     ReplayCompose::ReplayCompose(std::vector<xt::Module> transforms): xt::Module(), transforms(std::move(transforms))
     {
+        throw std::runtime_error("ReplayCompose applier is not yet implemented.");
     }
-    auto ReplayCompose::forward(std::initializer_list<std::any> tensors) -> std::any {
+
+    auto ReplayCompose::forward(std::initializer_list<std::any> tensors) -> std::any
+    {
         // std::vector <torch::Tensor> tensor_vec(tensors);
         // torch::Tensor input = tensor_vec[0];
         // int index = torch::randint(0, transforms.size() - 1, {}, torch::kInt32).item<int>();
         // input = std::any_cast<torch::Tensor>(transforms[index]({input}));
         // return input;
-
-
     }
 }
