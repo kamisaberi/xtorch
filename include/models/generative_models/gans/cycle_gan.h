d33eb8ef96d153116993f10996b4fccd0373ccc2
kamran saberifard <kamisaberi@gmail.com>
2025-07-05 19:40:54 +0330

    BigGAN model finished

--- include/models/generative_models/gans/cycle_gan.h
+++ include/models/generative_models/gans/cycle_gan.h
@@ -10,12 +10,22 @@ namespace xt::models
     private:
 
     public:
+        struct ResidualBlock : torch::nn::Module
+        {
+        public:
+            ResidualBlock(int64_t channels);
+            torch::Tensor forward(torch::Tensor x);
+
+        private:
+            torch::nn::Conv2d conv1, conv2;
+            torch::nn::InstanceNorm2d norm1, norm2;
+        };
+
         CycleGAN(int num_classes /* classes */, int in_channels = 3/* input channels */);
 
         CycleGAN(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
         void reset() override;
     };
-
 }
