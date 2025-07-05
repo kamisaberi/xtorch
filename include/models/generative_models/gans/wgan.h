b8f2133bcab502c34d150426b9408f846beb0344
kamran saberifard <kamisaberi@gmail.com>
2025-07-05 22:44:30 +0330

    WGAN model finished

--- include/models/generative_models/gans/wgan.h
+++ include/models/generative_models/gans/wgan.h
@@ -27,29 +27,9 @@ namespace xt::models
         // Critic Network
         struct CriticImpl : torch::nn::Module
         {
-            CriticImpl()
-            {
-                conv1 = register_module("conv1", torch::nn::Conv2d(
-                                            torch::nn::Conv2dOptions(1, 64, 4).stride(2).padding(1)));
-                conv2 = register_module("conv2", torch::nn::Conv2d(
-                                            torch::nn::Conv2dOptions(64, 128, 4).stride(2).padding(1)));
-                conv3 = register_module("conv3", torch::nn::Conv2d(
-                                            torch::nn::Conv2dOptions(128, 256, 4).stride(2).padding(1)));
-                fc = register_module("fc", torch::nn::Linear(256 * 3 * 3, 1));
-                lrelu = register_module("lrelu", torch::nn::LeakyReLU(
-                                            torch::nn::LeakyReLUOptions().negative_slope(0.2)));
-            }
+            CriticImpl();
 
-            torch::Tensor forward(torch::Tensor x)
-            {
-                x = lrelu->forward(conv1->forward(x)); // [batch, 64, 14, 14]
-                x = lrelu->forward(conv2->forward(x)); // [batch, 128, 7, 7]
-                x = lrelu->forward(conv3->forward(x)); // [batch, 256, 3, 3]
-                auto batch_size = x.size(0);
-                x = x.view({batch_size, -1}); // [batch, 256 * 3 * 3]
-                x = fc->forward(x); // [batch, 1]
-                return x;
-            }
+            torch::Tensor forward(torch::Tensor x);
 
             torch::nn::Conv2d conv1{nullptr}, conv2{nullptr}, conv3{nullptr};
             torch::nn::Linear fc{nullptr};
