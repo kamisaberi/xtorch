d433ae627951f4c5a9eb28597f1a2d82e7d88c00
kamran saberifard <kamisaberi@gmail.com>
2025-07-05 21:43:26 +0330

    ProGAN model finished

--- include/models/generative_models/gans/pro_gan.h
+++ include/models/generative_models/gans/pro_gan.h
@@ -55,74 +55,9 @@ namespace xt::models
         // Discriminator
         struct ProGANDiscriminatorImpl : torch::nn::Module
         {
-            ProGANDiscriminatorImpl()
-            {
-                from_img_28x28 = register_module("from_img_28x28", torch::nn::Conv2d(
-                                                     torch::nn::Conv2dOptions(1, 32, 3).stride(1).padding(1)));
-                from_img_16x16 = register_module("from_img_16x16", torch::nn::Conv2d(
-                                                     torch::nn::Conv2dOptions(1, 64, 1).stride(1)));
-                from_img_8x8 = register_module("from_img_8x8", torch::nn::Conv2d(
-                                                   torch::nn::Conv2dOptions(1, 128, 1).stride(1)));
-                from_img_4x4 = register_module("from_img_4x4", torch::nn::Conv2d(
-                                                   torch::nn::Conv2dOptions(1, 256, 1).stride(1)));
-                block3 = register_module("block3", DiscriminatorBlock(32, 64)); // 28x28 -> 16x16
-                block2 = register_module("block2", DiscriminatorBlock(64, 128)); // 16x16 -> 8x8
-                block1 = register_module("block1", DiscriminatorBlock(128, 256)); // 8x8 -> 4x4
-                final = register_module("final", torch::nn::Linear(256 * 4 * 4, 1));
-                lrelu = register_module("lrelu", torch::nn::LeakyReLU(
-                                            torch::nn::LeakyReLUOptions().negative_slope(0.2)));
-            }
+            ProGANDiscriminatorImpl();
 
-            torch::Tensor forward(torch::Tensor x, int resolution_level, float alpha)
-            {
-                auto batch_size = x.size(0);
-
-                if (resolution_level == 0)
-                {
-                    x = lrelu->forward(from_img_4x4->forward(x)); // [batch, 256, 4, 4]
-                }
-                else if (resolution_level == 1 || resolution_level == 2)
-                {
-                    x = lrelu->forward(from_img_8x8->forward(x)); // [batch, 128, 8, 8]
-                    if (resolution_level == 2)
-                    {
-                        auto x_prev = lrelu->forward(from_img_4x4->forward(torch::avg_pool2d(x, 2)));
-                        x = (1 - alpha) * x_prev + alpha * x; // Fade-in
-                    }
-                    x = block1->forward(x); // [batch, 256, 4, 4]
-                }
-                else if (resolution_level == 3 || resolution_level == 4)
-                {
-                    x = lrelu->forward(from_img_16x16->forward(x)); // [batch, 64, 16, 16]
-                    if (resolution_level == 4)
-                    {
-                        auto x_prev = lrelu->forward(from_img_8x8->forward(torch::avg_pool2d(x, 2)));
-                        x = (1 - alpha) * x_prev + alpha * x; // Fade-in
-                    }
-                    x = block2->forward(x); // [batch, 128, 8, 8]
-                    x = block1->forward(x); // [batch, 256, 4, 4]
-                }
-                else if (resolution_level == 5 || resolution_level == 6)
-                {
-                    x = lrelu->forward(from_img_28x28->forward(x)); // [batch, 32, 28, 28]
-                    if (resolution_level == 6)
-                    {
-                        auto x_prev = lrelu->forward(from_img_16x16->forward(torch::avg_pool2d(x, 2)));
-                        x = (1 - alpha) * x_prev + alpha * x; // Fade-in
-                    }
-                    x = block3->forward(x); // [batch, 64, 16, 16]
-                    x = block2->forward(x); // [batch, 128, 8, 8]
-                    x = block1->forward(x); // [batch, 256, 4, 4]
-                }
-                else
-                {
-                    throw std::runtime_error("Invalid resolution level");
-                }
-
-                x = x.view({batch_size, -1}); // [batch, 256 * 4 * 4]
-                x = final->forward(x); // [batch, 1]
-                return x;
-            }
+            torch::Tensor forward(torch::Tensor x, int resolution_level, float alpha);
 
             torch::nn::Conv2d from_img_28x28{nullptr}, from_img_16x16{nullptr}, from_img_8x8{nullptr}, from_img_4x4{
                                   nullptr
