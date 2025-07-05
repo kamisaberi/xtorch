0c376e37115e58306a024ab781e7e17d1406718d
kamran saberifard <kamisaberi@gmail.com>
2025-07-05 21:31:43 +0330

    CycleAN model finished

--- include/models/generative_models/gans/pro_gan.h
+++ include/models/generative_models/gans/pro_gan.h
@@ -25,75 +25,9 @@ namespace xt::models
 
         struct ProGANGeneratorImpl : torch::nn::Module
         {
-            ProGANGeneratorImpl(int latent_dim) : latent_dim_(latent_dim)
-            {
-                initial = register_module("initial", torch::nn::Linear(latent_dim, 256 * 4 * 4));
-                block1 = register_module("block1", GeneratorBlock(256, 128)); // 4x4 -> 8x8
-                block2 = register_module("block2", GeneratorBlock(128, 64)); // 8x8 -> 16x16
-                block3 = register_module("block3", GeneratorBlock(64, 32)); // 16x16 -> 28x28
-                to_img_4x4 = register_module("to_img_4x4", torch::nn::Conv2d(
-                                                 torch::nn::Conv2dOptions(256, 1, 1).stride(1)));
-                to_img_8x8 = register_module("to_img_8x8", torch::nn::Conv2d(
-                                                 torch::nn::Conv2dOptions(128, 1, 1).stride(1)));
-                to_img_16x16 = register_module("to_img_16x16", torch::nn::Conv2d(
-                                                   torch::nn::Conv2dOptions(64, 1, 1).stride(1)));
-                to_img_28x28 = register_module("to_img_28x28", torch::nn::Conv2d(
-                                                   torch::nn::Conv2dOptions(32, 1, 3).stride(1).padding(1)));
-                relu = register_module("relu", torch::nn::ReLU());
-            }
+            ProGANGeneratorImpl(int latent_dim);
 
-            torch::Tensor forward(torch::Tensor z, int resolution_level, float alpha)
-            {
-                auto batch_size = z.size(0);
-                z = relu->forward(initial->forward(z)); // [batch, 256 * 4 * 4]
-                z = z.view({batch_size, 256, 4, 4}); // [batch, 256, 4, 4]
-
-                if (resolution_level == 0)
-                {
-                    return torch::tanh(to_img_4x4->forward(z)); // [batch, 1, 4, 4]
-                }
-
-                z = block1->forward(z); // [batch, 128, 8, 8]
-                if (resolution_level == 1)
-                {
-                    auto img = to_img_8x8->forward(z);
-                    return torch::tanh(img);
-                }
-                else if (resolution_level == 2)
-                {
-                    auto img_prev = torch::upsample_nearest2d(to_img_4x4->forward(block1->forward(z)), {8, 8});
-                    auto img_curr = to_img_8x8->forward(z);
-                    return torch::tanh((1 - alpha) * img_prev + alpha * img_curr); // Fade-in
-                }
-
-                z = block2->forward(z); // [batch, 64, 16, 16]
-                if (resolution_level == 3)
-                {
-                    auto img = to_img_16x16->forward(z);
-                    return torch::tanh(img);
-                }
-                else if (resolution_level == 4)
-                {
-                    auto img_prev = torch::upsample_nearest2d(to_img_8x8->forward(block2->forward(z)), {16, 16});
-                    auto img_curr = to_img_16x16->forward(z);
-                    return torch::tanh((1 - alpha) * img_prev + alpha * img_curr); // Fade-in
-                }
-
-                z = block3->forward(z); // [batch, 32, 28, 28]
-                if (resolution_level == 5)
-                {
-                    auto img = to_img_28x28->forward(z);
-                    return torch::tanh(img);
-                }
-                else if (resolution_level == 6)
-                {
-                    auto img_prev = torch::upsample_nearest2d(to_img_16x16->forward(block2->forward(z)), {28, 28});
-                    auto img_curr = to_img_28x28->forward(z);
-                    return torch::tanh((1 - alpha) * img_prev + alpha * img_curr); // Fade-in
-                }
-
-                throw std::runtime_error("Invalid resolution level");
-            }
+            torch::Tensor forward(torch::Tensor z, int resolution_level, float alpha);
 
             int latent_dim_;
             torch::nn::Linear initial{nullptr};
