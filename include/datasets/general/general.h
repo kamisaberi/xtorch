4b4958df69cbcb96c969de36ba75737fbad46bd2
kamran saberifard <kamisaberi@gmail.com>
2025-07-06 07:58:22 +0330

    time_series_folder.cpp created

--- include/datasets/general/general.h
+++ include/datasets/general/general.h
@@ -2,11 +2,17 @@
 
 
 #include "audio_dataset.h"
+#include "audio_folder.h"
 #include "csv_dataset.h"
+#include "csv_folder.h"
 #include "edf_dataset.h"
+#include "edf_folder.h"
 #include "image_folder.h"
 #include "tensor_dataset.h"
 #include "text_dataset.h"
+#include "text_folder.h"
 #include "time_series_dataset.h"
+#include "time_series_folder.h"
 #include "video_dataset.h"
+#include "video_folder.h"
 #include "paired_image_dataset.h"
