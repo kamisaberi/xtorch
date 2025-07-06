e8b5fe12cc4cf22258fad00cd5a2bd108ad5f820
kamran saberifard <kamisaberi@gmail.com>
2025-07-06 07:50:03 +0330

    time_series_folder.cpp created

--- include/datasets/general/video_dataset.h
+++ include/datasets/general/video_dataset.h
@@ -21,19 +21,6 @@ namespace xt::datasets {
     };
 
 
-    class StackedVideoDataset : public xt::datasets::Dataset {
-    public :
-        StackedVideoDataset(const std::string &folder_path);
-        StackedVideoDataset(const std::string &folder_path,DataMode mode);
-        StackedVideoDataset(const std::string &folder_path,DataMode mode, bool load_sub_folders);
-        StackedVideoDataset(const std::string &folder_path,DataMode mode, bool load_sub_folders , std::unique_ptr<xt::Module> transformer);
-
-    private:
-        vector<string> labels_name;
-        bool load_sub_folders = false;
-        void load_data();
-    };
-
 
 
 }
