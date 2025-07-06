d06dc25edeacbd6b3548abc633772ef111a04c7c
kamran saberifard <kamisaberi@gmail.com>
2025-07-06 07:13:56 +0330

    audio_folder.cpp created

---
+++ include/datasets/general/audio_folder.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "../common.h"
+
+#include <sndfile.hh>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+namespace xt::datasets
+{
+    class AudioFolder : public xt::datasets::Dataset
+    {
+    public:
+        AudioFolder(const std::string& audio_dir, const std::string& label_file)
+        {
+            std::ifstream label_stream(label_file);
+            std::string line;
+            while (std::getline(label_stream, line))
+            {
+                std::istringstream iss(line);
+                std::string filename, label_str;
+                iss >> filename >> label_str;
+                files.push_back(audio_dir + "/" + filename + ".wav");
+                labels.push_back(std::stoi(label_str));
+            }
+        }
+
+        torch::data::Example<> get(size_t index) override
+        {
+            SndfileHandle file(files[index]);
+            std::vector<float> samples(file.frames());
+            file.read(&samples[0], file.frames());
+            auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU);
+            torch::Tensor waveform = torch::from_blob(samples.data(), {1, (int64_t)samples.size()}, options).clone();
+            torch::Tensor label = torch::full({1}, labels[index], torch::kLong);
+            return {waveform, label};
+        }
+
+        torch::optional<size_t> size() const override
+        {
+            return files.size();
+        }
+
+    private:
+        std::vector<std::string> files;
+        std::vector<int> labels;
+    };
+}
