35ed3a46ad50c8bd91d2bac5f7840038a3874fa1
kamran saberifard <kamisaberi@gmail.com>
2025-07-08 03:13:02 +0330

    download_from_gdrive function changed to download_from_google_drive

--- src/datasets/computer_vision/image_classification/celeba.cpp
+++ src/datasets/computer_vision/image_classification/celeba.cpp
@@ -38,6 +38,41 @@ namespace xt::datasets
         load_data();
     }
 
+    void CelebA::check_resources()
+    {
+        cout << "cr 01" << endl;
+        fs::path pth = this->root / dataset_folder_name / images_folder;
+
+        cout << "cr 02" << endl;
+        bool should_download = true;
+        if (fs::exists(pth))
+        {
+            cout << "cr 03" << endl;
+            auto cnt = xt::utils::fs::countFiles(pth);
+            if (cnt < 202'599)
+            {
+                cout << "cr 0301" << endl;
+                fs::remove_all(pth);
+                fs::remove_all(this->dataset_path);
+                should_download = true;
+            }
+        }
+        cout << "cr 04" << endl;
+        if (should_download)
+        {
+            cout << "cr 0400" << endl;
+            fs::create_directories(this->dataset_path);
+            for (auto resource : resources)
+            {
+                cout << "cr 0401"  << endl;
+                auto [file_id , md5_hash , file_name] = resource;
+                fs::path abs_path = this->dataset_path / file_name;
+                xt::utils::download_from_google_drive(file_id, md5_hash, abs_path.string());
+            }
+        }
+        cout << "cr 05" << endl;
+    }
+
 
     void CelebA::load_data()
     {
@@ -53,31 +88,6 @@ namespace xt::datasets
                 // torch::Tensor tensor = xt::utils::image::convertImageToTensor(file.path());
                 // data.push_back(tensor);
                 targets.push_back(0);
-            }
-        }
-    }
-
-    void CelebA::check_resources()
-    {
-        fs::path pth = this->root / dataset_folder_name / images_folder;
-
-        bool should_download = false;
-        if (fs::exists(pth))
-        {
-            int cnt = xt::utils::fs::countFiles(pth);
-            if (cnt < 202'599)
-            {
-                fs::remove_all(pth);
-                should_download = true;
-            }
-        }
-        if (should_download)
-        {
-            for (auto resource : resources)
-            {
-                auto [file_id , md5_hash , file_name] = resource;
-                fs::path abs_path = this->dataset_path / file_name;
-                xt::utils::download_from_google_drive(file_id, md5_hash, abs_path.string());
             }
         }
     }
