e6e5537476a2568637b32c484199f8754881a09b
kamran saberifard <kamisaberi@gmail.com>
2025-07-08 03:54:08 +0330

    download_from_gdrive function changed to download_from_google_drive

--- src/datasets/general/audio_dataset.cpp
+++ src/datasets/general/audio_dataset.cpp
@@ -1,10 +1,34 @@
-
 #include "include/datasets/general/audio_dataset.h"
 
-namespace xt::datasets {
+namespace xt::datasets
+{
+    AudioDataset::AudioDataset(const std::string& audio_dir, const std::string& label_file)
+    {
+        std::ifstream label_stream(label_file);
+        std::string line;
+        while (std::getline(label_stream, line))
+        {
+            std::istringstream iss(line);
+            std::string filename, label_str;
+            iss >> filename >> label_str;
+            files.push_back(audio_dir + "/" + filename + ".wav");
+            labels.push_back(std::stoi(label_str));
+        }
+    }
 
+    torch::data::Example<> AudioDataset::get(size_t index) override
+    {
+        SndfileHandle file(files[index]);
+        std::vector<float> samples(file.frames());
+        file.read(&samples[0], file.frames());
+        auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU);
+        torch::Tensor waveform = torch::from_blob(samples.data(), {1, (int64_t)samples.size()}, options).clone();
+        torch::Tensor label = torch::full({1}, labels[index], torch::kLong);
+        return {waveform, label};
+    }
 
-
-
-
+    torch::optional<size_t> AudioDataset::size() const override
+    {
+        return files.size();
+    }
 }
