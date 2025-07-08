f97e65ecf961bfe6a452f40c3f21bbd29dc8e851
kamran saberifard <kamisaberi@gmail.com>
2025-07-08 13:52:32 +0330

    some models created

--- include/models/computer_vision/image_classification/dense_net.h
+++ include/models/computer_vision/image_classification/dense_net.h
@@ -60,55 +60,11 @@ namespace xt::models {
     TORCH_MODULE(DenseNet121);
 
 
-
     // DenseNet169
     struct DenseNet169Impl : torch::nn::Module {
-        DenseNet169Impl(int num_classes = 10, int growth_rate = 32, int init_channels = 64) {
-            // Initial conv layer
-            conv0 = register_module("conv0", torch::nn::Conv2d(
-                    torch::nn::Conv2dOptions(3, init_channels, 3).stride(1).padding(1).bias(false)));
-            bn0 = register_module("bn0", torch::nn::BatchNorm2d(init_channels));
+        DenseNet169Impl(int num_classes = 10, int growth_rate = 32, int init_channels = 64);
 
-            // Dense blocks and transition layers
-            int num_features = init_channels;
-            dense1 = register_module("dense1", DenseBlock(/*num_layers*/6, num_features, growth_rate));
-            num_features += 6 * growth_rate;
-            trans1 = register_module("trans1", TransitionLayer(num_features, num_features / 2));
-            num_features /= 2;
-
-            dense2 = register_module("dense2", DenseBlock(/*num_layers*/12, num_features, growth_rate));
-            num_features += 12 * growth_rate;
-            trans2 = register_module("trans2", TransitionLayer(num_features, num_features / 2));
-            num_features /= 2;
-
-            dense3 = register_module("dense3", DenseBlock(/*num_layers*/32, num_features, growth_rate));
-            num_features += 32 * growth_rate;
-            trans3 = register_module("trans3", TransitionLayer(num_features, num_features / 2));
-            num_features /= 2;
-
-            dense4 = register_module("dense4", DenseBlock(/*num_layers*/32, num_features, growth_rate));
-            num_features += 32 * growth_rate;
-
-            // Final layers
-            bn_final = register_module("bn_final", torch::nn::BatchNorm2d(num_features));
-            fc = register_module("fc", torch::nn::Linear(num_features, num_classes));
-        }
-
-        torch::Tensor forward(torch::Tensor x) {
-            x = torch::relu(bn0->forward(conv0->forward(x))); // [batch, 64, 32, 32]
-            x = dense1->forward(x);
-            x = trans1->forward(x); // [batch, num_features/2, 16, 16]
-            x = dense2->forward(x);
-            x = trans2->forward(x); // [batch, num_features/2, 8, 8]
-            x = dense3->forward(x);
-            x = trans3->forward(x); // [batch, num_features/2, 4, 4]
-            x = dense4->forward(x);
-            x = torch::relu(bn_final->forward(x));
-            x = torch::avg_pool2d(x, x.size(2)); // Global avg pool: [batch, num_features, 1, 1]
-            x = x.view({x.size(0), -1}); // [batch, num_features]
-            x = fc->forward(x); // [batch, num_classes]
-            return x;
-        }
+        torch::Tensor forward(torch::Tensor x);
 
         torch::nn::Conv2d conv0{nullptr};
         torch::nn::BatchNorm2d bn0{nullptr}, bn_final{nullptr};
@@ -118,7 +74,6 @@ namespace xt::models {
     };
 
     TORCH_MODULE(DenseNet169);
-
 
 
     struct DenseNet121 : xt::Cloneable<DenseNet121> {
