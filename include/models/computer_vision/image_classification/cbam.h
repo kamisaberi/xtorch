e4a5b5a51e46959a07fcb83190020a5a0d445fca
kamran saberifard <kamisaberi@gmail.com>
2025-07-08 22:22:33 +0330

    some models created

--- include/models/computer_vision/image_classification/cbam.h
+++ include/models/computer_vision/image_classification/cbam.h
@@ -5,10 +5,11 @@
 
 using namespace std;
 
-namespace xt::models {
-
+namespace xt::models
+{
     // Channel Attention Module
-    struct ChannelAttentionImpl : torch::nn::Module {
+    struct ChannelAttentionImpl : torch::nn::Module
+    {
         ChannelAttentionImpl(int channels, int reduction = 16);
 
         torch::Tensor forward(torch::Tensor x);
@@ -19,8 +20,9 @@ namespace xt::models {
     TORCH_MODULE(ChannelAttention);
 
     // Spatial Attention Module
-    struct SpatialAttentionImpl : torch::nn::Module {
-        SpatialAttentionImpl(int channels) ;
+    struct SpatialAttentionImpl : torch::nn::Module
+    {
+        SpatialAttentionImpl(int channels);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -30,7 +32,8 @@ namespace xt::models {
     TORCH_MODULE(SpatialAttention);
 
     // CBAM Module
-    struct CBAMImpl : torch::nn::Module {
+    struct CBAMImpl : torch::nn::Module
+    {
         CBAMImpl(int channels, int reduction = 16);
 
         torch::Tensor forward(torch::Tensor x);
@@ -42,10 +45,11 @@ namespace xt::models {
     TORCH_MODULE(CBAM);
 
     // CNN with CBAM
-    struct CBAMNetImpl : torch::nn::Module {
+    struct CBAMNetImpl : torch::nn::Module
+    {
         CBAMNetImpl(int in_channels, int num_classes);
 
-        torch::Tensor forward(torch::Tensor x) ;
+        torch::Tensor forward(torch::Tensor x);
 
         torch::nn::Conv2d conv1{nullptr}, conv2{nullptr}, conv3{nullptr};
         torch::nn::BatchNorm2d bn1{nullptr}, bn2{nullptr}, bn3{nullptr};
@@ -57,15 +61,16 @@ namespace xt::models {
     TORCH_MODULE(CBAMNet);
 
 
-    struct CBAM : xt::Cloneable<CBAM> {
+    struct CBAM : xt::Cloneable<CBAM>
+    {
     protected:
 
     public:
         explicit CBAM(int num_classes/* classes */, int in_channels = 1/*  input channels */);
 
-        CBAM(int num_classes, int in_channels, std::vector <int64_t> input_shape);
+        CBAM(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
-        auto forward(std::initializer_list <std::any> tensors) -> std::any override;
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
         void reset() override;
     };
