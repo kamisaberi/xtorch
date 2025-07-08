4939fa4d8b34c77d7636eff4bb8796c0ce4001fc
kamran saberifard <kamisaberi@gmail.com>
2025-07-08 23:57:12 +0330

    some models created

--- include/models/computer_vision/image_classification/highway_network.h
+++ include/models/computer_vision/image_classification/highway_network.h
@@ -28,18 +28,18 @@ namespace xt::models {
     TORCH_MODULE(HighwayNetwork);
 
 
-    struct HighwayNetwork : xt::Cloneable<HighwayNetwork> {
-    private:
-
-    public:
-        HighwayNetwork(int num_classes /* classes */, int in_channels = 3/* input channels */);
-
-        HighwayNetwork(int num_classes, int in_channels, std::vector <int64_t> input_shape);
-
-        auto forward(std::initializer_list <std::any> tensors) -> std::any override;
-
-        void reset() override;
-    };
+    // struct HighwayNetwork : xt::Cloneable<HighwayNetwork> {
+    // private:
+    //
+    // public:
+    //     HighwayNetwork(int num_classes /* classes */, int in_channels = 3/* input channels */);
+    //
+    //     HighwayNetwork(int num_classes, int in_channels, std::vector <int64_t> input_shape);
+    //
+    //     auto forward(std::initializer_list <std::any> tensors) -> std::any override;
+    //
+    //     void reset() override;
+    // };
 
 
 }
