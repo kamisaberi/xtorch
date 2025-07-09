5308f961daf843dbb60899b168b215808771b71f
kamran saberifard <kamisaberi@gmail.com>
2025-07-09 23:53:10 +0330

    efficient_net models created

--- include/models/computer_vision/image_classification/inception.h
+++ include/models/computer_vision/image_classification/inception.h
@@ -9,10 +9,10 @@ using namespace std;
 namespace xt::models
 {
     // Inception Module
-    struct InceptionModuleImpl : torch::nn::Module
+    struct InceptionModule : xt::Module
     {
-        InceptionModuleImpl(int in_channels, int ch1x1, int ch3x3_reduce, int ch3x3, int ch5x5_reduce, int ch5x5,
-                            int pool_proj);
+        InceptionModule(int in_channels, int ch1x1, int ch3x3_reduce, int ch3x3, int ch5x5_reduce, int ch5x5,
+                        int pool_proj);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -23,13 +23,13 @@ namespace xt::models
         torch::nn::MaxPool2d pool{nullptr};
     };
 
-    TORCH_MODULE(InceptionModule);
+    // TORCH_MODULE(InceptionModule);
 
 
     // Inception-A Module (Basic Inception block with factorized convolutions)
-    struct InceptionAModuleImpl : torch::nn::Module
+    struct InceptionAModule : xt::Module
     {
-        InceptionAModuleImpl(int in_channels, int pool_features);
+        InceptionAModule(int in_channels, int pool_features);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -42,12 +42,12 @@ namespace xt::models
         torch::nn::AvgPool2d branch_pool{nullptr};
     };
 
-    TORCH_MODULE(InceptionAModule);
+    // TORCH_MODULE(InceptionAModule);
 
     // Inception-B Module (Grid size reduction with factorized convolutions)
-    struct InceptionBModuleImpl : torch::nn::Module
+    struct InceptionBModule : xt::Module
     {
-        InceptionBModuleImpl(int in_channels);
+        InceptionBModule(int in_channels);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -56,12 +56,12 @@ namespace xt::models
         torch::nn::BatchNorm2d bn3x3_1{nullptr}, bn3x3_2{nullptr}, bn3x3_3{nullptr};
     };
 
-    TORCH_MODULE(InceptionBModule);
+    // TORCH_MODULE(InceptionBModule);
 
     // Inception-C Module (Asymmetric convolutions: nx1 and 1xn)
-    struct InceptionCModuleImpl : torch::nn::Module
+    struct InceptionCModule : xt::Module
     {
-        InceptionCModuleImpl(int in_channels, int channels_7x7);
+        InceptionCModule(int in_channels, int channels_7x7);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -74,13 +74,13 @@ namespace xt::models
         torch::nn::AvgPool2d branch_pool{nullptr};
     };
 
-    TORCH_MODULE(InceptionCModule);
+    // TORCH_MODULE(InceptionCModule);
 
 
     // Auxiliary Classifier
-    struct AuxClassifierImpl : torch::nn::Module
+    struct AuxClassifier : xt::Module
     {
-        AuxClassifierImpl(int in_channels, int num_classes);
+        AuxClassifier(int in_channels, int num_classes);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -90,13 +90,13 @@ namespace xt::models
         torch::nn::AvgPool2d pool{nullptr};
     };
 
-    TORCH_MODULE(AuxClassifier);
+    // TORCH_MODULE(AuxClassifier);
 
 
     // Reduction-A Module
-    struct ReductionAModuleImpl : torch::nn::Module
+    struct ReductionAModule : xt::Module
     {
-        ReductionAModuleImpl(int in_channels, int k, int l, int m, int n);
+        ReductionAModule(int in_channels, int k, int l, int m, int n);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -105,12 +105,12 @@ namespace xt::models
         torch::nn::BatchNorm2d bn3x3{nullptr}, bn3x3dbl_1{nullptr}, bn3x3dbl_2{nullptr}, bn3x3dbl_3{nullptr};
     };
 
-    TORCH_MODULE(ReductionAModule);
+    // TORCH_MODULE(ReductionAModule);
 
     // Reduction-B Module
-    struct ReductionBModuleImpl : torch::nn::Module
+    struct ReductionBModule : xt::Module
     {
-        ReductionBModuleImpl(int in_channels);
+        ReductionBModule(int in_channels);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -121,13 +121,13 @@ namespace xt::models
         torch::nn::BatchNorm2d bn7x7_1{nullptr}, bn7x7_2{nullptr}, bn7x7_3{nullptr}, bn7x7_4{nullptr};
     };
 
-    TORCH_MODULE(ReductionBModule);
+    // TORCH_MODULE(ReductionBModule);
 
 
     // InceptionV1 (GoogLeNet)
-    struct InceptionV1Impl : torch::nn::Module
+    struct InceptionV1 : xt::Module
     {
-        InceptionV1Impl(int num_classes = 10);
+        InceptionV1(int num_classes = 10);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -137,17 +137,17 @@ namespace xt::models
         torch::nn::AdaptiveAvgPool2d avg_pool{nullptr};
         torch::nn::Dropout dropout{nullptr};
         torch::nn::Linear fc{nullptr};
-        InceptionModule inception3a{nullptr}, inception3b{nullptr}, inception4a{nullptr};
-        InceptionModule inception4b{nullptr}, inception4c{nullptr}, inception4d{nullptr};
-        InceptionModule inception4e{nullptr}, inception5a{nullptr}, inception5b{nullptr};
+        std::shared_ptr<InceptionModule> inception3a{nullptr}, inception3b{nullptr}, inception4a{nullptr};
+        std::shared_ptr<InceptionModule> inception4b{nullptr}, inception4c{nullptr}, inception4d{nullptr};
+        std::shared_ptr<InceptionModule> inception4e{nullptr}, inception5a{nullptr}, inception5b{nullptr};
     };
 
-    TORCH_MODULE(InceptionV1);
+    // TORCH_MODULE(InceptionV1);
 
     // InceptionV2
-    struct InceptionV2Impl : torch::nn::Module
+    struct InceptionV2 : xt::Module
     {
-        InceptionV2Impl(int num_classes = 10);
+        InceptionV2(int num_classes = 10);
         torch::Tensor forward(torch::Tensor x);
 
         torch::nn::Conv2d conv1{nullptr}, conv2{nullptr}, conv3{nullptr}, conv4{nullptr}, conv5{nullptr};
@@ -156,19 +156,21 @@ namespace xt::models
         torch::nn::AdaptiveAvgPool2d avg_pool{nullptr};
         torch::nn::Dropout dropout{nullptr};
         torch::nn::Linear fc{nullptr};
-        InceptionAModule inception3a{nullptr}, inception3b{nullptr}, inception3c{nullptr};
-        InceptionBModule inception4a{nullptr}, inception6a{nullptr};
-        InceptionAModule inception5a{nullptr}, inception5b{nullptr}, inception5c{nullptr}, inception5d{nullptr};
-        InceptionCModule inception7a{nullptr}, inception7b{nullptr}, inception7c{nullptr};
+        std::shared_ptr<InceptionAModule> inception3a{nullptr}, inception3b{nullptr}, inception3c{nullptr};
+        std::shared_ptr<InceptionBModule> inception4a{nullptr}, inception6a{nullptr};
+        std::shared_ptr<InceptionAModule> inception5a{nullptr}, inception5b{nullptr}, inception5c{nullptr}, inception5d{
+                                              nullptr
+                                          };
+        std::shared_ptr<InceptionCModule> inception7a{nullptr}, inception7b{nullptr}, inception7c{nullptr};
     };
 
-    TORCH_MODULE(InceptionV2);
+    // TORCH_MODULE(InceptionV2);
 
 
     // InceptionV3
-    struct InceptionV3Impl : torch::nn::Module
+    struct InceptionV3 : xt::Module
     {
-        InceptionV3Impl(int num_classes = 10, bool aux_logits = true) ;
+        InceptionV3(int num_classes = 10, bool aux_logits = true);
 
         std::tuple<torch::Tensor, torch::Tensor> forward(torch::Tensor x);
 
@@ -179,19 +181,20 @@ namespace xt::models
         torch::nn::AdaptiveAvgPool2d avg_pool{nullptr};
         torch::nn::Dropout dropout{nullptr};
         torch::nn::Linear fc{nullptr};
-        InceptionAModule inception_a1{nullptr}, inception_a2{nullptr}, inception_a3{nullptr};
-        InceptionBModule inception_b{nullptr}, inception_d{nullptr};
-        InceptionAModule inception_c1{nullptr}, inception_c2{nullptr}, inception_c3{nullptr}, inception_c4{nullptr};
-        InceptionCModule inception_e1{nullptr}, inception_e2{nullptr};
-        AuxClassifier aux_classifier{nullptr};
+        std::shared_ptr<InceptionAModule> inception_a1{nullptr}, inception_a2{nullptr}, inception_a3{nullptr};
+        std::shared_ptr<InceptionBModule> inception_b{nullptr}, inception_d{nullptr};
+        std::shared_ptr<InceptionAModule> inception_c1{nullptr}, inception_c2{nullptr}, inception_c3{nullptr},
+                                          inception_c4{nullptr};
+        std::shared_ptr<InceptionCModule> inception_e1{nullptr}, inception_e2{nullptr};
+        std::shared_ptr<AuxClassifier> aux_classifier{nullptr};
     };
 
-    TORCH_MODULE(InceptionV3);
+    // TORCH_MODULE(InceptionV3);
 
     // InceptionV4
-    struct InceptionV4Impl : torch::nn::Module
+    struct InceptionV4 : xt::Module
     {
-        InceptionV4Impl(int num_classes = 10);
+        InceptionV4(int num_classes = 10);
 
         torch::Tensor forward(torch::Tensor x);
 
@@ -203,15 +206,17 @@ namespace xt::models
         torch::nn::AdaptiveAvgPool2d avg_pool{nullptr};
         torch::nn::Dropout dropout{nullptr};
         torch::nn::Linear fc{nullptr};
-        InceptionAModule inception_a1{nullptr}, inception_a2{nullptr}, inception_a3{nullptr}, inception_a4{nullptr};
-        ReductionAModule reduction_a{nullptr};
-        InceptionBModule inception_b1{nullptr}, inception_b2{nullptr}, inception_b3{nullptr};
-        InceptionBModule inception_b4{nullptr}, inception_b5{nullptr}, inception_b6{nullptr}, inception_b7{nullptr};
-        ReductionBModule reduction_b{nullptr};
-        InceptionCModule inception_c1{nullptr}, inception_c2{nullptr}, inception_c3{nullptr};
+        std::shared_ptr<InceptionAModule> inception_a1{nullptr}, inception_a2{nullptr}, inception_a3{nullptr},
+                                          inception_a4{nullptr};
+        std::shared_ptr<ReductionAModule> reduction_a{nullptr};
+        std::shared_ptr<InceptionBModule> inception_b1{nullptr}, inception_b2{nullptr}, inception_b3{nullptr};
+        std::shared_ptr<InceptionBModule> inception_b4{nullptr}, inception_b5{nullptr}, inception_b6{nullptr},
+                                          inception_b7{nullptr};
+        std::shared_ptr<ReductionBModule> reduction_b{nullptr};
+        std::shared_ptr<InceptionCModule> inception_c1{nullptr}, inception_c2{nullptr}, inception_c3{nullptr};
     };
 
-    TORCH_MODULE(InceptionV4);
+    // TORCH_MODULE(InceptionV4);
 
 
     // struct InceptionV1 : xt::Cloneable<InceptionV1>
