e908ca6d720ae71bfd0b0957ead66f493955fddc
kamran saberifard <kamisaberi@gmail.com>
2025-07-09 15:49:48 +0330

    some models created

--- include/models/computer_vision/image_classification/amoeba_net.h
+++ include/models/computer_vision/image_classification/amoeba_net.h
@@ -5,14 +5,12 @@
 
 using namespace std;
 
-namespace xt::models
-{
+namespace xt::models {
     // Operation: 3x3 Convolution
-    struct Conv3x3 : xt::Module
-    {
+    struct Conv3x3 : xt::Module {
         Conv3x3(int in_channels, int out_channels);
 
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        auto forward(std::initializer_list <std::any> tensors) -> std::any override;
         torch::Tensor forward(torch::Tensor x);
 
         torch::nn::Conv2d conv{nullptr};
@@ -22,21 +20,20 @@ namespace xt::models
 //    TORCH_MODULE(Conv3x3);
 
     // Operation: 1x1 Convolution
-    struct Conv1x1Impl : torch::nn::Module
-    {
-        Conv1x1Impl(int in_channels, int out_channels);
+    struct Conv1x1 : xt::Module {
+        Conv1x1(int in_channels, int out_channels);
 
+        auto forward(std::initializer_list <std::any> tensors) -> std::any override;
         torch::Tensor forward(torch::Tensor x);
 
         torch::nn::Conv2d conv{nullptr};
         torch::nn::BatchNorm2d bn{nullptr};
     };
 
-    TORCH_MODULE(Conv1x1);
+//    TORCH_MODULE(Conv1x1);
 
     // Operation: 3x3 Max Pool
-    struct MaxPool3x3Impl : torch::nn::Module
-    {
+    struct MaxPool3x3Impl : torch::nn::Module {
         MaxPool3x3Impl();
 
         torch::Tensor forward(torch::Tensor x);
@@ -47,8 +44,7 @@ namespace xt::models
     TORCH_MODULE(MaxPool3x3);
 
     // Normal Cell
-    struct NormalCellImpl : torch::nn::Module
-    {
+    struct NormalCellImpl : torch::nn::Module {
         NormalCellImpl(int prev_channels, int channels);
 
         torch::Tensor forward(torch::Tensor prev, torch::Tensor curr);
@@ -61,8 +57,7 @@ namespace xt::models
     TORCH_MODULE(NormalCell);
 
     // Reduction Cell
-    struct ReductionCellImpl : torch::nn::Module
-    {
+    struct ReductionCellImpl : torch::nn::Module {
         ReductionCellImpl(int prev_channels, int channels);
 
         torch::Tensor forward(torch::Tensor prev, torch::Tensor curr);
@@ -76,8 +71,7 @@ namespace xt::models
     TORCH_MODULE(ReductionCell);
 
     // AmoebaNet-A (Simplified)
-    struct AmoebaNetImpl : torch::nn::Module
-    {
+    struct AmoebaNetImpl : torch::nn::Module {
         AmoebaNetImpl(int in_channels, int num_classes, int channels = 64);
 
         torch::Tensor forward(torch::Tensor x);
@@ -93,19 +87,15 @@ namespace xt::models
     TORCH_MODULE(AmoebaNet);
 
 
-
-
-
-
-    struct AmoabaNet : xt::Cloneable<AmoabaNet>
-    {
+    struct AmoabaNet : xt::Cloneable<AmoabaNet> {
     protected:
 
     public:
         explicit AmoabaNet(int num_classes/* classes */, int in_channels = 1/*  input channels */);
-        AmoabaNet(int num_classes, int in_channels, std::vector<int64_t> input_shape);
 
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        AmoabaNet(int num_classes, int in_channels, std::vector <int64_t> input_shape);
+
+        auto forward(std::initializer_list <std::any> tensors) -> std::any override;
 
         void reset() override;
     };
