f88b6479261381d85c00b12f2de3b921000a2740
kamran saberifard <kamisaberi@gmail.com>
2025-07-10 01:10:30 +0330

    pyramidal_net models created

--- include/models/computer_vision/image_classification/pyramidal_net.h
+++ include/models/computer_vision/image_classification/pyramidal_net.h
@@ -6,34 +6,34 @@
 namespace xt::models {
     // --- The Core Pyramidal Residual Block ---
 
-    struct PyramidalBasicBlockImpl : torch::nn::Module {
+    struct PyramidalBasicBlock : xt::Module {
         torch::nn::Conv2d conv1{nullptr}, conv2_nobn{nullptr};
         torch::nn::BatchNorm2d bn1{nullptr}, bn2{nullptr};
 
         int stride = 1;
 
-        PyramidalBasicBlockImpl(int in_planes, int out_planes, int stride = 1);
+        PyramidalBasicBlock(int in_planes, int out_planes, int stride = 1);
 
         torch::Tensor forward(torch::Tensor x);
     };
 
-    TORCH_MODULE(PyramidalBasicBlock);
+    // TORCH_MODULE(PyramidalBasicBlock);
 
 
     // --- The Full PyramidalNet Model ---
 
-    struct PyramidalNetImpl : torch::nn::Module {
+    struct PyramidalNet : xt::Module {
         torch::nn::Conv2d conv1;
         torch::nn::BatchNorm2d bn1;
         torch::nn::Sequential layer1, layer2, layer3;
         torch::nn::Linear linear;
 
-        PyramidalNetImpl(int N, int alpha, int num_classes = 10);
+        PyramidalNet(int N, int alpha, int num_classes = 10);
 
         torch::Tensor forward(torch::Tensor x);
     };
 
-    TORCH_MODULE(PyramidalNet);
+    // TORCH_MODULE(PyramidalNet);
 
 //    struct PyramidalNet : xt::Cloneable<PyramidalNet>
 //    {
