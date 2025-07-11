8990d7f542b2c88f59e27e85397766281ecaebc3
kamran saberifard <kamisaberi@gmail.com>
2025-07-11 18:25:19 +0330

    general/oversampling transform created

--- include/utils/images.h
+++ include/utils/images.h
@@ -11,7 +11,8 @@ namespace xt::utils::image {
 
     torch::Tensor convertImageToTensor(std::filesystem::path image ,vector<int> size = {0,0});
     torch::Tensor resize(const torch::Tensor &tensor, const std::vector<int64_t> &size);
-
+    cv::Mat tensor_to_mat_local(torch::Tensor tensor) ;
+    torch::Tensor mat_to_tensor_local(const cv::Mat& mat) ;
 }
 
 
