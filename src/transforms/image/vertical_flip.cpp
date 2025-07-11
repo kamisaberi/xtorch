641eefac9decc38f9327b945aead3464a0c58d92
kamran saberifard <kamisaberi@gmail.com>
2025-07-12 00:52:39 +0330

    image/vertical_flip  transform  created

--- src/transforms/image/vertical_flip.cpp
+++ src/transforms/image/vertical_flip.cpp
@@ -1,21 +1,34 @@
 #include "include/transforms/image/vertical_flip.h"
 
+
 namespace xt::transforms::image {
 
+    VerticalFlip::VerticalFlip() = default;
 
+    auto VerticalFlip::forward(std::initializer_list<std::any> tensors) -> std::any {
+        // 1. --- Input Validation ---
+        std::vector<std::any> any_vec(tensors);
+        if (any_vec.empty()) {
+            throw std::invalid_argument("VerticalFlip::forward received an empty list of tensors.");
+        }
+        torch::Tensor input_tensor = std::any_cast<torch::Tensor>(any_vec[0]);
 
-    VerticalFlip::VerticalFlip() {
-    }
-
-    torch::Tensor VerticalFlip::operator()(torch::Tensor input) {
-        int64_t input_dims = input.dim();
-        if (input_dims < 2) {
-            throw std::runtime_error("Input tensor must have at least 2 dimensions (e.g., [H, W]).");
+        if (!input_tensor.defined()) {
+            throw std::invalid_argument("Input tensor passed to VerticalFlip is not defined.");
         }
 
-        // Flip along the second-to-last dimension (height)
-        return torch::flip(input, {-2});
+        // 2. --- Convert to OpenCV Mat ---
+        cv::Mat input_mat = xt::utils::image::tensor_to_mat_local(input_tensor);
+
+        // 3. --- Apply the Vertical Flip operation ---
+        cv::Mat flipped_mat;
+        // A flip code of 0 specifies a flip around the x-axis (vertical).
+        cv::flip(input_mat, flipped_mat, 0);
+
+        // 4. --- Convert back to LibTorch Tensor ---
+        torch::Tensor output_tensor = xt::utils::image::mat_to_tensor_local(flipped_mat);
+
+        return output_tensor;
     }
 
-
-}
+} // namespace xt::transforms::image
