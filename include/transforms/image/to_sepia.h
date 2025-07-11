67df84d2d460ce876f4a7b753db15a74d4ed4877
kamran saberifard <kamisaberi@gmail.com>
2025-07-12 00:46:57 +0330

    image/to_sepia  transform  created

--- include/transforms/image/to_sepia.h
+++ include/transforms/image/to_sepia.h
@@ -3,15 +3,35 @@
 #include "../common.h"
 
 
-namespace xt::transforms::image
-{
-    class ToSepia final : public xt::Module
-    {
+namespace xt::transforms::image {
+
+    /**
+     * @class ToSepia
+     * @brief An image transformation that applies a sepia toning effect.
+     *
+     * This filter gives an image a warm, brownish monochrome appearance, similar
+     * to old photographs. It is achieved by applying a specific linear
+     * transformation to the RGB color channels.
+     */
+    class ToSepia : public xt::Module {
     public:
+        /**
+         * @brief Default constructor.
+         */
         ToSepia();
-        explicit ToSepia(std::vector<xt::Module> transforms);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+        /**
+         * @brief Executes the sepia toning operation.
+         * @param tensors An initializer list expected to contain a single 3D image
+         *                tensor with 3 channels, i.e., shape [3, H, W].
+         * @return An std::any containing the resulting sepia-toned torch::Tensor
+         *         with the same shape and type as the input.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        // The sepia transformation matrix will be a member for efficiency
+        torch::Tensor sepia_matrix_;
     };
-}
+
+} // namespace xt::transforms::image
