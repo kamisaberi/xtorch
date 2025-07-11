39def5daeb703589a979322bcf592cfb13ffe063
kamran saberifard <kamisaberi@gmail.com>
2025-07-12 00:54:06 +0330

    image/zoom_blur  transform  created

--- include/transforms/image/zoom_blur.h
+++ include/transforms/image/zoom_blur.h
@@ -3,15 +3,44 @@
 #include "../common.h"
 
 
-namespace xt::transforms::image
-{
-    class ZoomBlur final : public xt::Module
-    {
+namespace xt::transforms::image {
+
+    /**
+     * @class ZoomBlur
+     * @brief An image transformation that applies a radial "zoom" blur effect.
+     *
+     * This filter simulates the visual effect of zooming a camera during an
+     * exposure. It works by blending multiple scaled versions of the image.
+     * This implementation uses OpenCV for the underlying computation.
+     */
+    class ZoomBlur : public xt::Module {
     public:
+        /**
+         * @brief Default constructor. Uses a moderate zoom factor.
+         */
         ZoomBlur();
-        explicit ZoomBlur(std::vector<xt::Module> transforms);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+
+        /**
+         * @brief Constructs the ZoomBlur transform.
+         * @param max_zoom The maximum zoom factor to use for creating the blur.
+         *                 Values between 1.0 and 3.0 are typical.
+         * @param num_steps The number of scaled images to blend together. More
+         *                  steps create a smoother blur.
+         */
+        ZoomBlur(float max_zoom, int num_steps = 5);
+
+        /**
+         * @brief Executes the zoom blur operation.
+         * @param tensors An initializer list expected to contain a single 3D image
+         *                tensor (C, H, W).
+         * @return An std::any containing the resulting blurred torch::Tensor with
+         *         the same shape and type as the input.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        float max_zoom_;
+        int num_steps_;
     };
-}
+
+} // namespace xt::transforms::image
