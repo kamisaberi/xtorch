b9214d61468bbbb5468135736ca2a091f8953e6c
kamran saberifard <kamisaberi@gmail.com>
2025-07-12 00:52:24 +0330

    image/vertical_flip  transform  created

--- include/transforms/image/vertical_flip.h
+++ include/transforms/image/vertical_flip.h
@@ -1,15 +1,33 @@
-//TODO SHOULD CHANGE
 #pragma once
 #include "../common.h"
 
 
-namespace xt::transforms::image
-{
-    struct VerticalFlip
-    {
+
+namespace xt::transforms::image {
+
+    /**
+     * @class VerticalFlip
+     * @brief An image transformation that flips an image vertically.
+     *
+     * This data augmentation technique flips the image around the horizontal
+     * x-axis (top becomes bottom and vice-versa). This implementation uses
+     * OpenCV's flip function for the underlying computation.
+     */
+    class VerticalFlip : public xt::Module {
     public:
+        /**
+         * @brief Default constructor.
+         */
         VerticalFlip();
 
-        torch::Tensor operator()(torch::Tensor input);
+        /**
+         * @brief Executes the vertical flip operation.
+         * @param tensors An initializer list expected to contain a single 3D image
+         *                tensor (C, H, W).
+         * @return An std::any containing the resulting vertically-flipped
+         *         torch::Tensor with the same shape and type as the input.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
     };
-}
+
+} // namespace xt::transforms::image
