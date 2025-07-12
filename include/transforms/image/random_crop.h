958ff1ffd4716ea00c0bffdc95fde03738d194f2
kamran saberifard <kamisaberi@gmail.com>
2025-07-12 21:39:47 +0330

    image/random_crop transform created

--- include/transforms/image/random_crop.h
+++ include/transforms/image/random_crop.h
@@ -1,31 +1,65 @@
-//TODO SHOULD CHANGE
 #pragma once
 #include "../common.h"
 
 
-namespace xt::transforms::image
-{
-    struct RandomCrop {
-    public:
-        RandomCrop(std::vector<int64_t> size);
 
-        torch::Tensor operator()(torch::Tensor input);
+namespace xt::transforms::image {
+
+    /**
+     * @class RandomCrop
+     * @brief Crops a given image tensor at a random location.
+     *
+     * This transform crops a patch of a specified `size` from the input image.
+     * If the input image is smaller than the crop size, it can be padded first.
+     */
+    class RandomCrop : public xt::Module {
+    public:
+        /**
+         * @brief Default constructor. Does nothing as a crop size is required.
+         *        Provided for completeness but should not be used directly.
+         */
+        RandomCrop();
+
+        /**
+         * @brief Constructs the RandomCrop transform.
+         *
+         * @param size A pair `{height, width}` specifying the desired output size.
+         * @param padding An integer specifying the padding to add to each border
+         *                of the image before cropping. Can be used to make cropping
+         *                from the edges more likely.
+         * @param pad_if_needed A boolean that, if true, will pad the image to the
+         *                      target size if it is smaller than the crop size.
+         * @param fill The constant value used for padding.
+         */
+        explicit RandomCrop(
+            std::pair<int, int> size,
+            int padding = 0,
+            bool pad_if_needed = false,
+            double fill = 0.0
+        );
+
+        /**
+         * @brief Executes the random crop operation.
+         * @param tensors An initializer list expected to contain a single 3D image
+         *                tensor (C, H, W).
+         * @return An std::any containing the resulting cropped torch::Tensor.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
-        std::vector<int64_t> size;
+        // Helper to get the top-left coordinates for the crop
+        static std::pair<int, int> get_crop_params(
+            const torch::Tensor& img,
+            const std::pair<int, int>& output_size,
+            std::mt19937& gen
+        );
+
+        std::pair<int, int> size_;
+        int padding_;
+        bool pad_if_needed_;
+        double fill_;
+
+        std::mt19937 gen_;
     };
 
-
-    struct RandomCrop2 {
-    private:
-        int crop_height;
-        int crop_width;
-
-    public:
-        RandomCrop2(int height, int width);
-
-        torch::Tensor operator()(const torch::Tensor &input_tensor);
-    };
-
-
-}
+} // namespace xt::transforms::image
