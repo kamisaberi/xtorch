673e9e38cc4d8552151f53433ef4d39f5fa9f85e
kamran saberifard <kamisaberi@gmail.com>
2025-07-13 13:39:58 +0330

    graph/edge_add transform created

--- include/transforms/graph/edge_add.h
+++ include/transforms/graph/edge_add.h
@@ -2,16 +2,48 @@
 
 #include "../common.h"
 
+namespace xt::transforms::graph {
 
-namespace xt::transforms::graph
-{
-    class EdgeAdd final : public xt::Module
-    {
+    /**
+     * @class EdgeAdd
+     * @brief A graph transform that randomly adds new edges to the graph.
+     *
+     * This data augmentation technique can help regularize GNN models, especially on
+     * sparse graphs, by creating new message-passing pathways. The transform avoids
+     * adding self-loops or edges that already exist.
+     *
+     * @note For undirected graphs, this transform adds directed edges. The user is
+     *       responsible for adding the reverse edges if symmetry is required.
+     */
+    class EdgeAdd : public xt::Module {
     public:
-        EdgeAdd();
-        explicit EdgeAdd(std::vector<xt::Module> transforms);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        /**
+         * @brief Constructs the EdgeAdd transform.
+         *
+         * @param add_ratio The fraction of new edges to add, relative to the number
+         *                  of existing edges. E.g., 0.1 means adding 10% new edges.
+         * @param p The probability of applying the transform. Defaults to 1.0.
+         */
+        explicit EdgeAdd(
+                double add_ratio = 0.1,
+                double p = 1.0);
+
+        /**
+         * @brief Executes the edge addition operation on a graph.
+         *
+         * @param tensors An initializer list expected to contain two tensors:
+         *                1. `node_features` (torch::Tensor): Shape [num_nodes, num_node_features].
+         *                2. `edge_index` (torch::Tensor): Shape [2, num_edges], type long.
+         * @return An std::any containing a `std::vector<torch::Tensor>`. The vector
+         *         contains the new graph `{node_features, new_edge_index}`. Node
+         *         features are unchanged.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        double add_ratio_;
+        double p_;
+        mutable std::mt19937 random_engine_;
     };
-}
+
+} // namespace xt::transforms::graph
