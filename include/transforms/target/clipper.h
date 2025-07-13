8d756890a80b617ac7781b7bddc2fc5c5ea3328e
kamran saberifard <kamisaberi@gmail.com>
2025-07-14 00:03:39 +0330

    target/clipper transform created

--- include/transforms/target/clipper.h
+++ include/transforms/target/clipper.h
@@ -1,17 +1,41 @@
 #pragma once
 
 #include "../common.h"
+#include <initializer_list>
+#include <any>
 
+namespace xt::transforms::target {
 
-namespace xt::transforms::target
-{
-    class Clipper final : public xt::Module
-    {
+    /**
+     * @class Clipper
+     * @brief A target transformation that clamps or "clips" a numerical label
+     *        to be within a specified range [min, max].
+     *
+     * If the input label is less than the minimum value, it is set to the minimum.
+     * If it is greater than the maximum value, it is set to the maximum. Otherwise,
+     * it remains unchanged.
+     */
+    class Clipper : public xt::Module {
     public:
-        Clipper();
-        explicit Clipper(std::vector<xt::Module> transforms);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        /**
+         * @brief Constructs the Clipper transform.
+         *
+         * @param min_val The minimum allowed value (inclusive).
+         * @param max_val The maximum allowed value (inclusive).
+         */
+        explicit Clipper(long min_val, long max_val);
+
+        /**
+         * @brief Executes the clipping operation.
+         * @param tensors An initializer list expected to contain a single scalar
+         *                integer value (e.g., int, long) representing the label.
+         * @return An std::any containing the clipped label as a `long`.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        long min_val_;
+        long max_val_;
     };
-}
+
+} // namespace xt::transforms::target
