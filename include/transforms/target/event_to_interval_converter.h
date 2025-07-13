2a01422cc9babbb3a3f07fc7af71f245a50fc7ad
kamran saberifard <kamisaberi@gmail.com>
2025-07-14 00:12:15 +0330

    target/event_to_interval_convertor transform created

--- include/transforms/target/event_to_interval_converter.h
+++ include/transforms/target/event_to_interval_converter.h
@@ -3,15 +3,35 @@
 #include "../common.h"
 
 
-namespace xt::transforms::target
-{
-    class EventToIntervalConverter final : public xt::Module
-    {
-    public:
-        EventToIntervalConverter();
-        explicit EventToIntervalConverter(std::vector<xt::Module> transforms);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+#include "../common.h"
+#include <vector>
+#include <any>
 
-    private:
+namespace xt::transforms::target {
+
+    /**
+     * @class EventToIntervalConverter
+     * @brief A target transformation that converts a sequence of event timestamps
+     *        into a sequence of inter-event intervals (durations).
+     *
+     * Given a sorted list of N timestamps, this transform produces a list of
+     * N-1 durations, where each duration is the time between one event and the next.
+     */
+    class EventToIntervalConverter : public xt::Module {
+    public:
+        /**
+         * @brief Default constructor.
+         */
+        EventToIntervalConverter();
+
+        /**
+         * @brief Executes the interval calculation.
+         * @param tensors An initializer list expected to contain a single item:
+         *                a `std::vector<double>` of sorted event timestamps.
+         * @return An std::any containing the calculated intervals as a
+         *         `std::vector<double>`.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
     };
-}
+
+} // namespace xt::transforms::target
