2048561c9cd8a0ac11bf2c3c94d7ea03189ef290
kamran saberifard <kamisaberi@gmail.com>
2025-07-13 15:03:49 +0330

    graph/node_mix_up  transform created

--- include/transforms/graph/node_mix_up.h
+++ include/transforms/graph/node_mix_up.h
@@ -2,16 +2,50 @@
 
 #include "../common.h"
 
+namespace xt::transforms::graph {
 
-namespace xt::transforms::graph
-{
-    class NodeMixUp final : public xt::Module
-    {
+    /**
+     * @class NodeMixUp
+     * @brief A graph transform that creates new node features by interpolating between
+     *        pairs of nodes within the same graph.
+     *
+     * This data augmentation technique generates "virtual" nodes by taking a weighted
+     * average of features from randomly paired nodes. The interpolation weight `lambda`
+     * is sampled from a Beta distribution. This encourages the model to learn
+     * smoother representations.
+     */
+    class NodeMixUp : public xt::Module {
     public:
-        NodeMixUp();
-        explicit NodeMixUp(std::vector<xt::Module> transforms);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        /**
+         * @brief Constructs the NodeMixUp transform.
+         *
+         * @param alpha The concentration parameter for the Beta distribution. Higher
+         *              alpha pushes the interpolation weight `lambda` towards 0.5.
+         * @param mixup_ratio The fraction of nodes to select for mixing. For example,
+         *                    0.2 means 20% of the nodes will be paired up and mixed.
+         * @param p The probability of applying the transform. Defaults to 1.0.
+         */
+        explicit NodeMixUp(
+                double alpha = 0.2,
+                double mixup_ratio = 0.2,
+                double p = 1.0);
+
+        /**
+         * @brief Executes the node mixup operation.
+         *
+         * @param tensors An initializer list expected to contain two tensors:
+         *                1. `node_features` (torch::Tensor): Shape [num_nodes, ...].
+         *                2. `edge_index` (torch::Tensor): Shape [2, num_edges].
+         * @return An std::any containing a `std::vector<torch::Tensor>`. The vector
+         *         contains `{new_node_features, edge_index}`. The edge_index is unchanged.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        double alpha_;
+        double mixup_ratio_;
+        double p_;
+        mutable std::mt19937 random_engine_;
     };
-}
+
+} // namespace xt::transforms::graph
