476a36e2c7b3e035661f93786cfe1b317b623048
kamran saberifard <kamisaberi@gmail.com>
2025-07-14 01:00:49 +0330

    target/log_transformer transform created

--- include/transforms/target/log_transformer.h
+++ include/transforms/target/log_transformer.h
@@ -2,16 +2,33 @@
 
 #include "../common.h"
 
+#include <any>
 
-namespace xt::transforms::target
-{
-    class LogTransformer final : public xt::Module
-    {
+namespace xt::transforms::target {
+
+    /**
+     * @class LogTransformer
+     * @brief A target transformation that applies a natural logarithm (log(1+x))
+     *        to a numerical label.
+     *
+     * This is a variance-stabilizing transformation, often used to handle
+     * right-skewed data (e.g., prices, counts) and make its distribution
+     * more normal. The log(1+x) form is used to gracefully handle inputs of 0.
+     */
+    class LogTransformer : public xt::Module {
     public:
+        /**
+         * @brief Default constructor.
+         */
         LogTransformer();
-        explicit LogTransformer(std::vector<xt::Module> transforms);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
-    private:
+        /**
+         * @brief Executes the log transformation.
+         * @param tensors An initializer list expected to contain a single scalar
+         *                numerical value (e.g., float, double, int).
+         * @return An std::any containing the log-transformed value as a `double`.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
     };
-}
+
+} // namespace xt::transforms::target
