d030fa915b67982a0d4bf0ed13789e0872ad1662
kamran saberifard <kamisaberi@gmail.com>
2025-07-13 15:16:10 +0330

    text/add_token  transform created

--- include/transforms/text/add_token.h
+++ include/transforms/text/add_token.h
@@ -3,15 +3,48 @@
 #include "../common.h"
 
 
-namespace xt::transforms::text
-{
-    class AddToken final : public xt::Module
-    {
+namespace xt::transforms::text {
+
+    /**
+     * @brief An enum to specify where the token should be added.
+     */
+    enum class AddTokenLocation {
+        BEGIN,
+        END
+    };
+
+    /**
+     * @class AddToken
+     * @brief A text transformation that adds a special token to a sequence of tokens.
+     *
+     * This is commonly used to add classification ([CLS]), separator ([SEP]),
+     * or beginning-of-sequence ([BOS]) tokens for language models.
+     */
+    class AddToken : public xt::Module {
     public:
-        AddToken();
-        explicit AddToken(std::vector<xt::Module> transforms);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
+        /**
+         * @brief Constructs the AddToken transform.
+         * @param token The string token to add (e.g., "[CLS]").
+         * @param location Where to add the token in the sequence (BEGIN or END).
+         *                 Defaults to BEGIN.
+         */
+        explicit AddToken(
+                const std::string& token,
+                AddTokenLocation location = AddTokenLocation::BEGIN
+        );
+
+        /**
+         * @brief Executes the token addition operation.
+         * @param tensors An initializer list expected to contain a single
+         *                std::vector<std::string> representing the tokenized text.
+         * @return An std::any containing the resulting std::vector<std::string>
+         *         with the added token.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
+        std::string token_;
+        AddTokenLocation location_;
     };
-}
+
+} // namespace xt::transforms::text
