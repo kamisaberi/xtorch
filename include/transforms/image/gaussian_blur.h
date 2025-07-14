3489073bd1fa326c97ed32ba701ea7140f72ec75
kamran saberifard <kamisaberi@gmail.com>
2025-07-15 01:22:03 +0330

    some bugs fixed

--- include/transforms/image/gaussian_blur.h
+++ include/transforms/image/gaussian_blur.h
@@ -4,32 +4,46 @@
 #include "../common.h"
 
 
-namespace xt::transforms::image
-{
-    struct GaussianBlur
-    {
-    public:
-        GaussianBlur(std::vector<int64_t> kernel_size, float sigma);
+namespace xt::transforms::image {
 
-        torch::Tensor operator()(torch::Tensor input);
+    /**
+     * @class GaussianBlur
+     * @brief Applies a Gaussian blur to an image with fixed parameters.
+     *
+     * This transform blurs an image using a Gaussian filter with a specified
+     * kernel size and standard deviation (sigma). This is a deterministic operation.
+     */
+    class GaussianBlur : public xt::Module {
+    public:
+        /**
+         * @brief Default constructor. Does nothing, parameters are required.
+         */
+        GaussianBlur();
+
+        /**
+         * @brief Constructs the GaussianBlur transform.
+         *
+         * @param kernel_size The size of the Gaussian kernel. Must be a positive, odd integer.
+         * @param sigma A pair `{sigmaX, sigmaY}` for the standard deviation in each
+         *              direction. If sigmaY is 0, it's set to be the same as sigmaX.
+         * @param interpolation Not used in this transform, but kept for API consistency.
+         */
+        explicit GaussianBlur(
+            int kernel_size,
+            std::pair<double, double> sigma = {0.0, 0.0}
+        );
+
+        /**
+         * @brief Executes the Gaussian blur.
+         * @param tensors An initializer list expected to contain a single 3D image
+         *                tensor (C, H, W).
+         * @return An std::any containing the resulting blurred torch::Tensor.
+         */
+        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
-        std::vector<int64_t> kernel_size;
-        float sigma;
-
-        torch::Tensor generate_gaussian_kernel(int64_t k_h, int64_t k_w, float sigma, torch::Device device);
+        int kernel_size_;
+        std::pair<double, double> sigma_;
     };
 
-
-    struct GaussianBlurOpenCV
-    {
-    public:
-        GaussianBlurOpenCV(int ksize, double sigma_val);
-
-        torch::Tensor operator()(const torch::Tensor& input_tensor);
-
-    private:
-        cv::Size kernel_size;
-        double sigma;
-    };
-}
+} // namespace xt::transforms::image
