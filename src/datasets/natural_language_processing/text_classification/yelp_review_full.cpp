c302e4bbf5c1e525fd47b07b70c3247e01697f60
kamran saberifard <kamisaberi@gmail.com>
2025-07-18 20:52:24 +0330

    some bugs fixed

--- src/datasets/natural_language_processing/text_classification/yelp_review_full.cpp
+++ src/datasets/natural_language_processing/text_classification/yelp_review_full.cpp
@@ -1,4 +1,4 @@
-#include "include/datasets/natural_language_processing/text_classification/yelp_review_full.h"
+#include <datasets/natural_language_processing/text_classification/yelp_review_full.h>
 
 
 namespace xt::datasets
