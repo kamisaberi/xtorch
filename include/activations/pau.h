5adb4745f2edc646cf712e3ea77bb4f4ece077d7
= <kamisaberi@gmail.com>
2026-03-02 20:18:57 +0330

    pau.h documented

--- include/activations/pau.h
+++ include/activations/pau.h
@@ -5,10 +5,6 @@
 namespace xt::activations
 {
     torch::Tensor pau(
-        const torch::Tensor& x,
-        const torch::Tensor& P_coeffs, // Numerator coefficients [p_m, p_{m-1}, ..., p_1, p_0]
-        const torch::Tensor& Q_coeffs, // Denominator coefficients [q_n, q_{n-1}, ..., q_1, 1.0] (q_0 is fixed to 1)
-        double epsilon = 1e-7
     );
 
 
