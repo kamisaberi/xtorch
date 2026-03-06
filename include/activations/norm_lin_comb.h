8f7c27636f17d76e1d927834accef5c81116b541
= <kamisaberi@gmail.com>
2026-03-06 20:11:40 +0330

    norm_lin_comb.h documented

--- include/activations/norm_lin_comb.h
+++ include/activations/norm_lin_comb.h
@@ -5,8 +5,6 @@
 namespace xt::activations
 {
     torch::Tensor norm_lin_comb(
-        const torch::Tensor& x,
-        const std::vector<std::function<torch::Tensor(const torch::Tensor&)>>& base_functions,
         const torch::Tensor& coefficients, // Shape (num_base_functions)
         double eps = 1e-5
     );
