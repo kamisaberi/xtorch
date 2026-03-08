a68cace4120b9115d61be9afb66fba286c247181
= <kamisaberi@gmail.com>
2026-03-08 18:48:43 +0330

    margin_relu.h documented

--- include/activations/margin_relu.h
+++ include/activations/margin_relu.h
@@ -3,7 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor margin_relu(const torch::Tensor& x, double margin_neg = 0.1, double margin_pos = 0.9) ;
 
     struct MarginReLU : xt::Module {
     public:
