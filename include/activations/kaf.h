0efba6ef405546c2b42718e5a031928c45e8cdc1
= <kamisaberi@gmail.com>
2026-03-10 18:14:52 +0330

    kaf.h documented

--- include/activations/kaf.h
+++ include/activations/kaf.h
@@ -4,10 +4,6 @@
 
 namespace xt::activations
 {
-    torch::Tensor kaf(const torch::Tensor& x,
-                      const torch::Tensor& dictionary_coefs, // Shape [D] or [1, D] for broadcasting
-                      const torch::Tensor& boundary_params // Shape [D-1] or [1, D-1] for broadcasting
-    );
 
     struct KAF : xt::Module
     {
