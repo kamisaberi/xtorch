8984c6443f83a9512809ee4b202a109aad4893f7
= <kamisaberi@gmail.com>
2026-03-10 18:15:01 +0330

    kaf.h documented

--- include/activations/kaf.h
+++ include/activations/kaf.h
@@ -8,8 +8,6 @@ namespace xt::activations
     struct KAF : xt::Module
     {
     public:
-        KAF() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
