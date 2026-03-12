3b81b6c4c6e254c8839871fd34f45d071307a4dd
= <kamisaberi@gmail.com>
2026-03-12 18:20:19 +0330

    leaf.h documented

--- include/activations/leaf.h
+++ include/activations/leaf.h
@@ -3,11 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor leaf(const torch::Tensor& x,
-                       const torch::Tensor& s_weights, // Shape (L)
-                       const torch::Tensor& r_weights, // Shape (L)
-                       const torch::Tensor& u_weights // Shape (L)
-    );
 
     struct LEAF : xt::Module {
     public:
