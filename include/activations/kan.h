50a201878dd429720cf9a503275252fa30347ca3
= <kamisaberi@gmail.com>
2026-03-17 18:17:06 +0330

    kan.h documented

--- include/activations/kan.h
+++ include/activations/kan.h
@@ -8,8 +8,6 @@ namespace xt::activations
     struct KAN : xt::Module
     {
     public:
-        KAN() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
     };
