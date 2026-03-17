3bc0e118fcef39a09e4e89210babc28c7c180ebc
= <kamisaberi@gmail.com>
2026-03-17 18:16:59 +0330

    kan.h documented

--- include/activations/kan.h
+++ include/activations/kan.h
@@ -4,15 +4,6 @@
 
 namespace xt::activations
 {
-    torch::Tensor b_spline_basis(const torch::Tensor& x, const torch::Tensor& grid, int k_order);
-    torch::Tensor kan_spline_activation(
-        const torch::Tensor& x,
-        const torch::Tensor& spline_weights, // Shape (G + k - 1)
-        const torch::Tensor& grid_internal, // Shape (G + 1) for G intervals
-        int k_order, // e.g., 4 for cubic
-        double base_activation_weight // w
-    );
-
 
     struct KAN : xt::Module
     {
