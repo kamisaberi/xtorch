13cdc02b5cc6137c4cf552a98cf222820ef3d283
= <kamisaberi@gmail.com>
2026-03-24 19:14:24 +0330

    mod_relu.h documented

--- include/activations/mod_relu.h
+++ include/activations/mod_relu.h
@@ -6,11 +6,6 @@ namespace xt::activations {
     torch::Tensor mod_relu(const torch::Tensor& x, const torch::Tensor& b);
 
     struct ModReLU : xt::Module {
-    public:
-        ModReLU() = default;
-
-
-    private:
     };
 }
 
