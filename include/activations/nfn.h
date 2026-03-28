57d662a9022887e83341381e4431e9d9c5c53dcc
= <kamisaberi@gmail.com>
2026-03-28 19:55:09 +0330

    nfn.h documented

--- include/activations/nfn.h
+++ include/activations/nfn.h
@@ -3,11 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor nfn(
-        const torch::Tensor& x,
-        const torch::Tensor& alpha, // Shape (num_filters_out, num_filters_in, filter_size)
-        const torch::Tensor& beta // Shape (num_filters_out, num_filters_in, filter_size)
-    );
 
     struct NFN : xt::Module {
     public:
