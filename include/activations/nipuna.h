e4fa0e1990ce1dd4a996fe225f12174589aae299
= <kamisaberi@gmail.com>
2026-03-30 19:57:25 +0330

    nipuna.h documented

--- include/activations/nipuna.h
+++ include/activations/nipuna.h
@@ -3,8 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor nipuna(const torch::Tensor& x, double a = 0.25, double b = 0.05);
-
     struct Nipuna : xt::Module {
     public:
 
