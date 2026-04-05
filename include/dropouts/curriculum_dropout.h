3fb01d10bb7871eaef309d6bd9db7379d07a981c
= <kamisaberi@gmail.com>
2026-04-06 02:01:53 +0330

    finally net came back

--- include/dropouts/curriculum_dropout.h
+++ include/dropouts/curriculum_dropout.h
@@ -5,7 +5,6 @@
 namespace xt::dropouts
 {
     torch::Tensor curriculum_dropout(torch::Tensor x);
-
     struct CurriculumDropout : xt::Module
     {
     public:
@@ -16,13 +15,11 @@ namespace xt::dropouts
             bool increase_rate = false // Default: decrease rate from initial to final
         );
         auto forward(std::initializer_list<std::any> tensors) -> std::any override;
-
     private:
         double initial_dropout_rate_;
         double final_dropout_rate_;
         int64_t total_curriculum_steps_; // Number of steps/epochs over which the rate changes
         bool increase_rate_; // If true, rate goes from initial to final. If false, from final to initial.
-
         // A transient member to store the most recently calculated dropout rate for pretty_print or inspection
         // Not a registered parameter or buffer.
         mutable double current_p_ = 0.0;
