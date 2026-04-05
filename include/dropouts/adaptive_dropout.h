c0ec8f690d37054dc618c599777f70d4214d2491
= <kamisaberi@gmail.com>
2026-04-06 02:01:39 +0330

    finally net came back

--- include/dropouts/adaptive_dropout.h
+++ include/dropouts/adaptive_dropout.h
@@ -9,17 +9,12 @@
 
 namespace xt::dropouts
 {
-
-
     torch::Tensor adaptive_dropout(torch::Tensor x);
-
     struct AdaptiveDropout : xt::Module
     {
     public:
         explicit AdaptiveDropout(c10::IntArrayRef probability_shape = {}, double initial_dropout_rate = 0.05);
-
         auto forward(std::initializer_list<std::any> tensors) -> std::any override;
-
     private:
         torch::Tensor log_alpha_;
     };
