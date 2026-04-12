35e943d6495bf04850cee1aedf9b3f65bc585cec
= <kamisaberi@gmail.com>
2026-04-13 00:13:44 +0330

    rational.h activation documented

--- include/activations/rational.h
+++ include/activations/rational.h
@@ -4,12 +4,6 @@
 
 namespace xt::activations
 {
-    torch::Tensor rational(
-        const torch::Tensor& x,
-        const torch::Tensor& P_coeffs, // Numerator coefficients [p_m, ..., p_1, p_0]
-        const torch::Tensor& Q_coeffs, // Denominator coefficients [q_n, ..., q_1] (q_0 is fixed to 1)
-        double epsilon = 1e-7 // For denominator stability
-    );
 
 
 }
