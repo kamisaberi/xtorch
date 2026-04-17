c20631e9f422427ece87e938c6033429d87f6089
= <kamisaberi@gmail.com>
2026-04-18 00:20:04 +0330

    reglu.h activation documented

--- include/activations/reglu.h
+++ include/activations/reglu.h
@@ -5,11 +5,6 @@
 namespace xt::activations {
     torch::Tensor reglu(const torch::Tensor& x, int64_t dim = 1);
 
-    struct ReGLU : xt::Module {
-    public:
-        ReGLU() = default;
-
-    };
 }
 
 
