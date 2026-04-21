d3e0a0696313e12e6209ec82d8fd49a3db0b8f52
= <kamisaberi@gmail.com>
2026-04-22 00:04:55 +0330

    pmish.h documented

--- include/activations/pmish.h
+++ include/activations/pmish.h
@@ -3,7 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor pmish(const torch::Tensor& x, double alpha = 1.0, double beta = 0.5);
 
     struct PMish : xt::Module {
     public:
