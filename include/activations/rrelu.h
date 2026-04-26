dc3c3e894d61b9a1360267d9088a3c678fca9637
= <kamisaberi@gmail.com>
2026-04-27 00:31:03 +0330

    rrelu.h activation documented

--- include/activations/rrelu.h
+++ include/activations/rrelu.h
@@ -4,13 +4,6 @@
 
 namespace xt::activations
 {
-    torch::Tensor rrelu(
-        const torch::Tensor& x,
-        double lower = 1.0 / 8.0,
-        double upper = 1.0 / 3.0,
-        bool training = false,
-        c10::optional<at::Generator> generator = c10::nullopt
-    );
 
 
 }
