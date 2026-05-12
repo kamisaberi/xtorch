26a135684094c9478328c9dbf40adb24110f94fd
= <kamisaberi@gmail.com>
2026-05-13 00:37:47 +0330

    serf.h activation documented

--- include/activations/serf.h
+++ include/activations/serf.h
@@ -6,9 +6,6 @@ namespace xt::activations {
     torch::Tensor serf(const torch::Tensor& x, double k_param = 2.0, double lambda_param = 1.0);
 
     struct Serf : xt::Module {
-    public:
-
-    private:
     };
 }
 
