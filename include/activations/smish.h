d1f8404a68d6088488b2957de1a8024f8bed7a19
= <kamisaberi@gmail.com>
2026-05-16 01:23:05 +0330

    smish.h activation documented

--- include/activations/smish.h
+++ include/activations/smish.h
@@ -6,10 +6,6 @@ namespace xt::activations {
     torch::Tensor smish(const torch::Tensor& x, double alpha = 1.0, double beta = 1.0);
 
     struct Smish : xt::Module {
-    public:
-
-
-    private:
     };
 }
 
