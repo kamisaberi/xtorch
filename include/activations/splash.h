bc7592e80745323934233179f842bbe07c6186bb
= <kamisaberi@gmail.com>
2026-05-18 01:36:40 +0330

    splash.h activation documented

--- include/activations/splash.h
+++ include/activations/splash.h
@@ -6,9 +6,6 @@ namespace xt::activations {
     torch::Tensor splash(const torch::Tensor& x, double S = 1.0, double R = 0.5, double B = 1.0);
 
     struct SPLASH : xt::Module {
-    public:
-
-    private:
     };
 }
 
