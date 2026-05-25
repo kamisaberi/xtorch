bdbfc0f4e4d5ef322116d08bb0f152a5bb3e6d0c
= <kamisaberi@gmail.com>
2026-05-26 00:52:57 +0330

    serlu.h activation documented

--- include/activations/serlu.h
+++ include/activations/serlu.h
@@ -6,7 +6,4 @@ namespace xt::activations
 {
     torch::Tensor serlu(const torch::Tensor& x, double lambda_serlu = 1.0507, double alpha_serlu = 1.67326);
 
-    struct SERLU : xt::Module
-    {
-    };
 }
