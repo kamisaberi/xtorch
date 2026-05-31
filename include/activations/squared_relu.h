45e3ab112c201724ec5c0d53ebaccfe45c4e67cc
= <kamisaberi@gmail.com>
2026-06-01 01:42:00 +0330

    squared_relu.h activation documented

--- include/activations/squared_relu.h
+++ include/activations/squared_relu.h
@@ -8,7 +8,6 @@ namespace xt::activations {
     struct SquaredReLU : xt::Module {
     public:
         SquaredReLU() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
 
     private:
