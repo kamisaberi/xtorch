5e6c06450142063f4dd93409eb3c1c280ef67520
= <kamisaberi@gmail.com>
2026-06-11 00:57:30 +0330

    shifted_soft_plus.h activation documented

--- include/activations/shifted_softplus.h
+++ include/activations/shifted_softplus.h
@@ -4,7 +4,6 @@
 
 namespace xt::activations
 {
-    const double LN_2 = std::log(2.0); // More portable way to get log(2)
 
     torch::Tensor shifted_softplus(const torch::Tensor& x, double shift_val = LN_2);
 
