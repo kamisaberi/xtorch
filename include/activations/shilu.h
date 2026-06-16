609558d08b807a270db90f0998cd6ea433dd3701
= <kamisaberi@gmail.com>
2026-06-17 01:02:18 +0330

    shilu.h activation documented

--- include/activations/shilu.h
+++ include/activations/shilu.h
@@ -6,9 +6,6 @@ namespace xt::activations {
     torch::Tensor shilu(const torch::Tensor& x, double a = 1.0, double b = 0.0);
 
     struct ShiLU : xt::Module {
-    public:
-
-    private:
     };
 }
 
