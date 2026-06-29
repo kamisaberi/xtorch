8843c92b5c35414a14895ae09494463ab722cfa9
= <kamisaberi@gmail.com>
2026-06-29 17:37:18 +0330

    hard_elish.h documented

--- include/activations/hard_elish.h
+++ include/activations/hard_elish.h
@@ -3,15 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor hard_elish(torch::Tensor& x);
-
-    struct HardELiSH : xt::Module {
-    public:
-        HardELiSH() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-
-    private:
-    };
 }
 
 
