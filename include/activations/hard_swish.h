3e669703cfe93b086247d3b9252840c99b99a17e
= <kamisaberi@gmail.com>
2026-06-30 17:51:31 +0330

    hard_swish.h documented

--- include/activations/hard_swish.h
+++ include/activations/hard_swish.h
@@ -3,15 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor hard_swich(torch::Tensor x);
-
-    struct HardSwish : xt::Module {
-    public:
-        HardSwish() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-
-    private:
-    };
 }
 
 
