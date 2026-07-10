e4a4a91aaa3625df0b8d1a2b77baa26aa25c40af
= <kamisaberi@gmail.com>
2026-07-10 17:14:29 +0330

    gcu.h documented

--- include/activations/gcu.h
+++ include/activations/gcu.h
@@ -5,13 +5,6 @@
 namespace xt::activations {
     torch::Tensor gcu(torch::Tensor x);
 
-    struct GCU : xt::Module {
-    public:
-        GCU() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-
-    private:
-    };
 }
 
 
