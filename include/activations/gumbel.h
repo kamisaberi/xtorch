e9d715dcb764c52184d2d6c1f95ab54687b0069e
= <kamisaberi@gmail.com>
2026-07-11 17:34:52 +0330

    golu.h documented

--- include/activations/gumbel.h
+++ include/activations/gumbel.h
@@ -3,16 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor gumbel(torch::Tensor x, double beta = 1.0);
-
-    struct Gumbel : xt::Module {
-    public:
-        Gumbel() = default;
-
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-
-    private:
-    };
 }
 
 
