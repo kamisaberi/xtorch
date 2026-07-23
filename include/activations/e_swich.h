3415859471c77d1d4f1caeb0b399f074303c28c8
= <kamisaberi@gmail.com>
2026-07-24 02:06:02 +0330

    e_switch.h documented

--- include/activations/e_swich.h
+++ include/activations/e_swich.h
@@ -6,10 +6,6 @@ namespace xt::activations {
     torch::Tensor e_swish(torch::Tensor x, double beta = 1.25);
 
     struct ESwish : xt::Module {
-    public:
-        ESwish() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-    private:
     };
 }
 
