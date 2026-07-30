d3d840fa9ab4ee1898aa9affca45cf17ec2faa09
= <kamisaberi@gmail.com>
2026-07-31 02:01:26 +0330

    delu.h documented

--- include/activations/delu.h
+++ include/activations/delu.h
@@ -1,20 +1,5 @@
 #pragma once
 
-#include "common.h"
-
-namespace xt::activations
-{
-    torch::Tensor delu(torch::Tensor x, double alpha = 1.0, double gamma = 1.0);
-    struct DELU : xt::Module
-    {
-    public:
-        DELU() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-    private:
-
-
-    };
-}
 
 
 
