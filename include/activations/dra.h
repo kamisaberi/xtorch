bb307a64c07eac63de22d5e3c9b2f739747e28eb
= <kamisaberi@gmail.com>
2026-07-31 02:03:00 +0330

    dra.h documented

--- include/activations/dra.h
+++ include/activations/dra.h
@@ -3,14 +3,6 @@
 #include "common.h"
 
 namespace xt::activations {
-    torch::Tensor dra(torch::Tensor x, double alpha = 1.0);
-
-    struct DRA : xt::Module {
-    public:
-        DRA() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
-    private:
-    };
 }
 
 
