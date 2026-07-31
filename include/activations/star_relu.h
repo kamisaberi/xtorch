254dfa75e71db78326567446e4784143e029fc66
= <kamisaberi@gmail.com>
2026-08-01 01:51:13 +0330

    star_relu.h activation documented

--- include/activations/star_relu.h
+++ include/activations/star_relu.h
@@ -4,7 +4,7 @@
 
 namespace xt::activations {
     torch::Tensor star_relu(const torch::Tensor& x, double scale = 1.0, double bias = 0.0, double relu_slope = 1.0,
-                        double leaky_slope = 0.01);
+                        double leaky_slope = 0.02);
 
 
 }
