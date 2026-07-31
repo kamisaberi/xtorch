9d634d6f2e80b9a62c551a52782720f56f8bdcdf
= <kamisaberi@gmail.com>
2026-08-01 01:54:18 +0330

    swish.h activation documented

--- include/activations/swish.h
+++ include/activations/swish.h
@@ -10,7 +10,6 @@ namespace xt::activations
     {
     public:
         Swish() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
