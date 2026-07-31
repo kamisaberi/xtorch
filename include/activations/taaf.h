56007806795008fb05b9a4795401dc4a78efad57
= <kamisaberi@gmail.com>
2026-08-01 01:55:51 +0330

    taaf.h activation documented

--- include/activations/taaf.h
+++ include/activations/taaf.h
@@ -4,11 +4,6 @@
 
 namespace xt::activations
 {
-    torch::Tensor taaf(
-        const torch::Tensor& x,
-        const torch::Tensor& alpha, // Per-channel learnable parameter
-        double beta = 0.0 // Global hyperparameter or fixed
-    );
 
 
     struct TAAF : xt::Module
