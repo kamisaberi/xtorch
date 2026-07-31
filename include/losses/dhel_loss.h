cffb31c89962f445de829f2e730f2c341403d2c9
= <kamisaberi@gmail.com>
2026-08-01 03:29:18 +0330

    dhel_loss.h loss documented

--- include/losses/dhel_loss.h
+++ include/losses/dhel_loss.h
@@ -9,8 +9,6 @@ namespace xt::losses
     class DHELLoss : xt::Module
     {
     public:
-        DHELLoss() = default;
-        auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
     private:
     };
