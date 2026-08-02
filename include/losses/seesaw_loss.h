1e85c8f4cde1e95a17ceb29a928cce37f528b857
= <kamisaberi@gmail.com>
2026-08-03 00:46:36 +0330

    seasaw_loss.h loss documented

--- include/losses/seesaw_loss.h
+++ include/losses/seesaw_loss.h
@@ -2,14 +2,4 @@
 #include "common.h"
 
 
-namespace xt::losses
-{
-    torch::Tensor seesaw_loss(torch::Tensor x);
-    class SeesawLoss : xt::Module
-    {
-    public:
-
-
-    private:
-    };
 }
