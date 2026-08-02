a99c9daf964df84f9a94febc302dd486232bfe8a
= <kamisaberi@gmail.com>
2026-08-03 01:30:23 +0330

    zlpr_loss.h loss documented

--- include/losses/zlpr_loss.h
+++ include/losses/zlpr_loss.h
@@ -8,7 +8,6 @@ namespace xt::losses
     class ZLPRLoss : xt::Module
     {
     public:
-        ZLPRLoss() = default;
         auto forward(std::initializer_list<std::any> tensors) -> std::any  override;
 
 
