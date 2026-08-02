5754e78b2c874755e519fc707c742f86731f4931
= <kamisaberi@gmail.com>
2026-08-03 00:49:07 +0330

    self_adjusting_smooth_l1__loss.h loss documented

--- include/losses/self_adjusting_smooth_l1_loss.h
+++ include/losses/self_adjusting_smooth_l1_loss.h
@@ -4,12 +4,4 @@
 
 namespace xt::losses
 {
-    torch::Tensor self_adjusting_smooth_l1_loss(torch::Tensor x);
-    class SelfAdjustingSmoothL1Loss : xt::Module
-    {
-    public:
-
-
-    private:
-    };
 }
