a6a7ac9d232683dc4f129279611900d1a55dcef9
= <kamisaberi@gmail.com>
2026-08-03 12:51:09 +0330

    attention_dropout.h dropout documented

--- include/dropouts/auto_dropout.h
+++ include/dropouts/auto_dropout.h
@@ -10,7 +10,6 @@ namespace xt::dropouts
     {
     public:
         explicit AutoDropout(c10::IntArrayRef probability_shape = {}, double initial_dropout_rate = 0.05);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
         torch::Tensor log_alpha_;
