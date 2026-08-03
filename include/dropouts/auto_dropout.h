be135b705c1d8f88baec7fc6a6da2ae41cb5bb8b
= <kamisaberi@gmail.com>
2026-08-03 12:51:15 +0330

    attention_dropout.h dropout documented

--- include/dropouts/auto_dropout.h
+++ include/dropouts/auto_dropout.h
@@ -9,7 +9,6 @@ namespace xt::dropouts
     struct AutoDropout : xt::Module
     {
     public:
-        explicit AutoDropout(c10::IntArrayRef probability_shape = {}, double initial_dropout_rate = 0.05);
 
     private:
         torch::Tensor log_alpha_;
