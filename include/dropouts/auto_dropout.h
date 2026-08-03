37b215e6deff9240a7b477dd4dffffac48671061
= <kamisaberi@gmail.com>
2026-08-03 12:51:22 +0330

    attention_dropout.h dropout documented

--- include/dropouts/auto_dropout.h
+++ include/dropouts/auto_dropout.h
@@ -2,15 +2,3 @@
 
 #include "common.h"
 
-namespace xt::dropouts
-{
-    torch::Tensor auto_dropout(torch::Tensor x);
-
-    struct AutoDropout : xt::Module
-    {
-    public:
-
-    private:
-        torch::Tensor log_alpha_;
-    };
-}
