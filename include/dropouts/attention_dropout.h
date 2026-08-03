d3423577e7a341c6511c01327971683b74c9a1e7
= <kamisaberi@gmail.com>
2026-08-03 12:50:13 +0330

    attention_dropout.h dropout documented

--- include/dropouts/attention_dropout.h
+++ include/dropouts/attention_dropout.h
@@ -7,8 +7,6 @@ namespace xt::dropouts
     struct AttentionDropout : xt::Module
     {
     public:
-        AttentionDropout(double p = 0.1);
-        auto forward(std::initializer_list<std::any> tensors) -> std::any override;
 
     private:
         double p_; // Probability of an element to be zeroed.
